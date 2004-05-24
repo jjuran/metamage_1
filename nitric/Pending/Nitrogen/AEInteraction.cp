b978cb94af4dee6281487347e57011b29c9fa8ca
Joshua Juran <jjuran@metamage.com>
2004-05-24 21:09:32 +0000

    Added AESend(), AEProcessAppleEvent(), and required types.

--- nitric/Pending/Nitrogen/AEInteraction.cp
+++ nitric/Pending/Nitrogen/AEInteraction.cp
@@ -4,3 +4,35 @@
 #include "Nitrogen/AEInteraction.h"
 #endif
 
+namespace Nitrogen {
+	
+	Owned<AppleEvent> AESend(
+		const AppleEvent& appleEvent, 
+		AESendMode sendMode, 
+		AESendPriority sendPriority, 
+		long timeOutInTicks, 
+		AEIdleUPP idleProc, 
+		AEFilterUPP filterProc)
+	{
+		AEDesc reply;
+		ThrowOSStatus(
+			::AESend(
+				&appleEvent, 
+				&reply, 
+				sendMode, 
+				sendPriority, 
+				timeOutInTicks, 
+				idleProc, 
+				filterProc
+			)
+		);
+		return Owned<AppleEvent>::Seize( reply );
+	}
+	
+	void AEProcessAppleEvent( const EventRecord& event )
+	{
+		ThrowOSStatus( ::AEProcessAppleEvent( &event ) );
+	}
+	
+}
+
--- nitric/Pending/Nitrogen/AEInteraction.h
+++ nitric/Pending/Nitrogen/AEInteraction.h
@@ -12,9 +12,84 @@
 #ifndef NITROGEN_AEDATAMODEL_H
 #include "Nitrogen/AEDataModel.h"
 #endif
+#ifndef NITROGEN_FLAGTYPE_H
+#include "Nitrogen/FlagType.h"
+#endif
 
-namespace Nitrogen
-  {
-  }
+namespace Nitrogen {
+	
+	struct AESendModeTag  {};
+	typedef FlagType< AESendModeTag, ::AESendMode, 0 > AESendMode;
+	
+	struct AESendPriorityTag  {};
+	typedef FlagType< AESendPriorityTag, ::AESendPriority, 0 > AESendPriority;
+	
+	struct AEIdleUPP_Details : Basic_UPP_Details< ::AEIdleUPP,
+	                                              ::AEIdleProcPtr,
+	                                              ::NewAEIdleUPP,
+	                                              ::DisposeAEIdleUPP,
+	                                              ::InvokeAEIdleUPP >
+	{};
+	
+	typedef UPP< AEIdleUPP_Details > AEIdleUPP;
+	
+	struct AEFilterUPP_Details : Basic_UPP_Details< ::AEFilterUPP,
+	                                              ::AEFilterProcPtr,
+	                                              ::NewAEFilterUPP,
+	                                              ::DisposeAEFilterUPP,
+	                                              ::InvokeAEFilterUPP >
+	{};
+	
+	typedef UPP< AEFilterUPP_Details > AEFilterUPP;
+	
+	using ::AEIdleProcPtr;
+	
+	Owned<AppleEvent> AESend(
+		const AppleEvent& appleEvent, 
+		AESendMode sendMode, 
+		AESendPriority sendPriority = kAENormalPriority, 
+		long timeOutInTicks = kAEDefaultTimeout, 
+		AEIdleUPP idleProc = NULL, 
+		AEFilterUPP filterProc = NULL
+	);
+	
+	void AEProcessAppleEvent( const EventRecord& event );
+	
+	inline Owned<AEIdleUPP> NewAEIdleUPP( AEIdleProcPtr p )
+	{
+			return NewUPP<AEIdleUPP>( p );
+	}
+
+	inline void DisposeAEIdleUPP( Owned<AEIdleUPP> )
+	{
+	}
+	
+	inline bool InvokeAEIdleUPP( EventRecord& theEvent,
+	                             long& sleepTime,
+	                             RgnHandle& mouseRgn,
+	                             AEIdleUPP userUPP )
+	{
+		return userUPP( &theEvent, &sleepTime, &mouseRgn );
+	}
+
+	inline Owned<AEFilterUPP> NewAEFilterUPP( AEFilterProcPtr p )
+	{
+			return NewUPP<AEFilterUPP>( p );
+	}
+
+	inline void DisposeAEFilterUPP( Owned<AEFilterUPP> )
+	{
+	}
+	
+	inline bool InvokeAEFilterUPP( EventRecord& theEvent,
+	                               long returnID,
+	                               long transactionID,
+	                               const AEAddressDesc& sender,
+	                               AEFilterUPP userUPP )
+	{
+		return userUPP( &theEvent, returnID, transactionID, &sender );
+	}
+	
+}
 
 #endif
