138de58f4f6cf042f1a52d079cdea4c310983f1e
Joshua Juran <jjuran@metamage.com>
2004-05-25 03:27:06 +0000

    Added AEResolve() and required types.

--- nitric/Pending/Nitrogen/AEObjects.cp
+++ nitric/Pending/Nitrogen/AEObjects.cp
@@ -4,3 +4,33 @@
 #include "Nitrogen/AEObjects.h"
 #endif
 
+namespace Nitrogen {
+	
+	class AETokenEditor
+	{
+		private:
+			Owned<AEDesc, TokenDisposer>& desc;
+			AEDesc workingCopy;
+		public:
+			AETokenEditor( Owned<AEDesc, TokenDisposer>& desc ) : desc(desc), workingCopy(desc.Release())  {}
+			~AETokenEditor()  { desc = Owned<AEDesc, TokenDisposer>::Seize(workingCopy); }
+			
+			AEDesc& Get()  { return workingCopy; }
+			operator AEDesc&()  { return Get(); }
+	};
+	
+	void AEObjectInit()
+	{
+		ThrowOSStatus( ::AEObjectInit() );
+	}
+	
+	Owned<AEDesc, TokenDisposer>
+	AEResolve( const AEDesc& objectSpecifier, AEResolveCallbackFlags callbackFlags )
+	{
+		AEDesc token;
+		ThrowOSStatus( ::AEResolve( &objectSpecifier, callbackFlags, &token ) );
+		return Owned<AEDesc, TokenDisposer>::Seize( token );
+	}
+	
+}
+
--- nitric/Pending/Nitrogen/AEObjects.h
+++ nitric/Pending/Nitrogen/AEObjects.h
@@ -12,9 +12,34 @@
 #ifndef NITROGEN_AEDATAMODEL_H
 #include "Nitrogen/AEDataModel.h"
 #endif
+#ifndef NITROGEN_FLAGTYPE_H
+#include "Nitrogen/FlagType.h"
+#endif
 
-namespace Nitrogen
-  {
-  }
+namespace Nitrogen {
+	
+	struct TokenDisposer : public std::unary_function< AEDesc, void >
+	{
+		// parameter can't be const
+		void operator()( AEDesc token ) const
+		{
+			::AEDisposeToken( &token );
+		}
+	};
+	
+	template <>
+	struct LivelinessTraits< AEDesc, TokenDisposer >   { typedef SeizedValuesAreLive LivelinessTest; };
+	
+	struct AEResolveCallbackFlagsTag  {};
+	typedef FlagType< AEResolveCallbackFlagsTag, short, kAEIDoMinimum > AEResolveCallbackFlags;
+	
+	void AEObjectInit();
+	
+	Owned<AEDesc, TokenDisposer>
+	AEResolve( const AEDesc& objectSpecifier, AEResolveCallbackFlags callbackFlags );
+	
+	inline void AEDisposeToken( Owned<AEDesc, TokenDisposer> )  {}
+	
+}
 
 #endif
