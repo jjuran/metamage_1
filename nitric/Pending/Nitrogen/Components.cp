ce59cd86df38c1b8695fcfe2ab39cccd55e09c7c
Joshua Juran <jjuran@metamage.com>
2004-05-25 17:11:00 +0000

    Added OpenDefaultComponent() and types.

--- nitric/Pending/Nitrogen/Components.cp
+++ nitric/Pending/Nitrogen/Components.cp
@@ -4,3 +4,18 @@
 #include "Nitrogen/Components.h"
 #endif
 
+namespace Nitrogen {
+	
+	Owned< ComponentInstance >
+	OpenDefaultComponent( ComponentType componentType, ComponentSubType componentSubType )
+	{
+		ComponentInstance component = ::OpenDefaultComponent( componentType, componentSubType );
+		if ( component == NULL )
+		{
+			throw ErrorCode< OSStatus, ::cantFindHandler >();
+		}
+		return Owned< ComponentInstance >::Seize( component );
+	}
+	
+}
+
--- nitric/Pending/Nitrogen/Components.h
+++ nitric/Pending/Nitrogen/Components.h
@@ -10,9 +10,51 @@
 #include FRAMEWORK_HEADER(CarbonCore,Components.h)
 #endif
 
-namespace Nitrogen
-  {
-  }
+#ifndef NITROGEN_OWNED_H
+#include "Nitrogen/Owned.h"
+#endif
+#ifndef NITROGEN_SELECTORTYPE_H
+#include "Nitrogen/SelectorType.h"
+#endif
+#ifndef NITROGEN_ONLYONCE_H
+#include "Nitrogen/OnlyOnce.h"
+#endif
+#ifndef NITROGEN_OSSTATUS_H
+#include "Nitrogen/OSStatus.h"
+#endif
+
+namespace Nitrogen {
+	
+	void RegisterComponentManagerErrors();
+	
+	struct ComponentTypeTag  {};
+	typedef SelectorType< ComponentTypeTag, ::OSType, kAnyComponentType > ComponentType;
+	
+	struct ComponentSubTypeTag  {};
+	typedef SelectorType< ComponentSubTypeTag, ::OSType, kAnyComponentSubType > ComponentSubType;
+	
+	struct ComponentManufacturerTag  {};
+	typedef SelectorType< ComponentManufacturerTag, ::OSType, kAnyComponentManufacturer > ComponentManufacturer;
+	
+	using ::ComponentInstance;
+	
+	template <>
+	struct Disposer< ComponentInstance > : public std::unary_function< ComponentInstance, void >,
+	                                       private DefaultDestructionOSStatusPolicy
+	{
+		void operator()( ComponentInstance component ) const
+		{
+			OnlyOnce< RegisterComponentManagerErrors >();
+			DefaultDestructionOSStatusPolicy::HandleDestructionOSStatus( ::CloseComponent( component ) );
+		}
+	};
+	
+	Owned< ComponentInstance >
+	OpenDefaultComponent( ComponentType componentType, ComponentSubType componentSubType );
+	
+	inline void CloseComponent( Owned< ComponentInstance > )  {}
+	
+}
 
 #endif
 
