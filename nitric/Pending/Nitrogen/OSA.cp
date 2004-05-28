d5ff4930ec36a4a128432b4bef092e75e7f86c54
Joshua Juran <jjuran@metamage.com>
2004-05-28 00:27:03 +0000

    Checked in basic OSA support.

--- nitric/Pending/Nitrogen/OSA.cp
+++ nitric/Pending/Nitrogen/OSA.cp
@@ -4,3 +4,133 @@
 #include "Nitrogen/OSA.h"
 #endif
 
+#ifndef NITROGEN_ONLYONCE_H
+#include "Nitrogen/OnlyOnce.h"
+#endif
+
+namespace Nitrogen {
+	
+	void RegisterOSAErrors()
+	{
+		RegisterOSStatus< badComponentInstance       >();
+		RegisterOSStatus< errOSASystemError          >();
+		RegisterOSStatus< errOSAInvalidID            >();
+		RegisterOSStatus< errOSABadStorageType       >();
+		RegisterOSStatus< errOSAScriptError          >();
+		RegisterOSStatus< errOSABadSelector          >();
+		RegisterOSStatus< errOSASourceNotAvailable   >();
+		RegisterOSStatus< errOSANoSuchDialect        >();
+		RegisterOSStatus< errOSADataFormatObsolete   >();
+		RegisterOSStatus< errOSADataFormatTooNew     >();
+		RegisterOSStatus< errOSACorruptData          >();
+		RegisterOSStatus< errOSARecordingIsAlreadyOn >();
+		RegisterOSStatus< errOSAComponentMismatch    >();
+		RegisterOSStatus< errOSACantOpenComponent    >();
+	}
+	
+	Owned< ManagedOSAID >
+	OSALoad(
+		Shared< ComponentInstance > scriptingComponent, 
+		const AEDesc& scriptData, 
+		OSAModeFlags modeFlags)
+	{
+		OnlyOnce< RegisterOSAErrors >();
+		
+		::OSAID resultingScriptID;
+		ThrowOSStatus(
+			::OSALoad( scriptingComponent, &scriptData, modeFlags, &resultingScriptID )
+		);
+		
+		return Owned< ManagedOSAID >::Seize(
+			ManagedOSAID( scriptingComponent, OSAID::Make( resultingScriptID ) )
+		);
+	}
+	
+	Owned< AEDesc >
+	OSAStore(
+		ComponentInstance scriptingComponent, 
+		OSAID scriptID, 
+		DescType desiredType, 
+		OSAModeFlags modeFlags)
+	{
+		OnlyOnce< RegisterOSAErrors >();
+		
+		AEDesc resultingScriptData;
+		ThrowOSStatus(
+			::OSAStore( scriptingComponent, scriptID, desiredType, modeFlags, &resultingScriptData )
+		);
+		
+		return Owned< AEDesc >::Seize( resultingScriptData );
+	}
+	
+	Owned< ManagedOSAID >
+	OSAExecute(
+		Shared< ComponentInstance > scriptingComponent, 
+		OSAID compiledScriptID, 
+		OSAID contextID, 
+		OSAModeFlags modeFlags)
+	{
+		OnlyOnce< RegisterOSAErrors >();
+		
+		::OSAID resultingScriptValueID;
+		ThrowOSStatus(
+			::OSAExecute( scriptingComponent, compiledScriptID, contextID, modeFlags, &resultingScriptValueID )
+		);
+		
+		return Owned< ManagedOSAID >::Seize(
+			ManagedOSAID( scriptingComponent, OSAID::Make( resultingScriptValueID ) )
+		);
+	}
+	
+	Owned< AEDesc >
+	OSADisplay(
+		ComponentInstance scriptingComponent, 
+		OSAID scriptValueID, 
+		DescType desiredType, 
+		OSAModeFlags modeFlags)
+	{
+		OnlyOnce< RegisterOSAErrors >();
+		
+		AEDesc resultingText;
+		ThrowOSStatus(
+			::OSADisplay(
+				scriptingComponent, 
+				scriptValueID, 
+				desiredType, 
+				modeFlags, 
+				&resultingText
+			)
+		);
+		
+		return Owned< AEDesc >::Seize( resultingText );
+	}
+	
+	Owned< ManagedOSAID >
+	OSACompile(
+		Shared< ComponentInstance > scriptingComponent, 
+		const AEDesc& sourceData, 
+		OSAModeFlags modeFlags, 
+		Owned< ManagedOSAID > previousScriptID)
+	{
+		OnlyOnce< RegisterOSAErrors >();
+		
+		::OSAID previousAndResultingScriptID = previousScriptID.Release().id;
+		
+		OSAError err = ::OSACompile(
+			scriptingComponent, &sourceData, modeFlags, &previousAndResultingScriptID
+		);
+		
+		Owned< ManagedOSAID > resultingScriptID = Owned< ManagedOSAID >::Seize(
+			ManagedOSAID( scriptingComponent, OSAID::Make( previousAndResultingScriptID ) )
+		);
+		if ( err != noErr )
+		{
+			// resultingScriptID gets lost (but not leaked)
+		}
+		ThrowOSStatus( err );
+		
+		return resultingScriptID;
+	}
+	
+}
+
--- nitric/Pending/Nitrogen/OSA.h
+++ nitric/Pending/Nitrogen/OSA.h
@@ -10,9 +10,150 @@
 #include FRAMEWORK_HEADER(OpenScripting,OSA.h)
 #endif
 
-namespace Nitrogen
-  {
-  }
+#ifndef NITROGEN_AEDATAMODEL_H
+#include "Nitrogen/AEDataModel.h"
+#endif
+#ifndef NITROGEN_COMPONENTS_H
+#include "Nitrogen/Components.h"
+#endif
+#ifndef NITROGEN_FLAGTYPE_H
+#include "Nitrogen/FlagType.h"
+#endif
+#ifndef NITROGEN_IDTYPE_H
+#include "Nitrogen/IDType.h"
+#endif
+#ifndef NITROGEN_SHARED_H
+#include "Nitrogen/Shared.h"
+#endif
+
+namespace Nitrogen {
+	
+	void RegisterOSAErrors();
+	
+	struct OSAIDTag  {};
+	typedef IDType< OSAIDTag, ::OSAID, ::kOSANullScript > OSAID;
+	
+	struct OSAModeFlagsTag;
+	typedef FlagType< OSAModeFlagsTag, long, ::kOSAModeNull > OSAModeFlags;
+	
+	static const ComponentType    kOSAComponentType                    = ComponentType   ::Make( ::kOSAComponentType );
+	static const ComponentSubType kOSAGenericScriptingComponentSubtype = ComponentSubType::Make( ::kOSAGenericScriptingComponentSubtype );
+	
+	using ::kOSAFileType;
+	using ::kOSASuite;
+	
+	static const OSAID kOSANullScript = OSAID::Make( ::kOSANullScript );
+	
+	static const OSAModeFlags kOSAModeNull = OSAModeFlags::Make( ::kOSAModeNull );
+	
+	static const ResType kOSAScriptResourceType = ResType::Make( ::kOSAScriptResourceType );
+	
+	static const DescType typeOSAGenericStorage = DescType::Make( ::typeOSAGenericStorage );
+	
+	struct ManagedOSAID 
+	{
+		ManagedOSAID() : component(), id()  {}
+		ManagedOSAID( Shared< ComponentInstance > component, OSAID id ) 
+		  : component( component ), id( id )  {}
+		
+		operator OSAID() const  { return id; }
+		
+		Shared< ComponentInstance > component;
+		OSAID id;
+	};
+	
+	template <>
+	struct Disposer< ManagedOSAID > : public std::unary_function< ManagedOSAID, void >
+	{
+		void operator()( const ManagedOSAID& id ) const
+		{
+			::OSADispose( id.component, id.id );
+		}
+	};
+	
+	Owned< ManagedOSAID >
+	OSALoad(
+		Shared< ComponentInstance > scriptingComponent, 
+		const AEDesc& scriptData, 
+		OSAModeFlags modeFlags = kOSAModeNull
+	);
+	
+	Owned< AEDesc >
+	OSAStore(
+		ComponentInstance scriptingComponent, 
+		OSAID scriptID, 
+		DescType desiredType = typeOSAGenericStorage, 
+		OSAModeFlags modeFlags = kOSAModeNull
+	);
+	
+	inline
+	Owned< AEDesc >
+	OSAStore(
+		const ManagedOSAID& script, 
+		DescType desiredType = typeOSAGenericStorage, 
+		OSAModeFlags modeFlags = kOSAModeNull)
+	{
+		return OSAStore( script.component, script.id, desiredType, modeFlags );
+	}
+	
+	Owned< ManagedOSAID >
+	OSAExecute(
+		Shared< ComponentInstance > scriptingComponent, 
+		OSAID compiledScriptID, 
+		OSAID contextID = kOSANullScript, 
+		OSAModeFlags modeFlags = kOSAModeNull
+	);
+	
+	inline
+	Owned< ManagedOSAID >
+	OSAExecute(
+		const ManagedOSAID& script, 
+		OSAID contextID = kOSANullScript, 
+		OSAModeFlags modeFlags = kOSAModeNull)
+	{
+		return OSAExecute( script.component, script.id, contextID, modeFlags );
+	}
+	
+	Owned< AEDesc >
+	OSADisplay(
+		ComponentInstance scriptingComponent, 
+		OSAID scriptValueID, 
+		DescType desiredType = typeChar, 
+		OSAModeFlags modeFlags = kOSAModeNull
+	);
+	
+	inline
+	Owned< AEDesc >
+	OSADisplay(
+		const ManagedOSAID& scriptValue, 
+		DescType desiredType = typeChar, 
+		OSAModeFlags modeFlags = kOSAModeNull)
+	{
+		return OSADisplay( scriptValue.component, scriptValue.id, desiredType, modeFlags );
+	}
+	
+	inline void OSADispose( Owned< ManagedOSAID > )  {}
+	
+	Owned< ManagedOSAID >
+	OSACompile(
+		Shared< ComponentInstance > scriptingComponent, 
+		const AEDesc& sourceData, 
+		OSAModeFlags modeFlags = kOSAModeNull, 
+		Owned< ManagedOSAID > previousScriptID = Owned< ManagedOSAID >()
+	);
+	
+	
+	inline
+	Owned<ComponentInstance>
+	OpenGenericScriptingComponent()
+	{
+		return OpenDefaultComponent(
+			kOSAComponentType, 
+			kOSAGenericScriptingComponentSubtype
+		);
+	}
+	
+}
 
 #endif
 
