67ce7b351990c2fd916082e8b88a0214b4f138f2
Joshua Juran <jjuran@metamage.com>
2004-05-29 20:03:40 +0000

    Existing code plus Process_Container added.

--- nitric/Pending/Nitrogen/Processes.cp
+++ nitric/Pending/Nitrogen/Processes.cp
@@ -1,6 +1,100 @@
 // Processes.cp
 
+#ifndef NITROGEN_ONLYONCE_H
+#include "Nitrogen/OnlyOnce.h"
+#endif
+#ifndef NITROGEN_OSSTATUS_H
+#include "Nitrogen/OSStatus.h"
+#endif
 #ifndef NITROGEN_PROCESSES_H
 #include "Nitrogen/Processes.h"
 #endif
+
+namespace Nitrogen {
+	
+	void RegisterProcessManagerErrors()
+	{
+		RegisterOSStatus< paramErr      >();
+		RegisterOSStatus< memFullErr    >();
+		RegisterOSStatus< resNotFound   >();
+		RegisterOSStatus< procNotFound  >();
+		RegisterOSStatus< memFragErr    >();
+		RegisterOSStatus< appModeErr    >();
+		RegisterOSStatus< appMemFullErr >();
+		RegisterOSStatus< appIsDaemon   >();
+	}
+	
+	ProcessSerialNumber GetCurrentProcess()
+	{
+		OnlyOnce< RegisterProcessManagerErrors >();
+		
+		ProcessSerialNumber psn;
+		ThrowOSStatus( ::GetCurrentProcess( &psn ) );
+		return psn;
+	}
+	
+	ProcessSerialNumber GetFrontProcess()
+	{
+		OnlyOnce< RegisterProcessManagerErrors >();
+		
+		ProcessSerialNumber psn;
+		ThrowOSStatus( ::GetFrontProcess( &psn ) );
+		return psn;
+	}
+	
+	bool SameProcess(
+		const ProcessSerialNumber& one, 
+		const ProcessSerialNumber& other)
+	{
+		OnlyOnce< RegisterProcessManagerErrors >();
+		
+		::Boolean same;
+		ThrowOSStatus( ::SameProcess( &one, &other, &same ) );
+		return same;
+	}
+	
+	void SetFrontProcess( const ProcessSerialNumber& psn )
+	{
+		OnlyOnce< RegisterProcessManagerErrors >();
+		
+		ThrowOSStatus( ::SetFrontProcess( &psn ) );
+	}
+	
+	ProcessSerialNumber LaunchApplication( const FSSpec& file )
+	{
+		OnlyOnce< RegisterProcessManagerErrors >();
+		
+		OSErr 					err;
+		LaunchParamBlockRec 	pb;
+		
+		pb.reserved1			= 0;
+		pb.reserved2			= 0;
+		pb.launchBlockID 		= extendedBlock;
+		pb.launchEPBLength 		= extendedBlockLen;
+		pb.launchFileFlags 		= 0;
+		pb.launchControlFlags	= launchContinue | launchNoFileFlags | launchDontSwitch;
+		pb.launchAppSpec 		= const_cast< FSSpec* >( &file );
+		pb.launchAppParameters	= NULL;
+		
+		ThrowOSStatus( ::LaunchApplication( &pb ) );
+		
+		return pb.launchProcessSN;
+	}
+	
+	ProcessSerialNumber GetNextProcess( ProcessSerialNumber process )
+	{
+		OnlyOnce< RegisterProcessManagerErrors >();
+		
+		ThrowOSStatus( ::GetNextProcess( &process ) );
+		return process;
+	}
+	
+	void GetProcessInformation( const ProcessSerialNumber& process, ProcessInfoRec& info)
+	{
+		OnlyOnce< RegisterProcessManagerErrors >();
+		
+		ThrowOSStatus( ::GetProcessInformation( &process, &info ) );
+	}
+	
+}
 
--- nitric/Pending/Nitrogen/Processes.h
+++ nitric/Pending/Nitrogen/Processes.h
@@ -9,24 +9,147 @@
 #ifndef __PROCESSES__
 #include FRAMEWORK_HEADER(HIServices,Processes.h)
 #endif
+
+#ifndef NITROGEN_CFSTRING_H
+//#include "Nitrogen/CFString.h"
+#endif
+#ifndef NITROGEN_EVENTS_H
+//#include "Nitrogen/Events.h"
+#endif
+#ifndef NITROGEN_FILES_H
+//#include "Nitrogen/Files.h"
+#endif
 #ifndef NITROGEN_MACTYPES_H
 #include "Nitrogen/MacTypes.h"
 #endif
-#ifndef NITROGEN_EVENTS_H
-#include "Nitrogen/Events.h"
-#endif
-#ifndef NITROGEN_FILES_H
-#include "Nitrogen/Files.h"
+#ifndef NITROGEN_MAKE_H
+#include "Nitrogen/Make.h"
 #endif
 #ifndef NITROGEN_TEXTCOMMON_H
-#include "Nitrogen/TextCommon.h"
-#endif
-#ifndef NITROGEN_CFSTRING_H
-#include "Nitrogen/CFString.h"
+//#include "Nitrogen/TextCommon.h"
 #endif
 
-namespace Nitrogen
-  {
-  }
+namespace Nitrogen {
+	
+	void RegisterProcessManagerErrors();
+	
+	using ::ProcessSerialNumber;
+	using ::ProcessInfoRec;
+	
+	template < unsigned long > struct PSN_Constant  {};
+	
+	static const PSN_Constant< kNoProcess >      kNoProcess      = PSN_Constant< ::kNoProcess >();
+	static const PSN_Constant< kSystemProcess >  kSystemProcess  = PSN_Constant< ::kSystemProcess >();
+	static const PSN_Constant< kCurrentProcess > kCurrentProcess = PSN_Constant< ::kCurrentProcess >();
+	
+	template <>
+	struct Maker< ProcessSerialNumber >
+	{
+		ProcessSerialNumber operator()( unsigned long high, unsigned long low ) const
+		{
+			ProcessSerialNumber result;
+			result.highLongOfPSN = high;
+			result.lowLongOfPSN  = low;
+			return result;
+		}
+		
+		template < unsigned long k >
+		ProcessSerialNumber operator()( PSN_Constant< k > ) const
+		{
+			ProcessSerialNumber result = { 0, k };
+			return result;
+		}
+	};
+	
+	inline
+	bool
+	operator==( const ProcessSerialNumber& a, const ProcessSerialNumber& b )
+	{
+		return a.highLongOfPSN == b.highLongOfPSN
+			&& a.lowLongOfPSN  == b.lowLongOfPSN;
+	}
+	
+	inline
+	bool
+	operator!=( const ProcessSerialNumber& one, const ProcessSerialNumber& other )
+	{
+		return !( one == other );
+	}
+	
+	inline ProcessSerialNumber NoProcess()       { return Make< ProcessSerialNumber >( kNoProcess ); }
+	inline ProcessSerialNumber SystemProcess()   { return Make< ProcessSerialNumber >( kSystemProcess ); }
+	inline ProcessSerialNumber CurrentProcess()  { return Make< ProcessSerialNumber >( kCurrentProcess ); }
+	
+	ProcessSerialNumber GetCurrentProcess();
+	
+	ProcessSerialNumber GetFrontProcess();
+	
+	bool SameProcess(
+		const ProcessSerialNumber& one, 
+		const ProcessSerialNumber& other
+	);
+	
+	void SetFrontProcess( const ProcessSerialNumber& psn );
+	
+	ProcessSerialNumber LaunchApplication( const FSSpec& file );
+	
+	ProcessSerialNumber GetNextProcess( ProcessSerialNumber process );
+	
+	void GetProcessInformation( const ProcessSerialNumber& process, ProcessInfoRec& info);
+	
+	class Process_Container
+	{
+		public:
+			class const_iterator
+			{
+				public:
+					typedef ProcessSerialNumber value_type;
+					typedef void difference_type;
+					typedef const value_type *pointer;
+					typedef const value_type& reference;
+					typedef std::forward_iterator_tag iterator_category;
+				
+				private:
+					value_type value;
+					
+					void Increment()
+					{
+						try
+						{
+							value = GetNextProcess( value );
+						}
+						catch ( ErrorCode< OSStatus, procNotFound > )
+						{
+							value = NoProcess();
+						}
+					}
+				
+				public:
+					const_iterator() : value( NoProcess() )  {}
+					
+					const_iterator& operator++()    { Increment();  return *this; }
+					const_iterator operator++(int)  { const_iterator old = *this;  operator++();  return old; }
+					
+					reference operator*() const  { return value; }
+					pointer operator->() const   { return &value; }
+					
+					friend bool operator==( const const_iterator& a, const const_iterator& b )    { return a.value == b.value; }
+					friend bool operator!=( const const_iterator& a, const const_iterator& b )    { return !( a == b ); }
+			};
+		
+			Process_Container()  {}
+			
+			const_iterator begin() const  { const_iterator first;  return ++first; }
+			const_iterator end()   const  { return const_iterator(); }
+	};
+	
+	inline Process_Container Processes()
+	{
+		return Process_Container();
+	}
+	
+}
+
+#include "Nitrogen/Operators.h"
 
 #endif
