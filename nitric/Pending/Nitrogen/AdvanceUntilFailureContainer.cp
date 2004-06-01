4154cc5bf3d235456ff733c8c29d44d08b00f84e
Joshua Juran <jjuran@metamage.com>
2004-06-01 06:56:58 +0000

    Initial check-in.

---
+++ nitric/Pending/Nitrogen/AdvanceUntilFailureContainer.cp
@@ -0,0 +1,6 @@
+// AdvanceUntilFailureContainer.cp
+
+#ifndef NITROGEN_ADVANCEUNTILFAILURECONTAINER_H
+#include "Nitrogen/AdvanceUntilFailureContainer.h"
+#endif
+
---
+++ nitric/Pending/Nitrogen/AdvanceUntilFailureContainer.h
@@ -0,0 +1,80 @@
+// AdvanceUntilFailureContainer.h
+
+#ifndef NITROGEN_ADVANCEUNTILFAILURECONTAINER_H
+#define NITROGEN_ADVANCEUNTILFAILURECONTAINER_H
+
+#include <iterator>
+
+namespace Nitrogen
+  {
+   template < class Specifics >
+   class AdvanceUntilFailureContainer: private Specifics
+     {
+      private: 
+         // not implemented:
+            AdvanceUntilFailureContainer& operator=( const AdvanceUntilFailureContainer& );
+
+      public:
+         typedef typename Specifics::value_type value_type;
+         //typedef typename Specifics::size_type size_type;
+         //typedef typename Specifics::difference_type difference_type;
+
+         typedef value_type& reference;
+         typedef const value_type& const_reference;
+         
+         class const_iterator: private Specifics
+           {
+            friend class AdvanceUntilFailureContainer;
+ 
+             public:
+               //typedef AdvanceUntilFailureContainer::difference_type difference_type;
+               typedef void difference_type;
+               typedef AdvanceUntilFailureContainer::value_type value_type;
+               typedef const value_type *pointer;
+               typedef const value_type& reference;
+               typedef std::forward_iterator_tag iterator_category;
+           
+            private:
+               //size_type position;
+               value_type value;
+               
+               void GetNextValue()
+                 {
+                  try
+                    {
+                     value = Specifics::GetNextValue( value );
+                    }
+                  catch ( const typename Specifics::EndOfEnumeration& )
+                    {
+                     value = Specifics::end_value();
+                    }
+                 }
+               
+               const_iterator( const Specifics& b, value_type v )
+                 : Specifics( b ),
+                   value( v )
+                 {
+                 }
+               
+            public:
+               const_iterator()                          : value( Specifics::end_value() )   {}
+               
+               const_iterator& operator++()              { GetNextValue();  return *this; }
+               const_iterator operator++(int)            { const_iterator old = *this; operator++(); return old; }
+               
+               reference operator*() const               { return value; }
+               pointer operator->() const                { return &value; }
+               
+               friend bool operator==( const const_iterator& a, const const_iterator& b )    { return a.value == b.value; }
+               friend bool operator!=( const const_iterator& a, const const_iterator& b )    { return !( a == b ); }
+           };
+      
+         AdvanceUntilFailureContainer( const Specifics& base )
+           : Specifics( base )
+           {}
+         
+         const_iterator begin() const                    { const_iterator first( *this, Specifics::begin_value() );  return ++first; }
+         const_iterator end() const                      { return const_iterator( *this, Specifics::end_value() ); }
+     };
+  }
+#endif
