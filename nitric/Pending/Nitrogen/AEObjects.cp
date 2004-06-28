b5e973ba150b66482a3b7daed2cc269bd18ba701
Joshua Juran <jjuran@metamage.com>
2004-06-28 16:23:36 +0000

    Implemented object accessors.

--- nitric/Pending/Nitrogen/AEObjects.cp
+++ nitric/Pending/Nitrogen/AEObjects.cp
@@ -9,27 +9,86 @@ namespace Nitrogen {
 	class AETokenEditor
 	{
 		private:
-			Owned<AEDesc, TokenDisposer>& desc;
+			Owned< AEDesc, TokenDisposer >& desc;
 			AEDesc workingCopy;
 		public:
-			AETokenEditor( Owned<AEDesc, TokenDisposer>& desc ) : desc(desc), workingCopy(desc.Release())  {}
-			~AETokenEditor()  { desc = Owned<AEDesc, TokenDisposer>::Seize(workingCopy); }
+			AETokenEditor( Owned< AEDesc, TokenDisposer >& desc ) : desc( desc ), workingCopy( desc.Release() )  {}
+			~AETokenEditor()  { desc = Owned< AEDesc, TokenDisposer >::Seize( workingCopy ); }
 			
 			AEDesc& Get()  { return workingCopy; }
 			operator AEDesc&()  { return Get(); }
 	};
+	
+	OSLAccessor::OSLAccessor()
+	:
+		desiredClass(),
+		containerType(),
+		accessor(),
+		accessorRefCon(),
+		isSysHandler()
+	{}
+	
+	bool operator==( const OSLAccessor& a, const OSLAccessor& b )
+	{
+		return a.accessor        == b.accessor
+		    && a.accessorRefCon  == b.accessorRefCon
+		    && a.desiredClass    == b.desiredClass
+		    && a.containerType   == b.containerType
+		    && a.isSysHandler    == b.isSysHandler;
+	}
 	
 	void AEObjectInit()
 	{
 		ThrowOSStatus( ::AEObjectInit() );
 	}
 	
-	Owned<AEDesc, TokenDisposer>
+	Owned< AEDesc, TokenDisposer >
 	AEResolve( const AEDesc& objectSpecifier, AEResolveCallbackFlags callbackFlags )
 	{
 		AEDesc token;
 		ThrowOSStatus( ::AEResolve( &objectSpecifier, callbackFlags, &token ) );
-		return Owned<AEDesc, TokenDisposer>::Seize( token );
+		return Owned< AEDesc, TokenDisposer >::Seize( token );
+	}
+	
+	Owned< OSLAccessor > AEInstallObjectAccessor( const OSLAccessor& toInstall )
+	{
+		ThrowOSStatus
+		(
+			::AEInstallObjectAccessor
+			(
+				toInstall.desiredClass, 
+				toInstall.containerType, 
+				toInstall.accessor, 
+				toInstall.accessorRefCon, 
+				toInstall.isSysHandler
+			)
+		);
+		return Owned< OSLAccessor >::Seize( toInstall );
+	}
+	
+	OSLAccessor AEGetObjectAccessor
+	(
+		DescType desiredClass,
+		DescType containerType,
+		bool isSysHandler
+	)
+	{
+		::OSLAccessorUPP accessor;
+		long accessorRefCon;
+		
+		ThrowOSStatus
+		(
+			::AEGetObjectAccessor
+			(
+				desiredClass,
+				containerType,
+				&accessor,
+				&accessorRefCon,
+				isSysHandler
+			)
+		);
+		
+		return OSLAccessor( desiredClass, containerType, accessor, accessorRefCon, isSysHandler );
 	}
 	
 }
--- nitric/Pending/Nitrogen/AEObjects.h
+++ nitric/Pending/Nitrogen/AEObjects.h
@@ -30,15 +30,178 @@ namespace Nitrogen {
 	template <>
 	struct LivelinessTraits< AEDesc, TokenDisposer >   { typedef SeizedValuesAreLive LivelinessTest; };
 	
-	struct AEResolveCallbackFlagsTag  {};
-	typedef FlagType< AEResolveCallbackFlagsTag, short, kAEIDoMinimum > AEResolveCallbackFlags;
+	struct AEResolveCallbackFlags_Tag  {};
+	typedef FlagType< AEResolveCallbackFlags_Tag, short, kAEIDoMinimum > AEResolveCallbackFlags;
+	
+	
+	struct OSLAccessorUPP_Details: Basic_UPP_Details< ::OSLAccessorUPP,
+	                                                  ::OSLAccessorProcPtr,
+	                                                  ::NewOSLAccessorUPP,
+	                                                  ::DisposeOSLAccessorUPP,
+	                                                  ::InvokeOSLAccessorUPP >
+	{};
+	
+	typedef UPP< OSLAccessorUPP_Details > OSLAccessorUPP;
+	
+	inline Owned< OSLAccessorUPP > NewOSLAccessorUPP( ::OSLAccessorProcPtr p )
+	{
+		return NewUPP< OSLAccessorUPP >( p );
+	}
+	
+	inline void DisposeOSLAccessorUPP( Owned< OSLAccessorUPP > )  {}
+	
+	inline void InvokeOSLAccessorUPP( DescType desiredClass,
+	                                  const AEDesc& container,
+	                                  DescType containerClass,
+	                                  DescType form,
+	                                  const AEDesc& selectionData,
+	                                  AEDesc& value, 
+	                                  RefCon accessorRefcon,
+	                                  OSLAccessorUPP userUPP )
+	{
+		ThrowOSStatus( userUPP( desiredClass, &container, containerClass, form, &selectionData, &value, accessorRefcon ) );
+	}
+	
+	struct OSLAccessor
+	{
+		DescType         desiredClass;
+		DescType         containerType;
+		OSLAccessorUPP   accessor;
+		RefCon           accessorRefCon;
+		bool             isSysHandler;
+		
+		OSLAccessor();
+		
+		OSLAccessor( DescType       desiredClass,
+		             DescType       containerType,
+		             OSLAccessorUPP accessor,
+		             RefCon         accessorRefCon,
+		             bool           isSysHandler )
+		: 
+			desiredClass( desiredClass ),
+			containerType( containerType ),
+			accessor( accessor ),
+			accessorRefCon( accessorRefCon ),
+			isSysHandler( isSysHandler )
+		{}
+		
+	};
+	
+	bool operator==( const OSLAccessor& a, const OSLAccessor& b );
+	
+	inline bool operator!=( const OSLAccessor& a, const OSLAccessor& b )
+	{
+		return !( a == b );
+	}
+	
+	template <>
+	struct Disposer< OSLAccessor > : public std::unary_function< OSLAccessor, void >,
+	                                 private DefaultDestructionOSStatusPolicy
+	{
+		void operator()( const OSLAccessor& installation ) const
+		{
+			DefaultDestructionOSStatusPolicy::HandleDestructionOSStatus
+			(
+				::AERemoveObjectAccessor( installation.desiredClass,
+				                          installation.containerType,
+				                          installation.accessor,
+				                          installation.isSysHandler )
+			);
+		}
+	};
+	
+	typedef Owned< AEDesc, TokenDisposer > ( *OSLAccessorProcPtr )( DescType desiredClass, const AEDesc& container, DescType containerClass, DescType form, const AEDesc& selectionData, RefCon accessorRefcon );
+	
+	template < OSLAccessorProcPtr handler >
+	struct Adapt_OSLAccessor
+	{
+		static pascal OSErr ToCallback( ::DescType desiredClass, const AEDesc* container, ::DescType containerClass, ::DescType form, const AEDesc* selectionData, AEDesc* value, long accessorRefcon )
+		{
+			try
+			{
+				*result = handler( desiredClass, *container, containerClass, form, *selectionData, accessorRefcon ).Release();
+			}
+			catch ( OSStatus err )
+			{
+				return err.Get();
+			}
+			return noErr;
+		}
+	};
 	
 	void AEObjectInit();
 	
-	Owned<AEDesc, TokenDisposer>
+	Owned< AEDesc, TokenDisposer >
 	AEResolve( const AEDesc& objectSpecifier, AEResolveCallbackFlags callbackFlags );
 	
-	inline void AEDisposeToken( Owned<AEDesc, TokenDisposer> )  {}
+	Owned< OSLAccessor > AEInstallObjectAccessor( const OSLAccessor& toInstall );
+	
+	inline Owned< OSLAccessor > AEInstallObjectAccessor
+	(
+		DescType         desiredClass,
+		DescType         containerType,
+		OSLAccessorUPP   accessor,
+		RefCon           accessorRefCon = RefCon(),
+		bool             isSysHandler = false
+	)
+	{
+		return AEInstallObjectAccessor
+		(
+			OSLAccessor( desiredClass, containerType, accessor, accessorRefCon, isSysHandler )
+		);
+	}
+	
+	template < typename OSLAccessorUPP::ProcPtr accessor >
+	Owned< OSLAccessor > AEInstallObjectAccessor
+	(
+		DescType         desiredClass,
+		DescType         containerType,
+		RefCon           accessorRefCon = RefCon(),
+		bool             isSysHandler = false
+	)
+	{
+		return AEInstallObjectAccessor
+		(
+			OSLAccessor
+			(
+				desiredClass, 
+				containerType, 
+				StaticUPP< OSLAccessorUPP, accessor >(), 
+				accessorRefCon, 
+				isSysHandler
+			)
+		);
+	}
+	
+	template < AECoerceDescProcPtr accessor >
+	Owned< OSLAccessor > AEInstallObjectAccessor
+	(
+		DescType         desiredClass,
+		DescType         containerType,
+		RefCon           accessorRefCon = RefCon(),
+		bool             isSysHandler = false
+	)
+	{
+		return AEInstallObjectAccessor< Adapt_OSLAccessor< accessor >::ToCallback >
+		(
+			desiredClass, 
+			containerType, 
+			accessorRefCon, 
+			isSysHandler
+		);
+	}
+	
+	inline void AERemoveObjectAccessor( Owned< OSLAccessor > )  {}
+	
+	typedef OSLAccessor AEGetObjectAccessor_Result;
+	OSLAccessor AEGetObjectAccessor
+	(
+		DescType         desiredClass,
+		DescType         containerType,
+		bool             isSysHandler
+	);
+	
+	inline void AEDisposeToken( Owned< AEDesc, TokenDisposer > )  {}
 	
 }
 
