299ba200cf65a10a4f3f4ea77bf6e66e28b7f4da
Joshua Juran <jjuran@metamage.com>
2004-07-20 01:37:09 +0000

    ResourceID renamed ResID and moved here.

--- nitric/Pending/Nitrogen/Resources.h
+++ nitric/Pending/Nitrogen/Resources.h
@@ -12,9 +12,16 @@
 #ifndef NITROGEN_MACTYPES_H
 #include "Nitrogen/MacTypes.h"
 #endif
+#ifndef NITROGEN_IDTYPE_H
+#include "Nitrogen/IDType.h"
+#endif
 
 namespace Nitrogen
 {
+	
+	class ResID_Tag {};
+	typedef IDType< ResID_Tag, ::ResID, 0 > ResID;
+	typedef ResID ResourceID;
 	
 }
 
