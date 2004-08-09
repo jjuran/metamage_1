e9b635a8ea0b527c3fda3694914a80b514b8c267
Joshua Juran <jjuran@metamage.com>
2004-08-09 01:29:22 +0000

    Copied from mainstream.

---
+++ nitric/Pending/Nitrogen/Script.h
@@ -0,0 +1,13 @@
+// Script.h
+
+#ifndef NITROGEN_SCRIPT_H
+#define NITROGEN_SCRIPT_H
+
+#ifndef NITROGEN_FRAMEWORKHEADER_H
+#include "Nitrogen/FrameworkHeader.h"
+#endif
+#ifndef __SCRIPT__
+#include FRAMEWORK_HEADER(CarbonCore,Script.h)
+#endif
+
+#endif
