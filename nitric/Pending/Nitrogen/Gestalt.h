86672bc4dae655ac3f11cc8f24ee4e7889f9a0b6
Joshua Juran <jjuran@metamage.com>
2004-08-09 01:47:37 +0000

    Copied from mainstream.

---
+++ nitric/Pending/Nitrogen/Gestalt.h
@@ -0,0 +1,44 @@
+// Gestalt.h
+
+#ifndef NITROGEN_GESTALT_H
+#define NITROGEN_GESTALT_H
+
+#ifndef NITROGEN_FRAMEWORKHEADER_H
+#include "Nitrogen/FrameworkHeader.h"
+#endif
+#ifndef __GESTALT__
+#include FRAMEWORK_HEADER(CarbonCore,Gestalt.h)
+#endif
+#ifndef NITROGEN_MACTYPES_H
+#include "Nitrogen/MacTypes.h"
+#endif
+
+namespace Nitrogen
+  {
+   long Gestalt( OSType selector );
+   long Gestalt( OSType selector, long defaultValue );
+   
+   template < ::OSType selector > struct GestaltDefault;
+   
+   template < ::OSType selector >
+   long Gestalt()
+     {
+      return Gestalt( selector, GestaltDefault<selector>::defaultValue );
+     }
+   
+   struct GestaltAttrDefaults
+     {
+      static const long defaultValue = 0;
+     };
+ 
+    struct GestaltVersionDefaults
+     {
+      static const long defaultValue = 0;
+     };
+  
+   template <> struct GestaltDefault< gestaltMenuMgrAttr >: GestaltAttrDefaults {};
+   
+   void RegisterGestaltManagerErrors();
+  }
+
+#endif
