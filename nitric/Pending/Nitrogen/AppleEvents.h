356e8b7997648896111fcc528f7928162dc95b39
Joshua Juran <jjuran@metamage.com>
2004-08-16 01:23:36 +0000

    Added AEEventSource and typed constants.

--- nitric/Pending/Nitrogen/AppleEvents.h
+++ nitric/Pending/Nitrogen/AppleEvents.h
@@ -18,6 +18,42 @@
 
 namespace Nitrogen
   {
+	
+	// Keywords for Apple event parameters
+	static const AEKeyword keyDirectObject        = AEKeyword::Make( ::keyDirectObject        );
+	static const AEKeyword keyErrorNumber         = AEKeyword::Make( ::keyErrorNumber         );
+	static const AEKeyword keyErrorString         = AEKeyword::Make( ::keyErrorString         );
+	static const AEKeyword keyProcessSerialNumber = AEKeyword::Make( ::keyProcessSerialNumber );
+	static const AEKeyword keyPreDispatch         = AEKeyword::Make( ::keyPreDispatch         );
+	static const AEKeyword keySelectProc          = AEKeyword::Make( ::keySelectProc          );
+	static const AEKeyword keyAERecorderCount     = AEKeyword::Make( ::keyAERecorderCount     );
+	static const AEKeyword keyAEVersion           = AEKeyword::Make( ::keyAEVersion           );
+	
+	// Event Class
+	static const AEEventClass kCoreEventClass = AEEventClass::Make( ::kCoreEventClass );
+	
+	// Event ID's
+	static const AEEventID kAEOpenApplication = AEEventID::Make( ::kAEOpenApplication );
+	static const AEEventID kAEOpenDocuments   = AEEventID::Make( ::kAEOpenDocuments   );
+	static const AEEventID kAEPrintDocuments  = AEEventID::Make( ::kAEPrintDocuments  );
+	static const AEEventID kAEQuitApplication = AEEventID::Make( ::kAEQuitApplication );
+	static const AEEventID kAEAnswer          = AEEventID::Make( ::kAEAnswer          );
+	static const AEEventID kAEApplicationDied = AEEventID::Make( ::kAEApplicationDied );
+	static const AEEventID kAEShowPreferences = AEEventID::Make( ::kAEShowPreferences );
+	
+	// Constants for recording
+	// What type are these?
+	
+	class AEEventSource_Tag {};
+	typedef SelectorType< AEEventSource_Tag, ::AEEventSource > AEEventSource;
+	
+	static const AEEventSource kAEUnknownSource = AEEventSource::Make( ::kAEUnknownSource );
+	static const AEEventSource kAEDirectCall    = AEEventSource::Make( ::kAEDirectCall    );
+	static const AEEventSource kAESameProcess   = AEEventSource::Make( ::kAESameProcess   );
+	static const AEEventSource kAELocalProcess  = AEEventSource::Make( ::kAELocalProcess  );
+	static const AEEventSource kAERemoteProcess = AEEventSource::Make( ::kAERemoteProcess );
+	
+	
    struct AEEventHandler
      {
       AEEventClass        theAEEventClass;
