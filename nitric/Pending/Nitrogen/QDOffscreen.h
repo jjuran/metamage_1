fee60021795f473a7d0dc9b64e594b6e4934db0b
Joshua Juran <jjuran@metamage.com>
2004-08-19 18:55:26 +0000

    Copied from mainstream.

---
+++ nitric/Pending/Nitrogen/QDOffscreen.h
@@ -0,0 +1,23 @@
+// QDOffscreen.h
+
+#ifndef NITROGEN_QDOFFSCREEN_H
+#define NITROGEN_QDOFFSCREEN_H
+
+#ifndef NITROGEN_FRAMEWORKHEADER_H
+#include "Nitrogen/FrameworkHeader.h"
+#endif
+#ifndef __QDOFFSCREEN__
+#include FRAMEWORK_HEADER(QD,QDOffscreen.h)
+#endif
+#ifndef NITROGEN_MACERRORS_H
+#include "Nitrogen/MacErrors.h"
+#endif
+#ifndef NITROGEN_QUICKDRAW_H
+#include "Nitrogen/QuickDraw.h"
+#endif
+
+namespace Nitrogen
+  {
+  }
+
+#endif
