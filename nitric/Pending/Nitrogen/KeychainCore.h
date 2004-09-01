878bbcbd4e8d08d752786b251ec2f2e10981e4f8
Marshall Clow <marshall@idio.com>
2004-09-01 03:34:44 +0000

    Minor bug fixes

--- nitric/Pending/Nitrogen/KeychainCore.h
+++ nitric/Pending/Nitrogen/KeychainCore.h
@@ -1,6 +1,9 @@
 #ifndef NITROGEN_KEYCHAINCORE_H
 #define	NITROGEN_KEYCHAINCORE_H
 
+#if	!TARGET_RT_MAC_MACHO
+#error "These routines are only directly callable from MachO"
+#endif
 
 #ifndef __KEYCHAINCORE__
 #include <SecurityCore/KeychainCore.h>
@@ -8,6 +11,10 @@
 
 #ifndef NITROGEN_OWNED_H
 #include "Nitrogen/Owned.h"
+#endif
+
+#ifndef NITROGEN_OSSTATUS_H
+#include "Nitrogen/OSStatus.h"
 #endif
 
 namespace Nitrogen {
