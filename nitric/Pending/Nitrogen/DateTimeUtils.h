bbcdad5cc7dc637913ee4f3065290d85bd60da5c
Joshua Juran <jjuran@metamage.com>
2004-09-02 01:37:22 +0000

    Added DateTime typedef and some inline routines.

--- nitric/Pending/Nitrogen/DateTimeUtils.h
+++ nitric/Pending/Nitrogen/DateTimeUtils.h
@@ -38,9 +38,12 @@
 
 namespace Nitrogen
   {
-   class DateFormTag {};
-   typedef SelectorType< DateFormTag, SInt8, shortDate > DateForm;
+   class DateForm_Tag {};
+   typedef SelectorType< DateForm_Tag, SInt8, shortDate > DateForm;
    
+   typedef UInt32 DateTime;
+   
+   // 260
    inline Str255 LongDateString( const LongDateTime& dateTime,
                                  DateForm            longFlag,
                                  Handle              intlHandle = Handle() )
@@ -50,6 +53,7 @@ namespace Nitrogen
       return result;
      }
 
+   // 276
    inline Str255 LongTimeString( const LongDateTime& dateTime,
                                  bool                wantSeconds,
                                  Handle              intlHandle  = Handle() )
@@ -58,6 +62,89 @@ namespace Nitrogen
       ::LongTimeString( &dateTime, wantSeconds, result, intlHandle );
       return result;
      }
+	
+	// ...
+	
+	// 342
+	inline LongDateTime LongDateToSeconds( const LongDateRec& lDate )
+	{
+		LongDateTime lSecs;
+		::LongDateToSeconds( &lDate, &lSecs );
+		return lSecs;
+	}
+	
+	// 356
+	inline LongDateRec LongSecondsToDate( const LongDateTime& lSecs )
+	{
+		LongDateRec lDate;
+		::LongSecondsToDate( &lSecs, &lDate );
+		return lDate;
+	}
+	
+	// ...
+	
+	// 417
+	inline DateTime GetDateTime()
+	{
+		UInt32 secs;
+		::GetDateTime( &secs );
+		return secs;
+	}
+	
+	// ...
+	
+	// 477
+	inline DateTime DateToSeconds( const DateTimeRec& d )
+	{
+		UInt32 secs;
+		::DateToSeconds( &d, &secs );
+		return secs;
+	}
+	
+	// 491
+	inline DateTimeRec SecondsToDate( DateTime secs )
+	{
+		DateTimeRec d;
+		::SecondsToDate( secs, &d );
+		return d;
+	}
+	
+	template <>
+	struct Converter< LongDateTime, LongDateRec > : public std::unary_function< LongDateRec, LongDateTime >
+	{
+		LongDateTime operator()( const LongDateRec& input ) const
+		{
+			return LongDateToSeconds( input );
+		}
+	};
+	
+	template <>
+	struct Converter< LongDateRec, LongDateTime > : public std::unary_function< LongDateTime, LongDateRec >
+	{
+		LongDateRec operator()( const LongDateTime& input ) const
+		{
+			return LongSecondsToDate( input );
+		}
+	};
+	
+	template <>
+	struct Converter< DateTime, DateTimeRec > : public std::unary_function< DateTimeRec, DateTime >
+	{
+		DateTime operator()( const DateTimeRec& input ) const
+		{
+			return DateToSeconds( input );
+		}
+	};
+	
+	template <>
+	struct Converter< DateTimeRec, DateTime > : public std::unary_function< DateTime, DateTimeRec >
+	{
+		DateTimeRec operator()( const DateTime& input ) const
+		{
+			return SecondsToDate( input );
+		}
+	};
+	
   }
 
 #endif
