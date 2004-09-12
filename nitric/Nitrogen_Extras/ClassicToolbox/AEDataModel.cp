0487b8047b20c38e563571a40fcc0a6a0ee4d370
Joshua Juran <jjuran@metamage.com>
2004-09-12 00:11:19 +0000

    Classic-only bits copied from Nitrogen.

---
+++ nitric/Nitrogen_Extras/ClassicToolbox/AEDataModel.cp
@@ -0,0 +1,11 @@
+// AEDataModel.cp
+
+#ifndef CLASSICTOOLBOX_AEDATAMODEL_H
+#include "ClassicToolbox/AEDataModel.h"
+#endif
+
+namespace Nitrogen
+{
+	
+}
+
---
+++ nitric/Nitrogen_Extras/ClassicToolbox/AEDataModel.h
@@ -0,0 +1,36 @@
+// AEDataModel.h
+
+#ifndef CLASSICTOOLBOX_AEDATAMODEL_H
+#define CLASSICTOOLBOX_AEDATAMODEL_H
+
+#ifndef __EPPC__
+#include <EPPC.h>
+#endif
+
+#ifndef NITROGEN_AEDATAMODEL_H
+#include "Nitrogen/AEDataModel.h"
+#endif
+
+
+namespace Nitrogen
+  {
+	
+#if CALL_NOT_IN_CARBON
+	
+	// Deprecated addressing modes under Carbon
+	static DescType sTypeSessionID    = typeSessionID;
+	static DescType sTypeTargetID     = typeTargetID;
+	static DescType sTypeDispatcherID = typeDispatcherID;
+	
+#endif
+	
+   // TargetID is defined for Carbon, but typeTargetID is not.
+#if CALL_NOT_IN_CARBON
+	
+	template <> struct DescType_Traits< typeTargetID > : POD_DescType_Traits< TargetID > {};
+	
+#endif
+	
+}
+
+#endif
