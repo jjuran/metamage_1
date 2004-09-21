5aa3cae58c806084a8f6926ce9f2602ad66fa727
Joshua Juran <jjuran@metamage.com>
2004-09-21 21:00:32 +0000

    Initial check-in. Use N::PtrFun() instead of std::ptr_fun() if your function takes const reference arguments and you want your code to be standards-compliant. (Using the standard std::ptr_fun() would result in a reference to a reference, which is illegal.  Metrowerks uses a nonstandard std::ptr_fun() to avoid the problem, but libstdc++ does not.)

---
+++ nitric/Pending/Nitrogen/PointerToFunction.cp
@@ -0,0 +1,14 @@
+/*	====================
+ *	PointerToFunction.cp
+ *	====================
+ */
+
+#ifndef NITROGEN_POINTERTOFUNCTION_H
+#include "Nitrogen/PointerToFunction.h"
+#endif
+
+namespace Nitrogen
+{
+	
+}
+
---
+++ nitric/Pending/Nitrogen/PointerToFunction.h
@@ -0,0 +1,169 @@
+/*	===================
+ *	PointerToFunction.h
+ *	===================
+ */
+
+#ifndef NITROGEN_POINTERTOFUNCTION_H
+#define NITROGEN_POINTERTOFUNCTION_H
+
+// Standard C++
+#include <functional>
+
+
+namespace Nitrogen
+{
+	
+	template < class T1, class Result >
+	class PointerToUnaryFunction : public std::unary_function< T1, Result >
+	{
+		public:
+			typedef Result ( *function_type )( T1 );
+		
+		private:
+			function_type f;
+		
+		public:
+			PointerToUnaryFunction( function_type f ) : f( f )  {}
+			
+			Result operator()( T1 a ) const
+			{
+				return f( a );
+			}
+	};
+	
+	template < class T1, class Result >
+	class PointerToUnaryFunctionWithConstArg : public std::unary_function< T1, Result >
+	{
+		public:
+			typedef Result ( *function_type )( const T1& );
+		
+		private:
+			function_type f;
+		
+		public:
+			PointerToUnaryFunctionWithConstArg( function_type f ) : f( f )  {}
+			
+			Result operator()( const T1& a ) const
+			{
+				return f( a );
+			}
+	};
+	
+	template < class T1, class T2, class Result >
+	class PointerToBinaryFunction : public std::binary_function< T1, T2, Result >
+	{
+		public:
+			typedef Result ( *function_type )( T1, T2 );
+		
+		private:
+			function_type f;
+		
+		public:
+			PointerToBinaryFunction( function_type f ) : f( f )  {}
+			
+			Result operator()( T1 a, T2 b ) const
+			{
+				return f( a, b );
+			}
+	};
+	
+	template < class T1, class T2, class Result >
+	class PointerToBinaryFunctionWithConstArg1 : public std::binary_function< T1, T2, Result >
+	{
+		public:
+			typedef Result ( *function_type )( const T1&, T2 );
+		
+		private:
+			function_type f;
+		
+		public:
+			PointerToBinaryFunctionWithConstArg1( function_type f ) : f( f )  {}
+			
+			Result operator()( const T1& a, T2 b ) const
+			{
+				return f( a, b );
+			}
+	};
+	
+	template < class T1, class T2, class Result >
+	class PointerToBinaryFunctionWithConstArg2 : public std::binary_function< T1, T2, Result >
+	{
+		public:
+			typedef Result ( *function_type )( T1, const T2& );
+		
+		private:
+			function_type f;
+		
+		public:
+			PointerToBinaryFunctionWithConstArg2( function_type f ) : f( f )  {}
+			
+			Result operator()( T1 a, const T2& b ) const
+			{
+				return f( a, b );
+			}
+	};
+	
+	template < class T1, class T2, class Result >
+	class PointerToBinaryFunctionWithConstArgs : public std::binary_function< T1, T2, Result >
+	{
+		public:
+			typedef Result ( *function_type )( const T1&, const T2& );
+		
+		private:
+			function_type f;
+		
+		public:
+			PointerToBinaryFunctionWithConstArgs( function_type f ) : f( f )  {}
+			
+			Result operator()( const T1& a, const T2& b ) const
+			{
+				return f( a, b );
+			}
+	};
+	
+	template < class T1, class Result >
+	PointerToUnaryFunction< T1, Result >
+	PtrFun( Result (f)( T1 ) )
+	{
+		return PointerToUnaryFunction< T1, Result >( f );
+	}
+	
+	template < class T1, class Result >
+	PointerToUnaryFunctionWithConstArg< T1, Result >
+	PtrFun( Result (f)( const T1& ) )
+	{
+		return PointerToUnaryFunctionWithConstArg< T1, Result >( f );
+	}
+	
+	template < class T1, class T2, class Result >
+	PointerToBinaryFunction< T1, T2, Result >
+	PtrFun( Result (f)( T1, T2 ) )
+	{
+		return PointerToBinaryFunction< T1, T2, Result >( f );
+	}
+	
+	template < class T1, class T2, class Result >
+	PointerToBinaryFunctionWithConstArg1< T1, T2, Result >
+	PtrFun( Result (f)( const T1&, T2 ) )
+	{
+		return PointerToBinaryFunctionWithConstArg1< T1, T2, Result >( f );
+	}
+	
+	template < class T1, class T2, class Result >
+	PointerToBinaryFunctionWithConstArg2< T1, T2, Result >
+	PtrFun( Result (f)( T1, const T2& ) )
+	{
+		return PointerToBinaryFunctionWithConstArg2< T1, T2, Result >( f );
+	}
+	
+	template < class T1, class T2, class Result >
+	PointerToBinaryFunctionWithConstArgs< T1, T2, Result >
+	PtrFun( Result (f)( const T1&, const T2& ) )
+	{
+		return PointerToBinaryFunctionWithConstArgs< T1, T2, Result >( f );
+	}
+	
+}
+
+#endif
+
