e06f649a9881dfb4b5828953b9dc6df9b448901a
Joshua Juran <jjuran@metamage.com>
2004-09-30 05:31:28 +0000

    FSContents:  Initial check-in.

---
+++ nitric/Nitrogen_Extras/Iteration/FSContents.h
@@ -0,0 +1,77 @@
+/*	============
+ *	FSContents.h
+ *	============
+ */
+
+#ifndef FSCONTENTS_H
+#define FSCONTENTS_H
+
+// Nitrogen Extras / Utilities
+#include "Utilities/Files.h"
+
+// Nitrogen Extras / Iteration
+#include "Iteration/IndexedValueContainer.h"
+
+
+namespace Nitrogen
+{
+	
+	struct FSSpecContents_Specifics
+	{
+		typedef FSSpec    FileSpec;
+		typedef FSDirSpec DirSpec;
+	};
+	
+	struct FSRefContents_Specifics
+	{
+		typedef FSRef FileSpec;
+		typedef FSRef DirSpec;
+	};
+	
+	template < class Specifics >
+	struct FSContents_Specifics
+	{
+		typedef std::size_t                   size_type;
+		typedef std::ptrdiff_t                difference_type;
+		typedef typename Specifics::FileSpec  value_type;
+		
+		typedef typename Specifics::DirSpec  ContainerType;
+		
+		static std::size_t Size( ContainerType dir )
+		{
+			CInfoPBRec pb;
+			
+			return FSpGetCatInfo( Convert< FSDirSpec >( dir ), pb ).dirInfo.ioDrNmFls;
+		}
+		
+		static value_type GetValue( ContainerType dir, size_type position )
+		{
+			CInfoPBRec pb;
+			Str255 name;
+			
+			FSpGetCatInfo( Convert< FSDirSpec >( dir ), position + 1, pb, name );  // one-based
+			
+			return Convert< value_type >( Convert< FSDirSpec >( dir ) & name );
+		}
+		
+		typedef ContainerType ContainerState;
+		typedef ContainerState IteratorState;
+	};
+	
+	typedef IndexedValueContainer< FSContents_Specifics< FSSpecContents_Specifics > > FSSpecContents_Container;
+	typedef IndexedValueContainer< FSContents_Specifics< FSRefContents_Specifics  > > FSRefContents_Container;
+	
+	inline FSSpecContents_Container FSContents( const FSDirSpec& dir )
+	{
+		return FSSpecContents_Container( dir );
+	}
+	
+	inline FSRefContents_Container FSContents( const FSRef& dir )
+	{
+		return FSRefContents_Container( dir );
+	}
+	
+}
+
+#endif
+
