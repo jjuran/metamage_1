8e8bedc806278a0e7c841723d73984c7af094b5f
Joshua Juran <jjuran@metamage.com>
2004-11-05 23:47:53 +0000

    Moved from core Nitrogen.

---
+++ nitric/Nitrogen_Extras/Templates/TrappingFunction.cp
@@ -0,0 +1,14 @@
+/*	===================
+ *	TrappingFunction.cp
+ *	===================
+ */
+
+#ifndef TEMPLATES_TRAPPINGFUNCTION_H
+#include "Templates/TrappingFunction.h"
+#endif
+
+namespace Nitrogen
+{
+	
+}
+
---
+++ nitric/Nitrogen_Extras/Templates/TrappingFunction.h
@@ -0,0 +1,78 @@
+/*	==================
+ *	TrappingFunction.h
+ *	==================
+ */
+
+#ifndef TEMPLATES_TRAPPINGFUNCTION_H
+#define TEMPLATES_TRAPPINGFUNCTION_H
+
+// Nitrogen
+#ifndef TEMPLATES_POINTERTOFUNCTION_H
+#include "Templates/PointerToFunction.h"
+#endif
+
+
+namespace Nitrogen
+{
+	
+	template < class Exception, class Closure >
+	class ExceptionTrap : public ConstantFunction< typename Closure::result_type >
+	{
+		private:
+			Closure closure;
+		
+		public:
+			ExceptionTrap( const Closure& closure ) : closure( closure )  {}
+			
+			typename Closure::result_type operator()() const
+			{
+				try
+				{
+					throw;
+				}
+				catch ( Exception )  {}
+				
+				return closure();
+			}
+	};
+	
+	template < class Exception, class Closure >
+	ExceptionTrap< Exception, Closure > TrapException( Exception, const Closure& closure )
+	{
+		return ExceptionTrap< Exception, Closure >( closure );
+	}
+	
+	template < class F, class Trap >
+	class UnaryTrappingFunction : public std::unary_function< typename F::argument_type, typename F::result_type >
+	{
+		private:
+			F f;
+			Trap trap;
+		
+		public:
+			UnaryTrappingFunction( F f, Trap trap ) : f( f ), trap( trap )  {}
+			
+			typename F::result_type operator()( const typename F::argument_type& arg ) const
+			{
+				try
+				{
+					return f( arg );
+				}
+				catch ( ... )
+				{
+					return trap();
+				}
+			}
+	};
+	
+	template < class Trap, class F >
+	UnaryTrappingFunction< F, Trap > Trap1( const F&     f,
+	                                        const Trap&  trap )
+	{
+		return UnaryTrappingFunction< F, Trap >( f, trap );
+	}
+	
+}
+
+#endif
+
