b7b1fc32d2f6192f8c723182b3febdac9bd87870
Joshua Juran <jjuran@metamage.com>
2005-01-11 01:26:44 +0000

    #define OTUNIXERRORS 1 Added comments.

--- nitric/Nitrogen_Extras/NitrogenLib/NitrogenLib.hh
+++ nitric/Nitrogen_Extras/NitrogenLib/NitrogenLib.hh
@@ -21,7 +21,18 @@
 #else
 	
 	// MSL from CW Pro 6 has a math/fp interaction bug.
+	// If you include cmath, fp.h, then math.h, it breaks.
+	// We need to include cmath before fp.h, so we include math.h before fp.h.
+	// Carbon.h includes fp.h.
+	
 	#include <math.h>
+	
+	// OpenTransport.h will define some useful Unix error constants for us,
+	// but only if we define OTUNIXERRORS.  Since we precompile, this is our
+	// only shot to do it.
+	
+	#define OTUNIXERRORS 1
+	
 	#include <Carbon.h>
 	
 #endif
