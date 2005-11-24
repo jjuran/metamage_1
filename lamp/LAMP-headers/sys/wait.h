302db37f7b0f1e04454899552b272d7c14ac0679
Joshua Juran <jjuran@metamage.com>
2005-11-24 07:07:38 +0000

    Added POSIX macros for reading wait status.

--- lamp/LAMP-headers/sys/wait.h
+++ lamp/LAMP-headers/sys/wait.h
@@ -11,11 +11,23 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-pid_t wait( int* stat_loc );
-
-pid_t waitpid( pid_t pid, int* stat_loc, int options );
-
+	
+	#define WTERMSIG(stat)     ( (stat) & 0x7f )
+	
+	#define WCOREDUMP(stat)    ( (stat) & 0x80 )
+	
+	#define WEXITSTATUS(stat)  ( ((stat) >> 8) & 0xFF )
+	
+	#define WIFSIGNALED(stat)  ( WTERMSIG(stat) != 0 && WTERMSIG(stat) != 0x7F )
+	
+	#define WIFEXITED(stat)    ( WTERMSIG(stat) == 0 )
+	
+	#define WIFSTOPPED(stat)   ( WTERMSIG(stat) == 0x7F )
+	
+	pid_t wait( int* stat_loc );
+	
+	pid_t waitpid( pid_t pid, int* stat_loc, int options );
+	
 #ifdef __cplusplus
 }
 #endif
