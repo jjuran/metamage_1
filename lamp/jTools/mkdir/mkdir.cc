78598455e3b9a49d35840a0af18200f2bfa1fc1b
Joshua Juran <jjuran@metamage.com>
2006-02-06 07:26:59 +0000

    Use mkdir().

--- lamp/jTools/mkdir/mkdir.cc
+++ lamp/jTools/mkdir/mkdir.cc
@@ -6,7 +6,15 @@
 // Universal Interfaces
 #include <MacTypes.h>
 
+// Standard C
+#include "errno.h"
+
+// Standard C/C++
 #include <cstdio>
+#include <cstring>
+
+// POSIX
+#include "sys/stat.h"
 
 // Nitrogen
 #include "Nitrogen/OSStatus.h"
@@ -34,17 +42,14 @@ int O::Main( int argc, const char *const argv[] )
 	
 	for ( int index = 1;  index < argc;  ++index )
 	{
-		try
-		{
-			N::FSpDirCreate( Path2FSS( argv[ index ] ) );
-		}
-		catch ( N::OSStatus& err )
+		int result = mkdir( argv[ index ], 0700 );
+		
+		if ( result == -1 )
 		{
 			std::fprintf( stderr,
 			              "mkdir: %s: %s\n",
-			              argv[ index ],
-			              err == dupFNErr ? "already exists"
-			                              : "nonexistent directory in pathname" );
+			                      argv[ index ],
+			                          std::strerror( errno ) );
 			fail++;
 		}
 	}
