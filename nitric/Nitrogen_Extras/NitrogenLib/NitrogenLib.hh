af0ab991c875bd0b1e11383c5e780987a680ab9a
Joshua Juran <jjuran@metamage.com>
2006-04-09 11:51:46 +0000

    Trick Apple's Quickdraw.h into declaring Carbon accessors even while we're using inlines elsewhere.

--- nitric/Nitrogen_Extras/NitrogenLib/NitrogenLib.hh
+++ nitric/Nitrogen_Extras/NitrogenLib/NitrogenLib.hh
@@ -33,6 +33,27 @@
 	
 	#define OTUNIXERRORS 1
 	
+	#if defined(ACCESSOR_CALLS_ARE_FUNCTIONS) && !ACCESSOR_CALLS_ARE_FUNCTIONS
+		
+		// This symbol controls the declarations of various Carbon accessors.
+		// Some of them have macro/inline alternatives, and some do not.
+		// What we want is to use the inlines when available and the accessor
+		// prototypes otherwise (which we'll have to define ourselves).
+		// Unfortunately, Apple does not give us this option, so we have to do
+		// things piecemeal.
+		
+		#undef ACCESSOR_CALLS_ARE_FUNCTIONS
+		#define ACCESSOR_CALLS_ARE_FUNCTIONS 1
+		
+		// No inlines, need accessor declarations for later definition.
+		// Included by MacWindows, so do it first.
+		#include <Quickdraw.h>
+		
+		#undef ACCESSOR_CALLS_ARE_FUNCTIONS
+		#define ACCESSOR_CALLS_ARE_FUNCTIONS 0
+		
+	#endif
+	
 	#include <Carbon.h>
 	
 #endif
