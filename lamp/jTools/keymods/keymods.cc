63ddafe34f16e3eea569073349b742aecff704ec
Joshua Juran <jjuran@metamage.com>
2006-05-18 06:58:54 +0000

    Initial check-in.

---
+++ lamp/jTools/keymods/keymods.cc
@@ -0,0 +1,77 @@
+/*	==========
+ *	keymods.cc
+ *	==========
+ */
+
+// Universal Interfaces
+#include <Events.h>
+
+// Standard C/C++
+#include <cstdio>
+
+
+#pragma export on
+
+enum
+{
+	kKeyModifierBits = 32
+};
+
+const char* keyModifierNames[ kKeyModifierBits ] =
+{
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	
+	"command",
+	"shift",
+	"alpha",
+	"option",
+	"control",
+	"right-shift",
+	"right-option",
+	"right-control",
+	
+	"",
+	"function",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	""
+};
+
+int main( int argc, char const *const argv[] )
+{
+	UInt32 keymods = ::GetCurrentKeyModifiers();
+	
+	for ( int i = 0;  i < kKeyModifierBits;  ++i )
+	{
+		const char* name = keyModifierNames[i];
+		
+		if ( keymods & (1 << i) )
+		{
+			std::printf( "%s\n", name );
+		}
+	}
+	
+	return 0;
+}
+
+#pragma export reset
+
