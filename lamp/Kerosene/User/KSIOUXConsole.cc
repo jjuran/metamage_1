3f9883b42c0ed8f7c5b61362b786dfafe11fd551
Joshua Juran <jjuran@metamage.com>
2006-05-27 06:55:39 +0000

    Replace __write_console() with our own that calls write() directly, instead of going through WriteCharsToConsole() which loses the distinction between stdout and stderr.

--- lamp/Kerosene/User/KSIOUXConsole.cc
+++ lamp/Kerosene/User/KSIOUXConsole.cc
@@ -11,6 +11,18 @@
 // POSIX
 #include "unistd.h"
 
+// from file_struc.h
+typedef unsigned long	__file_handle;
+
+enum __io_results 
+{
+     __no_io_error,
+     __io_error,
+     __io_EOF
+};
+
+extern "C" int __write_console( __file_handle handle, unsigned char* buffer, size_t* count );
+
 
 enum
 {
@@ -36,6 +48,13 @@ long WriteCharsToConsole( char *buffer, long n )
 long ReadCharsFromConsole( char *buffer, long n )
 {
 	return read( kConsoleInputFileDescriptor, buffer, n );
+}
+
+int __write_console( __file_handle handle, unsigned char* buffer, size_t* count )
+{
+	*count = write( handle, buffer, *count );
+	
+	return *count >= 0 ? __no_io_error : __io_error;
 }
 
 #endif  // #if defined(__MWERKS__) && !defined(__MACH__)
