dcc510f96c153768f2407ad798e62d2e22ca7e0b
Joshua Juran <jjuran@metamage.com>
2006-07-23 20:55:52 +0000

    Added new routines, and placeholders for the rest.

--- nitric/Nitrogen_Extras/CarbonUnits/CarbonUnits/Quickdraw.cc
+++ nitric/Nitrogen_Extras/CarbonUnits/CarbonUnits/Quickdraw.cc
@@ -17,6 +17,18 @@
 
 // These functions are always declared in the headers and are always extern.
 
+pascal OSErr LockPortBits( GrafPtr port )
+{
+	return noErr;
+}
+
+pascal OSErr UnlockPortBits( GrafPtr port )
+{
+	return noErr;
+}
+
+// QDRegionToRects
+
 pascal Boolean QDSwapPort( CGrafPtr newCPort, CGrafPtr* oldCPortPtr )
 {
 	GrafPtr oldPort;
@@ -33,4 +45,99 @@ pascal Boolean QDSwapPort( CGrafPtr newCPort, CGrafPtr* oldCPortPtr )
 	
 	return oldPort != newPort;
 }
+
+// HandleToRgn
+// RgnToHandle
+// StdOpcode
+// IsValidPort
+
+pascal Boolean IsPortVisibleRegionEmpty( CGrafPtr port )
+{
+	return EmptyRgn( port->visRgn );
+}
+
+pascal Boolean IsPortClipRegionEmpty( CGrafPtr port )
+{
+	return EmptyRgn( port->clipRgn );
+}
+
+// SectRegionWithPortClipRegion
+// SectRegionWithPortVisibleRegion
+
+pascal Handle SwapPortPicSaveHandle( CGrafPtr port, Handle newHandle )
+{
+	Handle oldHandle = port->picSave;
+	
+	port->picSave = newHandle;
+	
+	return oldHandle;
+}
+
+pascal Handle SwapPortPolySaveHandle( CGrafPtr port, Handle newHandle )
+{
+	Handle oldHandle = port->polySave;
+	
+	port->polySave = newHandle;
+	
+	return oldHandle;
+}
+
+pascal Handle SwapPortRegionSaveHandle( CGrafPtr port, Handle newHandle )
+{
+	Handle oldHandle = port->rgnSave;
+	
+	port->rgnSave = newHandle;
+	
+	return oldHandle;
+}
+
+pascal void SetPortTextFont( CGrafPtr port, short txFont )
+{
+	port->txFont = txFont;
+}
+
+pascal void SetPortTextSize( CGrafPtr port, short txSize )
+{
+	port->txSize = txSize;
+}
+
+pascal void SetPortTextFace( CGrafPtr port, StyleParameter face )
+{
+	port->txFace = face;
+}
+
+pascal void SetPortTextMode( CGrafPtr port, short mode )
+{
+	port->txMode = mode;
+}
+
+// QDLocalToGlobalPoint
+// QDGlobalToLocalPoint
+// QDLocalToGlobalRect
+// QDGlobalToLocalRect
+// QDLocalToGlobalRegion
+// QDGlobalToLocalRegion
+
+pascal Boolean QDIsPortBuffered( CGrafPtr port )
+{
+	return false;
+}
+
+pascal Boolean QDIsPortBufferDirty( CGrafPtr port )
+{
+	return false;
+}
+
+pascal void QDFlushPortBuffer( CGrafPtr port, RgnHandle region )
+{
+	// do nothing
+}
+
+// QDGetDirtyRegion
+// QDSetDirtyRegion
+// QDAddRectToDirtyRegion
+// QDAddRegionToDirtyRegion
+// QDSaveRegionBits
+// QDRestoreRegionBits
+// QDDisposeRegionBits
 
