066bdb8671f347f2cba21708c6e1492410440d0d
Marshall Clow <marshall@idio.com>
2006-07-29 20:26:49 +0000

    Implemented missing functions

--- nitric/Nitrogen/CGImage.h
+++ nitric/Nitrogen/CGImage.h
@@ -44,6 +44,92 @@ namespace Nitrogen
    
    inline void CGImageRelease( Nucleus::Owned<CGImageRef> )
      {}
+	 
+	using ::CGImageGetTypeID;	// CFTypeID CGImageGetTypeID(void)
+	
+
+	class CGImageCreate_Failed {};
+	inline Nucleus::Owned<CGImageRef> CGImageCreate (
+				size_t width, size_t height, size_t bitsPerComponent, size_t bitsPerPixel, size_t bytesPerRow, 
+				CGColorSpaceRef colorspace, CGBitmapInfo bitmapInfo, CGDataProviderRef provider, 
+				const float decode[] = NULL, bool shouldInterpolate = false, CGColorRenderingIntent intent = kCGRenderingIntentDefault ) {
+		CGImageRef result = ::CGImageCreate ( width, height, bitsPerComponent, bitsPerPixel, bytesPerRow, 
+										colorspace, bitmapInfo, provider, decode, shouldInterpolate, intent );
+		if ( NULL == result ) throw CGImageCreate_Failed ();
+		return Nucleus::Owned<CGImageRef>::Seize ( result );
+		}
+	
+	class CGImageMaskCreate_Failed {};
+	inline Nucleus::Owned<CGImageRef> CGImageMaskCreate (
+				size_t width, size_t height, size_t bitsPerComponent, size_t bitsPerPixel, size_t bytesPerRow, 
+				CGDataProviderRef provider, const float decode[] = NULL, bool shouldInterpolate = false) {
+		CGImageRef result = ::CGImageMaskCreate ( width, height, bitsPerComponent, bitsPerPixel, bytesPerRow, 
+										provider, decode, shouldInterpolate );
+		if ( NULL == result ) throw CGImageMaskCreate_Failed ();
+		return Nucleus::Owned<CGImageRef>::Seize ( result );
+		}
+
+	class CGImageCreateCopy_Failed {};
+	inline Nucleus::Owned<CGImageRef> CGImageCreateCopy ( CGImageRef image ) {
+		CGImageRef result = ::CGImageCreateCopy ( image ); 
+		if ( NULL == result ) throw CGImageCreateCopy_Failed ();
+		return Nucleus::Owned<CGImageRef>::Seize ( result );
+		}
+	
+
+	class CGImageCreateWithJPEGDataProvider_Failed {};
+	inline Nucleus::Owned<CGImageRef> CGImageCreateWithJPEGDataProvider (
+				CGDataProviderRef source, const float decode[] = NULL,
+				bool shouldInterpolate = false, CGColorRenderingIntent intent = kCGRenderingIntentDefault ) {
+		CGImageRef result = ::CGImageCreateWithJPEGDataProvider ( source, decode, shouldInterpolate, intent );
+		if ( NULL == result ) throw CGImageCreateWithJPEGDataProvider_Failed ();
+		return Nucleus::Owned<CGImageRef>::Seize ( result );
+		}
+
+
+	class CGImageCreateWithPNGDataProvider_Failed {};
+	inline Nucleus::Owned<CGImageRef> CGImageCreateWithPNGDataProvider (
+				CGDataProviderRef source, const float decode[] = NULL,
+				bool shouldInterpolate = false, CGColorRenderingIntent intent = kCGRenderingIntentDefault ) {
+		CGImageRef result = ::CGImageCreateWithPNGDataProvider ( source, decode, shouldInterpolate, intent );
+		if ( NULL == result ) throw CGImageCreateWithPNGDataProvider_Failed ();
+		return Nucleus::Owned<CGImageRef>::Seize ( result );
+		}
+	
+
+	class CGImageCreateWithImageInRect_Failed {};
+	inline Nucleus::Owned<CGImageRef> CGImageCreateWithImageInRect ( CGImageRef image, CGRect rect ) {
+		CGImageRef result = ::CGImageCreateWithImageInRect ( image, rect );
+		if ( NULL == result ) throw CGImageCreateWithImageInRect_Failed ();
+		return Nucleus::Owned<CGImageRef>::Seize ( result );
+		}
+
+
+	class CGImageCreateWithMask_Failed {};
+	inline Nucleus::Owned<CGImageRef> CGImageCreateWithMask ( CGImageRef image, CGImageRef mask ) {
+		CGImageRef result = ::CGImageCreateWithMask ( image, mask );
+		if ( NULL == result ) throw CGImageCreateWithMask_Failed ();
+		return Nucleus::Owned<CGImageRef>::Seize ( result );
+		}
+
+//	CG_EXTERN CGImageRef CGImageCreateWithMaskingColors(CGImageRef image, const float components[]) AVAILABLE_MAC_OS_X_VERSION_10_4_AND_LATER;
+//	CG_EXTERN CGImageRef CGImageCreateCopyWithColorSpace(CGImageRef image, CGColorSpaceRef colorspace) AVAILABLE_MAC_OS_X_VERSION_10_3_AND_LATER;
+
+	using ::CGImageIsMask;
+	using ::CGImageGetWidth;
+	using ::CGImageGetHeight;
+	using ::CGImageGetBitsPerComponent;
+	using ::CGImageGetBitsPerPixel;
+	using ::CGImageGetBytesPerRow;
+
+//	CG_EXTERN CGColorSpaceRef CGImageGetColorSpace(CGImageRef image);
+	using ::CGImageGetAlphaInfo;
+	using ::CGImageGetDataProvider;
+//	const float *CGImageGetDecode(CGImageRef image);
+	using ::CGImageGetShouldInterpolate;
+	using ::CGImageGetRenderingIntent;
+	using ::CGImageGetBitmapInfo;
+	
   }
 
 #endif
