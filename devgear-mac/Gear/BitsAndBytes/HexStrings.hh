acb7e70c1aad3fef0acb5dcdfa6225bc933d6839
Joshua Juran <jjuran@metamage.com>
2006-08-20 03:26:41 +0000

    Moved looping functions out of header. Basic cleanup.

--- devgear-mac/Gear/BitsAndBytes/HexStrings.hh
+++ devgear-mac/Gear/BitsAndBytes/HexStrings.hh
@@ -1,11 +1,10 @@
 /*	=============
  *	HexStrings.hh
  *	=============
- *	
- *	No implementation necessary.
  */
 
-#pragma once
+#ifndef BITSANDBYTES_HEXSTRINGS_HH
+#define BITSANDBYTES_HEXSTRINGS_HH
 
 #include <string>
 
@@ -15,22 +14,7 @@
 namespace BitsAndBytes
 {
 	
-	static std::string EncodeAsHex( const void* data, std::size_t size )
-	{
-		std::string result;
-		
-		result.resize( size * 2 );
-		
-		const unsigned char* bytes = reinterpret_cast< const unsigned char* >( data );
-		
-		for ( std::size_t i = 0;  i < size;  ++i )
-		{
-			result[ i * 2     ] = NibbleAsHex( HighNibble( bytes[ i ] ) );
-			result[ i * 2 + 1 ] = NibbleAsHex( LowNibble ( bytes[ i ] ) );
-		}
-		
-		return result;
-	}
+	std::string EncodeAsHex( const void* data, std::size_t size );
 	
 	template < class Data >
 	std::string EncodeAsHex( const Data& data )
@@ -49,7 +33,8 @@ namespace BitsAndBytes
 		return EncodeAsHex( c );
 	}
 	
-	static unsigned char JuxtaposeNibbles( char high, char low )
+	// E.g. 0x02, 0x0B -> 0x2b
+	inline unsigned char JuxtaposeNibbles( char high, char low )
 	{
 		unsigned char result = high;
 		
@@ -59,30 +44,17 @@ namespace BitsAndBytes
 		return result;
 	}
 	
-	static unsigned char DecodeHexNibbles( char high, char low )
+	// E.g. '2', 'b' -> 0x2b
+	inline unsigned char DecodeHexNibbles( char high, char low )
 	{
 		return JuxtaposeNibbles( HexAsNibble( high ),
 		                         HexAsNibble( low  ) );
 	}
 	
-	static std::string DecodeHex( const std::string hexCodes )
-	{
-		std::string result;
-		
-		// FIXME:  Verify the hex data.
-		
-		result.resize( hexCodes.size() / 2 );
-		
-		for ( std::size_t i = 0;  i < result.size();  ++i )
-		{
-			char high = hexCodes[ i * 2     ];
-			char low  = hexCodes[ i * 2 + 1 ];
-			
-			result[ i ] = DecodeHexNibbles( high, low );
-		}
-		
-		return result;
-	}
+	// E.g. "666f6f20626171" -> "foo bar"
+	std::string DecodeHex( const std::string hexCodes );
 	
 }
+
+#endif
 
