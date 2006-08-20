32735a704b38272f98a6ac617fb368e3f8095e08
Joshua Juran <jjuran@metamage.com>
2006-08-20 03:26:10 +0000

    Moved recursive function out of header. Basic cleanup.

--- devgear-mac/Gear/BitsAndBytes/DecimalStrings.hh
+++ devgear-mac/Gear/BitsAndBytes/DecimalStrings.hh
@@ -1,66 +1,60 @@
 /*	=================
  *	DecimalStrings.hh
  *	=================
- *	
- *	No implementation necessary.
  */
 
-#pragma once
+#ifndef BITSANDBYTES_DECIMALSTRINGS_HH
+#define BITSANDBYTES_DECIMALSTRINGS_HH
 
 #include <string>
 
 #include "DecimalCodes.hh"
 
 
-namespace BitsAndBytes {
+namespace BitsAndBytes
+{
 	
-	using std::string;
+	std::string EncodeDecimal_( unsigned long number );
 	
-	// E.g.   76 ->   "76"
-	// E.g. 1984 -> "1984"
-	inline string EncodeDecimal_(unsigned long number)
+	inline std::string EncodeDecimal( unsigned long number )
 	{
-		return (number == 0) 
-			? "" 
-			:   EncodeDecimal_(number / 10) 
-			  + EncodeDecDigit(number % 10);
+		return number == 0 ? "0"
+		                   : EncodeDecimal_( number );
 	}
 	
-	inline string EncodeDecimal(unsigned long number)
+	inline std::string EncodeInteger( long number )
 	{
-		return (number == 0) 
-			? "0" 
-			: EncodeDecimal_(number);
-	}
-	
-	inline string EncodeInteger(long number)
-	{
-		return (number < 0) 
-			? string("-") + EncodeDecimal_(-number)
-			: EncodeDecimal(number);
+		return number < 0 ? std::string( "-" ) + EncodeDecimal_( -number )
+		                  :                      EncodeDecimal (  number );
 	}
 	
 	// E.g.   76 -> "76"
 	// E.g. 1984 -> "84"
-	inline string EncodeDecimal2(unsigned int number)
+	inline std::string EncodeDecimal2( unsigned int number )
 	{
 		char str[3] = "to";
-		str[0] = EncodeDecDigit(number /  10);
-		str[1] = EncodeDecDigit(number % 10);
+		
+		str[0] = EncodeDecDigit( number / 10 );
+		str[1] = EncodeDecDigit( number % 10 );
+		
 		return str;
 	}
 	
 	// E.g.   76 -> "0076"
 	// E.g. 1984 -> "1984"
-	inline string EncodeDecimal4(unsigned int number)
+	inline std::string EncodeDecimal4( unsigned int number )
 	{
 		char str[5] = "four";
-		str[0] = EncodeDecDigit(number / 1000);
-		str[1] = EncodeDecDigit(number / 100 % 10);
-		str[2] = EncodeDecDigit(number /  10 % 10);
-		str[3] = EncodeDecDigit(number       % 10);
+		
+		str[0] = EncodeDecDigit( number / 1000 );
+		str[1] = EncodeDecDigit( number / 100 % 10 );
+		str[2] = EncodeDecDigit( number /  10 % 10 );
+		str[3] = EncodeDecDigit( number       % 10 );
+		
 		return str;
 	}
 	
 }
+
+#endif
 
