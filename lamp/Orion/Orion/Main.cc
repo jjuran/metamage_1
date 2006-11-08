278f7afd294ca2d1ccc7f54af20fc1e53979c1a6
Joshua Juran <jjuran@metamage.com>
2006-11-08 05:02:00 +0000

    Catch and report errno exceptions.

--- lamp/Orion/Orion/Main.cc
+++ lamp/Orion/Orion/Main.cc
@@ -5,8 +5,17 @@
 
 #include "Orion/Main.hh"
 
+// Standard C
+#include "errno.h"
+
 // Nitrogen
 #include "Nitrogen/OSStatus.h"
+
+// POSeven
+#include "POSeven/Errno.hh"
+
+// OSErrno
+#include "OSErrno/OSErrno.hh"
 
 // Orion
 #include "Orion/StandardIO.hh"
@@ -16,6 +25,13 @@ namespace Orion
 {
 	
 	namespace N = Nitrogen;
+	namespace NN = Nucleus;
+	namespace P7 = POSeven;
+	
+	static void RegisterMacToUnixErrorConversions()
+	{
+		NN::RegisterExceptionConversion< P7::Errno, N::OSStatus >();
+	}
 	
 	extern "C" int main( int argc, const char* const argv[] );
 	
@@ -23,20 +39,35 @@ namespace Orion
 	
 	int main( int argc, const char* const argv[] )
 	{
+		RegisterMacToUnixErrorConversions();
+		
 		try
 		{
 			return Main( argc, argv );
 		}
-		catch ( ExitStatus& exit )
+		catch ( const ExitStatus& exit )
 		{
 			return exit.Get();
 		}
-		catch ( N::OSStatus& err )
+		catch ( const N::OSStatus& err )
 		{
 			Io::Err << "Orion: Main() threw OSStatus " << err.Get() << ".\n";
+			
+			P7::Errno errnum = NN::Convert< P7::Errno >( NN::TheExceptionBeingHandled() );
+			
+			if ( errnum != -1 )
+			{
+				Io::Err << "That converts to errno " << errnum << ": " << std::strerror( errnum ) << "\n";
+			}
+			
 			return 1;
 		}
-		catch ( std::exception& e )
+		catch ( const P7::Errno& err )
+		{
+			Io::Err << "Orion: Main() threw errno " << err.Get() << ": " << std::strerror( err ) << ".\n";
+			return 1;
+		}
+		catch ( const std::exception& e )
 		{
 			Io::Err << e.what() << "\n";
 			return 1;
