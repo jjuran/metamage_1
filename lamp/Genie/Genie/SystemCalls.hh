53892daa823fc3a0ab88562b8693bbcbc341f48c
Joshua Juran <jjuran@metamage.com>
2006-12-07 08:10:48 +0000

    Declares GetErrnoFromExceptionInSystemCall(). Initial check-in.

---
+++ lamp/Genie/Genie/SystemCalls.hh
@@ -0,0 +1,17 @@
+/*	==============
+ *	SystemCalls.hh
+ *	==============
+ */
+
+#ifndef GENIE_SYSTEMCALLS_HH
+#define GENIE_SYSTEMCALLS_HH
+
+namespace Genie
+{
+	
+	int GetErrnoFromExceptionInSystemCall();
+	
+}
+
+#endif
+
