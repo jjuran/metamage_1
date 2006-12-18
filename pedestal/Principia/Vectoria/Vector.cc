354665f846094df8a521b5bf05bd0dfe6dee7c16
Joshua Juran <jjuran@metamage.com>
2006-12-18 06:21:53 +0000

    Initial check-in.

---
+++ pedestal/Principia/Vectoria/Vector.cc
@@ -0,0 +1,52 @@
+/*	==================
+ *	Vectoria/Vector.cc
+ *	==================
+ */
+
+#include "Vectoria/Vector.hh"
+
+
+namespace Vectoria
+{
+	
+	namespace CompileTests
+	{
+		
+		typedef Vector< double, 4 > V4;
+		
+		typedef V4::Type Vector_4;
+		
+		static unsigned VectorLength( const Vector_4& v )
+		{
+			return Dimensionality( v );
+		}
+		
+		static double DotProduct_4( const Vector_4& a, const Vector_4& b )
+		{
+			return DotProduct( a, b );
+		}
+		
+		static double Magnitude_4( const Vector_4& v )
+		{
+			return Magnitude( v );
+		}
+		
+		static Vector_4 UnitLength_4( const Vector_4& v )
+		{
+			return UnitLength( v );
+		}
+		
+		static Vector_4 Projection_4( const Vector_4& p, const Vector_4& q )
+		{
+			return Projection( p, q );
+		}
+		
+		static Vector_4 Perpendicular_4( const Vector_4& p, const Vector_4& q )
+		{
+			return Perpendicular( p, q );
+		}
+		
+	}
+	
+}
+
