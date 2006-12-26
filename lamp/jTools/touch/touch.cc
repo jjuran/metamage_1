9144793aa3686fed10a179e02ecd4464b938fe11
Joshua Juran <jjuran@metamage.com>
2006-12-26 21:20:32 +0000

    Rewritten for POSIX API.  Only works for nonexistent files, but at least that actually works.

--- lamp/jTools/touch/touch.cc
+++ lamp/jTools/touch/touch.cc
@@ -3,85 +3,49 @@
  *	========
  */
 
-// Universal Interfaces
-#include <MacErrors.h>
-
-// MoreFiles
-#include "MoreFilesExtras.h"
-
-// Nitrogen Extras / Utilities
-#include "Utilities/Files.h"
+// POSIX
+#include "errno.h"
+#include "fcntl.h"
+#include "unistd.h"
 
 // Orion
 #include "Orion/Main.hh"
-#include "Orion/StandardIO.hh"
-#include "SystemCalls.hh"
 
 
-namespace N = Nitrogen;
 namespace O = Orion;
 
-using std::string;
 
-
-static int TryHFSSoftTouch(const FSSpec& item);
-static int TryHFSHeavyTouch(const FSSpec& item);
-
-int O::Main(int argc, char const* const argv[])
+int O::Main( int argc, char const* const argv[] )
 {
 	// Check for sufficient number of args
-	if (argc < 2) {
-		fprintf(stderr, "touch: missing arguments\n");
+	if ( argc < 2 )
+	{
+		std::fprintf( stderr, "touch: missing arguments\n" );
+		
 		return 1;
 	}
 	
 	// Try to touch each item.  Return whether any errors occurred.
 	int fail = 0;
 	
-	for ( int index = 1;  index < argc;  ++index )
+	for ( std::size_t index = 1;  index < argc;  ++index )
 	{
-		FSSpec item;
+		const char* pathname = argv[ index ];
 		
-		try
-		{
-			item = Path2FSS( argv[ index ] );
-		}
-		catch ( ... )
-		{
-			Io::Err << "touch: " << argv[ index ] << ": nonexistent directory in pathname\n";
-			fail++;
-		}
+		int fd = open( pathname, O_WRONLY | O_CREAT, 0600 );
 		
-		if ( N::FSpTestItemExists( item ) )
+		if ( fd == -1 )
 		{
-			fail += TryHFSSoftTouch( item );
+			std::fprintf( stderr, "touch: %s: %s\n", pathname, strerror( errno ) );
+			
+			++fail;
 		}
 		else
 		{
-			fail += TryHFSHeavyTouch( item );
+			close( fd );
 		}
 	}
 	
 	return (fail == 0) ? 0 : 1;
-}
-
-int TryHFSSoftTouch(const FSSpec& item)
-{
-	OSErr err = FSpBumpDate(&item);
-	if (err != noErr) {
-		Io::Err << "touch: " << item.name << "FSpBumpDate error " << err << "\n";
-		return 1;
-	}
-	return 0;
-}
-
-int TryHFSHeavyTouch(const FSSpec& item)
-{
-	OSErr err = FSpCreate(&item, 'R*ch', 'TEXT', smRoman);
-	if (err != noErr) {
-		Io::Err << "touch: " << item.name << "FSpCreate error " << err << "\n";
-		return 1;
-	}
-	return 0;
 }
 
