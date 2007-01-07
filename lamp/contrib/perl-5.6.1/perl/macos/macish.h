675d0989e835ddd9a59f6331c1164cfbd85b48cb
Joshua Juran <jjuran@metamage.com>
2007-01-07 11:07:06 +0000

    We now have symlink(), and readlink() at least has a stub.

--- lamp/contrib/perl-5.6.1/perl/macos/macish.h
+++ lamp/contrib/perl-5.6.1/perl/macos/macish.h
@@ -202,8 +202,6 @@ clock_t	MacPerl_times(struct tms *);
 #undef HAS_GETHOSTBYNAME
 #undef HOST_NOT_FOUND
 #undef HAS_ALARM
-#undef HAS_READLINK
-#undef HAS_SYMLINK
 #undef HAS_SOCKETPAIR
 #undef HAS_TRUNCATE
 
