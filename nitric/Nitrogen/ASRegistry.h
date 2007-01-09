edf53853bae998d8e3ee6dc6b9fa3cfc9f56dece
Joshua Juran <jjuran@metamage.com>
2007-01-09 08:33:48 +0000

    The conflation of descriptor type with object class, property ID, and a few other things is unfortunate, but now we have to live with it. This is the quick fix; elegant workaround expected later.

--- nitric/Nitrogen/ASRegistry.h
+++ nitric/Nitrogen/ASRegistry.h
@@ -23,7 +23,7 @@ namespace Nitrogen
 	// Do we ever need the missing value outside an object accessor?
 	inline Nucleus::Owned< AEDescList, AETokenDisposer > MissingValue()
 	{
-		return AECreateToken< typeType >( cMissingValue );
+		return AECreateToken< typeType >( DescType( cMissingValue ) );
 	}
 	
 }
