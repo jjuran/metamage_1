527758ba478a32e1c69ed4fbdd59928ff5280b7c
Joshua Juran <jjuran@metamage.com>
2007-02-13 14:46:34 +0000

    Updated FSTree_proc for FSTree_Special changes. Renamed FSTree_pid_foo to FSTree_PID_foo to indicate that 'pid' is a type of data, not the literal name. Added FSTree_PID_fd and FSTree_PID_fd_N.

--- lamp/Genie/Genie/FileSystem/FSTree_Proc.cc
+++ lamp/Genie/Genie/FileSystem/FSTree_Proc.cc
@@ -8,8 +8,12 @@
 // Nucleus
 #include "Nucleus/Convert.h"
 
+// POSeven
+#include "POSeven/Errno.hh"
+
 // Genie
 #include "Genie/FileSystem/FSTree_Directory.hh"
+#include "Genie/IO/Stream.hh"
 #include "Genie/Process.hh"
 #include "Genie/Yield.hh"
 
@@ -18,6 +22,7 @@ namespace Genie
 {
 	
 	namespace NN = Nucleus;
+	namespace P7 = POSeven;
 	
 	
 	struct proc_Details
@@ -29,18 +34,20 @@ namespace Genie
 		static const GenieProcessTable& ItemSequence()  { return gProcessTable; }
 		
 		static FSNode ConvertToFSNode( GenieProcessTable::ProcessMap::value_type proc );
+		
+		FSNode operator()( GenieProcessTable::ProcessMap::value_type proc ) const  { return ConvertToFSNode( proc ); }
 	};
 	
-	typedef FSTree_Special< proc_Details > FSTree_proc;
+	typedef FSTree_Special_Unique< proc_Details > FSTree_proc;
 	
 	
-	class FSTree_pid : public FSTree_Virtual
+	class FSTree_PID : public FSTree_Virtual
 	{
 		private:
 			pid_t itsPID;
 		
 		public:
-			FSTree_pid( pid_t pid );
+			FSTree_PID( pid_t pid );
 			
 			std::string Name() const;
 			
@@ -63,60 +70,105 @@ namespace Genie
 			
 			std::string ReadLink() const  { return NN::Convert< std::string >( getpid() ); }
 			
-			FSTreePtr ResolveLink() const  { return FSTreePtr( new FSTree_pid( getpid() ) ); }
+			FSTreePtr ResolveLink() const  { return FSTreePtr( new FSTree_PID( getpid() ) ); }
 	};
 	
-	class FSTree_pid_cwd : public FSTree
+	
+	struct PID_fd_Details;
+	
+	typedef FSTree_Special< PID_fd_Details > FSTree_PID_fd;
+	
+	struct PID_fd_Details
+	{
+		pid_t itsPID;
+		
+		PID_fd_Details( pid_t pid ) : itsPID( pid )  {}
+		
+		static std::string Name()  { return "fd"; }
+		
+		FSTreePtr Self()   const  { return FSTreePtr( new FSTree_PID_fd( PID_fd_Details( itsPID ) ) ); }
+		FSTreePtr Parent() const  { return FSTreePtr( new FSTree_PID   ( itsPID ) ); }
+		
+		FSTreePtr Lookup( const std::string& name ) const;
+		
+		const FileDescriptorMap& ItemSequence() const  { return gProcessTable[ itsPID ].FileDescriptors(); }
+		
+		FSNode ConvertToFSNode( FileDescriptorMap::value_type file ) const;
+		
+		FSNode operator()( FileDescriptorMap::value_type file ) const  { return ConvertToFSNode( file ); }
+	};
+	
+	class FSTree_PID_fd_N : public FSTree
+	{
+		private:
+			pid_t  itsPID;
+			int    itsFD;
+		
+		public:
+			FSTree_PID_fd_N( pid_t pid, int fd ) : itsPID( pid ), itsFD( fd )  {}
+			
+			bool IsLink() const  { return true; }
+			
+			std::string Name() const;
+			
+			FSTreePtr Parent() const  { return FSTreePtr( new FSTree_PID_fd( itsPID ) ); }
+			
+			std::string ReadLink() const  { return GetFSTreePathname( ResolveLink() ); }
+			
+			FSTreePtr ResolveLink() const;
+	};
+	
+	class FSTree_PID_cwd : public FSTree
 	{
 		private:
 			pid_t itsPID;
 		
 		public:
-			FSTree_pid_cwd( pid_t pid ) : itsPID( pid )  {}
+			FSTree_PID_cwd( pid_t pid ) : itsPID( pid )  {}
 			
 			bool IsLink() const  { return true; }
 			
 			std::string Name() const  { return "cwd"; }
 			
-			FSTreePtr Parent() const  { return FSTreePtr( new FSTree_pid( itsPID ) ); }
+			FSTreePtr Parent() const  { return FSTreePtr( new FSTree_PID( itsPID ) ); }
 			
 			std::string ReadLink() const  { return GetFSTreePathname( ResolveLink() ); }
 			
 			FSTreePtr ResolveLink() const  { return gProcessTable[ itsPID ].CurrentWorkingDirectory(); }
 	};
 	
-	class FSTree_pid_exe : public FSTree
+	class FSTree_PID_exe : public FSTree
 	{
 		private:
 			pid_t itsPID;
 		
 		public:
-			FSTree_pid_exe( pid_t pid ) : itsPID( pid )  {}
+			FSTree_PID_exe( pid_t pid ) : itsPID( pid )  {}
 			
 			bool IsLink() const  { return true; }
 			
 			std::string Name() const  { return "exe"; }
 			
-			FSTreePtr Parent() const  { return FSTreePtr( new FSTree_pid( itsPID ) ); }
+			FSTreePtr Parent() const  { return FSTreePtr( new FSTree_PID( itsPID ) ); }
 			
 			std::string ReadLink() const  { return GetFSTreePathname( ResolveLink() ); }
 			
 			FSTreePtr ResolveLink() const  { return FSTreeFromFSSpec( gProcessTable[ itsPID ].ProgramFile() ); }
 	};
 	
-	class FSTree_pid_root : public FSTree
+	class FSTree_PID_root : public FSTree
 	{
 		private:
 			pid_t itsPID;
 		
 		public:
-			FSTree_pid_root( pid_t pid ) : itsPID( pid )  {}
+			FSTree_PID_root( pid_t pid ) : itsPID( pid )  {}
 			
 			bool IsLink() const  { return true; }
 			
 			std::string Name() const  { return "root"; }
 			
-			FSTreePtr Parent() const  { return FSTreePtr( new FSTree_pid( itsPID ) ); }
+			FSTreePtr Parent() const  { return FSTreePtr( new FSTree_PID( itsPID ) ); }
 			
 			std::string ReadLink() const  { return GetFSTreePathname( ResolveLink() ); }
 			
@@ -142,7 +194,7 @@ namespace Genie
 		
 		pid_t pid = std::atoi( name.c_str() );
 		
-		return FSTreePtr( new FSTree_pid( pid ) );
+		return FSTreePtr( new FSTree_PID( pid ) );
 	}
 	
 	FSNode proc_Details::ConvertToFSNode( GenieProcessTable::ProcessMap::value_type proc )
@@ -151,27 +203,73 @@ namespace Genie
 		
 		std::string name = NN::Convert< std::string >( pid );
 		
-		FSTreePtr tree( new FSTree_pid( pid ) );
+		FSTreePtr tree( new FSTree_PID( pid ) );
 		
 		return FSNode( name, tree );
 	}
 	
 	
-	FSTree_pid::FSTree_pid( pid_t pid ) : itsPID( pid )
+	FSTree_PID::FSTree_PID( pid_t pid ) : itsPID( pid )
 	{
-		Map( "cwd",  FSTreePtr( new FSTree_pid_cwd ( pid ) ) );
-		Map( "exe",  FSTreePtr( new FSTree_pid_exe ( pid ) ) );
-		Map( "root", FSTreePtr( new FSTree_pid_root( pid ) ) );
+		Map( "fd",   FSTreePtr( new FSTree_PID_fd  ( pid ) ) );
+		Map( "cwd",  FSTreePtr( new FSTree_PID_cwd ( pid ) ) );
+		Map( "exe",  FSTreePtr( new FSTree_PID_exe ( pid ) ) );
+		Map( "root", FSTreePtr( new FSTree_PID_root( pid ) ) );
 	}
 	
-	std::string FSTree_pid::Name() const
+	std::string FSTree_PID::Name() const
 	{
 		return NN::Convert< std::string >( itsPID );
 	}
 	
-	FSTreePtr FSTree_pid::Self() const
+	FSTreePtr FSTree_PID::Self() const
 	{
-		return FSTreePtr( new FSTree_pid( *this ) );
+		return FSTreePtr( new FSTree_PID( *this ) );
+	}
+	
+	FSTreePtr PID_fd_Details::Lookup( const std::string& name ) const
+	{
+		int fd = std::atoi( name.c_str() );
+		
+		const FileDescriptorMap& files = gProcessTable[ itsPID ].FileDescriptors();
+		
+		if ( files.find( fd ) == files.end() )
+		{
+			P7::ThrowErrno( ENOENT );
+		}
+		
+		return FSTreePtr( new FSTree_PID_fd_N( itsPID, fd ) );
+	}
+	
+	FSNode PID_fd_Details::ConvertToFSNode( FileDescriptorMap::value_type file ) const
+	{
+		int fd = file.first;
+		
+		std::string name = NN::Convert< std::string >( fd );
+		
+		FSTreePtr tree( new FSTree_PID_fd_N( itsPID, fd ) );
+		
+		return FSNode( name, tree );
+	}
+	
+	
+	std::string FSTree_PID_fd_N::Name() const
+	{
+		return NN::Convert< std::string >( itsFD );
+	}
+	
+	FSTreePtr FSTree_PID_fd_N::ResolveLink() const
+	{
+		FileDescriptorMap& files = gProcessTable[ itsPID ].FileDescriptors();
+		
+		if ( files.find( itsFD ) == files.end() )
+		{
+			P7::ThrowErrno( ENOENT );
+		}
+		
+		StreamHandle& stream = IOHandle_Cast< StreamHandle >( *files[ itsFD ].handle );
+		
+		return stream.GetFile();
 	}
 	
 }
