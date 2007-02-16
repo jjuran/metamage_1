920dfccd26e104ca7bb9c5a678c3182ba9a9a129
Joshua Juran <jjuran@metamage.com>
2007-02-16 07:57:55 +0000

    Removed obsolete cruft.

--- lamp/Orion/Orion/StandardIO.hh
+++ lamp/Orion/Orion/StandardIO.hh
@@ -13,57 +13,12 @@
 #include "Io/Stream.hh"
 
 
-namespace Orion
-{
-	
-	int Read ( int fd,       char* data, std::size_t byteCount );
-	int Write( int fd, const char* data, std::size_t byteCount );
-	
-}
-
 namespace Io
 {
 	
-	typedef POSeven::FileDescriptor FD;
-	
-	static const FD in  = FD( 0 );
-	static const FD out = FD( 1 );
-	static const FD err = FD( 2 );
-	
-	static Stream< FD > In ( in  ),
-	                    Out( out ),
-	                    Err( err );
-	
-	inline int Read( FD fd, char* data, std::size_t byteCount )
-	{
-		return Orion::Read( fd, data, byteCount );
-	}
-	
-	inline int Write( FD fd, const char* data, std::size_t byteCount )
-	{
-		return Orion::Write( fd, data, byteCount );
-	}
-	
-	struct FD_Details
-	{
-		typedef int Storage;
-		typedef FD  Value;
-		
-		static void SetBlocking( FD fd, bool blocking )
-		{
-			
-		}
-		
-		static int Read( FD fd, char* data, std::size_t byteCount )
-		{
-			return Orion::Read( fd, data, byteCount );
-		}
-		
-		static int Write( FD fd, const char* data, std::size_t byteCount )
-		{
-			return Orion::Write( fd, data, byteCount );
-		}
-	};
+	static Stream< POSeven::FileDescriptor > In ( POSeven::kStdIn_FileNo  ),
+	                                         Out( POSeven::kStdOut_FileNo ),
+	                                         Err( POSeven::kStdErr_FileNo );
 	
 }
 
