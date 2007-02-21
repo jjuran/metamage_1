7e9bf5c14b57f56f3e945951da363b7bdab244b7
Joshua Juran <jjuran@metamage.com>
2007-02-21 07:59:38 +0000

    Made abstract; guts moved to MacFile.

--- lamp/Genie/Genie/IO/RegularFile.cc
+++ lamp/Genie/Genie/IO/RegularFile.cc
@@ -5,92 +5,9 @@
 
 #include "Genie/IO/RegularFile.hh"
 
-// Nucleus
-#include "Nucleus/NAssert.h"
-
-// Nitrogen
-#include "Nitrogen/Files.h"
-
-// Genie
-#include "Genie/FileSystem/StatFile.hh"
-
 
 namespace Genie
 {
-	
-	static FSSpec FSSpecFromFRefNum( N::FSFileRefNum refNum )
-	{
-		FSSpec result;
-		
-		FCBPBRec pb;
-		
-		pb.ioVRefNum = 0;
-		pb.ioFCBIndx = 0;
-		pb.ioRefNum  = refNum;
-		pb.ioNamePtr = result.name;
-		
-		N::ThrowOSStatus( ::PBGetFCBInfoSync( &pb ) );
-		
-		result.vRefNum = pb.ioFCBVRefNum;
-		result.parID   = pb.ioFCBParID;
-		
-		return result;
-	}
-	
-	RegularFileHandle::RegularFileHandle( NN::Owned< N::FSFileRefNum > refNum )
-	: refNum( refNum )
-	{
-	}
-	
-	FSTreePtr RegularFileHandle::GetFile()
-	{
-		return FSTreePtr( FSTreeFromFSSpec( FSSpecFromFRefNum( refNum ) ) );
-	}
-	
-	int RegularFileHandle::SysRead( char* data, std::size_t byteCount )
-	{
-		return N::FSRead( refNum, byteCount, data );
-	}
-	
-	int RegularFileHandle::SysWrite( const char* data, std::size_t byteCount )
-	{
-		return N::FSWrite( refNum, byteCount, data );
-	}
-	
-	void RegularFileHandle::Stat( struct stat* sb ) const
-	{
-		FSSpec file = FSSpecFromFRefNum( refNum );
-		
-		StatFile( file, sb );
-	}
-	
-	off_t RegularFileHandle::Seek( off_t offset, int whence )
-	{
-		N::FSIOPosMode mode;
-		
-		switch ( whence )
-		{
-			case SEEK_SET:
-				mode = N::fsFromStart;
-				break;
-			
-			case SEEK_CUR:
-				mode = N::fsFromMark;
-				break;
-			
-			case SEEK_END:
-				mode = N::fsFromLEOF;
-				break;
-			
-			default:
-				//P7::ThrowErrno( EINVAL );
-				throw N::ParamErr();
-		}
-		
-		N::SetFPos( refNum, mode, offset );
-		
-		return N::GetFPos( refNum );
-	}
 	
 }
 
--- lamp/Genie/Genie/IO/RegularFile.hh
+++ lamp/Genie/Genie/IO/RegularFile.hh
@@ -6,15 +6,6 @@
 #ifndef GENIE_IO_REGULARFILE_HH
 #define GENIE_IO_REGULARFILE_HH
 
-// POSIX
-#include "sys/types.h"
-
-// Nucleus
-#include "Nucleus/Owned.h"
-
-// Nitrogen
-#include "Nitrogen/Files.h"
-
  // Genie
  #include "Genie/IO/File.hh"
 
@@ -22,34 +13,16 @@
 namespace Genie
 {
 	
-	namespace N = Nitrogen;
-	namespace NN = Nucleus;
-	
 	class RegularFileHandle : public FileHandle
 	{
-		private:
-			NN::Owned< N::FSFileRefNum > refNum;
-		
 		public:
 			static TypeCode Type()  { return kRegularFileType; }
 			
-			RegularFileHandle( NN::Owned< N::FSFileRefNum > refNum );
-			
-			TypeCode ActualType() const  { return Type(); }
-			
-			FSTreePtr GetFile();
+			//TypeCode ActualType() const  { return Type(); }
 			
 			unsigned int SysPoll() const  { return kPollRead | kPollWrite | kPollExcept; }
 			
-			int SysRead( char* data, std::size_t byteCount );
-			
-			int SysWrite( const char* data, std::size_t byteCount );
-			
-			//void IOCtl( unsigned long request, int* argp );
-			
-			void Stat( struct stat* sb ) const;
-			
-			off_t Seek( off_t offset, int whence );
+			virtual off_t Seek( off_t offset, int whence ) = 0;
 	};
 	
 }
