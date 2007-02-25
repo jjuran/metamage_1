0208defc6a1f48d54d8efa793cc355422138bdd4
Joshua Juran <jjuran@metamage.com>
2007-02-25 04:11:11 +0000

    Stop "using std::string;"

--- lamp/jTools/A-line/A-line/SourceDotList.cc
+++ lamp/jTools/A-line/A-line/SourceDotList.cc
@@ -18,27 +18,25 @@ namespace ALine
 	namespace N = Nitrogen;
 	namespace NN = Nucleus;
 	
-	using std::string;
-	
 	using BitsAndBytes::eos;
 	
 	
-	std::vector< string > ReadSourceDotList( const FSSpec& sourceList )
+	std::vector< std::string > ReadSourceDotList( const FSSpec& sourceList )
 	{
 		Io::TextInputAdapter< NN::Owned< N::FSFileRefNum > > input( io::open_for_reading( sourceList ) );
 		
-		std::vector< string > files;
+		std::vector< std::string > files;
 		
 		while ( input.Ready() )
 		{
-			string line = input.Read();
+			std::string line = input.Read();
 			
 			if ( line.empty()             )  continue;
 			if ( line[ 0 ] == ';'         )  continue;
 			if ( !eos( line.find( ':' ) ) )  continue;
 			
 			files.push_back( line.substr( line.find_first_not_of( "\t" ),
-			                              string::npos ) );
+			                              line.npos ) );
 		}
 		
 		return files;
