5c9c8fa9c21c75495ff1a5fc4ec754b56241f722
Joshua Juran <jjuran@metamage.com>
2007-04-05 14:19:54 +0000

    Initial check-in.

---
+++ lamp/Orion/Orion/SignalLookup.hh
@@ -0,0 +1,20 @@
+/*	===============
+ *	SignalLookup.hh
+ *	===============
+ */
+
+#ifndef ORION_SIGNALLOOKUP_HH
+#define ORION_SIGNALLOOKUP_HH
+
+// Standard C++
+
+
+namespace Orion
+{
+	
+	int SignalLookup( const char* signame );
+	
+}
+
+#endif
+
