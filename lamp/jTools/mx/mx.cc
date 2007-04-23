fc9ee921eb713680a0ed156ab736ce645efcf92c
Joshua Juran <jjuran@metamage.com>
2007-04-23 07:23:20 +0000

    Cleanup.

--- lamp/jTools/mx/mx.cc
+++ lamp/jTools/mx/mx.cc
@@ -5,16 +5,10 @@
 
 // Standard C++
 #include <algorithm>
-#include <string>
+#include <vector>
 
 // Nitrogen
 #include "Nitrogen/OpenTransportProviders.h"
-
-// Resolver
-//#include "Resolver.hh"
-
-// Highway
-//#include "HighwayDNR.hh"
 
 // Orion
 #include "Orion/Main.hh"
@@ -23,29 +17,8 @@
 
 
 namespace N = Nitrogen;
-//namespace HW = Highway;
 namespace O = Orion;
 
-using std::string;
-using std::vector;
-
-//using Resolver::MX;
-
-
-/*
-static void PrintMX( const MX& mx )
-{
-	Io::Out << mx.name << " MX " << mx.preference << " " << mx.mxHost << "\n";
-	
-	HW::IPAddress ipAddr = HW::DNSResolve( mx.mxHost );
-	
-	string ipStr = HW::EncodeIP( ipAddr );
-	
-	std::printf( "%s A %s\n",
-	             mx.mxHost.c_str(),
-	             ipStr.c_str() );
-}
-*/
 
 inline bool operator<( const InetMailExchange& a, const InetMailExchange& b )
 {
@@ -56,15 +29,7 @@ static void PrintMX( const InetMailExchange& mx )
 {
 	Io::Out << "(missing)" << " MX " << mx.preference << " " << mx.exchange << "\n";
 	
-	N::InetHost ip = N::OTInetStringToAddress( InternetServices(), (char*)mx.exchange ).addrs[ 0 ];
-	
-	/*
-	InetHostInfo hInfo;
-	OSStatus err = O::OTInetStringToAddress( (char*)mx.exchange, &hInfo );
-	N::ThrowOSStatus( err );
-	
-	N::InetHost ip = hInfo.addrs[ 0 ];
-	*/
+	N::InetHost ip = N::OTInetStringToAddress( InternetServices(), (char*) mx.exchange ).addrs[ 0 ];
 	
 	Io::Out << mx.exchange << " A " << ip << "\n";
 }
@@ -75,21 +40,13 @@ int O::Main(int argc, const char *const argv[])
 	
 	const char* domain = argv[ 1 ];
 	
-	//vector< MX > results = Resolver::LookupMX( domain );
-	
 	std::vector< InetMailExchange > results;
 	
 	results.resize( 10 );  // Should be more than enough
 	
 	N::OTInetMailExchange( InternetServices(),
-	                       (char*)domain,
+	                       (char*) domain,
 	                       results );
-	
-	/*
-	UInt16 num = 10;
-	N::ThrowOSStatus( O::OTInetMailExchange( (char*)domain, &num, &results.front() ) );
-	results.resize( num );
-	*/
 	
 	std::sort( results.begin(),
 	           results.end() );
