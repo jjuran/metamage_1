163658b5ec96c8cb6670ff484334b0a0c0cf48ea
Joshua Juran <jjuran@metamage.com>
2007-04-23 06:45:34 +0000

    Implemented inet_ntoa(), with a single sprintf() call. No Open Transport required.

--- lamp/Resolver/Resolver.cc
+++ lamp/Resolver/Resolver.cc
@@ -3,11 +3,15 @@
  *	===========
  */
 
+// Standard C/C++
+#include <cstdio>
+
 // Standard C
-#include "errno.h"
+#include <errno.h>
 
 // POSIX
-#include "netdb.h"
+#include <arpa/inet.h>
+#include <netdb.h>
 
 // Nitrogen
 #include "Nitrogen/OpenTransportProviders.h"
@@ -18,12 +22,34 @@
 #endif
 
 
-	extern "C" { int h_errno; }
-	
 	namespace N = Nitrogen;
 	
 	#pragma mark -
+	#pragma mark ¥ arpa/inet ¥
+	
+	int inet_aton( const char* name, struct in_addr* out_addr )
+	{
+		return -1;
+	}
+	
+	char* inet_ntoa( struct in_addr addr )
+	{
+		static char result[ 16 ] = { 0 };
+		
+		std::sprintf( result, "%u.%u.%u.%u", (addr.s_addr >> 24) & 0xFF,
+		                                     (addr.s_addr >> 16) & 0xFF,
+		                                     (addr.s_addr >>  8) & 0xFF,
+		                                     (addr.s_addr      ) & 0xFF );
+		
+		//::OTInetHostToString( addr.s_addr, result );
+		
+		return result;
+	}
+	
+	#pragma mark -
 	#pragma mark ¥ netdb ¥
+	
+	extern "C" { int h_errno; }
 	
 	struct hostent* gethostbyname( const char* name )
 	{
