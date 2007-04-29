0d7ed8495a89656414361d40c31a1e5df67b146d
Joshua Juran <jjuran@metamage.com>
2007-04-29 00:45:11 +0000

    Actually sets environment variables and chain launches other programs.  Yay!

--- lamp/jTools/env/env.cc
+++ lamp/jTools/env/env.cc
@@ -6,6 +6,9 @@
 // Standard C++
 #include <string>
 
+// Standard C
+#include <stdlib.h>
+
 // POSIX
 #include <unistd.h>
 
@@ -27,9 +30,20 @@ static void DumpEnvironment()
 
 int main( int argc, char const *const argv[] )
 {
-	if ( argc == 1  &&  environ != NULL )
+	while ( *++argv != NULL  &&  std::strchr( *argv, '=' ) )
+	{
+		putenv( *argv );
+	}
+	
+	if ( argv == NULL )
 	{
 		DumpEnvironment();
+	}
+	else
+	{
+		execvp( argv[0], argv );
+		
+		_exit( 127 );
 	}
 	
 	return 0;
