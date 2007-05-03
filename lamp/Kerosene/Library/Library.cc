8dc7f169b729050a5504436bfab1bd0a9f12b297
Joshua Juran <jjuran@metamage.com>
2007-05-03 12:01:25 +0000

    Segregated library routines which throw exceptions and those which do not.

--- lamp/Kerosene/Library/Library.cc
+++ lamp/Kerosene/Library/Library.cc
@@ -11,10 +11,6 @@
 #include "stdlib.h"
 #include "string.h"
 
-// Standard C++
-#include <string>
-#include <vector>
-
 // POSIX
 #include "dirent.h"
 #include "fcntl.h"
@@ -24,6 +20,10 @@
 
 // Kerosene
 #include "SystemCalls.hh"
+
+
+// Exceptions are off here
+#pragma exceptions off
 
 
 	const char* sys_errlist[] =
@@ -140,63 +140,6 @@
 		
 		std::fprintf( stderr, "%s\n", std::strerror( errno ) );
 	}
-
-	static std::string LookupPath( const char* filename )
-	{
-		const char* pathVar = getenv( "PATH" );
-		
-		if ( pathVar == NULL )
-		{
-			return "";
-		}
-		
-		const char* pathEnd = pathVar + std::strlen( pathVar );
-		
-		while ( pathVar < pathEnd )
-		{
-			const char* separator = std::strchr( pathVar, ':' );
-			
-			if ( separator == NULL )
-			{
-				separator = pathEnd;
-			}
-			
-			// Watch out for empty path elements (e.g. "/bin:/sbin:" -- last is empty)
-			if ( separator != pathVar )
-			{
-				std::string dir( pathVar, separator - pathVar );
-				
-				std::string tryPath = dir + "/" + filename;
-				
-				const char* foo = separator;
-				
-				struct ::stat sb;
-				int status = stat( tryPath.c_str(), &sb );
-				
-				if ( status == 0  &&  sb.st_mode & S_IXUSR )
-				{
-					return tryPath;
-				}
-				
-				// This seems to trash separator in CFM68K
-				//int fd = open( tryPath.c_str(), O_RDONLY );
-				
-				separator = foo;
-				
-				if ( foo != separator )
-				{
-					//std::printf( "%s\n", "Warning: foo != separator" );
-				}
-				
-				//std::printf( "%s\n", " nope" );
-			}
-			
-			// If we're at the end, then this sets pathVar > pathEnd
-			pathVar = separator + 1;
-		}
-		
-		return "";
-	}
 	
 	#pragma mark -
 	#pragma mark ¥ stdlib ¥
@@ -265,101 +208,9 @@
 		return 0;
 	}
 	
-	int execl( const char* path, const char* arg0, ... )
-	{
-		va_list va;
-		std::vector< const char* > args;
-		
-		args.push_back( arg0 );
-		
-		va_start( va, arg0 );
-		
-		while ( const char* arg = va_arg( va, const char * ) )
-		{
-			args.push_back( arg );
-		}
-		
-		va_end( va );
-		
-		args.push_back( NULL );
-		
-		const char* const* argv = &args[0];
-		
-		return execv( path, argv );
-	}
-	
-	int execle( const char* path, const char* arg0, ... )
-	{
-		va_list va;
-		std::vector< const char* > args;
-		
-		args.push_back( arg0 );
-		
-		va_start( va, arg0 );
-		
-		while ( const char* arg = va_arg( va, const char * ) )
-		{
-			args.push_back( arg );
-		}
-		
-		args.push_back( NULL );
-		
-		const char* const* envp = va_arg( va, const char* const* );
-		
-		va_end( va );
-		
-		const char* const* argv = &args[0];
-		
-		return execve( path, argv, envp );
-	}
-	
-	int execlp( const char* file, const char* arg0, ... )
-	{
-		
-		va_list va;
-		std::vector< const char* > args;
-		
-		args.push_back( arg0 );
-		
-		va_start( va, arg0 );
-		
-		while ( const char* arg = va_arg( va, const char * ) )
-		{
-			args.push_back( arg );
-		}
-		
-		va_end( va );
-		
-		args.push_back( NULL );
-		
-		const char* const* argv = &args[0];
-		
-		return execvp( file, argv );
-	}
-	
 	int execv( const char* path, const char* const argv[] )
 	{
 		return execve( path, argv, Environ() );
-	}
-	
-	int execvp( const char* file, const char* const argv[] )
-	{
-		std::string path;
-		
-		if ( std::strchr( file, '/' ) == NULL )
-		{
-			path = LookupPath( file );
-			
-			if ( path == "" )
-			{
-				Errno() = ENOENT;
-				return -1;
-			}
-			
-			file = path.c_str();
-		}
-		
-		return execv( file, argv );
 	}
 	
 	int fork()
