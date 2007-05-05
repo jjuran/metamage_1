abd6ed407f082535cdac26eff442376693cd6154
Joshua Juran <jjuran@metamage.com>
2007-05-05 18:46:23 +0000

    Moved distance() here and added dummy prototypes to make gcc happy.

--- nitric/io/io.hh
+++ nitric/io/io.hh
@@ -10,7 +10,33 @@ namespace io
 	
 	class no_input_pending {};
 	
+	
+	inline ptrdiff_t distance( const void* begin, const void* end )
+	{
+		return (uintptr_t) end - (uintptr_t) begin;
+	}
+	
+	
 	template < class FileSpec > struct filespec_traits;
+	
+	namespace dummy
+	{
+		
+		class file_spec     {};
+		class stream        {};
+		class owned_stream  {};
+		class byte_count    {};
+		
+	}
+	
+	dummy::owned_stream open_for_reading( dummy::file_spec );
+	dummy::owned_stream open_for_writing( dummy::file_spec );
+	
+	dummy::byte_count get_file_size( dummy::stream );
+	
+	dummy::byte_count read( dummy::stream, char*, dummy::byte_count );
+	
+	dummy::byte_count write( dummy::stream, const char*, dummy::byte_count );
 	
 }
 
