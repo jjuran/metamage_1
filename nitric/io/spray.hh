bfff644a4e2a493943a2397c28977882e3386d1a
Joshua Juran <jjuran@metamage.com>
2007-05-05 18:47:14 +0000

    Initial check-in.

---
+++ nitric/io/spray.hh
@@ -0,0 +1,57 @@
+// io/spray.hh
+
+#ifndef IO_SPRAY_HH
+#define IO_SPRAY_HH
+
+#ifndef NUCLEUS_OWNED_H
+#include "Nucleus/Owned.h"
+#endif
+
+#ifndef IO_IO_HH
+#include "io/io.hh"
+#endif
+
+
+namespace io
+{
+	
+	class incomplete_spray {};
+	
+	template < class details >
+	class spray_putter
+	{
+		private:
+			typedef typename details::file_spec   file_spec;
+			typedef typename details::stream      stream;
+			typedef typename details::byte_count  byte_count;
+			
+			Nucleus::Owned< stream > itsOutput;
+		
+		public:
+			spray_putter( const file_spec& file ) : itsOutput( io::open_for_writing( file ) )  {}
+			
+			void operator()( const void *begin, const void *end ) const
+			{
+				byte_count bytesToWrite = distance( begin, end );
+				
+				byte_count bytesRead = io::write( itsOutput, (const char*) begin, bytesToWrite );
+				
+				if ( bytesRead != bytesToWrite )
+				{
+					throw incomplete_spray();
+				}
+			}
+	};
+	
+	template < class Flattener, class FileSpec >
+	inline void spray_file( const FileSpec& file, typename Flattener::Parameter param )
+	{
+		spray_putter< io::filespec_traits< FileSpec > > putter( file );
+		
+		Flattener().Put( param, putter );
+	}
+	
+}
+
+#endif
+
