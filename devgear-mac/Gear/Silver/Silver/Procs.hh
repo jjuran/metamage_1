18cb9adbea7845cdd4f8caa6045bbe4b1e1afafc
Joshua Juran <jjuran@metamage.com>
2007-05-17 08:07:21 +0000

    Move trap ProcPtr definitions into their own namespace so they can be imported.

--- devgear-mac/Gear/Silver/Silver/Procs.hh
+++ devgear-mac/Gear/Silver/Silver/Procs.hh
@@ -20,22 +20,27 @@ namespace Silver
 	
 	#define DEFINE_PROCPTR( name, result, args )  typedef pascal result (*name ## ProcPtr) args
 	
-	// Traps
+	namespace Trap_ProcPtrs
+	{
+		
+		DEFINE_PROCPTR( DrawText,      void,    (const void*, short,       short   ) );
+		DEFINE_PROCPTR( GetNextEvent,  short,   (EventMask,   EventRecord*         ) );
+		DEFINE_PROCPTR( InsertMenu,    void,    (MenuHandle,  short                ) );
+		DEFINE_PROCPTR( MenuKey,       long,    (short                             ) );
+		DEFINE_PROCPTR( MenuSelect,    long,    (Point                             ) );
+		DEFINE_PROCPTR( StillDown,     Boolean, (                                  ) );
+		DEFINE_PROCPTR( TEActivate,    void,    (TEHandle                          ) );
+		DEFINE_PROCPTR( TEClick,       void,    (Point,       short,       TEHandle) );
+		DEFINE_PROCPTR( TEKey,         void,    (short,       TEHandle             ) );
+		DEFINE_PROCPTR( TEPaste,       void,    (TEHandle                          ) );
+		DEFINE_PROCPTR( AppendResMenu, void,    (MenuRef,     ResType              ) );
+		DEFINE_PROCPTR( TESetSelect,   void,    (short,       short,       TEHandle) );
+		DEFINE_PROCPTR( SystemMenu,    void,    (long                              ) );
+		DEFINE_PROCPTR( ExitToShell,   void,    (                                  ) );
+		
+	}
 	
-	DEFINE_PROCPTR( DrawText,      void,    (const void*, short,       short   ) );
-	DEFINE_PROCPTR( GetNextEvent,  short,   (EventMask,   EventRecord*         ) );
-	DEFINE_PROCPTR( InsertMenu,    void,    (MenuHandle,  short                ) );
-	DEFINE_PROCPTR( MenuKey,       long,    (short                             ) );
-	DEFINE_PROCPTR( MenuSelect,    long,    (Point                             ) );
-	DEFINE_PROCPTR( StillDown,     Boolean, (                                  ) );
-	DEFINE_PROCPTR( TEActivate,    void,    (TEHandle                          ) );
-	DEFINE_PROCPTR( TEClick,       void,    (Point,       short,       TEHandle) );
-	DEFINE_PROCPTR( TEKey,         void,    (short,       TEHandle             ) );
-	DEFINE_PROCPTR( TEPaste,       void,    (TEHandle                          ) );
-	DEFINE_PROCPTR( AppendResMenu, void,    (MenuRef,     ResType              ) );
-	DEFINE_PROCPTR( TESetSelect,   void,    (short,       short,       TEHandle) );
-	DEFINE_PROCPTR( SystemMenu,    void,    (long                              ) );
-	DEFINE_PROCPTR( ExitToShell,   void,    (                                  ) );
+	using namespace Trap_ProcPtrs;
 	
 	#undef DEFINE_PROCPTR
 	
