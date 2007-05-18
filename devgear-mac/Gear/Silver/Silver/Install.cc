79cee59d20178c169fe6b073d79a6ab039e86523
Joshua Juran <jjuran@metamage.com>
2007-05-18 05:45:32 +0000

    Initial check-in.

---
+++ devgear-mac/Gear/Silver/Silver/Install.cc
@@ -0,0 +1,91 @@
+/*
+	
+	Install.cc
+	
+	Joshua Juran
+	
+*/
+
+#include "Silver/Install.hh"
+
+// Universal Interfaces
+#include <Resources.h>
+
+// Silver
+#include "Silver/CurrentA4.hh"
+
+
+namespace Silver
+{
+
+	class TempZone
+	{
+		private:
+			THz oldZone;
+		
+		public:
+			TempZone( THz zone ) : oldZone( ::GetZone() )
+			{
+				::SetZone( zone );
+			}
+			
+			~TempZone()  { ::SetZone( oldZone ); }
+	};
+	
+	
+	OSErr gError = noErr;
+	
+	static Handle GetAndLockResource( OSType resType, short resID )
+	{
+		Handle h = Get1Resource( resType, resID );
+		
+		if ( (gError = MemError()) != noErr )
+		{
+			return NULL;
+		}
+		
+		if ( h == NULL )
+		{
+			gError = resNotFound;
+			
+			return NULL;
+		}
+		
+		DetachResource( h );
+		
+		if ( (gError = MemError()) != noErr )
+		{
+			return NULL;
+		}
+		
+		HLockHi ( h );
+		HNoPurge( h );
+		
+		return h;
+	}
+	
+	static bool LoadAndLock()
+	{
+		TempZone tempZone = SystemZone();
+		
+		Handle initCode = GetAndLockResource( 'INIT', 0 );
+		
+		return initCode != NULL;
+	}
+	
+	OSErr Install( InstallerProcPtr installer )
+	{
+		CurrentA4 a4;
+		
+		bool locked = LoadAndLock();
+		
+		if ( !locked )
+		{
+			return gError;
+		}
+		
+		return installer();
+	}
+	
+}
+
