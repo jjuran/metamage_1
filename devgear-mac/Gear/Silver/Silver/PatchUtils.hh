4758b241e091f494d166aa44e20c040c562ec826
Joshua Juran <jjuran@metamage.com>
2007-05-18 05:47:12 +0000

    Non-trap-patching code moved to other files.

--- devgear-mac/Gear/Silver/Silver/PatchUtils.hh
+++ devgear-mac/Gear/Silver/Silver/PatchUtils.hh
@@ -10,40 +10,11 @@
 #define SILVER_PATCHUTILS_HH
 
 // Universal Interfaces
-#include <Resources.h>
-
-// Needed for globals
-#include <A4Stuff.h>
+#include <Traps.h>
 
 
 namespace Silver
 {
-
-	class MyA4
-	{
-		private:
-			long oldA4;
-		
-		public:
-			MyA4() : oldA4( ::SetCurrentA4() )  {}
-			
-			~MyA4()  { ::SetA4( oldA4 ); }
-	};
-	
-	class TempZone
-	{
-		private:
-			THz oldZone;
-		
-		public:
-			TempZone( THz zone ) : oldZone( ::GetZone() )
-			{
-				::SetZone( zone );
-			}
-			
-			~TempZone()  { ::SetZone( oldZone ); }
-	};
-	
 	
 	static ProcPtr ApplyTrapPatch_( short trap, ProcPtr patchPtr )
 	{
@@ -78,30 +49,6 @@ namespace Silver
 	void RemoveTrapPatch( short trap, PatchedProcPtr patchPtr )
 	{
 		RemoveTrapPatch_( trap, reinterpret_cast< ProcPtr >( patchPtr ) );
-	}
-	
-	
-	static Handle GetAndLockResource( OSType resType, short resID )
-	{
-		Handle h = Get1Resource( resType, resID );
-		if ( h == NULL )  return NULL;
-		
-		DetachResource( h );
-		if ( ResError() != noErr )  return NULL;
-		
-		HLockHi( h );
-		HNoPurge( h );
-		
-		return h;
-	}
-	
-	static bool LoadAndLock()
-	{
-		TempZone tempZone = SystemZone();
-		
-		Handle initCode = GetAndLockResource( 'INIT', 0 );
-		
-		return initCode != NULL;
 	}
 	
 }
