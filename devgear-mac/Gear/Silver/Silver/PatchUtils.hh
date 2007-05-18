224fa0cae23e54a95da7648fdad7115e5bd7dd4b
Joshua Juran <jjuran@metamage.com>
2007-05-18 07:35:27 +0000

    Optimized for space.

--- devgear-mac/Gear/Silver/Silver/PatchUtils.hh
+++ devgear-mac/Gear/Silver/Silver/PatchUtils.hh
@@ -10,26 +10,13 @@
 #define SILVER_PATCHUTILS_HH
 
 // Universal Interfaces
-#include <Traps.h>
+#include <MacTypes.h>
 
 
 namespace Silver
 {
 	
-	static ProcPtr ApplyTrapPatch_( short trap, ProcPtr patchPtr )
-	{
-		if ( patchPtr == NULL )
-		{
-			return NULL;
-		}
-		
-		TrapType trapType = (trap & 0x0800) ? ToolTrap : OSTrap;
-		
-		ProcPtr trapPtr = ::NGetTrapAddress( trap, trapType );
-		::NSetTrapAddress( patchPtr, trap, trapType );
-		
-		return trapPtr;
-	}
+	ProcPtr ApplyTrapPatch_( short trap, ProcPtr patchPtr );
 	
 	inline void RemoveTrapPatch_( short trap, ProcPtr patchPtr )
 	{
@@ -37,7 +24,7 @@ namespace Silver
 	}
 	
 	template < class PatchedProcPtr >
-	PatchedProcPtr ApplyTrapPatch( short trap, PatchedProcPtr patchPtr )
+	inline PatchedProcPtr ApplyTrapPatch( short trap, PatchedProcPtr patchPtr )
 	{
 		ProcPtr trapPtr = ApplyTrapPatch_( trap,
 		                                   reinterpret_cast< ProcPtr >( patchPtr ) );
@@ -46,11 +33,12 @@ namespace Silver
 	}
 	
 	template < class PatchedProcPtr >
-	void RemoveTrapPatch( short trap, PatchedProcPtr patchPtr )
+	inline void RemoveTrapPatch( short trap, PatchedProcPtr patchPtr )
 	{
 		RemoveTrapPatch_( trap, reinterpret_cast< ProcPtr >( patchPtr ) );
 	}
 	
 }
+
 #endif
 
