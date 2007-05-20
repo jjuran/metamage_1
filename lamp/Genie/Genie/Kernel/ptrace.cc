f2ba3b9aa87bdb468553a524a74c651b39ad1106
Joshua Juran <jjuran@metamage.com>
2007-05-20 08:09:05 +0000

    Initial check-in.

---
+++ lamp/Genie/Genie/Kernel/ptrace.cc
@@ -0,0 +1,55 @@
+/*	=========
+ *	ptrace.cc
+ *	=========
+ */
+
+// POSIX
+#include "sys/ptrace.h"
+#include "unistd.h"
+
+// Genie
+#include "Genie/Process.hh"
+#include "Genie/SystemCallRegistry.hh"
+#include "Genie/SystemCalls.hh"
+#include "Genie/Yield.hh"
+
+
+namespace Genie
+{
+	
+	DECLARE_MODULE_INIT( Kernel_ptrace )
+	DEFINE_MODULE_INIT(  Kernel_ptrace )
+	
+	
+	static long int ptrace( enum __ptrace_request request, pid_t pid, void* addr, int data )
+	{
+		Process& current = CurrentProcess();
+		
+		try
+		{
+			switch ( request )
+			{
+				case PTRACE_TRACEME:
+					if ( current.IsBeingTraced() )
+					{
+						return current.SetErrno( EPERM );
+					}
+					
+					current.StartTracing( current.GetPPID() );
+					
+					return 0;
+				
+				default:
+					return current.SetErrno( EINVAL );
+			}
+		}
+		catch ( ... )
+		{
+			return GetErrnoFromExceptionInSystemCall();
+		}
+	}
+	
+	REGISTER_SYSTEM_CALL( ptrace );
+	
+}
+
