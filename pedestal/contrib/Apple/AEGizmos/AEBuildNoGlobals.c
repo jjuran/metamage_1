ce972102201f168374a436ce6a4b87cfb9ad2158
Joshua Juran <jjuran@metamage.com>
2007-05-21 07:23:54 +0000

    Oops, didn't want that file.

--- pedestal/contrib/Apple/AEGizmos/AEBuildNoGlobals.c
+++
@@ -1,8 +0,0 @@
-/*
- *	AppleEvent Builder Function -- without global variables for error detection
- *	by Jens Peter Alfke
- */
-
-#define NO_SYNTAX_CODES
-
-#include "AEBuild.c"			/* As you can see, most of the code is in here */
