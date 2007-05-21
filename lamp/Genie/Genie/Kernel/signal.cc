a341b75950bfad5542536f886f956db7de3ba34d
Joshua Juran <jjuran@metamage.com>
2007-05-21 03:01:29 +0000

    kill() now supports killing by process group.

--- lamp/Genie/Genie/Kernel/signal.cc
+++ lamp/Genie/Genie/Kernel/signal.cc
@@ -28,27 +28,70 @@ namespace Genie
 	DECLARE_MODULE_INIT( Kernel_signal )
 	DEFINE_MODULE_INIT(  Kernel_signal )
 	
-	static int kill( pid_t pid, int sig )
+	
+	static void send_signal( Process& process, int sig )
 	{
-		GenieProcessTable::ProcessMap::const_iterator found = gProcessTable.Map().find( pid );
-		
-		if ( found == gProcessTable.end() )
-		{
-			return CurrentProcess().SetErrno( ESRCH );
-		}
-		
 		if ( sig != 0 )
 		{
-			Process& process = *found->second;
-			
 			process.Raise( sig );
+		}
+	}
+	
+	static int kill_pid( pid_t pid, int sig )
+	{
+		if ( Process* process = FindProcess( pid ) )
+		{
+			send_signal( *process, sig );
 			
-			if ( CurrentProcess().GetPID() == pid )
+			return 0;
+		}
+		
+		return CurrentProcess().SetErrno( ESRCH );
+	}
+	
+	static int kill_pgid( pid_t pgid, int sig )
+	{
+		bool killed_any = false;
+		
+		Process& current = CurrentProcess();
+		
+		typedef GenieProcessTable::iterator iterator;
+		
+		for ( iterator it = gProcessTable.begin();  it != gProcessTable.end();  ++it )
+		{
+			Process& proc = *it->second;
+			
+			if ( proc.GetLifeStage() == kProcessReleased )
 			{
-				// If we sent ourselves the signal, handle it now
-				process.HandlePendingSignals();
+				continue;
+			}
+			
+			bool pgid_matches = pgid == 0 ? &proc          != &current
+			                              : proc.GetPGID() == pgid;
+			
+			if ( pgid_matches )
+			{
+				send_signal( proc, sig );
+				
+				killed_any = true;
 			}
 		}
+		
+		return killed_any ? 0 : current.SetErrno( ESRCH );
+		
+	}
+	
+	static int kill( pid_t pid, int sig )
+	{
+		Process& current = CurrentProcess();
+		
+		int result = pid >   0 ? kill_pid ( pid,               sig )
+		           : pid ==  0 ? kill_pgid( current.GetPGID(), sig )
+		           : pid == -1 ? kill_pgid( 0,                 sig )
+		           :             kill_pgid( -pid,              sig );
+		
+		// In case we signalled ourself
+		current.HandlePendingSignals();
 		
 		return 0;
 	}
