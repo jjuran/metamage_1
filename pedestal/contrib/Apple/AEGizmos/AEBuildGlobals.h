d8c315defa0b481815c0e0cd818be11007ddf344
Joshua Juran <jjuran@metamage.com>
2007-05-21 07:34:04 +0000

    Added missing include. Made C++-compatible.

--- pedestal/contrib/Apple/AEGizmos/AEBuildGlobals.h
+++ pedestal/contrib/Apple/AEGizmos/AEBuildGlobals.h
@@ -2,8 +2,20 @@
  *	AEBuildGlobals.h								Copyright ©1991 Apple Computer, Inc.
  */
 
+#ifndef __AEBUILD__
+#include <AEBuild.h>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 extern AEBuild_SyntaxErrType
 	AEBuild_ErrCode;					/* Examine after AEBuild returns a syntax error */
 extern long
 	AEBuild_ErrPos;						/* Index of error in format string */
+
+#ifdef __cplusplus
+}
+#endif
+
