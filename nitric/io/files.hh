45c648fc6519d8cacba1472635892c1f4de2537f
Joshua Juran <jjuran@metamage.com>
2007-05-22 05:28:29 +0000

    Initial check-in.

---
+++ nitric/io/files.hh
@@ -0,0 +1,43 @@
+// io/files.hh
+
+#ifndef IO_FILES_HH
+#define IO_FILES_HH
+
+// Standard C++
+#include <string>
+
+// Nucleus
+#ifndef NUCLEUS_CONVERT_H
+#include "Nucleus/Convert.h"
+#endif
+
+// Io
+#ifndef IO_IO_HH
+#include "io/io.hh"
+#endif
+
+
+
+namespace io
+{
+	
+	namespace dummy
+	{
+		
+		class filename_parameter  {};
+		class filename_result     {};
+		
+	}
+	
+	dummy::filename_result get_filename( dummy::file_spec );
+	
+	template < class FileSpec >
+	inline std::string get_filename_string( const FileSpec& file )
+	{
+		return Nucleus::Convert< std::string >( get_filename( file ) );
+	}
+	
+}
+
+#endif
+
