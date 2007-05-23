b92e9020e94aed48ed78057c099c87b18db448b0
Joshua Juran <jjuran@metamage.com>
2007-05-23 07:13:00 +0000

    Added existence and deletion functions.

--- nitric/io/files.hh
+++ nitric/io/files.hh
@@ -27,6 +27,8 @@ namespace io
 		class filename_parameter  {};
 		class filename_result     {};
 		
+		class file_catalog_record  {};
+		
 	}
 	
 	dummy::filename_result get_filename( dummy::file_spec );
@@ -40,6 +42,41 @@ namespace io
 	dummy::file_spec get_preceding_directory          ( dummy::file_spec );
 	dummy::file_spec get_parent_directory_of_directory( dummy::file_spec );
 	
+	
+	bool item_is_file     ( dummy::file_catalog_record );
+	bool item_is_directory( dummy::file_catalog_record );
+	
+	bool item_exists     ( dummy::file_spec );
+	bool file_exists     ( dummy::file_spec );
+	bool directory_exists( dummy::file_spec );
+	
+	bool item_exists     ( dummy::file_spec, dummy::file_catalog_record& );
+	bool file_exists     ( dummy::file_spec, dummy::file_catalog_record& );
+	bool directory_exists( dummy::file_spec, dummy::file_catalog_record& );
+	
+	void delete_file     ( dummy::file_spec );
+	void delete_file_only( dummy::file_spec );
+	
+	void delete_empty_directory     ( dummy::file_spec );
+	void delete_empty_directory_only( dummy::file_spec );
+	
+	void recursively_delete                   ( dummy::file_spec );
+	void recursively_delete_directory_contents( dummy::file_spec );
+	
+	template < class FileSpec >
+	void recursively_delete( FileSpec item )
+	{
+		if ( file_exists( item ) )
+		{
+			delete_file( item );
+		}
+		else
+		{
+			recursively_delete_directory_contents( item );
+			
+			delete_empty_directory( item );
+		}
+	}
 }
 
 #endif
