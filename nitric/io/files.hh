92731bae7d65759c25e6ad172f7c78168b3268d4
Joshua Juran <jjuran@metamage.com>
2007-06-03 20:56:53 +0000

    Added public domain / attribution notice.

--- nitric/io/files.hh
+++ nitric/io/files.hh
@@ -1,4 +1,15 @@
 // io/files.hh
+// -----------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2007 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
 
 #ifndef IO_FILES_HH
 #define IO_FILES_HH
--- nitric/io/slurp.hh
+++ nitric/io/slurp.hh
@@ -1,4 +1,15 @@
 // io/slurp.hh
+// -----------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2007 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
 
 #ifndef IO_SLURP_HH
 #define IO_SLURP_HH
