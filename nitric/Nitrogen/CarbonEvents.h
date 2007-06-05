a2c714fa0b2ee98503b7c5da7ab540127841b57c
Joshua Juran <jjuran@metamage.com>
2007-06-05 05:13:14 +0000

    Added attribution / public domain notice.

--- nitric/Nitrogen/CarbonEvents.h
+++ nitric/Nitrogen/CarbonEvents.h
@@ -1,4 +1,15 @@
-// CarbonEvents.h
+// Nitrogen/CarbonEvents.h
+// -----------------------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2002-2007 by Lisa Lippincott, Marshall Clow, and Joshua Juran.
+//
+// This code was written entirely by the above contributors, who place it
+// in the public domain.
+
 
 #ifndef NITROGEN_CARBONEVENTS_H
 #define NITROGEN_CARBONEVENTS_H
