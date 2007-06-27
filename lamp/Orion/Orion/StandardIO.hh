03ea5785eb8fff2b1cd98c8a832ad0b276458493
Joshua Juran <jjuran@metamage.com>
2007-06-27 04:50:21 +0000

    Fixed to deal with fixes for g++ 4.1.  Yay two-phase lookup.

--- lamp/Orion/Orion/StandardIO.hh
+++ lamp/Orion/Orion/StandardIO.hh
@@ -44,7 +44,7 @@ namespace Io
 	template < class T >
 	void Put( const Stream< T >& s, const char* string, std::size_t size )
 	{
-		int written = io::write( s.Get(), string, size );
+		int written = io::write< int, T >( s.Get(), string, size );
 		// verify written against size
 	}
 	
