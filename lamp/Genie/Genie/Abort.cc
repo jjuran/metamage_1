52b0bc02669845c34fa6b4541f51cd7c2a7d7fa3
Joshua Juran <jjuran@metamage.com>
2007-07-13 18:01:58 +0000

    Initial check-in. Overrides MSL's abort() to kill just the current process instead of the entire app.

---
+++ lamp/Genie/Genie/Abort.cc
@@ -0,0 +1,43 @@
+/*	========
+ *	Abort.cc
+ *	========
+ */
+
+// Standard C
+#include <signal.h>
+#include <stdlib.h>
+
+// Nitrogen
+#include "Nitrogen/Threads.h"
+
+// Genie
+#include "Genie/Process.hh"
+#include "Genie/Yield.hh"
+
+
+namespace N = Nitrogen;
+
+
+void abort()
+{
+	try
+	{
+		Genie::Process& current = Genie::CurrentProcess();
+		
+		current.SetSignalAction( SIGABRT, SIG_DFL );
+		
+		current.Raise( SIGABRT );
+		
+		current.HandlePendingSignals();
+		
+		// Probably not reached
+		
+		N::SetThreadState( N::GetCurrentThread(), N::kStoppedThreadState );
+	}
+	catch ( ... )
+	{
+	}
+	
+	::ExitToShell();
+}
+	
