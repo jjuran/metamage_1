c58c4e9f4857188c40c803e9f9df11af7484ef9f
Joshua Juran <jjuran@metamage.com>
2007-07-27 06:45:36 +0000

    Initial check-in.

---
+++ lamp/jTools/daemonize/daemonize.cc
@@ -0,0 +1,63 @@
+/*	============
+ *	daemonize.cc
+ *	============
+ */
+
+// Standard C/C++
+#include <cstdio>
+
+// Standard C
+#include <errno.h>
+#include <signal.h>
+
+// POSIX
+#include <fcntl.h>
+#include <unistd.h>
+
+
+#define STR_LEN( str )  "" str, (sizeof str - 1)
+
+#pragma export on
+
+int main( int argc, char const *const argv[] )
+{
+	if ( argc < 2 )
+	{
+		(void) write( STDERR_FILENO, STR_LEN( "Usage: daemonize command [ arg1 ... argn ]\n" ) );
+		
+		return 1;
+	}
+	
+	// Ensure we are not a process group leader
+	fork_and_exit( 0 );
+	
+	// Start a new session with no controlling terminal
+	setsid();
+	
+	// Ignore SIGHUP
+	signal( SIGHUP, SIG_IGN );
+	
+	// Ensure we can't acquire a controlling terminal by being session leader
+	fork_and_exit( 0 );
+	
+	chdir( "/" );
+	
+	int devnull = open( "/dev/null", O_RDWR, 0 );
+	
+	dup2( devnull, STDIN_FILENO  );
+	dup2( devnull, STDOUT_FILENO );
+	dup2( devnull, STDERR_FILENO );
+	
+	close( devnull );
+	
+	(void) execvp( argv[ 1 ], argv + 1 );
+	
+	bool noSuchFile = errno == ENOENT;
+	
+	std::fprintf( stderr, "%s: %s: %s\n", argv[0], argv[1], std::strerror( errno ) );
+	
+	return noSuchFile ? 127 : 126;
+}
+
+#pragma export reset
+
