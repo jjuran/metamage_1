61fd13ee8874c952c1a00754909d05f609072085
Joshua Juran <jjuran@metamage.com>
2007-08-02 22:54:50 +0000

    vfork() overhauled:  Userspace longjmp() is now invoked within the kernel.

--- lamp/LAMP-headers/vfork.h
+++ lamp/LAMP-headers/vfork.h
@@ -13,12 +13,12 @@
 extern "C" {
 #endif
 	
-	extern jmp_buf gKerosene_vfork_jmp_buf;
-	
-	int SpawnVFork();  // Only returns 0 or -1
+	int SpawnVFork( void (*LongJmp)( int ) );  // Only returns 0 or -1
 	int Kerosene_SpawnVFork();
 	
-	#define Kerosene_vfork() ( Kerosene_SpawnVFork() ? -1 : setjmp(gKerosene_vfork_jmp_buf) )
+	jmp_buf* NewJmpBuf();
+	
+	#define Kerosene_vfork() ( Kerosene_SpawnVFork() ? -1 : setjmp( *NewJmpBuf() ) )
 	
 #ifdef __cplusplus
 }
