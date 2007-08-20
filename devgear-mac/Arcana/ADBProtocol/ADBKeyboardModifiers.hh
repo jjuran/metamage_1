5ead39534aed94ec20cf32498b1177489e120591
Joshua Juran <jjuran@metamage.com>
2007-08-20 02:41:28 +0000

    Initial check-in.

---
+++ devgear-mac/Arcana/ADBProtocol/ADBKeyboardModifiers.hh
@@ -0,0 +1,13 @@
+/*	=======================
+ *	ADBKeyboardModifiers.hh
+ *	=======================
+ */
+
+// Arcana
+#include "ADBProtocol.hh"
+
+
+bool KeyboardModifiersAreDistinct( Nitrogen::ADBAddress address );
+
+void SetKeyboardModifiersDistinctness( Nitrogen::ADBAddress address, bool distinct );
+
