53e626eecdf97f7f072ada7bb0fb17aa09a56b9a
Joshua Juran <jjuran@metamage.com>
2007-08-31 07:21:24 +0000

    Refactored for reusability. HTTP header comparisons are now case-insensitive. Fixed a few bugs that rarely if ever occurred in practice, but were definitely wrong.

--- lamp/jTools/htget/htget.cc
+++ lamp/jTools/htget/htget.cc
@@ -16,9 +16,14 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
+// Iota
+#include "iota/strings.hh"
+
 // Nucleus
+#include "Nucleus/Convert.h"
 #include "Nucleus/NAssert.h"
 
 // POSeven
@@ -39,6 +44,205 @@ namespace p7 = poseven;
 namespace O = Orion;
 
 
+namespace HTTP
+{
+	
+	static std::string RequestLine( const std::string& method, const std::string& urlPath )
+	{
+		return method + " " + urlPath + " HTTP/1.0" "\r\n";
+	}
+	
+	class MalformedHeader {};
+	
+	struct HeaderIndexTuple
+	{
+		std::size_t header_offset;
+		std::size_t colon_offset;
+		std::size_t value_offset;
+		std::size_t crlf_offset;
+	};
+	
+	inline HeaderIndexTuple MakeHeaderIndexTuple( std::size_t header,
+	                                              std::size_t colon,
+	                                              std::size_t value,
+	                                              std::size_t crlf )
+	{
+		HeaderIndexTuple result;
+		
+		result.header_offset = header;
+		result.colon_offset  = colon;
+		result.value_offset  = value;
+		result.crlf_offset   = crlf;
+		
+		return result;
+	}
+	
+	typedef std::vector< HeaderIndexTuple > HeaderIndex;
+	
+	static HeaderIndex MakeHeaderIndex( const char* header_stream, const char* end )
+	{
+		HeaderIndex result;
+		
+		const char* p = header_stream;
+		
+		while ( p < end )
+		{
+			const char* header = p;
+			
+			const char* crlf = std::strstr( p, "\r\n" );
+			
+			if ( crlf == p )
+			{
+				// Empty line indicates end of headers
+				return result;
+			}
+			
+			ASSERT( crlf != NULL );
+			
+			const char* colon = std::strchr( p, ':' );
+			
+			if ( colon == NULL  ||  colon > crlf )
+			{
+				throw MalformedHeader();
+			}
+			
+			while ( *++p != ' ' )  continue;
+			
+			const char* value = p;
+			
+			HeaderIndexTuple tuple = MakeHeaderIndexTuple( header - header_stream,
+			                                               colon  - header_stream,
+			                                               value  - header_stream,
+			                                               crlf   - header_stream );
+			
+			result.push_back( tuple );
+			
+			p = crlf + STRLEN( "\r\n" );
+		}
+		
+		// Ran out of data without finding end of headers
+		throw MalformedHeader();
+	}
+	
+	static bool strings_case_insensitively_equal( const char* a, std::size_t a_len,
+	                                              const char* b, std::size_t b_len )
+	{
+		if ( a_len != b_len )  return false;
+		
+		for ( const char* a_end = a + a_len;  a < a_end;  ++a, ++b )
+		{
+			if ( std::tolower( *a ) != std::tolower( *b ) )
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	static const HeaderIndexTuple* FindHeaderInStream( const char*         stream,
+	                                                   const HeaderIndex&  index,
+	                                                   const char*         name,
+	                                                   std::size_t         name_length )
+	{
+		for ( HeaderIndex::const_iterator it = index.begin();  it != index.end();  ++it )
+		{
+			const char* header = stream + it->header_offset;
+			
+			std::size_t length = it->colon_offset - it->header_offset;
+			
+			if ( strings_case_insensitively_equal( header, length, name, name_length ) )
+			{
+				return &*it;
+			}
+		}
+		
+		return NULL;
+	}
+	
+	
+	struct Header
+	{
+		std::string name;
+		std::string value;
+		
+		Header()  {}
+		
+		Header( const std::string& n, const std::string& v ) : name( n ), value( v )  {}
+	};
+	
+	static std::string HeaderLine( const std::string& name, const std::string& value )
+	{
+		return name + ": " + value + "\r\n";
+	}
+	
+	static std::string HeaderLine( const Header& header )
+	{
+		return HeaderLine( header.name, header.value );
+	}
+	
+	static void SendMessageHeader( p7::fd_t            out,
+	                               const std::string&  message_header )
+	{
+		p7::write( out, message_header.data(), message_header.size() );
+	}
+	
+	static void SendMessageBody( p7::fd_t  out,
+	                             p7::fd_t  message_body )
+	{
+		char buffer[ 4096 ];
+		
+		try
+		{
+			while ( int bytes_read = p7::read( message_body, buffer, 4096 ) )
+			{
+				p7::write( out, buffer, bytes_read );
+			}
+		}
+		catch ( const io::end_of_input& )
+		{
+		}
+	}
+	
+	static void SendMessage( p7::fd_t            out,
+	                         const std::string&  message_header,
+	                         p7::fd_t            message_body )
+	{
+		SendMessageHeader( out, message_header );
+		SendMessageBody  ( out, message_body   );
+	}
+	
+	static std::size_t GetContentLength( p7::fd_t message_body )
+	{
+		struct stat sb;
+		
+		P7::ThrowPOSIXResult( fstat( message_body, &sb ) );
+		
+		if ( !S_ISREG( sb.st_mode ) )
+		{
+			P7::ThrowErrno( ESPIPE );
+		}
+		
+		return sb.st_size;
+	}
+	
+	static Header GetContentLengthHeader( p7::fd_t message_body )
+	{
+		return Header( "Content-Length", NN::Convert< std::string >( GetContentLength( message_body ) ) );
+	}
+	
+}
+
+namespace poseven
+{
+	
+	static void connect( fd_t sock, const sockaddr_in& serverAddr )
+	{
+		P7::ThrowPOSIXResult( ::connect( sock, (const sockaddr*) &serverAddr, sizeof (sockaddr_in) ) );
+	}
+	
+}
+
 namespace htget
 {
 	
@@ -46,52 +250,37 @@ namespace htget
 	{
 		private:
 			p7::fd_t itsReceiver;
-			p7::fd_t itsSocket;
 			std::string itsReceivedData;
+			std::size_t itsStartOfHeaders;
+			std::size_t itsPlaceToLookForEndOfHeaders;
 			std::size_t itsContentLength;
 			std::size_t itsContentBytesReceived;
 			bool itHasReceivedAllHeaders;
 			bool itsContentLengthIsKnown;
 			bool itHasReachedEndOfInput;
 			
+			void ReceiveContent( const char* data, std::size_t byteCount );
 			void ReceiveData( const char* data, std::size_t byteCount );
 			bool IsComplete();
 		
 		public:
-			HTTPClientTransaction( p7::fd_t            sock,
-			                       const sockaddr_in&  serverAddr )
+			HTTPClientTransaction()
 			:
 				itsReceiver            ( p7::stdout_fileno ),
-				itsSocket              ( sock ),
+				itsStartOfHeaders      ( 0 ),
+				itsPlaceToLookForEndOfHeaders(),
 				itsContentLength       ( 0 ),
 				itsContentBytesReceived( 0 ),
 				itHasReceivedAllHeaders( false ),
 				itsContentLengthIsKnown( false ),
 				itHasReachedEndOfInput ( false )
 			{
-				P7::ThrowPOSIXResult( connect( sock, (const sockaddr*) &serverAddr, sizeof (sockaddr_in) ) );
 			}
 			
 			~HTTPClientTransaction()  {}
 			
-			void Download();
+			void Download( p7::fd_t socket );
 	};
-	
-}
-
-namespace Nucleus
-{
-	
-	template <>
-	struct OwnedDefaults< htget::HTTPClientTransaction* >
-	{
-		typedef DisposeWithDelete Disposer;
-	};
-	
-}
-
-namespace htget
-{
 	
 	namespace Globals
 	{
@@ -101,8 +290,6 @@ namespace htget
 		bool gSaveToFile      = false;
 		
 		std::string gSaveLocation;
-		
-		NN::Owned< HTTPClientTransaction* > gTransaction;
 		
 	}
 	
@@ -141,7 +328,7 @@ namespace htget
 		return false;
 	}
 	
-	void HTTPClientTransaction::Download()
+	void HTTPClientTransaction::Download( p7::fd_t socket )
 	{
 		while ( true )
 		{
@@ -158,7 +345,7 @@ namespace htget
 				bytesToRead = std::min( bytesToRead, bytesToGo );
 			}
 			
-			int received = read( itsSocket, data, bytesToRead );
+			int received = read( socket, data, bytesToRead );
 			
 			if ( received == 0 )
 			{
@@ -178,43 +365,11 @@ namespace htget
 		(void) IsComplete();
 	}
 	
-	static std::string GetHTTPHeader( const std::string&  headerName,
-	                                  const std::string&  allHeaders )
+	void HTTPClientTransaction::ReceiveContent( const char* data, std::size_t byteCount )
 	{
-		typedef std::size_t Position;
+		itsContentBytesReceived += byteCount;
 		
-		// FIXME:  Be case-insensitive.
-		
-		// Find the header in the headers
-		Position headerPos = allHeaders.find( headerName );
-		
-		if ( headerPos == allHeaders.npos )
-		{
-			return "";
-		}
-		
-		// This is never npos if allHeaders is valid.
-		Position eol = allHeaders.find( "\r\n", headerPos );
-		
-		ASSERT( eol != allHeaders.npos );
-		
-		return allHeaders.substr( headerPos, eol - headerPos );
-	}
-	
-	static std::string GetHTTPHeaderValue( const std::string& headerName, const std::string& allHeaders )
-	{
-		typedef std::size_t Position;
-		
-		std::string header = GetHTTPHeader( headerName, allHeaders );
-		
-		Position valuePos = header.find_first_not_of( " \t", headerName.size() + 1 );
-		
-		if ( valuePos == header.npos )
-		{
-			return "";
-		}
-		
-		return header.substr( valuePos, header.npos );
+		io::write( itsReceiver, data, byteCount );
 	}
 	
 	void HTTPClientTransaction::ReceiveData( const char* data, std::size_t byteCount )
@@ -224,70 +379,99 @@ namespace htget
 		{
 			// We haven't received all the headers yet.
 			// We need to concatenate all received data to check for the EOH marker.
-			itsReceivedData += std::string( data, byteCount );
-			std::size_t eohMarker = itsReceivedData.find( "\r\n\r\n" );
+			itsReceivedData.append( data, byteCount );
 			
-			if ( eohMarker != itsReceivedData.npos )
+			if ( itsStartOfHeaders == 0 )
 			{
-				// Found it, so we've got all the headers now
-				itHasReceivedAllHeaders = true;
-				
-				// The first CRLF ends the headers
-				std::size_t endOfHeaders = eohMarker + 2;
-				
-				// The content starts after the second
-				std::size_t startOfContent = endOfHeaders + 2;
-				
-				std::string allHeaders = itsReceivedData.substr( 0, startOfContent );
-				
-				if ( gDumpHeaders )
+				if ( std::size_t crlf = itsReceivedData.find( "\r\n" ) )
 				{
-					io::write( itsReceiver, allHeaders.data(), allHeaders.size() );
+					itsStartOfHeaders = crlf + STRLEN( "\r\n" );
+					
+					itsPlaceToLookForEndOfHeaders = itsStartOfHeaders;
 				}
+			}
+			
+			if ( itsPlaceToLookForEndOfHeaders == 0 )
+			{
+				return;
+			}
+			
+			if ( itsReceivedData.size() - itsPlaceToLookForEndOfHeaders < STRLEN( "\r\n" "\r\n" ) )
+			{
+				return;
+			}
+			
+			std::size_t eohMarker = itsReceivedData.find( "\r\n" "\r\n", itsPlaceToLookForEndOfHeaders );
+			
+			if ( eohMarker == itsReceivedData.npos )
+			{
+				itsPlaceToLookForEndOfHeaders = itsReceivedData.size() - STRLEN( "\r\n" "\r\n" ) + 1;
 				
-				if ( gExpectNoContent )
-				{
-					// itsContentLength and itsContentBytesReceived are already 0
-					itsContentLengthIsKnown = true;
-					return;
-				}
+				return;
+			}
+			
+			// Found it, so we've got all the headers now
+			itHasReceivedAllHeaders = true;
+			
+			// The first CRLF ends the headers
+			std::size_t endOfHeaders = eohMarker + 2;
+			
+			// The content starts after the second
+			std::size_t startOfContent = endOfHeaders + 2;
+			
+			if ( gDumpHeaders )
+			{
+				io::write( itsReceiver, itsReceivedData.data(), startOfContent );
+			}
+			
+			if ( gExpectNoContent )
+			{
+				// itsContentLength and itsContentBytesReceived are already 0
+				itsContentLengthIsKnown = true;
+				return;
+			}
+			
+			if ( gSaveToFile )
+			{
+				itsReceiver = p7::open( gSaveLocation.c_str(), O_WRONLY | O_CREAT | O_EXCL, 0644 ).Release();
+			}
+			
+			// Anything left over is content
+			std::size_t leftOver = itsReceivedData.size() - startOfContent;
+			
+			// Start writing content if we have any
+			if ( leftOver > 0 )
+			{
+				ReceiveContent( itsReceivedData.data() + startOfContent, leftOver );
 				
-				if ( gSaveToFile )
-				{
-					itsReceiver = p7::open( gSaveLocation.c_str(), O_WRONLY | O_CREAT | O_EXCL, 0644 ).Release();
-				}
+				itsReceivedData.resize( startOfContent );
+			}
+			
+			const char* header_stream = itsReceivedData.data() + itsStartOfHeaders;
+			
+			HTTP::HeaderIndex index = HTTP::MakeHeaderIndex( header_stream, &*itsReceivedData.end() );
+			
+			if ( const HTTP::HeaderIndexTuple* contentLengthTuple = HTTP::FindHeaderInStream( header_stream,
+			                                                                                  index,
+			                                                                                  STR_LEN( "Content-Length" ) ) )
+			{
+				std::string contentLength( header_stream + contentLengthTuple->value_offset,
+				                           header_stream + contentLengthTuple->crlf_offset );
 				
-				// Anything left over is content
-				std::size_t leftOver = byteCount - startOfContent;
-				itsContentBytesReceived = leftOver;
-				
-				// Start writing content if we have any
-				if ( leftOver > 0 )
-				{
-					io::write( itsReceiver, itsReceivedData.data() + startOfContent, leftOver );
-				}
-				
-				std::string contentLength = GetHTTPHeaderValue( "Content-Length", allHeaders );
-				
-				if ( !contentLength.empty() )
-				{
-					// Now get the *real* value, as opposed to its textual representation
-					itsContentLength = std::atoi( contentLength.c_str() );
-					itsContentLengthIsKnown = true;
-				}
-				else
-				{
-					// itsContentLength is already set to 0
-					// itsContentLengthIsKnown is already false
-				}
+				// Now get the *real* value, as opposed to its textual representation
+				itsContentLength = std::atoi( contentLength.c_str() );
+				itsContentLengthIsKnown = true;
+			}
+			else
+			{
+				// itsContentLength is already set to 0
+				// itsContentLengthIsKnown is already false
 			}
 		}
 		else
 		{
 			// We already have the headers, just count and write the data
-			itsContentBytesReceived += byteCount;
-			
-			io::write( itsReceiver, data, byteCount );
+			ReceiveContent( data, byteCount );
 		}
 	}
 	
@@ -324,17 +508,6 @@ namespace htget
 		return true;
 	}
 	
-	
-	static NN::Owned< HTTPClientTransaction* >
-	NewTransaction( p7::fd_t            socket,
-	                const sockaddr_in&  addr )
-	{
-		return NN::Owned< HTTPClientTransaction* >::Seize
-		(
-			new HTTPClientTransaction( socket, addr )
-		);
-	}
-	
 }
 
 
@@ -367,12 +540,14 @@ static struct in_addr ResolveHostname( const char* hostname )
 	return addr;
 }
 
+/*
 static void WriteLine( p7::fd_t stream, const std::string& text )
 {
 	std::string line = text + "\r\n";
 	
 	io::write( stream, line.data(), line.size() );
 }
+*/
 
 int O::Main( int argc, argv_t argv )
 {
@@ -463,15 +638,19 @@ int O::Main( int argc, argv_t argv )
 		gSaveLocation = pathname;
 	}
 	
-	gTransaction = htget::NewTransaction( sock, inetAddress );
+	p7::connect( sock, inetAddress );
 	
-	WriteLine( sock, method + " " + urlPath + " HTTP/1.0" );
-	WriteLine( sock, "Host: " + hostname );
-	WriteLine( sock, "" );
+	std::string message_header =   HTTP::RequestLine( method, urlPath )
+	                             + HTTP::HeaderLine( "Host", hostname )
+	                             + "\r\n";
+	
+	HTTP::SendMessageHeader( sock, message_header );
 	
 	shutdown( sock, SHUT_WR );
 	
-	gTransaction.Get()->Download();
+	htget::HTTPClientTransaction transaction;
+	
+	transaction.Download( sock );
 	
 	shutdown( sock, SHUT_RD );
 	
