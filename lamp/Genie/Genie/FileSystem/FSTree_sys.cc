2a3fade69d05b8f7908d27174fa46440a8437f88
Joshua Juran <jjuran@metamage.com>
2007-09-01 09:50:55 +0000

    Added /sys/mac/proc.

--- lamp/Genie/Genie/FileSystem/FSTree_sys.cc
+++ lamp/Genie/Genie/FileSystem/FSTree_sys.cc
@@ -5,8 +5,18 @@
 
 #include "Genie/FileSystem/FSTree_sys.hh"
 
+// Nucleus
+#include "Nucleus/Convert.h"
+
 // Nitrogen
 #include "Nitrogen/Gestalt.h"
+#include "Nitrogen/Processes.h"
+
+// POSeven
+#include "POSeven/Errno.hh"
+
+// BitsAndBytes
+#include "HexStrings.hh"
 
 // Genie
 #include "Genie/FileSystem/FSTree_Directory.hh"
@@ -18,6 +28,8 @@ namespace Genie
 {
 	
 	namespace N = Nitrogen;
+	namespace NN = Nucleus;
+	namespace P7 = POSeven;
 	
 	
 	class FSTree_sys_kernel : public FSTree_Virtual
@@ -44,6 +56,37 @@ namespace Genie
 			FSTreePtr Parent() const  { return GetSingleton< FSTree_sys >(); }
 	};
 	
+	struct sys_mac_proc_Details
+	{
+		static std::string Name()  { return "proc"; }
+		
+		static FSTreePtr Parent()  { return GetSingleton< FSTree_sys_mac >(); }
+		
+		static FSTreePtr Lookup( const std::string& name );
+		
+		static const N::Process_Container& ItemSequence()  { return N::Processes(); }
+		
+		static FSNode ConvertToFSNode( const N::ProcessSerialNumber& proc );
+		
+		FSNode operator()( const N::ProcessSerialNumber& proc ) const  { return ConvertToFSNode( proc ); }
+	};
+	
+	typedef FSTree_Special< sys_mac_proc_Details > FSTree_sys_mac_proc;
+	
+	class FSTree_sys_mac_proc_PSN : public FSTree_Virtual
+	{
+		private:
+			ProcessSerialNumber itsPSN;
+		
+		public:
+			FSTree_sys_mac_proc_PSN( const ProcessSerialNumber& psn );
+			
+			std::string Name() const;
+			
+			FSTreePtr Parent() const  { return GetSingleton< FSTree_sys_mac_proc >(); }
+	};
+	
+	
 	class FSTree_sys_mac_gestalt : public FSTree
 	{
 		public:
@@ -53,7 +96,7 @@ namespace Genie
 			
 			std::string Name() const  { return OnlyName(); }
 			
-			FSTreePtr Parent() const  { return FSTreePtr( GetSingleton< FSTree_sys_mac >() ); }
+			FSTreePtr Parent() const  { return GetSingleton< FSTree_sys_mac >(); }
 			
 			mode_t FileTypeMode() const  { return S_IFCHR; }
 			mode_t FilePermMode() const  { return S_IRUSR; }
@@ -75,7 +118,73 @@ namespace Genie
 	
 	FSTree_sys_mac::FSTree_sys_mac()
 	{
+		MapSingleton< FSTree_sys_mac_proc    >();
 		MapSingleton< FSTree_sys_mac_gestalt >();
+	}
+	
+	
+	static UInt32 DecodeHex32( const char* begin, const char* end )
+	{
+		using BitsAndBytes::DecodeHex;
+		
+		std::string decoded = DecodeHex( std::string( begin, end ) );
+		
+		if ( decoded.size() != sizeof (UInt32) )
+		{
+			P7::ThrowErrno( ENOENT );
+		}
+		
+		return *(UInt32*) decoded.data();
+	}
+	
+	FSTreePtr sys_mac_proc_Details::Lookup( const std::string& name_string )
+	{
+		ProcessSerialNumber psn = { 0, 0 };
+		
+		const char* name = name_string.c_str();
+		
+		if ( const char* hyphen = std::strchr( name, '-' ) )
+		{
+			psn.highLongOfPSN = DecodeHex32( name, hyphen );
+			
+			name = hyphen + 1;
+		}
+		
+		psn.lowLongOfPSN = DecodeHex32( name, &*name_string.end() );
+		
+		return FSTreePtr( new FSTree_sys_mac_proc_PSN( psn ) );
+	}
+	
+	static std::string NameFromPSN( const ProcessSerialNumber& psn )
+	{
+		using BitsAndBytes::EncodeAsHex;
+		
+		std::string name = EncodeAsHex( psn.lowLongOfPSN );
+		
+		if ( psn.highLongOfPSN != 0 )
+		{
+			name = EncodeAsHex( psn.highLongOfPSN ) + "-" + name;
+		}
+		
+		return name;
+	}
+	
+	FSNode sys_mac_proc_Details::ConvertToFSNode( const N::ProcessSerialNumber& psn )
+	{
+		FSTreePtr tree( new FSTree_sys_mac_proc_PSN( psn ) );
+		
+		return FSNode( NameFromPSN( psn ), tree );
+	}
+	
+	
+	FSTree_sys_mac_proc_PSN::FSTree_sys_mac_proc_PSN( const ProcessSerialNumber& psn ) : itsPSN( psn )
+	{
+		
+	}
+	
+	std::string FSTree_sys_mac_proc_PSN::Name() const
+	{
+		return NameFromPSN( itsPSN );
 	}
 	
 	
@@ -93,6 +202,7 @@ namespace Genie
 			void IOCtl( unsigned long request, int* argp );
 	};
 	
+	
 	void GestaltDeviceHandle::IOCtl( unsigned long request, int* argp )
 	{
 		N::GestaltSelector selector = N::GestaltSelector( request );
@@ -104,7 +214,6 @@ namespace Genie
 			*argp = value;
 		}
 	}
-	
 	
 	boost::shared_ptr< IOHandle > FSTree_sys_mac_gestalt::Open( OpenFlags flags ) const
 	{
