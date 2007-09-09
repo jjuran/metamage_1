f7c378d80009cfc90cffe2007f2b8fc1e2fa4763
Joshua Juran <jjuran@metamage.com>
2007-09-09 22:45:47 +0000

    Added support for PowerPC/Mach-O.

--- pedestal/Practica/Backtrace/StackCrawl.hh
+++ pedestal/Practica/Backtrace/StackCrawl.hh
@@ -14,19 +14,37 @@ namespace Backtrace
 {
 	
 	typedef const struct OpaqueCode68K* ReturnAddr68K;
-	typedef const struct OpaqueCodePPC* ReturnAddrPPC;
 	
-	union ReturnAddr
-	{
-		ReturnAddr68K addr68K;
-		ReturnAddrPPC addrPPC;
-	};
+	typedef const struct OpaqueCodePPCFrag* ReturnAddrPPCFrag;
+	
+	typedef const struct OpaqueCodePowerPCMachO* ReturnAddrPPCMachO;
+	
+#ifdef __MACH__
+	
+	typedef ReturnAddrPPCMachO ReturnAddrPPC;
+	
+#else
+	
+	typedef ReturnAddrPPCFrag ReturnAddrPPC;
+	
+#endif
 	
 	enum Architecture
 	{
-		kArchClassic68K = 0,
-		kArchPowerPCCFM = 1,
-		kArchCFM68K     = 1 << 4  // not supported
+		kArchClassic68K   = 0,
+		kArchPowerPCFrag  = 1,
+		kArchCFM68K       = 0 | (1 << 4),  // not supported
+		kArchPowerPCMachO = 1 | (1 << 4),
+		
+	#ifdef __MACH__
+		
+		kArchPowerPCNative = kArchPowerPCMachO
+		
+	#else
+		
+		kArchPowerPCNative = kArchPowerPCFrag
+		
+	#endif
 	};
 	
 	struct CallRecord
@@ -34,15 +52,19 @@ namespace Backtrace
 		union
 		{
 			ReturnAddr68K addr68K;
-			ReturnAddrPPC addrPPC;
+			
+			ReturnAddrPPCFrag addrPPCFrag;
+			
+			ReturnAddrPPCMachO addrPPCMachO;
 		};
 		
 		Architecture arch;
 		
 		CallRecord()  {}
 		
-		CallRecord( ReturnAddr68K addr ) : addr68K( addr ), arch( kArchClassic68K )  {}
-		CallRecord( ReturnAddrPPC addr ) : addrPPC( addr ), arch( kArchPowerPCCFM )  {}
+		CallRecord( ReturnAddr68K      addr ) : addr68K     ( addr ), arch( kArchClassic68K   )  {}
+		CallRecord( ReturnAddrPPCFrag  addr ) : addrPPCFrag ( addr ), arch( kArchPowerPCFrag  )  {}
+		CallRecord( ReturnAddrPPCMachO addr ) : addrPPCMachO( addr ), arch( kArchPowerPCMachO )  {}
 	};
 	
 	
