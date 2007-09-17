c620421ea8cacb9fd40ffc7b882499a7e662d4a2
Joshua Juran <jjuran@metamage.com>
2007-09-17 09:00:35 +0000

    Added support for C translation units. It turns out that making environ_t an array type breaks the link.

--- pedestal/Practica/iota/environ.hh
+++ pedestal/Practica/iota/environ.hh
@@ -13,8 +13,10 @@
 #endif
 
 
+#ifdef __cplusplus
 namespace iota
 {
+#endif
 	
 #if defined( __GNUC__ ) || defined( __MACH__ )
 	
@@ -27,27 +29,37 @@ namespace iota
 #endif
 	
 	typedef env_t const *envp_t;
-	typedef env_t const  environ_t[];
+	typedef env_t const *environ_t;
 	
+#ifdef __cplusplus
 }
+#endif
 
 #if TARGET_CPU_68K && !TARGET_RT_MAC_CFM
 	
 	// ToolScratch:  addr = 0x09CE, len = 8 bytes
 	
-# ifdef __cplusplus
-	
-	static iota::environ_t& environ = *reinterpret_cast< iota::environ_t* >(LMGetToolScratch() + 4);
-	
-# else
-	
-	#define                 environ  (*                 (iota::environ_t*) (LMGetToolScratch() + 4) )
-	
-# endif
+	#ifdef __cplusplus
+		
+		static iota::environ_t& environ = *reinterpret_cast< iota::environ_t* >(LMGetToolScratch() + 4);
+		
+	#else
+		
+		#define                 environ  (*                 (      environ_t*) (LMGetToolScratch() + 4) )
+		
+	#endif
 	
 #else
 	
-	extern iota::environ_t environ;
+	#ifdef __cplusplus
+		
+		extern iota::environ_t environ;
+		
+	#else
+		
+		extern environ_t environ;
+		
+	#endif
 	
 #endif
 
