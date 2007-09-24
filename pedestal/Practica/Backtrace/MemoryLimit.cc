a9a9b9094341167c9119382f8a2c734a1058a015
Joshua Juran <jjuran@metamage.com>
2007-09-24 01:41:32 +0000

    Don't apply the memory limit on OS X.

--- pedestal/Practica/Backtrace/MemoryLimit.cc
+++ pedestal/Practica/Backtrace/MemoryLimit.cc
@@ -17,6 +17,15 @@ namespace Backtrace
 	
 #ifdef __MACOS__
 	
+	static bool InOSX()
+	{
+		long response = 0;
+		
+		OSErr err = ::Gestalt( gestaltSystemVersion, &response );
+		
+		return err == noErr  &&  response >= 0x1000;
+	}
+	
 	static UInt32 LogicalRAMSize()
 	{
 		long response = 0;
@@ -28,6 +37,16 @@ namespace Backtrace
 	
 	const void* MemoryLimit()
 	{
+		if ( TARGET_API_MAC_CARBON )
+		{
+			static bool inOSX = InOSX();
+			
+			if ( inOSX )
+			{
+				return 0xffffffff;
+			}
+		}
+		
 		static UInt32 logicalRAMSize = LogicalRAMSize();
 		
 		return reinterpret_cast< const void* >( logicalRAMSize );
