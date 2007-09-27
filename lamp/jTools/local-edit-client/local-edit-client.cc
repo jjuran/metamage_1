50bf96a896704f8d30dbc48af02ac127ed854950
Joshua Juran <jjuran@metamage.com>
2007-09-27 06:43:23 +0000

    Compute the message body's MD5 hash and send a Content-MD5 header.

--- lamp/jTools/local-edit-client/local-edit-client.cc
+++ lamp/jTools/local-edit-client/local-edit-client.cc
@@ -21,6 +21,7 @@
 
 // Arcana
 #include "HTTP.hh"
+#include "MD5.hh"
 
 // Orion
 #include "Orion/GetOptions.hh"
@@ -32,11 +33,110 @@ namespace p7 = poseven;
 namespace O = Orion;
 
 
+static MD5::Result MD5DigestFile( p7::fd_t input )
+{
+	MD5::Engine md5;
+	
+	char buffer[ 4096 ];
+	
+	try
+	{
+		while ( std::size_t bytes_read = p7::read( input, buffer, 4096 ) )
+		{
+			if ( bytes_read == 4096 )
+			{
+				for ( const char* p = buffer;  p < buffer + 4096;  p += 64 )
+				{
+					md5.DoBlock( p );
+				}
+			}
+			else
+			{
+				const char* end_of_blocks = buffer + bytes_read - bytes_read % 64;
+				
+				for ( const char* p = buffer;  p < end_of_blocks;  p += 64 )
+				{
+					md5.DoBlock( p );
+				}
+				
+				md5.Finish( end_of_blocks, bytes_read % 64 * 8 );
+			}
+		}
+	}
+	catch ( const io::end_of_input& )
+	{
+	}
+	
+	return md5.GetResult();
+}
+
+static void EncodeBase64Triplet( unsigned triplet, unsigned char buffer[4] )
+{
+	const char* code = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	
+	buffer[0] = code[ (triplet >> 18)            ];
+	buffer[1] = code[ (triplet >> 12) & 0x00003f ];
+	buffer[2] = code[ (triplet >>  6) & 0x00003f ];
+	buffer[3] = code[ (triplet >>  0) & 0x00003f ];
+}
+
+static std::string EncodeBase64( const unsigned char* begin, const unsigned char* end )
+{
+	std::string result;
+	
+	result.reserve( (end - begin + 2) * 4 / 3 );
+	
+	unsigned char buffer[4];
+	
+	while ( begin < end - 2 )
+	{
+		unsigned triplet = (begin[0] << 16 )
+		                 | (begin[1] <<  8 )
+		                 | (begin[2] <<  0 );
+		
+		EncodeBase64Triplet( triplet, buffer );
+		
+		result.append( (char*) buffer, 4 );
+		
+		begin += 3;
+	}
+	
+	if ( begin < end )
+	{
+		unsigned triplet = begin[0] << 16;
+		
+		if ( end - begin == 2 )
+		{
+			triplet |= begin[1] << 8;
+		}
+		
+		EncodeBase64Triplet( triplet, buffer );
+		
+		if ( end - begin == 1 )
+		{
+			buffer[2] = '=';
+		}
+		
+		buffer[3] = '=';
+		
+		result.append( (char*) buffer, 4 );
+	}
+	
+	return result;
+}
+
 int O::Main( int argc, argv_t argv )
 {
 	const char* filename = argc > 1 ? argv[1] : "/dev/null";
 	
 	NN::Owned< p7::fd_t > message_body = p7::open( filename, O_RDONLY );
+	
+	MD5::Result digest = MD5DigestFile( message_body );
+	
+	std::string digest_b64 = EncodeBase64( digest.data, digest.data + 16 );
+	
+	//p7::lseek( message_body, 0, 0 );
+	lseek( message_body, 0, 0 );
 	
 	const p7::fd_t socket_in  = p7::fd_t( 6 );
 	const p7::fd_t socket_out = p7::fd_t( 7 );
@@ -57,6 +157,7 @@ int O::Main( int argc, argv_t argv )
 	
 	std::string message_header =   HTTP::RequestLine( method, urlPath )
 	                             //+ HTTP::HeaderLine( "Host", hostname )
+	                             + HTTP::HeaderLine( "Content-MD5", digest_b64 )
 	                             + contentLengthHeader
 	                             + "\r\n";
 	
