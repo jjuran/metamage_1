bc917fa71f10ffa8c01b345ef1b806687596b516
Joshua Juran <jjuran@metamage.com>
2007-10-07 15:34:14 +0000

    Gutted.

--- lamp/Genie/Genie/Yield.cc
+++ lamp/Genie/Genie/Yield.cc
@@ -5,26 +5,3 @@
 
 #include "Genie/Yield.hh"
 
-// Nitrogen
-#include "Nitrogen/Threads.h"
-
-// POSeven
-#include "POSeven/Errno.hh"
-
-// Pedestal
-#include "Pedestal/Application.hh"
-
-// Genie
-#include "Genie/Process.hh"
-
-
-namespace Genie
-{
-	
-	namespace N = Nitrogen;
-	namespace P7 = POSeven;
-	namespace Ped = Pedestal;
-	
-	
-}
-
