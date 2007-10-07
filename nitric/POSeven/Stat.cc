1efd41d035ddf9180031c66bdc4c33412be0b7c2
Joshua Juran <jjuran@metamage.com>
2007-10-07 15:42:58 +0000

    Initial check-in.

---
+++ nitric/POSeven/Stat.cc
@@ -0,0 +1,21 @@
+// Stat.cc
+// -------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2007 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#include "POSeven/Stat.hh"
+
+
+namespace poseven
+{
+	
+}
+
