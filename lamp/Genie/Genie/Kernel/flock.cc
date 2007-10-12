25a742e1ec49fb494f1c9f9b4ee7d07270ad9c4d
Joshua Juran <jjuran@metamage.com>
2007-10-12 10:26:24 +0000

    Initial check-in.

---
+++ lamp/Genie/Genie/Kernel/flock.cc
@@ -0,0 +1,59 @@
+/*	========
+ *	flock.cc
+ *	========
+ */
+
+// Standard C
+#include <errno.h>
+
+// POSIX
+#include "sys/file.h"
+
+// POSeven
+//#include "Errno.hh"
+
+// Genie
+//#include "Genie/FileDescriptors.hh"
+#include "Genie/SystemCallRegistry.hh"
+#include "Genie/SystemCalls.hh"
+
+
+namespace Genie
+{
+	
+	DECLARE_MODULE_INIT( Kernel_flock )
+	DEFINE_MODULE_INIT( Kernel_flock )
+	
+	
+	static int flock( int fd, int operation )
+	{
+		SystemCallFrame frame( "flock" );
+		
+		int non_blocking = operation & LOCK_NB;
+		
+		operation -= non_blocking;
+		
+		try
+		{
+			switch ( operation )
+			{
+				case LOCK_SH:
+				case LOCK_EX:
+				case LOCK_UN:
+				
+				default:
+					break;
+			}
+		}
+		catch ( ... )
+		{
+			return frame.SetErrnoFromException();
+		}
+		
+		return frame.SetErrno( EINVAL );
+	}
+	
+	REGISTER_SYSTEM_CALL( flock );
+	
+}
+
