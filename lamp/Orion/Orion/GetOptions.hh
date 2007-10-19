25bf9f6a936330f29b43bd114b82a2eddf2d1556
Joshua Juran <jjuran@metamage.com>
2007-10-19 02:52:52 +0000

    Added LGPL notice.

--- lamp/Orion/Orion/GetOptions.hh
+++ lamp/Orion/Orion/GetOptions.hh
@@ -1,7 +1,25 @@
-/*	=============
- *	GetOptions.hh
- *	=============
- */
+/*
+	===================
+	Orion/GetOptions.hh
+	===================
+	
+	Copyright 2005-2007 Joshua Juran.
+	
+	This file is part of Orion.
+	
+    Orion is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+	
+    Orion is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+	
+    You should have received a copy of the GNU Lesser General Public License
+    along with Orion.  If not, see <http://www.gnu.org/licenses/>.	
+*/
 
 #ifndef ORION_GETOPTIONS_HH
 #define ORION_GETOPTIONS_HH
