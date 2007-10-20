1769e4d528707deddea3cf3c54d92d66b466dd2a
Joshua Juran <jjuran@metamage.com>
2007-10-20 23:36:14 +0000

    Initial check-in.

---
+++ nitric/POSeven/IOPump.hh
@@ -0,0 +1,48 @@
+// Pump.hh
+// -------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2007 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#ifndef POSEVEN_PUMP_HH
+#define POSEVEN_PUMP_HH
+
+// Convergence
+#include "pump.hh"
+
+// POSeven
+#include "POSeven/Errno.hh"
+#include "POSeven/FileDescriptor.hh"
+
+
+namespace poseven
+{
+	
+	inline int pump( fd_t    fd_in,
+	                 off_t*  off_in,
+	                 fd_t    fd_out,
+	                 off_t*  off_out = NULL,
+	                 size_t  count = 0 )
+	{
+		return throw_posix_result( ::pump( fd_in, off_in, fd_out, off_out, count ) );
+	}
+	
+	inline int pump( fd_t    fd_in,
+	                 fd_t    fd_out,
+	                 off_t*  off_out = NULL,
+	                 size_t  count = 0 )
+	{
+		return pump( fd_in, NULL, fd_out, off_out, count );
+	}
+	
+}
+
+#endif
+
