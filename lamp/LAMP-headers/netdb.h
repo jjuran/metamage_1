7681e4bd238c09806bc305ae0f74dd044f0622cc
Joshua Juran <jjuran@metamage.com>
2007-11-03 18:00:20 +0000

    Copied from OpenBSD.

--- lamp/LAMP-headers/netdb.h
+++ lamp/LAMP-headers/netdb.h
@@ -1,4 +1,8 @@
-/*-
+/*	$OpenBSD: netdb.h,v 1.25 2005/12/13 00:35:22 millert Exp $	*/
+
+/*
+ * ++Copyright++ 1980, 1983, 1988, 1993
+ * -
  * Copyright (c) 1980, 1983, 1988, 1993
  *	The Regents of the University of California.  All rights reserved.
  *
@@ -10,10 +14,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * [¤3 Deleted as of 22Jul99, see
- *     ftp://ftp.cs.berkeley.edu/pub/4bsd/README.Impt.License.Change
- *	   for details]
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
  *
@@ -28,9 +29,6 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- *      @(#)netdb.h	8.1 (Berkeley) 6/2/93
- *	$Id$
  * -
  * Portions Copyright (c) 1993 by Digital Equipment Corporation.
  *
@@ -53,18 +51,52 @@
  * --Copyright--
  */
 
-/* Adapted for GUSI by Matthias Neeracher <neeri@iis.ee.ethz.ch> */
+/*
+ * Copyright (c) 1995, 1996, 1997, 1998, 1999 Craig Metz. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/*
+ *      @(#)netdb.h	8.1 (Berkeley) 6/2/93
+ *	$From: netdb.h,v 8.7 1996/05/09 05:59:09 vixie Exp $
+ */
 
 #ifndef _NETDB_H_
 #define _NETDB_H_
 
-#include <sys/types.h>
+#include <sys/param.h>
+#include <sys/cdefs.h>
 
 #define	_PATH_HEQUIV	"/etc/hosts.equiv"
 #define	_PATH_HOSTS	"/etc/hosts"
 #define	_PATH_NETWORKS	"/etc/networks"
 #define	_PATH_PROTOCOLS	"/etc/protocols"
 #define	_PATH_SERVICES	"/etc/services"
+
+extern int h_errno;
 
 /*
  * Structures returned by network data base library.  All addresses are
@@ -77,7 +109,18 @@ struct	hostent {
 	int	h_addrtype;	/* host address type */
 	int	h_length;	/* length of address */
 	char	**h_addr_list;	/* list of addresses from name server */
-#define	h_addr	h_addr_list[0]	/* address, for backward compatiblity */
+#define	h_addr	h_addr_list[0]	/* address, for backward compatibility */
+};
+
+/*
+ * Assumption here is that a network number
+ * fits in an in_addr_t -- probably a poor one.
+ */
+struct	netent {
+	char		*n_name;	/* official name of net */
+	char		**n_aliases;	/* alias list */
+	int		n_addrtype;	/* net address type */
+	in_addr_t	n_net;		/* network # */
 };
 
 struct	servent {
@@ -95,36 +138,194 @@ struct	protoent {
 
 /*
  * Error return codes from gethostbyname() and gethostbyaddr()
- * (left in h_errno).
+ * (left in extern int h_errno).
  */
 
-extern int h_errno;
-
+#define	NETDB_INTERNAL	-1	/* see errno */
+#define	NETDB_SUCCESS	0	/* no problem */
 #define	HOST_NOT_FOUND	1 /* Authoritative Answer Host not found */
 #define	TRY_AGAIN	2 /* Non-Authoritive Host not found, or SERVERFAIL */
 #define	NO_RECOVERY	3 /* Non recoverable errors, FORMERR, REFUSED, NOTIMP */
 #define	NO_DATA		4 /* Valid name, no data record of requested type */
 #define	NO_ADDRESS	NO_DATA		/* no address, look for MX record */
 
-#include <sys/cdefs.h>
+/* Values for getaddrinfo() and getnameinfo() */
+#define AI_PASSIVE	1	/* socket address is intended for bind() */
+#define AI_CANONNAME	2	/* request for canonical name */
+#define AI_NUMERICHOST	4	/* don't ever try hostname lookup */
+#define AI_EXT		8	/* enable non-portable extensions */
+#define AI_NUMERICSERV	16	/* don't ever try servname lookup */
+/* valid flags for addrinfo */
+#define AI_MASK \
+    (AI_PASSIVE | AI_CANONNAME | AI_NUMERICHOST | AI_NUMERICSERV)
+
+#define NI_NUMERICHOST	1	/* return the host address, not the name */
+#define NI_NUMERICSERV	2	/* return the service address, not the name */
+#define NI_NOFQDN	4	/* return a short name if in the local domain */
+#define NI_NAMEREQD	8	/* fail if either host or service name is unknown */
+#define NI_DGRAM	16	/* look up datagram service instead of stream */
+
+#define NI_MAXHOST	MAXHOSTNAMELEN	/* max host name returned by getnameinfo */
+#define NI_MAXSERV	32	/* max serv. name length returned by getnameinfo */
+
+/*
+ * Scope delimit character (KAME hack)
+ */
+#define SCOPE_DELIMITER '%'
+
+#define EAI_BADFLAGS	-1	/* invalid value for ai_flags */
+#define EAI_NONAME	-2	/* name or service is not known */
+#define EAI_AGAIN	-3	/* temporary failure in name resolution */
+#define EAI_FAIL	-4	/* non-recoverable failure in name resolution */
+#define EAI_NODATA	-5	/* no address associated with name */
+#define EAI_FAMILY	-6	/* ai_family not supported */
+#define EAI_SOCKTYPE	-7	/* ai_socktype not supported */
+#define EAI_SERVICE	-8	/* service not supported for ai_socktype */
+#define EAI_ADDRFAMILY	-9	/* address family for name not supported */
+#define EAI_MEMORY	-10	/* memory allocation failure */
+#define EAI_SYSTEM	-11	/* system error (code indicated in errno) */
+#define EAI_BADHINTS	-12	/* invalid value for hints */
+#define EAI_PROTOCOL	-13	/* resolved protocol is unknown */
+
+struct addrinfo {
+	int ai_flags;		/* input flags */
+	int ai_family;		/* protocol family for socket */
+	int ai_socktype;	/* socket type */
+	int ai_protocol;	/* protocol for socket */
+	socklen_t ai_addrlen;	/* length of socket-address */
+	struct sockaddr *ai_addr; /* socket-address for socket */
+	char *ai_canonname;	/* canonical name for service location (iff req) */
+	struct addrinfo *ai_next; /* pointer to next in list */
+};
+
+/*
+ * Flags for getrrsetbyname()
+ */
+#define RRSET_VALIDATED		1
+
+/*
+ * Return codes for getrrsetbyname()
+ */
+#define ERRSET_SUCCESS		0
+#define ERRSET_NOMEMORY		1
+#define ERRSET_FAIL		2
+#define ERRSET_INVAL		3
+#define ERRSET_NONAME		4
+#define ERRSET_NODATA		5
+
+/*
+ * Structures used by getrrsetbyname() and freerrset()
+ */
+struct rdatainfo {
+	unsigned int		rdi_length;	/* length of data */
+	unsigned char		*rdi_data;	/* record data */
+};
+
+struct rrsetinfo {
+	unsigned int		rri_flags;	/* RRSET_VALIDATED ... */
+	unsigned int		rri_rdclass;	/* class number */
+	unsigned int		rri_rdtype;	/* RR type number */
+	unsigned int		rri_ttl;	/* time to live */
+	unsigned int		rri_nrdatas;	/* size of rdatas array */
+	unsigned int		rri_nsigs;	/* size of sigs array */
+	char			*rri_name;	/* canonical name */
+	struct rdatainfo	*rri_rdatas;	/* individual records */
+	struct rdatainfo	*rri_sigs;	/* individual signatures */
+};
+
+#if __BSD_VISIBLE
+struct servent_data {
+	void *fp;
+	char **aliases;
+	int maxaliases;
+	int stayopen;
+	char *line;
+};
+
+struct protoent_data {
+	void *fp;
+	char **aliases;
+	int maxaliases;
+	int stayopen;
+	char *line;
+};
+#endif
 
 __BEGIN_DECLS
-void		endhostent __P((void));
-void		endprotoent __P((void));
-void		endservent __P((void));
-struct hostent	*gethostbyaddr __P((const void *, size_t, int));
-struct hostent	*gethostbyname __P((const char *));
-struct hostent	*gethostent __P((void));
-struct protoent	*getprotobyname __P((const char *));
-struct protoent	*getprotobynumber __P((int));
-struct protoent	*getprotoent __P((void));
-struct servent	*getservbyname __P((const char *, const char *));
-struct servent	*getservbyport __P((int, const char *));
-struct servent	*getservent __P((void));
-void		herror __P((const char *));
-char		*hstrerror __P((int));
-void		setprotoent __P((int));
-void		setservent __P((int));
+void		endhostent(void);
+void		endnetent(void);
+void		endprotoent(void);
+#if __BSD_VISIBLE
+void		endprotoent_r(struct protoent_data *);
+#endif
+void		endservent(void);
+#if __BSD_VISIBLE
+void		endservent_r(struct servent_data *);
+#endif
+struct hostent	*gethostbyaddr(const void *, socklen_t, int);
+struct hostent	*gethostbyname(const char *);
+struct hostent	*gethostbyname2(const char *, int);
+struct hostent	*gethostent(void);
+struct netent	*getnetbyaddr(in_addr_t, int);
+struct netent	*getnetbyname(const char *);
+struct netent	*getnetent(void);
+struct protoent	*getprotobyname(const char *);
+struct protoent	*getprotobynumber(int);
+struct protoent	*getprotoent(void);
+#if __BSD_VISIBLE
+int		getprotobyname_r(const char *, struct protoent *,
+		    struct protoent_data *);
+int		getprotobynumber_r(int, struct protoent *,
+		    struct protoent_data *);
+int		getprotoent_r(struct protoent *, struct protoent_data *);
+#endif
+struct servent	*getservbyname(const char *, const char *);
+struct servent	*getservbyport(int, const char *);
+struct servent	*getservent(void);
+#if __BSD_VISIBLE
+int		getservbyname_r(const char *, const char *, struct servent *,
+		    struct servent_data *);
+int		getservbyport_r(int, const char *, struct servent *,
+		    struct servent_data *);
+int		getservent_r(struct servent *, struct servent_data *);
+#endif
+void		herror(const char *);
+const char	*hstrerror(int);
+void		sethostent(int);
+/* void		sethostfile(const char *); */
+void		setnetent(int);
+void		setprotoent(int);
+#if __BSD_VISIBLE
+void		setprotoent_r(int, struct protoent_data *);
+#endif
+void		setservent(int);
+#if __BSD_VISIBLE
+void		setservent_r(int, struct servent_data *);
+#endif
+
+int		getaddrinfo(const char *, const char *,
+		    const struct addrinfo *, struct addrinfo **);
+void		freeaddrinfo(struct addrinfo *);
+int		getnameinfo(const struct sockaddr *, socklen_t,
+		    char *, size_t, char *, size_t, int);
+const char	*gai_strerror(int);
+int		net_addrcmp(struct sockaddr *, struct sockaddr *);
+int		getrrsetbyname(const char *, unsigned int, unsigned int, unsigned int, struct rrsetinfo **);
+void		freerrset(struct rrsetinfo *);
 __END_DECLS
+
+/* This is nec'y to make this include file properly replace the sun version. */
+#ifdef sun
+#ifdef __GNU_LIBRARY__
+#include <rpc/netdb.h>
+#else
+struct rpcent {
+	char	*r_name;	/* name of server for this rpc program */
+	char	**r_aliases;	/* alias list */
+	int	r_number;	/* rpc program number */
+};
+struct rpcent	*getrpcbyname(), *getrpcbynumber(), *getrpcent();
+#endif /* __GNU_LIBRARY__ */
+#endif /* sun */
 
 #endif /* !_NETDB_H_ */
