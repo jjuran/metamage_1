b9fd3e194490efb7771eb4c721711a196d43173b
Joshua Juran <jjuran@metamage.com>
2007-11-04 13:18:49 +0000

    Copied from OpenBSD.

--- lamp/LAMP-headers/arpa/inet.h
+++ lamp/LAMP-headers/arpa/inet.h
@@ -1,7 +1,11 @@
+/*	$OpenBSD: inet.h,v 1.10 2004/01/22 21:48:02 espie Exp $	*/
+
 /*
+ * ++Copyright++ 1983, 1993
+ * -
  * Copyright (c) 1983, 1993
- *	The Regents of the University of California.  All rights reserved.
- *
+ *    The Regents of the University of California.  All rights reserved.
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
@@ -10,13 +14,10 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * [¤3 Deleted as of 22Jul99, see
- *     ftp://ftp.cs.berkeley.edu/pub/4bsd/README.Impt.License.Change
- *	   for details]
- * 4. Neither the name of the University nor the names of its contributors
+ * 3. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -28,32 +29,65 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
+ * -
+ * Portions Copyright (c) 1993 by Digital Equipment Corporation.
+ * 
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies, and that
+ * the name of Digital Equipment Corporation not be used in advertising or
+ * publicity pertaining to distribution of the document or software without
+ * specific, written prior permission.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND DIGITAL EQUIPMENT CORP. DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS.   IN NO EVENT SHALL DIGITAL EQUIPMENT
+ * CORPORATION BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
+ * DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR
+ * PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS
+ * ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS
+ * SOFTWARE.
+ * -
+ * --Copyright--
+ */
+
+/*
  *	@(#)inet.h	8.1 (Berkeley) 6/2/93
+ *	$From: inet.h,v 8.6 1996/08/08 06:54:29 vixie Exp $
  */
 
 #ifndef _INET_H_
 #define	_INET_H_
 
-/* Adapted for GUSI by Matthias Neeracher <neeri@iis.ee.ethz.ch> */
-
 /* External definitions for functions in inet(3) */
 
+#include <sys/param.h>
+#if (!defined(BSD)) || (BSD < 199306)
+# include <sys/bitypes.h>
+#else
+# include <sys/types.h>
+#endif
 #include <sys/cdefs.h>
 
-/* XNS mandates availability of xtonx() and uintx_t -- neeri */
-
-#include <netinet/in.h>
-#include <machine/endian.h>
-
 __BEGIN_DECLS
-in_addr_t	 	inet_addr __P((const char *));
-int		 		inet_aton __P((const char *, struct in_addr *));
-in_addr_t	 	inet_lnaof __P((struct in_addr));
-struct in_addr	inet_makeaddr __P((uint32_t , in_addr_t));
-in_addr_t	 	inet_netof __P((struct in_addr));
-in_addr_t		inet_network __P((const char *));
-char		     *inet_ntoa __P((struct in_addr));
+in_addr_t	 inet_addr(const char *);
+int		 inet_aton(const char *, struct in_addr *);
+in_addr_t	 inet_lnaof(struct in_addr);
+struct in_addr	 inet_makeaddr(in_addr_t , in_addr_t);
+char *		 inet_neta(in_addr_t, char *, size_t)
+			__attribute__((__bounded__(__string__,2,3)));
+in_addr_t	 inet_netof(struct in_addr);
+in_addr_t	 inet_network(const char *);
+char		*inet_net_ntop(int, const void *, int, char *, size_t)
+			__attribute__((__bounded__(__string__,4,5)));
+int		 inet_net_pton(int, const char *, void *, size_t)
+			__attribute__((__bounded__(__string__,3,4)));
+char		*inet_ntoa(struct in_addr);
+int		 inet_pton(int, const char *, void *);
+const char	*inet_ntop(int, const void *, char *, size_t)
+			__attribute__ ((__bounded__(__string__,3,4)));
+unsigned int	 inet_nsap_addr(const char *, unsigned char *, int);
+char		*inet_nsap_ntoa(int, const unsigned char *, char *);
 __END_DECLS
 
 #endif /* !_INET_H_ */
