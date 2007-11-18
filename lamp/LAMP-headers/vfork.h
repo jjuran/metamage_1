bcdeece2614407dd3a733ee0f46f48f675e2b0ac
Joshua Juran <jjuran@metamage.com>
2007-11-18 07:23:07 +0000

    Eliminated macros for the C++ case.

--- lamp/LAMP-headers/vfork.h
+++ lamp/LAMP-headers/vfork.h
@@ -22,15 +22,13 @@ extern "C" {
 	
 	jmp_buf* NewJmpBuf();
 	
-	#define VFORK() ( SpawnVFork( Kerosene_LongJmp ) ? -1 : setjmp( *NewJmpBuf() ) )
-	
 	#ifdef __cplusplus
 		
-		inline pid_t vfork()  { return VFORK(); }
+		inline pid_t vfork()  { return pid_t( SpawnVFork( Kerosene_LongJmp ) ? -1 : setjmp( *NewJmpBuf() ) ); }
 		
 	#else
 		
-		#define vfork()  ( (pid_t) Kerosene_vfork() )
+		#define vfork()  ( (pid_t) (SpawnVFork( Kerosene_LongJmp ) ? -1 : setjmp( *NewJmpBuf() ) ) )
 		
 	#endif
 	
