413ee57193c030c761f51c033b7cef2db64825be
Joshua Juran <jjuran@metamage.com>
2007-11-27 04:04:31 +0000

    Wrote new project config storage.

--- lamp/jTools/A-line/CompileDriver/ProjectCatalog.cc
+++ lamp/jTools/A-line/CompileDriver/ProjectCatalog.cc
@@ -28,6 +28,60 @@ namespace CompileDriver
 	using namespace io::path_descent_operators;
 	
 	
+	static ProjectCatalog gProjectCatalog;
+	
+	
+	void RecursivelyAddPendingSubprojects();
+	
+	
+	void AddProjectConfigFile( const std::string&      name,
+	                           const PlatformDemands&  demands,
+	                           const ProjectConfig&    config )
+	{
+		gProjectCatalog[ name ][ demands ] = config;
+	}
+	
+	static bool ProjectPlatformIsCompatible( const ProjectConfigCandidates::value_type&  candidate,
+	                                         Platform                                    target )
+	{
+		const PlatformDemands& projectDemands = candidate.first;
+		
+		return projectDemands.Test( target );
+	}
+	
+	const ProjectConfig& GetProjectConfig( const std::string& name, Platform targetPlatform )
+	{
+		ProjectCatalog::const_iterator itProject = gProjectCatalog.find( name );
+		
+		if ( itProject == gProjectCatalog.end() )
+		{
+			RecursivelyAddPendingSubprojects();
+			
+			itProject = gProjectCatalog.find( name );
+			
+			if ( itProject == gProjectCatalog.end() )
+			{
+				throw NoSuchProject( name );
+			}
+		}
+		
+		const ProjectConfigCandidates& candidates = itProject->second;
+		
+		const ProjectConfigCandidates::const_iterator itCandidate = std::find_if( candidates.begin(),
+		                                                                          candidates.end(),
+		                                                                          std::bind2nd( more::ptr_fun( ProjectPlatformIsCompatible ),
+		                                                                                        targetPlatform ) );
+		
+		if ( itCandidate == candidates.end() )
+		{
+			// FIXME:  Indicate that there are projects with this name,
+			// but that they're not compatible with the current target
+			throw NoSuchProject( name );
+		}
+		
+		return itCandidate->second;
+	}
+	
 	static void ScanItemForProjects( const std::string&                                       item,
 	                                 std::back_insert_iterator< std::vector< std::string > >  output )
 	{
--- lamp/jTools/A-line/CompileDriver/ProjectCatalog.hh
+++ lamp/jTools/A-line/CompileDriver/ProjectCatalog.hh
@@ -8,12 +8,47 @@
 
 // Standard C++
 #include <iterator>
+#include <map>
 #include <string>
 #include <vector>
+
+// A-line
+#include "CompileDriver/Platform.hh"
+#include "CompileDriver/ProjectConfig.hh"
 
 
 namespace CompileDriver
 {
+	
+	struct ProjectConfig
+	{
+		std::string  itsPathname;
+		std::string  itsProjectFolder;
+		ConfData     itsConfigData;
+		
+		ProjectConfig()  {}
+		
+		ProjectConfig( const std::string&  pathname,
+		               const std::string&  folder,
+		               const ConfData&     data ) : itsPathname     ( pathname ),
+		                                            itsProjectFolder( folder   ),
+		                                            itsConfigData   ( data     )
+		{
+		}
+	};
+	
+	// A map from platform requirements to project config data
+	typedef std::map< PlatformDemands, ProjectConfig > ProjectConfigCandidates;
+	
+	// A map from project name to config file set
+	typedef std::map< std::string, ProjectConfigCandidates > ProjectCatalog;
+	
+	
+	void AddProjectConfigFile( const std::string&      name,
+	                           const PlatformDemands&  demands,
+	                           const ProjectConfig&    config );
+	
+	const ProjectConfig& GetProjectConfig( const std::string& name, Platform platform );
 	
 	void ScanDirForProjects( const std::string&                                       dirPath,
 	                         std::back_insert_iterator< std::vector< std::string > >  output );
