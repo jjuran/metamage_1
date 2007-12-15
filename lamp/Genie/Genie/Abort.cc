a7941722c8059d2740e71633a8ab8ebc6aed5c53
Joshua Juran <jjuran@metamage.com>
2007-12-15 23:57:57 +0000

    Warn that we've just stopped the thread.

--- lamp/Genie/Genie/Abort.cc
+++ lamp/Genie/Genie/Abort.cc
@@ -7,11 +7,15 @@
 #include <signal.h>
 #include <stdlib.h>
 
+// Iota
+#include "iota/strings.hh"
+
 // Nitrogen
 #include "Nitrogen/Threads.h"
 
 // Genie
 #include "Genie/Process.hh"
+#include "Genie/SystemConsole.hh"
 #include "Genie/Yield.hh"
 
 
@@ -33,6 +37,7 @@ void abort()
 			current.HandlePendingSignals();
 			
 			// Probably not reached
+			Genie::WriteToSystemConsole( STR_LEN( "Genie: abort(): process failed to terminate, stopping its thread\n" ) );
 		}
 		
 		N::SetThreadState( N::GetCurrentThread(), N::kStoppedThreadState );
