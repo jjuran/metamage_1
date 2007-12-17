321e6c8e133c9bfa89ce9c42fe76b4ec90b35b36
Joshua Juran <jjuran@metamage.com>
2007-12-17 08:56:58 +0000

    The check for OS X is no longer sufficient (since the same issue occurs in SheepShaver) or necessary (since we 'audition' the memory limit check against the top return address).

--- pedestal/Practica/Backtrace/MemoryLimit.cc
+++ pedestal/Practica/Backtrace/MemoryLimit.cc
@@ -17,15 +17,6 @@ namespace Backtrace
 	
 #ifdef __MACOS__
 	
-	static bool InOSX()
-	{
-		long response = 0;
-		
-		OSErr err = ::Gestalt( gestaltSystemVersion, &response );
-		
-		return err == noErr  &&  response >= 0x1000;
-	}
-	
 	static UInt32 LogicalRAMSize()
 	{
 		long response = 0;
@@ -37,16 +28,6 @@ namespace Backtrace
 	
 	const void* MemoryLimit()
 	{
-		if ( TARGET_API_MAC_CARBON )
-		{
-			static bool inOSX = InOSX();
-			
-			if ( inOSX )
-			{
-				return reinterpret_cast< const void* >( 0xffffffff );
-			}
-		}
-		
 		static UInt32 logicalRAMSize = LogicalRAMSize();
 		
 		return reinterpret_cast< const void* >( logicalRAMSize );
