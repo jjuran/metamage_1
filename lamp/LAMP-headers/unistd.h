d95886c37e636a4d3cd03ba6f16c8d849a6dcba9
Joshua Juran <jjuran@metamage.com>
2007-12-25 07:00:32 +0000

    Include vfork.h so our clients don't have to do so, which isn't portable.

--- lamp/LAMP-headers/unistd.h
+++ lamp/LAMP-headers/unistd.h
@@ -39,6 +39,8 @@
 #include <sys/types.h>
 #include <sys/unistd.h>
 
+#include <vfork.h>
+
 #define	STDIN_FILENO	0	/* standard input file descriptor */
 #define	STDOUT_FILENO	1	/* standard output file descriptor */
 #define	STDERR_FILENO	2	/* standard error file descriptor */
