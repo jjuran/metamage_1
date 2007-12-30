2c600b729efbd4b0bd5e7f8fbcbcc5a0fbd9e1fc
Joshua Juran <jjuran@metamage.com>
2007-12-30 16:10:40 +0000

    Added library implementation of ttyname() based on isatty() and readlink().

--- lamp/Kerosene/Library/Library.cc
+++ lamp/Kerosene/Library/Library.cc
@@ -28,6 +28,7 @@
 
 // Iota
 #include "iota/environ.hh"
+#include "iota/strings.hh"
 
 
 // Exceptions are off here
@@ -260,6 +261,84 @@
 	int tcsetpgrp( int fd, pid_t pgrp )
 	{
 		return ioctl( fd, TIOCSPGRP, (int*) &pgrp );
+	}
+	
+	static int decimal_magnitude( unsigned x )
+	{
+		int result = 0;
+		
+		while ( x > 0 )
+		{
+			x /= 10;
+			
+			++result;
+		}
+		
+		return result;
+	}
+	
+	static char* inscribe_decimal_backwards( unsigned x, char* p )
+	{
+		while ( x > 0 )
+		{
+			*--p = x % 10 + '0';
+		}
+		
+		return p;
+	}
+	
+	extern "C" int readlink_k( const char* pathname, char* buffer, size_t buffer_size );
+	
+	static int ttyname_k( int fd, char* buffer, size_t buffer_size )
+	{
+		if ( !isatty( fd ) )
+		{
+			//errno = ENOTTY;
+			
+			return -1;
+		}
+		
+		char pathname[] = "/dev/fd/123456789";
+		
+		char* begin = pathname + STRLEN( "/dev/fd/" );
+		
+		char* end = begin + decimal_magnitude( fd );
+		
+		if ( begin < end )
+		{
+			inscribe_decimal_backwards( fd, end );
+		}
+		else
+		{
+			*end++ = '0';
+		}
+		
+		*end = '\0';
+		
+		return readlink_k( pathname, buffer, buffer_size );
+	}
+	
+	const char* ttyname( int fd )
+	{
+		static char buffer[ 256 ];  // should be enough for a terminal name
+		
+		ssize_t length = ttyname_k( fd, buffer, sizeof buffer );
+		
+		if ( length < 0 )
+		{
+			return NULL;
+		}
+		
+		if ( length + 1 > sizeof buffer )
+		{
+			errno = ERANGE;
+			
+			return NULL;
+		}
+		
+		buffer[ length ] = '\0';
+		
+		return buffer;
 	}
 	
 	#pragma mark -
