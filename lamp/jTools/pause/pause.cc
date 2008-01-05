198a0771e1713575ce4b46d7043a381832a7ba69
Joshua Juran <jjuran@metamage.com>
2008-01-05 12:24:46 +0000

    Initial check-in.

---
+++ lamp/jTools/pause/pause.cc
@@ -0,0 +1,27 @@
+/*	========
+ *	pause.cc
+ *	========
+ */
+
+// POSIX
+#include <unistd.h>
+
+// Iota
+#include "iota/strings.hh"
+
+
+#pragma export on
+
+int main( int argc, char const *const argv[] )
+{
+	(void) write( STDOUT_FILENO, STR_LEN( "Hit return to continue\n" ) );
+	
+	char c;
+	
+	ssize_t bytes = read( STDIN_FILENO, &c, sizeof c );
+	
+	return bytes ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+#pragma export reset
+
