bfad9c26a917301f2c335b93cd3a01dfc44f55b2
Joshua Juran <jjuran@metamage.com>
2008-01-19 09:43:16 +0000

    Separated from PortView.cc.

---
+++ pedestal/Principia/Vectoria/HomogeneousClipping.hh
@@ -0,0 +1,23 @@
+/*	===============================
+ *	Vectoria/HomogeneousClipping.hh
+ *	===============================
+ */
+
+#ifndef VECTORIA_HOMOGENEOUSCLIPPING_HH
+#define VECTORIA_HOMOGENEOUSCLIPPING_HH
+
+#include "Vectoria/Matrix.hh"
+
+
+namespace Vectoria
+{
+	
+	Matrix< double, 4, 4 > MakePortToClipTransform( double  near_distance,
+	                                                double  far_distance,
+	                                                double  focal_length,
+	                                                double  aspect_ratio );
+	
+}
+
+#endif
+
