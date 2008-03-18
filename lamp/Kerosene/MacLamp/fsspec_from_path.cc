5e521896a2e4d8c48e5e87d01bcf41da8856c46e
Joshua Juran <jjuran@metamage.com>
2008-03-18 11:22:13 +0000

    Initial check-in.

---
+++ lamp/Kerosene/MacLamp/fsspec_from_path.cc
@@ -0,0 +1,115 @@
+/*	===================
+ *	fsspec_from_path.cc
+ *	===================
+ */
+
+// Standard C
+#include "errno.h"
+
+// Standard C++
+#include <string>
+
+// POSIX
+#include "sys/stat.h"
+
+// POSeven
+#include "POSeven/Errno.hh"
+#include "POSeven/Pathnames.hh"
+
+// Nitrogen
+#include "Nitrogen/Files.h"
+#include "Nitrogen/Str.h"
+
+// MacLamp
+#include "fsspec_from_path.hh"
+
+
+//
+	
+	namespace N = Nitrogen;
+	namespace p7 = poseven;
+	
+	int fsspec_from_path( const char*  pathname,
+	                      FSSpec*      outFSS )
+	{
+		try
+		{
+			struct ::stat stat_buffer;
+			
+			int status = stat( pathname, &stat_buffer );
+			
+			if ( status == -1 )
+			{
+				if ( errno != ENOENT )
+				{
+					return status;
+				}
+				else
+				{
+					stat_buffer.st_mode = 0;  // not a dir
+				}
+			}
+			
+			std::string parent_pathname_string;
+			std::string basename_string;
+			
+			bool is_dir = S_ISDIR( stat_buffer.st_mode );
+			
+			if ( !is_dir )
+			{
+				parent_pathname_string = io::get_preceding_directory( pathname );
+				basename_string        = io::get_filename_string    ( pathname );
+				
+				status = stat( parent_pathname_string.c_str(), &stat_buffer );
+				
+				if ( status == -1 )
+				{
+					return status;
+				}
+			}
+			
+			N::FSVolumeRefNum vRefNum = N::FSVolumeRefNum( -stat_buffer.st_dev );
+			N::FSDirID        dirID   = N::FSDirID       (  stat_buffer.st_ino );
+			
+			if ( vRefNum == 0 )
+			{
+				errno = EXDEV;
+				
+				return -1;
+			}
+			
+			N::Str255 filename = basename_string;
+			
+			FSSpec dummy_FSSpec;
+			
+			if ( outFSS == NULL )
+			{
+				outFSS = &dummy_FSSpec;
+			}
+			
+			*outFSS = N::FSMakeFSSpec( vRefNum, dirID, filename );
+			
+			return 0;
+		}
+		catch ( const std::bad_alloc& )
+		{
+			errno = ENOMEM;
+		}
+		catch ( ... )
+		{
+			errno = ENOENT;
+		}
+		
+		return -1;
+	}
+	
+	FSSpec make_fsspec_from_path( const char* pathname )
+	{
+		FSSpec spec;	
+		
+		p7::throw_posix_result( fsspec_from_path( pathname, &spec ) );
+		
+		return spec;
+	}
+//
+
---
+++ lamp/Kerosene/MacLamp/fsspec_from_path.hh
@@ -0,0 +1,46 @@
+/*	===================
+ *	fsspec_from_path.hh
+ *	===================
+ */
+
+#ifndef KEROSENE_FSSPECFROMPATH_HH
+#define KEROSENE_FSSPECFROMPATH_HH
+
+// Universal Interfaces
+#include <TargetConditionals.h>
+#if TARGET_RT_MAC_MACHO
+#include <Carbon/Carbon.h>
+#else
+#include <Files.h>
+#endif
+
+
+#ifdef __cplusplus
+#include <string>
+#endif
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+	int fsspec_from_path( const char*  pathname,
+	                      FSSpec*      outFSS );
+	
+	FSSpec make_fsspec_from_path( const char* pathname );
+	
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+	
+	inline FSSpec make_fsspec_from_path( const std::string& pathname )
+	{
+		return make_fsspec_from_path( pathname.c_str() );
+	}
+	
+#endif
+
+#endif
+
