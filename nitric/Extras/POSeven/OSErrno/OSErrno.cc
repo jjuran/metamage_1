b1ee9c2cb81545436fb6b4d6f86e155334e3ec6e
Joshua Juran <jjuran@metamage.com>
2008-04-08 09:45:44 +0000

    Mapped bdNamErr -> ENAMETOOLONG.

--- nitric/Extras/POSeven/OSErrno/OSErrno.cc
+++ nitric/Extras/POSeven/OSErrno/OSErrno.cc
@@ -24,24 +24,25 @@ namespace OSErrno
 		
 		switch ( error.Get() )
 		{
-			case eofErr         :  result = 0;          break;
+			case eofErr         :  result = 0;             break;
 			case nsvErr         :  // fall through
-			case fnfErr         :  result = ENOENT;     break;
-			case ioErr          :  result = EIO;        break;
+			case fnfErr         :  result = ENOENT;        break;
+			case ioErr          :  result = EIO;           break;
 			case fnOpnErr       :  // fall through
-			case rfNumErr       :  result = EBADF;      break;
-			case memFullErr     :  result = ENOMEM;     break;
-			case portInUse      :  result = EACCES;     break;
-			case userCanceledErr:  result = ECANCELED;  break;
-			case fBsyErr        :  result = EBUSY;      break;
-			case dupFNErr       :  result = EEXIST;     break;
-			case diffVolErr     :  result = EXDEV;      break;
-			case dirNFErr       :  result = ENOTDIR;    break;
-			case notAFileErr    :  result = EISDIR;     break;
+			case rfNumErr       :  result = EBADF;         break;
+			case memFullErr     :  result = ENOMEM;        break;
+			case portInUse      :  result = EACCES;        break;
+			case userCanceledErr:  result = ECANCELED;     break;
+			case fBsyErr        :  result = EBUSY;         break;
+			case dupFNErr       :  result = EEXIST;        break;
+			case diffVolErr     :  result = EXDEV;         break;
+			case dirNFErr       :  result = ENOTDIR;       break;
+			case notAFileErr    :  result = EISDIR;        break;
 			case posErr         :  // fall through
-			case paramErr       :  result = EINVAL;     break;
-			case dskFulErr      :  result = ENOSPC;     break;
-			case vLckdErr       :  result = EROFS;      break;
+			case paramErr       :  result = EINVAL;        break;
+			case dskFulErr      :  result = ENOSPC;        break;
+			case vLckdErr       :  result = EROFS;         break;
+			case bdNamErr       :  result = ENAMETOOLONG;  break;
 			
 			default:
 				throw error;
