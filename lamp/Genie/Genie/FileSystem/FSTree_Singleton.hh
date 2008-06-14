b8ffc3b1334114077d572db281e59435a34db13d
Joshua Juran <jjuran@metamage.com>
2008-06-14 10:29:38 +0000

    Call Init() after construction (but only once, regardless of reentry). Fixed global chain corruption bug when static initialization was reentered.

--- lamp/Genie/Genie/FileSystem/FSTree_Singleton.hh
+++ lamp/Genie/Genie/FileSystem/FSTree_Singleton.hh
@@ -14,9 +14,24 @@ namespace Genie
 {
 	
 	template < class FSTree_Type >
+	const FSTreePtr& MakeSingleton()
+	{
+		FSTree_Type* raw_ptr = NULL;
+		
+		static FSTreePtr singleton = FSTreePtr( raw_ptr = new FSTree_Type() );
+		
+		if ( raw_ptr != NULL )
+		{
+			raw_ptr->Init();
+		}
+		
+		return singleton;
+	}
+	
+	template < class FSTree_Type >
 	FSTreePtr GetSingleton()
 	{
-		static FSTreePtr singleton = FSTreePtr( new FSTree_Type() );
+		static const FSTreePtr& singleton = MakeSingleton< FSTree_Type >();
 		
 		return singleton;
 	}
