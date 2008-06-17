238e93c02b52def998b68f0e0904a39bb1172ba6
Joshua Juran <jjuran@metamage.com>
2008-06-17 00:27:50 +0000

    Implemented /sys/mac/name for OS X.

--- lamp/Genie/Genie/FileSystem/FSTree_sys_mac_name.cc
+++ lamp/Genie/Genie/FileSystem/FSTree_sys_mac_name.cc
@@ -15,9 +15,9 @@ namespace Genie
 	namespace N = Nitrogen;
 	
 	
-	std::string sys_mac_name_Query::operator()() const
+	static std::string GetStringResource( ::ResID id )
 	{
-		::Handle h = N::GetResource( N::ResType( 'STR ' ), N::ResID( -16413 ) );
+		::Handle h = N::GetResource( N::ResType( 'STR ' ), N::ResID( id ) );
 		
 		std::size_t length = **h;
 		
@@ -25,13 +25,68 @@ namespace Genie
 		
 		std::string result;
 		
-		result.resize( length + 1 );
+		result.resize( length );
 		
 		std::copy( begin, begin + length, result.begin() );
 		
-		result.end()[ -1 ] = '\n';
+		return result;
+	}
+	
+	static UInt32 SystemVersion()
+	{
+		static UInt32 sysv = N::Gestalt( N::GestaltSelector( gestaltSystemVersion ) );
+		
+		return sysv;
+	}
+	
+	static std::string CFStringGetStdString( CFStringRef string )
+	{
+		CFIndex length = CFStringGetLength( string );
+		
+		std::string result;
+		
+		result.resize( length );
+		
+		CFIndex usedBufLen = 0;
+		
+		CFIndex nConverted = CFStringGetBytes( string,
+		                                       CFRangeMake( 0, length ),
+		                                       kCFStringEncodingMacRoman,
+		                                       '\0',
+		                                       false,
+		                                       (UInt8*) &result[0],
+		                                       length,
+		                                       &usedBufLen );
+		
+		if ( usedBufLen != length )
+		{
+			throw N::ParamErr();
+		}
 		
 		return result;
+	}
+	
+	static std::string GetMachineName()
+	{
+		if ( !TARGET_RT_MAC_MACHO && (!TARGET_API_MAC_CARBON  ||  SystemVersion() < 0x00001000) )
+		{
+			return GetStringResource( -16413 );
+		}
+		
+		if ( CSCopyMachineName != (void*) kUnresolvedCFragSymbolAddress )
+		{
+			if ( CFStringRef userName = CSCopyMachineName() )
+			{
+				return CFStringGetStdString( userName );
+			}
+		}
+		
+		return "";
+	}
+	
+	std::string sys_mac_name_Query::operator()() const
+	{
+		return GetMachineName() + "\n";
 	}
 	
 }
