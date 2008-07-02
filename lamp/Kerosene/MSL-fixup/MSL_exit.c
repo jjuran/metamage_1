372452d56d91f9cdb933129a38b07c1848b393f6
Joshua Juran <jjuran@metamage.com>
2008-07-02 05:04:19 +0000

    Override exit() for A4 code to destroy global objects. Our version is just like MSL's version, except that it specifically omits the call to __destroy_global_chain() for A4 code (which we don't), and we omit critical regions (which are easy enough to implement for the Thread Manager, but not needed) and the non-Mac bits. As collateral we also override atexit() since it shares file-scoped variables with exit().

--- lamp/Kerosene/MSL-fixup/MSL_exit.c
+++ lamp/Kerosene/MSL-fixup/MSL_exit.c
@@ -27,5 +27,58 @@ void __exit( int status )
 	_exit( status );  // Doesn't return
 }
 
+#if defined( __MC68K__ ) && !__A5__
+
+// We need to override exit() for 68K Kerosene, since MSL makes a point of
+// specifically omitting the call to __destroy_global_chain() for A4 code.
+
+extern int  atexit( void (*func)(void) );
+extern void exit( int status );
+extern void __destroy_global_chain( void );
+
+#define max_funcs	64
+
+static void (*atexit_funcs[ max_funcs ])(void);
+static long	atexit_curr_func = 0;
+
+void (*__stdio_exit)(void) = 0;
+
+
+int atexit( void (*func)(void) )
+{
+	
+	if ( atexit_curr_func == max_funcs )
+	{
+		return -1;
+	}
+	
+	atexit_funcs[ atexit_curr_func++ ] = func;
+	
+	return 0;
+}
+
+
+void exit( int status )
+{
+	
+	while ( atexit_curr_func > 0 )
+	{
+		(*atexit_funcs[ --atexit_curr_func ])();
+	}
+	
+	__destroy_global_chain();
+	
+	if ( __stdio_exit )
+	{
+		(*__stdio_exit)();
+		
+		__stdio_exit = 0;
+	}
+	
+	_exit( status );
+}
+
+#endif  // defined( __MC68K__ ) && !__A5__
+
 #endif
 
