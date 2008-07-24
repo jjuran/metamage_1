ff64559333266a9369a16b5e125b6e6feacda7e3
Joshua Juran <jjuran@metamage.com>
2008-07-24 07:23:31 +0000

    jgetty now vforks and stays running, blocks in wait(), and resumes control after the shell exits. If the shell's status is unsuccessful and the window hasn't already been disconnected (by request to close), then jgetty remains running to keep the window open until closed. Now it's jgetty, not login, that starts the new session.

--- lamp/jTools/jgetty/jgetty.cc
+++ lamp/jTools/jgetty/jgetty.cc
@@ -5,7 +5,12 @@
 
 // POSIX
 #include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/wait.h>
 #include <unistd.h>
+
+// Lamp
+#include "lamp/winio.h"
 
 
 int main( int argc, char const *const argv[] )
@@ -26,12 +31,62 @@ int main( int argc, char const *const argv[] )
 		close( console );
 	}
 	
-	char* exec_argv[] = { "/bin/login", NULL };
+	int sid = setsid();  // New session
 	
-	int result = execv( "/bin/login", exec_argv );
+	if ( sid < 0 )
+	{
+		return 2;
+	}
 	
-	_exit( 127 );
+	int result = ioctl( STDIN_FILENO, TIOCSCTTY, NULL );  // Reattach to terminal
 	
-	return 0;  // Not reached
+	if ( result < 0 )
+	{
+		return 3;
+	}
+	
+	pid_t pid = vfork();
+	
+	if ( pid < 0 )
+	{
+		return 4;
+	}
+	
+	if ( pid == 0 )
+	{
+		char* login_argv[] = { "/bin/login", NULL };
+		
+		(void) execv( login_argv[0], login_argv );
+		
+		_exit( 127 );
+	}
+	
+	int wait_status = -1;
+	
+	int waited = wait( &wait_status );
+	
+	if ( waited < 0 )
+	{
+		return 5;
+	}
+	
+	// If the child exited unsuccessfully and the terminal is still connected,
+	// keep the window around.
+	if ( wait_status != 0  &&  write( STDOUT_FILENO, "", 0 ) == 0 )
+	{
+		// Reset ourselves as the foreground process group of the terminal,
+		// so we'll get SIGHUP when the window is closed.
+		int pgrpset = tcsetpgrp( STDIN_FILENO, sid );
+		
+		result = ioctl( STDIN_FILENO, WIOCSTITLE, (int*) "(Done)" );
+		
+		// Loop so resizing the window doesn't kill us (via SIGWINCH).
+		while ( true )
+		{
+			pause();
+		}
+	}
+	
+	return 0;
 }
 
