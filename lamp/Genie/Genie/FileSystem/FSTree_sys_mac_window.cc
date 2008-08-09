f3fe72fe8a5043fd3b2c883c413098f6730a23d5
Joshua Juran <jjuran@metamage.com>
2008-08-09 09:31:10 +0000

    Added /sys/.../window/REF/name.

--- lamp/Genie/Genie/FileSystem/FSTree_sys_mac_window.cc
+++ lamp/Genie/Genie/FileSystem/FSTree_sys_mac_window.cc
@@ -8,9 +8,22 @@
 // Standard C++
 #include <algorithm>
 
+// Nucleus
+#include "Nucleus/Convert.h"
+
+// Nitrogen
+#include "Nitrogen/MacWindows.h"
+
+// Genie
+#include "Genie/FileSystem/FSTree_QueryFile.hh"
+
 
 namespace Genie
 {
+	
+	namespace N = Nitrogen;
+	namespace NN = Nucleus;
+	
 	
 	bool sys_mac_window_Details::KeyIsValid( const Key& key )
 	{
@@ -19,12 +32,55 @@ namespace Genie
 		return std::find( sequence.begin(), sequence.end(), key ) != sequence.end();
 	}
 	
+	extern const Functional_Traits< WindowRef_KeyName_Traits::Key >::Mapping sys_mac_window_REF_Mappings[];
+	
 	FSTreePtr sys_mac_window_Details::GetChildNode( const FSTreePtr&    parent,
 		                                            const std::string&  name,
 		                                            const Key&          key )
 	{
-		return MakeFSTree( new FSTree_Functional< WindowRef_KeyName_Traits::Key >( parent, name, key ) );
+		return Premapped_Factory< Key, sys_mac_window_REF_Mappings >( parent, name, key );
 	}
+	
+	
+	class sys_mac_window_REF_name_Query
+	{
+		private:
+			typedef N::WindowRef Key;
+			
+			Key itsKey;
+		
+		public:
+			sys_mac_window_REF_name_Query( const Key& key ) : itsKey( key )
+			{
+			}
+			
+			std::string operator()() const
+			{
+				N::Str255 name = N::GetWTitle( itsKey );
+				
+				std::string output = NN::Convert< std::string >( name ) + "\n";
+				
+				return output;
+			}
+	};
+	
+	static FSTreePtr Name_Factory( const FSTreePtr&                parent,
+	                               const std::string&              name,
+	                               WindowRef_KeyName_Traits::Key   key )
+	{
+		typedef sys_mac_window_REF_name_Query Query;
+		
+		typedef FSTree_QueryFile< Query > QueryFile;
+		
+		return MakeFSTree( new QueryFile( parent, name, Query( key ) ) );
+	}
+	
+	const Functional_Traits< WindowRef_KeyName_Traits::Key >::Mapping sys_mac_window_REF_Mappings[] =
+	{
+		{ "name", &Name_Factory },
+		
+		{ NULL, NULL }
+	};
 	
 }
 
