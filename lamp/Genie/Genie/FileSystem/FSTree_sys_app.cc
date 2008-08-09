ec41f2e6d7814f354184ceecddf0c0c66cb05e64
Joshua Juran <jjuran@metamage.com>
2008-08-09 08:53:36 +0000

    Added /sys/app/{freemem,heapsize}.

--- lamp/Genie/Genie/FileSystem/FSTree_sys_app.cc
+++ lamp/Genie/Genie/FileSystem/FSTree_sys_app.cc
@@ -5,6 +5,13 @@
 
 #include "Genie/FileSystem/FSTree_sys_app.hh"
 
+#ifndef __MACMEMORY__
+#include <MacMemory.h>
+#endif
+
+// Nucleus
+#include "Nucleus/Convert.h"
+
 // Genie
 #include "Genie/FileSystem/FSTree_QueryFile.hh"
 #include "Genie/FileSystem/FSTree_sys_mac_window.hh"
@@ -13,8 +20,68 @@
 namespace Genie
 {
 	
+	namespace NN = Nucleus;
+	
+	
+	struct GetFreeMem
+	{
+		typedef long Result;
+		
+		Result operator()() const
+		{
+			return ::FreeMem();
+		}
+	};
+	
+#if !TARGET_API_MAC_CARBON
+	
+	struct GetHeapSize
+	{
+		typedef long Result;
+		
+		Result operator()() const
+		{
+			THz zone = ::ApplicationZone();
+			
+			return zone->bkLim - (Ptr) &zone->heapData;
+		}
+	};
+	
+#endif
+	
+	template < class Get >
+	class sys_app_Query
+	{
+		public:
+			std::string operator()() const
+			{
+				std::string output = NN::Convert< std::string >( Get()() ) + "\n";
+				
+				return output;
+			}
+	};
+	
+	template < class Get >
+	static FSTreePtr Query_Factory( const FSTreePtr&    parent,
+	                                const std::string&  name )
+	{
+		typedef sys_app_Query< Get > Query;
+		
+		typedef FSTree_QueryFile< Query > QueryFile;
+		
+		return MakeFSTree( new QueryFile( parent, name ) );
+	}
+	
 	const Singleton_Mapping sys_app_Mappings[] =
 	{
+		{ "freemem", &Query_Factory< GetFreeMem > },
+		
+	#if !TARGET_API_MAC_CARBON
+		
+		{ "heapsize", &Query_Factory< GetHeapSize > },
+		
+	#endif
+		
 		{ "window", &Singleton_Factory< FSTree_sys_mac_window > },
 		
 		{ NULL, NULL }
