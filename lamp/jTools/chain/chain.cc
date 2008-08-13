59b59610d549c9c89892f5a4042aaa30bb4257b6
Joshua Juran <jjuran@metamage.com>
2008-08-13 07:44:44 +0000

    Initial check-in.

---
+++ lamp/jTools/chain/chain.cc
@@ -0,0 +1,96 @@
+/*	========
+ *	chain.cc
+ *	========
+ */
+
+// Standard C/C++
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+
+// POSIX
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+#pragma exceptions off
+
+
+enum link_type
+{
+	link_none,  // not a link
+	link_then,  // ';'
+	link_end    // trailing NULL
+};
+
+static link_type get_link_type( const char*& arg )
+{
+	if ( arg == NULL )
+	{
+		return link_end;
+	}
+	
+	if ( arg[0] == ';' && arg[1] == '\0' )
+	{
+		return link_then;
+	}
+	
+	if ( arg[0] == '\\' )
+	{
+		++arg;
+	}
+	
+	return link_none;
+}
+
+int main( int argc, const char *argv[] )
+{
+	const char **argq = argv;  // skip argv[0] later
+	
+	link_type type;
+	
+	do
+	{
+		const char **argp = argq + 1;
+		
+		while ( (type = get_link_type( *++argq )) == link_none )
+		{
+			continue;
+		}
+		
+		if ( argp == argq )
+		{
+			continue;
+		}
+		
+		*argq = NULL;  // null out the separator
+		
+		// Don't fork if we're last
+		pid_t pid = type == link_end ? 0 : vfork();
+		
+		if ( pid < 0 )
+		{
+			std::perror( "chain: vfork" );
+			
+			return 126;
+		}
+		
+		// child process, or end of chain
+		if ( pid == 0 )
+		{
+			(void) execvp( argp[ 0 ], argp );
+			
+			std::fprintf( stderr, "%s: %s: %s\n", argv[0], argp[0], std::strerror( errno ) );
+			
+			_exit( 127 );
+		}
+		
+		int wait_status = -1;
+		
+		wait( &wait_status );
+	}
+	while ( type != link_end );
+	
+	return 0;
+}
+
