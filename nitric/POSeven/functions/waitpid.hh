740471cd647a696d593b3615ca340309f05b8a3f
Joshua Juran <jjuran@metamage.com>
2008-08-29 14:24:11 +0000

    Initial check-in.

---
+++ nitric/POSeven/functions/waitpid.hh
@@ -0,0 +1,45 @@
+// waitpid.hh
+// ----------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2008 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#ifndef POSEVEN_FUNCTIONS_WAITPID_HH
+#define POSEVEN_FUNCTIONS_WAITPID_HH
+
+// POSIX
+#include <sys/wait.h>
+
+// POSeven
+#include "POSeven/Errno.hh"
+#include "POSeven/types/wait_t.hh"
+
+
+namespace poseven
+{
+	
+	inline pid_t waitpid( pid_t pid, wait_t& status, int options = 0 )
+	{
+		return pid_t( throw_posix_result( ::waitpid( pid, (int*) &status, options ) ) );
+	}
+	
+	inline wait_result waitpid( pid_t pid, int options = 0 )
+	{
+		wait_result result;
+		
+		result.pid = waitpid( pid, result.status, options );
+		
+		return result;
+	}
+	
+}
+
+#endif
+
---
+++ nitric/POSeven/types/wait_t.hh
@@ -0,0 +1,77 @@
+// wait_t.hh
+// ---------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2008 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#ifndef POSEVEN_TYPES_WAIT_T_HH
+#define POSEVEN_TYPES_WAIT_T_HH
+
+// Nucleus
+#include "Nucleus/Enumeration.h"
+
+// POSeven
+#include "POSeven/types/exit_t.hh"
+#include "POSeven/types/pid_t.hh"
+
+
+namespace poseven
+{
+	
+	enum wait_t
+	{
+		wait_t_min = -1,
+		
+		wait_t_max = Nucleus::Enumeration_Traits< int >::max
+	};
+	
+	inline int wtermsig( wait_t status )
+	{
+		return status & 0x7f;
+	}
+	
+	inline bool wcoredump( wait_t status )
+	{
+		return status & 0x80;
+	}
+	
+	inline exit_t wexitstatus( wait_t status )
+	{
+		return exit_t( (status >> 8) & 0xff );
+	}
+	
+	inline bool wifexited( wait_t status )
+	{
+		return wtermsig( status ) == 0;
+	}
+	
+	inline bool wifstopped( wait_t status )
+	{
+		return wtermsig( status ) == 0x7f;
+	}
+	
+	inline bool wifsignaled( wait_t status )
+	{
+		return !wifexited( status ) && !wifstopped( status );
+	}
+	
+	struct wait_result
+	{
+		pid_t   pid;
+		wait_t  status;
+		
+		operator pid_t () const  { return pid;    }
+		operator wait_t() const  { return status; }
+	};
+	
+}
+
+#endif
+
