331ba3946b52c8a152b935e2d5a9d397ebc6b165
Joshua Juran <jjuran@metamage.com>
2008-09-18 15:54:16 +0000

    Made little-endian compatible.

--- pedestal/Practica/GrafX/Pixel32.hh
+++ pedestal/Practica/GrafX/Pixel32.hh
@@ -25,13 +25,44 @@ namespace GrafX
 		Pixel32( const ::RGBColor& color );
 	};
 	
-	inline Pixel32::Pixel32( const ::RGBColor& color )
-	:
-		alpha( 0                ),
-		red  ( color.red   >> 8 ),
-		green( color.green >> 8 ),
-		blue ( color.blue  >> 8 )
+	inline Pixel32::Pixel32( const ::RGBColor& color ) : alpha( 0                ),
+	                                                     red  ( color.red   >> 8 ),
+	                                                     green( color.green >> 8 ),
+	                                                     blue ( color.blue  >> 8 )
 	{
+	}
+	
+	
+	struct Pixel32_Direct
+	{
+	#if TARGET_RT_BIG_ENDIAN
+		
+		unsigned char alpha;
+		unsigned char red;
+		unsigned char green;
+		unsigned char blue;
+		
+	#else
+		
+		unsigned char blue;
+		unsigned char green;
+		unsigned char red;
+		unsigned char alpha;
+		
+	#endif
+		
+		Pixel32_Direct()  {}
+		
+		Pixel32_Direct( const ::RGBColor& color );
+	};
+	
+	inline Pixel32_Direct::Pixel32_Direct( const ::RGBColor& color )
+	{
+		alpha = 0;
+		
+		red   = color.red   >> 8;
+		green = color.green >> 8;
+		blue  = color.blue  >> 8;
 	}
 	
 }
