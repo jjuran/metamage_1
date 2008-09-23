97a7e36514482f2a988d19dd3a7f5db4b182c8e4
Joshua Juran <jjuran@metamage.com>
2008-09-23 15:05:04 +0000

    Initial check-in.

---
+++ nitric/POSeven/functions/signal.hh
@@ -0,0 +1,49 @@
+// signal.hh
+// ---------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2008 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#ifndef POSEVEN_FUNCTIONS_SIGNAL_HH
+#define POSEVEN_FUNCTIONS_SIGNAL_HH
+
+// POSeven
+#include "POSeven/Errno.hh"
+#include "POSeven/types/signo_t.hh"
+
+
+namespace poseven
+{
+	
+	typedef void (*signal_handler_t)(int);
+	
+	static const signal_handler_t sig_dfl = SIG_DFL;
+	static const signal_handler_t sig_ign = SIG_IGN;
+	static const signal_handler_t sig_err = SIG_ERR;
+	
+	inline signal_handler_t throw_posix_result( signal_handler_t result )
+	{
+		if ( result < 0 )
+		{
+			throw_errno_internal( errno );
+		}
+		
+		return result;
+	}
+	
+	inline void signal( signo_t signo, signal_handler_t handler )
+	{
+		throw_posix_result( ::signal( signo, handler ) );
+	}
+	
+}
+
+#endif
+
---
+++ nitric/POSeven/types/signo_t.hh
@@ -0,0 +1,77 @@
+// signo_t.hh
+// ----------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2008 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#ifndef POSEVEN_TYPES_SIGNO_T_HH
+#define POSEVEN_TYPES_SIGNO_T_HH
+
+// Standard C
+#include <signal.h>
+
+// Nucleus
+#include "Nucleus/Enumeration.h"
+
+
+namespace poseven
+{
+	
+	enum signo_t
+	{
+		signo_t_min = Nucleus::Enumeration_Traits< int >::min,
+		
+		sigio = SIGIO,
+		
+		sigbus = SIGBUS,
+		sigfpe = SIGFPE,
+		sighup = SIGHUP,
+		sigill = SIGILL,
+		sigint = SIGINT,
+		sigsys = SIGSYS,
+		sigurg = SIGURG,
+		
+		sigabrt = SIGABRT,
+		sigalrm = SIGALRM,
+		sigchld = SIGCHLD,
+		sigcont = SIGCONT,
+		sigkill = SIGKILL,
+		sigpipe = SIGPIPE,
+		sigprof = SIGPROF,
+		sigquit = SIGQUIT,
+		sigsegv = SIGSEGV,
+		sigstop = SIGSTOP,
+		sigterm = SIGTERM,
+		sigtrap = SIGTRAP,
+		sigtstp = SIGTSTP,
+		sigttin = SIGTTIN,
+		sigttou = SIGTTOU,
+		sigusr1 = SIGUSR1,
+		sigusr2 = SIGUSR2,
+		sigxcpu = SIGXCPU,
+		sigxfsz = SIGXFSZ,
+		
+		sigwinch = SIGWINCH,
+		
+		sigvtalrm = SIGVTALRM,
+		
+	#ifdef SIGPWR
+		
+		sigpwr = SIGPWR,
+		
+	#endif
+		
+		signo_t_max = Nucleus::Enumeration_Traits< int >::max
+	};
+	
+}
+
+#endif
+
