a2d5de8d43ddf294f924405ed256e1c03916b294
Joshua Juran <jjuran@metamage.com>
2008-09-26 20:51:01 +0000

    Replaced by pump.h.

--- lamp/Convergence/pump.hh
+++ lamp/Convergence/pump.hh
@@ -6,18 +6,7 @@
 #ifndef PUMP_HH
 #define PUMP_HH
 
-// POSIX
-#include <sys/types.h>
-
-#ifndef __LAMP__
-
-#ifdef __cplusplus
-extern "C"
-#endif
-
-ssize_t pump( int fd_in, off_t* off_in, int fd_out, off_t* off_out, size_t count );
-
-#endif
+#include "pump.h"
 
 #endif
 
