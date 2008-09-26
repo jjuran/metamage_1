3a15af07fe4f005274ce52a556ff4843e7c700df
Joshua Juran <jjuran@metamage.com>
2008-09-26 15:56:10 +0000

    Initial check-in.

---
+++ nitric/POSeven/functions/connect.hh
@@ -0,0 +1,39 @@
+// connect.hh
+// ----------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2008 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#ifndef POSEVEN_FUNCTIONS_CONNECT_HH
+#define POSEVEN_FUNCTIONS_CONNECT_HH
+
+// POSIX
+#include <unistd.h>
+
+// POSeven
+#include "POSeven/Errno.hh"
+#include "POSeven/types/sockaddr.hh"
+
+
+namespace poseven
+{
+	
+	template < address_family af >
+	inline void connect( fd_t sock, const typename sockaddr_traits< af >::address_type& server_address )
+	{
+		throw_posix_result( ::connect( sock,
+		                               (const sockaddr*) &server_address,
+		                               sizeof (typename sockaddr_traits< af >::address_type) ) );
+	}
+	
+}
+
+#endif
+
---
+++ nitric/POSeven/types/sockaddr.hh
@@ -0,0 +1,43 @@
+// sockaddr.hh
+// -----------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2008 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#ifndef POSEVEN_TYPES_SOCKADDR_HH
+#define POSEVEN_TYPES_SOCKADDR_HH
+
+// POSIX
+#include <sys/socket.h>
+
+// Nucleus
+#include "Nucleus/Enumeration.h"
+
+
+namespace poseven
+{
+	
+	enum address_family
+	{
+		af_unspec    = AF_UNSPEC,
+		af_unix      = AF_UNIX,
+		af_inet      = AF_INET,
+		af_ipx       = AF_IPX,
+		af_appletalk = AF_APPLETALK,
+		
+		address_family_max = Nucleus::Enumeration_Traits< unsigned short >::max
+	};
+	
+	template < address_family family > struct sockaddr_traits;
+	
+}
+
+#endif
+
