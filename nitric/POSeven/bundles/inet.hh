7c7a26bbfd4559c4b9b7110a5abd8c100ee061b1
Joshua Juran <jjuran@metamage.com>
2008-09-27 18:59:01 +0000

    Added bind().

--- nitric/POSeven/bundles/inet.hh
+++ nitric/POSeven/bundles/inet.hh
@@ -15,6 +15,7 @@
 #define POSEVEN_BUNDLES_INET_HH
 
 // POSeven
+#include "POSeven/functions/bind.hh"
 #include "POSeven/functions/connect.hh"
 #include "POSeven/functions/socket.hh"
 #include "POSeven/types/sockaddr_in.hh"
@@ -22,6 +23,20 @@
 
 namespace poseven
 {
+	
+	inline void bind( fd_t fd, in_addr_t addr, in_port_t port )
+	{
+		bind< af_inet >( fd, sockaddr_traits< af_inet >::make( addr, port ) );
+	}
+	
+	inline Nucleus::Owned< fd_t > bind( in_addr_t addr, in_port_t port )
+	{
+		Nucleus::Owned< fd_t > fd = socket( pf_inet, sock_stream );
+		
+		bind( fd, addr, port );
+		
+		return fd;
+	}
 	
 	inline void connect( fd_t sock, in_addr_t addr, in_port_t port )
 	{
