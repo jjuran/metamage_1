c69d0df90fd78e4d8f5dea4df247a9eeac8eff73
Joshua Juran <jjuran@metamage.com>
2008-10-05 07:34:55 +0000

    Revise to pass on systems where vfork() is really fork().

--- lamp/Tests/test-longjmp-past-vfork/test-longjmp-past-vfork.cc
+++ lamp/Tests/test-longjmp-past-vfork/test-longjmp-past-vfork.cc
@@ -25,18 +25,32 @@ static void sigsegv_handler( int signo )
 
 static void spawn()
 {
+	static bool shared_memory = false;
+	
 	pid_t pid = vfork();
 	
 	if ( pid == 0 )
 	{
+		// This doesn't affect the parent if vfork() is really fork().
+		shared_memory = true;
+		
 		longjmp( global_jmpbuf, 1 );
 	}
 	
-	// vfork() returns in the parent, but the stack is already invalid.
+	// If memory is shared between parent and child, then we have a 'real'
+	// vfork().  In this case, when vfork() returns in the parent, the stack is
+	// already invalid.
 	// What we're testing is that the kernel won't let this happen.
 	// If we get here, we failed, so report it and get the hell out.
 	
-	write( STDOUT_FILENO, STR_LEN( "not ok 1\n" ) );
+	if ( shared_memory )
+	{
+		write( STDOUT_FILENO, STR_LEN( "not " ) );
+	}
+	
+	// Otherwise, vfork() is really fork() and there's no problem.
+	
+	write( STDOUT_FILENO, STR_LEN( "ok 1\n" ) );
 	
 	_exit( 0 );
 }
