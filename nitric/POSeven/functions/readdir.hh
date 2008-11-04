37e6144f1357782e4404821839aa21f397bec0a5
Joshua Juran <jjuran@metamage.com>
2008-11-04 12:54:36 +0000

    Split out from Directory.hh.

---
+++ nitric/POSeven/functions/readdir.hh
@@ -0,0 +1,40 @@
+// readdir.hh
+// ----------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2007 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#ifndef POSEVEN_FUNCTIONS_READDIR_HH
+#define POSEVEN_FUNCTIONS_READDIR_HH
+
+// POSeven
+#include "POSeven/Errno.hh"
+#include "POSeven/types/dir_t.hh"
+
+
+namespace poseven
+{
+	
+	inline const dirent& readdir( dir_t dir )
+	{
+		const dirent* entry = ::readdir( dir );
+		
+		if ( entry == NULL )
+		{
+			throw_errno( errno );
+		}
+		
+		return *entry;
+	}
+	
+}
+
+#endif
+
---
+++ nitric/POSeven/types/dir_t.hh
@@ -0,0 +1,29 @@
+// dir_t.hh
+// --------
+//
+// Maintained by Joshua Juran
+
+// Part of the Nitrogen project.
+//
+// Written 2007 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#ifndef POSEVEN_TYPES_DIR_T_HH
+#define POSEVEN_TYPES_DIR_T_HH
+
+// POSIX
+#include <dirent.h>
+
+
+namespace poseven
+{
+	
+	typedef DIR* dir_t;
+	
+}
+
+#endif
+
