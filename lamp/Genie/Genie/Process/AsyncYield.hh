39367d3e3903f028ffd8b777ba1c6c89cd241329
Joshua Juran <jjuran@metamage.com>
2008-11-15 21:01:18 +0000

    Initial check-in.

---
+++ lamp/Genie/Genie/Process/AsyncYield.hh
@@ -0,0 +1,18 @@
+/*	=============
+ *	AsyncYield.hh
+ *	=============
+ */
+
+#ifndef GENIE_PROCESS_ASYNCYIELD_HH
+#define GENIE_PROCESS_ASYNCYIELD_HH
+
+
+namespace Genie
+{
+	
+	void AsyncYield();  // defined in Process.cc
+	
+}
+
+#endif
+
