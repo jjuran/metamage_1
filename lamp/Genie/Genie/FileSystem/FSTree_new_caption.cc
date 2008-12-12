3b85eae4c5086a536c086b821018015683b79d4d
Joshua Juran <jjuran@metamage.com>
2008-12-12 01:07:57 +0000

    Implement caption text for real.

--- lamp/Genie/Genie/FileSystem/FSTree_new_caption.cc
+++ lamp/Genie/Genie/FileSystem/FSTree_new_caption.cc
@@ -5,6 +5,12 @@
 
 #include "Genie/FileSystem/FSTree_new_caption.hh"
 
+// POSIX
+#include <fcntl.h>
+
+// Nucleus
+#include "Nucleus/Saved.h"
+
 // ClassicToolbox
 #include "ClassicToolbox/MacWindows.h"
 
@@ -14,14 +20,32 @@
 
 // Genie
 #include "Genie/FileSystem/FSTree_Directory.hh"
+#include "Genie/FileSystem/FSTree_sys_window_REF.hh"
 #include "Genie/FileSystem/Views.hh"
+#include "Genie/IO/VirtualFile.hh"
 
 
 namespace Genie
 {
 	
 	namespace N = Nitrogen;
+	namespace NN = Nucleus;
 	namespace Ped = Pedestal;
+	
+	
+	static void InvalidateCurrentWindow()
+	{
+		N::InvalRect( N::GetPortBounds( N::GetQDGlobalsThePort() ) );
+	}
+	
+	static void InvalidateWindowRef( N::WindowRef window )
+	{
+		NN::Saved< N::Port_Value > savePort;
+		
+		N::SetPortWindowPort( window );
+		
+		InvalidateCurrentWindow();
+	}
 	
 	
 	extern const Functional_Traits< void >::Mapping Caption_view_Mappings[];
@@ -119,8 +143,91 @@ namespace Genie
 	}
 	
 	
-	typedef FSTree Caption_text;
+	class CaptionTextFileHandle : public VirtualFileHandle
+	{
+		public:
+			CaptionTextFileHandle( const FSTreePtr& file ) : VirtualFileHandle( file )
+			{
+			}
+			
+			const FSTree* ViewKey() const  { return GetFile()->Parent().get(); }
+			
+			std::string& String() const  { return gCaptionTextMap[ ViewKey() ]; }
+			
+			ssize_t SysRead( char* buffer, std::size_t byteCount );
+			
+			ssize_t SysWrite( const char* buffer, std::size_t byteCount );
+			
+			off_t GetEOF() const  { return String().size(); }
+			
+			void SetEOF( off_t eof )  { String().resize( eof ); }
+	};
 	
+	ssize_t CaptionTextFileHandle::SysRead( char* buffer, std::size_t byteCount )
+	{
+		std::string& s = String();
+		
+		ASSERT( GetFileMark() <= s.size() );
+		
+		byteCount = std::min( byteCount, s.size() - GetFileMark() );
+		
+		std::copy( s.begin() + GetFileMark(),
+		           s.begin() + GetFileMark() + byteCount,
+		           buffer );
+		
+		return Advance( byteCount );
+	}
+	
+	ssize_t CaptionTextFileHandle::SysWrite( const char* buffer, std::size_t byteCount )
+	{
+		std::string& s = String();
+		
+		if ( GetFileMark() + byteCount > s.size() )
+		{
+			s.resize( GetFileMark() + byteCount );
+		}
+		
+		std::copy( buffer,
+		           buffer + byteCount,
+		           s.begin() + GetFileMark() );
+		
+		InvalidateWindowRef( GetWindowRef( ViewKey()->Parent().get() ) );
+		
+		return Advance( byteCount );
+	}
+	
+	class FSTree_Caption_text : public FSTree
+	{
+		public:
+			FSTree_Caption_text( const FSTreePtr&    parent,
+			                     const std::string&  name ) : FSTree( parent, name )
+			{
+			}
+			
+			std::string& String() const  { return gCaptionTextMap[ Parent().get() ]; }
+			
+			mode_t FilePermMode() const  { return S_IRUSR | S_IWUSR; }
+			
+			off_t GetEOF() const  { return String().size(); }
+			
+			void SetEOF( off_t eof ) const  { String().resize( eof ); }
+			
+			boost::shared_ptr< IOHandle > Open( OpenFlags flags ) const;
+	};
+	
+	boost::shared_ptr< IOHandle > FSTree_Caption_text::Open( OpenFlags flags ) const
+	{
+		if ( const bool truncating  = flags & O_TRUNC )
+		{
+			SetEOF( 0 );
+			
+			InvalidateWindowRef( GetWindowRef( Parent()->Parent().get() ) );
+		}
+		
+		IOHandle* result = new CaptionTextFileHandle( shared_from_this() );
+		
+		return boost::shared_ptr< IOHandle >( result );
+	}
 	
 	
 	template < class FSTree_Type >
@@ -132,7 +239,7 @@ namespace Genie
 	
 	const Functional_Traits< void >::Mapping Caption_view_Mappings[] =
 	{
-		{ "text", &Factory< Caption_text > },
+		{ "text", &Factory< FSTree_Caption_text > },
 		
 		{ NULL, NULL }
 	};
