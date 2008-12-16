af1fc3ea1d41b6c7b1eb8bc1f6915edeaa8a901d
Joshua Juran <jjuran@metamage.com>
2008-12-16 10:20:33 +0000

    Cleanup.

--- lamp/Genie/Genie/FileSystem/Views.hh
+++ lamp/Genie/Genie/FileSystem/Views.hh
@@ -31,24 +31,22 @@ namespace Genie
 	
 	void RemoveAllViewParameters( const FSTree* parent );
 	
+	
 	struct ViewFactory
 	{
 		virtual std::auto_ptr< Pedestal::View > operator()() const = 0;
 	};
+	
 	
 	void AddViewParameters( const FSTree*                            parent,
 	                        const std::string&                       name,
 	                        const boost::shared_ptr< ViewFactory >&  factory,
 	                        const FSTreePtr&                         delegate );
 	
-	void AddViewFactory( const FSTree* parent, const std::string& name, const boost::shared_ptr< ViewFactory >& factory );
-	
 	void AddViewWindowKey( const FSTree* parent, const std::string& name, const FSTree* windowKey );
 	
+	
 	const boost::shared_ptr< ViewFactory >& GetViewFactory( const FSTree* parent, const std::string& name );
-	
-	
-	void AddViewDelegate( const FSTree* parent, const std::string& name, const FSTreePtr& delegate );
 	
 	const FSTreePtr& GetViewDelegate( const FSTree* parent, const std::string& name );
 	
