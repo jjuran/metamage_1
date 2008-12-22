9aa7078980ebae5f431f12a3c3779faf0c5c27f1
Joshua Juran <jjuran@metamage.com>
2008-12-22 04:26:39 +0000

    Add flags argument to pump() to match splice().

--- lamp/LAMP-headers/fcntl.h
+++ lamp/LAMP-headers/fcntl.h
@@ -103,7 +103,7 @@ extern "C" {
 	
 	int creat( const char* pathname, mode_t mode );
 	
-	ssize_t pump( int fd_in, off_t* off_in, int fd_out, off_t* off_out, size_t count );
+	ssize_t pump( int fd_in, off_t* off_in, int fd_out, off_t* off_out, size_t count, unsigned flags );
 	
 #ifdef __cplusplus
 }
