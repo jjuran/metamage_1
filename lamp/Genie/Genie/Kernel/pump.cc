51fd9c5f4825476640ac44a32d9808f96269bdcb
Joshua Juran <jjuran@metamage.com>
2008-12-22 04:25:10 +0000

    Add flags argument to match splice().

--- lamp/Genie/Genie/Kernel/pump.cc
+++ lamp/Genie/Genie/Kernel/pump.cc
@@ -20,7 +20,7 @@
 namespace Genie
 {
 	
-	static ssize_t pump( int fd_in, off_t* off_in, int fd_out, off_t* off_out, size_t count )
+	static ssize_t pump( int fd_in, off_t* off_in, int fd_out, off_t* off_out, size_t count, unsigned flags )
 	{
 		SystemCallFrame frame( "pump" );
 		
