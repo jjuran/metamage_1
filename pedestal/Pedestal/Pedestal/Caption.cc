b8cfb6093a86e2c267eafa30a0ed56cc58cc2efe
Joshua Juran <jjuran@metamage.com>
2008-12-29 20:17:49 +0000

    Clip to our bounds if we're drawing unwrapped.

--- pedestal/Pedestal/Pedestal/Caption.cc
+++ pedestal/Pedestal/Pedestal/Caption.cc
@@ -5,6 +5,9 @@
 
 #include "Pedestal/Caption.hh"
 
+// Nucleus
+#include "Nucleus/Saved.h"
+
 // Nitrogen
 #include "Nitrogen/TextEdit.h"
 
@@ -13,15 +16,20 @@ namespace Pedestal
 {
 	
 	namespace N = Nitrogen;
+	namespace NN = Nucleus;
 	
 	
 	void Caption::Draw( const Rect& bounds )
 	{
+		NN::Saved< N::Clip_Value > savedClip;
+		
 		Rect newBounds = bounds;
 		
 		if ( !Wrapped() )
 		{
 			newBounds.right = 30000;
+			
+			N::ClipRect( bounds );
 		}
 		
 		N::TETextBox( Text(), newBounds );
