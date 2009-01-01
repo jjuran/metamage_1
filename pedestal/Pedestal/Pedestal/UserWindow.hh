91761e985b615998084dbdadd8a49277d5c89741
Joshua Juran <jjuran@metamage.com>
2009-01-01 19:59:54 +0000

    Derive UserView from Superview and remove redundant overrides.

--- pedestal/Pedestal/Pedestal/UserWindow.hh
+++ pedestal/Pedestal/Pedestal/UserWindow.hh
@@ -10,20 +10,22 @@
 #include <memory>
 
 // Pedestal
-#include "Pedestal/View.hh"
+#include "Pedestal/Superview.hh"
 #include "Pedestal/Window.hh"
 
 
 namespace Pedestal
 {
 	
-	class UserView : public View
+	class UserView : public Superview
 	{
 		private:
 			boost::shared_ptr< View > itsView;
 		
 		public:
 			~UserView();
+			
+			View& Subview()  { return *itsView; }
 			
 			void Set( boost::shared_ptr< View > const& view )  { itsView = view; }
 			void Set( std::auto_ptr    < View >        view )  { itsView = view; }
@@ -37,28 +39,9 @@ namespace Pedestal
 			template < class ViewType >
 			const ViewType& Get() const  { return static_cast< const ViewType& >( *itsView ); }
 			
-			void Idle     ( const EventRecord& event )  {        itsView->Idle     ( event ); }
-			void MouseDown( const EventRecord& event )  {        itsView->MouseDown( event ); }
-			bool KeyDown  ( const EventRecord& event )  { return itsView->KeyDown  ( event ); }
-			
-			boost::shared_ptr< Quasimode > EnterShiftSpaceQuasimode( const EventRecord& event )
-			{
-				return itsView->EnterShiftSpaceQuasimode( event );
-			}
-			
 			void Resize( short width, short height )  { itsView->Resize( width, height ); }
 			
 			void Draw( const Rect& bounds )  { itsView->Draw( bounds ); }
-			
-			void Activate( bool activating )  { itsView->Activate( activating ); }
-			
-			bool SetCursor( const EventRecord&  event,
-			                RgnHandle           mouseRgn  )
-			{
-				return itsView->SetCursor( event, mouseRgn );
-			}
-			
-			bool UserCommand( MenuItemCode code  )  { return itsView->UserCommand( code ); }
 	};
 	
 	class UserWindow : public Window
