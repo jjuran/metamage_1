d388abf3f1f673afbc8c6d7e4214834744aa5421
Joshua Juran <jjuran@metamage.com>
2009-01-02 10:23:42 +0000

    Remove superfluous TextLength() TextHandle() methods.

--- pedestal/Pedestal/Pedestal/TEView.hh
+++ pedestal/Pedestal/Pedestal/TEView.hh
@@ -51,10 +51,6 @@ namespace Pedestal
 			
 			Rect Bounds() const  { return Pedestal::Bounds( Get() ); }
 			
-			// Text manipulation
-			SInt16 TextLength() const  { return itsTE[0]->teLength; }
-			Handle TextHandle()        { return itsTE[0]->hText;    }
-			
 			void SetSelection( short start, short end )
 			{
 				Nitrogen::TESetSelect( start, end, itsTE );
