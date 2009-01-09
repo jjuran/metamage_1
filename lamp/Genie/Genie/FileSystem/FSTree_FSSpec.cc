9d56e0bb6653acc569d6df09d246f372f82080cb
Joshua Juran <jjuran@metamage.com>
2009-01-09 09:12:28 +0000

    Let renameat() call FSTree::Rename(). Move FSSpec-specific implementation into FSTree_HFS::Rename().

--- lamp/Genie/Genie/FileSystem/FSTree_FSSpec.cc
+++ lamp/Genie/Genie/FileSystem/FSTree_FSSpec.cc
@@ -338,6 +338,8 @@ namespace Genie
 			
 			void Delete() const;
 			
+			void Rename( const FSTreePtr& destination ) const;
+			
 			ino_t Inode() const;
 			
 			void CopyFile( const FSTreePtr& destination ) const;
@@ -808,6 +810,234 @@ namespace Genie
 		}
 		
 		N::ThrowOSStatus( deleteErr );
+	}
+	
+	
+	static const char* Basename( const char* path )
+	{
+		const char* slash = std::strrchr( path, '/' );
+		
+		if ( slash == NULL )
+		{
+			return path;
+		}
+		
+		return slash + 1;
+	}
+	
+	inline std::string UntweakMacFilename( std::string name )
+	{
+		std::replace( name.begin(), name.end(), ':', '/' );
+		
+		return name;
+	}
+	
+	static bool NamesAreSame( ConstStr63Param a, ConstStr63Param b )
+	{
+		// b may be a null string to indicate a will be reused, so they're the same
+		return b[0] == 0  ||  std::equal( a,
+		                                  a + 1 + a[0],
+		                                  b );
+	}
+	
+	static bool FileIsLocked( const FSSpec& file )
+	{
+		CInfoPBRec cInfo;
+		
+		N::FSpGetCatInfo( file, cInfo );
+		
+		bool locked = cInfo.hFileInfo.ioFlAttrib & kioFlAttribLockedMask;
+		
+		return locked;
+	}
+	
+	class FileLockBypass
+	{
+		private:
+			FSSpec  itsFileSpec;
+			bool    itWasLocked;
+		
+		public:
+			FileLockBypass( const FSSpec& file ) : itsFileSpec( file ),
+			                                       itWasLocked( FileIsLocked( file ) )
+			{
+				if ( itWasLocked )
+				{
+					N::FSpRstFLock( file );
+				}
+			}
+			
+			~FileLockBypass()
+			{
+				if ( itWasLocked )
+				{
+					OSErr err = ::FSpSetFLock( &itsFileSpec );
+				}
+			}
+			
+			void SetFile( const FSSpec& file )
+			{
+				itsFileSpec = file;
+			}
+	};
+	
+	static void RenameItem( const FSSpec& srcFile, const FSSpec& destFile )
+	{
+		ASSERT( srcFile.vRefNum == destFile.vRefNum );
+		ASSERT( srcFile.parID   == destFile.parID   );
+		
+		FileLockBypass lockBypass( srcFile );
+		
+		// Rename source to dest
+		N::FSpRename( srcFile, destFile.name );
+		
+		lockBypass.SetFile( destFile );
+	}
+	
+	static void MoveAndRename( const FSSpec& srcFile, const FSSpec& destFile )
+	{
+		// Darn, we have to move *and* rename.  Use MoreFiles.
+		
+		// destFolder is the parent of destFile.
+		// It's semantically invalid (though it might work) to pass an
+		// FSSpec with null fields to Mac OS, but MoreFiles won't do that.
+		// (It breaks it into individual parts before passing them.)
+			
+		FSSpec destFolder;
+		
+		destFolder.vRefNum = destFile.vRefNum;
+		destFolder.parID   = destFile.parID;
+		
+		destFolder.name[0] = '\0';
+		
+		FileLockBypass lockBypass( srcFile );
+		
+		N::ThrowOSStatus( ::FSpMoveRenameCompat( &srcFile, &destFolder, destFile.name ) );
+		
+		lockBypass.SetFile( destFile );
+	}
+	
+	static void SetLongName( const FSSpec& item, const std::string& name )
+	{
+		if ( name.length() > 31 )
+		{
+			N::FSpDTSetComment( item, name );
+		}
+	}
+	
+	static FSSpec GetFSSpecForRenameDestination( const FSTreePtr& file )
+	{
+		struct ::stat stat_buffer;
+		
+		// Throws ENOENT if parent doesn't exist
+		file->Parent()->Stat( stat_buffer );
+		
+		if ( stat_buffer.st_dev <= 0 )
+		{
+			// Not FSSpecified
+			p7::throw_errno( EXDEV );
+		}
+		
+		FSSpec result;
+		
+		// vRefNum and dir ID from parent
+		result.vRefNum = -stat_buffer.st_dev;
+		result.parID   =  stat_buffer.st_ino;
+		
+		// The requested name (not the on-disk name, in the event of case conflict)
+		N::CopyToPascalString( UntweakMacFilename( file->Name() ),
+		                       result.name,
+		                       sizeof result.name - 1 );
+		
+		return result;
+	}
+	
+	void FSTree_HFS::Rename( const FSTreePtr& destFile ) const
+	{
+		const FSTree_HFS* srcFile = this;
+		
+		if ( !srcFile->Exists() )
+		{
+			p7::throw_errno( ENOENT );
+		}
+		
+		bool destExists = destFile->Exists();
+		
+		bool srcIsDir  = srcFile ->IsDirectory();
+		bool destIsDir = destFile->IsDirectory();
+		
+		if ( destExists  &&  srcIsDir != destIsDir )
+		{
+			p7::throw_errno( destIsDir ? EISDIR : ENOTDIR );
+		}
+		
+		FSSpec srcFileSpec = srcFile->GetFSSpec( false );
+		FSSpec destFileSpec;
+		
+		try
+		{
+			destFileSpec = destFile->GetFSSpec( !destExists );
+		}
+		catch ( ... )
+		{
+			// Case-sensitivity conflict
+			destFileSpec = GetFSSpecForRenameDestination( destFile );
+		}
+		
+		// Can't move across volumes
+		if ( srcFileSpec.vRefNum != destFileSpec.vRefNum )
+		{
+			p7::throw_errno( EXDEV );
+		}
+		
+		N::FSVolumeRefNum vRefNum = N::FSVolumeRefNum( srcFileSpec.vRefNum );
+		
+		bool sameName = NamesAreSame( srcFileSpec.name, destFileSpec.name );
+		
+		if ( srcFileSpec.parID == destFileSpec.parID )
+		{
+			// Same dir.
+			
+			if ( sameName )
+			{
+				// Identical names in the same directory.  Nothing to do.
+				return;
+			}
+			
+			// destExists is false for case changes
+			if ( destExists )
+			{
+				// Delete existing dest file
+				N::FSpDelete( destFileSpec );
+			}
+			
+			RenameItem( srcFileSpec, destFileSpec );
+			
+			SetLongName( destFileSpec, destFile->Name() );
+			
+			// And we're done
+			return;
+		}
+		
+		if ( destExists )
+		{
+			// Can't be the same file, because it's in a different directory.
+			
+			N::FSpDelete( destFileSpec );
+		}
+		
+		if ( sameName )
+		{
+			// Same name, different dir; move only.
+			N::FSpCatMove( srcFileSpec, N::FSDirID( destFileSpec.parID ) );
+		}
+		else
+		{
+			// Darn, we have to move *and* rename.
+			MoveAndRename( srcFileSpec, destFileSpec );
+			
+			SetLongName( destFileSpec, destFile->Name() );
+		}
 	}
 	
 	
