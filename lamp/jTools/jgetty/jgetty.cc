55095d608192fe464b9e0cb87629d4c9c5374d3b
Joshua Juran <jjuran@metamage.com>
2009-01-22 09:40:19 +0000

    Reimplemented via /new/window (instead of /dev/new/console).

--- lamp/jTools/jgetty/jgetty.cc
+++ lamp/jTools/jgetty/jgetty.cc
@@ -3,82 +3,97 @@
  *	=========
  */
 
-// POSIX
-#include <fcntl.h>
-#include <sys/ioctl.h>
-#include <sys/wait.h>
-#include <unistd.h>
+// Iota
+#include "iota/strings.hh"
 
-// Lamp
-#include "lamp/winio.h"
+// POSeven
+#include "POSeven/extras/spew.hh"
+#include "POSeven/functions/chdir.hh"
+#include "POSeven/functions/execv.hh"
+#include "POSeven/functions/ioctl.hh"
+#include "POSeven/functions/link.hh"
+#include "POSeven/functions/open.hh"
+#include "POSeven/functions/rename.hh"
+#include "POSeven/functions/setsid.hh"
+#include "POSeven/functions/symlink.hh"
+#include "POSeven/functions/utime.hh"
+#include "POSeven/functions/vfork.hh"
+#include "POSeven/functions/wait.hh"
+
+
+namespace NN = Nucleus;
+namespace p7 = poseven;
 
 
 int main( int argc, char const *const argv[] )
 {
-	int console = open( "/dev/new/console", O_RDWR, 0 );
+	p7::chdir( "/new/window" );
 	
-	if ( console == -1 )
+	const short width  = 2 * 4 +  6 * 80 + 15;
+	const short height = 2 * 4 + 11 * 24;
+	
+	p7::spew( "size", STR_LEN( "503x272" "\n" ) );
+	
+	p7::utime( "ref" );
+	
+	p7::spew( "ref/text-font", STR_LEN( "4" "\n" ) );
+	p7::spew( "ref/text-size", STR_LEN( "9" "\n" ) );
+	
+	p7::link( "/new/scrollframe", "view"     );
+	p7::link( "/new/frame",       "view/v"   );
+	p7::link( "/new/console",     "view/v/v" );
+	
+	p7::symlink( "v/v", "view/target" );
+	
+	p7::spew( "view/vertical", STR_LEN( "1" "\n" ) );
+	p7::spew( "view/v/margin", STR_LEN( "4" "\n" ) );
+	
+	p7::rename( "view/v/v/tty", "tty" );
+	
+	NN::Owned< p7::fd_t > tty = p7::open( "tty", p7::o_rdwr );
+	
+	dup2( tty, p7::stdin_fileno  );
+	dup2( tty, p7::stdout_fileno );
+	dup2( tty, p7::stderr_fileno );
+	
+	if ( tty.get() > p7::stderr_fileno )
 	{
-		return 1;
+		p7::close( tty );
 	}
 	
-	dup2( console, STDIN_FILENO  );
-	dup2( console, STDOUT_FILENO );
-	dup2( console, STDERR_FILENO );
+	p7::pid_t sid = p7::setsid();  // New session
 	
-	if ( console > STDERR_FILENO )
+	p7::ioctl( p7::stdin_fileno, TIOCSCTTY, NULL );  // Reattach to terminal
+	
+	if ( const char* name = ttyname( p7::stdin_fileno ) )
 	{
-		close( console );
+		std::string title = name;
+		
+		p7::spew( "title", title + "\n" );
 	}
 	
-	int sid = setsid();  // New session
-	
-	if ( sid < 0 )
-	{
-		return 2;
-	}
-	
-	int result = ioctl( STDIN_FILENO, TIOCSCTTY, NULL );  // Reattach to terminal
-	
-	if ( result < 0 )
-	{
-		return 3;
-	}
-	
-	pid_t pid = vfork();
-	
-	if ( pid < 0 )
-	{
-		return 4;
-	}
+	p7::pid_t pid = p7::vfork();
 	
 	if ( pid == 0 )
 	{
+		p7::chdir( "/" );
+		
 		char* login_argv[] = { "/bin/login", NULL };
 		
-		(void) execv( login_argv[0], login_argv );
-		
-		_exit( 127 );
+		p7::execv( login_argv[0], login_argv );
 	}
 	
-	int wait_status = -1;
-	
-	int waited = wait( &wait_status );
-	
-	if ( waited < 0 )
-	{
-		return 5;
-	}
+	p7::wait_t wait_status = p7::wait();
 	
 	// If the child exited unsuccessfully and the terminal is still connected,
 	// keep the window around.
-	if ( wait_status != 0  &&  write( STDOUT_FILENO, "", 0 ) == 0 )
+	if ( wait_status != 0  &&  ::write( p7::stdout_fileno, "", 0 ) == 0 )
 	{
 		// Reset ourselves as the foreground process group of the terminal,
 		// so we'll get SIGHUP when the window is closed.
-		int pgrpset = tcsetpgrp( STDIN_FILENO, sid );
+		int pgrpset = tcsetpgrp( p7::stdin_fileno, sid );
 		
-		result = ioctl( STDIN_FILENO, WIOCSTITLE, "(Done)" );
+		p7::spew( "title", STR_LEN( "(Done)" "\n" ) );
 		
 		// Loop so resizing the window doesn't kill us (via SIGWINCH).
 		while ( true )
