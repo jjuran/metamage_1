856f8d29654f0be94177d813e5a3fb618076db72
Joshua Juran <jjuran@metamage.com>
2009-02-06 08:27:09 +0000

    Unbreak previous damage.

--- lamp/Genie/Genie/FileSystem/FSTree_new_console.cc
+++ lamp/Genie/Genie/FileSystem/FSTree_new_console.cc
@@ -275,11 +275,6 @@ namespace Genie
 	
 	bool Console::KeyDown( const EventRecord& event )
 	{
-		if ( Ped::KeyIsPreHandled( *this, event ) )
-		{
-			return true;
-		}
-		
 		char c   =  event.message & charCodeMask;
 		char key = (event.message & keyCodeMask) >> 8;
 		
