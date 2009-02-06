495ff2ba43d6790c254a0570a475f890c6c4a2b7
Joshua Juran <jjuran@metamage.com>
2009-02-06 20:04:15 +0000

    Remove obsolete class UserView.

--- pedestal/Pedestal/Pedestal/UserWindow.cc
+++ pedestal/Pedestal/Pedestal/UserWindow.cc
@@ -9,9 +9,5 @@
 namespace Pedestal
 {
 	
-	UserView::~UserView()
-	{
-	}
-	
 }
 
--- pedestal/Pedestal/Pedestal/UserWindow.hh
+++ pedestal/Pedestal/Pedestal/UserWindow.hh
@@ -6,37 +6,12 @@
 #ifndef PEDESTAL_USERWINDOW_HH
 #define PEDESTAL_USERWINDOW_HH
 
-// Standard C++
-#include <memory>
-
 // Pedestal
-#include "Pedestal/Superview.hh"
 #include "Pedestal/Window.hh"
 
 
 namespace Pedestal
 {
-	
-	class UserView : public Superview
-	{
-		private:
-			boost::shared_ptr< View > itsView;
-		
-		public:
-			~UserView();
-			
-			View& Subview()  { return *itsView; }
-			
-			void Set( boost::shared_ptr< View > const& view )  { itsView = view; }
-			void Set( std::auto_ptr    < View >        view )  { itsView = view; }
-			
-			View      & Get()        { return *itsView; }
-			View const& Get() const  { return *itsView; }
-			
-			void Resize( short width, short height )  { itsView->Resize( width, height ); }
-			
-			void Draw( const Rect& bounds )  { itsView->Draw( bounds ); }
-	};
 	
 	class UserWindow : public Window
 	{
