5afb6d271cb6c49f1dbb54885774ee43869bf805
Joshua Juran <jjuran@metamage.com>
2009-03-04 10:05:32 +0000

    Cleanup.

--- nitric/Nitrogen/Timer.h
+++ nitric/Nitrogen/Timer.h
@@ -14,17 +14,15 @@
 #ifndef NITROGEN_TIMER_H
 #define NITROGEN_TIMER_H
 
-#ifndef __TIMER__
+// Mac OS
 #include <Timer.h>
-#endif
 
-#ifndef NITROGEN_OSSTATUS_H
-#include "Nitrogen/OSStatus.h"
-#endif
-#ifndef NITROGEN_UPP_H
-#include "Nitrogen/UPP.h"
-#endif
+// Nucleus
 #include "Nucleus/ErrorsRegistered.h"
+
+// Nitrogen
+#include "Nitrogen/OSStatus.h"
+#include "Nitrogen/UPP.h"
 
 
 namespace Nitrogen
@@ -34,22 +32,25 @@ namespace Nitrogen
 	
 	using ::TMTask;
 	using ::TMTaskPtr;
-  }
+	
+}
 
 namespace Nucleus
-  {
+{
+	
 	template <>
-	struct Disposer< Nitrogen::TMTaskPtr > : public  std::unary_function< Nitrogen::TMTaskPtr, void >,
-                                             private Nitrogen::DefaultDestructionOSStatusPolicy
+	struct Disposer< TMTaskPtr > : public  std::unary_function< TMTaskPtr, void >,
+                                   private Nitrogen::DefaultDestructionOSStatusPolicy
 	{
-		void operator()( Nitrogen::TMTaskPtr tmTaskPtr ) const
+		void operator()( TMTaskPtr tmTaskPtr ) const
 		{
 			NUCLEUS_REQUIRE_ERRORS( Nitrogen::TimeManager );
 			
 			HandleDestructionOSStatus( ::RemoveTimeTask( reinterpret_cast< ::QElemPtr >( tmTaskPtr ) ) );
 		}
 	};
-  }
+	
+}
 
 namespace Nitrogen
 {
@@ -104,10 +105,12 @@ namespace Nitrogen
 		return NewUPP< TimerUPP >( p );
 	}
 	
-	inline void DisposeTimerUPP( Nucleus::Owned< TimerUPP > )  {}
+	inline void DisposeTimerUPP( Nucleus::Owned< TimerUPP > )
+	{
+	}
 	
-	inline void InvokeTimerUPP( TMTaskPtr tmTaskPtr,
-	                            TimerUPP  userUPP )
+	inline void InvokeTimerUPP( TMTaskPtr  tmTaskPtr,
+	                            TimerUPP   userUPP )
 	{
 		::InvokeTimerUPP( tmTaskPtr, userUPP.Get() );
 	}
