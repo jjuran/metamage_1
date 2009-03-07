1685b54a651ea46f976b8305001f1a4e74f14a40
Joshua Juran <jjuran@metamage.com>
2009-03-07 18:00:17 +0000

    Initial check-in.

---
+++ lamp/Genie/Genie/FileSystem/FSTree_IconSuite.hh
@@ -0,0 +1,33 @@
+/*	===================
+ *	FSTree_IconSuite.hh
+ *	===================
+ */
+
+#ifndef GENIE_FILESYSTEM_FSTREE_ICONSUITE_HH
+#define GENIE_FILESYSTEM_FSTREE_ICONSUITE_HH
+
+// Nucleus
+#include "Nucleus/Shared.h"
+
+// Nitrogen
+#include "Nitrogen/Icons.h"
+
+// Genie
+#include "Genie/FileSystem/FSTree.hh"
+
+
+namespace Genie
+{
+	
+	FSTreePtr
+	//
+	New_FSTree_IconSuite( const FSTreePtr&                                  parent,
+			              const std::string&                                name,
+			              const Nucleus::Shared< Nitrogen::IconSuiteRef >&  iconSuite );
+	
+	const Nucleus::Shared< Nitrogen::IconSuiteRef >& Fetch_IconSuite();
+	
+}
+
+#endif
+
