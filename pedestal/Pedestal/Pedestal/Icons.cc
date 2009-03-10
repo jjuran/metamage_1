2b2dbcb8a013db8aa387d7b37dab24c4900c7f0f
Joshua Juran <jjuran@metamage.com>
2009-03-10 17:32:26 +0000

    Move Pedestal classes to Pedestal.

--- pedestal/Pedestal/Pedestal/Icons.cc
+++ pedestal/Pedestal/Pedestal/Icons.cc
@@ -6,12 +6,68 @@
 #include "Pedestal/Icons.hh"
 
 
+namespace Nitrogen
+{
+	
+	static void PlotIconHandle( const Rect&        area,
+	                            IconAlignmentType  align,
+	                            IconTransformType  transform,
+	                            Handle             icon )
+	{
+		ThrowOSStatus( ::PlotIconHandle( &area, align, transform, icon ) );
+	}
+	
+}
+
 namespace Pedestal
 {
 	
 	namespace N = Nitrogen;
-	namespace NN = Nucleus;
 	
+	
+	void PlainIcon::Draw( const Rect& bounds, bool erasing )
+	{
+		if ( N::Handle data = Data() )
+		{
+			if ( erasing )
+			{
+				N::EraseRect( bounds );
+			}
+			
+			N::PlotIconHandle( bounds, Alignment(), Transform(), data );
+		}
+	}
+	
+	
+	void IconID::Draw( const Rect& bounds, bool erasing )
+	{
+		if ( erasing )
+		{
+			N::EraseRect( bounds );
+		}
+		
+		try
+		{
+			N::PlotIconID( bounds, Alignment(), Transform(), ID() );
+		}
+		catch ( const Undefined& )
+		{
+		}
+	}
+	
+	
+	void IconSuite::Draw( const Rect& bounds, bool erasing )
+	{
+		if ( N::IconSuiteRef ref = Ref() )
+		{
+			if ( erasing )
+			{
+				N::EraseRect( bounds );
+			}
+			
+			N::PlotIconSuite( bounds, Alignment(), Transform(), ref );
+		}
+	}
 	
 }
 
