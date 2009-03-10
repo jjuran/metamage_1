5ffd4586d7a052a3d1a0f64b1979d64fd38eff0c
Joshua Juran <jjuran@metamage.com>
2009-03-10 16:40:54 +0000

    Decruftify.

--- pedestal/Pedestal/Pedestal/Icons.hh
+++ pedestal/Pedestal/Pedestal/Icons.hh
@@ -9,67 +9,9 @@
 // Nitrogen
 #include "Nitrogen/Icons.h"
 
-// Pedestal
-#include "Pedestal/Graphic.hh"
-
 
 namespace Pedestal
 {
-	
-	class PlainIcon : public Graphic
-	{
-		typedef Nitrogen::PlainIconHandle Handle;
-		
-		typedef Nucleus::Owned< Handle >  Value;
-		typedef Nucleus::Owned< Handle >  Parameter;
-		typedef Handle                    GetResult;
-		
-		private:
-			Value value;
-		
-		public:
-			PlainIcon( Parameter v ) : value( v )  {}
-			
-			GetResult Get() const  { return value; }
-			
-			void Plot( const Rect& area ) const;
-	};
-	
-	class MaskedIcon : public Graphic
-	{
-		typedef Nitrogen::MaskedIconHandle Handle;
-		
-		typedef Nucleus::Owned< Handle >  Value;
-		typedef Nucleus::Owned< Handle >  Parameter;
-		typedef Handle                    GetResult;
-		
-		private:
-			Value value;
-		
-		public:
-			MaskedIcon( Parameter v ) : value( v )  {}
-			
-			GetResult Get() const  { return value; }
-			
-			void Plot( const Rect& area ) const;
-	};
-	
-	class IconID : public Graphic
-	{
-		typedef Nitrogen::ResID Value;
-		typedef Nitrogen::ResID Parameter;
-		typedef Nitrogen::ResID GetResult;
-		
-		private:
-			Value value;
-		
-		public:
-			IconID( Parameter v ) : value( v )  {}
-			
-			GetResult Get() const  { return value; }
-			
-			void Plot( const Rect& area ) const;
-	};
 	
 }
 
