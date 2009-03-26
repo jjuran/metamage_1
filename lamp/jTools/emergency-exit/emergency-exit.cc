5cd1573c24f4580bb90fbdbe0c3d165085292d7b
Joshua Juran <jjuran@metamage.com>
2009-03-26 02:37:19 -0700

    Initial check-in of emergency-exit.
    Emergency-exit calls ExitToShell().  It should be used only when quitting Genie would cause undesirable results, such as a crash.

---
+++ lamp/jTools/emergency-exit/emergency-exit.cc
@@ -0,0 +1,17 @@
+/*	=================
+ *	emergency-exit.cc
+ *	=================
+ */
+
+// Mac OS
+#include <Processes.h>
+
+
+int main( void )
+{
+	ExitToShell();
+	
+	// Not reached
+	return 0;
+}
+
