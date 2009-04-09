6d4241fdb4d2b5d77b809497876c141aa38a009c
Joshua Juran <jjuran@metamage.com>
2009-04-09 14:51:28 -0700

    Remove Abort() and call DeliverFatalSignal( SIGABRT ) instead.
    Abort() did two things differently that were broken:
    * stopping the thread is Bad if it's the main thread
    * writing to the system console is worthless if the main thread stops and never processes update events

--- lamp/Genie/Genie/Abort.cc
+++ lamp/Genie/Genie/Abort.cc
@@ -3,54 +3,22 @@
  *	========
  */
 
+// Mac OS
+#include <Processes.h>
+
 // Standard C
 #include <signal.h>
 #include <stdlib.h>
 
-// Iota
-#include "iota/strings.hh"
-
-// Nitrogen
-#include "Nitrogen/Threads.h"
-
 // Genie
 #include "Genie/Process.hh"
-#include "Genie/SystemConsole.hh"
 
-
-namespace Genie
-{
-	
-	namespace N = Nitrogen;
-	
-	
-	static void Abort()
-	{
-		Process& current = CurrentProcess();
-		
-		if ( current.GetSchedule() != kProcessUnscheduled )
-		{
-			current.ResetSignalAction( SIGABRT );
-			
-			current.Raise( SIGABRT );
-			
-			// restartability is immaterial for fatal signal
-			current.HandlePendingSignals( kInterruptNever );
-			
-			// Probably not reached
-			WriteToSystemConsole( STR_LEN( "Genie: abort(): process failed to terminate, stopping its thread\n" ) );
-		}
-		
-		N::SetThreadState( N::GetCurrentThread(), N::kStoppedThreadState );
-	}
-	
-}
 
 void abort()
 {
 	try
 	{
-		Genie::Abort();
+		Genie::DeliverFatalSignal( SIGABRT );
 	}
 	catch ( ... )
 	{
