b87bb4096290391bd00c7c2ab87eeb28f1d8000c
Joshua Juran <jjuran@metamage.com>
2009-04-23 17:11:10 -0700

    Normalize whitespace.

--- lamp/LAMP-headers/sys/stat.h
+++ lamp/LAMP-headers/sys/stat.h
@@ -45,21 +45,22 @@
 #include <sys/types.h>
 #include <time.h>
 
-struct stat {
-	dev_t	st_dev;			/* inode's device */
-	ino_t	st_ino;			/* inode's number */
-	mode_t	st_mode;		/* inode protection mode */
-	nlink_t	st_nlink;		/* number of hard links */
-	uid_t	st_uid;			/* user ID of the file's owner */
-	gid_t	st_gid;			/* group ID of the file's group */
-	dev_t	st_rdev;		/* device type */
-	struct timespec	st_atim;		/* time of last access */
-	struct timespec	st_mtim;		/* time of last data modification */
-	struct timespec	st_ctim;		/* time of last file status change */
-	off_t	st_size;		/* file size, in bytes */
-	long	st_blocks;		/* blocks allocated for file */
-	unsigned long	st_blksize;	/* optimal blocksize for I/O */
-	unsigned long	st_flags;	/* user defined flags for file */
+struct stat
+{
+	dev_t            st_dev;      /* inode's device */
+	ino_t            st_ino;      /* inode's number */
+	mode_t           st_mode;     /* inode protection mode */
+	nlink_t          st_nlink;    /* number of hard links */
+	uid_t            st_uid;      /* user ID of the file's owner */
+	gid_t            st_gid;      /* group ID of the file's group */
+	dev_t            st_rdev;     /* device type */
+	struct timespec  st_atim;     /* time of last access */
+	struct timespec  st_mtim;     /* time of last data modification */
+	struct timespec  st_ctim;     /* time of last file status change */
+	off_t            st_size;     /* file size, in bytes */
+	long             st_blocks;   /* blocks allocated for file */
+	unsigned long    st_blksize;  /* optimal blocksize for I/O */
+	unsigned long    st_flags;    /* user defined flags for file */
 };
 
 #define st_atime  st_atim.tv_sec
