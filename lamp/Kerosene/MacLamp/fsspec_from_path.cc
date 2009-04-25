e5fae4e0c25f9c8cc1e0bbd459ca7ade2b8429f3
Joshua Juran <jjuran@metamage.com>
2009-04-24 22:05:01 -0700

    Pull FSSpec members directly out of struct stat, without making I/O calls.

--- lamp/Kerosene/MacLamp/fsspec_from_path.cc
+++ lamp/Kerosene/MacLamp/fsspec_from_path.cc
@@ -6,22 +6,11 @@
 // Standard C
 #include "errno.h"
 
-// Standard C++
-#include <string>
-
 // POSIX
 #include "sys/stat.h"
 
 // POSeven
 #include "POSeven/Errno.hh"
-#include "POSeven/Pathnames.hh"
-
-// Nitrogen
-#include "Nitrogen/Files.h"
-#include "Nitrogen/Str.h"
-
-// Kerosene
-#include "MacFilenameFromUnixFilename.hh"
 
 // MacLamp
 #include "fsspec_from_path.hh"
@@ -29,84 +18,50 @@
 
 //
 	
-	namespace N = Nitrogen;
 	namespace p7 = poseven;
-	namespace K = Kerosene;
 	
 	int fsspec_from_path( const char*  pathname,
 	                      FSSpec*      outFSS )
 	{
-		try
+		struct ::stat stat_buffer;
+		
+		int status = stat( pathname, &stat_buffer );
+		
+		if ( status == -1  &&  errno != ENOENT )
 		{
-			struct ::stat stat_buffer;
-			
-			int status = stat( pathname, &stat_buffer );
-			
-			if ( status == -1 )
-			{
-				if ( errno != ENOENT )
-				{
-					return status;
-				}
-				else
-				{
-					stat_buffer.st_mode = 0;  // not a dir
-				}
-			}
-			
-			std::string parent_pathname_string;
-			std::string basename_string;
-			
-			bool is_dir = S_ISDIR( stat_buffer.st_mode );
-			
-			if ( !is_dir )
-			{
-				parent_pathname_string = io::get_preceding_directory( pathname );
-				basename_string        = io::get_filename_string    ( pathname );
-				
-				basename_string = K::MacFilenameFromUnixFilename( basename_string );
-				
-				status = stat( parent_pathname_string.c_str(), &stat_buffer );
-				
-				if ( status == -1 )
-				{
-					return status;
-				}
-			}
-			
-			N::FSVolumeRefNum vRefNum = N::FSVolumeRefNum( -stat_buffer.st_dev );
-			N::FSDirID        dirID   = N::FSDirID       (  stat_buffer.st_ino );
-			
-			if ( vRefNum == 0 )
-			{
-				errno = EXDEV;
-				
-				return -1;
-			}
-			
-			N::Str255 filename = basename_string;
-			
-			FSSpec dummy_FSSpec;
-			
-			if ( outFSS == NULL )
-			{
-				outFSS = &dummy_FSSpec;
-			}
-			
-			*outFSS = N::FSMakeFSSpec( vRefNum, dirID, filename );
-			
-			return 0;
-		}
-		catch ( const std::bad_alloc& )
-		{
-			errno = ENOMEM;
-		}
-		catch ( ... )
-		{
-			errno = ENOENT;
+			return -1;
 		}
 		
-		return -1;
+		typedef UInt32 FSDirID;
+		
+		const FSVolumeRefNum vRefNum = -stat_buffer.st_dev;
+		const FSDirID        parID   = stat_buffer.st_rdev;
+		
+		const unsigned name_length = stat_buffer.st_name[0];
+		
+		if ( vRefNum == 0  ||  name_length == 0 )
+		{
+			errno = EXDEV;
+			
+			return -1;
+		}
+		
+		if ( name_length > 31 )
+		{
+			errno = ENAMETOOLONG;
+			
+			return -1;
+		}
+		
+		if ( outFSS != NULL )
+		{
+			outFSS->vRefNum = vRefNum;
+			outFSS->parID   = parID;
+			
+			std::memcpy( outFSS->name, stat_buffer.st_name, 1 + name_length );
+		}
+		
+		return 0;
 	}
 	
 	FSSpec make_fsspec_from_path( const char* pathname )
