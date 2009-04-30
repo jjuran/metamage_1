d870b8eb64f692f80443744006744f522c4a3789
Joshua Juran <jjuran@metamage.com>
2009-04-30 16:57:47 -0700

    Remove gratuitous indentation.

--- lamp/Kerosene/Library/exec.cc
+++ lamp/Kerosene/Library/exec.cc
@@ -19,100 +19,96 @@
 #include "unistd.h"
 
 
-//
-
-	static std::string LookupPath( const char* filename )
+static std::string LookupPath( const char* filename )
+{
+	const char* pathVar = getenv( "PATH" );
+	
+	if ( pathVar == NULL )
 	{
-		const char* pathVar = getenv( "PATH" );
-		
-		if ( pathVar == NULL )
-		{
-			return "";
-		}
-		
-		const char* pathEnd = pathVar + std::strlen( pathVar );
-		
-		while ( pathVar < pathEnd )
-		{
-			const char* separator = std::strchr( pathVar, ':' );
-			
-			if ( separator == NULL )
-			{
-				separator = pathEnd;
-			}
-			
-			// Watch out for empty path elements (e.g. "/bin:/sbin:" -- last is empty)
-			if ( separator != pathVar )
-			{
-				std::string dir( pathVar, separator - pathVar );
-				
-				std::string tryPath = dir + "/" + filename;
-				
-				struct ::stat sb;
-				int status = stat( tryPath.c_str(), &sb );
-				
-				if ( status == 0  &&  sb.st_mode & S_IXUSR )
-				{
-					return tryPath;
-				}
-			}
-			
-			// If we're at the end, then this sets pathVar > pathEnd
-			pathVar = separator + 1;
-		}
-		
 		return "";
 	}
 	
-	int execl( const char* path, const char* arg0, ... )
-	{
-		return execv( path, &arg0 );
-	}
+	const char* pathEnd = pathVar + std::strlen( pathVar );
 	
-	int execle( const char* path, const char* arg0, ... )
+	while ( pathVar < pathEnd )
 	{
-		va_list va;
+		const char* separator = std::strchr( pathVar, ':' );
 		
-		va_start( va, arg0 );
-		
-		while ( const char* arg = va_arg( va, const char * ) )
+		if ( separator == NULL )
 		{
+			separator = pathEnd;
 		}
 		
-		const char* const* envp = va_arg( va, const char* const* );
-		
-		va_end( va );
-		
-		const char* const* argv = &arg0;
-		
-		return execve( path, argv, envp );
-	}
-	
-	int execlp( const char* file, const char* arg0, ... )
-	{
-		return execvp( file, &arg0 );
-	}
-	
-	int execvp( const char* file, const char* const argv[] )
-	{
-		std::string path;
-		
-		if ( std::strchr( file, '/' ) == NULL )
+		// Watch out for empty path elements (e.g. "/bin:/sbin:" -- last is empty)
+		if ( separator != pathVar )
 		{
-			path = LookupPath( file );
+			std::string dir( pathVar, separator - pathVar );
 			
-			if ( path == "" )
+			std::string tryPath = dir + "/" + filename;
+			
+			struct ::stat sb;
+			int status = stat( tryPath.c_str(), &sb );
+			
+			if ( status == 0  &&  sb.st_mode & S_IXUSR )
 			{
-				errno = ENOENT;
-				
-				return -1;
+				return tryPath;
 			}
-			
-			file = path.c_str();
 		}
 		
-		return execv( file, argv );
+		// If we're at the end, then this sets pathVar > pathEnd
+		pathVar = separator + 1;
 	}
 	
-//
+	return "";
+}
+
+int execl( const char* path, const char* arg0, ... )
+{
+	return execv( path, &arg0 );
+}
+
+int execle( const char* path, const char* arg0, ... )
+{
+	va_list va;
+	
+	va_start( va, arg0 );
+	
+	while ( const char* arg = va_arg( va, const char * ) )
+	{
+	}
+	
+	const char* const* envp = va_arg( va, const char* const* );
+	
+	va_end( va );
+	
+	const char* const* argv = &arg0;
+	
+	return execve( path, argv, envp );
+}
+
+int execlp( const char* file, const char* arg0, ... )
+{
+	return execvp( file, &arg0 );
+}
+
+int execvp( const char* file, const char* const argv[] )
+{
+	std::string path;
+	
+	if ( std::strchr( file, '/' ) == NULL )
+	{
+		path = LookupPath( file );
+		
+		if ( path == "" )
+		{
+			errno = ENOENT;
+			
+			return -1;
+		}
+		
+		file = path.c_str();
+	}
+	
+	return execv( file, argv );
+}
 
