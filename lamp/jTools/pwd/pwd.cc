06501fbb08d8cddead7b700c9f948a18387eb572
Joshua Juran <jjuran@metamage.com>
2009-05-04 15:58:10 -0700

    Apply getcwd_k() semantics update.
    On buffer overrun, getcwd_k() returns the bitwise inverse of the path length.

--- lamp/jTools/pwd/pwd.cc
+++ lamp/jTools/pwd/pwd.cc
@@ -14,19 +14,26 @@ int main( int argc, char const *const argv[] )
 {
 	std::string path;
 	
-	ssize_t size = 256;  // reasonable default
+	ssize_t got = ~256;  // reasonable default
 	
 	// Even though we know the size after one iteration, it's theoretically possible
 	// that the cwd has moved and now has a longer pathname.
 	do
 	{
+		const size_t size = ~got;
+		
 		path.resize( size );
 		
-		size = getcwd_k( &path[ 0 ], path.size() );
+		got = getcwd_k( &path[ 0 ], path.size() );
 	}
-	while ( size > path.size() );
+	while ( got < -1 );
 	
-	path.resize( size );
+	if ( got == -1 )
+	{
+		return EXIT_FAILURE;
+	}
+	
+	path.resize( got );
 	
 	path += "\n";
 	
