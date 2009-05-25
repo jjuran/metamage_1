5ee325641d29665fcf177e27a8f765c752cca307
Joshua Juran <jjuran@metamage.com>
2009-05-25 04:48:05 +0000

    Define a dummy() function so ar doesn't complain that fcntl.o is empty.

--- nitric/POSeven/functions/fcntl.cc
+++ nitric/POSeven/functions/fcntl.cc
@@ -5,7 +5,7 @@
 
 // Part of the Nitrogen project.
 //
-// Written 2009 by Joshua Juran.
+// Written 2006-2007 by Joshua Juran.
 //
 // This code was written entirely by the above contributor, who places it
 // in the public domain.
@@ -14,8 +14,12 @@
 #include "POSeven/functions/fcntl.hh"
 
 
-namespace poseven
+namespace
 {
+	
+	void dummy()
+	{
+	}
 	
 }
 
