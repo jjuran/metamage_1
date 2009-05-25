9e6c6b930d7f1ff7f985933e328dab3645b1fcc0
Joshua Juran <jjuran@metamage.com>
2009-05-25 02:00:46 -0700

    Add /sys/app/window/front.
    /sys/app/window/front is a symlink to the currently front window's directory,
    unless there are no windows, in which case it doesn't exist.

--- lamp/Genie/Genie/FS/sys_app_window.cc
+++ lamp/Genie/Genie/FS/sys_app_window.cc
@@ -5,15 +5,66 @@
 
 #include "Genie/FS/sys_app_window.hh"
 
+// Mac OS
+#include <MacWindows.h>
+
+// POSeven
+#include "POSeven/Errno.hh"
+
 // Genie
 #include "Genie/FS/FSTree_sys_mac_window.hh"
+#include "Genie/FS/ResolvePathname.hh"
 
 
 namespace Genie
 {
 	
+	namespace p7 = poseven;
+	
+	
+	class sys_app_window_front : public FSTree
+	{
+		public:
+			sys_app_window_front( const FSTreePtr&    parent,
+			                      const std::string&  name ) : FSTree( parent, name )
+			{
+			}
+			
+			bool Exists() const  { return ::FrontWindow() != NULL; }
+			
+			bool IsLink() const  { return Exists(); }
+			
+			std::string ReadLink() const;
+			
+			FSTreePtr ResolveLink() const;
+	};
+	
+	std::string sys_app_window_front::ReadLink() const
+	{
+		const WindowRef windowPtr = ::FrontWindow();
+		
+		if ( windowPtr == NULL )
+		{
+			p7::throw_errno( ENOENT );
+		}
+		
+		std::string result = "list/";
+		
+		result += Pointer_KeyName_Traits< ::WindowRef >::NameFromKey( windowPtr );
+		
+		return result;
+	}
+	
+	FSTreePtr sys_app_window_front::ResolveLink() const
+	{
+		return ResolveAbsolutePath( "/sys/app/window/" + ReadLink() );
+	}
+	
+	
 	const FSTree_Premapped::Mapping sys_app_window_Mappings[] =
 	{
+		{ "front", &Basic_Factory< sys_app_window_front >, true },
+		
 		{ "list", &Basic_Factory< FSTree_sys_mac_window > },
 		
 		{ NULL, NULL }
