63a1c6a646e7e49c645545ad3f6d9c833ab2033e
Joshua Juran <jjuran@metamage.com>
2009-05-30 22:59:36 -0700

    TESyncScrap:  Patch the system to sync the TextEdit scrap on edit.
    Patch TECut() and TECopy() to flush the TE scrap immediately.
    Patch TEPaste() to update the TE scrap first.

---
+++ pedestal/Hacks/TESyncScrap/TESyncScrap.cc
@@ -0,0 +1,96 @@
+/*
+	
+	TESyncScrap
+	
+	Joshua Juran
+	
+*/
+
+// Mac OS
+#include <Resources.h>
+#include <Scrap.h>
+#include <TextEdit.h>
+
+// Silver
+#include "Silver/Install.hh"
+#include "Silver/Patch.hh"
+#include "Silver/Procs.hh"
+#include "Silver/Traps.hh"
+
+
+#pragma exceptions off
+
+
+namespace Ag = Silver;
+
+
+namespace
+{
+	
+	static OSErr FlushScrap()
+	{
+		OSErr err = ZeroScrap();
+		
+		if ( err == noErr )
+		{
+			err = TEToScrap();
+		}
+		
+		return err;
+	}
+	
+	void TECut_Patch( TEHandle hTE, Ag::TECutProcPtr nextHandler )
+	{
+		nextHandler( hTE );
+		
+		FlushScrap();
+	}
+	
+	void TECopy_Patch( TEHandle hTE, Ag::TECopyProcPtr nextHandler )
+	{
+		nextHandler( hTE );
+		
+		FlushScrap();
+	}
+	
+	void TEPaste_Patch( TEHandle hTE, Ag::TEPasteProcPtr nextHandler )
+	{
+		OSErr err = TEFromScrap();
+		
+		nextHandler( hTE );
+	}
+	
+	
+	void TEInit_Patch( Ag::TEInitProcPtr nextHandler )
+	{
+		const Handle hack = ::Get1NamedResource( 'Hack', "\p" "TESyncScrap" );
+		
+		if ( hack != NULL )
+		{
+			::ReleaseResource( hack );
+		}
+		else
+		{
+			Ag::TrapPatch< _TECopy,  TECopy_Patch  >::Install();
+			Ag::TrapPatch< _TECut,   TECut_Patch   >::Install();
+			Ag::TrapPatch< _TEPaste, TEPaste_Patch >::Install();
+		}
+		
+		nextHandler();
+	}
+	
+}
+
+
+static OSErr Installer()
+{
+	Ag::TrapPatch< _TEInit, TEInit_Patch >::Install();
+	
+	return noErr;
+}
+
+int main()
+{
+	return Ag::Install( Installer );
+}
+
