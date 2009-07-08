66e3be7fb7d61dfeab764e24c856075571b7484c
Joshua Juran <jjuran@metamage.com>
2009-07-08 02:17:23 -0700

    Add p7::pwrite().

---
+++ nitric/POSeven/functions/pwrite.hh
@@ -0,0 +1,40 @@
+// pwrite.hh
+// ---------
+
+// Written 2009 by Joshua Juran.
+//
+// This code was written entirely by the above contributor, who places it
+// in the public domain.
+
+
+#ifndef POSEVEN_FUNCTIONS_PWRITE_HH
+#define POSEVEN_FUNCTIONS_PWRITE_HH
+
+// Standard C++
+#include <string>
+
+// POSIX
+#include <unistd.h>
+
+// POSeven
+#include "POSeven/Errno.hh"
+#include "POSeven/types/fd_t.hh"
+
+
+namespace poseven
+{
+	
+	inline ssize_t pwrite( fd_t fd, const char* buffer, size_t n_bytes, off_t offset )
+	{
+		return throw_posix_result( ::pwrite( fd, buffer, n_bytes, offset ) );
+	}
+	
+	inline ssize_t pwrite( fd_t fd, const std::string& string, off_t offset )
+	{
+		return pwrite( fd, string.data(), string.length(), offset );
+	}
+	
+}
+
+#endif
+
