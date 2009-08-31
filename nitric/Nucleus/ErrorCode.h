ff887180b1f432b1d94486d6cc281bb04c37c67b
Joshua Juran <jjuran@metamage.com>
2009-08-31 12:14:41 -0700

    Nucleus/ErrorCode.h:  Define NUCLEUS_RICH_ERRORCODES to 1 if undefined.
    Later we'll default to 0 for Metrowerks C++, after we make other changes.

--- nitric/Nucleus/ErrorCode.h
+++ nitric/Nucleus/ErrorCode.h
@@ -5,7 +5,7 @@
 
 // Part of the Nitrogen project.
 //
-// Written 2002-2007 by Lisa Lippincott and Joshua Juran.
+// Written 2002-2009 by Lisa Lippincott and Joshua Juran.
 //
 // This code was written entirely by the above contributors, who place it
 // in the public domain.
@@ -102,6 +102,11 @@
 #endif
 
 #include <map>
+
+
+#ifndef NUCLEUS_RICH_ERRORCODES
+    #define NUCLEUS_RICH_ERRORCODES 1
+#endif
 
 
 namespace Nucleus
