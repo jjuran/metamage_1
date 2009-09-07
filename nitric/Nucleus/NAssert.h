204788b4a5a6f8dbf5c26ff10a7f152c4afc24d8
Joshua Juran <jjuran@metamage.com>
2009-09-07 01:49:07 -0700

    Nucleus:  Replace NAssert's ASSERT() macro with debug/assert's.

--- nitric/Nucleus/NAssert.h
+++ nitric/Nucleus/NAssert.h
@@ -4,11 +4,6 @@
 // Maintained by Joshua Juran
 
 // Part of the Nitrogen project.
-//
-// Written 2005-2007 by Joshua Juran.
-//
-// This code was written entirely by the above contributor, who places it
-// in the public domain.
 
 
 #ifndef NUCLEUS_NASSERT_H
@@ -18,24 +13,9 @@
 #include "Nucleus/Debugging.h"
 #endif
 
+// Debug
+#include "debug/assert.hh"
 
-#if TARGET_CONFIG_DEBUGGING
-	
-	#define ASSERT( cond )  if ( !(cond) ) Nucleus::ReportAssertionFailureAndAbort( #cond,  __FILE__, __LINE__ ); else 0
-	
-#else
-	
-	#define ASSERT( cond )  if ( 0 ) (void) !(cond); else 0
-	
-#endif
-
-
-namespace Nucleus
-{
-	
-	void ReportAssertionFailureAndAbort( const char* text, const char* file, int line );
-	
-}
 
 #endif
 
