0f1419c59154d1f6bd072ca43ea8719396f5e771
Joshua Juran <jjuran@metamage.com>
2009-09-09 03:51:05 -0700

    Don't use Orion's main() since we're not throwing exceptions.

--- lamp/jTools/touch/touch.cc
+++ lamp/jTools/touch/touch.cc
@@ -14,54 +14,49 @@
 #include <unistd.h>
 #include <utime.h>
 
-// Orion
-#include "Orion/Main.hh"
+
+#pragma exceptions off
 
 
-namespace tool
+int main( int argc, char *const *argv )
 {
-	
-	int Main( int argc, iota::argv_t argv )
+	// Check for sufficient number of args
+	if ( argc < 2 )
 	{
-		// Check for sufficient number of args
-		if ( argc < 2 )
-		{
-			std::fprintf( stderr, "touch: missing arguments\n" );
-			
-			return 1;
-		}
+		std::fprintf( stderr, "touch: missing arguments\n" );
 		
-		// Try to touch each item.  Return whether any errors occurred.
-		int exit_status = EXIT_SUCCESS;
-		
-		for ( std::size_t index = 1;  index < argc;  ++index )
-		{
-			const char* pathname = argv[ index ];
-			
-			int result = utime( pathname, NULL );
-			
-			if ( result < 0 )
-			{
-				if ( errno == ENOENT )
-				{
-					result = open( pathname, O_WRONLY | O_CREAT, 0666 );
-					
-					if ( result >= 0 )
-					{
-						close( result );
-						
-						continue;
-					}
-				}
-				
-				std::fprintf( stderr, "touch: %s: %s\n", pathname, std::strerror( errno ) );
-				
-				exit_status = EXIT_FAILURE;
-			}
-		}
-		
-		return exit_status;
+		return 1;
 	}
 	
+	// Try to touch each item.  Return whether any errors occurred.
+	int exit_status = EXIT_SUCCESS;
+	
+	for ( std::size_t index = 1;  index < argc;  ++index )
+	{
+		const char* pathname = argv[ index ];
+		
+		int result = utime( pathname, NULL );
+		
+		if ( result < 0 )
+		{
+			if ( errno == ENOENT )
+			{
+				result = open( pathname, O_WRONLY | O_CREAT, 0666 );
+				
+				if ( result >= 0 )
+				{
+					close( result );
+					
+					continue;
+				}
+			}
+			
+			std::fprintf( stderr, "touch: %s: %s\n", pathname, std::strerror( errno ) );
+			
+			exit_status = EXIT_FAILURE;
+		}
+	}
+	
+	return exit_status;
 }
 
