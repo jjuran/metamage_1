470cc350cd190f61e6d39cc7867e1e406e5567e2
Joshua Juran <jjuran@metamage.com>
2009-09-10 20:40:08 -0700

    iota:  Delete unused code, CALL() and iota::equal_strings().
    CALL()  prints an error and aborts if its argument is a system call failure.
    iota::equal_strings() is a simple wrapper around strcmp().  Neither was used.

--- pedestal/Practica/iota/strings.hh
+++ pedestal/Practica/iota/strings.hh
@@ -6,9 +6,6 @@
 #ifndef IOTA_STRINGS_HH
 #define IOTA_STRINGS_HH
 
-// Standard C/C++
-#include <cstring>
-
 // Expands to a value equal to strlen( str ), but for string constants only,
 // and in constant (zero) time instead of linear time.
 #define STRLEN( str )  (sizeof "" str - 1)
@@ -16,16 +13,6 @@
 // Expands to two comma-separated values -- the string constant and its length.
 #define STR_LEN( str )  str, STRLEN( str )
 
-
-namespace iota
-{
-	
-	inline bool equal_strings( const char* a, const char* b )
-	{
-		return std::strcmp( a, b ) == 0;
-	}
-	
-}
 
 #endif
 
