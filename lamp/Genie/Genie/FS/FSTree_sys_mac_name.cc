708f2ceb965b3d76d3590f62c8eb5c6cca1db84d
Joshua Juran <jjuran@metamage.com>
2009-09-26 04:39:51 -0700

    Rename GetMachineName() to GetWorkstationName() and move to its own unit.

--- lamp/Genie/Genie/FS/FSTree_sys_mac_name.cc
+++ lamp/Genie/Genie/FS/FSTree_sys_mac_name.cc
@@ -5,44 +5,16 @@
 
 #include "Genie/FS/FSTree_sys_mac_name.hh"
 
-// MacFeatures
-#include "MacFeatures/Features.hh"
-
 // Genie
-#include "Genie/Utilities/CFStringGetStdString.hh"
-#include "Genie/Utilities/GetStringResource.hh"
+#include "Genie/Utilities/GetWorkstationName.hh"
 
 
 namespace Genie
 {
 	
-	namespace N = Nitrogen;
-	
-	
-	using MacFeatures::Is_Running_OSXNative;
-	
-	
-	static std::string GetMachineName()
-	{
-		if ( !Is_Running_OSXNative() )
-		{
-			return GetStringResource( -16413 );
-		}
-		
-		if ( CSCopyMachineName != (void*) kUnresolvedCFragSymbolAddress )
-		{
-			if ( CFStringRef userName = CSCopyMachineName() )
-			{
-				return CFStringGetStdString( userName );
-			}
-		}
-		
-		return "";
-	}
-	
 	std::string sys_mac_name::Read( const FSTree* that, bool binary )
 	{
-		return GetMachineName();
+		return GetWorkstationName();
 	}
 	
 }
