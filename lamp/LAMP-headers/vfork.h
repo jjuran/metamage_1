493778669c029a7bed6f2e6cbef8a8f3806c5324
Joshua Juran <jjuran@metamage.com>
2009-09-27 06:55:00 -0700

    Lamp:  Use the inline function definition of vfork() for both C and C++.
    Replace C++ construction syntax with C casts.
    Initialize temp_jmp_buf at its definition.
    Remove the macro definition and preprocessor conditional.
    
    Metrowerks C handles inline functions just fine, so there's no need for dual
    definitions.

--- lamp/LAMP-headers/vfork.h
+++ lamp/LAMP-headers/vfork.h
@@ -17,31 +17,15 @@
 extern "C" {
 #endif
 	
-	static jmp_buf* _kerosene_temp_jmp_buf;
-	
 	int _vfork_start( jmp_buf* buffer );
 	
-	#ifdef __cplusplus
+	inline pid_t vfork()
+	{
+		jmp_buf *const temp_jmp_buf = (jmp_buf*) alloca( sizeof (jmp_buf) );
 		
-		inline pid_t vfork()
-		{
-			jmp_buf* temp_jmp_buf;
-			
-			return _vfork_start( temp_jmp_buf
-			                     = (jmp_buf*) alloca( sizeof (jmp_buf) ) )
-			       ? pid_t( -1 )
-			       : pid_t( setjmp( *temp_jmp_buf ) );
-		}
-		
-	#else
-		
-		#define vfork()                                                   \
-		        (_vfork_start( _kerosene_temp_jmp_buf                      \
-		                       = (jmp_buf*) alloca( sizeof (jmp_buf) ) )  \
-		         ? (pid_t) -1                                             \
-		         : (pid_t) setjmp( *_kerosene_temp_jmp_buf ) )
-		
-	#endif
+		return (pid_t) _vfork_start( temp_jmp_buf ) ? -1
+		                                            : setjmp( *temp_jmp_buf );
+	}
 	
 #ifdef __cplusplus
 }
