094b6101e5a16566da75091adb14ffc3782cb3e7
Joshua Juran <jjuran@metamage.com>
2009-10-30 05:05:10 -0700

    Call more efficient encoded_hex_char() in inscribe_n_hex_digits().

--- base/iota/iota/hexidecimal.cc
+++ base/iota/iota/hexidecimal.cc
@@ -67,35 +67,35 @@ namespace iota
 				// fall through
 			
 			case 8:
-				*p++ = printable_hex_digit( x >> 28 );
+				*p++ = encoded_hex_char( x >> 28 );
 				// fall through
 			
 			case 7:
-				*p++ = printable_hex_digit( x >> 24 );
+				*p++ = encoded_hex_char( x >> 24 );
 				// fall through
 			
 			case 6:
-				*p++ = printable_hex_digit( x >> 20 );
+				*p++ = encoded_hex_char( x >> 20 );
 				// fall through
 			
 			case 5:
-				*p++ = printable_hex_digit( x >> 16 );
+				*p++ = encoded_hex_char( x >> 16 );
 				// fall through
 			
 			case 4:
-				*p++ = printable_hex_digit( x >> 12 );
+				*p++ = encoded_hex_char( x >> 12 );
 				// fall through
 			
 			case 3:
-				*p++ = printable_hex_digit( x >>  8 );
+				*p++ = encoded_hex_char( x >>  8 );
 				// fall through
 			
 			case 2:
-				*p++ = printable_hex_digit( x >>  4 );
+				*p++ = encoded_hex_char( x >>  4 );
 				// fall through
 			
 			case 1:
-				*p   = printable_hex_digit( x       );
+				*p   = encoded_hex_char( x       );
 				// fall through
 			
 			case 0:
