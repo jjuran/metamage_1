9f06cdceae20d346220c325c61748ab75e78e4cf
Joshua Juran <jjuran@metamage.com>
2009-11-02 12:04:44 -0800

    Read /etc/traps (if available) for a mapping of A-trap words to names.
    We store a 2-byte offset for each of 4096 possible A-traps, for a total array
    size of 8K.  We allocate another 12K dynamically to store the names.

---
+++ lamp/jTools/d68k/traps.hh
@@ -0,0 +1,20 @@
+/*
+	traps.hh
+	--------
+	
+	Copyright 2009, Joshua Juran
+*/
+
+#ifndef D68K_TRAPS_HH
+#define D68K_TRAPS_HH
+
+
+namespace tool
+{
+	
+	const char* get_trap_name( unsigned short trap_word );
+	
+}
+
+#endif
+
