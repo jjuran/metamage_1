a8bb1569835b40918d58f960aca07e1783fdc825
Joshua Juran <jjuran@metamage.com>
2009-11-04 18:02:54 -0800

    Convergence:  Delegate copyfile.hh to lamp/copyfile.h in compat.
    Having copyfile.hh at top directory level prevents poseven/extras/copyfile.hh
    from including it in gcc -- the latter winds up including itself instead.  The
    simplest approach here is to rename the include.

--- lamp/Convergence/copyfile.hh
+++ lamp/Convergence/copyfile.hh
@@ -6,15 +6,7 @@
 #ifndef COPYFILE_HH
 #define COPYFILE_HH
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-	
-	int copyfile( const char* src, const char* dest );
-	
-#ifdef __cplusplus
-}
-#endif
+#include "compat/copyfile.h"
 
 #endif
 
