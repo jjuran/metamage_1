384a42b0c15ecff7bb0898cef27d779a183b50b8
Joshua Juran <jjuran@metamage.com>
2009-12-09 06:16:40 -0800

    Add append_hex_encoded_byte unit.  Initial check-in.

---
+++ lamp/Genie/Genie/FS/append_hex_encoded_byte.hh
@@ -0,0 +1,23 @@
+/*
+	append_hex_encoded_byte.hh
+	--------------------------
+	
+	Copyright 2009, Joshua Juran
+*/
+
+#ifndef GENIE_FS_APPENDHEXENCODEDBYTE_HH
+#define GENIE_FS_APPENDHEXENCODEDBYTE_HH
+
+// Standard C++
+#include <string>
+
+
+namespace Genie
+{
+	
+	void append_hex_encoded_byte( std::string& s, unsigned char c );
+	
+}
+
+#endif
+
