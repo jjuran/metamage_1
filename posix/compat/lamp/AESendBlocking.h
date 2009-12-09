9262953be410dcdb3cb16de2fbf54594f9a2d895
Joshua Juran <jjuran@metamage.com>
2009-12-09 02:32:56 -0800

    Move AESendBlocking() and OTInetMailExchange_k() prototypes to compat/lamp.
    This is the end of Kerosene/Common.

---
+++ posix/compat/lamp/AESendBlocking.h
@@ -0,0 +1,22 @@
+/*
+	AESendBlocking.h
+	----------------
+	
+	Joshua Juran
+*/
+
+#ifndef LAMP_AESENDBLOCKING_H
+#define LAMP_AESENDBLOCKING_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+long AESendBlocking( const struct AppleEvent *appleEvent, struct AppleEvent *reply );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ posix/compat/lamp/OTInetMailExchange_k.h
@@ -0,0 +1,22 @@
+/*
+	OTInetMailExchange_k.h
+	----------------------
+	
+	Joshua Juran
+*/
+
+#ifndef LAMP_OTINETMAILEXCHANGEK_H
+#define LAMP_OTINETMAILEXCHANGEK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+long OTInetMailExchange_k( char *domain, unsigned short *count, struct InetMailExchange *result );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
