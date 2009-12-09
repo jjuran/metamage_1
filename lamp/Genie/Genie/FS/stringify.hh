6c4a64f9aa7ec2564e0767d52d0d1461564c9ee2
Joshua Juran <jjuran@metamage.com>
2009-12-09 05:57:56 -0800

    Add 'stringify' unit.  Initial check-in.

---
+++ lamp/Genie/Genie/FS/stringify.hh
@@ -0,0 +1,67 @@
+/*
+	stringify.hh
+	------------
+	
+	Copyright 2009, Joshua Juran
+*/
+
+#ifndef GENIE_FS_STRINGIFY_HH
+#define GENIE_FS_STRINGIFY_HH
+
+// Standard C++
+#include <string>
+
+
+namespace Genie
+{
+	
+	struct stringify_short
+	{
+		static std::string apply( short x, bool binary );
+	};
+	
+	struct stringify_int
+	{
+		static std::string apply( int x, bool binary );
+	};
+	
+	struct stringify_unsigned
+	{
+		static std::string apply( unsigned x, bool binary );
+	};
+	
+	struct stringify_16_bit_hex
+	{
+		static std::string apply( unsigned short x, bool binary );
+	};
+	
+	struct stringify_32_bit_hex
+	{
+		static std::string apply( unsigned x, bool binary );
+	};
+	
+	struct stringify_pointer
+	{
+		static std::string apply( const void* x, bool binary )
+		{
+			return stringify_32_bit_hex::apply( (unsigned) x, binary );
+		}
+	};
+	
+	struct stringify_string
+	{
+		static std::string apply( const char* s, bool binary )
+		{
+			return s;
+		}
+	};
+	
+	struct stringify_pascal_string
+	{
+		static std::string apply( const unsigned char* s, bool binary );
+	};
+	
+}
+
+#endif
+
