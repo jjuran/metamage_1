27eb45212ff30579365396c32fe1e3561257f934
Joshua Juran <jjuran@metamage.com>
2009-12-15 10:06:13 -0800

    Add /sys/mac/gdev/main as a symlink to the main screen.

--- lamp/Genie/Genie/FS/FSTree_sys_mac_gdev.cc
+++ lamp/Genie/Genie/FS/FSTree_sys_mac_gdev.cc
@@ -5,15 +5,65 @@
 
 #include "Genie/FS/FSTree_sys_mac_gdev.hh"
 
+// Mac OS
+#ifndef __QUICKDRAW__
+#include <Quickdraw.h>
+#endif
+
+// iota
+#include "iota/hexidecimal.hh"
+#include "iota/strings.hh"
+
 // Genie
+#include "Genie/FS/ResolvePathname.hh"
 #include "Genie/FS/sys_mac_gdev_list.hh"
 
 
 namespace Genie
 {
 	
+	class sys_mac_gdev_main : public FSTree
+	{
+		public:
+			sys_mac_gdev_main( const FSTreePtr&    parent,
+			                   const std::string&  name ) : FSTree( parent, name )
+			{
+			}
+			
+			bool Exists() const  { return true; }
+			
+			bool IsLink() const  { return true; }
+			
+			std::string ReadLink() const;
+			
+			FSTreePtr ResolveLink() const;
+	};
+	
+	std::string sys_mac_gdev_main::ReadLink() const
+	{
+		const GDHandle gdH = ::GetMainDevice();
+		
+		ASSERT( gdH != NULL );
+		
+		std::string result = "list/12345678";
+		
+		const size_t hex_offset = STRLEN( "list/" );
+		
+		iota::encode_32_bit_hex( (unsigned) gdH, &result[ hex_offset ] );
+		
+		return result;
+	}
+	
+	FSTreePtr sys_mac_gdev_main::ResolveLink() const
+	{
+		return ResolveAbsolutePath( "/sys/mac/gdev/" + ReadLink() );
+	}
+	
+	
 	const FSTree_Premapped::Mapping sys_mac_gdev_Mappings[] =
 	{
+		{ "main", &Basic_Factory< sys_mac_gdev_main > },
+		
 		{ "list", &New_FSTree_sys_mac_gdev_list },
 		
 		{ NULL, NULL }
