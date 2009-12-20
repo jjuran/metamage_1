313d0fc2955041b956c143c70cec523200923bdf
Joshua Juran <jjuran@metamage.com>
2009-12-19 19:11:45 -0800

    Genie:  Add WindowList_contains().

---
+++ lamp/Genie/Genie/Utilities/WindowList_contains.cc
@@ -0,0 +1,29 @@
+/*
+	WindowList_contains.cc
+	----------------------
+	
+	Copyright 2009, Joshua Juran
+*/
+
+#include "Genie/Utilities/WindowList_contains.hh"
+
+// plus
+#include "plus/contains.hh"
+
+// Nitrogen
+#include "Nitrogen/MacWindows.h"
+
+
+namespace Genie
+{
+	
+	namespace N = Nitrogen;
+	
+	
+	bool WindowList_contains( WindowRef window )
+	{
+		return window != NULL  &&  plus::contains( N::WindowList(), window );
+	}
+	
+}
+
