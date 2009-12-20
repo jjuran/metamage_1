03d5691e6c39444b712a3e33f4c59f3239709cc4
Joshua Juran <jjuran@metamage.com>
2009-12-19 21:07:09 -0800

    Move FSTree_sys_window out of the header.

--- lamp/Genie/Genie/FS/FSTree_sys_window.hh
+++ lamp/Genie/Genie/FS/FSTree_sys_window.hh
@@ -6,28 +6,12 @@
 #ifndef GENIE_FILESYSTEM_FSTREESYSWINDOW_HH
 #define GENIE_FILESYSTEM_FSTREESYSWINDOW_HH
 
-// Standard C++
-#include <map>
-
 // Genie
-#include "Genie/FS/FSTree_Directory.hh"
+#include "Genie/FS/FSTree.hh"
 
 
 namespace Genie
 {
-	
-	class FSTree_sys_window : public FSTree_Directory
-	{
-		public:
-			FSTree_sys_window( const FSTreePtr&    parent,
-			                   const std::string&  name ) : FSTree_Directory( parent, name )
-			{
-			}
-			
-			FSTreePtr Lookup_Child( const std::string& name ) const;
-			
-			void IterateIntoCache( FSTreeCache& cache ) const;
-	};
 	
 	FSTreePtr NewWindow();
 	
