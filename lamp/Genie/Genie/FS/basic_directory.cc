0f46047a3cdb511dfda448c41f5f0bfa523e1869
Joshua Juran <jjuran@metamage.com>
2009-12-20 19:18:17 -0800

    Genie:  Add basic_directory.  Initial check-in.

---
+++ lamp/Genie/Genie/FS/basic_directory.cc
@@ -0,0 +1,58 @@
+/*
+	basic_directory.cc
+	------------------
+	
+	Copyright 2009, Joshua Juran
+*/
+
+#include "Genie/FS/basic_directory.hh"
+
+
+namespace Genie
+{
+	
+	class basic_directory : public FSTree_Directory
+	{
+		private:
+			Lookup_Proc   itsLookup;
+			Iterate_Proc  itsIterate;
+		
+		public:
+			basic_directory( const FSTreePtr&    parent,
+			                 const std::string&  name,
+			                 Lookup_Proc         lookup,
+			                 Iterate_Proc        iterate )
+			:
+				FSTree_Directory( parent, name ),
+				itsLookup ( lookup  ),
+				itsIterate( iterate )
+			{
+			}
+			
+			FSTreePtr Lookup_Child( const std::string& name ) const;
+			
+			void IterateIntoCache( FSTreeCache& cache ) const;
+	};
+	
+	
+	FSTreePtr basic_directory::Lookup_Child( const std::string& name ) const
+	{
+		return itsLookup( Self(), name );
+	}
+	
+	void basic_directory::IterateIntoCache( FSTreeCache& cache ) const
+	{
+		itsIterate( cache );
+	}
+	
+	
+	FSTreePtr new_basic_directory( const FSTreePtr&    parent,
+	                               const std::string&  name,
+	                               Lookup_Proc         lookup,
+	                               Iterate_Proc        iterate )
+	{
+		return FSTreePtr( new basic_directory( parent, name, lookup, iterate ) );
+	}
+	
+}
+
---
+++ lamp/Genie/Genie/FS/basic_directory.hh
@@ -0,0 +1,30 @@
+/*
+	basic_directory.hh
+	------------------
+	
+	Copyright 2009, Joshua Juran
+*/
+
+#ifndef GENIE_FS_BASICDIRECTORY_HH
+#define GENIE_FS_BASICDIRECTORY_HH
+
+// Genie
+#include "Genie/FS/FSTree_Directory.hh"
+
+
+namespace Genie
+{
+	
+	typedef FSTreePtr (*Lookup_Proc )( const FSTreePtr& parent, const std::string& name );
+	typedef void      (*Iterate_Proc)( FSTreeCache& cache );
+	
+	
+	FSTreePtr new_basic_directory( const FSTreePtr&    parent,
+	                               const std::string&  name,
+	                               Lookup_Proc         lookup,
+	                               Iterate_Proc        iterate );
+	
+}
+
+#endif
+
