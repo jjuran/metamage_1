f5722d2c094cb00ebc6ffd49087f9ef07263a141
Joshua Juran <jjuran@metamage.com>
2009-12-28 06:48:35 -0800

    keymods:  Rewrite to avoid using stdio and thus be smaller.

--- lamp/jTools/keymods/keymods.cc
+++ lamp/jTools/keymods/keymods.cc
@@ -3,13 +3,16 @@
  *	==========
  */
 
-// Universal Interfaces
+// Mac OS
 #ifndef __EVENTS__
 #include <Events.h>
 #endif
 
-// Standard C/C++
-#include <cstdio>
+// POSIX
+#include <unistd.h>
+
+// iota
+#include "iota/strings.hh"
 
 
 enum
@@ -17,43 +20,51 @@ enum
 	kKeyModifierBits = 32
 };
 
-const char* keyModifierNames[ kKeyModifierBits ] =
+struct string_constant
 {
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
+	const char*  data;
+	unsigned     length;
+};
+
+#define _( s )  { STR_LEN( s "\n" ) }
+
+const string_constant keyModifierNames[ kKeyModifierBits ] =
+{
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
 	
-	"command",
-	"shift",
-	"alpha",
-	"option",
-	"control",
-	"right-shift",
-	"right-option",
-	"right-control",
+	_( "command" ),
+	_( "shift"   ),
+	_( "alpha"   ),
+	_( "option"  ),
+	_( "control" ),
+	_( "right-shift"   ),
+	_( "right-option"  ),
+	_( "right-control" ),
 	
-	"",
-	"function",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
+	_( "" ),
+	_( "function" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
 	
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	"",
-	""
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" ),
+	_( "" )
 };
 
 int main( int argc, char const *const argv[] )
@@ -62,11 +73,11 @@ int main( int argc, char const *const argv[] )
 	
 	for ( int i = 0;  i < kKeyModifierBits;  ++i )
 	{
-		const char* name = keyModifierNames[i];
+		const string_constant& name = keyModifierNames[i];
 		
 		if ( keymods & (1 << i) )
 		{
-			std::printf( "%s\n", name );
+			::write( STDOUT_FILENO, name.data, name.length );
 		}
 	}
 	
