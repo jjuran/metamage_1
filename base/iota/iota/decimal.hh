12fcf1fa37100250a2ed6352980feee745407c1e
Joshua Juran <jjuran@metamage.com>
2010-01-31 03:06:00 -0800

    iota:  Add parse_unsigned_decimal() and parse_decimal().
    In MSL at least, atoi() is implemented in terms of strtol(), which is overkill
    since we're only interested in the decimal case and don't need an algorithm that
    works for any radix.
    
    The new functions offer the simplicity of atoi() with the iteration semantics of
    strtol() (as well as the distinction between signed and unsigned values).

--- base/iota/iota/decimal.hh
+++ base/iota/iota/decimal.hh
@@ -10,6 +10,21 @@
 namespace iota
 {
 	
+	unsigned parse_unsigned_decimal( const char **pp );
+	
+	int parse_decimal( const char **pp );
+	
+	inline unsigned parse_unsigned_decimal( const char *p )
+	{
+		return parse_unsigned_decimal( &p );
+	}
+	
+	inline int parse_decimal( const char *p )
+	{
+		return parse_decimal( &p );
+	}
+	
+	
 	inline unsigned pure_decimal_magnitude( unsigned x )
 	{
 		unsigned result = 0;
