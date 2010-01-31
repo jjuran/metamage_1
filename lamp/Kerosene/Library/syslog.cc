94ce5d0a50f2c20ae39a8ad7c263141c79ca8205
Joshua Juran <jjuran@metamage.com>
2010-01-31 14:41:39 -0800

    Kerosene:  Add openlog() and syslog() stubs needed to link git-daemon.

---
+++ lamp/Kerosene/Library/syslog.cc
@@ -0,0 +1,20 @@
+/*
+	syslog.cc
+	---------
+*/
+
+// POSIX
+#include "syslog.h"
+
+
+#pragma exceptions off
+
+
+void openlog( const char *ident, int logopt, int facility )
+{
+}
+
+void syslog( int priority, const char *message, ... )
+{
+}
+
