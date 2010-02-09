9e14dc18dd03dfdf66c0f6dc3f1bf420e27f4a76
Joshua Juran <jjuran@metamage.com>
2010-02-09 12:37:29 -0800

    [FIX]:  Call InitializeCallbacks() for microtools, which use errno and malloc().

--- lamp/Kerosene/runtime/microtool/microtool.c
+++ lamp/Kerosene/runtime/microtool/microtool.c
@@ -10,6 +10,9 @@ extern void _set_dispatcher( void *address );
 
 extern void* _initialize_environ( char **envp );
 
+// Call InitProc() to set references to cleanup proc and errno
+extern void InitializeCallbacks();
+
 // Call main() and return
 extern int _lamp_main( int argc, char **argv, char **envp, void *dispatcher );
 extern int       main( int argc, char **argv );
@@ -22,6 +25,8 @@ int _lamp_main( int argc, char **argv, char **envp, void *dispatcher )
 	_set_dispatcher( dispatcher );
 	
 	(void) _initialize_environ( envp );
+	
+	InitializeCallbacks();
 	
 	return main( argc, argv );
 }
