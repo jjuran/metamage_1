f8cc0db89b5f27a051c546902759d8a9c235fb7a
Joshua Juran <jjuran@metamage.com>
2010-03-03 01:45:02 -0800

    mac_pathname:  Add mac_pathname_from_path().  Initial check-in.

---
+++ lamp/Kerosene/mac_pathname/mac_pathname_from_path.hh
@@ -0,0 +1,33 @@
+/*
+	mac_pathname_from_path.hh
+	-------------------------
+	
+	Joshua Juran
+*/
+
+#ifndef KEROSENE_MACPATHNAMEFROMPATH_HH
+#define KEROSENE_MACPATHNAMEFROMPATH_HH
+
+
+#ifdef __cplusplus
+#include <string>
+#endif
+
+/*
+#ifdef __cplusplus
+extern "C" {
+#endif
+	
+#ifdef __cplusplus
+}
+#endif
+*/
+
+#ifdef __cplusplus
+	
+	std::string mac_pathname_from_path( const char* path );
+	
+#endif
+
+#endif
+
