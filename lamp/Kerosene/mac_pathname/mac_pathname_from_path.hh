5d162308ba6773b9916b56c369937f0a85887326
Joshua Juran <jjuran@metamage.com>
2010-03-18 12:53:26 -0700

    Kerosene:  Clean up cruft from mac_pathname_from_path.hh.

--- lamp/Kerosene/mac_pathname/mac_pathname_from_path.hh
+++ lamp/Kerosene/mac_pathname/mac_pathname_from_path.hh
@@ -8,26 +8,10 @@
 #ifndef KEROSENE_MACPATHNAMEFROMPATH_HH
 #define KEROSENE_MACPATHNAMEFROMPATH_HH
 
-
-#ifdef __cplusplus
 #include <string>
-#endif
 
-/*
-#ifdef __cplusplus
-extern "C" {
-#endif
-	
-#ifdef __cplusplus
-}
-#endif
-*/
 
-#ifdef __cplusplus
-	
-	std::string mac_pathname_from_path( const char* path );
-	
-#endif
+std::string mac_pathname_from_path( const char* path );
 
 #endif
 
