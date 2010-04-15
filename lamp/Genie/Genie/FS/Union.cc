ee227a5bbd04d435d44e60770d693aac3ab8fcb3
Joshua Juran <jjuran@metamage.com>
2010-04-14 19:50:21 -0700

    Genie:  Add Union FSTree which overlays one FSTree over another.

---
+++ lamp/Genie/Genie/FS/Union.cc
@@ -0,0 +1,78 @@
+/*
+	Union.cc
+	--------
+	
+	Copyright 2010, Joshua Juran
+*/
+
+#include "Genie/FS/Union.hh"
+
+// Standard C++
+#include <set>
+
+
+namespace Genie
+{
+	
+	FSTreePtr FSTree_Union::Lookup_Child( const std::string& name, const FSTree* parent ) const
+	{
+		if ( parent == NULL )
+		{
+			parent = this;
+		}
+		
+		FSTreePtr child = itsTop->Lookup( name, parent );
+		
+		return child->Exists() ? child : itsBottom->Lookup( name, parent );
+	}
+	
+	void FSTree_Union::IterateIntoCache( FSTreeCache& cache ) const
+	{
+		std::set< std::string > names_that_have_been_seen;
+		
+		FSIteratorPtr top = itsTop->Iterate();
+		
+		top->Advance();  // .
+		top->Advance();  // ..
+		
+		while ( true )
+		{
+			FSNode node = top->Get();
+			
+			if ( node.name.empty() )
+			{
+				break;
+			}
+			
+			names_that_have_been_seen.insert( node.name );
+			
+			cache.push_back( node );
+			
+			top->Advance();
+		}
+		
+		FSIteratorPtr bottom = itsBottom->Iterate();
+		
+		bottom->Advance();  // .
+		bottom->Advance();  // ..
+		
+		while ( true )
+		{
+			FSNode node = bottom->Get();
+			
+			if ( node.name.empty() )
+			{
+				break;
+			}
+			
+			if ( !names_that_have_been_seen.count( node.name ) )
+			{
+				cache.push_back( node );
+			}
+			
+			bottom->Advance();
+		}
+	}
+	
+}
+
