12d94169f7cd168d602c0886dfe643c1654efe9d
Joshua Juran <jjuran@metamage.com>
2010-04-14 19:48:36 -0700

    Genie:  Add Root_Overlay FSTree containing virtual items of / (e.g. /dev, /sys).

---
+++ lamp/Genie/Genie/FS/Root_Overlay.hh
@@ -0,0 +1,23 @@
+/*
+	Root_Overlay.hh
+	---------------
+	
+	Copyright 2010, Joshua Juran
+*/
+
+#ifndef GENIE_FS_ROOTOVERLAY_HH
+#define GENIE_FS_ROOTOVERLAY_HH
+
+// Genie
+#include "Genie/FS/FSTree_Directory.hh"
+
+
+namespace Genie
+{
+	
+	extern const FSTree_Premapped::Mapping Root_Overlay_Mappings[];
+	
+}
+
+#endif
+
