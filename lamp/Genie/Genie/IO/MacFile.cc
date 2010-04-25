8a52f7f3618fd5beed248ae0833e56e60ef00206
Joshua Juran <jjuran@metamage.com>
2010-04-25 05:31:12 -0700

    Genie:  Set a file to a 'binary' type if control characters are written.
    If a write to file offset 0 contains control characters (in the first 16 bytes),
    set the file's type and creator to '????' if it's already text; if no control
    characters are present then set the type back to text (provided it's currently
    '????').

--- lamp/Genie/Genie/IO/MacFile.cc
+++ lamp/Genie/Genie/IO/MacFile.cc
@@ -6,6 +6,7 @@
 #include "Genie/IO/MacFile.hh"
 
 // Genie
+#include "Genie/FileSignature.hh"
 #include "Genie/FS/FSSpec.hh"
 #include "Genie/FS/FSTree_RsrcFile.hh"
 #include "Genie/IO/RegularFile.hh"
@@ -73,6 +74,92 @@ namespace Genie
 		return result;
 	}
 	
+	static void ToggleBinaryFileSignature( const FSSpec& file, bool to_binary )
+	{
+		const ::OSType creators[] = { TextFileCreator(), '\?\?\?\?' };
+		const ::OSType types   [] = { 'TEXT',            '\?\?\?\?' };
+		
+		FInfo fInfo = N::FSpGetFInfo( file );
+		
+		if ( fInfo.fdType == types[ !to_binary ] )
+		{
+			fInfo.fdCreator = creators[ to_binary ];
+			fInfo.fdType    = types   [ to_binary ];
+			
+			N::FSpSetFInfo( file, fInfo );
+		}
+	}
+	
+	static void CheckFileSignature( const FSSpec& file, const char* data, size_t n_bytes )
+	{
+		// bitmap of which control characters make a file 'binary' (i.e. non-text)
+		
+		const unsigned mask = 1 << '\x00'
+		                    | 1 << '\x01'
+		                    | 1 << '\x02'
+		                    | 1 << '\x03'
+		                    | 1 << '\x04'
+		                    | 1 << '\x05'
+		                    | 1 << '\x06'
+		                    | 1 << '\x07'
+		                    | 1 << '\x08'
+		                    | 0 << '\t'  // Tab
+		                    | 0 << '\n'  // Line Feed
+		                    | 1 << '\x0b'
+		                    | 1 << '\x0c'
+		                    | 0 << '\r'  // Return
+		                    | 1 << '\x0e'
+		                    | 1 << '\x0f'
+		                    | 1 << '\x10'
+		                    | 1 << '\x11'
+		                    | 1 << '\x12'
+		                    | 1 << '\x13'
+		                    | 1 << '\x14'
+		                    | 1 << '\x15'
+		                    | 1 << '\x16'
+		                    | 1 << '\x17'
+		                    | 1 << '\x18'
+		                    | 1 << '\x19'
+		                    | 1 << '\x1a'
+		                    | 1 << '\x1b'
+		                    | 1 << '\x1c'
+		                    | 1 << '\x1d'
+		                    | 1 << '\x1e'
+		                    | 1 << '\x1f';
+		
+		// Enough of the file header to tell if it's 'binary'.
+		// PEF is text through the first 12, so make it 16.
+		const size_t enough_bytes = 16;
+		
+		const char* end = data + std::min( n_bytes, enough_bytes );
+		
+		bool is_binary = false;
+		
+		for ( const char* p = data;  p < end;  ++p )
+		{
+			const char c = *p;
+			
+			if ( c < 32  &&  1 << c & mask )
+			{
+				// Control character found.
+				// Set type/creator for a 'binary' file.
+				
+				is_binary = true;
+				
+				break;
+			}
+		}
+		
+		try
+		{
+			ToggleBinaryFileSignature( file, is_binary );
+		}
+		catch ( ... )
+		{
+		}
+	}
+	
+	
 	MacFileHandle::MacFileHandle( const n::shared< N::FSFileRefNum >&  refNum,
 	                              OpenFlags                            flags,
 	                              FileGetter                           getFile )
@@ -127,6 +214,11 @@ namespace Genie
 		                           byteCount,
 		                           data,
 		                           async );
+		
+		if ( offset == 0 )
+		{
+			CheckFileSignature( FSSpecFromFRefNum( itsRefNum ), data, byteCount );
+		}
 		
 		return written;
 	}
