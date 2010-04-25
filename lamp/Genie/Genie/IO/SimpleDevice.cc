ceb96bad7950dab4ef6f147639149e43662eb8b2
Joshua Juran <jjuran@metamage.com>
2010-04-24 22:05:40 -0700

    Genie:  Support mmap() of /dev/zero.
    Actually, this works on any simple device (/dev/{console,null,zero}) -- since
    this is the Simplest Thing That Could Possibly Work.  Of course, it's only
    actually *supported* for /dev/zero.
    
    A possible future direction is supporting /dev/null for mapping memory that
    isn't (necessarly) zeroed, but only if performance warrants it.

--- lamp/Genie/Genie/IO/SimpleDevice.cc
+++ lamp/Genie/Genie/IO/SimpleDevice.cc
@@ -5,12 +5,19 @@
 
 #include "Genie/IO/SimpleDevice.hh"
 
+// Nitrogen
+#include "Nitrogen/MacMemory.hh"
+
 // Genie
 #include "Genie/FS/ResolvePathname.hh"
+#include "Genie/mmap/Handle_memory_mapping.hh"
 
 
 namespace Genie
 {
+	
+	namespace N = Nitrogen;
+	
 	
 	FSTreePtr SimpleDeviceHandle::GetFile()
 	{
@@ -29,6 +36,11 @@ namespace Genie
 	ssize_t SimpleDeviceHandle::SysWrite( const char* data, std::size_t byteCount )
 	{
 		return io.writer( data, byteCount );
+	}
+	
+	memory_mapping::shared_ptr SimpleDeviceHandle::Map( size_t length, off_t offset )
+	{
+		return seize_ptr( new Handle_memory_mapping( N::NewHandleClear( length ) ) );
 	}
 	
 }
