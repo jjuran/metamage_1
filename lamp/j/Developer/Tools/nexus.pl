15116b7e6cc8125ba4b795c792a62589e2030f53
Joshua Juran <jjuran@metamage.com>
2010-04-25 22:59:44 -0700

    nexus.pl:  A script to copy an MPW subset to a RAM disk.  Initial check-in.
    The nexus.pl script looks for a RAM disk, renames it to 'Nexus', and copies over
    files from MPW, including ToolServer and a subset of Interfaces and Libraries.
    Also copied is part of the CodeWarrior for MPW distribution, which is expected
    to be present.
    
    We skip headers and libraries which are specific to MPW-provided compilers, MPW
    tools, and even parts of CodeWarrior that we don't use, like SIOUX and two-byte-
    integer libc.  Also omitted are deprecated headers from Universal Interfaces,
    like Types.h.

---
+++ lamp/j/Developer/Tools/nexus.pl
@@ -0,0 +1,255 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $copier;
+
+my @whitelist = qw( fenv.h fp.h vBasicOps.h vBigNum.h vBLAS.h vDSP.h vectorOps.h vfp.h );
+
+# Types.h is included by SANE.h
+
+my @blacklist =
+qw
+(
+	CursorCtl.h Desk.h DriverSupport.h Errors.h ErrMgr.h Errors.h FragLoad.h
+	FSpio.h GestaltEqu.h IntEnv.h Interrupts.h Kernel.h Keychain.h Language.h
+	MacRuntime.h MC68000Test.h Memory.h MP.h MPWLibsDebug.h NullDef.h
+	OpenTptAppleTalk.h OpenTptClient.h OpenTptCommon.h OpenTptConfig.h
+	OpenTptDevLinks.h OpenTptGlobalNew.h OpenTptInternet.h OpenTptISDN.h
+	OpenTptLinks.h OpenTptModule.h OpenTptPCISupport.h OpenTptSerial.h
+	OpenTptXTI.h OSEvents.h OTDebug.h OTSharedLibs.h Picker.h PictUtil.h
+	SeekDefs.h SIO.h SizeTDef.h SoundComponents.h SoundInput.h Speech.h Strings.h
+	Unicode.h VaListTDef.h WCharTDef.h Windows.h
+);
+
+my %xfer =
+(
+	"Interfaces&Libraries" =>
+	{
+		"Interfaces" =>
+		{
+			"CIncludes" =>
+			sub
+			{
+				my $want = /^[A-Z]/;
+				my $x    = $_;
+				
+				!$want == not grep { $_ eq $x } $want ? @blacklist : @whitelist;
+			},
+			"." => [ map { $_ . "Includes"  } qw( CWANSI CWC R ) ]
+		},
+		"Libraries"  =>
+		{
+			"MW68KLibraries" => sub { /MPW/ || /SIOUX/ || /2i/ },
+			"MWPPCLibraries" => sub { /MPW/ || /SIOUX/ },
+			"Libraries" => sub { /^SIO/ || /^ToolLibs/ },
+			"PPCLibraries" => sub { /^PPC/ || /^StdC/ || /^MrC/ },
+			"." => [ "SharedLibraries" ]
+		},
+	},
+	"MPW" =>
+	{
+		"Tools" => [ qw( FLEXlm license.dat InformDbgr MWC68K MWCPPC MWLink68K MWLinkPPC Rez ) ],
+		"." =>
+		[
+			qw( Help MPW.Help StartupTS SysErrs.err ToolServer ),
+			map { $_ . "Startup Items" } "", "TS "
+		]
+	},
+);
+
+
+sub read_file
+{
+	my ( $path ) = @_;
+	
+	open my $in, "<", $path or die "Can't open $path: $!\n";
+	
+	local $/;
+	
+	my $data = <$in>;
+}
+
+sub write_file
+{
+	my ( $path, $data ) = @_;
+	
+	open my $out, ">", $path or die "Can't open $path: $!\n";
+	
+	print $out $data;
+	
+	return;
+}
+
+
+sub transfer_one
+{
+	my ( $src, $dest ) = @_;
+	
+	if ( -f "$src" )
+	{
+		print $copier "$src\0$dest\0\n" or die "Error writing to copier: $!\n";
+	}
+	else
+	{
+		transfer( "$src", "$dest" );
+	}
+}
+
+sub transfer
+{
+	my ( $src, $dest, $xfer ) = @_;
+	
+	local $| = 1;
+	
+	my ( $basename ) = $src =~ m{ ( [^/]* ) $}x;
+	
+	print "\r";
+	
+	print "Copy $basename..." . (" " x (31 - length $basename));
+	
+	print $copier "\0$dest\0\n" or die "Error writing to copier: $!\n" unless $basename eq ".";
+	
+	if ( ref $xfer eq "HASH" )
+	{
+		foreach my $name ( keys %$xfer )
+		{
+			my $set = $xfer->{$name};
+			
+			-d "$src/$name" or die "Missing $src/$name\n";
+			
+			transfer( "$src/$name", "$dest/$name", $set );
+		}
+	}
+	elsif ( ref $xfer eq "ARRAY" )
+	{
+		foreach my $name ( @$xfer )
+		{
+			transfer_one( "$src/$name", "$dest/$name" );
+		}
+	}
+	else
+	{
+		my $filter = ref $xfer eq "CODE" ? $xfer : sub { undef };
+		
+		opendir my $dir, $src or die "Can't open $src: $!\n";
+		
+		foreach my $name ( readdir $dir )
+		{
+			next if $name eq ".";
+			next if $name eq "..";
+			next if $name eq ".DS_Store";
+			
+			next if grep { $filter->() } $name;
+			
+			transfer_one( "$src/$name", "$dest/$name" );
+		}
+	}
+}
+
+sub nexus
+{
+	my $ramdisk = readlink "/sys/mac/vol/ram";
+	
+	if ( $ramdisk )
+	{
+		my $name = read_file( "/sys/mac/vol/ram/name" );
+		
+		chomp $name;
+		
+		print "RAM disk '$name' found at /sys/mac/vol/$ramdisk\n";
+	}
+}
+
+sub auto
+{
+	my $nexus = readlink "/Volumes/Nexus";
+	
+	if ( defined $nexus )
+	{
+		print "Nexus found at $nexus\n";
+	}
+	else
+	{
+		print "Nexus volume not found.\n";
+		
+		if ( my $ramdisk = readlink "/sys/mac/vol/ram" )
+		{
+			my $name = read_file( "/sys/mac/vol/ram/name" );
+			
+			chomp $name;
+			
+			print "RAM disk '$name' found at /sys/mac/vol/$ramdisk\n";
+			
+			write_file( "/sys/mac/vol/ram/name", "Nexus\n" );
+			
+			print "Renamed RAM disk volume to 'Nexus'.\n";
+			
+			$nexus = "/sys/mac/vol/ram/mnt";
+		}
+		else
+		{
+			return;
+		}
+	}
+	
+	my $toolserver = readlink "/sys/mac/vol/boot/dt/appls/MPSX/latest";
+	
+	if ( !defined $toolserver )
+	{
+		die "ToolServer not found on the boot volume.\n";
+	}
+	
+	print "ToolServer found at $toolserver.\n";
+	
+	( my $mpw = $toolserver ) =~ s{ / [^/]+ $}{}x;
+	
+	print "MPW found at $mpw.\n";
+	
+	( my $dev_apps = $mpw ) =~ s{ / [^/]+ $}{}x;
+	
+	my $intfs_libs = "$dev_apps/Interfaces&Libraries";
+	
+	-d $intfs_libs or die "Interfaces&Libraries not found.\n";
+	
+	print "Interfaces&Libraries found at $intfs_libs.\n";
+	
+	-d "$nexus/tmp" or mkdir "$nexus/tmp" or die "mkdir: $nexus/tmp: $!\n";
+	
+	open $copier, "| copier" or die "Can't run the copier: $!\n";
+	
+	transfer( $dev_apps, "$nexus/tmp/Applications", \%xfer );
+	
+	local $| = 1;
+	
+	print "\r";
+	
+	print "Finishing..." . (" " x 40);
+	
+	close $copier or die "Error closing the copier: $?\n";
+	
+	rename "$nexus/tmp/Applications", "$nexus/Applications" or die "Can't move Applications out of tmp: $!\n";
+	
+	print "\r";
+	
+	print "Done." . (" " x 50);
+	print "\n";
+	
+	return;
+}
+
+
+my $command = shift;
+
+$command = "" if !defined $command;
+
+my %handler_for =
+(
+	""   => sub { nexus },
+	auto => sub { auto  },
+);
+
+my $handler = $handler_for{$command};
+
+$handler->() if defined $handler;
+
