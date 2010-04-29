ec41dc707bf70d88fb5dd5f6a25e4ab5ccbf9dbf
Joshua Juran <jjuran@metamage.com>
2010-04-28 21:41:08 -0700

    plus::concat:  Use reserve() and append() instead of a do-it-yourself handoff.
    This fixes two issues:  One, that the capacity isn't tweaked (which is simple to
    fix) and two, that it always allocates, even if the concatenated result wouldn't
    require it (less simple and duplicates logic).

--- base/plus/plus/concat.cc
+++ base/plus/plus/concat.cc
@@ -7,35 +7,22 @@
 
 #include "plus/concat.hh"
 
-// Standard C
-#include <string.h>
+// plus
+#include "plus/var_string.hh"
 
 
 namespace plus
 {
 	
-	static void concat( const char* a, string::size_type  a_size,
-	                    const char* b, string::size_type  b_size,
-	                    string& result )
-	{
-		const string::size_type total_size = a_size + b_size;
-		
-		char* buffer = (char*) ::operator new( total_size + 1 );
-		
-		memcpy( buffer,          a, a_size );
-		memcpy( buffer + a_size, b, b_size );
-		
-		buffer[ total_size ] = '\0';
-		
-		result.assign( buffer, total_size, delete_basic );
-	}
-	
 	string concat( const char*  a, string::size_type  a_size,
 	               const char*  b, string::size_type  b_size )
 	{
-		string result;
+		var_string result;
 		
-		concat( a, a_size, b, b_size, result );
+		result.reserve( a_size + b_size );
+		
+		result.append( a, a_size );
+		result.append( b, b_size );
 		
 		return result;
 	}
