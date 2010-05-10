ec903f014c9abc52f98625fdf39edc9f47af7dad
Joshua Juran <jjuran@metamage.com>
2010-05-09 18:31:34 -0700

    demangle:  Command-line interface to recall's demangler.  Initial check-in

---
+++ toolchain/demangle/demangle.cc
@@ -0,0 +1,50 @@
+/*
+	demangle.cc
+	-----------
+	
+	Copyright 2010, Joshua Juran
+*/
+
+// recall
+#include "recall/demangle.hh"
+
+// plus
+#include "plus/var_string.hh"
+
+// poseven
+#include "poseven/functions/write.hh"
+
+// Orion
+#include "Orion/Main.hh"
+
+
+namespace p7 = poseven;
+
+
+namespace tool
+{
+	
+	static void demangle( const char* name )
+	{
+		plus::var_string demangled;
+		
+		name[0] == '.' ? recall::demangle_MWCPPC( demangled, name )
+		               : recall::demangle_MWC68K( demangled, name );
+		
+		demangled += "\n";
+		
+		p7::write( p7::stdout_fileno, demangled );
+	}
+	
+	int Main( int argc, iota::argv_t argv )
+	{
+		while ( *++argv != NULL )
+		{
+			demangle( *argv );
+		}
+		
+		return 0;
+	}
+
+}
+
