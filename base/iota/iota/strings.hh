5d5c8969f3b9b541d5ea80d1deed86c930b2b477
Joshua Juran <jjuran@metamage.com>
2010-05-12 17:46:36 -0700

    iota:  Add string_c_str trait template and get_string_c_str() function template

--- base/iota/iota/strings.hh
+++ base/iota/iota/strings.hh
@@ -14,5 +14,54 @@
 #define STR_LEN( str )  str, STRLEN( str )
 
 
+namespace iota
+{
+	
+	template < class String >
+	struct string_c_str
+	{
+		static const char* get( const String& s )
+		{
+			return s.c_str();
+		}
+	};
+	
+	template <>
+	struct string_c_str< const char* >
+	{
+		static const char* get( const char* s )
+		{
+			return s;
+		}
+	};
+	
+	template <>
+	struct string_c_str< char* > : string_c_str< const char* >
+	{
+	};
+	
+	template < unsigned n >
+	struct string_c_str< const char[n] > : string_c_str< const char* >
+	{
+	};
+	
+#ifndef __MWERKS__
+	
+	template < unsigned n >
+	struct string_c_str< char[n] > : string_c_str< const char* >
+	{
+	};
+	
+#endif
+	
+	
+	template < class String >
+	inline const char* get_string_c_str( const String& s )
+	{
+		return string_c_str< String >::get( s );
+	}
+	
+}
+
 #endif
 
