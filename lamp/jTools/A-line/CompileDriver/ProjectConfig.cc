jjuran/metamage_1

commit a5db1bfefa0ee623c3b1c7b0ea127bdbd6d65242

A-line:  Use memcmp() and avoid three needless string constructions.

lamp/jTools/A-line/CompileDriver/ProjectConfig.cc
@@ -225,14 +225,16 @@ namespace tool
 		
 	}
 	
+	static bool ends_with( const char* a, size_t a_size, const char* b, size_t b_size )
+	{
+		const ssize_t diff_size = a_size - b_size;
+		
+		return diff_size >= 0  &&  memcmp( a + diff_size, b, b_size ) == 0;
+	}
+	
 	static void AddPendingConfigFile( const std::string& filePath )
 	{
-		std::string filename = io::get_filename( filePath );
-		std::string extension = ".conf";
-		
-		std::string::difference_type rootSize = filename.size() - extension.size();
-		
-		if ( rootSize <= 0  ||  filename.substr( rootSize, std::string::npos ) != extension )
+		if ( ends_with( filePath.data(), filePath.size(), STR_LEN( ".conf" ) ) )
 		{
 			return;
 		}

commit 0d85d853c0f317c867fa9d3d6c51fdd66fa59d06

[FIX] A-line:  Fix negated logic for conf detection in catalog mode
This had been broken since a5db1bfefa0ee623c3b1c7b0ea127bdbd6d65242 on April 12:
"A-line:  Use memcmp() and avoid three needless string constructions."

lamp/jTools/A-line/CompileDriver/ProjectConfig.cc
@@ -234,7 +234,7 @@ namespace tool
 	
 	static void AddPendingConfigFile( const plus::string& filePath )
 	{
-		if ( ends_with( filePath.data(), filePath.size(), STR_LEN( ".conf" ) ) )
+		if ( !ends_with( filePath.data(), filePath.size(), STR_LEN( ".conf" ) ) )
 		{
 			return;
 		}