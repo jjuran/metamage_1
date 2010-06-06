5f97695416b450c0bb0a4e516b0032945a9475df
Joshua Juran <jjuran@metamage.com>
2010-06-06 04:07:01 -0700

    MacFeatures:  Move non-trivial logic out of header

--- pedestal/Practica/MacFeatures/Features.hh
+++ pedestal/Practica/MacFeatures/Features.hh
@@ -6,64 +6,67 @@
 #ifndef MACFEATURES_FEATURES_HH
 #define MACFEATURES_FEATURES_HH
 
-// Nitrogen
-#include "Nitrogen/Gestalt.hh"
+// Mac OS
+#ifndef __MACTYPES__
+#include <MacTypes.h>
+#endif
 
-
-namespace Nitrogen
-{
-	
-	static const Gestalt_Selector gestaltAppearanceAttr            = Gestalt_Selector( ::gestaltAppearanceAttr            );
-	static const Gestalt_Selector gestaltMacOSCompatibilityBoxAttr = Gestalt_Selector( ::gestaltMacOSCompatibilityBoxAttr );
-	static const Gestalt_Selector gestaltSystemVersion             = Gestalt_Selector( ::gestaltSystemVersion             );
-	
-}
 
 namespace MacFeatures
 {
 	
-	inline UInt32 SystemVersion()
-	{
-		return Nitrogen::Gestalt( Nitrogen::gestaltSystemVersion );
-	}
+	UInt32 SystemVersion();
+	
+	bool Has_AppearanceManager();
+	
+	bool Is_Running_InClassic();
+	
+	bool Is_Running_OSXNative();
+	
+	bool Has_OSXSystem();
+	
+	
+#if TARGET_API_MAC_CARBON
 	
 	inline bool Has_AppearanceManager()
 	{
 		// Carbon implies 8.1 or later; 8.0 or later implies Appearance.
-		return TARGET_API_MAC_CARBON  ||  Nitrogen::Gestalt( Nitrogen::gestaltAppearanceAttr, 0 );
+		return true;
 	}
+	
+#endif
+	
+#if TARGET_RT_MAC_MACHO
 	
 	inline bool Is_Running_InClassic()
 	{
-		return !TARGET_RT_MAC_MACHO  &&  Nitrogen::Gestalt( Nitrogen::gestaltMacOSCompatibilityBoxAttr, 0 ) & gestaltMacOSCompatibilityBoxPresent;
+		// Mach-O is definitely not in Classic.  But everything else could be.
+		return false;
 	}
+	
+#endif
+	
+#if TARGET_RT_MAC_MACHO  ||  !TARGET_API_MAC_CARBON
 	
 	inline bool Is_Running_OSXNative()
 	{
-		return TARGET_RT_MAC_MACHO  ||  TARGET_API_MAC_CARBON  &&  SystemVersion() >= 0x1000;
+		// Mach-O is always OS-X-native, non-Carbon is never OS-X-native
+		return TARGET_RT_MAC_MACHO;
 	}
+	
+#endif
+	
+#if TARGET_RT_MAC_MACHO
 	
 	inline bool Has_OSXSystem()
 	{
-		if ( TARGET_RT_MAC_MACHO )
-		{
-			return true;
-		}
+		// Anything that runs in OS 9 is indeterminate since it can run in
+		// Classic, but Mach-O is always OS X.
 		
-		const UInt32 sysv = SystemVersion();
-		
-		if ( TARGET_API_MAC_CARBON  &&  sysv >= 0x1000 )
-		{
-			return true;
-		}
-		
-		if ( sysv < 0x0900 )
-		{
-			return false;
-		}
-		
-		return Is_Running_InClassic();
+		return true;
 	}
+	
+#endif
 	
 }
 
