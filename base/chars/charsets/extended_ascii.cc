873411307d871f8bcab06d8dd792026953ed246d
Joshua Juran <jjuran@metamage.com>
2010-07-19 19:56:56 -0700

    chars:  Framework for Unicode <-> extended ASCII conversion
    Define unicode_from_extended_ascii() and extended_ascii_from_unicode(),
    each of which requires a set of mappings to perform the conversion.

---
+++ base/chars/charsets/extended_ascii.cc
@@ -0,0 +1,36 @@
+/*
+	extended_ascii.cc
+	-----------------
+*/
+
+#include "charsets/extended_ascii.hh"
+
+// Standard C++
+#include <algorithm>
+
+
+namespace chars
+{
+	
+	static inline bool operator<( const unicode_mapping& mapping, unichar_t uc )
+	{
+		return mapping.unicode < uc;
+	}
+	
+	char extended_ascii_from_unicode( unichar_t              uc,
+	                                  const unicode_mapping  map[] )
+	{
+		const unicode_mapping* map_end = map + n_extended_ASCII_mappings;
+		
+		const unicode_mapping* it = std::lower_bound( map, map_end, uc );
+		
+		if ( it != map_end  &&  it->unicode == uc )
+		{
+			return it->code;
+		}
+		
+		return 0;
+	}
+	
+}
+
---
+++ base/chars/charsets/unicode.hh
@@ -0,0 +1,17 @@
+/*
+	unicode.hh
+	----------
+*/
+
+#ifndef CHARSETS_UNICODE_HH
+#define CHARSETS_UNICODE_HH
+
+namespace chars
+{
+	
+	typedef unsigned int unichar_t;
+	
+}
+
+#endif
+
