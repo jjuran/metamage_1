454ddb8780277812f2f2e9a78805a62326e4c6af
Joshua Juran <jjuran@metamage.com>
2010-07-28 23:02:06 -0700

    chars:  Add MacRoman character set mappings

---
+++ base/chars/charsets/MacRoman.hh
@@ -0,0 +1,23 @@
+/*
+	MacRoman.hh
+	-----------
+*/
+
+#ifndef CHARSETS_MACROMAN_HH
+#define CHARSETS_MACROMAN_HH
+
+// chars
+#include "charsets/extended_ascii.hh"
+
+
+namespace chars
+{
+	
+	extern const unsigned short MacRoman_decoder_table[];
+	
+	extern const struct unicode_mapping MacRoman_encoder_map[];
+	
+}
+
+#endif
+
