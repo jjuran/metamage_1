d5b52d4925004e3a7cef680dae2a7e6b4755ce79
Joshua Juran <jjuran@metamage.com>
2010-08-02 03:06:25 -0700

    compat/lamp:  Declare _lamp_stack_space()

---
+++ posix/compat/lamp/stack.h
@@ -0,0 +1,20 @@
+/*
+	stack.h
+	-------
+*/
+
+#ifndef LAMP_STACK_H
+#define LAMP_STACK_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+unsigned _lamp_stack_space( void );
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
