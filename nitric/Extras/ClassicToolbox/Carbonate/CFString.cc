8dbe08cd4f2da2c8e2cd40b9b6b2c71e8521aed5
Joshua Juran <jjuran@metamage.com>
2010-09-10 16:56:03 -0700

    Carbonate:  Remove cruft from CFString
    Add a UTF16Char* constructor so CFStringCreateWithCharacters() can stay.
    Redefine CFStringCreateWithPascalString() via CFStringCreateWithBytes(),
    (which is not defined yet).

--- nitric/Extras/ClassicToolbox/Carbonate/CFString.cc
+++ nitric/Extras/ClassicToolbox/Carbonate/CFString.cc
@@ -1,42 +1,29 @@
 // Carbonate/CFString.cc
 
-
+// Mac OS
 #ifndef __CFSTRING__
 #include <CFString.h>
 #endif
 
 #if !TARGET_API_MAC_CARBON
 
-#include <string>
-
 #include "Carbonate/CFBase.hh"
 
 
-
-//#define CFSTR( x )  ( x )
-
-typedef std::basic_string< UniChar > UniString;
-
 struct __CFString : CFObject
 {
-	std::string  chars8;
-	UniString    chars16;
-	
-	__CFString()  {}
-	
-	__CFString( const char* cStr ) : chars8( cStr )  {}
-	
-	__CFString( ConstStr255Param pStr ) : chars8( pStr + 1, pStr + 1 + pStr[ 0 ] )  {}
-	
-	__CFString( const char   * chars, std::size_t length ) : chars8 ( chars, length )  {}
-	__CFString( const UniChar* chars, std::size_t length ) : chars16( chars, length )  {}
+	__CFString( const UTF16Char* u, UInt32 n_chars );
 };
 
 CFStringRef CFStringCreateWithPascalString( CFAllocatorRef    alloc,
                                             ConstStr255Param  pStr,
                                             CFStringEncoding  encoding )
 {
-	return new __CFString( pStr );
+	return CFStringCreateWithBytes( alloc,
+	                                pStr + 1,
+	                                pStr[ 0 ],
+	                                encoding,
+	                                false );
 }
 
 CFStringRef CFStringCreateWithCharacters( CFAllocatorRef  alloc,
