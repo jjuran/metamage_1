ffb7a5367e7b6ba58033edd4d2492a261a4aa385
Joshua Juran <jjuran@metamage.com>
2010-09-11 14:51:50 -0700

    Genie:  Move and rename 68K DispatchSystemCall()
    The new name is dispatch_68k_system_call().

---
+++ lamp/Genie/Genie/Dispatch/system_call.68k.hh
@@ -0,0 +1,18 @@
+/*
+	system_call.68k.hh
+	------------------
+*/
+
+#ifndef GENIE_DISPATCH_SYSTEMCALL_68K_HH
+#define GENIE_DISPATCH_SYSTEMCALL_68K_HH
+
+
+namespace Genie
+{
+	
+	extern "C" void dispatch_68k_system_call( ... );
+	
+}
+
+#endif
+
