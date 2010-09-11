6d832ae6d0925edefde0324617c1782f950a0054
Joshua Juran <jjuran@metamage.com>
2010-09-11 16:46:13 -0700

    syscalls:  Split into 68k, ppc, and syscall stubs
    Separate the 68K code, PPC code, and the actual syscall definitions.
    
    A future A-line patch will ignore the unneeded unit so that changing the
    code for one architecture does require recompiling the other.

---
+++ lamp/Kerosene/syscalls/syscalls.68k.c
@@ -0,0 +1,42 @@
+/*
+	syscalls.68k.c
+	--------------
+*/
+
+
+extern int syscall( int number, ... );
+
+#ifdef __MC68K__
+	
+	asm int syscall( int number, ... )
+	{
+		MOVE.L  4(SP), D0    // copy system call number to d0
+		MOVE.L  (SP)+, (SP)  // overwrite it with the return address, and pop
+		
+		TRAP  #0
+	}
+	
+	#define DEFINE_STUB_7F( name )    \
+		extern void name();           \
+		asm void name()               \
+		{                             \
+			MOVEQ #__NR_##name,D0  ;  \
+			TRAP  #0               ;  \
+		}
+	
+	#define DEFINE_STUB( name )       \
+		extern void name();           \
+		asm void name()               \
+		{                             \
+			MOVE.W #__NR_##name,D0 ;  \
+			TRAP  #0               ;  \
+		}
+	
+	#define DEFINE_STUB_FF( name )  DEFINE_STUB( name )
+	
+	
+	// syscalls
+	#include "define_syscalls.h"
+	
+#endif
+
