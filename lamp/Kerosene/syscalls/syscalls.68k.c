c9b3cdb4a965dcf6e1101ce969deca58ebfeb781
Joshua Juran <jjuran@metamage.com>
2010-09-17 20:39:56 -0700

    syscalls/68k:  Support SC compiler (Symantec C)
    SC doesn't define __MC68K__, so check for __SC__.
    
    SC's asm keyword is wholly different from Metrowerks'.  It can only be used
    inline, not for an entire function body, and in fact can't actually process
    assembly code at all -- it just inserts machine codes into the object code.

--- lamp/Kerosene/syscalls/syscalls.68k.c
+++ lamp/Kerosene/syscalls/syscalls.68k.c
@@ -6,7 +6,9 @@
 
 extern int syscall( int number, ... );
 
-#ifdef __MC68K__
+#if defined( __MC68K__ )  ||  defined( __SC__ )
+	
+#ifdef __MWERKS__
 	
 	asm int syscall( int number, ... )
 	{
@@ -31,6 +33,25 @@ extern int syscall( int number, ... );
 			MOVE.W #__NR_##name,D0 ;  \
 			TRAP  #0               ;  \
 		}
+	
+#else
+	
+	#define DEFINE_SYSCALL_7F( name )   \
+		extern void name( void );       \
+		void name( void )               \
+		{                               \
+			asm( 0x7000 + __NR_##name,  \
+			     0x4e40 );              \
+		}
+
+	#define DEFINE_SYSCALL( name )     \
+		void name( void )              \
+		{                              \
+			asm( 0x303c, __NR_##name,  \
+			     0x4e40 );             \
+		}
+
+#endif
 	
 	
 	// syscalls
