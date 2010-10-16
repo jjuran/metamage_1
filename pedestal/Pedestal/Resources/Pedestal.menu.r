0befdf479a8b399f08958084ecffdcb9eced90f2
Joshua Juran <jjuran@metamage.com>
2010-10-15 23:59:53 -0700

    Pedestal:  Rewrite Pedestal.menu.r with structure

--- pedestal/Pedestal/Resources/Pedestal.menu.r
+++ pedestal/Pedestal/Resources/Pedestal.menu.r
@@ -1,27 +1,56 @@
-data 'MENU' (128, "Apple") {
-	$"0001 0000 0000 0000 0000 FFFF FFFF 0114"            /* ..........ÿÿÿÿ.. */
-	$"1641 626F 7574 2050 6564 6573 7461 6CC9"            /* .About PedestalÉ */
-	$"205B 6162 6F75 5D00 0000 0000"                      /*  [abou]..... */
+resource 'MENU' (128, "Apple") {
+	1,
+	textMenuProc,
+	allEnabled,
+	enabled,
+	apple,
+	{	/* array: 1 elements */
+		/* [1] */
+		"About PedestalÉ [abou]", noIcon, noKey, noMark, plain
+	}
 };
 
-data 'MENU' (130, "Edit") {
-	$"0003 0000 0000 0000 0000 FFFF FFFB 0445"            /* ..........ÿÿÿû.E */
-	$"6469 740B 556E 646F 205B 756E 646F 5D00"            /* dit.Undo [undo]. */
-	$"5A00 0001 2D00 0000 000A 4375 7420 5B63"            /* Z...-.....Cut [c */
-	$"7574 205D 0058 0000 0B43 6F70 7920 5B63"            /* ut ].X...Copy [c */
-	$"6F70 795D 0043 0000 0C50 6173 7465 205B"            /* opy].C...Paste [ */
-	$"7073 7465 5D00 5600 000C 436C 6561 7220"            /* pste].V...Clear  */
-	$"5B63 6C65 615D 0000 0000 1153 656C 6563"            /* [clea].....Selec */
-	$"7420 416C 6C20 5B73 6C63 745D 0041 0000"            /* t All [slct].A.. */
-	$"00"                                                 /* . */
+resource 'MENU' (129, "File") {
+	2,
+	textMenuProc,
+	0x7FFFFFF7,
+	enabled,
+	"File",
+	{	/* array: 5 elements */
+		/* [1] */
+		"New [new ]", noIcon, "N", noMark, plain,
+		/* [2] */
+		"OpenÉ [open]", noIcon, "O", noMark, plain,
+		/* [3] */
+		"Close [clos]", noIcon, "W", noMark, plain,
+		/* [4] */
+		"-", noIcon, noKey, noMark, plain,
+		/* [5] */
+		"Quit [quit]", noIcon, "Q", noMark, plain
+	}
 };
 
-data 'MENU' (129, "File") {
-	$"0002 0000 0000 0000 0000 FFFF FFEF 0446"            /* ..........ÿÿÿï.F */
-	$"696C 650A 4E65 7720 5B6E 6577 205D 004E"            /* ile.New [new ].N */
-	$"0000 0C4F 7065 6EC9 205B 6F70 656E 5D00"            /* ...OpenÉ [open]. */
-	$"4F00 000C 436C 6F73 6520 5B63 6C6F 735D"            /* O...Close [clos] */
-	$"0057 0000 012D 0000 0000 0B51 7569 7420"            /* .W...-.....Quit  */
-	$"5B71 7569 745D 0051 0000 00"                        /* [quit].Q... */
+resource 'MENU' (130, "Edit") {
+	3,
+	textMenuProc,
+	0x7FFFFFFD,
+	enabled,
+	"Edit",
+	{	/* array: 7 elements */
+		/* [1] */
+		"Undo [undo]", noIcon, "Z", noMark, plain,
+		/* [2] */
+		"-", noIcon, noKey, noMark, plain,
+		/* [3] */
+		"Cut [cut ]", noIcon, "X", noMark, plain,
+		/* [4] */
+		"Copy [copy]", noIcon, "C", noMark, plain,
+		/* [5] */
+		"Paste [pste]", noIcon, "V", noMark, plain,
+		/* [6] */
+		"Clear [clea]", noIcon, noKey, noMark, plain,
+		/* [7] */
+		"Select All [slct]", noIcon, "A", noMark, plain
+	}
 };
 
