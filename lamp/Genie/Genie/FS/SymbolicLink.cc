71312c5a985b52ea12495d26c558a3d765044832
Joshua Juran <jjuran@metamage.com>
2010-10-16 03:26:30 -0700

    Genie:  Move FSTree_SymbolicLink declaration to header
    Move FSTree_SymbolicLink into the header so clients can derive from it.

--- lamp/Genie/Genie/FS/SymbolicLink.cc
+++ lamp/Genie/Genie/FS/SymbolicLink.cc
@@ -5,31 +5,14 @@
 
 #include "Genie/FS/SymbolicLink.hh"
 
-// Genie
-#include "Genie/FS/ReadableSymLink.hh"
-#include "Genie/FS/ResolvePathname.hh"
-
 
 namespace Genie
 {
 	
-	class FSTree_SymbolicLink : public FSTree_ReadableSymLink
+	plus::string FSTree_SymbolicLink::ReadLink() const
 	{
-		private:
-			plus::string itsTarget;
-		
-		public:
-			FSTree_SymbolicLink( const FSTreePtr&     parent,
-			                     const plus::string&  name,
-			                     const plus::string&  target )
-			:
-				FSTree_ReadableSymLink( parent, name ),
-				itsTarget( target )
-			{
-			}
-			
-			plus::string ReadLink() const  { return itsTarget; }
-	};
+		return itsTarget;
+	}
 	
 	FSTreePtr New_FSTree_SymbolicLink( const FSTreePtr&     parent,
 	                                   const plus::string&  name,
--- lamp/Genie/Genie/FS/SymbolicLink.hh
+++ lamp/Genie/Genie/FS/SymbolicLink.hh
@@ -7,11 +7,29 @@
 #define GENIE_FS_SYMBOLICLINK_HH
 
 // Genie
-#include "Genie/FS/FSTree.hh"
+#include "Genie/FS/ReadableSymLink.hh"
 
 
 namespace Genie
 {
+	
+	class FSTree_SymbolicLink : public FSTree_ReadableSymLink
+	{
+		private:
+			plus::string itsTarget;
+		
+		public:
+			FSTree_SymbolicLink( const FSTreePtr&     parent,
+			                     const plus::string&  name,
+			                     const plus::string&  target )
+			:
+				FSTree_ReadableSymLink( parent, name ),
+				itsTarget( target )
+			{
+			}
+			
+			plus::string ReadLink() const;
+	};
 	
 	FSTreePtr New_FSTree_SymbolicLink( const FSTreePtr&     parent,
 	                                   const plus::string&  name,
