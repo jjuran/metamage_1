881b6e4b3e23336dd62fbccac1d81f76dbe28385
Joshua Juran <jjuran@metamage.com>
2010-10-26 18:55:01 -0700

    Genie:  Untemplatize /sys/mac/event premaps

--- lamp/Genie/Genie/FS/sys/mac/event.cc
+++ lamp/Genie/Genie/FS/sys/mac/event.cc
@@ -6,15 +6,18 @@
 #include "Genie/FS/sys/mac/event.hh"
 
 // Genie
+#include "Genie/FS/premapped.hh"
 #include "Genie/FS/sys/mac/event/post.hh"
 
 
 namespace Genie
 {
 	
+	#define PREMAPPED( map )  &premapped_factory, (const void*) map
+	
 	const FSTree_Premapped::Mapping sys_mac_event_Mappings[] =
 	{
-		{ "post", &Premapped_Factory< sys_mac_event_post_Mappings > },
+		{ "post", PREMAPPED( sys_mac_event_post_Mappings ) },
 		
 		{ NULL, NULL }
 	};
