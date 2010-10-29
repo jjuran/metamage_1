e854a6faee2288ef24fba211ed4b2dda5dbb1fec
Joshua Juran <jjuran@metamage.com>
2010-10-28 19:30:15 -0700

    ls:  Rewrite multi-byte character logic for UTF-8
    Lamp doesn't have a useful multi-byte string library.  Patch quote_name()
    to assume valid, printable UTF-8 input.

--- posix/contrib/coreutils/ls/ls.c
+++ posix/contrib/coreutils/ls/ls.c
@@ -98,7 +98,36 @@
 #include "strftime.h"
 #include "xstrtol.h"
 #include "areadlink.h"
-#include "mbsalign.h"
+
+#undef MB_CUR_MAX
+#define MB_CUR_MAX  3
+
+int gnu_mbswidth( const char *buf, int flags )
+{
+	return mbsnwidth( buf, strlen( buf ), flags );
+}
+
+int mbsnwidth( const char *buf, size_t nbytes, int flags )
+{
+	int columns = 0;
+	
+	const char* end = buf + nbytes;
+	
+	while ( buf < end )
+	{
+		/* Assume UTF-8.  Continuation bytes are 10xxxxxx */
+		
+		if ( (*buf & 0xC0) != 0x80 )
+		{
+			/* Assume single-column characters */
+			++columns;
+		}
+		
+		++buf;
+	}
+	
+	return columns;
+}
 
 /* Include <sys/capability.h> last to avoid a clash of <sys/types.h>
    include guards with some premature versions of libcap.
@@ -3812,96 +3841,25 @@ quote_name (FILE *out, const char *name, struct quoting_options const *options,
           displayed_width = 0;
 
           while (p < plimit)
-            switch (*p)
-              {
-                case ' ': case '!': case '"': case '#': case '%':
-                case '&': case '\'': case '(': case ')': case '*':
-                case '+': case ',': case '-': case '.': case '/':
-                case '0': case '1': case '2': case '3': case '4':
-                case '5': case '6': case '7': case '8': case '9':
-                case ':': case ';': case '<': case '=': case '>':
-                case '?':
-                case 'A': case 'B': case 'C': case 'D': case 'E':
-                case 'F': case 'G': case 'H': case 'I': case 'J':
-                case 'K': case 'L': case 'M': case 'N': case 'O':
-                case 'P': case 'Q': case 'R': case 'S': case 'T':
-                case 'U': case 'V': case 'W': case 'X': case 'Y':
-                case 'Z':
-                case '[': case '\\': case ']': case '^': case '_':
-                case 'a': case 'b': case 'c': case 'd': case 'e':
-                case 'f': case 'g': case 'h': case 'i': case 'j':
-                case 'k': case 'l': case 'm': case 'n': case 'o':
-                case 'p': case 'q': case 'r': case 's': case 't':
-                case 'u': case 'v': case 'w': case 'x': case 'y':
-                case 'z': case '{': case '|': case '}': case '~':
-                  /* These characters are printable ASCII characters.  */
-                  *q++ = *p++;
-                  displayed_width += 1;
-                  break;
-                default:
-                  /* If we have a multibyte sequence, copy it until we
-                     reach its end, replacing each non-printable multibyte
-                     character with a single question mark.  */
-                  {
-                    DECLARE_ZEROED_AGGREGATE (mbstate_t, mbstate);
-                    do
-                      {
-                        wchar_t wc;
-                        size_t bytes;
-                        int w;
-
-                        bytes = mbrtowc (&wc, p, plimit - p, &mbstate);
-
-                        if (bytes == (size_t) -1)
-                          {
-                            /* An invalid multibyte sequence was
-                               encountered.  Skip one input byte, and
-                               put a question mark.  */
-                            p++;
-                            *q++ = '?';
-                            displayed_width += 1;
-                            break;
-                          }
-
-                        if (bytes == (size_t) -2)
-                          {
-                            /* An incomplete multibyte character
-                               at the end.  Replace it entirely with
-                               a question mark.  */
-                            p = plimit;
-                            *q++ = '?';
-                            displayed_width += 1;
-                            break;
-                          }
-
-                        if (bytes == 0)
-                          /* A null wide character was encountered.  */
-                          bytes = 1;
-
-                        w = wcwidth (wc);
-                        if (w >= 0)
-                          {
-                            /* A printable multibyte character.
-                               Keep it.  */
-                            for (; bytes > 0; --bytes)
-                              *q++ = *p++;
-                            displayed_width += w;
-                          }
-                        else
-                          {
-                            /* An unprintable multibyte character.
-                               Replace it entirely with a question
-                               mark.  */
-                            p += bytes;
-                            *q++ = '?';
-                            displayed_width += 1;
-                          }
-                      }
-                    while (! mbsinit (&mbstate));
-                  }
-                  break;
-              }
-
+          {
+            /*
+               Assume UTF-8.
+               Assume that all sequences are valid.
+               Assume that all multibyte characters are printable.
+               Assume that all multibyte characters occupy a single column.
+            */
+            char c = *p++;
+            
+            displayed_width += (c & 0xC0) != 0x80;
+            
+            if ( !(c & 0x80)  &&  !isprint( c ) )
+            {
+              c = '?';
+            }
+            
+            *q++ = c;
+          }
+          
           /* The buffer may have shrunk.  */
           len = q - buf;
         }
