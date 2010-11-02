fa6c95353df09c3aaa0ed6ddf0ab9a75ad59a518
Joshua Juran <jjuran@metamage.com>
2010-11-02 01:31:08 -0700

    poseven:  Make listen() extern

--- posix/poseven/poseven/functions/listen.hh
+++ posix/poseven/poseven/functions/listen.hh
@@ -14,21 +14,14 @@
 #ifndef POSEVEN_FUNCTIONS_LISTEN_HH
 #define POSEVEN_FUNCTIONS_LISTEN_HH
 
-// POSIX
-#include <sys/socket.h>
-
 // poseven
-#include "poseven/types/errno_t.hh"
 #include "poseven/types/fd_t.hh"
 
 
 namespace poseven
 {
 	
-	inline void listen( fd_t fd, int backlog )
-	{
-		throw_posix_result( ::listen( fd, backlog ) );
-	}
+	void listen( fd_t fd, int backlog );
 	
 }
 
