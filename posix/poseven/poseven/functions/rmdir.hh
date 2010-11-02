271e9b990c76a2ebd3d337e50f9858984e42d925
Joshua Juran <jjuran@metamage.com>
2010-11-01 23:24:47 -0700

    poseven:  Make rmdir() extern

--- posix/poseven/poseven/functions/rmdir.hh
+++ posix/poseven/poseven/functions/rmdir.hh
@@ -13,17 +13,11 @@
 // iota
 #include "iota/string_traits.hh"
 
-// poseven
-#include "poseven/types/errno_t.hh"
-
 
 namespace poseven
 {
 	
-	inline void rmdir( const char* path )
-	{
-		throw_posix_result( ::rmdir( path ) );
-	}
+	void rmdir( const char* path );
 	
 	template < class String >
 	inline void rmdir( const String& path )
