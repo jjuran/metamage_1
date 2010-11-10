ce47ba6b9ba3c6c5204442581b876ac4357d4d2f
Joshua Juran <jjuran@metamage.com>
2010-11-09 22:32:02 -0800

    [FIX] Replace obsolete u_types with unsigned types
    Git's cache.h includes <netinet/tcp.h> for some reason.  Fix the header.
    
    I only removed the obsolete types from <sys/types.h> because some *other*
    third-party code had conflicting duplicate definitions.

--- lamp/LAMP-headers/netinet/tcp.h
+++ lamp/LAMP-headers/netinet/tcp.h
@@ -34,28 +34,28 @@
 
 /* Adapted for GUSI by Matthias Neeracher <neeri@iis.ee.ethz.ch> */
 
-typedef	u_long	tcp_seq;
+typedef	unsigned long  tcp_seq;
 /*
  * TCP header.
  * Per RFC 793, September, 1981.
  */
 struct tcphdr {
-	u_short	th_sport;		/* source port */
-	u_short	th_dport;		/* destination port */
+	unsigned short  th_sport;  /* source port */
+	unsigned short	th_dport;		/* destination port */
 	tcp_seq	th_seq;			/* sequence number */
 	tcp_seq	th_ack;			/* acknowledgement number */
-	u_char	th_off:4,		/* data offset */
-		th_x2:4;		/* (unused) */
-	u_char	th_flags;
+	unsigned char   th_off:4;  /* data offset */
+	unsigned char   th_x2:4;   /* (unused) */
+	unsigned char   th_flags;
 #define	TH_FIN	0x01
 #define	TH_SYN	0x02
 #define	TH_RST	0x04
 #define	TH_PUSH	0x08
 #define	TH_ACK	0x10
 #define	TH_URG	0x20
-	u_short	th_win;			/* window */
-	u_short	th_sum;			/* checksum */
-	u_short	th_urp;			/* urgent pointer */
+	unsigned short  th_win;  /* window */
+	unsigned short  th_sum;  /* checksum */
+	unsigned short  th_urp;  /* urgent pointer */
 };
 
 #define	TCPOPT_EOL		0
