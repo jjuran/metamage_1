032181aa499be8e3f0cdfe9ebd98bb6425bbb6eb
Joshua Juran <jjuran@metamage.com>
2010-11-12 18:50:47 -0800

    Genie:  Add empty {ENTER,EXIT}_USERLAND() macros

---
+++ lamp/Genie/Genie/userland.hh
@@ -0,0 +1,28 @@
+/*
+	userland.hh
+	-----------
+*/
+
+#ifndef GENIE_USERLAND_HH
+#define GENIE_USERLAND_HH
+
+
+namespace Genie
+{
+	
+#ifdef __MC68K__
+	
+	#define ENTER_USERLAND()  /**/
+	#define EXIT_USERLAND()   /**/
+	
+#else
+	
+	#define ENTER_USERLAND()  /**/
+	#define EXIT_USERLAND()   /**/
+	
+#endif
+	
+}
+
+#endif
+
