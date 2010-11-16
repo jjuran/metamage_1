c38270e66cab42ad32d85d7fdd3b99f0181a8d73
Joshua Juran <jjuran@metamage.com>
2010-11-15 19:41:49 -0800

    abort:  Add option to abort from a signal handler

--- lamp/jTools/abort/abort.cc
+++ lamp/jTools/abort/abort.cc
@@ -4,14 +4,27 @@
  */
 
 // Standard C
+#include <signal.h>
 #include <stdlib.h>
 
 
 #pragma exceptions off
 
 
+static void sigusr1( int )
+{
+	abort();
+}
+
 int main( int argc, char const *const argv[] )
 {
+	if ( argc > 1  &&  strcmp( argv[1], "signal" ) == 0 )
+	{
+		signal( SIGUSR1, &sigusr1 );
+		
+		raise( SIGUSR1 );
+	}
+	
 	abort();
 	
 	return 1;
