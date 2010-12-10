94fb450733923ecbf1701dfe0d7fc4cc439ad645
Joshua Juran <jjuran@metamage.com>
2010-12-10 04:00:38 -0800

    Genie:  Add check_signals()
    Call HandlePendingSignals() on the current process.  It will either throw
    EINTR or return true in the event of caught signal (depending on the value
    of the may_throw parameter), or return false otherwise; the check_signals()
    function propagates that result.

---
+++ lamp/Genie/Genie/api/signals.cc
@@ -0,0 +1,25 @@
+/*
+	signals.cc
+	----------
+*/
+
+#include "Genie/api/signals.hh"
+
+// Genie
+#include "Genie/current_process.hh"
+#include "Genie/Process.hh"
+
+
+namespace Genie
+{
+	
+	bool check_signals( bool may_throw )
+	{
+		Process& current = current_process();
+		
+		return current.HandlePendingSignals( may_throw ? kInterruptUnlessRestarting
+		                                               : kInterruptNever );
+	}
+	
+}
+
---
+++ lamp/Genie/Genie/api/signals.hh
@@ -0,0 +1,18 @@
+/*
+	signals.hh
+	----------
+*/
+
+#ifndef GENIE_API_SIGNALS_HH
+#define GENIE_API_SIGNALS_HH
+
+
+namespace Genie
+{
+	
+	bool check_signals( bool may_throw );
+	
+}
+
+#endif
+
