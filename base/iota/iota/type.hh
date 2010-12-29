673e3feb0ed08de0e8fa29408eadce40b0804eb1
Joshua Juran <jjuran@metamage.com>
2010-12-29 05:28:20 -0800

    iota:  Add type_ template
    Let
    
    	struct metafunction : type_< Result > {};
    
    be shorthand for
    
    	struct metafunction { typedef Result type; };

---
+++ base/iota/iota/type.hh
@@ -0,0 +1,22 @@
+/*
+	iota/type.hh
+	------------
+*/
+
+#ifndef IOTA_TYPE_HH
+#define IOTA_TYPE_HH
+
+
+namespace iota
+{
+	
+	template < class Type >
+	struct type_
+	{
+		typedef Type type;
+	};
+	
+}
+
+#endif
+
