35b97c810046fe5cf2431b57a40ec914983cd1db
Joshua Juran <jjuran@metamage.com>
2011-01-23 12:54:19 -0800

    Lamp headers:  Remove any AF/PF not found in Linux
    If Linux doesn't have it, it can't possibly be relevant.

--- lamp/LAMP-headers/sys/socket.h
+++ lamp/LAMP-headers/sys/socket.h
@@ -106,38 +106,14 @@ struct	linger {
 #define	AF_LOCAL	1		/* local to host (pipes, portals) */
 #define	AF_UNIX		AF_LOCAL	/* backward compatibility */
 #define	AF_INET		2		/* internetwork: UDP, TCP, etc. */
-#define	AF_IMPLINK	3		/* arpanet imp addresses */
-#define	AF_PUP		4		/* pup protocols: e.g. BSP */
-#define	AF_CHAOS	5		/* mit CHAOS protocols */
-#define	AF_NS		6		/* XEROX NS protocols */
-#define	AF_ISO		7		/* ISO protocols */
-#define	AF_OSI		AF_ISO
-#define	AF_ECMA		8		/* european computer manufacturers */
-#define	AF_DATAKIT	9		/* datakit protocols */
-#define	AF_CCITT	10		/* CCITT protocols, X.25 etc */
 #define	AF_SNA		11		/* IBM SNA */
 #define AF_DECnet	12		/* DECnet */
-#define AF_DLI		13		/* DEC Direct data link interface */
-#define AF_LAT		14		/* LAT */
-#define	AF_HYLINK	15		/* NSC Hyperchannel */
 #define	AF_APPLETALK	16		/* Apple Talk */
 #define	AF_ROUTE	17		/* Internal Routing Protocol */
-#define	AF_LINK		18		/* Link layer interface */
-#define	pseudo_AF_XTP	19		/* eXpress Transfer Protocol (no AF) */
-#define	AF_COIP		20		/* connection-oriented IP, aka ST II */
-#define	AF_CNT		21		/* Computer Network Technology */
-#define pseudo_AF_RTIP	22		/* Help Identify RTIP packets */
 #define	AF_IPX		23		/* Novell Internet Protocol */
 #define	AF_INET6	24		/* IPv6 */
-#define pseudo_AF_PIP	25		/* Help Identify PIP packets */
 #define AF_ISDN		26		/* Integrated Services Digital Network*/
-#define AF_E164		AF_ISDN		/* CCITT E.164 recommendation */
-#define AF_NATM		27		/* native ATM access */
-#define	AF_ENCAP	28
-#define	AF_SIP		29		/* Simple Internet Protocol */
 #define AF_KEY		30
-#define pseudo_AF_HDRCMPLT 31		/* Used by BPF to not rewrite headers
-					   in interface output routine */
 #define	AF_BLUETOOTH	32		/* Bluetooth */
 
 #define	AF_MAX		33
@@ -188,36 +164,14 @@ struct sockproto {
 #define	PF_LOCAL	AF_LOCAL
 #define	PF_UNIX		PF_LOCAL	/* backward compatibility */
 #define	PF_INET		AF_INET
-#define	PF_IMPLINK	AF_IMPLINK
-#define	PF_PUP		AF_PUP
-#define	PF_CHAOS	AF_CHAOS
-#define	PF_NS		AF_NS
-#define	PF_ISO		AF_ISO
-#define	PF_OSI		AF_ISO
-#define	PF_ECMA		AF_ECMA
-#define	PF_DATAKIT	AF_DATAKIT
-#define	PF_CCITT	AF_CCITT
 #define	PF_SNA		AF_SNA
 #define PF_DECnet	AF_DECnet
-#define PF_DLI		AF_DLI
-#define PF_LAT		AF_LAT
-#define	PF_HYLINK	AF_HYLINK
 #define	PF_APPLETALK	AF_APPLETALK
 #define	PF_ROUTE	AF_ROUTE
-#define	PF_LINK		AF_LINK
-#define	PF_XTP		pseudo_AF_XTP	/* really just proto family, no AF */
-#define	PF_COIP		AF_COIP
-#define	PF_CNT		AF_CNT
 #define	PF_IPX		AF_IPX		/* same format as AF_NS */
 #define PF_INET6	AF_INET6
-#define PF_RTIP		pseudo_AF_RTIP	/* same format as AF_INET */
-#define PF_PIP		pseudo_AF_PIP
 #define PF_ISDN		AF_ISDN
-#define PF_NATM		AF_NATM
-#define PF_ENCAP	AF_ENCAP
-#define	PF_SIP		AF_SIP
 #define PF_KEY		AF_KEY
-#define PF_BPF		pseudo_AF_HDRCMPLT
 #define	PF_BLUETOOTH	AF_BLUETOOTH
 #define	PF_MAX		AF_MAX
 
