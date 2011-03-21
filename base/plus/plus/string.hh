f1b61b82c973de03aad17370a558d2cd7e8e11c3
Joshua Juran <jjuran@metamage.com>
2011-03-21 04:14:11 -0700

    plus:  Add overloaded concat() templates

--- base/plus/plus/string.hh
+++ base/plus/plus/string.hh
@@ -8,6 +8,9 @@
 #ifndef PLUS_STRING_HH
 #define PLUS_STRING_HH
 
+// iota
+#include "iota/string_traits.hh"
+
 
 namespace plus
 {
@@ -297,8 +300,38 @@ namespace plus
 	}
 	
 	
-	string concat( const char* a, string::size_type  a_size,
-	               const char* b, string::size_type  b_size );
+	string concat( const char*  a, string::size_type  a_size,
+	               const char*  b, string::size_type  b_size );
+	
+	template < class A >
+	string concat( const A&     a,
+	               const char*  b, string::size_type  b_size )
+	{
+		return concat( iota::get_string_data( a ),
+		               iota::get_string_size( a ),
+		               b,
+		               b_size );
+	}
+	
+	template < class B >
+	string concat( const char*  a, string::size_type a_size,
+	               const B&     b )
+	{
+		return concat( a,
+		               a_size,
+		               iota::get_string_data( b ),
+		               iota::get_string_size( b ) );
+	}
+	
+	template < class A, class B >
+	string concat( const A&  a,
+	               const B&  b )
+	{
+		return concat( iota::get_string_data( a ),
+		               iota::get_string_size( a ),
+		               iota::get_string_data( b ),
+		               iota::get_string_size( b ) );
+	}
 	
 	string operator+( const string& a, const string& b );
 	
