78b20468a7b1042a0a4c61537e9bf264c5b91c04
Joshua Juran <jjuran@metamage.com>
2011-07-17 04:19:53 -0700

    Genie:  Define extern AESendBlocking() syscall

--- lamp/Genie/Genie/Kernel/AESendBlocking.cc
+++ lamp/Genie/Genie/Kernel/AESendBlocking.cc
@@ -3,6 +3,9 @@
  *	=================
  */
 
+// Relix
+#include "relix/AESendBlocking.h"
+
 // Nitrogen
 #include "Mac/Toolbox/Types/OSStatus.hh"
 
@@ -18,44 +21,41 @@
 #include "Genie/SystemCallRegistry.hh"
 
 
-namespace Genie
+namespace N = Nitrogen;
+
+
+OSStatus AESendBlocking( const AppleEvent* appleEventPtr, AppleEvent* replyPtr )
 {
+	using namespace Genie;
 	
-	namespace N = Nitrogen;
-	
-	
-	static OSStatus AESendBlocking( const AppleEvent* appleEventPtr, AppleEvent* replyPtr )
+	try
 	{
-		try
-		{
-			Mac::AppleEvent const& appleEvent = static_cast< const Mac::AppleEvent& >( *appleEventPtr );
-			Mac::AppleEvent      & reply      = static_cast<       Mac::AppleEvent& >( *replyPtr      );
-			
-			(void) N::AESend( appleEvent,
-			                  Mac::kAEQueueReply | Mac::kAECanInteract );
-			
-			// Now that we've sent the event, retrieve the return ID
-			N::AEReturnID_32Bit returnID = N::AEGetAttributePtr< Mac::keyReturnIDAttr >( appleEvent );
-			
-			// Subscribe to AEFramework's queued reply delivery and wake-up service
-			N::ExpectReply( returnID, &reply );
-			
-			// Sleep until the reply is delivered
-			current_process().Stop();
-		}
-		catch ( const Mac::OSStatus& err )
-		{
-			return err;
-		}
+		Mac::AppleEvent const& appleEvent = static_cast< const Mac::AppleEvent& >( *appleEventPtr );
+		Mac::AppleEvent      & reply      = static_cast<       Mac::AppleEvent& >( *replyPtr      );
 		
-		return noErr;
+		(void) N::AESend( appleEvent,
+		                  Mac::kAEQueueReply | Mac::kAECanInteract );
+		
+		// Now that we've sent the event, retrieve the return ID
+		N::AEReturnID_32Bit returnID = N::AEGetAttributePtr< Mac::keyReturnIDAttr >( appleEvent );
+		
+		// Subscribe to AEFramework's queued reply delivery and wake-up service
+		N::ExpectReply( returnID, &reply );
+		
+		// Sleep until the reply is delivered
+		current_process().Stop();
+	}
+	catch ( const Mac::OSStatus& err )
+	{
+		return err;
 	}
 	
-	#pragma force_active on
-	
-	REGISTER_SYSTEM_CALL( AESendBlocking );
-	
-	#pragma force_active reset
-	
+	return noErr;
 }
+
+#pragma force_active on
+
+REGISTER_SYSTEM_CALL( AESendBlocking );
+
+#pragma force_active reset
 
