a832891bb8d4278174ca84192ca0c3d2f4877e51
Joshua Juran <jjuran@metamage.com>
2011-07-24 19:54:22 -0700

    text-input:  Add get_line_bare_from_feed()

--- base/text-input/text_input/get_line_from_feed.hh
+++ base/text-input/text_input/get_line_from_feed.hh
@@ -40,6 +40,33 @@ namespace text_input
 		}
 	}
 	
+	template < class Reader >
+	const plus::string* get_line_bare_from_feed( text_input::feed&  feed,
+	                                             Reader             read )
+	{
+		typedef plus::string::size_type size_type;
+		
+		while ( true )
+		{
+			if ( const plus::string* result = feed.get_line_bare() )
+			{
+				return result;
+			}
+			
+			const size_type length = feed.buffer_length;
+			
+			const size_type n_read = read( feed.buffer(), length );
+			
+			if ( n_read == 0 )
+			{
+				// end of file
+				return feed.get_fragment();
+			}
+			
+			feed.accept_input( n_read );
+		}
+	}
+	
 }
 
 #endif
