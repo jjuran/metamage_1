4ddb5ff7d01f85b40f3e7f986fba340065eea099
Joshua Juran <jjuran@metamage.com>
2011-07-24 20:18:17 -0700

    text-input:  Remove get_line_from_feed()

--- base/text-input/text_input/get_line_from_feed.hh
+++ base/text-input/text_input/get_line_from_feed.hh
@@ -14,33 +14,6 @@ namespace text_input
 {
 	
 	template < class Reader >
-	const plus::string* get_line_from_feed( text_input::feed&  feed,
-	                                        Reader             read )
-	{
-		typedef plus::string::size_type size_type;
-		
-		while ( true )
-		{
-			if ( const plus::string* result = feed.get_line() )
-			{
-				return result;
-			}
-			
-			const size_type length = feed.buffer_length;
-			
-			const size_type n_read = read( feed.buffer(), length );
-			
-			if ( n_read == 0 )
-			{
-				// end of file
-				return feed.get_fragment();
-			}
-			
-			feed.accept_input( n_read );
-		}
-	}
-	
-	template < class Reader >
 	const plus::string* get_line_bare_from_feed( text_input::feed&  feed,
 	                                             Reader             read )
 	{
