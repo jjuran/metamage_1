dc01c45038231ecaf437c7154bf0029c27617d2d
Joshua Juran <jjuran@metamage.com>
2011-08-02 00:40:32 -0700

    v68k:  Add fetchers for MOVE's secondary EA

--- engines/v68k/v68k/effective_address.hh
+++ engines/v68k/v68k/effective_address.hh
@@ -22,6 +22,10 @@ namespace v68k
 	uint32_t fetch_effective_word_address( processor_state& state );
 	uint32_t fetch_effective_long_address( processor_state& state );
 	
+	uint32_t fetch_2nd_effective_byte_address( processor_state& state );
+	uint32_t fetch_2nd_effective_word_address( processor_state& state );
+	uint32_t fetch_2nd_effective_long_address( processor_state& state );
+	
 	uint32_t fetch_byte_from_effective_address( processor_state& state );
 	uint32_t fetch_word_from_effective_address( processor_state& state );
 	uint32_t fetch_long_from_effective_address( processor_state& state );
