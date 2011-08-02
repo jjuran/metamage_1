7382dec2bfa328cd471dd6c111bf3451ba475657
Joshua Juran <jjuran@metamage.com>
2011-08-02 13:35:13 -0700

    v68k:  Add effective address validity predicates

---
+++ engines/v68k/v68k/ea_types.hh
@@ -0,0 +1,75 @@
+/*
+	ea_types.hh
+	-----------
+*/
+
+#ifndef V68K_EATYPES_HH
+#define V68K_EATYPES_HH
+
+// C99
+#include <stdint.h>
+
+
+namespace v68k
+{
+	
+	inline bool ea_is_data_register( uint16_t mode, uint16_t n = 0 )
+	{
+		return mode == 0;
+	}
+	
+	inline bool ea_is_address_register( uint16_t mode, uint16_t n = 0 )
+	{
+		return mode == 1;
+	}
+	
+	inline bool ea_is_postincrement( uint16_t mode, uint16_t n = 0 )
+	{
+		return mode == 3;
+	}
+	
+	inline bool ea_is_predecrement( uint16_t mode, uint16_t n = 0 )
+	{
+		return mode == 4;
+	}
+	
+	
+	inline bool ea_is_valid( uint16_t mode, uint16_t n )
+	{
+		return mode < 7  ||  n <= 4;
+	}
+	
+	inline bool ea_is_register( uint16_t mode, uint16_t n = 0 )
+	{
+		return mode <= 1;
+	}
+	
+	inline bool ea_is_data( uint16_t mode, uint16_t n )
+	{
+		return ea_is_valid( mode, n )  &&  !ea_is_address_register( mode );
+	}
+	
+	inline bool ea_is_memory( uint16_t mode, uint16_t n )
+	{
+		return ea_is_valid( mode, n )  &&  !ea_is_register( mode );
+	}
+	
+	inline bool ea_is_control( uint16_t mode, uint16_t n )
+	{
+		return mode == 2  ||  mode - 5u <= 1  || mode == 7  &&  n < 4;
+	}
+	
+	inline bool ea_is_alterable( uint16_t mode, uint16_t n )
+	{
+		return mode < 7;
+	}
+	
+	inline bool ea_is_data_alterable( uint16_t mode, uint16_t n = 0 )
+	{
+		return mode < 7  &&  mode != 1;
+	}
+	
+}
+
+#endif
+
