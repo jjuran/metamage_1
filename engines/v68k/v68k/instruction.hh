007e0eb227c73caa1b3fe923fa2b51a86c8067bf
Joshua Juran <jjuran@metamage.com>
2011-08-07 22:08:33 -0700

    v68k:  Add load/store instruction flags
    Define instruction flags for specifying the size of stored data, whether it
    also needs to be loaded from the destination as input to the operation, and
    whether the destination is a register vs. memory.
    
    Overload operator|() so we can write loads_and|stores_word_data|in_register
    (for example) and still evaluate as instruction_flags_t.

--- engines/v68k/v68k/instruction.hh
+++ engines/v68k/v68k/instruction.hh
@@ -24,8 +24,20 @@ namespace v68k
 		not_before_68020 = 0x20,
 		not_before_68030 = 0x30,
 		not_before_68040 = 0x40,
-		not_before_mask  = 0x70
+		not_before_mask  = 0x70,
+		
+		loads_and        = 0x0080,
+		stores_byte_data = 0x0100,
+		stores_word_data = 0x0200,
+		stores_long_data = 0x0300,
+		stores_data_mask = 0x0700,
+		in_register      = 0x0800
 	};
+	
+	inline instruction_flags_t operator|( instruction_flags_t a, instruction_flags_t b )
+	{
+		return instruction_flags_t( int( a ) | int( b ) );
+	}
 	
 	struct instruction
 	{
