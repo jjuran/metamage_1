2b6bcc7514be6a77d15c296096f5f2c846dfd8cb
Joshua Juran <jjuran@metamage.com>
2011-10-01 15:47:02 -0700

    pack68k.pl:  Strip trailing comments

--- engines/v68k/utils/pack68k.pl
+++ engines/v68k/utils/pack68k.pl
@@ -7,6 +7,8 @@ while (<>)
 {
 	next if m{^\s*(\#.*)?$};  # skip blanks and comments
 	
+	s{\#.*}{};  # strip trailing comments
+	
 	s{\s}{}g;  # remove whitespace
 	
 	s{ ([0-9A-Fa-f]{2}) }{chr hex $1}gxe;
