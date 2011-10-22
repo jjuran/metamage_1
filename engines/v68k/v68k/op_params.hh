55094004e12c36c939b59ded4de59bb4f8b03db5
Joshua Juran <jjuran@metamage.com>
2011-10-22 02:55:22 -0700

    v68k:  Define op_size_t
    This is either a fixed size (1-3, one greater than common size codes), or a
    recipe for extracting the size code from the instruction opcode.

--- engines/v68k/v68k/op_params.hh
+++ engines/v68k/v68k/op_params.hh
@@ -15,6 +15,23 @@ namespace v68k
 	
 	const int max_params = 4;
 	
+	enum op_size_t
+	{
+		byte_sized = 1,
+		word_sized = 2,
+		long_sized = 3,
+		
+		max_actual_size = long_sized,
+		
+		op_size_in_0040 = 0x0040 | 6 | 1,  // MOVEM, MOVEP
+		op_size_in_00C0 = 0x00C0 | 6 | 0,  // normal
+		op_size_in_0100 = 0x0100 | 8 | 1,  // ADDA/SUBA
+		
+		op_size_shift_mask = 6|8,
+		
+		unsized = 0
+	};
+	
 	struct op_params
 	{
 		uint32_t params[ max_params ];
