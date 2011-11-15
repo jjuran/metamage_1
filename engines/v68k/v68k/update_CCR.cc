613666006a64b1fa49f86e343a96e12d451983aa
Joshua Juran <jjuran@metamage.com>
2011-11-15 08:38:32 -0800

    v68k:  Add update_CCR_BTST()

--- engines/v68k/v68k/update_CCR.cc
+++ engines/v68k/v68k/update_CCR.cc
@@ -5,9 +5,21 @@
 
 #include "v68k/update_CCR.hh"
 
+// v68k
+#include "v68k/op_params.hh"
+#include "v68k/state.hh"
+
 
 namespace v68k
 {
+	
+	static void update_CCR_BTST( processor_state& s, const op_params& pb )
+	{
+		const uint32_t bit = pb.first;
+		
+		s.regs.nzvc &= ~0x4;
+		s.regs.nzvc |= (~pb.second >> bit & 0x1) << 2;
+	}
 	
 	static void spare_CCR( processor_state& s, const op_params& pb )
 	{
@@ -19,7 +31,7 @@ namespace v68k
 		&spare_CCR,
 		&spare_CCR,
 		&spare_CCR,
-		&spare_CCR
+		&update_CCR_BTST
 	};
 	
 }
