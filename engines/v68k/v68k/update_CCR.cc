d5e3230a7d79051a032235581cbcf8c89d78e4ea
Joshua Juran <jjuran@metamage.com>
2011-11-17 23:01:40 -0800

    v68k:  Add update_CCR_{ADD,SUB}()

--- engines/v68k/v68k/update_CCR.cc
+++ engines/v68k/v68k/update_CCR.cc
@@ -6,12 +6,56 @@
 #include "v68k/update_CCR.hh"
 
 // v68k
+#include "v68k/macros.hh"
 #include "v68k/op_params.hh"
 #include "v68k/state.hh"
 
 
 namespace v68k
 {
+	
+	#define N( x )  (!!(x) << 3)
+	#define Z( x )  (!!(x) << 2)
+	#define V( x )  (!!(x) << 1)
+	#define C( x )  (!!(x) << 0)
+	
+	
+	static inline uint8_t common_NZ( int32_t data )
+	{
+		return + N( data <  0 )
+		       | Z( data == 0 );
+	}
+	
+	static inline uint8_t additive_VC( int32_t a, int32_t b, int32_t c )
+	{
+		const bool S = a < 0;
+		const bool D = b < 0;
+		const bool R = c < 0;
+		
+		return + V( (S == D) & (S != R) )
+		       | C( S & D | !R & D | S & !R );
+	}
+	
+	static void update_CCR_ADD( processor_state& s, const op_params& pb )
+	{
+		const int32_t a = sign_extend( pb.first,  pb.size );
+		const int32_t b = sign_extend( pb.second, pb.size );
+		const int32_t c = sign_extend( pb.result, pb.size );
+		
+		s.regs.nzvc = common_NZ( c )
+		            | additive_VC( a, b, c );
+	}
+	
+	static void update_CCR_SUB( processor_state& s, const op_params& pb )
+	{
+		const int32_t a = pb.first;
+		const int32_t b = pb.second;
+		
+		const int32_t d = b - a;
+		
+		s.regs.nzvc = common_NZ( d )
+		            | additive_VC( a, d, b );  // b is the sum
+	}
 	
 	static void update_CCR_BTST( processor_state& s, const op_params& pb )
 	{
@@ -28,8 +72,8 @@ namespace v68k
 	
 	CCR_updater the_CCR_updaters[] =
 	{
-		&spare_CCR,
-		&spare_CCR,
+		&update_CCR_ADD,
+		&update_CCR_SUB,
 		&spare_CCR,
 		&update_CCR_BTST
 	};
