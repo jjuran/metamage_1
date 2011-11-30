b4270f769b91e07ba540302c06953406d076c200
Josh Juran <jjuran@metamage.com>
2011-11-29 22:28:29 -0800

    v68k:  Add SWAP tests

---
+++ engines/v68k/t/v68k-swap/v68k-swap.cc
@@ -0,0 +1,70 @@
+/*
+	v68k-swap.cc
+	------------
+*/
+
+// Standard C
+#include <string.h>
+
+// v68k
+#include "v68k/emulator.hh"
+#include "v68k/endian.hh"
+
+// tap-out
+#include "tap/test.hh"
+
+
+#pragma exceptions off
+
+
+static const unsigned n_tests = 2;
+
+
+using v68k::big_word;
+using v68k::big_longword;
+
+using tap::ok_if;
+
+
+static void swap()
+{
+	using namespace v68k;
+	
+	uint8_t mem[ 4096 ];
+	
+	memset( mem, 0xFF, sizeof mem );  // spike memory with bad addresses
+	
+	uint32_t* vectors = (uint32_t*) mem;
+	
+	vectors[0] = big_longword( 4096 );  // isp
+	vectors[1] = big_longword( 1024 );  // pc
+	
+	uint16_t* code = (uint16_t*) (mem + 1024);
+	
+	code[ 0 ] = big_word( 0x4840 );  // SWAP  D0
+	
+	emulator emu( mc68000, mem, sizeof mem );
+	
+	emu.reset();
+	
+	emu.regs.nzvc = 0xF;
+	
+	emu.regs.d[0] = 0x12345678;
+	
+	
+	emu.step();
+	
+	ok_if( emu.regs.d[0] == 0x56781234 );
+	
+	ok_if( emu.regs.nzvc == 0xF );
+}
+
+int main( int argc, char** argv )
+{
+	tap::start( "v68k-swap", n_tests );
+	
+	swap();
+	
+	return 0;
+}
+
