1847383d44793210bc43e4a9444ea01edef77866
Josh Juran <jjuran@metamage.com>
2011-11-29 22:21:30 -0800

    v68k:  Add EOR and TST tests

---
+++ engines/v68k/t/v68k-eor/v68k-eor.cc
@@ -0,0 +1,72 @@
+/*
+	v68k-eor.cc
+	-----------
+*/
+
+// Standard C
+#include <string.h>
+
+// v68k
+#include "v68k/emulator.hh"
+#include "v68k/endian.hh"
+
+// tap-out
+#include "tap/test.hh"
+
+
+#pragma exceptions off
+
+
+static const unsigned n_tests = 3;
+
+
+using v68k::big_word;
+using v68k::big_longword;
+
+using tap::ok_if;
+
+
+static void eor()
+{
+	using namespace v68k;
+	
+	uint8_t mem[ 4096 ];
+	
+	memset( mem, 0xFF, sizeof mem );  // spike memory with bad addresses
+	
+	uint32_t* vectors = (uint32_t*) mem;
+	
+	vectors[0] = big_longword( 4096 );  // isp
+	vectors[1] = big_longword( 1024 );  // pc
+	
+	uint16_t* code = (uint16_t*) (mem + 1024);
+	
+	code[ 0 ] = big_word( 0xB181 );  // EOR.L  D0,D1
+	
+	emulator emu( mc68000, mem, sizeof mem );
+	
+	emu.reset();
+	
+	emu.regs.nzvc = 0;
+	
+	emu.regs.d[0] = 0x00000001;
+	emu.regs.d[1] = 0x00000002;
+	
+	
+	emu.step();
+	
+	ok_if( emu.regs.nzvc == 0x0 );
+	
+	ok_if( emu.regs.d[0] == 1 );
+	ok_if( emu.regs.d[1] == 3 );
+}
+
+int main( int argc, char** argv )
+{
+	tap::start( "v68k-eor", n_tests );
+	
+	eor();
+	
+	return 0;
+}
+
---
+++ engines/v68k/t/v68k-tst/v68k-tst.cc
@@ -0,0 +1,80 @@
+/*
+	v68k-tst.cc
+	-----------
+*/
+
+// Standard C
+#include <string.h>
+
+// v68k
+#include "v68k/emulator.hh"
+#include "v68k/endian.hh"
+
+// tap-out
+#include "tap/test.hh"
+
+
+#pragma exceptions off
+
+
+static const unsigned n_tests = 3;
+
+
+using v68k::big_word;
+using v68k::big_longword;
+
+using tap::ok_if;
+
+
+static void tst()
+{
+	using namespace v68k;
+	
+	uint8_t mem[ 4096 ];
+	
+	memset( mem, 0xFF, sizeof mem );  // spike memory with bad addresses
+	
+	uint32_t* vectors = (uint32_t*) mem;
+	
+	vectors[0] = big_longword( 4096 );  // isp
+	vectors[1] = big_longword( 1024 );  // pc
+	
+	uint16_t* code = (uint16_t*) (mem + 1024);
+	
+	code[ 0 ] = big_word( 0x4A80 );  // TST.L  D0
+	code[ 1 ] = big_word( 0x4A40 );  // TST.W  D0
+	code[ 2 ] = big_word( 0x4A00 );  // TST.B  D0
+	
+	emulator emu( mc68000, mem, sizeof mem );
+	
+	emu.reset();
+	
+	emu.regs.nzvc = 0;
+	
+	emu.regs.d[0] = 0x0000FF80;
+	
+	
+	emu.step();
+	
+	ok_if( emu.regs.nzvc == 0x0 );
+	
+	
+	emu.step();
+	
+	ok_if( emu.regs.nzvc == 0x8 );
+	
+	
+	emu.step();
+	
+	ok_if( emu.regs.nzvc == 0x8 );
+}
+
+int main( int argc, char** argv )
+{
+	tap::start( "v68k-tst", n_tests );
+	
+	tst();
+	
+	return 0;
+}
+
