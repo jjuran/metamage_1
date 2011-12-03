5e3e762e29f6c38de9706a303ef93fbbc6f5f896
Josh Juran <jjuran@metamage.com>
2011-12-02 22:23:08 -0800

    v68k:  Add DBcc tests

---
+++ engines/v68k/t/v68k-dbcc/v68k-dbcc.cc
@@ -0,0 +1,86 @@
+/*
+	v68k-dbcc.cc
+	------------
+*/
+
+// Standard C
+#include <string.h>
+
+// v68k
+#include "v68k/emulator.hh"
+#include "v68k/endian.hh"
+
+// tap-out
+#include "tap/test.hh"
+
+
+#pragma exceptions off
+
+
+static const unsigned n_tests = 4;
+
+
+using v68k::big_word;
+using v68k::big_longword;
+
+using tap::ok_if;
+
+
+static void dbcc()
+{
+	using namespace v68k;
+	
+	uint8_t mem[ 4096 ];
+	
+	memset( mem, 0xFF, sizeof mem );  // spike memory with bad addresses
+	
+	uint32_t* vectors = (uint32_t*) mem;
+	
+	vectors[0] = big_longword( 4096 );  // isp
+	vectors[1] = big_longword( 1024 );  // pc
+	
+	uint16_t* code = (uint16_t*) (mem + 1024);
+	
+	code[ 0 ] = big_word( 0x50C8 );  // DBT  D0,*+0
+	code[ 1 ] = big_word( 0xFFFE );
+	code[ 2 ] = big_word( 0x51C8 );  // DBF  D0,*+0
+	code[ 3 ] = big_word( 0xFFFE );
+	code[ 4 ] = big_word( 0x7000 );  // MOVEQ #0,D0
+	
+	emulator emu( mc68000, mem, sizeof mem );
+	
+	emu.reset();
+	
+	emu.regs.nzvc = 0xF;
+	
+	emu.regs.d[0] = 0x00010001;
+	
+	
+	emu.step();
+	
+	ok_if( emu.regs.d[0] == 0x00010001 );
+	
+	
+	emu.step();
+	
+	ok_if( emu.regs.d[0] == 0x00010000 );
+	
+	emu.step();
+	
+	ok_if( emu.regs.d[0] == 0x0001FFFF );
+	
+	
+	emu.step();
+	
+	ok_if( emu.regs.d[0] == 0x00000000 );
+}
+
+int main( int argc, char** argv )
+{
+	tap::start( "v68k-dbcc", n_tests );
+	
+	dbcc();
+	
+	return 0;
+}
+
