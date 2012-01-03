929c510bce86feee37dd62a3c978edbd5bf127a3
Josh Juran <jjuran@metamage.com>
2012-01-03 04:16:17 -0800

    Genie:  Make FSTreeCache explicitly non-copyable

--- lamp/Genie/Genie/FS/FSTreeCache.hh
+++ lamp/Genie/Genie/FS/FSTreeCache.hh
@@ -15,11 +15,20 @@ namespace Genie
 	
 	class FSTreeCache
 	{
+		private:
+			// non-copyable
+			FSTreeCache           ( const FSTreeCache& );
+			FSTreeCache& operator=( const FSTreeCache& );
+		
 		protected:
 			// protected destructor prevents slicing
 			~FSTreeCache()  {}
 		
 		public:
+			FSTreeCache()
+			{
+			}
+			
 			typedef const FSNode& const_reference;
 			
 			void push_back( const FSNode& node );
