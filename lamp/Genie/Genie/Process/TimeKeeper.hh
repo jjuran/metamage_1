b7952def55a6b1b3dc39278e153c3799bf88961f
Josh Juran <jjuran@metamage.com>
2012-01-03 09:48:34 -0800

    Genie:  Use uint64_t, not UInt64 in TimeKeeper.hh

--- lamp/Genie/Genie/Process/TimeKeeper.hh
+++ lamp/Genie/Genie/Process/TimeKeeper.hh
@@ -6,10 +6,8 @@
 #ifndef GENIE_PROCESS_TIMEKEEPER_HH
 #define GENIE_PROCESS_TIMEKEEPER_HH
 
-// Mac OS
-#ifndef __MACTYPES__
-#include <MacTypes.h>
-#endif
+// Standard C
+#include <stdint.h>
 
 
 namespace Genie
@@ -17,10 +15,10 @@ namespace Genie
 	
 	struct Times
 	{
-		UInt64 user;
-		UInt64 system;
-		UInt64 child_user;
-		UInt64 child_system;
+		uint64_t user;
+		uint64_t system;
+		uint64_t child_user;
+		uint64_t child_system;
 		
 		Times() : user(), system(), child_user(), child_system()  {}
 	};
@@ -28,18 +26,18 @@ namespace Genie
 	class TimeKeeper
 	{
 		private:
-			UInt64 itsLastTimerCheckpoint;
-			UInt64 itsLastResume;
-			UInt64 itsLastActivity;
+			uint64_t itsLastTimerCheckpoint;
+			uint64_t itsLastResume;
+			uint64_t itsLastActivity;
 			
 			Times itsTimes;
 		
 		public:
 			TimeKeeper();
 			
-			const UInt64& GetTimeOfLastResume() const  { return itsLastResume; }
+			const uint64_t& GetTimeOfLastResume() const  { return itsLastResume; }
 			
-			const UInt64& GetTimeOfLastActivity() const  { return itsLastActivity; }
+			const uint64_t& GetTimeOfLastActivity() const  { return itsLastActivity; }
 			
 			const Times& GetTimes() const  { return itsTimes; }
 			
