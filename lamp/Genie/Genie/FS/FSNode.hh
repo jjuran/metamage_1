cde27b482605c692452051f43b07f732ef800c38
Josh Juran <jjuran@metamage.com>
2012-01-07 21:59:51 -0800

    Genie:  Replace FSNode with vfs::dir_entry

--- lamp/Genie/Genie/FS/FSNode.hh
+++ lamp/Genie/Genie/FS/FSNode.hh
@@ -6,31 +6,14 @@
 #ifndef GENIE_FS_FSNODE_HH
 #define GENIE_FS_FSNODE_HH
 
-// POSIX
-#include <sys/types.h>
-
-// plus
-#include "plus/string.hh"
+// vfs
+#include "vfs/dir_entry.hh"
 
 
 namespace Genie
 {
 	
-	struct FSNode
-	{
-		ino_t         inode;
-		plus::string  name;
-		
-		FSNode()
-		{
-		}
-		
-		FSNode( ino_t i, const plus::string& n ) : inode( i ), name( n )
-		{
-		}
-		
-		~FSNode();
-	};
+	typedef vfs::dir_entry FSNode;
 	
 }
 
