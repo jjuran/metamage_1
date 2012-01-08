01659258b28668d42138358e98353f527b258e02
Josh Juran <jjuran@metamage.com>
2012-01-08 03:27:54 -0800

    Genie:  Replace FSTreeCache with vfs::dir_contents

--- lamp/Genie/Genie/FS/FSTreeCache.hh
+++ lamp/Genie/Genie/FS/FSTreeCache.hh
@@ -6,8 +6,8 @@
 #ifndef GENIE_FS_FSTREECACHE_HH
 #define GENIE_FS_FSTREECACHE_HH
 
-// plus
-#include "plus/ref_count.hh"
+// vfs
+#include "vfs/dir_contents.hh"
 
 // Genie
 #include "Genie/FS/FSNode.hh"
@@ -16,26 +16,7 @@
 namespace Genie
 {
 	
-	class FSTreeCache : public plus::ref_count< FSTreeCache >
-	{
-		private:
-			// non-copyable
-			FSTreeCache           ( const FSTreeCache& );
-			FSTreeCache& operator=( const FSTreeCache& );
-		
-		public:
-			FSTreeCache()
-			{
-			}
-			
-			virtual ~FSTreeCache()
-			{
-			}
-			
-			typedef const FSNode& const_reference;
-			
-			void push_back( const FSNode& node );
-	};
+	typedef vfs::dir_contents FSTreeCache;
 	
 }
 
