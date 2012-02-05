a0828578bfcf0da4b9e996d0c77edb1621fd020a
Josh Juran <jjuran@metamage.com>
2012-02-05 05:17:16 -0800

    Genie:  Use readlink method for SymbolicLink

--- lamp/Genie/Genie/FS/SymbolicLink.cc
+++ lamp/Genie/Genie/FS/SymbolicLink.cc
@@ -5,22 +5,44 @@
 
 #include "Genie/FS/SymbolicLink.hh"
 
+// Genie
+#include "Genie/FS/node_method_set.hh"
+
 
 namespace Genie
 {
+	
+	static plus::string readlink( const FSTree* node )
+	{
+		const FSTree_SymbolicLink* file = static_cast< const FSTree_SymbolicLink* >( node );
+		
+		return file->Target();
+	}
+	
+	static const node_method_set methods =
+	{
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		&readlink
+	};
 	
 	FSTree_SymbolicLink::FSTree_SymbolicLink( const FSTreePtr&     parent,
 	                                          const plus::string&  name,
 	                                          const plus::string&  target )
 	:
-		FSTree_ReadableSymLink( parent, name ),
+		FSTree( parent, name, S_IFLNK | 0777, &methods ),
 		itsTarget( target )
 	{
-	}
-	
-	plus::string FSTree_SymbolicLink::ReadLink() const
-	{
-		return itsTarget;
 	}
 	
 	FSTreePtr New_FSTree_SymbolicLink( const FSTreePtr&     parent,
