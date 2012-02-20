2110537bcde86ff2508b4f879698a8bc4e87e64c
Josh Juran <jjuran@metamage.com>
2012-02-20 10:12:15 -0800

    Genie:  Add dir_method_set

---
+++ lamp/Genie/Genie/FS/dir_method_set.hh
@@ -0,0 +1,47 @@
+/*
+	dir_method_set.hh
+	-----------------
+*/
+
+#ifndef GENIE_FS_DIRMETHODSET_HH
+#define GENIE_FS_DIRMETHODSET_HH
+
+// POSIX
+#include <sys/types.h>
+
+// plus
+#include "plus/string.hh"
+
+// Genie
+#include "Genie/FS/FSTreeCache.hh"
+#include "Genie/FS/FSTreePtr.hh"
+#include "Genie/IO/IOPtr.hh"
+
+
+namespace Genie
+{
+	
+	typedef FSTreePtr (*lookup_method)( const FSTree*, const plus::string&, const FSTree* );
+	
+	typedef void (*listdir_method)( const FSTree* node, FSTreeCache& cache );
+	
+	typedef void (*mkdir_method)( const FSTree* node, mode_t mode );
+	
+	typedef IOPtr (*opendir_method)( const FSTree* node );
+	
+	typedef IOPtr (*chdir_method)( const FSTree* node );
+	
+	
+	struct dir_method_set
+	{
+		lookup_method   lookup;
+		listdir_method  listdir;
+		mkdir_method    mkdir;
+		opendir_method  opendir;
+		chdir_method    chdir;
+	};
+	
+}
+
+#endif
+
