ab2b12f035c6ac7e5cb3533ddb987003280f13fb
Josh Juran <jjuran@metamage.com>
2012-03-04 03:02:03 -0800

    [NEW] Debugger

---
+++ tools/mac/Debugger/Debugger.c
@@ -0,0 +1,23 @@
+/*
+	Debugger.c
+	----------
+*/
+
+// Mac OS X
+#ifdef __APPLE__
+#include <CoreServices/CoreServices.h>
+#endif
+
+// Mac OS
+#ifndef __MACTYPES__
+#include <MacTypes.h>
+#endif
+
+
+int main( void )
+{
+	Debugger();
+	
+	return 0;
+}
+
