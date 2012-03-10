8467d0f130af66f8684bcfae1efac5458fad4d10
Josh Juran <jjuran@metamage.com>
2012-03-10 05:15:42 -0800

    v68k-callbacks:  Add illegal_instruction

--- engines/v68k/libs/v68k-callbacks/callback/bridge.cc
+++ engines/v68k/libs/v68k-callbacks/callback/bridge.cc
@@ -5,7 +5,11 @@
 
 #include "callback/bridge.hh"
 
+// POSIX
+#include <unistd.h>
+
 // Standard C
+#include <signal.h>
 #include <stdlib.h>
 
 
@@ -32,9 +36,26 @@ static uint32_t unimplemented_callback( v68k::emulator& emu )
 }
 
 
+#define ERR_MSG( msg )  "v68k: exception: " msg "\n"
+
+#define STR_LEN( s )  "" s, (sizeof s - 1)
+
+#define WRITE_ERR( msg )  write( STDERR_FILENO, STR_LEN( ERR_MSG( msg ) ) )
+
+static uint32_t illegal_instruction_callback( v68k::emulator& emu )
+{
+	WRITE_ERR( "Illegal Instruction" );
+	
+	raise( SIGILL );
+	
+	return nil;
+}
+
+
 static const function_type the_callbacks[] =
 {
 	&unimplemented_callback,
+	&illegal_instruction_callback,
 	NULL
 };
 
--- engines/v68k/libs/v68k-callbacks/callback/bridge.hh
+++ engines/v68k/libs/v68k-callbacks/callback/bridge.hh
@@ -16,6 +16,7 @@ namespace callback {
 enum
 {
 	unimplemented,
+	illegal_instruction,
 	n
 };
 
