bf0fa8bc3e4e9688a0d5668bb0b7a4a1493595c9
Josh Juran <jjuran@metamage.com>
2012-03-12 01:05:51 -0700

    pause:  Make C-compatible
    Also, fix argv type declaration.

--- lamp/jTools/pause/pause.cc
+++ lamp/jTools/pause/pause.cc
@@ -10,13 +10,13 @@
 #include "iota/strings.hh"
 
 
-int main( int argc, char const *const argv[] )
+int main( int argc, char** argv )
 {
-	(void) write( STDOUT_FILENO, STR_LEN( "Hit return to continue\n" ) );
+	ssize_t bytes = write( STDOUT_FILENO, STR_LEN( "Hit return to continue\n" ) );
 	
 	char c;
 	
-	ssize_t bytes = read( STDIN_FILENO, &c, sizeof c );
+	bytes = read( STDIN_FILENO, &c, sizeof c );
 	
 	return bytes ? 0 : 1;
 }
