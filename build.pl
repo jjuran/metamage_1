d99cb50acded2708f6b3a293ba3a3ed603ed097f
Josh Juran <jjuran@metamage.com>
2012-03-24 00:17:37 -0700

    Compile::Driver:  Move main() to Compile::Driver

--- build.pl
+++ build.pl
@@ -5,35 +5,10 @@ use FindBin '$RealBin';
 use lib "$RealBin/tools/perl";
 
 use Compile::Driver;
-use Compile::Driver::Configuration;
-use Compile::Driver::Jobs;
-use Compile::Driver::Module;
-use Compile::Driver::Options;
 
 use warnings;
 use strict;
 
-my @args = Compile::Driver::Options::set_options( @ARGV );
 
-my $configuration = Compile::Driver::Configuration::->new( Compile::Driver::Options::specs );
-
-foreach my $name ( @args )
-{
-	my $module = $configuration->get_module( $name, "[mandatory]" );
-}
-
-my $desc = { NAME => "[program args]", DATA => { use => [ @args ] } };
-
-my $module = Compile::Driver::Module::->new( $configuration, $desc );
-
-my @prereqs = @{ $module->recursive_prerequisites };
-
-pop @prereqs;
-
-my @jobs = map { Compile::Driver::jobs_for( $configuration->get_module( $_ ) ) } @prereqs;
-
-foreach my $job ( @jobs )
-{
-	Compile::Driver::Jobs::do_job( $job );
-}
+Compile::Driver::main( @ARGV );
 
--- tools/perl/Compile/Driver.pm
+++ tools/perl/Compile/Driver.pm
@@ -1,6 +1,9 @@
 package Compile::Driver;
 
+use Compile::Driver::Configuration;
 use Compile::Driver::Jobs;
+use Compile::Driver::Module;
+use Compile::Driver::Options;
 
 use warnings;
 use strict;
@@ -64,6 +67,34 @@ sub jobs_for
 	push @jobs, $link;
 	
 	return @jobs;
+}
+
+sub main
+{
+	my @args = Compile::Driver::Options::set_options( @_ );
+	
+	my $configuration = Compile::Driver::Configuration::->new( Compile::Driver::Options::specs );
+	
+	foreach my $name ( @args )
+	{
+		my $module = $configuration->get_module( $name, "[mandatory]" );
+	}
+	
+	my $desc = { NAME => "[program args]", DATA => { use => [ @args ] } };
+	
+	my $module = Compile::Driver::Module::->new( $configuration, $desc );
+	
+	my @prereqs = @{ $module->recursive_prerequisites };
+	
+	pop @prereqs;
+	
+	my @jobs = map { Compile::Driver::jobs_for( $configuration->get_module( $_ ) ) } @prereqs;
+	
+	foreach my $job ( @jobs )
+	{
+		Compile::Driver::Jobs::do_job( $job );
+	}
+	
 }
 
 1;
