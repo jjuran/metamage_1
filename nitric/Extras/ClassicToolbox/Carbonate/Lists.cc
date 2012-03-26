af24946add2fc9266bda870504389d0ea7387107
Josh Juran <jjuran@metamage.com>
2012-03-26 02:41:18 -0700

    Carbonate:  Fix error compiling Lists.cc as Carbon

--- nitric/Extras/ClassicToolbox/Carbonate/Lists.cc
+++ nitric/Extras/ClassicToolbox/Carbonate/Lists.cc
@@ -1,7 +1,11 @@
 // Carbonate/Lists.cc
 
 
+#ifndef __LISTS__
 #include <Lists.h>
+#endif
+
+#if !TARGET_API_MAC_CARBON
 
 #if ACCESSOR_CALLS_ARE_FUNCTIONS
 // Compile the Carbon accessors as extern pascal functions.
@@ -10,4 +14,6 @@
 #endif
 
 // These functions are always declared in the headers and are always extern.
+
+#endif
 
