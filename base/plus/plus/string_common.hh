88e632d06a7f651e30cfa5e5410f0e412fd04be3
Josh Juran <jjuran@metamage.com>
2012-04-01 04:52:02 -0700

    plus:  Add string_check_size()

---
+++ base/plus/plus/string_common.hh
@@ -0,0 +1,18 @@
+/*
+	string_common.hh
+	----------------
+*/
+
+#ifndef PLUS_STRINGCOMMON_HH
+#define PLUS_STRINGCOMMON_HH
+
+
+namespace plus
+{
+	
+	void string_check_size( long size );
+	
+}
+
+#endif
+
