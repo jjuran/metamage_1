271eddd2e5196a96e02d2e7eb9abba68e6328b84
Josh Juran <jjuran@metamage.com>
2012-04-09 01:22:22 -0700

    Genie:  Move FSTree_Union out of header

--- lamp/Genie/Genie/FS/Union.cc
+++ lamp/Genie/Genie/FS/Union.cc
@@ -30,6 +30,31 @@ namespace Genie
 	namespace p7 = poseven;
 	
 	
+	class FSTree_Union : public FSTree
+	{
+		private:
+			FSTreePtr itsTop;
+			FSTreePtr itsBottom;
+		
+		public:
+			FSTree_Union( const FSTreePtr&     parent,
+			              const plus::string&  name );
+			
+			void SetTop( const FSTreePtr& top )
+			{
+				itsTop = top;
+			}
+			
+			void SetBottom( const FSTreePtr& bottom )
+			{
+				itsBottom = bottom;
+			}
+			
+			FSTreePtr Lookup_Child( const plus::string& name, const FSTree* parent ) const;
+			
+			void IterateIntoCache( FSTreeCache& cache ) const;
+	};
+	
 	static FSTreePtr union_lookup( const FSTree*        node,
 	                               const plus::string&  name,
 	                               const FSTree*        parent )
--- lamp/Genie/Genie/FS/Union.hh
+++ lamp/Genie/Genie/FS/Union.hh
@@ -15,31 +15,6 @@
 namespace Genie
 {
 	
-	class FSTree_Union : public FSTree
-	{
-		private:
-			FSTreePtr itsTop;
-			FSTreePtr itsBottom;
-		
-		public:
-			FSTree_Union( const FSTreePtr&     parent,
-			              const plus::string&  name );
-			
-			void SetTop( const FSTreePtr& top )
-			{
-				itsTop = top;
-			}
-			
-			void SetBottom( const FSTreePtr& bottom )
-			{
-				itsBottom = bottom;
-			}
-			
-			FSTreePtr Lookup_Child( const plus::string& name, const FSTree* parent ) const;
-			
-			void IterateIntoCache( FSTreeCache& cache ) const;
-	};
-	
 	FSTreePtr New_FSTree_Union( const FSTreePtr&     parent,
 	                            const plus::string&  name,
 	                            const FSTreePtr&     top,
