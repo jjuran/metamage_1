fd839bbd09512b739905753fd33f7e8f82ea784b
Josh Juran <jjuran@metamage.com>
2012-04-11 04:07:51 -0700

    Genie:  Remove obsolete FSTreeCache_Impl.hh

--- lamp/Genie/Genie/FS/Iterate.cc
+++ lamp/Genie/Genie/FS/Iterate.cc
@@ -6,11 +6,10 @@
 #include "Genie/FS/Iterate.hh"
 
 // vfs
-#include "vfs/dir_contents.hh"
+#include "vfs/dir_contents_impl.hh"
 
 // Genie
 #include "Genie/FS/FSTree.hh"
-#include "Genie/FS/FSTreeCache_Impl.hh"
 #include "Genie/FS/inode.hh"
 #include "Genie/FS/listdir.hh"
 #include "Genie/FS/parent_inode.hh"
@@ -32,7 +31,7 @@ namespace Genie
 			
 			vfs::dir_entry Get() const
 			{
-				const FSTreeCache_Impl* impl = static_cast< const FSTreeCache_Impl* >( contents.get() );
+				const vfs::dir_contents_impl* impl = static_cast< const vfs::dir_contents_impl* >( contents.get() );
 				
 				return nextOffset < impl->size() ? impl->at( nextOffset )
 				                                 : vfs::dir_entry();
@@ -54,14 +53,14 @@ namespace Genie
 	
 	FSIteratorPtr Iterate( const FSTreePtr& dir )
 	{
-		FSTreeCache_Impl cache;
+		vfs::dir_contents_impl cache;
 		
 		cache.push_back( vfs::dir_entry( inode       ( dir.get() ), "."  ) );
 		cache.push_back( vfs::dir_entry( parent_inode( dir.get() ), ".." ) );
 		
 		listdir( dir.get(), cache );
 		
-		FSTreeCache_Impl* newCache = new FSTreeCache_Impl();
+		vfs::dir_contents_impl* newCache = new vfs::dir_contents_impl();
 		
 		FSTreeCachePtr cachePtr( newCache );
 		
