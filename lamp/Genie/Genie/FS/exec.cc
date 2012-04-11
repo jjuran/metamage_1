d2a6f54d461050d36348d1f622dd6b79eb4f1f7b
Josh Juran <jjuran@metamage.com>
2012-04-10 19:31:52 -0700

    Genie:  Add exec() for nodes

---
+++ lamp/Genie/Genie/FS/exec.cc
@@ -0,0 +1,41 @@
+/*
+	exec.cc
+	-------
+*/
+
+#include "Genie/FS/exec.hh"
+
+// poseven
+#include "poseven/types/errno_t.hh"
+
+// Genie
+#include "Genie/FS/FSTree.hh"
+#include "Genie/FS/file_method_set.hh"
+#include "Genie/FS/node_method_set.hh"
+
+
+namespace Genie
+{
+	
+	namespace p7 = poseven;
+	
+	
+	shared_exec_handle exec( const FSTree* node )
+	{
+		const node_method_set* methods = node->methods();
+		
+		const file_method_set* file_methods;
+		
+		if ( methods  &&  (file_methods = methods->file_methods) )
+		{
+			if ( file_methods->loadexec )
+			{
+				return file_methods->loadexec( node );
+			}
+		}
+		
+		throw p7::errno_t( ENOEXEC );
+	}
+	
+}
+
---
+++ lamp/Genie/Genie/FS/exec.hh
@@ -0,0 +1,18 @@
+/*
+	exec.hh
+	-------
+*/
+
+// Genie
+#include "Genie/code/shared_exec_handle.hh"
+
+
+namespace Genie
+{
+	
+	class FSTree;
+	
+	shared_exec_handle exec( const FSTree* node );
+	
+}
+
