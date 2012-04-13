e3ed39e3bf9b67d26b4872c5546f545cb23869aa
Josh Juran <jjuran@metamage.com>
2012-04-13 06:40:28 -0700

    Genie:  Simplify append_hex_encoded_byte()

--- lamp/Genie/Genie/FS/append_hex_encoded_byte.cc
+++ lamp/Genie/Genie/FS/append_hex_encoded_byte.cc
@@ -17,12 +17,8 @@ namespace Genie
 	
 	void append_hex_encoded_byte( plus::var_string& s, unsigned char c )
 	{
-		const plus::string::size_type size = s.size();
-		
-		s.resize( size + 2 * sizeof c );
-		
-		s[ size     ] = gear::encoded_hex_char( c >> 4 );
-		s[ size + 1 ] = gear::encoded_hex_char( c >> 0 );
+		s += gear::encoded_hex_char( c >> 4 );
+		s += gear::encoded_hex_char( c >> 0 );
 	}
 	
 }
