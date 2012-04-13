d96889c6c677c9e389e99b718970de806e5b1bf5
Josh Juran <jjuran@metamage.com>
2012-04-13 01:49:40 -0700

    Move file_method_set.hh from Genie to vfs

---
+++ posix/vfs/vfs/file_method_set.hh
@@ -0,0 +1,35 @@
+/*
+	file_method_set.hh
+	------------------
+*/
+
+#ifndef VFS_FILEMETHODSET_HH
+#define VFS_FILEMETHODSET_HH
+
+// vfs
+#include "vfs/node_fwd.hh"
+#include "vfs/program_ptr.hh"
+
+
+namespace vfs
+{
+	
+	typedef void (*attach_method  )( const node*, const node* );
+	typedef void (*copyfile_method)( const node*, const node* );
+	typedef void (*hardlink_method)( const node*, const node* );
+	
+	typedef program_ptr (*loadexec_method)( const node* );
+	
+	
+	struct file_method_set
+	{
+		attach_method    attach;
+		copyfile_method  copyfile;
+		hardlink_method  hardlink;
+		loadexec_method  loadexec;
+	};
+	
+}
+
+#endif
+
