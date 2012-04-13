cb07cfce095dd71a49c0e49469478f5456483740
Josh Juran <jjuran@metamage.com>
2012-04-13 01:50:59 -0700

    Move link_method_set.hh from Genie to vfs

---
+++ posix/vfs/vfs/link_method_set.hh
@@ -0,0 +1,36 @@
+/*
+	link_method_set.hh
+	------------------
+*/
+
+#ifndef VFS_LINKMETHODSET_HH
+#define VFS_LINKMETHODSET_HH
+
+// plus
+#include "plus/string.hh"
+
+// vfs
+#include "vfs/node_ptr.hh"
+
+
+namespace vfs
+{
+	
+	typedef plus::string (*readlink_method)( const node* );
+	
+	typedef node_ptr (*resolve_method)( const node* );
+	
+	typedef void (*symlink_method)( const node*, const plus::string& );
+	
+	
+	struct link_method_set
+	{
+		readlink_method readlink;
+		resolve_method  resolve;
+		symlink_method  symlink;
+	};
+	
+}
+
+#endif
+
