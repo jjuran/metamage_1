0d2c7f3923d1ff2044d36ce0acede1b4ff858b21
Josh Juran <jjuran@metamage.com>
2012-04-13 02:06:44 -0700

    vfs:  Add symbolic_link node

---
+++ posix/vfs/vfs/nodes/symbolic_link.cc
@@ -0,0 +1,98 @@
+/*
+	symbolic_link.cc
+	----------------
+*/
+
+#include "vfs/nodes/symbolic_link.hh"
+
+// POSIX
+#include <sys/stat.h>
+
+// poseven
+#include "poseven/types/errno_t.hh"
+
+// vfs
+#include "vfs/node.hh"
+#include "vfs/methods/link_method_set.hh"
+
+
+namespace vfs
+{
+	
+	namespace p7 = poseven;
+	
+	
+	struct symlink_extra
+	{
+		plus::datum_storage  target;
+		remove_method        remove;
+	};
+	
+	static void remove( const node* that )
+	{
+		symlink_extra& extra = *(symlink_extra*) that->extra();
+		
+		if ( extra.remove )
+		{
+			extra.remove( that );
+		}
+		else
+		{
+			p7::throw_errno( EPERM );
+		}
+	}
+	
+	static plus::string readlink( const node* that )
+	{
+		symlink_extra& extra = *(symlink_extra*) that->extra();
+		
+		return reinterpret_cast< const plus::string& >( extra.target );
+	}
+	
+	static const link_method_set link_methods =
+	{
+		&readlink
+	};
+	
+	static const node_method_set methods =
+	{
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		&remove,
+		NULL,
+		NULL,
+		&link_methods
+	};
+	
+	static void dispose_symlink( const node* that )
+	{
+		symlink_extra& extra = *(symlink_extra*) that->extra();
+		
+		plus::destroy( extra.target );
+	}
+	
+	node_ptr new_symbolic_link( const node*          parent,
+	                            const plus::string&  name,
+	                            plus::string         target,
+	                            remove_method        remove )
+	{
+		node* result = new node( parent,
+		                         name,
+		                         S_IFLNK | 0777,
+		                         &methods,
+		                         sizeof (symlink_extra),
+		                         &dispose_symlink );
+		
+		symlink_extra& extra = *(symlink_extra*) result->extra();
+		
+		plus::construct_from_move( extra.target, target.move() );
+		
+		extra.remove = remove;
+		
+		return result;
+	}
+	
+}
+
---
+++ posix/vfs/vfs/nodes/symbolic_link.hh
@@ -0,0 +1,28 @@
+/*
+	symbolic_link.hh
+	----------------
+*/
+
+#ifndef VFS_NODES_SYMBOLICLINK_HH
+#define VFS_NODES_SYMBOLICLINK_HH
+
+// plus
+#include "plus/string.hh"
+
+// vfs
+#include "vfs/node_ptr.hh"
+#include "vfs/methods/node_method_set.hh"
+
+
+namespace vfs
+{
+	
+	node_ptr new_symbolic_link( const node*          parent,
+	                            const plus::string&  name,
+	                            plus::string         target,
+	                            remove_method        remove = NULL );
+	
+}
+
+#endif
+
