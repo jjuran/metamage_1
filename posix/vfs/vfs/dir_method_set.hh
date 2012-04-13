f0d712b19fd1fa7939460a3ed04c0a7cde3b2b5e
Josh Juran <jjuran@metamage.com>
2012-04-13 01:48:23 -0700

    Move dir_method_set.hh from Genie to vfs

---
+++ posix/vfs/vfs/dir_method_set.hh
@@ -0,0 +1,47 @@
+/*
+	dir_method_set.hh
+	-----------------
+*/
+
+#ifndef VFS_DIRMETHODSET_HH
+#define VFS_DIRMETHODSET_HH
+
+// POSIX
+#include <sys/types.h>
+
+// plus
+#include "plus/string.hh"
+
+// vfs
+#include "vfs/dir_contents_fwd.hh"
+#include "vfs/handle_ptr.hh"
+#include "vfs/node_ptr.hh"
+
+
+namespace vfs
+{
+	
+	typedef node_ptr (*lookup_method)( const node*, const plus::string&, const node* );
+	
+	typedef void (*listdir_method)( const node* it, vfs::dir_contents& cache );
+	
+	typedef void (*mkdir_method)( const node* it, mode_t mode );
+	
+	typedef handle_ptr (*opendir_method)( const node* it );
+	
+	typedef handle_ptr (*chdir_method)( const node* it );
+	
+	
+	struct dir_method_set
+	{
+		lookup_method   lookup;
+		listdir_method  listdir;
+		mkdir_method    mkdir;
+		opendir_method  opendir;
+		chdir_method    chdir;
+	};
+	
+}
+
+#endif
+
