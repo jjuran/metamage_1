a38ba92906ca4087f090e7cba98bcb60298a6161
Josh Juran <jjuran@metamage.com>
2012-04-13 01:42:03 -0700

    Move node_method_set.hh from Genie to vfs

--- lamp/Genie/Genie/FS/node_method_set.hh
+++ lamp/Genie/Genie/FS/node_method_set.hh
@@ -6,58 +6,9 @@
 #ifndef GENIE_FS_NODEMETHODSET_HH
 #define GENIE_FS_NODEMETHODSET_HH
 
-// POSIX
-#include <sys/types.h>
+// vfs
+#include "vfs/node_method_set.hh"
 
-
-// #include <time.h>
-struct timespec;
-
-// #include <sys/stat.h>
-struct stat;
-
-
-namespace vfs
-{
-	
-	class node;
-	
-	
-	typedef void (*stat_method)( const node*, struct ::stat& );
-	
-	typedef void (*chmod_method)( const node*, mode_t );
-	
-	typedef void (*touch_method)( const node* );
-	typedef void (*utime_method)( const node*, const struct timespec times[2] );
-	
-	typedef void (*remove_method)( const node* );
-	
-	typedef void (*rename_method)( const node*, const node* );
-	
-	struct data_method_set;
-	struct link_method_set;
-	struct dir_method_set;
-	struct file_method_set;
-	struct misc_method_set;
-	
-	
-	struct node_method_set
-	{
-		stat_method     stat;
-		chmod_method    chmod;
-		touch_method    touch;
-		utime_method    utime;
-		remove_method   remove;
-		rename_method   rename;
-		
-		const data_method_set*  data_methods;
-		const link_method_set*  link_methods;
-		const dir_method_set*   dir_methods;
-		const file_method_set*  file_methods;
-		const misc_method_set*  misc_methods;
-	};
-	
-}
 
 namespace Genie
 {
