6f5a0d1fd56e6e7818738654917d441a27b48c31
Josh Juran <jjuran@metamage.com>
2012-04-13 01:52:11 -0700

    Move misc_method_set.hh from Genie to vfs

--- lamp/Genie/Genie/FS/misc_method_set.hh
+++ lamp/Genie/Genie/FS/misc_method_set.hh
@@ -6,29 +6,9 @@
 #ifndef GENIE_FS_MISCMETHODSET_HH
 #define GENIE_FS_MISCMETHODSET_HH
 
-// POSIX
-#include <sys/types.h>
-
 // vfs
-#include "vfs/node_ptr.hh"
+#include "vfs/misc_method_set.hh"
 
-
-namespace vfs
-{
-	
-	typedef node_ptr (*parent_method)( const node* );
-	
-	typedef ino_t (*inode_method)( const node* );
-	
-	
-	struct misc_method_set
-	{
-		parent_method  parent;
-		inode_method   parent_inode;
-		inode_method   inode;
-	};
-	
-}
 
 namespace Genie
 {
---
+++ posix/vfs/vfs/misc_method_set.hh
@@ -0,0 +1,34 @@
+/*
+	misc_method_set.hh
+	------------------
+*/
+
+#ifndef VFS_MISCMETHODSET_HH
+#define VFS_MISCMETHODSET_HH
+
+// POSIX
+#include <sys/types.h>
+
+// vfs
+#include "vfs/node_ptr.hh"
+
+
+namespace vfs
+{
+	
+	typedef node_ptr (*parent_method)( const node* );
+	
+	typedef ino_t (*inode_method)( const node* );
+	
+	
+	struct misc_method_set
+	{
+		parent_method  parent;
+		inode_method   parent_inode;
+		inode_method   inode;
+	};
+	
+}
+
+#endif
+
