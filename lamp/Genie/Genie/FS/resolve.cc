ca6bae03e1c541189be4009c0fc97f48e2a4b253
Josh Juran <jjuran@metamage.com>
2012-04-14 01:18:47 -0700

    Genie:  Move resolve() into vfs namespace

--- lamp/Genie/Genie/FS/resolve.cc
+++ lamp/Genie/Genie/FS/resolve.cc
@@ -5,17 +5,23 @@
 
 #include "Genie/FS/resolve.hh"
 
+// vfs
+#include "vfs/node.hh"
+#include "vfs/methods/link_method_set.hh"
+#include "vfs/methods/node_method_set.hh"
+#include "vfs/node_ptr.hh"
+
 // Genie
-#include "Genie/FS/FSTree.hh"
 #include "Genie/FS/ResolvePathname.hh"
-#include "Genie/FS/link_method_set.hh"
-#include "Genie/FS/node_method_set.hh"
 
 
-namespace Genie
+namespace vfs
 {
 	
-	FSTreePtr resolve( const FSTree* it )
+	using Genie::ResolvePathname;
+	
+	
+	node_ptr resolve( const node* it )
 	{
 		const node_method_set* methods = it->methods();
 		
