2a9da0aaedc02eb55c35eea655257588aa2b0cda
Josh Juran <jjuran@metamage.com>
2012-04-14 01:10:16 -0700

    vfs:  Add declaration of resolve() primitive
    We can't supply the definition yet, because it requires resolve_pathname(),
    which calls resolve_links_in_place(), which depends on resolve().

---
+++ posix/vfs/vfs/primitives/resolve.hh
@@ -0,0 +1,21 @@
+/*
+	resolve.hh
+	----------
+*/
+
+#ifndef VFS_PRIMITIVES_RESOLVE
+#define VFS_PRIMITIVES_RESOLVE
+
+// vfs
+#include "vfs/node_ptr.hh"
+
+
+namespace vfs
+{
+	
+	node_ptr resolve( const node* it );
+	
+}
+
+#endif
+
