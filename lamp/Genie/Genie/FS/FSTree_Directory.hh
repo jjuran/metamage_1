9a85747e7958b6b384c3987cfd91f80d1f32a050
Josh Juran <jjuran@metamage.com>
2012-04-18 00:33:53 -0700

    Genie:  Rewrite FSTree_Directory via vfs fixed_dir

--- lamp/Genie/Genie/FS/FSTree_Directory.hh
+++ lamp/Genie/Genie/FS/FSTree_Directory.hh
@@ -6,8 +6,8 @@
 #ifndef GENIE_FILESYSTEM_FSTREE_DIRECTORY_HH
 #define GENIE_FILESYSTEM_FSTREE_DIRECTORY_HH
 
-// plus
-#include "plus/string.hh"
+// vfs
+#include "vfs/nodes/fixed_dir.hh"
 
 // Genie
 #include "Genie/FS/FSTreePtr.hh"
@@ -19,20 +19,11 @@ namespace Genie
 	namespace premapped
 	{
 		
-		typedef void (*destructor)( const FSTree* );
+		typedef vfs::node_destructor  destructor;
+		typedef vfs::node_factory     function;
+		typedef vfs::fixed_mapping    mapping;
 		
-		typedef FSTreePtr (*function)( const FSTree*,
-		                               const plus::string&,
-		                               const void* );
-		
-		struct mapping
-		{
-			const char*  name;
-			function     f;
-			const void*  args;
-		};
-		
-		extern const mapping empty_mappings[];
+		using vfs::empty_mappings;
 		
 	}
 	
