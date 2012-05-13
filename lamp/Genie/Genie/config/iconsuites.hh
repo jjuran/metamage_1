2c9e058d9f2f8f45bd95754193e935f76266e8ac
Josh Juran <jjuran@metamage.com>
2012-05-13 03:51:31 -0700

    Genie:  Add CONFIG_ICONSUITES configuration switch

---
+++ lamp/Genie/Genie/config/iconsuites.hh
@@ -0,0 +1,18 @@
+/*
+	config/iconsuites.hh
+	--------------------
+*/
+
+#ifndef GENIE_CONFIG_ICONSUITES_HH
+#define GENIE_CONFIG_ICONSUITES_HH
+
+// Genie
+#include "Genie/config/color.hh"
+
+
+#ifndef CONFIG_ICONSUITES
+#define CONFIG_ICONSUITES  CONFIG_COLOR
+#endif
+
+#endif
+
