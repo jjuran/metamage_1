d60b21694a89cf84456c6529ab1e2e9019a1a259
Josh Juran <jjuran@metamage.com>
2012-07-18 23:15:25 -0700

    must:  Add must_pipe()

---
+++ posix/must/must/pipe.h
@@ -0,0 +1,24 @@
+/*
+	must/pipe.h
+	-----------
+*/
+
+#ifndef MUST_PIPE_H
+#define MUST_PIPE_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+int must_pipe( int fds[2] );
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
+
