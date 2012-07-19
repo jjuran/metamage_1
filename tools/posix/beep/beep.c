91056ff2e311e6da68991963261b82b043fb2460
Josh Juran <jjuran@metamage.com>
2012-07-18 21:22:05 -0700

    beep:  Use must_write() to silence warning

--- tools/posix/beep/beep.c
+++ tools/posix/beep/beep.c
@@ -3,15 +3,20 @@
 	------
 */
 
-// POSIX
-#include <unistd.h>
+// must
+#include "must/write.h"
+
+
+#ifndef STDOUT_FILENO
+#define STDOUT_FILENO 1
+#endif
 
 
 int main( void )
 {
 	const char c = 0x07;
 	
-	write( STDOUT_FILENO, &c, sizeof c );
+	must_write( STDOUT_FILENO, &c, sizeof c );
 	
 	return 0;
 }
