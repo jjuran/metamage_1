bebb91b96d1a9ad467d1da0967ab7d5e811e0c8f
Josh Juran <jjuran@metamage.com>
2012-08-03 10:43:37 -0700

    relix:  Add thread synchronization utilities

--- relix/kernel/relix/task/scheduler.hh
+++ relix/kernel/relix/task/scheduler.hh
@@ -13,11 +13,39 @@
 namespace relix
 {
 	
+	void pthread_wakeup();
+	void pthread_yield();
+	
+#ifdef __RELIX__
+	
+	inline
+	void pthread_yield() {}
+	
+#endif
+	
 	bool is_active();
 	
 	void mark_thread_active( pid_t tid );
 	
 	void mark_thread_inactive( pid_t tid );
+	
+	
+	class synchronized_scope
+	{
+	#ifndef __RELIX__
+		
+		private:
+			// non-copyable
+			synchronized_scope           ( const synchronized_scope& );
+			synchronized_scope& operator=( const synchronized_scope& );
+		
+		public:
+			synchronized_scope();
+			
+			~synchronized_scope();
+		
+	#endif
+	};
 	
 }
 
