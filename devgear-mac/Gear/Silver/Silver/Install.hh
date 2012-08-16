9490e9b6ee8bda8196b024303b9c1b1b8aa4c9f1
Josh Juran <jjuran@metamage.com>
2012-08-16 02:13:03 -0700

    Silver:  Include framework header on OS X

--- devgear-mac/Gear/Silver/Silver/Install.hh
+++ devgear-mac/Gear/Silver/Silver/Install.hh
@@ -9,6 +9,11 @@
 #ifndef SILVER_INSTALL_HH
 #define SILVER_INSTALL_HH
 
+// Mac OS X
+#ifdef __APPLE__
+#include <CoreServices/CoreServices.h>
+#endif
+
 // Mac OS
 #ifndef __MACTYPES__
 #include <MacTypes.h>
