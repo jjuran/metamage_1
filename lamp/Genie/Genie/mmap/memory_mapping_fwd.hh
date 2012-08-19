d93b49c7b265c734f28c5f25ccfb5675ea2e5f6b
Josh Juran <jjuran@metamage.com>
2012-08-18 19:29:58 -0700

    Genie:  Add memory_mapping_fwd.hh

---
+++ lamp/Genie/Genie/mmap/memory_mapping_fwd.hh
@@ -0,0 +1,18 @@
+/*
+	memory_mapping_fwd.hh
+	---------------------
+*/
+
+#ifndef GENIE_MMAP_MEMORYMAPPINGFWD_HH
+#define GENIE_MMAP_MEMORYMAPPINGFWD_HH
+
+
+namespace Genie
+{
+	
+	class memory_mapping;
+	
+}
+
+#endif
+
