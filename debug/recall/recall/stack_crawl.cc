4314e0df8c765a260beb82a55e94b45e12bcbf2f
Josh Juran <jjuran@metamage.com>
2012-08-20 02:32:53 -0700

    recall:  Disable stack crawls in LLVM builds

--- debug/recall/recall/stack_crawl.cc
+++ debug/recall/recall/stack_crawl.cc
@@ -14,7 +14,7 @@
 #endif
 
 
-#if defined( __x86_64__ )  ||  defined( __powerpc__ )
+#if defined( __llvm__ ) || defined( __x86_64__ )  ||  defined( __powerpc__ )
 #define CONFIG_STACK_CRAWLS 0
 #else
 #define CONFIG_STACK_CRAWLS 1
