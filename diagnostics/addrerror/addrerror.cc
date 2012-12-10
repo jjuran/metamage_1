fa644ad729b992a35287303646f8f1732fe0e4e2
Josh Juran <jjuran@metamage.com>
2012-12-09 21:26:33 -0800

    [NEW] addrerror:  Crash on misaligned read

---
+++ diagnostics/addrerror/addrerror.cc
@@ -0,0 +1,23 @@
+/*
+	addrerror.cc
+	------------
+*/
+
+
+#pragma exceptions off
+
+
+static int address_error_read_test()
+{
+	int x = 0;
+	
+	return *(short*) ((char*) &x + 1);
+}
+
+int main()
+{
+	address_error_read_test();
+	
+	return 0;
+}
+
