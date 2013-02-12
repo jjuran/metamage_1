97f48b45c1f00ee531e9fa53aeff614e7e536b7e
Josh Juran <jjuran@metamage.com>
2013-02-12 01:40:29 -0800

    POSIX headers:  Clean up sys/wait.h

--- lamp/POSIX-headers/sys/wait.h
+++ lamp/POSIX-headers/sys/wait.h
@@ -1,36 +1,37 @@
-/*	==========
- *	sys/wait.h
- *	==========
- */
+/*
+	sys/wait.h
+	----------
+*/
 
 #ifndef RELIX_SYS_WAIT_H
 #define RELIX_SYS_WAIT_H
 
-#include "sys/types.h"
+// POSIX
+#include <sys/types.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-	
-	#define WNOHANG    1
-	#define WUNTRACED  2
-	
-	#define WTERMSIG(stat)     ( (stat) & 0x7f )
-	
-	#define WCOREDUMP(stat)    ( (stat) & 0x80 )
-	
-	#define WEXITSTATUS(stat)  ( ((stat) >> 8) & 0xFF )
-	
-	#define WIFSIGNALED(stat)  ( WTERMSIG(stat) != 0 && WTERMSIG(stat) != 0x7F )
-	
-	#define WIFEXITED(stat)    ( WTERMSIG(stat) == 0 )
-	
-	#define WIFSTOPPED(stat)   ( WTERMSIG(stat) == 0x7F )
-	
-	pid_t wait( int* stat_loc );
-	
-	pid_t waitpid( pid_t pid, int* stat_loc, int options );
-	
+
+#define WNOHANG    1
+#define WUNTRACED  2
+
+#define WTERMSIG(stat)     ( (stat) & 0x7f )
+
+#define WCOREDUMP(stat)    ( (stat) & 0x80 )
+
+#define WEXITSTATUS(stat)  ( ((stat) >> 8) & 0xFF )
+
+#define WIFSIGNALED(stat)  ( WTERMSIG(stat) != 0 && WTERMSIG(stat) != 0x7F )
+
+#define WIFEXITED(stat)    ( WTERMSIG(stat) == 0 )
+
+#define WIFSTOPPED(stat)   ( WTERMSIG(stat) == 0x7F )
+
+pid_t wait( int* stat_loc );
+
+pid_t waitpid( pid_t pid, int* stat_loc, int options );
+
 #ifdef __cplusplus
 }
 #endif
