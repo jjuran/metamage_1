2899e88867f6d3eecb707330c87b4ffd0993fc12
Josh Juran <jjuran@metamage.com>
2013-05-27 15:57:25 -0700

    v68k-utils:  Use load primitive when emulated

--- engines/v68k/libs/v68k-utils/utils/load.cc
+++ engines/v68k/libs/v68k-utils/utils/load.cc
@@ -5,6 +5,13 @@
 
 #include "utils/load.hh"
 
+// Mac OS
+#ifdef __MC68K__
+#ifndef __GESTALT__
+#include <Gestalt.h>
+#endif
+#endif
+
 // POSIX
 #include <fcntl.h>
 #include <unistd.h>
@@ -26,8 +33,58 @@ namespace v68k  {
 namespace utils {
 
 
+#ifdef __MC68K__
+
+static asm void* load( const char* path : __A0 ) : __A0
+{
+	MOVEA.L  A0,A1    ; // copy A0
+	
+	// ENTER strlen0
+loop:
+	TST.B    (A1)+    ;  // while ( *a1++ != '\0' )
+	BNE.S    loop     ;  //    continue;
+	
+	SUBA     A0,A1    ;  // a1 -= a0;
+	MOVE.L   A1,D0    ;  // d0 = a1;
+	// LEAVE strlen0
+	
+	JSR  0xFFFFFFFC   ;  // the actual load callback
+	
+	MOVE.L   A0,D2    ;  // if ( *a0 != NULL ) ;
+	BNE.S    no_errno ;  // else
+	MOVE.L   D1,errno ;  //     errno = d1;
+no_errno:
+	RTS
+}
+
+static asm void* load_with_size( const char* path : __A0, uint32_t* size ) : __A0
+{
+	LINK     A6,#0    ;
+	
+	JSR      load
+	
+	MOVEA.L  -8(A6),A1;
+	MOVE.L   D0,(A1)  ;
+	
+	UNLK     A6       ;
+	RTS
+}
+
+#endif
+
 void* load_file( const char* path, uint32_t* size )
 {
+#ifdef __MC68K__
+	
+	SInt32 response;
+	
+	if ( ::Gestalt( 'v68k', &response ) == noErr )
+	{
+		return size ? load_with_size( path, size ) : load( path );
+	}
+	
+#endif
+	
 	int fd = open( path, O_RDONLY );
 	
 	if ( fd < 0 )
