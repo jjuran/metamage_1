7f40af468448e4db5bc39dfcdcebadf7f0c1f0fe
Josh Juran <jjuran@metamage.com>
2013-07-12 05:08:43 -0700

    Genie:  Implement stream methods for pipes

--- lamp/Genie/Genie/IO/Pipe.cc
+++ lamp/Genie/Genie/IO/Pipe.cc
@@ -20,6 +20,8 @@
 // vfs
 #include "vfs/enum/poll_result.hh"
 #include "vfs/filehandle/functions/nonblocking.hh"
+#include "vfs/filehandle/methods/filehandle_method_set.hh"
+#include "vfs/filehandle/methods/stream_method_set.hh"
 
 // Genie
 #include "Genie/api/signals.hh"
@@ -62,12 +64,60 @@ namespace Genie
 			ssize_t SysRead( char* data, std::size_t byteCount );
 	};
 	
+	static unsigned pipein_poll( vfs::filehandle* that )
+	{
+		return static_cast< PipeInHandle& >( *that ).SysPoll();
+	}
+	
+	static ssize_t pipein_read( vfs::filehandle* that, char* buffer, size_t n )
+	{
+		return static_cast< PipeInHandle& >( *that ).SysRead( buffer, n );
+	}
+	
+	static unsigned pipeout_poll( vfs::filehandle* that )
+	{
+		return static_cast< PipeOutHandle& >( *that ).SysPoll();
+	}
+	
+	static ssize_t pipeout_write( vfs::filehandle* that, const char* buffer, size_t n )
+	{
+		return static_cast< PipeOutHandle& >( *that ).SysWrite( buffer, n );
+	}
+	
+	static const vfs::stream_method_set pipein_stream_methods =
+	{
+		&pipein_poll,
+		&pipein_read,
+	};
+	
+	static const vfs::stream_method_set pipeout_stream_methods =
+	{
+		&pipeout_poll,
+		NULL,
+		&pipeout_write,
+	};
+	
+	static const vfs::filehandle_method_set pipein_methods =
+	{
+		NULL,
+		NULL,
+		&pipein_stream_methods,
+	};
+	
+	static const vfs::filehandle_method_set pipeout_methods =
+	{
+		NULL,
+		NULL,
+		&pipeout_stream_methods,
+	};
+	
 	
 	PipeInHandle::PipeInHandle( const boost::intrusive_ptr< plus::conduit >&  conduit,
 	                            bool                                          nonblocking )
 	:
 		StreamHandle( nonblocking ? O_WRONLY | O_NONBLOCK
-		                          : O_WRONLY ),
+		                          : O_WRONLY,
+		              &pipein_methods ),
 		itsConduit( conduit )
 	{
 	}
@@ -93,7 +143,8 @@ namespace Genie
 	                              bool                                          nonblocking )
 	:
 		StreamHandle( nonblocking ? O_RDONLY | O_NONBLOCK
-		                          : O_RDONLY ),
+		                          : O_RDONLY,
+		              &pipeout_methods ),
 		itsConduit( conduit )
 	{
 	}
