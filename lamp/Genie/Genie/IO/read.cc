9ee8493e1db63ce603306937e2e57d0f97949b14
Josh Juran <jjuran@metamage.com>
2013-07-17 04:41:12 -0700

    vfs, Genie:  Add stream read primitive

---
+++ lamp/Genie/Genie/IO/read.cc
@@ -0,0 +1,51 @@
+/*
+	read.cc
+	-------
+*/
+
+#include "vfs/filehandle/primitives/read.hh"
+
+// vfs
+#include "vfs/filehandle.hh"
+#include "vfs/filehandle/methods/bstore_method_set.hh"
+#include "vfs/filehandle/methods/filehandle_method_set.hh"
+#include "vfs/filehandle/methods/stream_method_set.hh"
+
+// Genie
+#include "Genie/IO/Stream.hh"
+
+
+namespace vfs
+{
+	
+	using namespace Genie;
+	
+	
+	ssize_t read( filehandle& that, char* buffer, size_t n )
+	{
+		if ( const filehandle_method_set* methods = that.methods() )
+		{
+			if ( const stream_method_set* stream_methods = methods->stream_methods )
+			{
+				if ( stream_methods->read )
+				{
+					return stream_methods->read( &that, buffer, n );
+				}
+			}
+			
+			if ( const bstore_method_set* bstore_methods = methods->bstore_methods )
+			{
+				if ( bstore_methods->pread )
+				{
+					ssize_t n_read = bstore_methods->pread( &that, buffer, n, that.get_mark() );
+					
+					return that.advance_mark( n_read );
+				}
+			}
+		}
+		
+		return IOHandle_Cast< StreamHandle >( that ).Read( buffer, n );
+	}
+	
+}
+
