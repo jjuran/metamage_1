jjuran/metamage_1

commit fa297bc41c735150ff523a2aa9fa73cca2c0ec1d

Genie:  Call vfs bytestore primitives

lamp/Genie/Genie/FS/FSTree_Proc.cc
@@ -29,6 +29,8 @@
 #include "vfs/dir_contents.hh"
 #include "vfs/dir_entry.hh"
 #include "vfs/file_descriptor.hh"
+#include "vfs/filehandle/methods/filehandle_method_set.hh"
+#include "vfs/filehandle/primitives/geteof.hh"
 #include "vfs/functions/pathname.hh"
 #include "vfs/node/types/fixed_dir.hh"
 
@@ -605,9 +607,9 @@ namespace Genie
 	{
 		IOHandle* handle = get_proc_fd_handle( node );
 		
-		if ( RegularFileHandle* file = IOHandle_Cast< RegularFileHandle >( handle ) )
+		if ( handle->methods()->bstore_methods != NULL )
 		{
-			return file->GetEOF();
+			return geteof( *handle );
 		}
 		
 		return 0;

lamp/Genie/Genie/FS/truncate.cc
@@ -5,26 +5,20 @@
 
 #include "vfs/filehandle/functions/truncate.hh"
 
-// poseven
-#include "poseven/types/errno_t.hh"
-
-// Genie
-#include "Genie/IO/RegularFile.hh"
+// vfs
+#include "vfs/filehandle.hh"
+#include "vfs/filehandle/methods/filehandle_method_set.hh"
+#include "vfs/filehandle/primitives/seteof.hh"
 
 
 namespace vfs
 {
 	
-	namespace p7 = poseven;
-	
-	
 	void truncate( filehandle* that )
 	{
-		using namespace Genie;
-		
-		if ( RegularFileHandle* fh = IOHandle_Cast< RegularFileHandle >( that ) )
+		if ( that->methods()  &&  that->methods()->bstore_methods )
 		{
-			fh->SetEOF( 0 );
+			seteof( *that, 0 );
 		}
 	}
 	

lamp/Genie/Genie/IO/RegularFile.cc
@@ -20,6 +20,9 @@
 
 // vfs
 #include "vfs/node.hh"
+#include "vfs/filehandle/primitives/geteof.hh"
+#include "vfs/filehandle/primitives/pread.hh"
+#include "vfs/filehandle/primitives/pwrite.hh"
 #include "vfs/mmap/types/file_memory_mapping.hh"
 #include "vfs/primitives/geteof.hh"
 #include "vfs/primitives/seteof.hh"
@@ -65,13 +68,11 @@ namespace Genie
 	
 	void file_memory_mapping::msync( void* addr, size_t len, int flags ) const
 	{
-		RegularFileHandle& file = static_cast< RegularFileHandle& >( *get_file() );
-		
 		const int mmap_flags = get_flags();
 		
 		if ( (mmap_flags & (MAP_SHARED|MAP_PRIVATE)) == MAP_SHARED )
 		{
-			file.Positioned_Write( (const char*) addr, len, its_offset );
+			pwrite( *get_file(), (const char*) addr, len, its_offset );
 		}
 	}
 	
@@ -133,16 +134,17 @@ namespace Genie
 	
 	ssize_t RegularFileHandle::Append( const char* buffer, size_t n_bytes )
 	{
-		itsMark = GetEOF();
+		itsMark = geteof( *this );
 		
-		return Positioned_Write( buffer, n_bytes, itsMark );
+		return pwrite( *this, buffer, n_bytes, itsMark );
 	}
 	
 	ssize_t RegularFileHandle::SysRead( char* buffer, size_t n_bytes )
 	{
-		ssize_t read = Positioned_Read( buffer,
-		                                n_bytes,
-		                                GetFileMark() );
+		ssize_t read = pread( *this,
+		                      buffer,
+		                      n_bytes,
+		                      GetFileMark() );
 		
 		return Advance( read );
 	}
@@ -152,9 +154,10 @@ namespace Genie
 		const bool appending = GetFlags() & O_APPEND;
 		
 		ssize_t written = appending ? Append( buffer, n_bytes )
-		                            : Positioned_Write( buffer,
-		                                                n_bytes,
-		                                                GetFileMark() );
+		                            : pwrite( *this,
+		                                      buffer,
+		                                      n_bytes,
+		                                      GetFileMark() );
 		
 		return Advance( written );
 	}
@@ -174,7 +177,7 @@ namespace Genie
 				break;
 			
 			case SEEK_END:
-				base = GetEOF();
+				base = geteof( *this );
 				break;
 			
 			default:
@@ -230,7 +233,7 @@ namespace Genie
 		
 		void* addr = mapping->get_address();
 		
-		ssize_t count = Positioned_Read( (char*) addr, length, offset );
+		ssize_t count = pread( *this, (char*) addr, length, offset );
 		
 		if ( count < length )
 		{

lamp/Genie/Genie/Kernel/stat.cc
@@ -11,6 +11,7 @@
 #include "sys/stat.h"
 
 // vfs
+#include "vfs/filehandle/primitives/geteof.hh"
 #include "vfs/primitives/chmod.hh"
 #include "vfs/primitives/geteof.hh"
 #include "vfs/primitives/stat.hh"
@@ -143,7 +144,7 @@ namespace Genie
 			{
 				try
 				{
-					sb->st_size = IOHandle_Cast< RegularFileHandle >( handle ).GetEOF();
+					sb->st_size = geteof( handle );
 				}
 				catch ( ... )
 				{

lamp/Genie/Genie/SystemCalls.cc
@@ -22,6 +22,9 @@
 
 // vfs
 #include "vfs/primitives/seteof.hh"
+#include "vfs/filehandle/primitives/pread.hh"
+#include "vfs/filehandle/primitives/pwrite.hh"
+#include "vfs/filehandle/primitives/seteof.hh"
 
 // relix-kernel
 #include "relix/signal/caught_signal.hh"
@@ -312,11 +315,10 @@ namespace Genie
 		
 		try
 		{
-			RegularFileHandle& file = GetFileHandleWithCast< RegularFileHandle >( fd, ESPIPE );
-			
-			int get = file.Positioned_Read( reinterpret_cast< char* >( buf ),
-			                                count,
-			                                offset );
+			ssize_t get = pread( get_filehandle( fd ),
+			                     (char*) buf,
+			                     count,
+			                     offset );
 			
 			return get;
 		}
@@ -451,14 +453,7 @@ namespace Genie
 	{
 		try
 		{
-			IOHandle* h = &get_filehandle( fd );
-			
-			typedef RegularFileHandle FileHandle;
-			
-			if ( FileHandle* fh = IOHandle_Cast< FileHandle >( h ) )
-			{
-				fh->SetEOF( length );
-			}
+			seteof( get_filehandle( fd ), length );
 		}
 		catch ( ... )
 		{
@@ -478,11 +473,11 @@ namespace Genie
 		
 		try
 		{
-			RegularFileHandle& file = GetFileHandleWithCast< RegularFileHandle >( fd, ESPIPE );
+			vfs::filehandle& file = get_filehandle( fd );
 			
 			const char* buffer = reinterpret_cast< const char* >( buf );
 			
-			const ssize_t put = file.Positioned_Write( buffer, count, offset );
+			const ssize_t put = pwrite( file, buffer, count, offset );
 			
 			return put;
 		}

commit 109f33a30ec746d1675f76bcb2ca393ba00f94b8

[FIX] Genie:  Null-check in proc_fd_link_geteof()
This was broken by fa297bc41c735150ff523a2aa9fa73cca2c0ec1d.

lamp/Genie/Genie/FS/FSTree_Proc.cc
@@ -608,7 +608,7 @@ namespace Genie
 	{
 		IOHandle* handle = get_proc_fd_handle( node );
 		
-		if ( handle->methods()->bstore_methods != NULL )
+		if ( handle->methods()  &&  handle->methods()->bstore_methods )
 		{
 			return geteof( *handle );
 		}