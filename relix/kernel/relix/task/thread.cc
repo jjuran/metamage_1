a35240b79a283d0eb0f69ca7968d67a13ad52b45
Josh Juran <jjuran@metamage.com>
2013-09-24 01:29:20 -0700

    relix:  Add class thread

---
+++ relix/kernel/relix/task/thread.cc
@@ -0,0 +1,23 @@
+/*
+	thread.cc
+	----------
+*/
+
+#include "relix/task/thread.hh"
+
+
+namespace relix
+{
+	
+	thread::thread( int id )
+	:
+		its_id( id )
+	{
+	}
+	
+	thread::~thread()
+	{
+	}
+	
+}
+
