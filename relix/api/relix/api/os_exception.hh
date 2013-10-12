9692e0d5db3c6006a32772622b73a1d0abb3c27e
Josh Juran <jjuran@metamage.com>
2013-10-12 12:55:09 -0700

    relix:  Add errno_from_os_exception()

---
+++ relix/api/relix/api/os_exception.hh
@@ -0,0 +1,18 @@
+/*
+	os_exception.hh
+	---------------
+*/
+
+#ifndef RELIX_OSEXCEPTION_HH
+#define RELIX_OSEXCEPTION_HH
+
+
+namespace relix
+{
+	
+	int errno_from_os_exception();
+	
+}
+
+#endif
+
