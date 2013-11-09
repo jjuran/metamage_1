7dbe570d651dbfa7f3bfb0a05d61f4a5231b6855
Josh Juran <jjuran@metamage.com>
2013-11-09 00:10:44 -0800

    iota:  Fix dummy symbol for newer Apple toolchains
    Later Apple toolchains got smarter about being annoying.

--- base/iota/iota/dummy.hh
+++ base/iota/iota/dummy.hh
@@ -6,17 +6,21 @@
 #ifndef IOTA_DUMMY_HH
 #define IOTA_DUMMY_HH
 
+#ifdef __APPLE__
+
 
 namespace iota
 {
 	
-	namespace
+	namespace _
 	{
 		
 		void dummy()
 		{
 			/*
 				* anonymous namespace avoids no-prototype warnings
+					(or at least it did with gcc 4.0, but not 4.2 or clang)
+				
 				* extern linkage silences no-symbol link warnings
 			*/
 		}
@@ -24,6 +28,8 @@ namespace iota
 	}
 	
 }
+
+#endif  // #ifdef __APPLE__
 
 #endif
 
