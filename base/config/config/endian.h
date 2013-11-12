f3459e884e171ef015d06aa66cae25d9e5181b20
Josh Juran <jjuran@metamage.com>
2013-11-12 15:19:53 -0800

    config/endian:  Recognize __BYTE_ORDER__ macro

--- base/config/config/endian.h
+++ base/config/config/endian.h
@@ -8,6 +8,14 @@
 
 
 #ifndef CONFIG_LITTLE_ENDIAN
+	#ifdef __BYTE_ORDER__
+		#if defined( __ORDER_BIG_ENDIAN__ )  &&  __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+			#define CONFIG_LITTLE_ENDIAN 0
+		#endif
+		#if defined( __ORDER_LITTLE_ENDIAN__ )  &&  __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+			#define CONFIG_LITTLE_ENDIAN 1
+		#endif
+	#endif
 	#if defined( __BIG_ENDIAN__ ) || defined( __MC68K__ )
 		#define CONFIG_LITTLE_ENDIAN  0
 	#endif
