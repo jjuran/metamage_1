80238db0d5c9d347b90f6a89cca0d0f2701fefee
Josh Juran <jjuran@metamage.com>
2013-11-14 21:09:13 -0800

    vfs:  Add file_descriptor_fwd.hh

---
+++ posix/vfs/vfs/file_descriptor_fwd.hh
@@ -0,0 +1,18 @@
+/*
+	file_descriptor_fwd.hh
+	----------------------
+*/
+
+#ifndef VFS_FILEDESCRIPTORFWD_HH
+#define VFS_FILEDESCRIPTORFWD_HH
+
+
+namespace vfs
+{
+	
+	struct file_descriptor;
+	
+}
+
+#endif
+
