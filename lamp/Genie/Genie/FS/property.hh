1dc429dff6a3fdb04cd94b046abaf8d208fdfc26
Josh Juran <jjuran@metamage.com>
2013-12-24 18:55:09 -0800

    Genie -> vfs:  Move property.hh

--- lamp/Genie/Genie/FS/property.hh
+++ lamp/Genie/Genie/FS/property.hh
@@ -6,77 +6,9 @@
 #ifndef GENIE_FS_PROPERTY_HH
 #define GENIE_FS_PROPERTY_HH
 
-// plus
-#include "plus/var_string_fwd.hh"
-
 // vfs
-#include "vfs/node_fwd.hh"
+#include "vfs/property.hh"
 
-
-namespace vfs
-{
-	
-	typedef void (*property_get_hook)( plus::var_string&  out,
-	                                   const node*        that,
-	                                   bool               binary );
-	
-	typedef void (*property_set_hook)( const node*  that,
-	                                   const char*  begin,
-	                                   const char*  end,
-	                                   bool         binary );
-	
-	struct readonly_property
-	{
-		static const int fixed_size = -1;
-		
-		static const bool can_get = true;
-		static const bool can_set = false;
-		
-		// Not defined
-		static void set( const node* that, const char* begin, const char* end, bool binary );
-	};
-	
-	struct writeonly_property
-	{
-		static const int fixed_size = -1;
-		
-		static const bool can_get = false;
-		static const bool can_set = true;
-		
-		// Not defined
-		static void get( plus::var_string& result, const node* that, bool binary );
-	};
-	
-	struct readwrite_property
-	{
-		static const int fixed_size = -1;
-		
-		static const bool can_get = true;
-		static const bool can_set = true;
-	};
-	
-	struct property_params
-	{
-		int                size;
-		property_get_hook  get;
-		property_set_hook  set;
-	};
-	
-	template < class Property >
-	struct property_params_factory
-	{
-		static const property_params value;
-	};
-	
-	template < class Property >
-	const property_params property_params_factory< Property >::value =
-	{
-		Property::fixed_size,
-		Property::can_get ? &Property::get : 0,  // NULL
-		Property::can_set ? &Property::set : 0   // NULL
-	};
-	
-}
 
 namespace Genie
 {
