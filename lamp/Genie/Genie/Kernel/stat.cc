dbb8d5d8bf8ef8d198e477b72b63b1333dfe7fc1
Josh Juran <jjuran@metamage.com>
2013-12-29 00:40:38 -0800

    Genie -> relix:  Move fstat() system call

--- lamp/Genie/Genie/Kernel/stat.cc
+++ lamp/Genie/Genie/Kernel/stat.cc
@@ -3,29 +3,14 @@
  *	=======
  */
 
-// Standard C/C++
-#include <cstring>
-
-// POSIX
-//#include "stdlib.h"
-#include "sys/stat.h"
-
-// vfs
-#include "vfs/node.hh"
-#include "vfs/filehandle/primitives/geteof.hh"
-#include "vfs/primitives/geteof.hh"
-#include "vfs/primitives/stat.hh"
-
 // relix-kernel
-#include "relix/api/get_fd_handle.hh"
 #include "relix/syscall/faccessat.hh"
 #include "relix/syscall/fchmod.hh"
 #include "relix/syscall/fchmodat.hh"
+#include "relix/syscall/fstat.hh"
 #include "relix/syscall/fstatat.hh"
 
 // Genie
-#include "Genie/current_process.hh"
-#include "Genie/IO/RegularFile.hh"
 #include "Genie/SystemCallRegistry.hh"
 
 
@@ -35,40 +20,9 @@ namespace Genie
 	using relix::faccessat;
 	using relix::fchmod;
 	using relix::fchmodat;
+	using relix::fstat;
 	using relix::fstatat;
 	
-	
-	static int fstat( int fd, struct stat* sb )
-	{
-		std::memset( (void*) sb, '\0', sizeof (struct stat) );
-		
-		sb->st_size = off_t( -1 );
-		
-		try
-		{
-			vfs::filehandle& handle = relix::get_fd_handle( fd );
-			
-			stat( *handle.GetFile(), *sb );
-			
-			if ( sb->st_size == off_t( -1 ) )
-			{
-				try
-				{
-					sb->st_size = geteof( handle );
-				}
-				catch ( ... )
-				{
-					sb->st_size = 0;
-				}
-			}
-		}
-		catch ( ... )
-		{
-			return set_errno_from_exception();
-		}
-		
-		return 0;
-	}
 	
 	#pragma force_active on
 	
