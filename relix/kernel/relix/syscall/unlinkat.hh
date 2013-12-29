99ff0ec0d63fba007befb1c8200b9469aa4cf1c0
Josh Juran <jjuran@metamage.com>
2013-12-28 16:45:01 -0800

    Genie -> relix:  Move unlinkat() system call

---
+++ relix/kernel/relix/syscall/unlinkat.hh
@@ -0,0 +1,18 @@
+/*
+	renameat.hh
+	-----------
+*/
+
+#ifndef RELIX_SYSCALL_UNLINKAT_HH
+#define RELIX_SYSCALL_UNLINKAT_HH
+
+
+namespace relix
+{
+	
+	int unlinkat( int dirfd, const char* path, int flags );
+	
+}
+
+#endif
+
