a85e106d6620bb3b381b5401104ce2081f58eb09
Josh Juran <jjuran@metamage.com>
2014-01-03 04:26:49 -0800

    vfs:  Define general_method_set

---
+++ posix/vfs/vfs/filehandle/methods/general_method_set.hh
@@ -0,0 +1,37 @@
+/*
+	general_method_set.hh
+	---------------------
+*/
+
+#ifndef VFS_FILEHANDLE_METHODS_GENERALMETHODSET_HH
+#define VFS_FILEHANDLE_METHODS_GENERALMETHODSET_HH
+
+// vfs
+#include "vfs/memory_mapping_ptr.hh"
+#include "vfs/node_ptr.hh"
+
+
+namespace vfs
+{
+	
+	class filehandle;
+	
+	
+	typedef memory_mapping_ptr (*mmap_method)( filehandle* that, size_t length, int prot, int flags, off_t offset );
+	
+	typedef void (*ioctl_method)( filehandle*, unsigned long request, int* argp );
+	
+	typedef node_ptr (*getfile_method)( filehandle* that );
+	
+	
+	struct general_method_set
+	{
+		mmap_method     mmap;
+		ioctl_method    ioctl;
+		getfile_method  getfile;
+	};
+	
+}
+
+#endif
+
