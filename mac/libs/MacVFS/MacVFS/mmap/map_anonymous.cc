4ecf5bbeb2350b7023223283c12a39e6efaf5eed
Josh Juran <jjuran@metamage.com>
2014-01-05 14:20:15 -0800

    MacVFS:  Rewrite map_anonymous()
    Requests of 64K or less will now use malloc() instead of Mac OS handles.
    This is likely to improve performance of a hypothetical sequence of many
    small requests, should such a pattern occur.

--- mac/libs/MacVFS/MacVFS/mmap/map_anonymous.cc
+++ mac/libs/MacVFS/MacVFS/mmap/map_anonymous.cc
@@ -8,47 +8,21 @@
 // Standard C
 #include <string.h>
 
-// Nitrogen
-#include "Nitrogen/MacMemory.hh"
-
-// MacVFS
-#include "MacVFS/mmap/Handle_memory_mapping.hh"
+// vfs
+#include "vfs/memory_mapping.hh"
+#include "vfs/mmap/functions/map_uninitialized.hh"
 
 
 namespace vfs
 {
 	
-	namespace n = nucleus;
-	namespace N = Nitrogen;
-	
-	
 	memory_mapping_ptr map_anonymous( std::size_t length, int prot, int flags )
 	{
-		::Handle h = NULL;
+		memory_mapping_ptr result = map_uninitialized( length, prot, flags );
 		
-		// dlmalloc defaults to 64K requests.  Don't use temp mem for those.
+		memset( result->get_address(), '\0', length );
 		
-		if ( length <= 64 * 1024 )
-		{
-			h = ::NewHandleClear( length );
-		}
-		
-		n::owned< Mac::Handle > handle;
-		
-		if ( h != NULL )
-		{
-			handle = n::owned< Mac::Handle >::seize( h );
-		}
-		else
-		{
-			// Allocation above either failed or wasn't attempted.
-			
-			handle = N::TempNewHandle( length );
-			
-			memset( *handle.get(), '\0', length );
-		}
-		
-		return new Handle_memory_mapping( handle, length, flags );
+		return result;
 	}
 	
 }
