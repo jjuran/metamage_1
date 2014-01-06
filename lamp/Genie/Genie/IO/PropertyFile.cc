46b80cd106488af519b92a946e92162f4596fe95
Josh Juran <jjuran@metamage.com>
2014-01-06 04:54:51 -0800

    Genie -> vfs:  Move/rewrite property_reader

--- lamp/Genie/Genie/IO/PropertyFile.cc
+++ lamp/Genie/Genie/IO/PropertyFile.cc
@@ -5,17 +5,11 @@
 
 #include "Genie/IO/PropertyFile.hh"
 
-// Standard C++
-#include <algorithm>
-
 // poseven
 #include "poseven/types/errno_t.hh"
 
 // vfs
 #include "vfs/node.hh"
-#include "vfs/filehandle/methods/bstore_method_set.hh"
-#include "vfs/filehandle/methods/filehandle_method_set.hh"
-#include "vfs/filehandle/types/property_reader.hh"
 #include "vfs/filehandle/types/property_writer.hh"
 
 
@@ -23,52 +17,6 @@ namespace Genie
 {
 	
 	namespace p7 = poseven;
-	
-	
-	static ssize_t propertyreader_pread( vfs::filehandle* file, char* buffer, size_t n, off_t offset )
-	{
-		return static_cast< PropertyReaderFileHandle& >( *file ).Positioned_Read( buffer, n, offset );
-	}
-	
-	static off_t propertyreader_geteof( vfs::filehandle* file )
-	{
-		return static_cast< PropertyReaderFileHandle& >( *file ).GetEOF();
-	}
-	
-	static const vfs::bstore_method_set propertyreader_bstore_methods =
-	{
-		&propertyreader_pread,
-		&propertyreader_geteof,
-	};
-	
-	static const vfs::filehandle_method_set propertyreader_methods =
-	{
-		&propertyreader_bstore_methods,
-	};
-	
-	
-	PropertyReaderFileHandle::PropertyReaderFileHandle( const vfs::node&     file,
-	                                                    int                  flags,
-	                                                    const plus::string&  value )
-	:
-		RegularFileHandle( &file, flags, &propertyreader_methods ),
-		itsData( value )
-	{
-	}
-	
-	ssize_t PropertyReaderFileHandle::Positioned_Read( char* buffer, size_t n_bytes, off_t offset )
-	{
-		if ( offset >= itsData.size() )
-		{
-			return 0;
-		}
-		
-		n_bytes = std::min< size_t >( n_bytes, itsData.size() - offset );
-		
-		memcpy( buffer, &itsData[ offset ], n_bytes );
-		
-		return n_bytes;
-	}
 	
 	
 	PropertyWriterFileHandle::PropertyWriterFileHandle( const vfs::node&  file,
@@ -105,13 +53,6 @@ namespace Genie
 
 namespace vfs
 {
-	
-	filehandle_ptr new_property_reader( const node&          file,
-	                                    int                  flags,
-	                                    const plus::string&  value )
-	{
-		return new Genie::PropertyReaderFileHandle( file, flags, value );
-	}
 	
 	filehandle_ptr new_property_writer( const node&          file,
 	                                    int                  flags,
--- lamp/Genie/Genie/IO/PropertyFile.hh
+++ lamp/Genie/Genie/IO/PropertyFile.hh
@@ -6,35 +6,15 @@
 #ifndef GENIE_IO_PROPERTYFILE_HH
 #define GENIE_IO_PROPERTYFILE_HH
 
-// plus
-#include "plus/string.hh"
-
 // vfs
 #include "vfs/node_fwd.hh"
 
 // Genie
-#include "Genie/IO/RegularFile.hh"
 #include "Genie/IO/Stream.hh"
 
 
 namespace Genie
 {
-	
-	class PropertyReaderFileHandle : public RegularFileHandle
-	{
-		private:
-			plus::string itsData;
-		
-		public:
-			PropertyReaderFileHandle( const vfs::node&     file,
-			                          int                  flags,
-			                          const plus::string&  value );
-			
-			ssize_t Positioned_Read( char* buffer, size_t n_bytes, off_t offset );
-			
-			off_t GetEOF()  { return itsData.size(); }
-	};
-	
 	
 	class PropertyWriterFileHandle : public StreamHandle
 	{
