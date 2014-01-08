cd7dae93fd78f1a166e5c7c767ea77e49ac81882
Josh Juran <jjuran@metamage.com>
2014-01-08 08:05:20 -0800

    mac-sys-utils:  Add temp_free_mem()

---
+++ mac/libs/mac-sys-utils/mac_sys/temp_free_mem.hh
@@ -0,0 +1,19 @@
+/*
+	temp_free_mem.hh
+	----------------
+*/
+
+#ifndef MACSYS_TEMPFREEMEM_HH
+#define MACSYS_TEMPFREEMEM_HH
+
+
+namespace mac {
+namespace sys {
+	
+	long temp_free_mem();
+	
+}
+}
+
+#endif
+
