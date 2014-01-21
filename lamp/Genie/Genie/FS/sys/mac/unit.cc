1f5c653eebaa678dfdf7a04e654b3ab419514057
Josh Juran <jjuran@metamage.com>
2014-01-21 06:14:27 -0800

    Genie:  Use mac-sys unit_table in /sys/mac/unit

--- lamp/Genie/Genie/FS/sys/mac/unit.cc
+++ lamp/Genie/Genie/FS/sys/mac/unit.cc
@@ -10,6 +10,12 @@
 // Standard C
 #include <ctype.h>
 
+// mac-types
+#include "mac_types/AuxDCE.hh"
+
+// mac-sys-utils
+#include "mac_sys/unit_table.hh"
+
 // gear
 #include "gear/inscribe_decimal.hh"
 #include "gear/parse_decimal.hh"
@@ -38,30 +44,14 @@
 #include "Genie/Utilities/canonical_positive_integer.hh"
 
 
-namespace Nitrogen
-{
-	
-	UnitTableDrivers_Container_Specifics::key_type
-	//
-	UnitTableDrivers_Container_Specifics::get_next_key( key_type key )
-	{
-		const key_type end = end_key();
-		
-		while ( ++key < end  &&  *key == NULL )
-		{
-			continue;
-		}
-		
-		return key;
-	}
-	
-}
-
 namespace Genie
 {
 	
 	namespace N = Nitrogen;
 	namespace p7 = poseven;
+	
+	
+	using mac::types::AuxDCEHandle;
 	
 	
 	struct decode_unit_number
@@ -78,17 +68,11 @@ namespace Genie
 	}
 	
 	
-	static inline AuxDCEHandle* GetUTableBase()
-	{
-		return (AuxDCEHandle*) LMGetUTableBase();
-	}
-	
-	
 	static bool is_valid_unit_number( UInt32 i )
 	{
-		AuxDCEHandle* base = GetUTableBase();
+		AuxDCEHandle* base = mac::sys::get_unit_table_base();
 		
-		const UInt16 count = LMGetUnitTableEntryCount();
+		const short count = mac::sys::get_unit_table_entry_count();
 		
 		return i < count  &&  base[ i ] != NULL;
 	}
@@ -104,32 +88,9 @@ namespace Genie
 		}
 	};
 	
-	const unsigned char* GetDriverName_WithinHandle( AuxDCEHandle dceHandle )
-	{
-		ASSERT( dceHandle != NULL );
-		
-		if ( dceHandle[0]->dCtlDriver != NULL )
-		{
-			const bool ramBased = dceHandle[0]->dCtlFlags & dRAMBasedMask;
-			
-			const Ptr drvr = dceHandle[0]->dCtlDriver;
-			
-			// Dereferences a handle if ramBased
-			const DRVRHeaderPtr header = ramBased ? *(DRVRHeader **) drvr
-			                                      :  (DRVRHeader * ) drvr;
-			
-			if ( header != NULL )
-			{
-				return header->drvrName;
-			}
-		}
-		
-		return "\p";
-	}
-	
 	static N::Str255 GetDriverName( AuxDCEHandle dceHandle )
 	{
-		const unsigned char* name = GetDriverName_WithinHandle( dceHandle );
+		const unsigned char* name = mac::sys::get_driver_name( dceHandle );
 		
 		// Safely copy Pascal string onto stack
 		return N::Str255( name );
@@ -241,7 +202,7 @@ namespace Genie
 				throw undefined_property();
 			}
 			
-			AuxDCEHandle dceHandle = GetUTableBase()[ key ];
+			AuxDCEHandle dceHandle = mac::sys::get_unit_table_base()[ key ];
 			
 			const typename Accessor::result_type data = Accessor::Get( dceHandle );
 			
@@ -272,31 +233,23 @@ namespace Genie
 		return fixed_dir( parent, name, sys_mac_unit_N_Mappings );
 	}
 	
-	class unit_IteratorConverter
+	static void unit_iterate( const FSTree* parent, vfs::dir_contents& cache )
 	{
-		public:
-			vfs::dir_entry operator()( N::UnitTableDrivers_Container::const_reference ref ) const
+		const short n = mac::sys::get_unit_table_entry_count();
+		
+		AuxDCEHandle* it = mac::sys::get_unit_table_base();
+		
+		for ( int i = 0;  i < n;  ++i, ++it )
+		{
+			if ( *it )
 			{
-				const int i = &ref - GetUTableBase();
-				
 				const ino_t inode = i;
 				
 				plus::string name = gear::inscribe_decimal( i );
 				
-				return vfs::dir_entry( inode, name );
+				cache.push_back( vfs::dir_entry( inode, name ) );
 			}
-	};
-	
-	static void unit_iterate( const FSTree* parent, vfs::dir_contents& cache )
-	{
-		unit_IteratorConverter converter;
-		
-		N::UnitTableDrivers_Container sequence = N::UnitTableDrivers();
-		
-		std::transform( sequence.begin(),
-		                sequence.end(),
-		                std::back_inserter( cache ),
-		                converter );
+		}
 	}
 	
 	
--- lamp/Genie/Genie/FS/sys/mac/unit.hh
+++ lamp/Genie/Genie/FS/sys/mac/unit.hh
@@ -6,18 +6,8 @@
 #ifndef GENIE_FS_SYS_MAC_UNIT_HH
 #define GENIE_FS_SYS_MAC_UNIT_HH
 
-// Mac OS
-#ifndef __LOWMEM__
-#include <LowMem.h>
-#endif
-
 // plus
 #include "plus/string.hh"
-
-// nucleus
-#ifndef NUCLEUS_ADVANCEUNTILDONESEQUENCE_HH
-#include "nucleus/advance_until_done_sequence.hh"
-#endif
 
 // ClassicToolbox
 #ifndef CLASSICTOOLBOX_DEVICES_HH
@@ -28,47 +18,8 @@
 #include "Genie/FS/FSTreePtr.hh"
 
 
-namespace Nitrogen
-{
-	
-	struct UnitTableDrivers_Container_Specifics
-	{
-		typedef UInt16             size_type;
-		typedef SInt16             difference_type;
-		typedef AuxDCEHandle       value_type;
-		typedef const value_type&  const_reference;
-		typedef const value_type*  const_pointer;
-		typedef const_pointer      key_type;
-		
-		static key_type get_next_key( key_type key );
-		
-		static const_pointer GetPointer( key_type ptr )  { return ptr; }
-		
-		static key_type begin_key()  { return get_next_key( reinterpret_cast< AuxDCEHandle* >( LMGetUTableBase() ) - 1 ); }
-		static key_type end_key()    { return (AuxDCEHandle*) LMGetUTableBase() + LMGetUnitTableEntryCount(); }
-	};
-	
-	class UnitTableDrivers_Container : public nucleus::advance_until_done_sequence< UnitTableDrivers_Container_Specifics >
-	{
-		friend UnitTableDrivers_Container UnitTableDrivers();
-		
-		private:
-			UnitTableDrivers_Container()
-			: nucleus::advance_until_done_sequence< UnitTableDrivers_Container_Specifics >( UnitTableDrivers_Container_Specifics() )
-			{}
-	};
-	
-	inline UnitTableDrivers_Container UnitTableDrivers()
-	{
-		return UnitTableDrivers_Container();
-	}
-	
-}
-
 namespace Genie
 {
-	
-	const unsigned char* GetDriverName_WithinHandle( AuxDCEHandle dceHandle );
 	
 	FSTreePtr New_FSTree_sys_mac_unit( const FSTree*        parent,
 	                                   const plus::string&  name,
