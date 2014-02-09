62a95d6e781e09b54c1195ee37a4e9d038ea7268
Josh Juran <jjuran@metamage.com>
2014-02-09 03:18:50 -0800

    v68k:  Axe unneeded <stdint.h> includes

--- engines/v68k/v68k/fetcher.hh
+++ engines/v68k/v68k/fetcher.hh
@@ -6,9 +6,6 @@
 #ifndef V68K_FETCHER_HH
 #define V68K_FETCHER_HH
 
-// C99
-#include <stdint.h>
-
 
 namespace v68k
 {
--- engines/v68k/v68k/load_store.hh
+++ engines/v68k/v68k/load_store.hh
@@ -6,9 +6,6 @@
 #ifndef V68K_LOADSTORE_HH
 #define V68K_LOADSTORE_HH
 
-// C99
-#include <stdint.h>
-
 
 namespace v68k
 {
