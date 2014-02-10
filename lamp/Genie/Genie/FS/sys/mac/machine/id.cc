45a635d16d7db3fb2761cece2587e38dddfb7ff0
Josh Juran <jjuran@metamage.com>
2014-02-10 11:37:22 -0800

    Genie:  mac::sys::gestalt() in /sys/mac/machine/id

--- lamp/Genie/Genie/FS/sys/mac/machine/id.cc
+++ lamp/Genie/Genie/FS/sys/mac/machine/id.cc
@@ -5,33 +5,51 @@
 
 #include "Genie/FS/sys/mac/machine/id.hh"
 
+// mac-sys-utils
+#include "mac_sys/gestalt.hh"
+
 // plus
 #include "plus/serialize.hh"
 #include "plus/var_string.hh"
 
-// Nitrogen
-#include "Nitrogen/Gestalt.hh"
+// poseven
+#include "poseven/types/errno_t.hh"
+
+
+#ifndef TARGET_API_MAC_CARBON
+#define TARGET_API_MAC_CARBON  1
+#endif
 
 
 namespace Genie
 {
 	
-	namespace N = Nitrogen;
+	namespace p7 = poseven;
 	
 	
-	typedef plus::serialize_unsigned< UInt32 >::deconstruct deconstruct_id;
+	typedef unsigned long uint32_t;
+	
+	typedef plus::serialize_unsigned< uint32_t >::deconstruct deconstruct_id;
 	
 	
-	static inline UInt32 GetMachineID()
+	const uint32_t gestaltMachineType = 'mach';
+	
+	
+	static inline uint32_t GetMachineID()
 	{
-		const UInt32 id = N::Gestalt( N::GestaltSelector( gestaltMachineType ) );
+		const long id = mac::sys::gestalt( gestaltMachineType, -1 );
+		
+		if ( TARGET_API_MAC_CARBON  &&  id < 0 )
+		{
+			p7::throw_errno( ENOENT );
+		}
 		
 		return id;
 	}
 	
 	void sys_mac_machine_id::get( plus::var_string& result, const vfs::node* that, bool binary )
 	{
-		const UInt32 id = GetMachineID();
+		const uint32_t id = GetMachineID();
 		
 		deconstruct_id::apply( result, id, binary );
 	}
