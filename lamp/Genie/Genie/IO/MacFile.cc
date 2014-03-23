72e92f9651cbc20d4b59a7d9caa4fd21bb363190
Josh Juran <jjuran@metamage.com>
2014-03-23 11:39:39 -0700

    Genie:  Simplify OpenMacFileHandle()

--- lamp/Genie/Genie/IO/MacFile.cc
+++ lamp/Genie/Genie/IO/MacFile.cc
@@ -298,10 +298,11 @@ namespace Genie
 	                   ForkOpener     openFork,
 	                   FileGetter     getFile )
 	{
-		Mac::FSIOPermissions rdPerm = Mac::FSIOPermissions( flags + 1 - O_RDONLY  &  FREAD  );
-		Mac::FSIOPermissions wrPerm = Mac::FSIOPermissions( flags + 1 - O_RDONLY  &  FWRITE );
+		const int mac_perm = (flags + 1 - O_RDONLY) & 3;
 		
-		n::owned< Mac::FSFileRefNum > fileHandle = openFork( fileSpec, rdPerm | wrPerm );
+		const Mac::FSIOPermissions perm = Mac::FSIOPermissions( mac_perm );
+		
+		n::owned< Mac::FSFileRefNum > fileHandle = openFork( fileSpec, perm );
 		
 		return new MacFileHandle( fileHandle, flags, getFile );
 	}
