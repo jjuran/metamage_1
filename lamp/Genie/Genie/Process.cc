b6204d5e0848d91930aee9fcb49168457f38b514
Josh Juran <jjuran@metamage.com>
2014-03-25 23:21:22 -0700

    Genie:  Store exe in process_image on exec

--- lamp/Genie/Genie/Process.cc
+++ lamp/Genie/Genie/Process.cc
@@ -553,6 +553,13 @@ namespace Genie
 		return new relix::process_image();
 	}
 	
+	static boost::intrusive_ptr< relix::process_image >
+	//
+	new_process_image( const vfs::node& exe )
+	{
+		return new relix::process_image( exe );
+	}
+	
 	static vfs::filehandle_ptr open_device( const char* path, size_t length )
 	{
 		return open( *vfs::resolve_absolute_path( *relix::root(), path, length ), O_RDWR, 0 );
@@ -832,7 +839,7 @@ namespace Genie
 		// Create the new thread
 		looseThread = new_os_thread( &Process::thread_start, this, min_stack );
 		
-		get_process().set_process_image( *new_process_image() );
+		get_process().set_process_image( *new_process_image( *context.executable ) );
 		
 		// Make the new thread belong to this process and save the old one
 		itsThread.swap( looseThread );
