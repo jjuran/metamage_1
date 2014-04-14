3f932d6351245adef06498a7d1a706133d8a6a9d
Josh Juran <jjuran@metamage.com>
2014-04-14 01:59:36 -0700

    [NEW] cmp.68k:  Test CMPA

---
+++ sys/t/m68k/cmp.68k/cmp.68k.cc
@@ -0,0 +1,119 @@
+/*
+	cmp.68k.cc
+	----------
+*/
+
+#ifndef __MC68K__
+#error This unit is for 68K only.
+#endif
+
+// Standard C
+#include <stdint.h>
+
+// tap-out
+#include "tap/test.hh"
+
+
+#pragma exceptions off
+
+
+static const unsigned n_tests = 11;
+
+
+using tap::ok_if;
+
+
+static uint16_t the_CCR = 0;
+
+
+static asm void cmpa_w( uint16_t a : __D0, uint32_t b : __A0 )
+{
+	MOVE.W  the_CCR,D2      ;
+	MOVE    D2,CCR          ;
+	
+	CMPA.W  D0,A0          ;
+	
+	MOVE    SR,the_CCR      ;
+	ANDI.W  #0x00FF,the_CCR ;
+	
+	RTS
+}
+
+static asm void cmpa_l( uint32_t a : __D0, uint32_t b : __A0 )
+{
+	MOVE.W  the_CCR,D2      ;
+	MOVE    D2,CCR          ;
+	
+	CMPA.L  D0,A0          ;
+	
+	MOVE    SR,the_CCR      ;
+	ANDI.W  #0x00FF,the_CCR ;
+	
+	RTS
+}
+
+
+static void cmpa()
+{
+	the_CCR = 0x00;
+	
+	
+	cmpa_l( 0, 0 );
+	
+	ok_if( (the_CCR & 0x1F) == 0x04 );
+	
+	cmpa_l( 0, 1 );
+	
+	ok_if( (the_CCR & 0x1F) == 0x00 );
+	
+	cmpa_l( 1, 0 );
+	
+	ok_if( (the_CCR & 0x1F) == 0x09 );
+	
+	cmpa_l( 0, 0xFFFFFFFF );
+	
+	ok_if( (the_CCR & 0x1F) == 0x08 );
+	
+	cmpa_l( 0xFFFFFFFF, 0 );
+	
+	ok_if( (the_CCR & 0x1F) == 0x01 );
+	
+	
+	cmpa_w( 0, 0 );
+	
+	ok_if( (the_CCR & 0x1F) == 0x04 );
+	
+	cmpa_w( 0, 1 );
+	
+	ok_if( (the_CCR & 0x1F) == 0x00 );
+	
+	cmpa_w( 1, 0 );
+	
+	ok_if( (the_CCR & 0x1F) == 0x09 );
+	
+	cmpa_w( 0, 0xFFFFFFFF );
+	
+	ok_if( (the_CCR & 0x1F) == 0x08 );
+	
+	cmpa_w( 0xFFFFFFFF, 0 );
+	
+	ok_if( (the_CCR & 0x1F) == 0x01 );
+	
+	
+	const uint32_t x = 0x7654abcd;
+	
+	cmpa_w( x, x );  // 0x7654abcd - 0xffffabcd
+	
+	ok_if( (the_CCR & 0x1F) == 0x01 );
+}
+
+
+int main( int argc, char** argv )
+{
+	tap::start( "cmp.68k", n_tests );
+	
+	cmpa();
+	
+	return 0;
+}
+
