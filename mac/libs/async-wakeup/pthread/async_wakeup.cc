1fe9dd08be72ccfa98ebb91ec3494c191a79b0bd
Josh Juran <jjuran@metamage.com>
2014-05-08 02:56:15 -0700

    async_wakeup:  Add stubs for Mach-O
    Obviously this is nonfunctional, but it pacifies the current insanity of
    having to recatalog when switching between the master and hack branches.

---
+++ mac/libs/async-wakeup/pthread/async_wakeup.cc
@@ -0,0 +1,21 @@
+/*
+	async_wakeup.cc
+	---------------
+*/
+
+#include "mac_sys/async_wakeup.hh"
+
+
+namespace mac {
+namespace sys {
+	
+	void request_async_wakeup()
+	{
+	}
+	
+	void clear_async_wakeup()
+	{
+	}
+	
+}
+}
