1bd5015ef384d9264d344fea9effa061a09ee33d
Josh Juran <jjuran@metamage.com>
2014-06-01 12:05:50 -0700

    MacFixup:  Use UPP.h in Threads.h

--- mac/compat/MacFixup/MacFixup/Threads.h
+++ mac/compat/MacFixup/MacFixup/Threads.h
@@ -14,9 +14,8 @@
 #ifndef MACFIXUP_THREADS_H
 #define MACFIXUP_THREADS_H
 
-#ifndef __MIXEDMODE__
-#include <MixedMode.h>
-#endif
+// MacFixup
+#include "UPP.h"
 
 
 #if TARGET_CPU_68K  &&  TARGET_RT_MAC_CFM  &&  !OPAQUE_UPP_TYPES
@@ -63,94 +62,14 @@ enum
 	uppDebuggerThreadSchedulerProcInfo = 0x000000F0
 };
 
-#ifdef __cplusplus
-
-#define DEFINE_UPP_CONSTRUCTOR( Name )                                    \
-inline Name##UPP New##Name##UPP( Name##ProcPtr proc )                     \
-{                                                                         \
-	return (Name##UPP) NewRoutineDescriptor( (ProcPtr) (proc),            \
-	                                         upp##Name##ProcInfo,         \
-	                                         GetCurrentArchitecture() );  \
-}
-
-#define DEFINE_UPP_DESTRUCTOR( Name )                    \
-inline void Dispose##Name##UPP( Name##UPP upp)           \
-{                                                        \
-	DisposeRoutineDescriptor( (UniversalProcPtr) upp );  \
-}
-
-#define DEFINE_UPP_INVOKER_1( Name, R, P1 )                                                \
-inline R Invoke##Name##UPP( P1 p1, Name##UPP upp )                                         \
-{                                                                                          \
-	return (R) CALL_ONE_PARAMETER_UPP( (UniversalProcPtr) upp, upp##Name##ProcInfo, p1 );  \
-}
-
-#define DEFINE_UPP_INVOKER_2( Name, R, P1, P2 )                                                \
-inline R Invoke##Name##UPP( P1 p1, P2 p2, Name##UPP upp )                                      \
-{                                                                                              \
-	return (R) CALL_TWO_PARAMETER_UPP( (UniversalProcPtr) upp, upp##Name##ProcInfo, p1, p2 );  \
-}
-
-DEFINE_UPP_CONSTRUCTOR( ThreadEntry             )
-DEFINE_UPP_CONSTRUCTOR( ThreadScheduler         )
-DEFINE_UPP_CONSTRUCTOR( ThreadSwitch            )
-DEFINE_UPP_CONSTRUCTOR( ThreadTermination       )
-DEFINE_UPP_CONSTRUCTOR( DebuggerNewThread       )
-DEFINE_UPP_CONSTRUCTOR( DebuggerDisposeThread   )
-DEFINE_UPP_CONSTRUCTOR( DebuggerThreadScheduler )
-
-DEFINE_UPP_DESTRUCTOR( ThreadEntry             )
-DEFINE_UPP_DESTRUCTOR( ThreadScheduler         )
-DEFINE_UPP_DESTRUCTOR( ThreadSwitch            )
-DEFINE_UPP_DESTRUCTOR( ThreadTermination       )
-DEFINE_UPP_DESTRUCTOR( DebuggerNewThread       )
-DEFINE_UPP_DESTRUCTOR( DebuggerDisposeThread   )
-DEFINE_UPP_DESTRUCTOR( DebuggerThreadScheduler )
-
-DEFINE_UPP_INVOKER_1( ThreadEntry,             voidPtr,  void*               )
-DEFINE_UPP_INVOKER_1( ThreadScheduler,         ThreadID, SchedulerInfoRecPtr )
-DEFINE_UPP_INVOKER_2( ThreadSwitch,            void,     ThreadID,  void*    )
-DEFINE_UPP_INVOKER_2( ThreadTermination,       void,     ThreadID,  void*    )
-DEFINE_UPP_INVOKER_1( DebuggerNewThread,       void,     ThreadID            )
-DEFINE_UPP_INVOKER_1( DebuggerDisposeThread,   void,     ThreadID            )
-DEFINE_UPP_INVOKER_1( DebuggerThreadScheduler, ThreadID, SchedulerInfoRecPtr )
-
-#undef DEFINE_UPP_CONSTRUCTOR
-#undef DEFINE_UPP_DESTRUCTOR
-
-#undef DEFINE_UPP_INVOKER_1
-#undef DEFINE_UPP_INVOKER_2
-
-#else  // defined(__cplusplus)
-
-#define NewThreadEntryUPP(             proc ) (ThreadEntryUPP)             NewRoutineDescriptor( (ProcPtr) (proc), uppThreadEntryProcInfo,             GetCurrentArchitecture() )
-#define NewThreadSchedulerUPP(         proc ) (ThreadSchedulerUPP)         NewRoutineDescriptor( (ProcPtr) (proc), uppThreadSchedulerProcInfo,         GetCurrentArchitecture() )
-#define NewThreadSwitchUPP(            proc ) (ThreadSwitchUPP)            NewRoutineDescriptor( (ProcPtr) (proc), uppThreadSwitchProcInfo,            GetCurrentArchitecture() )
-#define NewThreadTerminationUPP(       proc ) (ThreadTerminationUPP)       NewRoutineDescriptor( (ProcPtr) (proc), uppThreadTerminationProcInfo,       GetCurrentArchitecture() )
-#define NewDebuggerNewThreadUPP(       proc ) (DebuggerNewThreadUPP)       NewRoutineDescriptor( (ProcPtr) (proc), uppDebuggerNewThreadProcInfo,       GetCurrentArchitecture() )
-#define NewDebuggerDisposeThreadUPP(   proc ) (DebuggerDisposeThreadUPP)   NewRoutineDescriptor( (ProcPtr) (proc), uppDebuggerDisposeThreadProcInfo,   GetCurrentArchitecture() )
-#define NewDebuggerThreadSchedulerUPP( proc ) (DebuggerThreadSchedulerUPP) NewRoutineDescriptor( (ProcPtr) (proc), uppDebuggerThreadSchedulerProcInfo, GetCurrentArchitecture() )
-
-#define DisposeThreadEntryUPP(             upp )  DisposeRoutineDescriptor( (UniversalProcPtr) upp )
-#define DisposeThreadSchedulerUPP(         upp )  DisposeRoutineDescriptor( (UniversalProcPtr) upp )
-#define DisposeThreadSwitchUPP(            upp )  DisposeRoutineDescriptor( (UniversalProcPtr) upp )
-#define DisposeThreadTerminationUPP(       upp )  DisposeRoutineDescriptor( (UniversalProcPtr) upp )
-#define DisposeDebuggerNewThreadUPP(       upp )  DisposeRoutineDescriptor( (UniversalProcPtr) upp )
-#define DisposeDebuggerDisposeThreadUPP(   upp )  DisposeRoutineDescriptor( (UniversalProcPtr) upp )
-#define DisposeDebuggerThreadSchedulerUPP( upp )  DisposeRoutineDescriptor( (UniversalProcPtr) upp )
-
-#define InvokeThreadEntryUPP(             param,         upp )  (voidPtr)  CALL_ONE_PARAMETER_UPP( (UniversalProcPtr) (upp), uppThreadEntryProcInfo,             (param)         )
-#define InvokeThreadSchedulerUPP(         schedulerInfo, upp )  (ThreadID) CALL_ONE_PARAMETER_UPP( (UniversalProcPtr) (upp), uppThreadSchedulerProcInfo,         (schedulerInfo) )
-#define InvokeThreadSwitchUPP(            thread, param, upp )  (void)     CALL_TWO_PARAMETER_UPP( (UniversalProcPtr) (upp), uppThreadSwitchProcInfo,            (thread, param) )
-#define InvokeThreadTerminationUPP(       thread, param, upp )  (void)     CALL_TWO_PARAMETER_UPP( (UniversalProcPtr) (upp), uppThreadTerminationProcInfo,       (thread, param) )
-#define InvokeDebuggerNewThreadUPP(       thread,        upp )  (void)     CALL_ONE_PARAMETER_UPP( (UniversalProcPtr) (upp), uppDebuggerNewThreadProcInfo,       (thread)        )
-#define InvokeDebuggerDisposeThreadUPP(   thread,        upp )  (void)     CALL_ONE_PARAMETER_UPP( (UniversalProcPtr) (upp), uppDebuggerNewThreadProcInfo,       (thread)        )
-#define InvokeDebuggerThreadSchedulerUPP( schedulerInfo, upp )  (ThreadID) CALL_ONE_PARAMETER_UPP( (UniversalProcPtr) (upp), uppDebuggerThreadSchedulerProcInfo, (schedulerInfo) )
-
-
-#endif  // defined(__cplusplus)
+DEFINE_UPP_1( ThreadEntry,             voidPtr,  void*               )
+DEFINE_UPP_1( ThreadScheduler,         ThreadID, SchedulerInfoRecPtr )
+DEFINE_UPP_2( ThreadSwitch,            void,     ThreadID,  void*    )
+DEFINE_UPP_2( ThreadTermination,       void,     ThreadID,  void*    )
+DEFINE_UPP_1( DebuggerNewThread,       void,     ThreadID            )
+DEFINE_UPP_1( DebuggerDisposeThread,   void,     ThreadID            )
+DEFINE_UPP_1( DebuggerThreadScheduler, ThreadID, SchedulerInfoRecPtr )
 
 #endif  // TARGET_CPU_68K  &&  TARGET_RT_MAC_CFM  && !OPAQUE_UPP_TYPES
 
 #endif
-
