c15c6288871f90ea5fe3263c29322d1af85f4fac
Josh Juran <jjuran@metamage.com>
2014-06-04 17:39:21 -0700

    [FIX] Genie:  Don't assert non-null syscall stack
    We now allow an unallocated stack under CONFIG_SYSCALL_STACKS.

--- lamp/Genie/Genie/api/stack.cc
+++ lamp/Genie/Genie/api/stack.cc
@@ -8,9 +8,6 @@
 // Relix
 #include "relix/task/syscall_stack_base.hh"
 
-// Debug
-#include "debug/assert.hh"
-
 // Genie
 #include "Genie/current_process.hh"
 #include "Genie/Process.hh"
@@ -20,7 +17,10 @@ void* current_stack_base()
 {
 	void* const memory = Genie::current_process().get_syscall_stack_memory();
 	
-	ASSERT( memory != NULL );
+	if ( memory == NULL )
+	{
+		return NULL;
+	}
 	
 	return relix::base_address_of_syscall_stack( memory );
 }
