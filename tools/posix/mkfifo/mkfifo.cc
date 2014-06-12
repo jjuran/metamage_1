ceea5c53642a385c445c7141dd45d57212e709f8
Josh Juran <jjuran@metamage.com>
2014-06-11 22:04:32 -0700

    [NEW] mkfifo

---
+++ tools/posix/mkfifo/mkfifo.cc
@@ -0,0 +1,62 @@
+/*
+	mkfifo.cc
+	---------
+*/
+
+// POSIX
+#include <unistd.h>
+#include <sys/stat.h>
+
+// Standard C
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define PROGRAM  "mkfifo"
+
+#define USAGE  "usage: " PROGRAM " path ...\n"
+
+#define STR_LEN( s )  "" s, (sizeof s - 1)
+
+
+#define CHECK_N( call )  check_n( call, #call )
+
+static int exit_status = 0;
+
+static inline int check_n( int result, const char* code )
+{
+	if ( result < 0 )
+	{
+		fprintf( stderr, PROGRAM " error: %s: %s\n", code, strerror( errno ) );
+		
+		exit_status = 1;
+	}
+	
+	return result;
+}
+
+static int usage()
+{
+	write( STDERR_FILENO, STR_LEN( USAGE ) );
+	
+	return 2;
+}
+
+int main( int argc, char** argv )
+{
+	if ( argc < 1 + 1 )
+	{
+		return usage();
+	}
+	
+	for ( int i = 1;  i < argc;  ++i )
+	{
+		const char* path = argv[ i ];
+		
+		CHECK_N( mknod( path, S_IFIFO | 0666, 0 ) );
+	}
+	
+	return exit_status;
+}
