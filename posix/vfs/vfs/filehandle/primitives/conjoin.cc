30004100626ddafcb449e9deae21a1ffad1c9124
Josh Juran <jjuran@metamage.com>
2014-07-02 07:56:46 -0700

    vfs:  Check conjoin method in conjoin() primitive

--- posix/vfs/vfs/filehandle/primitives/conjoin.cc
+++ posix/vfs/vfs/filehandle/primitives/conjoin.cc
@@ -7,6 +7,8 @@
 
 // vfs
 #include "vfs/filehandle.hh"
+#include "vfs/filehandle/methods/filehandle_method_set.hh"
+#include "vfs/filehandle/methods/general_method_set.hh"
 
 
 namespace vfs
@@ -14,6 +16,19 @@ namespace vfs
 	
 	void conjoin( filehandle& that, filehandle& target )
 	{
+		if ( const filehandle_method_set* methods = that.methods() )
+		{
+			if ( const general_method_set* general_methods = methods->general_methods )
+			{
+				if ( general_methods->conjoin )
+				{
+					general_methods->conjoin( that, target );
+					
+					return;
+				}
+			}
+		}
+		
 		that.Attach( &target );
 	}
 	
