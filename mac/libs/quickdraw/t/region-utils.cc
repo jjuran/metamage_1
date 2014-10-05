32b518cf3c9b2b41c6ab76c58127f6653e51e8b2
Josh Juran <jjuran@metamage.com>
2014-10-04 17:59:20 -0700

    qd-tests:  Test quickdraw::set_region_bbox()

--- mac/libs/quickdraw/t/region-utils.cc
+++ mac/libs/quickdraw/t/region-utils.cc
@@ -4,6 +4,7 @@
 */
 
 // quickdraw
+#include "qd/rect.hh"
 #include "qd/regions.hh"
 
 // tap-out
@@ -13,7 +14,7 @@
 #pragma exceptions off
 
 
-static const unsigned n_tests = 4;
+static const unsigned n_tests = 4 + 2;
 
 
 typedef quickdraw::region_geometry_t  geometry_t;
@@ -53,12 +54,51 @@ static void geometry()
 	EXPECT( g.n_h_coords == 4 );
 }
 
+static void bbox()
+{
+	Rect bbox;
+	
+	const short dexter[] =
+	{
+		2, 1, 8, 64,
+		
+		2,  1, 4,          0x7FFF,
+		3,  1, 4,          0x7FFF,
+		5,        16, 64,  0x7FFF,
+		8,        16, 64,  0x7FFF,
+		
+		0x7FFF
+	};
+	
+	quickdraw::set_region_bbox( bbox, dexter + 4 );
+	
+	EXPECT_CMP( &bbox, sizeof bbox, dexter, sizeof bbox );
+	
+	const short sinister[] =
+	{
+		2, 1, 8, 64,
+		
+		2,        16, 64,  0x7FFF,
+		3,        16, 64,  0x7FFF,
+		5,  1, 4,          0x7FFF,
+		8,  1, 4,          0x7FFF,
+		
+		0x7FFF
+	};
+	
+	quickdraw::set_region_bbox( bbox, sinister + 4 );
+	
+	EXPECT_CMP( &bbox, sizeof bbox, sinister, sizeof bbox );
+}
+
 
 int main( int argc, char** argv )
 {
 	tap::start( "region-utils", n_tests );
 	
 	geometry();
+	
+	bbox();
 	
 	return 0;
 }
