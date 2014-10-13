af89f95fbb7b5c821137f1b4729abfef341fda68
Josh Juran <jjuran@metamage.com>
2014-10-13 05:36:52 -0700

    macos:  Create new WMgrPort in InitWindows()

--- v68k/modules/macos/Windows.cc
+++ v68k/modules/macos/Windows.cc
@@ -5,7 +5,21 @@
 
 #include "Windows.hh"
 
+// Mac OS
+#ifndef __MACMEMORY__
+#include <MacMemory.h>
+#endif
+#ifndef __QUICKDRAW__
+#include <Quickdraw.h>
+#endif
+
+
+GrafPtr WMgrPort : 0x09DE;
+
 
 pascal void InitWindows_patch()
 {
+	WMgrPort = (GrafPtr) NewPtr( sizeof (GrafPort) );
+	
+	OpenPort( WMgrPort );
 }
