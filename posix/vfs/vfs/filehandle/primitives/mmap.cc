4963cb6a55c3ecee0e0d3f40f3479569065de857
Josh Juran <jjuran@metamage.com>
2015-01-21 22:51:30 -0800

    vfs:  Check file access in mmap()

--- posix/vfs/vfs/filehandle/primitives/mmap.cc
+++ posix/vfs/vfs/filehandle/primitives/mmap.cc
@@ -5,6 +5,10 @@
 
 #include "vfs/filehandle/primitives/mmap.hh"
 
+// POSIX
+#include <fcntl.h>
+#include <sys/mman.h>
+
 // poseven
 #include "poseven/types/errno_t.hh"
 
@@ -23,8 +27,30 @@ namespace vfs
 	namespace p7 = poseven;
 	
 	
+	static inline bool needs_write_access( int prot, int flags )
+	{
+		return (prot & PROT_WRITE)  &&  (flags & MAP_SHARED);
+	}
+	
 	memory_mapping_ptr mmap( filehandle* that, size_t length, int prot, int flags, off_t offset )
 	{
+		const int accmode = that->get_flags() & O_ACCMODE;
+		
+		if ( accmode == O_RDWR )
+		{
+			// reading and writing both allowed
+		}
+		else if ( accmode == O_RDONLY  &&  !needs_write_access( prot, flags ) )
+		{
+			// read-only, but write access isn't required
+		}
+		else
+		{
+			// no read access, or read-only with PROT_WRITE / MAP_SHARED
+			
+			p7::throw_errno( EACCES );
+		}
+		
 		if ( const filehandle_method_set* methods = that->methods() )
 		{
 			if ( const general_method_set* general_methods = methods->general_methods )
