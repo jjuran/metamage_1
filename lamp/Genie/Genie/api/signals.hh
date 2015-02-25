a5ca97494971cda071cdc24ceee338cfc0d7a4ec
Josh Juran <jjuran@metamage.com>
2015-02-24 21:45:53 -0800

    Genie:  Remove unused Genie::broken_pipe import

--- lamp/Genie/Genie/api/signals.hh
+++ lamp/Genie/Genie/api/signals.hh
@@ -6,16 +6,11 @@
 #ifndef GENIE_API_SIGNALS_HH
 #define GENIE_API_SIGNALS_HH
 
-// relix-kernel
-#include "relix/api/broken_pipe.hh"
-
 
 namespace Genie
 {
 	
 	bool check_signals( bool may_throw );
-	
-	using relix::broken_pipe;
 	
 }
 
