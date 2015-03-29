658a9c2a6ab95a3210ba3f917566757a42b7593c
Josh Juran <jjuran@metamage.com>
2015-03-29 05:07:31 -0700

    plus:  Add integer hex() function

---
+++ base/plus/plus/integer_hex.cc
@@ -0,0 +1,77 @@
+/*
+	integer_hex.cc
+	--------------
+*/
+
+#include "plus/integer_hex.hh"
+
+// iota
+#include "iota/endian.hh"
+
+// plus
+#include "plus/hexadecimal.hh"
+
+
+namespace plus
+{
+	
+	static inline
+	iota::span straight_int_buffer( const integer& x )
+	{
+		return x.buffer();
+	}
+	
+	static
+	string reversed_int_buffer( const integer& x )
+	{
+		iota::span buffer = x.buffer();
+		
+		const char* p = buffer.data();
+		unsigned long n_bytes = buffer.size();
+		
+		string result;
+		
+		char* begin = result.reset( n_bytes );
+		
+		char* q = begin + n_bytes;
+		
+		while ( n_bytes-- > 0 )
+		{
+			// Fill the result with the reversed buffer.
+			*--q = *p++;
+		}
+		
+		return result;
+	}
+	
+	template < class Buffer >
+	static
+	string hex_from_int_buffer( const Buffer& buffer )
+	{
+		const char* p = buffer.data();
+		const char* q = buffer.size() + p;
+		
+		// Omit leading zero bytes so the result is not dependent on limb size.
+		
+		while ( *p == 0 )
+		{
+			++p;
+		}
+		
+		return hex_lower( p, q - p );
+	}
+	
+	string hex( const integer& x )
+	{
+		if ( x.is_zero() )
+		{
+			return string::null;
+		}
+		
+		const bool le = iota::is_little_endian();
+		
+		return le ? hex_from_int_buffer( reversed_int_buffer( x ) )
+		          : hex_from_int_buffer( straight_int_buffer( x ) );
+	}
+	
+}
---
+++ base/plus/plus/integer_hex.hh
@@ -0,0 +1,21 @@
+/*
+	integer_hex.hh
+	--------------
+*/
+
+#ifndef PLUS_INTEGERHEX_HH
+#define PLUS_INTEGERHEX_HH
+
+// plus
+#include "plus/integer.hh"
+#include "plus/string.hh"
+
+
+namespace plus
+{
+	
+	string hex( const integer& x );
+	
+}
+
+#endif
