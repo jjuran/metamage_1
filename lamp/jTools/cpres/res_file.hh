372a8b5d3410d40c0302d9136d23c853e3f3b389
Josh Juran <jjuran@metamage.com>
2015-05-16 04:35:29 -0700

    cpres:  Use Unicode filing routines when available
    This changes the behavior of the --data option:  Previously, if a source
    file's data fork couldn't be opened as a resource file (for any reason),
    cpres would fall back to the resource fork.  Now it doesn't.  (A-line is
    unaffected since it already Rezzes into the data fork for Mach-O, so the
    source and destination for cpres use the same fork.)

---
+++ lamp/jTools/cpres/res_file.hh
@@ -0,0 +1,28 @@
+/*
+	res_file.hh
+	-----------
+*/
+
+#ifndef CPRES_RESFILE_HH
+#define CPRES_RESFILE_HH
+
+// Nitrogen
+#ifndef NITROGEN_RESOURCES_HH
+#include "Nitrogen/Resources.hh"
+#endif
+
+
+enum ForkType
+{
+	dataFork,
+	rsrcFork,
+};
+
+
+nucleus::owned< Nitrogen::ResFileRefNum >
+open_res_file( const char* path, ForkType fork );
+
+nucleus::owned< Nitrogen::ResFileRefNum >
+open_res_file( const char* path, ForkType fork, bool exists );
+
+#endif
