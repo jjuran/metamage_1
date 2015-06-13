3379cd8e8b8a41f48aa6a7bcf092e64632d1afef
Josh Juran <jjuran@metamage.com>
2015-06-13 10:08:34 -0700

    plus:  Add memoized nth_power_of_ten()

---
+++ base/plus/plus/nth_power_of_ten.cc
@@ -0,0 +1,41 @@
+/*
+	nth_power_of_ten.cc
+	-------------------
+*/
+
+#include "plus/nth_power_of_ten.hh"
+
+// Standard C++
+#include <vector>
+
+
+namespace plus
+{
+	
+	static std::vector< integer > memoized_powers_of_ten;
+	
+	
+	const integer& nth_power_of_ten( unsigned n )
+	{
+		if ( n >= memoized_powers_of_ten.size() )
+		{
+			const integer ten = 10;
+			
+			if ( memoized_powers_of_ten.empty() )
+			{
+				memoized_powers_of_ten.push_back(   1 );
+				memoized_powers_of_ten.push_back( ten );
+			}
+			
+			for ( unsigned i = memoized_powers_of_ten.size();  i <= n;  ++i )
+			{
+				const integer& last = memoized_powers_of_ten.back();
+				
+				memoized_powers_of_ten.push_back( last * ten );
+			}
+		}
+		
+		return memoized_powers_of_ten[ n ];
+	}
+	
+}
---
+++ base/plus/plus/nth_power_of_ten.hh
@@ -0,0 +1,20 @@
+/*
+	nth_power_of_ten.hh
+	-------------------
+*/
+
+#ifndef PLUS_NTHPOWEROFTEN_HH
+#define PLUS_NTHPOWEROFTEN_HH
+
+// plus
+#include "plus/integer.hh"
+
+
+namespace plus
+{
+	
+	const integer& nth_power_of_ten( unsigned n );
+	
+}
+
+#endif
