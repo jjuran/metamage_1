b123d9ce97c91cdc08feda3a310a31dca63282b6
Josh Juran <jjuran@metamage.com>
2015-07-07 02:19:54 -0400

    [NEW] md5:  New MD5 library
    Basically, this is the MD5 library renamed with an API cleanup and a few
    tweaks under the hood.  On case-insensitive filesystems (like HFS+), the
    two projects conflict and can't be built simultaneously.

---
+++ crypto/md5/md5/md5.cc
@@ -0,0 +1,164 @@
+/*
+	md5.cc
+	------
+*/
+
+/*
+	See RFC 1321, "The MD5 Message-Digest Algorithm".
+	<http://www.faqs.org/rfcs/rfc1321.html>
+*/
+
+#include "md5/md5.hh"
+
+// Standard C
+#include <string.h>
+
+// iota
+#include "iota/endian.hh"
+
+// md5
+#include "md5/rounds.hh"
+
+
+namespace crypto
+{
+	
+	using iota::swap_4_bytes;
+	using iota::little_u32;
+	
+	
+	void md5_init( md5_state& state )
+	{
+		state.n_blocks = 0;
+		
+		state.digest.a = swap_4_bytes( 0x01234567 );
+		state.digest.b = swap_4_bytes( 0x89abcdef );
+		state.digest.c = swap_4_bytes( 0xfedcba98 );
+		state.digest.d = swap_4_bytes( 0x76543210 );
+	}
+	
+	void md5_digest_block( md5_state& state, void const* data )
+	{
+		u32 block[ 16 ];
+		
+		if ( iota::is_little_endian() )
+		{
+			// Ensure the block is word-aligned.
+			
+			memcpy( block, data, sizeof block );
+		}
+		else
+		{
+			// Byte-swap each word.
+			
+			const char* p = (const char*) data;
+			
+			char* q = (char*) block;
+			
+			for ( int i = 0;  i < 16; ++i )
+			{
+				q += 4;
+				
+				char* r = q;
+				
+				*--r = *p++;
+				*--r = *p++;
+				*--r = *p++;
+				*--r = *p++;
+			}
+		}
+		
+		md5_rounds( state.digest, block );
+		
+		++state.n_blocks;
+	}
+	
+	void md5_finish( md5_state&   state,
+	                 void const*  data,
+	                 size_t       n_bytes,
+	                 int          n_more_bits )
+	{
+		typedef unsigned char u8;
+		
+		const unsigned n_bits = n_bytes * 8 + n_more_bits;
+		
+		const u64 total_bits = state.n_blocks * 512 + n_bits;
+		
+		const unsigned overflow_bits = (n_bits + 64) % 512;
+		const unsigned bits_to_pad = 512 - overflow_bits;  // between 1 and 512
+		const unsigned n_final_bits = n_bits + bits_to_pad + 64;
+		
+		const unsigned n_final_blocks = n_final_bits / 512;
+		
+		const bool using_both_blocks = n_final_blocks - 1;
+		
+		u64  final_data_u64[ 16 ] = { 0 };  // ensure 64-bit alignment
+		u32* final_data = (u32*) final_data_u64;
+		
+		u32* const end_block = final_data + 16;
+		u32* const pad_block = using_both_blocks ? final_data : end_block;
+		
+		memcpy( pad_block, data, n_bytes );
+		
+		u8* const pad_byte_ptr = (u8*) pad_block + n_bytes;
+		
+		// Extra input bits and padding bits.
+		
+		// Make a mask with ones where the extra bits are, and zeroes afterword.
+		unsigned char bits_mask = (0xFF << (8 - n_more_bits));
+		
+		// The first padding byte has leftover bits, if any, and a 1 appended.
+		
+		*pad_byte_ptr &= bits_mask;
+		*pad_byte_ptr |= (0x80 >> n_more_bits);
+		
+		// Append the bit length
+		if ( iota::is_little_endian() )
+		{
+			final_data_u64[ 16 - 1 ] = total_bits;
+		}
+		else
+		{
+			u32* const length_ptr = final_data + 32 - 2;
+			
+			u32 const* const total_ptr = (u32*) &total_bits;
+			
+			length_ptr[ 0 ] = little_u32( total_ptr[ 1 ] );
+			length_ptr[ 1 ] = little_u32( total_ptr[ 0 ] );
+		}
+		
+		if ( using_both_blocks )
+		{
+			md5_digest_block( state, final_data );
+		}
+		
+		md5_digest_block( state, end_block );
+		
+		md5_digest& digest = state.digest;
+		
+		digest.a = little_u32( digest.a );
+		digest.b = little_u32( digest.b );
+		digest.c = little_u32( digest.c );
+		digest.d = little_u32( digest.d );
+	}
+	
+	md5_digest md5( const void* data, size_t n_bytes, int n_more_bits )
+	{
+		const unsigned n_last_bytes = n_bytes % 64;
+		
+		const char* p   = (const char*) data;
+		const char* end = (const char*) data + n_bytes - n_last_bytes;
+		
+		md5_engine engine;
+		
+		while ( p < end )
+		{
+			engine.digest_block( p );
+			
+			p += 64;
+		}
+		
+		return engine.finish( p, n_last_bytes, n_more_bits );
+	}
+	
+}  // namespace crypto
---
+++ crypto/md5/md5/rounds.68k.cc
@@ -0,0 +1,204 @@
+/*
+	rounds.cc
+	---------
+*/
+
+/*
+	See RFC 1321, "The MD5 Message-Digest Algorithm".
+	<http://www.faqs.org/rfcs/rfc1321.html>
+*/
+
+// md5
+#include "md5/rounds.hh"
+#include "md5/table.hh"
+
+
+namespace crypto
+{
+	
+#ifdef __MC68K__
+	
+	asm
+	void md5_rounds( md5_digest& digest, u32* const block )
+	{
+		LINK     A6,#0
+		MOVEM.L  D3-D7/A2-A4,-(SP)
+		
+		MOVEA.L  8(A6),A0
+		MOVEA.L  12(A6),A1
+		
+		MOVE.L   (A0)+,D0
+		MOVE.L   (A0)+,D1
+		MOVE.L   (A0)+,D2
+		MOVE.L   (A0)+,D3
+		
+		LEA      md5_table,A2
+		
+		MOVEQ.L  #0,D7
+		
+	round1:
+		MOVEQ.L  #7,D6
+		
+	round1_inner:
+		// F
+		MOVE.L   D1,D4
+		AND.L    D2,D4
+		MOVE.L   D1,D5
+		NOT.L    D5
+		AND.L    D3,D5
+		OR.L     D5,D4
+		
+		ADD.L    D4,D0
+		
+		ADD.L    (A1,D7.W),D0
+		ADD.L    (A2,D7.W),D0
+		ROL.L    D6,D0
+		ADD.L    D1,D0
+		
+		ADDQ.W   #5,D6
+		
+		ADDQ.W   #4,D7
+		EXG      D3,D2
+		EXG      D2,D1
+		EXG      D1,D0
+		
+		CMPI.W   #22,D6
+		BLE.S    round1_inner
+		CMPI.W   #64,D7
+		BLT.S    round1
+		
+	round2:
+		MOVEQ.L  #5,D6
+		MOVEA.W  #4,A3
+		MOVEA.L  A3,A4
+		
+	round2_inner:
+		
+		// G
+		MOVE.L   D3,D4
+		AND.L    D1,D4
+		MOVE.L   D3,D5
+		NOT.L    D5
+		AND.L    D2,D5
+		OR.L     D5,D4
+		
+		ADD.L    D4,D0
+		
+		MOVE.L   A4,D4
+		ADD.L    D7,D4
+		ANDI.L   #0x3F,D4
+		
+		ADD.L    (A1,D4.W),D0
+		ADD.L    (A2,D7.W),D0
+		ROL.L    D6,D0
+		ADD.L    D1,D0
+		
+		ADD.W    A3,D6
+		ADDQ.L   #1,A3
+		ADDA.W   #16,A4
+		
+		ADDQ.W   #4,D7
+		EXG      D3,D2
+		EXG      D2,D1
+		EXG      D1,D0
+		
+		CMPI.W   #20,D6
+		BLE.S    round2_inner
+		CMPI.W   #128,D7
+		BLT.S    round2
+		
+		MOVEQ.L  #7,D5
+	round3:
+		MOVEQ.L  #4,D6
+		MOVEA.W  #20,A4
+		
+	round3_inner:
+		
+		// H
+		MOVE.L   D1,D4
+		EOR.L    D2,D4
+		EOR.L    D3,D4
+		
+		ADD.L    D4,D0
+		
+		MOVE.L   A4,D4
+		SUB.L    D7,D4
+		ANDI.L   #0x3F,D4
+		
+		ADD.L    (A1,D4.W),D0
+		ADD.L    (A2,D7.W),D0
+		ROL.L    D6,D0
+		ADD.L    D1,D0
+		
+		ADD.W    D5,D6
+		EORI.L   #2,D5
+		ADDA.W   #16,A4
+		
+		ADDQ.W   #4,D7
+		EXG      D3,D2
+		EXG      D2,D1
+		EXG      D1,D0
+		
+		CMPI.W   #23,D6
+		BLE.S    round3_inner
+		CMPI.W   #192,D7
+		BLT.S    round3
+		
+		MOVEQ.L  #0,D5
+	round4:
+		MOVEQ.L  #6,D6
+		MOVEA.W  #4,A3
+		
+	round4_inner:
+		
+		// I
+		MOVE.L   D3,D4
+		NOT.L    D4
+		OR.L     D1,D4
+		EOR.L    D2,D4
+		
+		ADD.L    D4,D0
+		
+		MOVE.L   D5,D4
+		SUB.L    D7,D4
+		ANDI.L   #0x3F,D4
+		
+		ADD.L    (A1,D4.W),D0
+		ADD.L    (A2,D7.W),D0
+		ROL.L    D6,D0
+		ADD.L    D1,D0
+		
+		ADD.W    A3,D6
+		ADDQ.L   #1,A3
+		EORI.W   #32,D5
+		
+		ADDQ.W   #4,D7
+		EXG      D3,D2
+		EXG      D2,D1
+		EXG      D1,D0
+		
+		CMPI.W   #21,D6
+		BLE.S    round4_inner
+		CMPI.W   #256,D7
+		BLT.S    round4
+		
+		// Accumulate the result.
+		ADD.L    D3,-(A0)
+		ADD.L    D2,-(A0)
+		ADD.L    D1,-(A0)
+		ADD.L    D0,-(A0)
+		
+		MOVEM.L  (SP)+,D3-D7/A2-A4
+		UNLK     A6
+		RTS
+	}
+	
+#elif defined( __GNUC__ )
+	
+	void dummy()
+	{
+	}
+	
+#endif
+	
+}  // namespace crypto
