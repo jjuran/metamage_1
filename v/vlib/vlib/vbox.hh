21a4a271af211dc4e813bbf2f4682f1e890547ff
Josh Juran <jjuran@metamage.com>
2015-11-23 01:31:14 -0500

    vlib:  Allow vbox-inline 64/32/16/8-bit ints, bool

--- v/vlib/vlib/vbox.hh
+++ v/vlib/vlib/vbox.hh
@@ -6,6 +6,9 @@
 #ifndef VLIB_VBOX_HH
 #define VLIB_VBOX_HH
 
+// Standard C
+#include <stdint.h>
+
 // plus
 #include "plus/datum_storage.hh"
 #include "plus/ibox.hh"
@@ -20,6 +23,15 @@ namespace vlib
 	{
 		Vbox_alloc = -1,
 		Vbox_empty,
+		Vbox_bool,
+		Vbox_i8,
+		Vbox_u8,
+		Vbox_i16,
+		Vbox_u16,
+		Vbox_i32,
+		Vbox_u32,
+		Vbox_i64,
+		Vbox_u64,
 		Vbox_bigint,
 		Vbox_string,
 		Vbox_pointer,
@@ -39,12 +51,26 @@ namespace vlib
 		signed char data[ sizeof (vu_string) ];
 	};
 	
+	union vu_ints
+	{
+		uint64_t  u64;
+		int64_t   i64;
+		uint32_t  u32;
+		int32_t   i32;
+		uint16_t  u16;
+		int16_t   i16;
+		uint8_t   u8;
+		int8_t    i8;
+		bool      boolean;
+	};
+	
 	union vu
 	{
 		vu_alloc   alloc;
 		vu_string  string;
 		vu_chars   chars;
 		vu_ibox    ibox;
+		vu_ints    ints;
 	};
 	
 	class vbox
@@ -76,6 +102,69 @@ namespace vlib
 				u.alloc.semantics = semantics;
 			}
 			
+			vbox( bool x, char semantics )
+			{
+				u.ints.boolean    = x;
+				u.alloc.type      = Vbox_bool;
+				u.alloc.semantics = semantics;
+			}
+			
+			vbox( int8_t x, char semantics )
+			{
+				u.ints.i8         = x;
+				u.alloc.type      = Vbox_i8;
+				u.alloc.semantics = semantics;
+			}
+			
+			vbox( uint8_t x, char semantics )
+			{
+				u.ints.u8         = x;
+				u.alloc.type      = Vbox_u8;
+				u.alloc.semantics = semantics;
+			}
+			
+			vbox( int16_t x, char semantics )
+			{
+				u.ints.i16        = x;
+				u.alloc.type      = Vbox_i16;
+				u.alloc.semantics = semantics;
+			}
+			
+			vbox( uint16_t x, char semantics )
+			{
+				u.ints.u16        = x;
+				u.alloc.type      = Vbox_u16;
+				u.alloc.semantics = semantics;
+			}
+			
+			vbox( int32_t x, char semantics )
+			{
+				u.ints.i32        = x;
+				u.alloc.type      = Vbox_i32;
+				u.alloc.semantics = semantics;
+			}
+			
+			vbox( uint32_t x, char semantics )
+			{
+				u.ints.u32        = x;
+				u.alloc.type      = Vbox_u32;
+				u.alloc.semantics = semantics;
+			}
+			
+			vbox( int64_t x, char semantics )
+			{
+				u.ints.i64        = x;
+				u.alloc.type      = Vbox_i64;
+				u.alloc.semantics = semantics;
+			}
+			
+			vbox( uint64_t x, char semantics )
+			{
+				u.ints.u64        = x;
+				u.alloc.type      = Vbox_u64;
+				u.alloc.semantics = semantics;
+			}
+			
 			vbox( const vu_ibox& ix, char semantics );
 			
 			vbox( const vu_string& s, char semantics );
@@ -102,6 +191,17 @@ namespace vlib
 			vbox_type type() const  { return vbox_type( u.alloc.type ); }
 			
 			char semantics() const  { return u.alloc.semantics; }
+			
+			bool boolean() const  { return u.ints.boolean; }
+			
+			int8_t   i8()  const  { return u.ints.i8;  }
+			uint8_t  u8()  const  { return u.ints.u8;  }
+			int16_t  i16() const  { return u.ints.i16; }
+			uint16_t u16() const  { return u.ints.u16; }
+			int32_t  i32() const  { return u.ints.i32; }
+			uint32_t u32() const  { return u.ints.u32; }
+			int64_t  i64() const  { return u.ints.i64; }
+			uint64_t u64() const  { return u.ints.u64; }
 			
 			const void* pointer() const  { return u.alloc.pointer; }
 			
