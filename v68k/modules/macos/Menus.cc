6216e0b6cb86529b782fc2df41ff62d8a8e6e170
Josh Juran <jjuran@metamage.com>
2016-02-23 09:34:10 -0500

    macos:  Initialize MBarHeight in InitMenus()

--- v68k/modules/macos/Menus.cc
+++ v68k/modules/macos/Menus.cc
@@ -5,7 +5,15 @@
 
 #include "Menus.hh"
 
+// macos
+#include "QDGlobals.hh"
+
+
+short MBarHeight : 0x0BAA;
+
+const short default_MBarHeight = 20;
 
 pascal void InitMenus_patch()
 {
+	MBarHeight = default_MBarHeight;
 }
