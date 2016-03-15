d9125041f268c19d17303c2d029b4b7f044c6085
Josh Juran <jjuran@metamage.com>
2016-03-15 02:31:59 -0400

    [NEW] app:  Launch an application in the emulator

---
+++ v68k/tools/app/app.cc
@@ -0,0 +1,54 @@
+/*
+	app.cc
+	------
+*/
+
+// Standard C
+#include <string.h>
+
+
+typedef unsigned char Str31[ 32 ];
+
+struct LaunchParamBlockRec
+{
+	void* appName;
+	short reserved2;
+	// ...
+};
+
+static inline
+short asm Launch( void* pb : __A0 ) : __D0
+{
+	DC.W     0xA9F2  // _Launch
+}
+
+int main( int argc, char** argv )
+{
+	if ( argc < 2 )
+	{
+		return 0;
+	}
+	
+	const char* path = argv[ 1 ];
+	
+	const size_t len = strlen( path );
+	
+	if ( len > 31 )
+	{
+		return 2;
+	}
+	
+	Str31 appName;
+	
+	unsigned char* p = appName;
+	
+	*p++ = len;
+	
+	memcpy( p, path, len );
+	
+	LaunchParamBlockRec pb = { appName };
+	
+	Launch( &pb );
+	
+	return 1;
+}
