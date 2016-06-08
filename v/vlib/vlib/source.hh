ec8ded54082818077f2818c3c86f2734cec74c78
Josh Juran <jjuran@metamage.com>
2016-06-08 14:56:15 -0400

    vlib:  Use a string object in source_spec

--- v/vlib/vlib/source.hh
+++ v/vlib/vlib/source.hh
@@ -6,6 +6,9 @@
 #ifndef VLIB_SOURCE_HH
 #define VLIB_SOURCE_HH
 
+// plus
+#include "plus/string.hh"
+
 
 namespace vlib
 {
@@ -13,8 +16,8 @@ namespace vlib
 	class source_spec
 	{
 		private:
-			const char*  its_file;
-			unsigned     its_line;
+			plus::string  its_file;
+			unsigned      its_line;
 		
 		public:
 			source_spec( const char* path ) : its_file(), its_line()
@@ -28,8 +31,8 @@ namespace vlib
 			
 			bool empty() const  { return its_line == 0; }
 			
-			const char* file() const  { return its_file; };
-			unsigned    line() const  { return its_line; };
+			const plus::string& file() const  { return its_file; };
+			unsigned            line() const  { return its_line; };
 			
 			void next_line()  { ++its_line; }
 	};
