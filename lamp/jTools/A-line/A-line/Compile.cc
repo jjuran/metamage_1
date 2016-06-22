d469a0df2321b9cc55fe9a7ba43c09a13699e4b9
Josh Juran <jjuran@metamage.com>
2016-06-22 01:21:16 -0400

    A-line, Compile::Driver:  Define CONFIG_DEBUGGING
    In addition to TARGET_CONFIG_DEBUGGING, that is.  The "TARGET" prefix is
    an Apple idiom which we're discontinuing.  Elsewhere we use "CONFIG_FOO"
    by itself.

--- lamp/jTools/A-line/A-line/Compile.cc
+++ lamp/jTools/A-line/A-line/Compile.cc
@@ -605,10 +605,12 @@ namespace tool
 		
 		if ( target_info.build != buildRelease )
 		{
+			options.DEFINE_MACRO_VALUE( "CONFIG_DEBUGGING", 1 );
 			options.DEFINE_MACRO_VALUE( "TARGET_CONFIG_DEBUGGING", 1 );
 		}
 		else
 		{
+			options.DEFINE_MACRO_VALUE( "CONFIG_DEBUGGING", 0 );
 			options.DEFINE_MACRO_VALUE( "TARGET_CONFIG_DEBUGGING", 0 );
 		}
 	}
--- tools/perl/Compile/Driver/Job/Compile.pm
+++ tools/perl/Compile/Driver/Job/Compile.pm
@@ -106,6 +106,7 @@ sub command
 	
 	my %d;
 	
+	$d{ CONFIG_DEBUGGING } = $conf->debugging + 0;
 	$d{ TARGET_CONFIG_DEBUGGING } = $conf->debugging + 0;
 	
 	if ( $conf->is_carbon )
