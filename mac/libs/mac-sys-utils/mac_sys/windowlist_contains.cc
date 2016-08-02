60d0c0e19bffcd2be555a5b171df4999474f3ea6
Josh Juran <jjuran@metamage.com>
2016-08-02 02:37:44 -0400

    mac-sys-utils:  Add windowlist_contains()

---
+++ mac/libs/mac-sys-utils/mac_sys/windowlist_contains.cc
@@ -0,0 +1,40 @@
+/*
+	windowlist_contains.hh
+	----------------------
+*/
+
+#include "mac_sys/windowlist_contains.hh"
+
+// Mac OS X
+#ifdef __APPLE__
+#include <Carbon/Carbon.h>
+#endif
+
+// Mac OS
+#ifndef __MACWINDOWS__
+#include <MacWindows.h>
+#endif
+
+
+namespace mac {
+namespace sys {
+	
+	bool windowlist_contains( void* window )
+	{
+		WindowRef w = GetWindowList();
+		
+		while ( w != NULL )
+		{
+			if ( w == window )
+			{
+				return true;
+			}
+			
+			w = GetNextWindow( w );
+		}
+		
+		return false;
+	}
+	
+}
+}
