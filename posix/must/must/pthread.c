d7d607b6344b7926a3482f9191269bedc134fd3e
Josh Juran <jjuran@metamage.com>
2016-09-03 16:15:58 -0400

    must:  Add some pthread condvar routines

--- posix/must/must/pthread.c
+++ posix/must/must/pthread.c
@@ -28,3 +28,23 @@ void must_pthread_mutex_unlock( pthread_mutex_t* mutex )
 		abort();
 	}
 }
+
+void must_pthread_cond_wait( pthread_cond_t* cond, pthread_mutex_t* mutex )
+{
+	int error = pthread_cond_wait( cond, mutex );
+	
+	if ( error )
+	{
+		abort();
+	}
+}
+
+void must_pthread_cond_broadcast( pthread_cond_t* cond )
+{
+	int error = pthread_cond_broadcast( cond );
+	
+	if ( error )
+	{
+		abort();
+	}
+}
--- posix/must/must/pthread.h
+++ posix/must/must/pthread.h
@@ -18,6 +18,10 @@ extern "C" {
 void must_pthread_mutex_lock  ( pthread_mutex_t* mutex );
 void must_pthread_mutex_unlock( pthread_mutex_t* mutex );
 
+void must_pthread_cond_wait( pthread_cond_t* cond, pthread_mutex_t* mutex );
+
+void must_pthread_cond_broadcast( pthread_cond_t* cond );
+
 
 #ifdef __cplusplus
 }
