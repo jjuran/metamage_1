7dc185bc6cec028ae9ddfbdb36aa5a9107e0d7ca
Josh Juran <jjuran@metamage.com>
2016-09-29 22:16:03 -0400

    posix-utils:  Fix open_or_connect() for OS X 10.4

--- posix/posix-utils/posix/open_or_connect.cc
+++ posix/posix-utils/posix/open_or_connect.cc
@@ -19,11 +19,33 @@
 namespace posix
 {
 	
+	static
+	bool might_be_socket( int error )
+	{
+	#ifdef __APPLE__
+		
+		/*
+			Observed behavior on 10.4:
+			EOPNOTSUPP is 45, but open() sets errno to 102.
+			
+			Look in /usr/include/sys/errno.h --  I'm not making this up.
+		*/
+		
+		if ( error == 102 )
+		{
+			return true;
+		}
+		
+	#endif
+		
+		return errno == EOPNOTSUPP  ||  errno == ENXIO;
+	}
+	
 	int open_or_connect( const char* path, int flags, mode_t mode )
 	{
 		int fd = open( path, flags, mode );
 		
-		if ( fd < 0  &&  (errno == EOPNOTSUPP  ||  errno == ENXIO) )
+		if ( fd < 0  &&  might_be_socket( errno ) )
 		{
 			sockaddr_un un = { 0 };
 			
