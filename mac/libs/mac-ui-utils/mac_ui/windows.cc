bceb1843c9067952cc33d079f31383826473975b
Josh Juran <jjuran@metamage.com>
2016-10-06 19:11:58 -0400

    [NEW] mac-ui-utils:  Add window size/pos accessors

---
+++ mac/libs/mac-ui-utils/mac_ui/windows.cc
@@ -0,0 +1,67 @@
+/*
+	windows.hh
+	----------
+*/
+
+#include "mac_ui/windows.hh"
+
+// Mac OS X
+#ifdef __APPLE__
+#include <Carbon/Carbon.h>
+#endif
+
+// Mac OS
+#ifndef __MACWINDOWS__
+#include <MacWindows.h>
+#endif
+
+
+namespace mac {
+namespace ui  {
+
+Point get_window_position( WindowRef window )
+{
+#if ! TARGET_API_MAC_CARBON
+	
+	WindowPeek w = (WindowPeek) window;
+	
+	Point position;
+	
+	position.v  = -window->portBits.bounds.top;
+	position.h = -window->portBits.bounds.left;
+	
+	return position;
+	
+#endif
+	
+	Rect bounds;
+	GetWindowBounds( window, kWindowGlobalPortRgn, &bounds );
+	
+	return (Point&) bounds;
+}
+
+Point get_window_size( WindowRef window )
+{
+#if ! TARGET_API_MAC_CARBON
+	
+	Rect& bounds = window->portRect;
+	
+	goto measure;
+	
+#else
+	
+	Rect bounds;
+	
+#endif
+	
+	GetWindowBounds( window, kWindowGlobalPortRgn, &bounds );
+	
+measure:
+	
+	Point size = { bounds.bottom - bounds.top, bounds.right - bounds.left };
+	
+	return size;
+}
+
+}
+}
---
+++ mac/libs/mac-ui-utils/mac_ui/windows.hh
@@ -0,0 +1,30 @@
+/*
+	windows.hh
+	----------
+*/
+
+#ifndef MACUI_WINDOWS_HH
+#define MACUI_WINDOWS_HH
+
+// Mac OS X
+#ifdef __APPLE__
+#include <ApplicationServices/ApplicationServices.h>
+#endif
+
+// Mac OS
+#ifndef __QUICKDRAW__
+#include <Quickdraw.h>
+#endif
+
+
+namespace mac {
+namespace ui  {
+	
+	Point get_window_position( WindowRef window );
+	
+	Point get_window_size( WindowRef window );
+	
+}
+}
+
+#endif
