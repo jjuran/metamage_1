bc42e6b96cfd12199649678c3b14d5371d82285e
Josh Juran <jjuran@metamage.com>
2016-12-18 16:35:51 -0500

    Pedestal:  Don't declare UPPs in classic 68K
    Turns out we were declaring an EventHandlerUPP named UPP_VARNAME.  Oops.

--- pedestal/Pedestal/Pedestal/WindowEventHandlers.cc
+++ pedestal/Pedestal/Pedestal/WindowEventHandlers.cc
@@ -19,11 +19,20 @@ namespace Pedestal
 	
 #if ! TARGET_RT_MAC_CFM
 	
+	/*
+		Null definitions for classic 68K and Mach-O.
+		(UPPs are plain functions, not routine descriptors.)
+	*/
+	
 	#define DEFINE_UPP2( UPPTypeName, proc )  /**/
 	
 	#define UPP_ARG( proc )  &proc
 	
 #else
+	
+	/*
+		CFM:  Create a routine descriptor at static initialization time.
+	*/
 	
 	#define UPP_VARNAME( proc )  proc ## _upp
 	
@@ -36,11 +45,23 @@ namespace Pedestal
 	
 	#define DEFINE_UPP( BaseName, proc )  DEFINE_UPP2( BaseName ## UPP, proc )
 	
-#if TARGET_API_MAC_CARBON
+#if ! TARGET_RT_MAC_CFM  ||  TARGET_API_MAC_CARBON
+	
+	/*
+		For non-CFM, use the null macro above.
+		For Carbon CFM, create a routine descriptor as usual.
+	*/
 	
 	#define DEFINE_CARBON_UPP( BaseName, proc )  DEFINE_UPP( BaseName, proc )
 	
 #else
+	
+	/*
+		For non-Carbon CFM, declare the UPP variable extern, but don't
+		define it.  Since these are Carbon event handlers, installing them
+		would be a run-time error, so make it a link error instead, and don't
+		waste resources creating the routine descriptors.
+	*/
 	
 	#define DEFINE_CARBON_UPP( BaseName, proc )  \
 	extern BaseName ## UPP UPP_VARNAME( proc );
