5f69e5c558a42fce460e46dcad02b6681e761fc3
Josh Juran <jjuran@metamage.com>
2016-12-22 16:46:27 -0500

    Vertice:  Enable compositing mode

--- apps/mac/Vertice/Vertice/Document.cc
+++ apps/mac/Vertice/Vertice/Document.cc
@@ -98,6 +98,9 @@ namespace Vertice
 		Mac::WindowAttributes attrs = Mac::kWindowCloseBoxAttribute
 		                            | Mac::kWindowResizableAttribute
 		                            | Mac::kWindowCollapseBoxAttribute
+		                          #ifdef MAC_OS_X_VERSION_10_2
+		                            | Mac::kWindowCompositingAttribute
+		                          #endif
 		                            ;
 		
 		return Ped::CreateWindow( Mac::kDocumentWindowClass, attrs, bounds );
