c6c6a3d98a76c754212882bb87fc1d00ff0e2e43
Josh Juran <jjuran@metamage.com>
2016-12-31 20:08:21 -0500

    Nitrogen:  Normalize CGDataProvider.hh whitespace

--- nitric/Nitrogen/CGDataProvider.hh
+++ nitric/Nitrogen/CGDataProvider.hh
@@ -28,53 +28,83 @@
 #endif
 
 
-namespace nucleus {
-   template <> struct disposer< CGDataProviderRef >
-     {
-      typedef CGDataProviderRef  argument_type;
-      typedef void               result_type;
-      
-      void operator()( CGDataProviderRef image ) const
-        {
-         ::CGDataProviderRelease ( image );
-        }
-     };
-  }
+namespace nucleus
+{
+	
+	template <> struct disposer< CGDataProviderRef >
+	{
+		typedef CGDataProviderRef  argument_type;
+		typedef void               result_type;
+		
+		void operator()( CGDataProviderRef image ) const
+		{
+			::CGDataProviderRelease( image );
+		}
+	};
+	
+}
 
 namespace Nitrogen
-  {
+{
 #if UNIVERSAL_INTERFACES_VERSION >= 0x0400
-  
-  using ::CGDataProviderGetTypeID;
-  
+	
+	using ::CGDataProviderGetTypeID;
+	
 //	CG_EXTERN CGDataProviderRef CGDataProviderCreate(void *info, const CGDataProviderCallbacks *callbacks);
-//  CG_EXTERN CGDataProviderRef CGDataProviderCreateDirectAccess(void *info, size_t size, const CGDataProviderDirectAccessCallbacks *callbacks);
+//	CG_EXTERN CGDataProviderRef CGDataProviderCreateDirectAccess(void *info, size_t size, const CGDataProviderDirectAccessCallbacks *callbacks);
 //	CG_EXTERN CGDataProviderRef CGDataProviderCreateWithData(void *info, const void *data, size_t size, CGDataProviderReleaseDataCallback releaseData);
-
+	
 	class CGDataProviderCreateWithCFData_Failed {};
-	inline nucleus::owned<CGDataProviderRef> CGDataProviderCreateWithCFData ( CFDataRef data ) {
-		CGDataProviderRef result = ::CGDataProviderCreateWithCFData ( data );
-		if ( NULL == result ) throw CGDataProviderCreateWithCFData_Failed ();
-		return nucleus::owned< CGDataProviderRef>::seize ( result );
+	
+	inline
+	nucleus::owned< CGDataProviderRef >
+	//
+	CGDataProviderCreateWithCFData( CFDataRef data )
+	{
+		CGDataProviderRef result = ::CGDataProviderCreateWithCFData( data );
+		
+		if ( result == NULL )
+		{
+			throw CGDataProviderCreateWithCFData_Failed();
 		}
-  
+		
+		return nucleus::owned< CGDataProviderRef>::seize( result );
+	}
+	
 #endif
 	
 	class CGDataProviderCreateWithURL_Failed {};
-	inline nucleus::owned<CGDataProviderRef> CGDataProviderCreateWithURL ( CFURLRef url ) {
-		CGDataProviderRef result = ::CGDataProviderCreateWithURL ( url );
-		if ( NULL == result ) throw CGDataProviderCreateWithURL_Failed ();
-		return nucleus::owned< CGDataProviderRef>::seize ( result );
-		}
 	
-   inline nucleus::owned<CGDataProviderRef> CGDataProviderRetain( CGDataProviderRef image )
-     {
-      return nucleus::owned<CGDataProviderRef>::seize( ::CGDataProviderRetain( image ) );
-     }
-   
-   inline void CGDataProviderRelease( nucleus::owned<CGDataProviderRef> )
-     {}
-
-  }
+	inline
+	nucleus::owned< CGDataProviderRef >
+	//
+	CGDataProviderCreateWithURL( CFURLRef url )
+	{
+		CGDataProviderRef result = ::CGDataProviderCreateWithURL( url );
+		
+		if ( result == NULL )
+		{
+			throw CGDataProviderCreateWithURL_Failed();
+		}
+		
+		return nucleus::owned< CGDataProviderRef>::seize( result );
+	}
+	
+	inline
+	nucleus::owned< CGDataProviderRef >
+	//
+	CGDataProviderRetain( CGDataProviderRef dataProvider )
+	{
+		::CGDataProviderRetain( dataProvider );
+		
+		return nucleus::owned< CGDataProviderRef >::seize( dataProvider );
+	}
+	
+	inline
+	void CGDataProviderRelease( nucleus::owned< CGDataProviderRef > )
+	{
+	}
+	
+}
 
 #endif
