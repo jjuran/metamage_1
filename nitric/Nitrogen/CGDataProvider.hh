19ee15bb56ed1ca02ebaffc2af576e1890496232
Josh Juran <jjuran@metamage.com>
2016-12-31 22:59:50 -0500

    Nitrogen:  Add CGDataProviderCreateWithData()

--- nitric/Nitrogen/CGDataProvider.hh
+++ nitric/Nitrogen/CGDataProvider.hh
@@ -3,7 +3,7 @@
 
 // Part of the Nitrogen project.
 //
-// Written 2004-2006 by Marshall Clow, Lisa Lippincott, and Joshua Juran.
+// Written 2004-2016 by Marshall Clow, Lisa Lippincott, and Josh Juran.
 //
 // This code was written entirely by the above contributors, who place it
 // in the public domain.
@@ -56,9 +56,71 @@ namespace Nitrogen
 	
 	using ::CGDataProviderGetTypeID;
 	
-//	CG_EXTERN CGDataProviderRef CGDataProviderCreate(void *info, const CGDataProviderCallbacks *callbacks);
-//	CG_EXTERN CGDataProviderRef CGDataProviderCreateDirectAccess(void *info, size_t size, const CGDataProviderDirectAccessCallbacks *callbacks);
-//	CG_EXTERN CGDataProviderRef CGDataProviderCreateWithData(void *info, const void *data, size_t size, CGDataProviderReleaseDataCallback releaseData);
+#endif
+	
+	/*
+		Normally we don't add new arguments to functions, but this is a
+		special case:  The caller is passing ownership of the data to the new
+		data provider.  This raises a question about exception safety:  What
+		happens if ::CGDataProviderCreateWithData() fails and an exception is
+		thrown?  Either the caller remains the owner until successful creation
+		of the data provider, and must catch and rethrow the exception (after
+		manually releasing the data if necessary), or the called function
+		assumes ownership immediately, and must release the data itself on
+		failure.  Clearly, the former violates the spirit of Nitrogen.
+		
+		The new fifth argument is a release-data callback of the same type as
+		the fourth argument -- in fact, the expected usage is to pass the same
+		function as both arguments.  If creating the data provider fails, and
+		the `fail` argument is non-NULL, it will be called to release the data
+		before the exception is thrown.  If the caller prefers to retain
+		ownership on exception, it can pass NULL here.  To avoid likely errors,
+		neither release-data callback argument is optional.
+	*/
+	
+	class CGDataProviderCreateWithData_Failed {};
+	
+	static
+	nucleus::owned< CGDataProviderRef >
+	//
+	CGDataProviderCreateWithData( void*                              info,
+	                              const void*                        data,
+	                              size_t                             size,
+	                              CGDataProviderReleaseDataCallback  free,
+	                              CGDataProviderReleaseDataCallback  fail )
+	{
+		CGDataProviderRef result = ::CGDataProviderCreateWithData( info,
+		                                                           data,
+		                                                           size,
+		                                                           free );
+		
+		if ( result == NULL )
+		{
+			if ( fail != NULL )
+			{
+				fail( info, data, size );
+			}
+			
+			throw CGDataProviderCreateWithData_Failed();
+		}
+		
+		return nucleus::owned< CGDataProviderRef >::seize( result );
+	}
+	
+	static inline
+	nucleus::owned< CGDataProviderRef >
+	//
+	CGDataProviderCreateWithData( const void*                        data,
+	                              size_t                             size,
+	                              CGDataProviderReleaseDataCallback  free,
+	                              CGDataProviderReleaseDataCallback  fail )
+	{
+		namespace N = Nitrogen;
+		
+		return N::CGDataProviderCreateWithData( NULL, data, size, free, fail );
+	}
+	
+#if UNIVERSAL_INTERFACES_VERSION >= 0x0400
 	
 	class CGDataProviderCreateWithCFData_Failed {};
 	
