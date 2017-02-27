5aad1e24922543bd687dec15f2e247e37ae0a5fa
Josh Juran <jjuran@metamage.com>
2017-02-27 15:01:55 -0500

    quickdraw:  Add region_iterator

---
+++ mac/libs/quickdraw/qd/region_iterator.cc
@@ -0,0 +1,72 @@
+/*
+	region_iterator.cc
+	------------------
+*/
+
+#include "qd/region_iterator.hh"
+
+// quickdraw
+#include "qd/region_detail.hh"
+
+
+namespace quickdraw
+{
+	
+	region_iterator::region_iterator( short size, const short* extent )
+	:
+		its_segments( size )
+	{
+		its_next_coordinate = extent;
+		
+		its_next_band.v1 = *its_next_coordinate++;
+	}
+	
+	void region_iterator::advance()
+	{
+		using quickdraw::Region_end;
+		
+		while ( true )
+		{
+			short h = *its_next_coordinate++;
+			
+			if ( h == Region_end )
+			{
+				break;
+			}
+			
+			its_segments ^= h;
+		}
+		
+		if ( its_segments.size() & 0x1 )
+		{
+			its_segments ^= 0x7FFF;
+		}
+		
+		its_next_band.v0 = its_next_band.v1;
+		its_next_band.v1 = *its_next_coordinate++;
+		
+		its_next_band.h_begin = its_segments.begin();
+		its_next_band.h_end   = its_segments.end  ();
+	}
+	
+	const region_band* region_iterator::next()
+	{
+		using quickdraw::Region_end;
+		using quickdraw::precedes_in_region;
+		
+		if ( its_next_band.v1 == Region_end )
+		{
+			return NULL;
+		}
+		
+		advance();
+		
+		if ( its_segments.empty()  &&  its_next_band.v1 == Region_end )
+		{
+			return NULL;
+		}
+		
+		return &its_next_band;
+	}
+	
+}
---
+++ mac/libs/quickdraw/qd/region_iterator.hh
@@ -0,0 +1,44 @@
+/*
+	region_iterator.hh
+	------------------
+*/
+
+#ifndef QUICKDRAW_REGIONITERATOR_HH
+#define QUICKDRAW_REGIONITERATOR_HH
+
+// quickdraw
+#include "qd/segments.hh"
+
+
+namespace quickdraw
+{
+	
+	struct region_band
+	{
+		short v0;
+		short v1;
+		
+		const short* h_begin;
+		const short* h_end;
+	};
+	
+	class region_iterator
+	{
+		private:
+			segments_box its_segments;
+			
+			const short* its_next_coordinate;
+			
+			region_band its_next_band;
+			
+			void advance();
+		
+		public:
+			region_iterator( short size, const short* extent );
+			
+			const region_band* next();
+	};
+	
+}
+
+#endif
