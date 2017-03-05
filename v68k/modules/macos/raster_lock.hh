4ef432482a9865110ed8892a589b6ec5c3a071f0
Josh Juran <jjuran@metamage.com>
2017-03-05 05:38:54 -0500

    macos:  Make raster_lock conditional

--- v68k/modules/macos/raster_lock.hh
+++ v68k/modules/macos/raster_lock.hh
@@ -13,19 +13,27 @@ void unlock_raster();
 class raster_lock
 {
 	private:
+		const bool condition;
+		
 		// non-copyable
 		raster_lock           ( const raster_lock& );
 		raster_lock& operator=( const raster_lock& );
 	
 	public:
-		raster_lock()
+		raster_lock( bool cond = true ) : condition( cond )
 		{
-			lock_raster();
+			if ( cond )
+			{
+				lock_raster();
+			}
 		}
 		
 		~raster_lock()
 		{
-			unlock_raster();
+			if ( condition )
+			{
+				unlock_raster();
+			}
 		}
 };
 
