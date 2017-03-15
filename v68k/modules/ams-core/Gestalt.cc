60477125485f0c70a27bd6cfc32289f3dc9fbffa
Josh Juran <jjuran@metamage.com>
2017-03-15 08:06:49 -0400

    ams-core:  Fix _Gestalt trap
    It turns out the _Gestalt trap itself actually returns two values -- the
    error code in D0 and the actual result in A0.  Assigning the result into
    the address passed as the 'response' argument is performed by glue code.
    
    Unfortunately, we have no way specify that a function returns results in
    multiple registers in C++.  So, we have to write the patch in assembler.

--- v68k/modules/ams-core/Gestalt.cc
+++ v68k/modules/ams-core/Gestalt.cc
@@ -13,8 +13,9 @@ enum
 
 Gestalt_ProcPtr old_Gestalt;
 
-pascal short Gestalt_patch( unsigned long  selector : __D0,
-                            long*          response : __A1 ) : __D0
+static
+short Gestalt_payload( unsigned long  selector : __D0,
+                       long*          response : __A1 ) : __D0
 {
 	switch ( selector )
 	{
@@ -23,8 +24,31 @@ pascal short Gestalt_patch( unsigned long  selector : __D0,
 			break;
 		
 		default:
-			return old_Gestalt( selector, response );
+			return -1;  // Any non-zero value will do here.
 	}
 	
 	return 0;
+}
+
+asm
+pascal short Gestalt_patch( unsigned long selector : __D0 ) : __D0
+{
+	MOVE.L   A1,-(SP)         // save A1, the 'response' address
+	
+	JSR      Gestalt_payload
+	
+	MOVEA.L  (SP)+,A1         // restore A1
+	
+	TST.W    D0
+	BEQ.S    found
+	
+	// Not found -- try the native implementation.
+	MOVEA.L  old_Gestalt,A0
+	JMP      (A0)
+	
+found:
+	// Found -- move the result to A0, where the glue code expects to find it.
+	MOVEA.L  (A1),A0
+	
+	RTS
 }
--- v68k/modules/ams-core/Gestalt.hh
+++ v68k/modules/ams-core/Gestalt.hh
@@ -6,11 +6,10 @@
 #ifndef GESTALT_HH
 #define GESTALT_HH
 
-typedef short (*Gestalt_ProcPtr)( unsigned long : __D0, long* : __A1 ) : __D0;
+typedef short (*Gestalt_ProcPtr)( unsigned long : __D0 ) : __D0;
 
 extern Gestalt_ProcPtr old_Gestalt;
 
-pascal short Gestalt_patch( unsigned long  selector : __D0,
-                            long*          response : __A1 ) : __D0;
+pascal short Gestalt_patch( unsigned long selector : __D0 ) : __D0;
 
 #endif
