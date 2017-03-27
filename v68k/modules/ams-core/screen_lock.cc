c3905f8b7ba5e8e5a5c4a701fea2c7b2938679b3
Josh Juran <jjuran@metamage.com>
2017-03-27 17:13:20 -0400

    ams-core:  Clean up screen_lock.cc includes

--- v68k/modules/ams-core/screen_lock.cc
+++ v68k/modules/ams-core/screen_lock.cc
@@ -5,18 +5,6 @@
 
 #include "screen_lock.hh"
 
-// Mac OS
-#ifndef __RESOURCES__
-#include <Resources.h>
-#endif
-
-// POSIX
-#include <unistd.h>
-
-// Standard C
-#include <stdlib.h>
-#include <string.h>
-
 
 static inline
 asm void inline_lock_screen()
