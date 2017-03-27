606583bbe375ab73a09c7992a8133c55fe597a8f
Josh Juran <jjuran@metamage.com>
2017-03-27 16:41:11 -0400

    ams-core:  Axe unused cursor_lock

--- v68k/modules/ams-core/Cursor.hh
+++ v68k/modules/ams-core/Cursor.hh
@@ -13,25 +13,6 @@ void show_cursor();
 
 void update_cursor_location();
 
-class cursor_lock
-{
-	private:
-		// non-copyable
-		cursor_lock           ( const cursor_lock& );
-		cursor_lock& operator=( const cursor_lock& );
-	
-	public:
-		cursor_lock()
-		{
-			hide_cursor();
-		}
-		
-		~cursor_lock()
-		{
-			show_cursor();
-		}
-};
-
 pascal void InitCursor_patch();
 
 pascal void SetCursor_patch( const Cursor* crsr );
