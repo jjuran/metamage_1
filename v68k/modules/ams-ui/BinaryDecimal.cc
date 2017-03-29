befea9676b7a6529067c51e21470c80275830f08
Josh Juran <jjuran@metamage.com>
2017-03-28 20:49:30 -0400

    ams-ui:  Implement PACK 7's StringToNum()

--- v68k/modules/ams-ui/BinaryDecimal.cc
+++ v68k/modules/ams-ui/BinaryDecimal.cc
@@ -18,6 +18,7 @@
 
 // gear
 #include "gear/inscribe_decimal.hh"
+#include "gear/parse_decimal.hh"
 
 // ams-common
 #include "callouts.hh"
@@ -39,6 +40,20 @@ StringPtr NumToString_method( long x : __D0, StringPtr p : __A0 )
 	return q;
 }
 
+static
+long StringToNum_method( ConstStr255Param string : __A0 )
+{
+	char buffer[ 256 ];
+	
+	short len = string[ 0 ];
+	
+	fast_memcpy( buffer, string + 1, len );
+	
+	buffer[ len ] = '\0';
+	
+	return gear::parse_decimal( buffer );
+}
+
 asm void Pack7_patch( short method )
 {
 	MOVEA.L  (SP)+,A1
@@ -48,9 +63,15 @@ asm void Pack7_patch( short method )
 	TST.W    D1
 	BEQ      dispatch_NumToString
 	
+	CMPI.W   #0x0001,D1
+	BEQ      dispatch_StringToNum
+	
 	_Debugger
 	_ExitToShell
 	
 dispatch_NumToString:
 	JMP      NumToString_method
+	
+dispatch_StringToNum:
+	JMP      StringToNum_method
 }
