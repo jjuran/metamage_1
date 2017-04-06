991a5e651edea281c7ef41ad9e1b273f1f9e94d9
Josh Juran <jjuran@metamage.com>
2017-04-05 21:30:55 -0400

    vlib:  Add {pre,post}-{inc,dec}rement op dispatch

--- v/vlib/vlib/dispatch/operators.hh
+++ v/vlib/vlib/dispatch/operators.hh
@@ -13,15 +13,19 @@
 namespace vlib
 {
 	
+	struct Target;
 	class Value;
 	
 	typedef Value (*handler_1arg)( op_type op, const Value& v );
 	typedef Value (*handler_2arg)( op_type op, const Value& a, const Value& b );
 	
+	typedef Value (*handler_step)( op_type op, const Target& target );
+	
 	struct operators
 	{
 		handler_1arg  unary;
 		handler_2arg  binary;
+		handler_step  advance;
 	};
 	
 }
