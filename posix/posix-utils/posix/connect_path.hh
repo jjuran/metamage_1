55fac0fd57b4151a29c0a368106a4bf456542583
Josh Juran <jjuran@metamage.com>
2017-04-21 19:47:52 -0400

    posix-utils:  Factor out connect_path()

---
+++ posix/posix-utils/posix/connect_path.hh
@@ -0,0 +1,17 @@
+/*
+	connect_path.hh
+	---------------
+*/
+
+#ifndef POSIX_CONNECTPATH_HH
+#define POSIX_CONNECTPATH_HH
+
+
+namespace posix
+{
+	
+	int connect_path( const char* path );
+	
+}
+
+#endif
--- posix/posix-utils/posix/open_or_connect.cc
+++ posix/posix-utils/posix/open_or_connect.cc
@@ -7,13 +7,12 @@
 
 // POSIX
 #include <fcntl.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/un.h>
 
 // Standard C
 #include <errno.h>
-#include <string.h>
+
+// posix-utils
+#include "posix/connect_path.hh"
 
 
 namespace posix
@@ -47,40 +46,7 @@ namespace posix
 		
 		if ( fd < 0  &&  might_be_socket( errno ) )
 		{
-			sockaddr_un un = { 0 };
-			
-			un.sun_family = AF_LOCAL;
-			
-			const size_t length = strlen( path );
-			
-			if ( length >= sizeof un.sun_path )
-			{
-				errno = ENAMETOOLONG;
-				
-				return fd;
-			}
-			
-			memcpy( un.sun_path, path, length + 1 );
-			
-			fd = socket( PF_LOCAL, SOCK_STREAM, 0 );
-			
-			if ( fd < 0 )
-			{
-				return fd;
-			}
-			
-			int nok = connect( fd, (const sockaddr*) &un, sizeof un );
-			
-			if ( nok )
-			{
-				int saved_errno = errno;
-				
-				close( fd );
-				
-				errno = saved_errno;
-				
-				return -1;
-			}
+			fd = connect_path( path );
 		}
 		
 		return fd;
