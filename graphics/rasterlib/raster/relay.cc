c766c2a50dae835e152a1722538f9b9484bc4253
Josh Juran <jjuran@metamage.com>
2017-06-09 14:51:05 -0400

    rasterlib:  Allow relays without CONFIG_SETPSHARED
    We already support degraded scenarios where pthread_cond_wait() can fail
    when called by multiple processes simultaneously, by allowing the caller
    to fall back on polling.  Now do the same when process-shared interlocks
    aren't supported at all.

--- graphics/rasterlib/raster/relay.cc
+++ graphics/rasterlib/raster/relay.cc
@@ -5,6 +5,9 @@
 
 #include "raster/relay.hh"
 
+// Standard C
+#include <errno.h>
+
 // config
 #include "config/setpshared.h"
 
@@ -23,12 +26,23 @@ namespace raster
 		pthread_mutexattr_t mutex_attr;
 		pthread_condattr_t  cond_attr;
 		
-	#if CONFIG_SETPSHARED
-		
-		const int pshared = PTHREAD_PROCESS_SHARED;
+		/*
+			We initialize and destroy the mutex and condvar attributes
+			even if PTHREAD_PROCESS_SHARED isn't supported, because it
+			makes the source code simpler, and the scenario in which it's
+			wasted work is already negatively affected by the lack of
+			support.  If, on the other hand, setting the attribute were
+			the workaround (rather than not setting it), then I'd consider
+			extra compile-time logic to avoid adding a run-time burden to
+			the normal case.
+		*/
 		
 		must_pthread_mutexattr_init( &mutex_attr );
 		must_pthread_condattr_init ( &cond_attr  );
+		
+	#if CONFIG_SETPSHARED
+		
+		const int pshared = PTHREAD_PROCESS_SHARED;
 		
 		must_pthread_mutexattr_setpshared( &mutex_attr, pshared );
 		must_pthread_condattr_setpshared ( &cond_attr,  pshared );
@@ -38,12 +52,8 @@ namespace raster
 		must_pthread_mutex_init( &relay.mutex, &mutex_attr );
 		must_pthread_cond_init ( &relay.cond,  &cond_attr  );
 		
-	#if CONFIG_SETPSHARED
-		
 		must_pthread_mutexattr_destroy( &mutex_attr );
 		must_pthread_condattr_destroy ( &cond_attr  );
-		
-	#endif
 		
 		relay.reserved = 0;
 		relay.seed     = 0;
@@ -64,11 +74,19 @@ namespace raster
 	
 	void wait( sync_relay& relay )
 	{
+	#if CONFIG_SETPSHARED
+		
 		must_pthread_mutex_lock( &relay.mutex );
 		
 		int err = pthread_cond_wait( &relay.cond, &relay.mutex );
 		
 		must_pthread_mutex_unlock( &relay.mutex );
+		
+	#else
+		
+		const int err = ENOSYS;
+		
+	#endif
 		
 		if ( err )
 		{
