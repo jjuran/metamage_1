68e1e0e25fff68ce7a29b02fd645877a22096f60
Josh Juran <jjuran@metamage.com>
2017-07-12 10:24:04 -0400

    poseven:  Normalize whitespace in bundles/inet.hh

--- posix/poseven/poseven/bundles/inet.hh
+++ posix/poseven/poseven/bundles/inet.hh
@@ -1,15 +1,7 @@
-// inet.hh
-// -------
-//
-// Maintained by Joshua Juran
-
-// Part of the Nitrogen project.
-//
-// Written 2008 by Joshua Juran.
-//
-// This code was written entirely by the above contributor, who places it
-// in the public domain.
-
+/*
+	inet.hh
+	-------
+*/
 
 #ifndef POSEVEN_BUNDLES_INET_HH
 #define POSEVEN_BUNDLES_INET_HH
@@ -35,19 +27,22 @@
 namespace poseven
 {
 	
-	inline nucleus::owned< fd_t > accept( fd_t listener, sockaddr_in& address )
+	inline
+	nucleus::owned< fd_t > accept( fd_t listener, sockaddr_in& address )
 	{
 		return accept< af_inet >( listener, address );
 	}
 	
-	inline void bind( fd_t fd, in_addr_t addr, in_port_t port )
+	inline
+	void bind( fd_t fd, in_addr_t addr, in_port_t port )
 	{
 		bind< af_inet >( fd, sockaddr_traits< af_inet >::make( addr, port ) );
 	}
 	
-	inline nucleus::owned< fd_t > bind( in_addr_t    addr,
-	                                    in_port_t    port,
-	                                    socket_type  type = sock_stream )
+	inline
+	nucleus::owned< fd_t > bind( in_addr_t    addr,
+	                             in_port_t    port,
+	                             socket_type  type = sock_stream )
 	{
 		nucleus::owned< fd_t > fd = socket( pf_inet, type );
 		
@@ -56,14 +51,17 @@ namespace poseven
 		return fd;
 	}
 	
-	inline void connect( fd_t fd, in_addr_t addr, in_port_t port )
+	inline
+	void connect( fd_t fd, in_addr_t addr, in_port_t port )
 	{
-		connect< af_inet >( fd, sockaddr_traits< af_inet >::make( addr, port ) );
+		connect< af_inet >( fd,
+		                    sockaddr_traits< af_inet >::make( addr, port ) );
 	}
 	
-	inline nucleus::owned< fd_t > connect( in_addr_t    addr,
-	                                       in_port_t    port,
-	                                       socket_type  type = sock_stream )
+	inline
+	nucleus::owned< fd_t > connect( in_addr_t    addr,
+	                                in_port_t    port,
+	                                socket_type  type = sock_stream )
 	{
 		nucleus::owned< fd_t > fd = socket( pf_inet, type );
 		
