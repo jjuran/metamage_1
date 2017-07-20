0a2a9ccbdc9860aa956feddf1a5b3ed1a9fd5097
Josh Juran <jjuran@metamage.com>
2017-07-20 19:04:56 -0400

    A-line:  Rewrite get_project_providing_prefix()
    Scrap find_if() and bind2nd(), and write a simple loop instead.

--- lamp/jTools/A-line/A-line/prefix.cc
+++ lamp/jTools/A-line/A-line/prefix.cc
@@ -5,13 +5,6 @@
 
 #include "A-line/prefix.hh"
 
-// Standard C++
-#include <algorithm>
-#include <functional>
-
-// plus
-#include "plus/pointer_to_function.hh"
-
 // A-line
 #include "A-line/Project.hh"
 #include "A-line/ProjectCommon.hh"
@@ -20,38 +13,31 @@
 namespace tool
 {
 	
-	static inline
-	bool project_has_prefix( const plus::string& project_name, Platform platform )
-	{
-		return GetProject( project_name, platform ).HasPrecompiledHeader();
-	}
-	
 	const Project* get_project_providing_prefix( const Project&  project,
 	                                             Platform        platform )
 	{
-		const Project* result = NULL;
-		
 		if ( project.HasPrecompiledHeader() )
 		{
-			result = &project;
+			return &project;
 		}
-		else
+		
+		// This project doesn't have a precompiled header, but maybe a used one does
+		typedef std::vector< plus::string >::const_iterator const_iterator;
+		
+		const_iterator const end = project.AllUsedProjects().end();
+		const_iterator it        = project.AllUsedProjects().begin();
+		
+		while ( it != end )
 		{
-			// This project doesn't have a precompiled header, but maybe a used one does
-			typedef std::vector< plus::string >::const_iterator const_iterator;
+			const Project& project = GetProject( *it++, platform );
 			
-			const_iterator it = std::find_if( project.AllUsedProjects().begin(),
-			                                  project.AllUsedProjects().end(),
-			                                  std::bind2nd( plus::ptr_fun( project_has_prefix ),
-			                                                platform ) );
-			
-			if ( it != project.AllUsedProjects().end() )
+			if ( project.HasPrecompiledHeader() )
 			{
-				result = &GetProject( *it, platform );
+				return &project;
 			}
 		}
 		
-		return result;
+		return NULL;
 	}
 	
 }
