3af21583ee66dade3cdac49a8fa4b5b53b03a711
Josh Juran <jjuran@metamage.com>
2017-07-26 04:02:01 -0400

    bignum:  Add gcd()

---
+++ base/bignum/bignum/gcd.hh
@@ -0,0 +1,20 @@
+/*
+	gcd.hh
+	------
+*/
+
+#ifndef BIGNUM_GCD_HH
+#define BIGNUM_GCD_HH
+
+// bignum
+#include "bignum/integer.hh"
+
+
+namespace bignum
+{
+	
+	integer gcd( integer a, integer b );
+	
+}
+
+#endif
