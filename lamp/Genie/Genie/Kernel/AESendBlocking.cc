35654b1322627351cec594b54c08bb2fb3f2a7b9
Josh Juran <jjuran@metamage.com>
2017-08-03 02:29:39 -0400

    Genie:  Check signals and loop in AESendBlocking()
    Anticipate a thread waking before its Apple event reply is received.  It
    should only happen in response to a signal, so check for that.
    
    This fixes a crash in tlsrvr when the process is continued while waiting
    for an Apple event reply (though this is masked by a failed assertion in
    Process::Continue() in debug mode), when it calls AEGetParamPtr() on the
    reply structure after assuming it had been populated with received data.

--- lamp/Genie/Genie/Kernel/AESendBlocking.cc
+++ lamp/Genie/Genie/Kernel/AESendBlocking.cc
@@ -18,8 +18,12 @@
 
 // relix
 #include "relix/api/current_thread.hh"
+#include "relix/signal/signal.hh"
 #include "relix/task/scheduler.hh"
 #include "relix/task/thread.hh"
+
+// Genie
+#include "Genie/api/signals.hh"
 
 
 namespace N = Nitrogen;
@@ -34,6 +38,8 @@ OSStatus AESendBlocking( const AppleEvent* appleEventPtr, AppleEvent* replyPtr )
 		Mac::AppleEvent const& appleEvent = static_cast< const Mac::AppleEvent& >( *appleEventPtr );
 		Mac::AppleEvent      & reply      = static_cast<       Mac::AppleEvent& >( *replyPtr      );
 		
+		reply.dataHandle = NULL;
+		
 		(void) N::AESend( appleEvent,
 		                  Mac::kAEQueueReply | Mac::kAECanInteract );
 		
@@ -46,12 +52,38 @@ OSStatus AESendBlocking( const AppleEvent* appleEventPtr, AppleEvent* replyPtr )
 		const thread& current = current_thread();
 		const pid_t   tid     = current.id();
 		
+	stop_and_wait:
+		
 		relix::mark_thread_inactive( tid );
 		
 		// Sleep until the reply is delivered
 		stop_os_thread( current.get_os_thread() );
 		
 		relix::mark_thread_active( tid );
+		
+		/*
+			Check for fatal signals; ignore caught ones (for now).
+			
+			TODO:  Return errAEWaitCanceled for a caught signal, and check
+			on entry to determine if the Apple event has already been sent by
+			a prior entry to this call.  We could possibly use keyReturnIDAttr
+			for this, as long as the caller doesn't provide us with an event
+			that's been sent previously.
+		*/
+		
+		check_signals( false );  // Don't throw caught signals
+		
+		if ( reply.dataHandle == NULL )
+		{
+			goto stop_and_wait;
+		}
+	}
+	catch ( const relix::signal& sig )
+	{
+		relix::the_signal = sig;
+		
+		// Only a fatal signal gets here, so actual error code doesn't matter
+		return errAEWaitCanceled;
 	}
 	catch ( const Mac::OSStatus& err )
 	{
