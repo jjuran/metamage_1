dffa1d85f2c793a9dd1bd8f9d7d42469f02e5fc6
Josh Juran <jjuran@metamage.com>
2017-08-05 17:45:12 -0400

    mac-sys-utils:  Directly access unit table globals

--- mac/libs/mac-sys-utils/mac_sys/unit_table.cc
+++ mac/libs/mac-sys-utils/mac_sys/unit_table.cc
@@ -11,7 +11,7 @@
 #include <Devices.h>
 #endif
 #ifndef __LOWMEM__
-#include <LowMem.h>
+//#include <LowMem.h>
 #endif
 #endif
 
@@ -19,13 +19,27 @@
 namespace mac {
 namespace sys {
 	
-	// FIXME:  Load these from InterfaceLib for Carbon
+	/*
+		LMGetUnitTableEntryCount() has been observed to have a NULL T-Vector
+		address in System 7.5.2 on a Pippin.  On the other hand, direct access
+		to the low memory global UnitNtryCnt (as well as UTableBase) works
+		just fine, even through Mac OS 9.2.2 (both booting natively and in
+		Classic on Mac OS X).  We could check for the NULL function address
+		before calling it, but there's no reason to do so -- just read the low
+		memory locations.
+		
+		Special thanks to Keith Kaisershot for helping me test this.
+		
+		TODO:  In Carbon, try to load these symbols from InterfaceLib.
+	*/
 	
 	mac::types::AuxDCE*** get_unit_table_base()
 	{
 	#if !TARGET_API_MAC_CARBON
 		
-		return (mac::types::AuxDCE***) ::LMGetUTableBase();
+//		return (mac::types::AuxDCE***) ::LMGetUTableBase();
+		
+		return *(mac::types::AuxDCE****) 0x011C;  // UTableBase
 		
 	#endif
 		
@@ -36,7 +50,9 @@ namespace sys {
 	{
 	#if !TARGET_API_MAC_CARBON
 		
-		return ::LMGetUnitTableEntryCount();
+//		return ::LMGetUnitTableEntryCount();
+		
+		return *(short*) 0x01D2;  // UnitNtryCnt
 		
 	#endif
 		
