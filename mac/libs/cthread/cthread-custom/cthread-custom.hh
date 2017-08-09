5dc7b6ed04073b0c26807556ab72c64345b901a7
Josh Juran <jjuran@metamage.com>
2017-08-09 00:57:15 -0400

    [NEW] cthread-custom:  Hand-spun 68K Mac threads

---
+++ mac/libs/cthread/cthread-custom/cthread-custom.hh
@@ -0,0 +1,40 @@
+/*
+	cthread-custom.hh
+	-----------------
+*/
+
+#ifndef CTHREADCUSTOM_HH
+#define CTHREADCUSTOM_HH
+
+
+namespace cthread
+{
+	
+	struct parameter_block;
+	
+	typedef unsigned long thread_id;
+	
+namespace custom
+{
+	
+	using cthread::parameter_block;
+	
+	void thread_yield();
+	
+	unsigned long current_thread_stack_space();
+	
+	thread_id current_thread();
+	
+	thread_id create_thread( parameter_block& pb, unsigned stack_size );
+	
+	void destroy_thread( thread_id id );
+	
+	bool is_thread_stopped( thread_id id );
+	
+	void stop_thread( thread_id id );
+	void wake_thread( thread_id id );
+	
+}
+}
+
+#endif
