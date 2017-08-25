4faaad5785d82cb24bdfb22b2ea2acb3747e5567
Josh Juran <jjuran@metamage.com>
2017-08-25 19:03:22 -0400

    [NEW] mac-file-utils:  Add parent_directory()

---
+++ mac/libs/mac-file-utils/mac_file/parent_directory.cc
@@ -0,0 +1,50 @@
+/*
+	parent_directory.cc
+	-------------------
+*/
+
+#include "mac_file/parent_directory.hh"
+
+// Mac OS X
+#ifdef __APPLE__
+#include <CoreServices/CoreServices.h>
+#endif
+
+// Mac OS
+#ifndef __FILES__
+#include <Files.h>
+#endif
+
+
+namespace mac  {
+namespace file {
+	
+	using types::VRefNum_DirID;
+	
+	
+	VRefNum_DirID parent_directory( const VRefNum_DirID& dir )
+	{
+		CInfoPBRec pb;
+		
+		DirInfo& dirInfo = pb.dirInfo;
+		
+		short vRefNum = dir.vRefNum;
+		
+		dirInfo.ioNamePtr = NULL;
+		dirInfo.ioVRefNum = vRefNum;
+		dirInfo.ioDrDirID = dir.dirID;
+		dirInfo.ioFDirIndex = -1;
+		
+		if ( OSStatus err = PBGetCatInfoSync( &pb ) )
+		{
+			vRefNum           = 0;
+			dirInfo.ioDrParID = err;
+		}
+		
+		VRefNum_DirID result = { vRefNum, dirInfo.ioDrParID };
+		
+		return result;
+	}
+	
+}
+}
