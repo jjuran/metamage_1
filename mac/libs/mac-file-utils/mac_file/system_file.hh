144caf10db353c3a8a0a54b445447cd1ce685c5c
Josh Juran <jjuran@metamage.com>
2017-08-27 18:49:03 -0400

    mac-file-utils:  Add system_file()

---
+++ mac/libs/mac-file-utils/mac_file/system_file.hh
@@ -0,0 +1,21 @@
+/*
+	system_file.hh
+	--------------
+*/
+
+#ifndef MACFILE_SYSTEMFILE_HH
+#define MACFILE_SYSTEMFILE_HH
+
+// mac-types
+#include "mac_types/FSSpec.hh"
+
+
+namespace mac  {
+namespace file {
+	
+	types::FSSpec system_file();
+	
+}
+}
+
+#endif
