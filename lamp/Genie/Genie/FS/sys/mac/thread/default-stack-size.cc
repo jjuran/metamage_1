6dfb285b255d0c9b9430d0ee3d44dac229fdc764
Josh Juran <jjuran@metamage.com>
2017-08-28 16:15:47 -0400

    Genie:  Ensure /sys/mac/thread/default-stack-size
    Gestalt-check, and fall back to zero for now.

--- lamp/Genie/Genie/FS/sys/mac/thread/default-stack-size.cc
+++ lamp/Genie/Genie/FS/sys/mac/thread/default-stack-size.cc
@@ -7,6 +7,7 @@
 
 // mac-sys-utils
 #include "mac_sys/default_thread_stack_size.hh"
+#include "mac_sys/has/ThreadManager.hh"
 
 // plus
 #include "plus/serialize.hh"
@@ -18,9 +19,14 @@ namespace Genie
 	
 	void sys_mac_thread_defaultstacksize::get( plus::var_string& result, const vfs::node* that, bool binary )
 	{
+		using mac::sys::default_thread_stack_size;
+		using mac::sys::has_ThreadManager;
+		
 		typedef plus::serialize_unsigned< std::size_t > serialize;
 		
-		const std::size_t data = mac::sys::default_thread_stack_size();
+		const bool has = has_ThreadManager();
+		
+		const std::size_t data = has ? default_thread_stack_size() : 0;
 		
 		serialize::deconstruct::apply( result, data, binary );
 	}
