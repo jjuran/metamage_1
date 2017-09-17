ca5c021ce2ce1e4af50fac4d45b9c81c572bbded
Josh Juran <jjuran@metamage.com>
2017-09-17 17:33:43 -0400

    ams-common:  Define JInitCrsr and JSetCrsr

--- v68k/ams/common/cursor_jump.hh
+++ v68k/ams/common/cursor_jump.hh
@@ -6,10 +6,15 @@
 #ifndef CURSORJUMP_HH
 #define CURSORJUMP_HH
 
+struct Cursor;
 
 typedef void (*CursorProcPtr)();  // pascal and C have same calling sequence
 
+typedef pascal void (*SetCursorProcPtr)( const Cursor* crsr );
+
 CursorProcPtr JHideCursor : 0x0800;
 CursorProcPtr JShowCursor : 0x0804;
+CursorProcPtr JInitCrsr   : 0x0814;
+SetCursorProcPtr JSetCrsr : 0x0818;
 
 #endif
