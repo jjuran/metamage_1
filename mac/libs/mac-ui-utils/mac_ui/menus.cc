2a90e26fbe79d6bd365469e0719444edd3fd1dcd
Josh Juran <jjuran@metamage.com>
2017-09-17 23:01:51 -0400

    mac-ui-utils:  Optimize deletion of all menu items
    If the MenuRef isn't opaque, it's much simpler to empty the menu by just
    zeroing three fields and resizing the handle (one trap call), than if we
    make trap calls (that involve copying memory) in a loop.  As a bonus, if
    we ever want to support the 64K ROM (which lacks DeleteMenuItem()), this
    will actually work.

--- mac/libs/mac-ui-utils/mac_ui/menus.cc
+++ mac/libs/mac-ui-utils/mac_ui/menus.cc
@@ -11,6 +11,23 @@ namespace ui  {
 
 void delete_all_menu_items( MenuRef menu )
 {
+#if ! OPAQUE_TOOLBOX_STRUCTS
+	
+	menu[0]->menuWidth  = 0;
+	menu[0]->menuHeight = 0;
+	
+	unsigned char* p = menu[0]->menuData;
+	
+	p += 1 + p[ 0 ];
+	
+	*p++ = '\0';
+	
+	SetHandleSize( (Handle) menu, p - (unsigned char*) *menu );
+	
+	return;
+	
+#endif
+	
 	uint16_t n = CountMenuItems( menu );
 	
 	while ( n > 0 )
