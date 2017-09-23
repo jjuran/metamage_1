d4f0f9beaa52d3db628750143398142c5718f9cc
Josh Juran <jjuran@metamage.com>
2017-09-23 01:30:58 -0400

    ams-ui:  Make WDEF_0() a pascal function

--- v68k/modules/ams-ui/WDEF.hh
+++ v68k/modules/ams-ui/WDEF.hh
@@ -6,6 +6,8 @@
 #ifndef WDEF_HH
 #define WDEF_HH
 
-long WDEF_0( short varCode, struct GrafPort* w, short message, long param );
+struct GrafPort;
+
+pascal long WDEF_0( short varCode, GrafPort* w, short message, long param );
 
 #endif
