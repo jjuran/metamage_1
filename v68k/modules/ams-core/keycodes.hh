a8c48c786a08a137f467333651bcc76852b3a14f
Josh Juran <jjuran@metamage.com>
2017-09-27 17:53:28 -0400

    ams-core:  Add keycode reverse lookup tables

---
+++ v68k/modules/ams-core/keycodes.hh
@@ -0,0 +1,13 @@
+/*
+	keycodes.hh
+	-----------
+*/
+
+#ifndef KEYCODES_HH
+#define KEYCODES_HH
+
+extern const signed char reverse_lookup_general_key[];
+extern const signed char reverse_lookup_from_keypad[];
+extern const signed char reverse_lookup_F_digit_key[];
+
+#endif
