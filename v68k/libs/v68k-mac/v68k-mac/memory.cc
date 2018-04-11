f94449ea34bf3059b9413fa5c201a82d31782d26
Josh Juran <jjuran@metamage.com>
2018-04-11 17:37:07 -0400

    v68k-mac:  Add SPATalkB, SPConfig lowmem globals

--- v68k/libs/v68k-mac/v68k-mac/memory.cc
+++ v68k/libs/v68k-mac/v68k-mac/memory.cc
@@ -43,6 +43,7 @@ enum
 	tag_KeyMaps,
 	tag_KeyMaps_word_7 = tag_KeyMaps + 7,  // 16 bytes
 	tag_UnitNtryCnt,
+	tag_SPATBConfig,
 	tag_SPKbdPrint,
 	tag_Time,
 	tag_Time_low_word,
@@ -183,6 +184,7 @@ static const global globals[] =
 	{ 0x0172, 2,    tag_MBState_esc },  // MBState, Tocks (Button escapes)
 	{ 0x0174, 16,   tag_KeyMaps     },  // KeyMap, KeyPadMap, 4 more bytes
 	{ 0x01D2, 2,    tag_UnitNtryCnt },
+	{ 0x01FA, 2,    tag_SPATBConfig },  // SPATalkB, SPConfig
 	{ 0x0207, 2,    tag_SPKbdPrint  },  // SPKbd, SPPrint
 	{ 0x020C, 0x44, tag_Time        },
 	{ 0x021E, 2,    tag_Kbd_Alarm   },  // KbdType, AlarmState
