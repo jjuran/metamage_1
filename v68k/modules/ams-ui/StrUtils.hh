604f609beb5eeabd103fbf9aeadca6eac0b4c6e6
Josh Juran <jjuran@metamage.com>
2018-05-01 05:06:18 -0400

    ams-ui:  Cull unused GetString_patch() declaration

--- v68k/modules/ams-ui/StrUtils.hh
+++ v68k/modules/ams-ui/StrUtils.hh
@@ -10,6 +10,4 @@ pascal unsigned char** NewString_patch( const unsigned char* s );
 
 pascal void SetString_patch( unsigned char** h, const unsigned char* s );
 
-pascal unsigned char** GetString_patch( short resID );
-
 #endif
