c86453e5226cad11d6583ed1bf63e5144f8a55e1
Josh Juran <jjuran@metamage.com>
2018-05-28 12:46:19 -0400

    libc:  Normalize whitespace in syscalls.68k.c.h

--- lamp/Kerosene/syscalls/syscalls.68k.c.h
+++ lamp/Kerosene/syscalls/syscalls.68k.c.h
@@ -4,46 +4,46 @@
 */
 
 #ifdef __MWERKS__
-	
-	asm int syscall( int number, ... )
-	{
-		MOVE.L  4(SP), D0    // copy system call number to d0
-		MOVE.L  (SP)+, (SP)  // overwrite it with the return address, and pop
-		
-		TRAP  #2
-	}
-	
-	#define DEFINE_SYSCALL_7F( name ) \
-		extern void name();           \
-		asm void name()               \
-		{                             \
-			MOVEQ #__NR_##name,D0  ;  \
-			TRAP  #2               ;  \
-		}
-	
-	#define DEFINE_SYSCALL( name )    \
-		extern void name();           \
-		asm void name()               \
-		{                             \
-			MOVE.W #__NR_##name,D0 ;  \
-			TRAP  #2               ;  \
-		}
-	
-#else
-	
-	#define DEFINE_SYSCALL_7F( name )   \
-		extern void name( void );       \
-		void name( void )               \
-		{                               \
-			asm( 0x7000 + __NR_##name,  \
-			     0x4e42 );              \
-		}
 
-	#define DEFINE_SYSCALL( name )     \
-		void name( void )              \
-		{                              \
-			asm( 0x303c, __NR_##name,  \
-			     0x4e42 );             \
-		}
+asm int syscall( int number, ... )
+{
+	MOVE.L   4(SP), D0    // copy system call number to d0
+	MOVE.L   (SP)+, (SP)  // overwrite it with the return address, and pop
+	
+	TRAP  #2
+}
+
+#define DEFINE_SYSCALL_7F( name )    \
+	extern void name();              \
+	asm void name()                  \
+	{                                \
+		MOVEQ.L  #__NR_##name,D0  ;  \
+		TRAP     #2               ;  \
+	}
+
+#define DEFINE_SYSCALL( name )       \
+	extern void name();              \
+	asm void name()                  \
+	{                                \
+		MOVE.W   #__NR_##name,D0  ;  \
+		TRAP     #2               ;  \
+	}
+
+#else
+
+#define DEFINE_SYSCALL_7F( name )   \
+	extern void name( void );       \
+	void name( void )               \
+	{                               \
+		asm( 0x7000 + __NR_##name,  \
+		     0x4e42 );              \
+	}
+
+#define DEFINE_SYSCALL( name )     \
+	void name( void )              \
+	{                              \
+		asm( 0x303c, __NR_##name,  \
+		     0x4e42 );             \
+	}
 
 #endif
