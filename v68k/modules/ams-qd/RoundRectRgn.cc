c04badaab1f9f07fdd6ced46436b6ea8558b23f4
Josh Juran <jjuran@metamage.com>
2018-08-15 12:51:48 -0400

    ams-qd:  Use circular_region() to make roundrects

--- v68k/modules/ams-qd/RoundRectRgn.cc
+++ v68k/modules/ams-qd/RoundRectRgn.cc
@@ -11,86 +11,8 @@
 #endif
 
 // ams-qd
-#include "OvalRgn.hh"
-#include "Regions.hh"
+#include "circular_region.hh"
 
-
-static
-RgnHandle rectangular_utility_region()
-{
-	static RgnHandle rgn = NewRgn();
-	
-	return rgn;
-}
-
-static
-void XorRgnRect( RgnHandle  rgn,
-                 short      left,
-                 short      top,
-                 short      right,
-                 short      bottom )
-{
-	RgnHandle tmp = rectangular_utility_region();
-	
-	SetRectRgn( tmp, left, top, right, bottom );
-	
-	XorRgn( tmp, rgn, rgn );
-}
-
-static
-void round_rect_corners( RgnHandle rgn, RgnHandle corners )
-{
-	const Rect* bbox;
-	bbox = &rgn[0]->rgnBBox;
-	
-	const short width  = bbox->right - bbox->left;
-	const short height = bbox->bottom - bbox->top;
-	
-	bbox = &corners[0]->rgnBBox;
-	
-	const short ovalWidth  = bbox->right - bbox->left;
-	const short ovalHeight = bbox->bottom - bbox->top;
-	
-	const short hRadius = ovalWidth  / 2;
-	const short vRadius = ovalHeight / 2;
-	
-	static RgnHandle tmp = NewRgn();
-	
-	SetRectRgn( tmp,
-	            0,       0,
-	            hRadius, vRadius );
-	
-	SectRgn( corners, tmp, tmp );
-	XorRgn( tmp, rgn, rgn );
-	
-	SetRectRgn( tmp,
-	            ovalWidth - hRadius, 0,
-	            ovalWidth,           vRadius );
-	
-	SectRgn( corners, tmp, tmp );
-	OffsetRgn( tmp, width - ovalWidth, 0 );
-	XorRgn( tmp, rgn, rgn );
-	
-	SetRectRgn( tmp,
-	            0,       ovalHeight - vRadius,
-	            hRadius, ovalHeight );
-	
-	SectRgn( corners, tmp, tmp );
-	OffsetRgn( tmp, 0, height - ovalHeight );
-	XorRgn( tmp, rgn, rgn );
-	
-	SetRectRgn( tmp,
-	            ovalWidth - hRadius, ovalHeight - vRadius,
-	            ovalWidth,           ovalHeight );
-	
-	SectRgn( corners, tmp, tmp );
-	OffsetRgn( tmp, width - ovalWidth, height - ovalHeight );
-	XorRgn( tmp, rgn, rgn );
-	
-	SetEmptyRgn( tmp );
-}
-
-static RgnHandle corners = NewRgn_patch();
 
 void RoundRectRgn( MacRegion**  rgn,
                    short        width,
@@ -98,15 +20,33 @@ void RoundRectRgn( MacRegion**  rgn,
                    short        ovalWidth,
                    short        ovalHeight )
 {
-	XorRgnRect( rgn, 0, 0, width, height );
-	
-	OvalRgn( corners, ovalWidth, ovalHeight );
-	
-	XorRgnRect( corners, 0, 0, ovalWidth, ovalHeight );
-	
-	round_rect_corners( rgn, corners );
-	
-	SetEmptyRgn( corners );
+	if ( ovalWidth == ovalHeight )
+	{
+		RgnHandle tmp = circular_region( ovalWidth, width, height );
+		
+		XorRgn( tmp, rgn, rgn );
+	}
+	else
+	{
+		// TODO
+	}
+}
+
+static inline
+void FramedRoundRectHollowRgn( MacRegion**  rgn,
+                               short        width,
+                               short        height,
+                               short        ovalWidth,
+                               short        ovalHeight,
+                               short        penWidth,
+                               short        penHeight )
+{
+	if ( (width -= 2 * penWidth) > 0  &&  (height -= 2 * penHeight) > 0 )
+	{
+		RoundRectRgn( rgn, width, height, ovalWidth, ovalHeight );
+		
+		OffsetRgn( rgn, penWidth, penHeight );
+	}
 }
 
 void FramedRoundRectRgn( MacRegion**  rgn,
@@ -117,18 +57,13 @@ void FramedRoundRectRgn( MacRegion**  rgn,
                          short        penWidth,
                          short        penHeight )
 {
-	const short hRadius = ovalWidth  / 2;
-	const short vRadius = ovalHeight / 2;
+	FramedRoundRectHollowRgn( rgn,
+	                          width,
+	                          height,
+	                          ovalWidth,
+	                          ovalHeight,
+	                          penWidth,
+	                          penHeight );
 	
-	XorRgnRect( rgn, hRadius, 0,                  width - hRadius, penHeight );
-	XorRgnRect( rgn, hRadius, height - penHeight, width - hRadius, height    );
-	
-	XorRgnRect( rgn, 0,                vRadius, penWidth, height - vRadius );
-	XorRgnRect( rgn, width - penWidth, vRadius, width,    height - vRadius );
-	
-	FramedOvalRgn( corners, ovalWidth, ovalHeight, penWidth, penHeight );
-	
-	round_rect_corners( rgn, corners );
-	
-	SetEmptyRgn( corners );
+	RoundRectRgn( rgn, width, height, ovalWidth, ovalHeight );
 }
