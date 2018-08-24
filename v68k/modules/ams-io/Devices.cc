81d1e39cbe140f6fa53977e9fbabcfdf6a4dfc6a
Josh Juran <jjuran@metamage.com>
2018-08-24 03:04:49 -0400

    ams-io:  Add initial Console Port output driver

--- v68k/modules/ams-io/Devices.cc
+++ v68k/modules/ams-io/Devices.cc
@@ -10,10 +10,63 @@
 #include <Devices.h>
 #endif
 
+// POSIX
+#include <unistd.h>
+
+
+#define LENGTH( array )  (sizeof array / sizeof *array)
+
+
+typedef OSErr (*driver_routine)( short trap_word : __D1, IOParam* pb : __A0 );
+
+#define CIn_write  NULL
+
+static
+OSErr COut_write( short trap_word : __D1, IOParam* pb : __A0 )
+{
+	ssize_t n_written = write( STDOUT_FILENO, pb->ioBuffer, pb->ioReqCount );
+	
+	if ( n_written < 0 )
+	{
+		return pb->ioResult = ioErr;
+	}
+	
+	pb->ioActCount = n_written;
+	
+	return pb->ioResult = noErr;
+}
+
+struct driver
+{
+	const uint8_t*  name;
+	driver_routine  write;
+};
+
+#define DRIVER( name )  { "\p." #name, name##_write }
+
+static const driver drivers[] =
+{
+	DRIVER( CIn  ),
+	DRIVER( COut ),
+};
 
 short Write_patch( short trap_word : __D1, IOParam* pb : __A0 )
 {
-	return pb->ioResult = notOpenErr;
+	UInt16 index = -100 - pb->ioRefNum;
+	
+	if ( index >= LENGTH( drivers ) )
+	{
+		return pb->ioResult = unitEmptyErr;
+	}
+	
+	const driver& d = drivers[ index ];
+	
+	if ( d.write == NULL )
+	{
+		return pb->ioResult = writErr;
+	}
+	
+	return d.write( trap_word, pb );
 }
 
 short KillIO_patch( short trap_word : __D1, IOParam* pb : __A0 )
