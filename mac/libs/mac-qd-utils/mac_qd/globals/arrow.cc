24ad9602a0b986f2bbcce2d74c174c91a2616b44
Josh Juran <jjuran@metamage.com>
2018-09-08 14:41:57 -0400

    mac-qd-utils:  Add mac::qd::arrow()

---
+++ mac/libs/mac-qd-utils/mac_qd/globals/arrow.cc
@@ -0,0 +1,38 @@
+/*
+	arrow.cc
+	--------
+*/
+
+#include "mac_qd/globals/arrow.hh"
+
+// Mac OS X
+#ifdef __APPLE__
+#include <ApplicationServices/ApplicationServices.h>
+#endif
+
+// missing-macos
+#ifdef MAC_OS_X_VERSION_10_7
+#ifndef MISSING_QUICKDRAW_H
+#include "missing/Quickdraw.h"
+#endif
+#endif
+
+
+namespace mac {
+namespace qd  {
+
+#if OPAQUE_TOOLBOX_STRUCTS
+
+static Cursor qd_arrow;
+
+const Cursor& arrow()
+{
+	GetQDGlobalsArrow( &qd_arrow );
+	
+	return qd_arrow;
+}
+
+#endif
+
+}
+}
