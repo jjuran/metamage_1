95561e6718704e01cc3f4a796b06b0d6feeaec34
Josh Juran <jjuran@metamage.com>
2018-10-20 00:30:20 -0400

    [NEW] kill-systemstats:  Sends KILL to systemstats
    Superuser privileges are required.  I recommend making this setuid-root.

---
+++ tools/posix/kill-systemstats/kill-systemstats.cc
@@ -0,0 +1,15 @@
+/*
+	kill-systemstats.cc
+	-------------------
+*/
+
+// Standard C
+#include <stdlib.h>
+
+
+int main( int argc, char** argv )
+{
+	system( "/usr/bin/killall -9 systemstats" );
+	
+	return 0;
+}
