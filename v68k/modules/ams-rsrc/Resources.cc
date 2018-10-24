e8699b028bcd53fdccab85009d23dc1979f56c44
Josh Juran <jjuran@metamage.com>
2018-10-24 06:51:56 -0400

    ams-rsrc:  Search resource maps in GetResource()

--- v68k/modules/ams-rsrc/Resources.cc
+++ v68k/modules/ams-rsrc/Resources.cc
@@ -30,6 +30,7 @@
 
 // ams-common
 #include "FCB.hh"
+#include "master_pointer.hh"
 #include "module_A4.hh"
 
 // ams-rsrc
@@ -48,6 +49,7 @@ short CurMap      : 0x0A5A;
 short ResErr      : 0x0A60;
 
 
+typedef rsrc_data_element  rsrc_data;
 typedef rsrc_map_header**  RsrcMapHandle;
 
 
@@ -55,6 +57,55 @@ static inline
 asm Handle PtrToHand( const void* p : __A0, long size : __D0 )
 {
 	DC.W     0xA9E3  // _PtrToHand
+}
+
+static
+rsrc_header* find_rsrc( const rsrc_map_header& map, ResType type, short id )
+{
+	const type_list& types = *(type_list*) ((Ptr) &map + map.offset_to_types);
+	
+	uint16_t n_types_1 = types.count_1;
+	
+	const type_header* it = types.list;
+	
+	do
+	{
+		if ( it->type != type )  continue;
+		
+		uint16_t n_rsrcs_1 = it->count_1;
+		uint16_t offset    = it->offset;
+		
+		rsrc_header* rsrc = (rsrc_header*) ((Ptr) &types + offset);
+		
+		do
+		{
+			if ( rsrc->id == id )
+			{
+				return rsrc;
+			}
+		}
+		while ( ++rsrc, n_rsrcs_1-- > 0 );
+	}
+	while ( ++it, n_types_1-- > 0 );
+	
+	return NULL;
+}
+
+static
+const rsrc_data* get_data( const rsrc_map_header& map, const rsrc_header& rsrc )
+{
+	FCB* fcb = get_FCB( map.refnum );
+	
+	Ptr p = fcb->fcbBfAdr;
+	
+	uint32_t offset = rsrc.offset_high_byte << 16
+	                | rsrc.offset_low_word;
+	
+	offset += map.fork_header.offset_to_data;
+	
+	p += offset;
+	
+	return (const rsrc_data*) p;
 }
 
 static
@@ -230,8 +281,55 @@ bool try_to_get( char*              insert_end,
 }
 
 static
+Handle new_res_handle( RsrcMapHandle rsrc_map, rsrc_header& rsrc, ResType type )
+{
+	if ( rsrc.handle == NULL )
+	{
+		const rsrc_data* data = get_data( **rsrc_map, rsrc );
+		
+		// data may be misaligned
+		
+		Size data_length;
+		BlockMoveData( &data->length, &data_length, sizeof (Size) );
+		
+		if ( Handle h = PtrToHand( data->bytes, data_length ) )
+		{
+			master_pointer& mp = *(master_pointer*) h;
+			
+			mp.flags  = kHandleIsResourceMask;
+			mp.offset = (char*) &rsrc - (char*) *rsrc_map;
+			mp.type   = type;
+			mp.base   = rsrc_map;
+			
+			rsrc.handle = h;
+		}
+		else
+		{
+			ResErr = memFullErr;
+			return NULL;
+		}
+	}
+	
+	return rsrc.handle;
+}
+
+static
 Handle GetResource_core( ResType type : __D0, short id : __D1 )
 {
+	Handle h = TopMapHndl;
+	
+	while ( h != NULL )
+	{
+		RsrcMapHandle rsrc_map = (RsrcMapHandle) h;
+		
+		if ( rsrc_header* rsrc = find_rsrc( **rsrc_map, type, id ) )
+		{
+			return new_res_handle( rsrc_map, *rsrc, type );
+		}
+		
+		h = rsrc_map[0]->next_map;
+	}
+	
 	/*
 		The app name can only be 31 bytes (not 32), but with the extra byte,
 		"TYPE" is word-aligned.
