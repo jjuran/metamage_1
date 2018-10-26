6dcf6d6dfbc3513bd7ad378c17b410911436c4c6
Josh Juran <jjuran@metamage.com>
2018-10-26 00:47:49 -0400

    kill-systemstats:  Set the real user ID to root
    Set the real user ID so killall will match root-owned processes.

--- tools/posix/kill-systemstats/kill-systemstats.cc
+++ tools/posix/kill-systemstats/kill-systemstats.cc
@@ -3,12 +3,17 @@
 	-------------------
 */
 
+// POSIX
+#include <unistd.h>
+
 // Standard C
 #include <stdlib.h>
 
 
 int main( int argc, char** argv )
 {
+	setuid( 0 );  // set real-user-id for killall
+	
 	system( "/usr/bin/killall -9 systemstats" );
 	
 	return 0;
