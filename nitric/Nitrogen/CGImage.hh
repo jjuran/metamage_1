jjuran/metamage_1

commit 01437d56360502482d58b7b50529c10b48217e51

Nitrogen:  Use CGFloat, not float, for LP64

nitric/Nitrogen/CGImage.hh
@@ -54,6 +54,8 @@ namespace Nitrogen
 	// This definition of CGBitmapInfo should be regarded as private.
 	typedef CGImageAlphaInfo CGBitmapInfo;
 	
+	typedef float CGFloat;
+	
 #endif
 	
 	inline
@@ -87,7 +89,7 @@ namespace Nitrogen
 	               CGColorSpaceRef         colorspace,
 	               CGBitmapInfo            bitmapInfo,
 	               CGDataProviderRef       provider,
-	               const float             decode[]          = NULL,
+	               const CGFloat           decode[]          = NULL,
 	               bool                    shouldInterpolate = false,
 	               CGColorRenderingIntent  intent            = kCGRenderingIntentDefault )
 	{
@@ -122,7 +124,7 @@ namespace Nitrogen
 	                   size_t             bitsPerPixel,
 	                   size_t             bytesPerRow,
 	                   CGDataProviderRef  provider,
-	                   const float        decode[]          = NULL,
+	                   const CGFloat      decode[]          = NULL,
 	                   bool               shouldInterpolate = false )
 	{
 		CGImageRef result = ::CGImageMaskCreate( width,
@@ -167,7 +169,7 @@ namespace Nitrogen
 	nucleus::owned< CGImageRef >
 	//
 	CGImageCreateWithJPEGDataProvider( CGDataProviderRef       source,
-	                                   const float             decode[]          = NULL,
+	                                   const CGFloat           decode[]          = NULL,
 	                                   bool                    shouldInterpolate = false,
 	                                   CGColorRenderingIntent  intent            = kCGRenderingIntentDefault )
 	{
@@ -192,7 +194,7 @@ namespace Nitrogen
 	nucleus::owned< CGImageRef >
 	//
 	CGImageCreateWithPNGDataProvider( CGDataProviderRef       source,
-	                                  const float             decode[]          = NULL,
+	                                  const CGFloat           decode[]          = NULL,
 	                                  bool                    shouldInterpolate = false,
 	                                  CGColorRenderingIntent  intent            = kCGRenderingIntentDefault )
 	{

commit 183ad5c1589459e09fe1b5ca37dfa2c7a0b04a34

Nitrogen:  Fix 10.4 build regression in CGImage.hh
This was broken by 01437d56360502482d58b7b50529c10b48217e51.

nitric/Nitrogen/CGImage.hh
@@ -54,6 +54,10 @@ namespace Nitrogen
 	// This definition of CGBitmapInfo should be regarded as private.
 	typedef CGImageAlphaInfo CGBitmapInfo;
 	
+#endif
+	
+#ifndef MAC_OS_X_VERSION_10_5
+	
 	typedef float CGFloat;
 	
 #endif