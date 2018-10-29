jjuran/metamage_1

commit 8175006dc737e1de47a5885db655affbb683bdd6

Nitrogen:  Use FSIORefNum, not SInt16, for LP64

nitric/Nitrogen/Files.cc
@@ -25,6 +25,10 @@
 #include "debug/assert.hh"
 
 
+#ifndef __APPLE__
+typedef SInt16 FSIORefNum;
+#endif
+
 namespace nucleus
 {
 	
@@ -917,7 +921,7 @@ Return Value
                                             const UniChar *forkName,
                                             FSIOPermssn    permissions )
      {
-      SInt16 result;
+      FSIORefNum result;
       ThrowOSStatus( ::FSOpenFork( &ref, forkNameLength, forkName, permissions, &result ) );
       return nucleus::owned<FSForkRefNum>::seize( FSForkRefNum( result ) );
      }
@@ -1066,7 +1070,7 @@ Return Value
    FSGetForkCBInfo_Result FSGetForkCBInfo( FSForkRefNum desiredRefNum )
      {
       FSGetForkCBInfo_Result result;
-      SInt16 actualRefNum;
+      FSIORefNum actualRefNum;
       ThrowOSStatus( ::FSGetForkCBInfo( desiredRefNum,
                                         0,
                                         0,
@@ -1082,7 +1086,7 @@ Return Value
                                            FSForkIterator& iterator )
      {
       FSGetForkCBInfo_Result result;
-      SInt16 actualRefNum;
+      FSIORefNum actualRefNum;
       SInt16 realIterator = iterator;
       ThrowOSStatus( ::FSGetForkCBInfo( 0,
                                         volume,

commit 1487e7d39d709f59f4cafa3de6b29d69e070b853

Nitrogen:  Fix 10.4 build regression in Files.cc
This was broken by 8175006dc737e1de47a5885db655affbb683bdd6.

nitric/Nitrogen/Files.cc
@@ -25,7 +25,7 @@
 #include "debug/assert.hh"
 
 
-#ifndef __APPLE__
+#ifndef MAC_OS_X_VERSION_10_5
 typedef SInt16 FSIORefNum;
 #endif
 