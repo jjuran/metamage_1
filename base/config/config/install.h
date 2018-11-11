d2c0e53f2e7f41ff9d0d462db88713e5468caae8
Josh Juran <jjuran@metamage.com>
2018-11-11 16:53:56 -0500

    config:  Add install.h (and ignore site_config.h)

---
+++ base/config/config/install.h
@@ -0,0 +1,16 @@
+/*
+	install.h
+	---------
+*/
+
+#ifndef CONFIG_INSTALL_H
+#define CONFIG_INSTALL_H
+
+// config
+#include "config/site_config.h"
+
+#ifndef CONFIG_INSTALL_PREFIX
+#define CONFIG_INSTALL_PREFIX  "/usr/local"
+#endif
+
+#endif
