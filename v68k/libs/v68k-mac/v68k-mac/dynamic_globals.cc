cedd4a6c1bbfb0e3d408be0060cbacc624b2aa42
Josh Juran <jjuran@metamage.com>
2019-02-05 20:23:24 -0500

    Revert "v68k-mac:  Throttle repeated reads of Ticks"
    We need to rethink this.  It's true that some applications poll Ticks in
    a tight loop, chewing up CPU.  But others have work to do between reads,
    and throttling just makes them laggy.  Perhaps we could make accesses to
    Ticks instant for interrupt-level code, and throttle cooperative code by
    taking an exception and calling reactor_wait().  However, the translator
    doesn't currently reference the machine state, so it can't even know the
    interrupt mask, much less take an exception.  For now, the throttle acts
    more as hazard than guardrail, so out it goes.
    
    This reverts commit d8b20b1eb0d3aef326440ad76d53606903e1348e.

--- v68k/libs/v68k-mac/v68k-mac/dynamic_globals.cc
+++ v68k/libs/v68k-mac/v68k-mac/dynamic_globals.cc
@@ -28,28 +28,11 @@ uint32_t get_Ticks()
 {
 	using namespace v68k::time;
 	
-	static uint32_t Ticks = 0;
-	
 	const uint64_t delta = microsecond_clock() - initial_clock;
 	
 	const unsigned microseconds_per_tick = 1000 * 1000 * 100 / 6015;
 	
-	uint32_t ticks = delta / microseconds_per_tick;
-	
-	if ( ticks == Ticks )
-	{
-		const uint32_t wakeup = ++ticks * 1ull * microseconds_per_tick - delta;
-		
-		timespec ts;
-		ts.tv_sec = 0;
-		ts.tv_nsec = wakeup * 1000;
-		
-		nanosleep( &ts, NULL );
-		
-		return ticks;
-	}
-	
-	return Ticks = ticks;
+	return delta / microseconds_per_tick;
 }
 
 uint32_t get_Time()
