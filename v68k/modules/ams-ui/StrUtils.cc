78a457b66336fa3e9823f5c76ef18d1a26a7e935
Josh Juran <jjuran@metamage.com>
2019-02-19 23:06:17 -0500

    ams-ui:  Rewrite NewString to avoid PtrToHand glue

--- v68k/modules/ams-ui/StrUtils.cc
+++ v68k/modules/ams-ui/StrUtils.cc
@@ -10,14 +10,20 @@
 #include <MacMemory.h>
 #endif
 
+enum
+{
+	_PtrToHand = 0xA9E3,
+};
+
+static inline asm
+StringHandle PtrToHand( const void* p : __A0, UInt32 n : __D0 )
+{
+	_PtrToHand
+}
 
 pascal unsigned char** NewString_patch( const unsigned char* s )
 {
-	Handle h = NULL;
-	
-	PtrToHand( (Ptr) s, &h, 1 + s[ 0 ] );
-	
-	return (StringHandle) h;
+	return PtrToHand( s, 1 + s[ 0 ] );
 }
 
 pascal void SetString_patch( unsigned char** h, const unsigned char* s )
