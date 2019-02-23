38c2c5e0d9bdc3b768d2fb3e06d43b89ced9828d
Josh Juran <jjuran@metamage.com>
2019-02-22 21:49:53 -0500

    ams-common:  Add timeval functions

--- v68k/ams/common/time.cc
+++ v68k/ams/common/time.cc
@@ -12,6 +12,34 @@
 #include "math/integer.hh"
 
 
+void time( timeval* now )
+{
+	gettimeofday( now, NULL );
+}
+
+timeval timeval_sub( const timeval& a, const timeval& b )
+{
+	time_t  sec  = a.tv_sec  - b.tv_sec;
+	int32_t usec = a.tv_usec - b.tv_usec;
+	
+	if ( usec < 0 )
+	{
+		usec += 1000000;
+		--sec;
+	}
+	
+	timeval result = { sec, usec };
+	
+	return result;
+}
+
+bool timeval_less( const timeval& a, const timeval& b )
+{
+	return
+		                          a.tv_sec  < b.tv_sec  ||
+		a.tv_sec == b.tv_sec  &&  a.tv_usec < b.tv_usec;
+}
+
 unsigned long long microseconds( const timeval& tv )
 {
 	using math::integer::long_multiply;
