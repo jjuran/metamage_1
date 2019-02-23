bc41e0e14980c988f86d040f7f302d10f61b16a5
Josh Juran <jjuran@metamage.com>
2019-02-22 20:47:17 -0500

    ams-common:  Use 68K-optimized long_multiply()
    This drops Lode Runner's idle CPU use from 12% to 9%, and that's without
    emulating a 68020.

--- v68k/ams/common/time.cc
+++ v68k/ams/common/time.cc
@@ -8,10 +8,15 @@
 // POSIX
 #include <sys/time.h>
 
+// math
+#include "math/integer.hh"
+
 
 unsigned long long microseconds( const timeval& tv )
 {
-	return tv.tv_sec * 1000000ull + tv.tv_usec;
+	using math::integer::long_multiply;
+	
+	return long_multiply( tv.tv_sec, 1000000 ) + tv.tv_usec;
 }
 
 unsigned long long time_microseconds()
