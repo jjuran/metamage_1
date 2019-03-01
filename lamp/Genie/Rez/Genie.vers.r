51bfc2b09124145732b9be6deb394601864cf476
Josh Juran <jjuran@metamage.com>
2019-03-01 16:08:57 -0500

    Genie:  Bump copyright and version number

--- lamp/Genie/Rez/Genie.vers.r
+++ lamp/Genie/Rez/Genie.vers.r
@@ -1,15 +1,15 @@
-#define COPYRIGHT  "1999-2018 Josh Juran"
+#define COPYRIGHT  "1999-2019 Josh Juran"
 
 #define COPY "\0xA9"
 
 resource 'vers' (1) {
 	0x00,
-	0x72,
+	0x73,
 	alpha,
 	1,
 	smRoman,
-	"0.7.2-alpha.1",
-	"0.7.2-alpha.1, " COPY COPYRIGHT
+	"0.7.3-alpha.1",
+	"0.7.3-alpha.1, " COPY COPYRIGHT
 };
 
 /* Owner resource */
