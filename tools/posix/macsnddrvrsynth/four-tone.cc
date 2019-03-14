cfe82a33086939f3e7bafbabbaedfb0cdbad0359
blitter <keithkaisershot@gmail.com>
2019-03-13 21:05:52 -0700

    [NEW] macsnddrvrsynth: Player-agnostic Mac Sound Driver audio synthesizing tool
    Square-wave and free-form synthesizers are fully implemented and working. Four-tone is not supported yet.

---
+++ tools/posix/macsnddrvrsynth/four-tone.cc
@@ -0,0 +1,16 @@
+/*
+	four-tone.cc
+	------------
+*/
+
+#include "four-tone.hh"
+
+
+size_t synthesize_four_tone_samples( ft_buffer& buffer, uint8_t* output, size_t size )
+{
+	size_t samples_synthesized = 0;
+	
+	// TODO
+	
+	return samples_synthesized;
+}
---
+++ tools/posix/macsnddrvrsynth/free-form.hh
@@ -0,0 +1,14 @@
+/*
+	free-form.hh
+	------------
+*/
+
+#ifndef MACSNDDRVRSYNTH_FREEFORM_HH
+#define MACSNDDRVRSYNTH_FREEFORM_HH
+
+#include "command-queue.hh"
+
+
+size_t synthesize_free_form_samples( ff_buffer& buffer, uint8_t* output, size_t size );
+
+#endif
