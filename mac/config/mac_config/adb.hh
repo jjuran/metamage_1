42cdffe150d80d1017786daccb9a1ecee2805f1e
Josh Juran <jjuran@metamage.com>
2019-05-26 21:14:27 -0400

    mac-config:  Take ADB for granted in 68020+ code

--- mac/config/mac_config/adb.hh
+++ mac/config/mac_config/adb.hh
@@ -20,11 +20,14 @@
 
 /*
 	Take ADB for granted if we're on PPC or if System 7.6 or later is required.
+	Or, if we've been compiled for 68020+, since all such Macs have ADB.
 	System 7.5.5 is the last version that runs on a Mac Plus.
 */
 
 #ifndef CONFIG_ADB_GRANTED
 	#ifdef __POWERPC__
+		#define CONFIG_ADB_GRANTED  1
+	#elif __68020__
 		#define CONFIG_ADB_GRANTED  1
 	#else
 		#define CONFIG_ADB_GRANTED  CONFIG_SYSTEM_7_6_REQUIRED
