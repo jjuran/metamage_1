4fc3a314138359d6db95ea737691dd42d429b449
Josh Juran <jjuran@metamage.com>
2019-06-17 22:03:42 -0400

    ams-ui:  Move draw_text_line() up

--- v68k/modules/ams-ui/TextEdit.cc
+++ v68k/modules/ams-ui/TextEdit.cc
@@ -23,7 +23,38 @@ void draw_text_line( const char*  p,
                      short        h,
                      short        v,
                      short        rectWidth,
-                     short        just );
+                     short        just )
+{
+	// Treat teFlushDefault like teFlushLeft.
+	
+	if ( just & 1 )
+	{
+		// center- or right-justified
+		
+		short textWidth = TextWidth( p, 0, n );
+		
+		short margin = rectWidth - textWidth;
+		
+		switch ( just )
+		{
+			case teCenter:
+				margin /= 2;
+				// fall through
+			
+			case teFlushRight:
+				h += margin;
+				break;
+			
+			default:
+				// Invalid parameter
+				break;
+		}
+	}
+	
+	MoveTo( h, v );
+	
+	DrawText( p, 0, n );
+}
 
 pascal void TEInit_patch()
 {
@@ -161,43 +192,4 @@ pascal void TETextBox_patch( const char* p, long n, const Rect* r, short just )
 		
 		p = q + (q >= first_space);
 	}
-}
-
-static
-void draw_text_line( const char*  p,
-                     long         n,
-                     short        h,
-                     short        v,
-                     short        rectWidth,
-                     short        just )
-{
-	// Treat teFlushDefault like teFlushLeft.
-	
-	if ( just & 1 )
-	{
-		// center- or right-justified
-		
-		short textWidth = TextWidth( p, 0, n );
-		
-		short margin = rectWidth - textWidth;
-		
-		switch ( just )
-		{
-			case teCenter:
-				margin /= 2;
-				// fall through
-			
-			case teFlushRight:
-				h += margin;
-				break;
-			
-			default:
-				// Invalid parameter
-				break;
-		}
-	}
-	
-	MoveTo( h, v );
-	
-	DrawText( p, 0, n );
 }
