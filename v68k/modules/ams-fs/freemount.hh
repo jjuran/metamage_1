3f960689e330b84aef57b26241f5285262aef8f6
Josh Juran <jjuran@metamage.com>
2019-08-02 07:30:31 -0400

    ams-fs:  Factor out more fundamental try_to_get()

--- v68k/modules/ams-fs/freemount.hh
+++ v68k/modules/ams-fs/freemount.hh
@@ -9,6 +9,8 @@
 // plus
 #include "plus/var_string.hh"
 
+int try_to_get( int I, int O, const plus::string& path, plus::var_string& r );
+
 int try_to_get( const char* begin, unsigned len, plus::var_string& data );
 
 #endif
