20b8095a39e1ef6c99faf6859439cd04632828d0
Josh Juran <jjuran@metamage.com>
2019-08-02 07:36:47 -0400

    ams-fs:  Add one-fd try_to_get() overload

--- v68k/modules/ams-fs/freemount.hh
+++ v68k/modules/ams-fs/freemount.hh
@@ -11,6 +11,12 @@
 
 int try_to_get( int I, int O, const plus::string& path, plus::var_string& r );
 
+inline
+int try_to_get( int fd, const plus::string& path, plus::var_string& data )
+{
+	return try_to_get( fd, fd, path, data );
+}
+
 int try_to_get( const char* begin, unsigned len, plus::var_string& data );
 
 #endif
