d1ed3624e822c01df4c4139be4eca495519f77e7
Josh Juran <jjuran@metamage.com>
2020-02-11 07:18:59 -0500

    ams-core:  Help Prince of Persia find its VBL task
    I actually discovered the solution to this issue by reading disassembled
    code from Lemmings, which isn't helped at all by this fix, but whatever.

--- v68k/modules/ams-core/VBL.cc
+++ v68k/modules/ams-core/VBL.cc
@@ -28,6 +28,23 @@ short CurPageOption : 0x0936;
 static timer_node VBL_timer_node;
 static bool       timer_scheduled;
 
+/*
+	The SoundMusicSys module used in games like Prince of Persia expects to
+	find its newly installed VBL task in the VBL queue, but it doesn't check
+	the queue head.  In order to make it work properly, place our own "task"
+	at the head of the queue so the check succeeds.
+*/
+
+static VBLTask dummy_VBL_task;
+
+static
+void dummy_VBL_proc( VBLTask* task : __A0 )
+{
+	// Don't do anything; just stick around in the queue
+	
+	task->vblCount = 0x7FFF;
+}
+
 static
 void timeval_add( timeval& tv, uint32_t more_usecs )
 {
@@ -92,6 +109,13 @@ void do_VBL()
 			// Dequeue this task
 			*slot = it->qLink;
 			
+			if ( VBLQueue.qHead == VBLQueue.qTail )
+			{
+				// This should be our dummy VBL
+				
+				VBLQueue.qHead = VBLQueue.qTail = NULL;
+			}
+			
 			if ( VBLQueue.qHead == NULL  &&  CurPageOption >= 0 )
 			{
 				unlock_screen();
@@ -133,6 +157,17 @@ short VInstall_patch( QElem* vbl : __A0 )
 	
 	Enqueue( vbl, &VBLQueue );
 	
+	if ( VBLQueue.qHead == vbl )
+	{
+		// place our dummy VBL task at the head of the queue
+		
+		dummy_VBL_task.qLink    = vbl;
+		dummy_VBL_task.vblAddr  = (VBLUPP) &dummy_VBL_proc;
+		dummy_VBL_task.vblCount = 0x7FFF;
+		
+		VBLQueue.qHead = (QElem*) &dummy_VBL_task;
+	}
+	
 	if ( ! timer_scheduled )
 	{
 		if ( CurPageOption >= 0 )
@@ -157,6 +192,13 @@ short VRemove_patch( QElem* vbl : __A0 )
 	if ( OSErr err = Dequeue( vbl, &VBLQueue ) )
 	{
 		return err;
+	}
+	
+	if ( VBLQueue.qHead == VBLQueue.qTail )
+	{
+		// This should be our dummy VBL
+		
+		VBLQueue.qHead = VBLQueue.qTail = NULL;
 	}
 	
 	if ( VBLQueue.qHead == NULL )
