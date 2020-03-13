jjuran/metamage_1

commit cf17fcbae57628ea0deeb79f9bf4a4310376e337

ams-common, ams-fs:  Overhaul file refnum scheme
In the real world, file reference numbers are byte offsets, not indices.
(Battle Chess, at least, utterly relies on this to snoop the FCB table.)

v68k/modules/ams-fs/Files.cc
@@ -44,7 +44,7 @@ IO_ProcPtr   old_Write;
 static inline
 short FCB_index( const FCB* fcb )
 {
-	return fcb - FCBSPtr->fcbs + 1;
+	return (Ptr) fcb - (Ptr) FCBSPtr;
 }
 
 static

v68k/ams/common/FCB.hh
@@ -53,9 +53,9 @@ FCBS* FCBSPtr : 0x034E;
 inline
 FCB* get_FCB( unsigned short refNum )
 {
-	if ( --refNum < kFCBCount )
+	if ( refNum > 0 )
 	{
-		return &FCBSPtr->fcbs[ refNum ];
+		return (FCB*) ((char*) FCBSPtr + refNum);
 	}
 	
 	return NULL;

commit e6a952192e6734c6019207fbdd1741cd4f7299e5

ams-fs:  Fix appfs check for app data fork filenum
This was broken by cf17fcbae57628ea0deeb79f9bf4a4310376e337.

v68k/modules/ams-fs/Files.cc
@@ -73,7 +73,7 @@ FCB* find_next_empty_FCB()
 static inline
 bool is_current_application( uint32_t flNum )
 {
-	const FCB& curApFCB = FCBSPtr->fcbs[ CurApRefNum - 1 ];
+	const FCB& curApFCB = *get_FCB( CurApRefNum );
 	
 	return curApFCB.fcbFlNum == flNum;
 }