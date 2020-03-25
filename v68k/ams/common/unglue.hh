b2c376cc1dd85e2c277a357f1b1b807ea77f32c1
Josh Juran <jjuran@metamage.com>
2020-03-24 21:05:00 -0400

    ams-common:  Add CopyHandle() utility function

--- v68k/ams/common/unglue.hh
+++ v68k/ams/common/unglue.hh
@@ -9,9 +9,13 @@
 
 enum
 {
-	_PtrToHand = 0xA9E3,
+	_SetHandleSize = 0xA024,
+	
+	_PtrToHand   = 0xA9E3,
+	_HandAndHand = 0xA9E4,
 };
 
+typedef char**           Handle;
 typedef unsigned char**  StringHandle;
 typedef unsigned long    UInt32;
 
@@ -19,6 +23,18 @@ inline asm
 StringHandle PtrToHand( const void* p : __A0, UInt32 n : __D0 )
 {
 	_PtrToHand
+}
+
+inline asm
+Handle CopyHandle( Handle src : __A1, Handle dst : __A0 )
+{
+	MOVEQ.L  #0,D0
+	_SetHandleSize
+	
+	EXG      A0,A1
+	_HandAndHand
+	
+	// dst is now in A0
 }
 
 #endif
