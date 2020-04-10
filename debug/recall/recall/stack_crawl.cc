9e5b08be231800c75595d2d08f5780fb1390e9d3
Josh Juran <jjuran@metamage.com>
2020-04-10 01:28:36 -0400

    recall:  Do crawl the stack when LLVM-compiled

--- debug/recall/recall/stack_crawl.cc
+++ debug/recall/recall/stack_crawl.cc
@@ -14,7 +14,7 @@
 #endif
 
 
-#if defined( __llvm__ )  ||  defined( __ELF__ )  ||  defined(__INTERIX)
+#if defined( __ELF__ )  ||  defined(__INTERIX)
 #define CONFIG_STACK_CRAWLS 0
 #else
 #define CONFIG_STACK_CRAWLS 1
