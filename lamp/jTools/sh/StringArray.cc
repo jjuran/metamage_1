6e6d4a554eb76835303b4fc7ddf5616b97bf6918
Josh Juran <jjuran@metamage.com>
2020-07-17 02:16:02 -0400

    sh:  Axe unused StringArray code

--- lamp/jTools/sh/StringArray.cc
+++ lamp/jTools/sh/StringArray.cc
@@ -1,6 +1,7 @@
-// ==============
-// StringArray.cc
-// ==============
+/*
+	StringArray.cc
+	--------------
+*/
 
 #include "StringArray.hh"
 
@@ -23,34 +24,6 @@ namespace ShellShock
 		return result;
 	}
 	
-	void InitializeSVector( SVector& sv, std::size_t size )
-	{
-		sv.resize( size + 1 );
-		
-		// In case allocation fails mid-way, we'll know which slots have data.
-		// This also fills NULL into the last slot.
-		std::fill( sv.begin(),
-		           sv.end(),
-		           (char*)NULL );
-	}
-	
-	StringArray::StringArray()
-	{
-		array.push_back( NULL );
-	}
-	
-	StringArray::StringArray( char const* const* arrayPointer )
-	{
-		int count = CountStringArray( arrayPointer );
-		
-		// Assume arrayPointer to be already sorted.
-		std::copy( arrayPointer,
-		           arrayPointer + count,
-		           std::back_inserter( strings ) );
-		
-		Update();
-	}
-	
 	StringArray::StringArray( const std::vector< plus::string >& args )
 	{
 		// Assume arrayPointer to be already sorted.
@@ -60,81 +33,7 @@ namespace ShellShock
 		
 		Update();
 	}
-	
-	void StringArray::Append( const plus::string& s )
-	{
-		strings.push_back( s );
 		
-		/*
-		array.back() = strings.back().c_str();
-		array.push_back( NULL );
-		*/
-		
-		Update();
-	}
-	
-	void StringArray::Prepend( const plus::string& s )
-	{
-		//strings.push_front( s );
-		
-		strings.resize( strings.size() + 1 );
-		
-		std::copy( strings.begin(),
-		           strings.end() - 1,
-		           strings.begin() + 1 );
-		
-		strings.front() = s;
-		
-		/*
-		array.resize( array.size() + 1 );
-		
-		std::copy( array.begin(),
-		           array.end() - 1,
-		           array.begin() + 1 );
-		
-		array.front() = strings.front().c_str();
-		*/
-		
-		Update();
-	}
-	
-	void StringArray::Insert( SVector::iterator location, const plus::string& s )
-	{
-		std::size_t offset = location - array.begin();
-		
-		strings.resize( strings.size() + 1 );
-		
-		std::copy_backward( strings.begin() + offset,
-		                    strings.end() - 1,
-		                    strings.end() );
-		
-		strings[ offset ] = s;
-		
-		Update();
-	}
-	
-	void StringArray::Overwrite( SVector::iterator location, const plus::string& s )
-	{
-		std::size_t offset = location - array.begin();
-		
-		strings[ offset ] = s;
-		
-		Update();
-	}
-	
-	void StringArray::Remove( SVector::iterator location )
-	{
-		std::size_t offset = location - array.begin();
-		
-		std::copy( strings.begin() + offset + 1,
-		           strings.end(),
-		           strings.begin() + offset );
-		
-		strings.resize( strings.size() - 1 );
-		
-		Update();
-	}
-	
 	void StringArray::Update()
 	{
 		array.reserve( strings.size() + 1 );
@@ -145,21 +44,6 @@ namespace ShellShock
 		}
 		
 		array.push_back( NULL );
-	}
-	
-	void StringArray::Clear()
-	{
-		array.clear();  // Don't leave dangling pointers; clear array first
-		array.push_back( NULL );
-		strings.clear();
-	}
-	
-	void StringArray::Swap( StringArray& other )
-	{
-		using std::swap;
-		
-		swap( strings, other.strings );
-		swap( array,   other.array   );
 	}
 	
 }
--- lamp/jTools/sh/StringArray.hh
+++ lamp/jTools/sh/StringArray.hh
@@ -1,9 +1,10 @@
-// ==============
-// StringArray.hh
-// ==============
+/*
+	StringArray.hh
+	--------------
+*/
 
-#ifndef SHELLSHOCK_STRINGARRAY_HH
-#define SHELLSHOCK_STRINGARRAY_HH
+#ifndef STRINGARRAY_HH
+#define STRINGARRAY_HH
 
 // Standard C++
 #include <vector>
@@ -15,43 +16,26 @@
 namespace ShellShock
 {
 	
-	typedef std::vector< const char* > SVector;
-	
 	std::size_t CountStringArray( char const* const* arrayPointer );
-	
-	void InitializeSVector( SVector& sv, std::size_t size );
 	
 	class StringArray
 	{
-		typedef SVector Array;
+		typedef std::vector< const char* > Array;
 		
 		private:
-			StringArray( const StringArray& );
-			
+			// non-copyable
+			StringArray           ( const StringArray& );
 			StringArray& operator=( const StringArray& );
 		
-		protected:
 			std::vector< plus::string > strings;
 			Array array;
 		
 		public:
-			StringArray();
-			StringArray( char const* const* arrayPointer );
 			StringArray( const std::vector< plus::string >& args );
 			
 			char** GetPointer()  { return const_cast< char** >( &array[ 0 ] ); }
 			
-			void Append ( const plus::string& s );
-			void Prepend( const plus::string& s );
-			
-			void Insert   ( SVector::iterator location, const plus::string& s );
-			void Overwrite( SVector::iterator location, const plus::string& s );
-			void Remove   ( SVector::iterator location );
-			void Clear();
-			
 			void Update();
-			
-			void Swap( StringArray& other );
 	};
 	
 }
