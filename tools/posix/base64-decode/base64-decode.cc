f5f07be97bc116f2b061ef2c84d872ecde5b380d
Josh Juran <jjuran@metamage.com>
2020-10-14 14:37:18 -0400

    [NEW] base64-decode:  Command-line base64 decoder

---
+++ tools/posix/base64-decode/base64-decode.cc
@@ -0,0 +1,77 @@
+/*
+	base64-decode.cc
+	----------------
+*/
+
+// POSIX
+#include <unistd.h>
+
+// Standard C
+#include <string.h>
+
+// iota
+#include "iota/freestore_free.hh"
+
+// more-posix
+#include "more/perror.hh"
+
+// plus
+#include "plus/var_string.hh"
+
+// vxo
+#include "vxo/error.hh"
+#include "vxo/lib/base64_decode.hh"
+
+
+#pragma exceptions off
+
+
+#define PROGRAM  "base64-decode"
+
+
+static inline
+void report_error( const char* msg )
+{
+	more::perror( PROGRAM, msg );
+}
+
+static
+void write_ln( int fd, plus::var_string& s )
+{
+	char*  p = &s[ 0 ];
+	size_t n = s.size();
+	
+	char c = p[ n ];
+	
+	p[ n ] = '\n';
+	
+	write( STDOUT_FILENO, p, n + 1 );
+	
+	p[ n ] = c;
+}
+
+int main( int argc, char** argv )
+{
+	char** argp = argv + 1;
+	
+	bool failed = false;
+	
+	while ( *argp )
+	{
+		const char* input = *argp++;
+		
+		vxo::Box box = vxo::base64_decode( input, strlen( input ) );
+		
+		if ( const vxo::Error* error = box.is< vxo::Error >() )
+		{
+			report_error( error->get() );
+			
+			failed = true;
+			continue;
+		}
+		
+		write_ln( STDOUT_FILENO, *(plus::var_string*) &box );
+	}
+	
+	return failed;
+}
