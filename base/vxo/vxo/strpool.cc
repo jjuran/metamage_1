a63064fd8d0d4d31e4e7057d02a22829a8566bbf
Josh Juran <jjuran@metamage.com>
2020-12-12 11:30:27 -0500

    vxo:  Add StrPool type for interning strings
    A StrPool is a repository for interned strings.  An input string will be
    copied into one of several "lane" buffers, which don't move in memory --
    allowing the string's address to be returned and used thereafter for the
    life of the program.  StrPool is not a subtype of Box and isn't designed
    to be contained or copied; usually there should be only one per program.

---
+++ base/vxo/vxo/strpool.cc
@@ -0,0 +1,83 @@
+/*
+	strpool.cc
+	----------
+*/
+
+#include "vxo/strpool.hh"
+
+// Standard C
+#include <stdlib.h>
+#include <string.h>
+
+// Standard C++
+#include <algorithm>
+
+// debug
+#include "debug/assert.hh"
+
+// plus
+#include "plus/string.hh"
+
+
+namespace vxo
+{
+
+const size_t default_lane_size = 1024;
+
+const char* Lane::append( const char* p, size_t n )
+{
+	ASSERT( bytes_free() >= n );
+	
+	char* dst = its_data + its_mark;
+	
+	memcpy( dst, p, n );
+	
+	its_mark += n;
+	
+	return dst;
+}
+
+StrPool::~StrPool()
+{
+	for ( size_t i = 0;  i < its_lanes.size();  ++i )
+	{
+		free( its_lanes[ i ] );
+	}
+}
+
+const char* StrPool::operator[]( const plus::string& key )
+{
+	Item* lb = std::lower_bound( its_bone.begin(), its_bone.end(), key );
+	
+	if ( lb != end()  &&  *lb == key )
+	{
+		return *lb;
+	}
+	
+	// We didn't find the string already interned, so add it.
+	
+	const size_t size = key.size() + 1;  // Leave room for a trailing NUL byte
+	
+	if ( its_lanes.empty()  ||  its_lanes.back()->bytes_free() < size )
+	{
+		size_t lane_size = size > default_lane_size ? size : default_lane_size;
+		size_t full_size = sizeof (Lane) - max_lane_size + lane_size;
+		
+		if ( Lane* lane = (Lane*) malloc( full_size ) )
+		{
+			its_lanes.push_back( lane );
+		}
+		else
+		{
+			return NULL;
+		}
+	}
+	
+	const char* inserted = its_lanes.back()->append( key.c_str(), size );
+	
+	*its_bone.insert( lb ) = inserted;
+	
+	return inserted;
+}
+
+}
---
+++ base/vxo/vxo/strpool.hh
@@ -0,0 +1,75 @@
+/*
+	strpool.hh
+	----------
+*/
+
+#ifndef VXO_STRPOOL_HH
+#define VXO_STRPOOL_HH
+
+// vxo
+#include "vxo/ptrvec.hh"
+#include "vxo/string.hh"
+
+
+namespace vxo
+{
+	
+	const size_t max_lane_size = 64 * 2014;
+	
+	class Lane
+	{
+		private:
+			size_t its_size;
+			size_t its_mark;
+			
+			char its_data[ max_lane_size ];
+		
+		public:
+			explicit Lane( size_t size ) : its_size( size ), its_mark()
+			{
+			}
+			
+			size_t bytes_free() const  { return its_size - its_mark; }
+			
+			const char* append( const char* p, size_t n );
+	};
+	
+	class StrPool
+	{
+		private:
+			PtrVec_< Lane > its_lanes;
+			
+			PtrVec_< const char > its_bone;
+			
+			// non-copyable
+			StrPool           ( const StrPool& );
+			StrPool& operator=( const StrPool& );
+		
+		public:
+			typedef const char* Item;
+			typedef const char* value_type;
+			
+			typedef value_type*       iterator;
+			typedef value_type const* const_iterator;
+			
+			typedef value_type& reference;
+			typedef value_type  const_reference;
+			
+			StrPool() {}
+			
+			~StrPool();
+			
+			size_t size() const  { return its_bone.size(); }
+			
+			Item const* begin() const  { return its_bone.begin(); }
+			Item const* end()   const  { return begin() + size(); }
+			
+			Item const* cbegin() const  { return begin(); }
+			Item const* cend()   const  { return end();   }
+			
+			Item operator[]( const plus::string& key );
+	};
+	
+}
+
+#endif
