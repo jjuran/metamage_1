d200e45bb5c175ca11d352dc9744648b5c88f65f
Josh Juran <jjuran@metamage.com>
2021-01-02 00:24:18 -0500

    Genie:  Unindent execve.cc

--- lamp/Genie/Genie/Kernel/execve.cc
+++ lamp/Genie/Genie/Kernel/execve.cc
@@ -31,66 +31,66 @@
 
 namespace Genie
 {
+
+namespace N = Nitrogen;
+
+
+static int execve( const char*        path,
+                   const char* const  argv[],
+                   const char* const  envp[] )
+{
+	// On a successful exec, we don't return to the dispatcher, but we compensate.
+	// Exec() calls Suspend(), which is equivalent to LeaveSystemCall().
+	// ResumeAfterFork() calls Resume() and LeaveSystemCall().
 	
-	namespace N = Nitrogen;
-	
-	
-	static int execve( const char*        path,
-	                   const char* const  argv[],
-	                   const char* const  envp[] )
+	try
 	{
-		// On a successful exec, we don't return to the dispatcher, but we compensate.
-		// Exec() calls Suspend(), which is equivalent to LeaveSystemCall().
-		// ResumeAfterFork() calls Resume() and LeaveSystemCall().
+		Process& current = current_process();
+		
+		// Start a new thread with the child's process context
+		current.Exec( path, argv, envp );
+		
+		// Not reached
+	}
+	catch ( const Mac::OSStatus& err )
+	{
+		if ( err == resNotFound )
+		{
+			return set_errno( EINVAL );
+		}
+		
+		plus::string errMsg = "\n";
 		
 		try
 		{
-			Process& current = current_process();
-			
-			// Start a new thread with the child's process context
-			current.Exec( path, argv, envp );
-			
-			// Not reached
+			throw;
 		}
-		catch ( const Mac::OSStatus& err )
+		catch ( const N::ErrMessage& msg )
 		{
-			if ( err == resNotFound )
-			{
-				return set_errno( EINVAL );
-			}
-			
-			plus::string errMsg = "\n";
-			
-			try
-			{
-				throw;
-			}
-			catch ( const N::ErrMessage& msg )
-			{
-				errMsg = ", errMessage: " + plus::string( msg.errMessage ) + "\n";
-			}
-			catch ( ... )  {}
-			
-			if ( err.Get() != fnfErr )
-			{
-				std::printf( "execve: %s: OSStatus %d%s", path, int( err.Get() ), errMsg.c_str() );
-			}
-			
-			return set_errno_from_exception();
+			errMsg = ", errMessage: " + plus::string( msg.errMessage ) + "\n";
 		}
-		catch ( ... )
+		catch ( ... )  {}
+		
+		if ( err.Get() != fnfErr )
 		{
-			return set_errno_from_exception();
+			std::printf( "execve: %s: OSStatus %d%s", path, int( err.Get() ), errMsg.c_str() );
 		}
 		
-		// Not reached
-		return 0;
+		return set_errno_from_exception();
+	}
+	catch ( ... )
+	{
+		return set_errno_from_exception();
 	}
 	
-	#pragma force_active on
-	
-	REGISTER_SYSTEM_CALL( execve );
-	
-	#pragma force_active reset
-	
+	// Not reached
+	return 0;
+}
+
+#pragma force_active on
+
+REGISTER_SYSTEM_CALL( execve );
+
+#pragma force_active reset
+
 }
