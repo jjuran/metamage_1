jjuran/metamage_1

commit 23928b7a825722837bcd2c62d310801dfea735b0

Genie:  Add explicit sys_cpu_mux_params

lamp/Genie/Genie/FS/sys/cpu.cc
@@ -28,6 +28,7 @@
 #include "plus/var_string.hh"
 
 // vfs
+#include "vfs/node.hh"
 #include "vfs/property.hh"
 #include "vfs/node/types/property_file.hh"
 
@@ -146,26 +147,6 @@ namespace Genie
 		return name;
 	}
 	
-	struct GetCPUFamily
-	{
-		typedef const char* Result;
-		
-		static Result Get()
-		{
-			return GetCPUName( GetCPUCode( 'cpuf' ) );
-		}
-	};
-	
-	struct GetCPUType
-	{
-		typedef const char* Result;
-		
-		static Result Get()
-		{
-			return GetCPUName( GetCPUCode( 'cput' ) );
-		}
-	};
-	
 	struct GetPrivilegeMode
 	{
 		typedef const char* Result;
@@ -189,25 +170,44 @@ namespace Genie
 		}
 	};
 	
-	template < class Accessor >
-	struct sys_cpu_Property : vfs::readonly_property
+	static
+	void get( plus::var_string& result, const vfs::node* that, bool binary, const plus::string& name )
 	{
-		static void get( plus::var_string& result, const vfs::node* that, bool binary )
+		const char c = name[ 0 ];
+		
+		switch ( c )
 		{
-			result = Accessor::Get();
+			case 'f':  result = GetCPUName( GetCPUCode( 'cpuf' ) );  break;
+			case 't':  result = GetCPUName( GetCPUCode( 'cput' ) );  break;
+			
+		#if TARGET_CPU_68K
+			
+			case 'm':  result = GetPrivilegeMode::Get();
+				break;
+			
+		#endif
+			
+			default:
+				break;
 		}
+	}
+	
+	static const vfs::property_params sys_cpu_mux_params =
+	{
+		vfs::no_fixed_size,
+		(vfs::property_get_hook) &get,
 	};
 	
-	#define PROPERTY( prop )  &vfs::new_property, &vfs::property_params_factory< prop >::value
+	#define PROPERTY( prop )  &vfs::new_property, &prop##_params
 	
 	const vfs::fixed_mapping sys_cpu_Mappings[] =
 	{
-		{ "family", PROPERTY( sys_cpu_Property< GetCPUFamily > ) },
-		{ "type",   PROPERTY( sys_cpu_Property< GetCPUType   > ) },
+		{ "family", PROPERTY( sys_cpu_mux ) },
+		{ "type",   PROPERTY( sys_cpu_mux ) },
 		
 	#if TARGET_CPU_68K
 		
-		{ "mode", PROPERTY( sys_cpu_Property< GetPrivilegeMode > ) },
+		{ "mode", PROPERTY( sys_cpu_mux },
 		
 	#endif
 		

commit dca1db32b66ab7bec3da031c506fbf38f9412b5a

Genie:  Fix syntax error typo gobbled up by mxcpp
This was broken in 23928b7a825722837bcd2c62d310801dfea735b0, over a year
ago.  It needed a 68K build without separate preprocessing to expose it.

lamp/Genie/Genie/FS/sys/cpu.cc
@@ -207,7 +207,7 @@ namespace Genie
 		
 	#if TARGET_CPU_68K
 		
-		{ "mode", PROPERTY( sys_cpu_mux },
+		{ "mode", PROPERTY( sys_cpu_mux ) },
 		
 	#endif
 		