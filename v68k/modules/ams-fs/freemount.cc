2924ad24c3496c534d395c3ea1ac1f06add27f4e
Josh Juran <jjuran@metamage.com>
2021-03-18 23:09:42 -0400

    ams-fs:  Add try_to_list()

--- v68k/modules/ams-fs/freemount.cc
+++ v68k/modules/ams-fs/freemount.cc
@@ -11,6 +11,9 @@
 // Standard C++
 #include <new>
 
+// chars
+#include "conv/mac_utf8.hh"
+
 // plus
 #include "plus/mac_utf8.hh"
 
@@ -22,6 +25,7 @@
 
 
 using plus::string;
+using plus::utf8_from_mac;
 using plus::var_string;
 
 int try_to_get( int in, int out, const string& path, var_string& data )
@@ -74,6 +78,58 @@ int try_to_put( int fd, const plus::string& path, const plus::string& data )
 		F::synced_put( in, out, plus::utf8_from_mac( path ), data );
 		
 		return 0;
+	}
+	catch ( const F::path_error& e )
+	{
+		return -e.error;
+	}
+	catch ( ... )
+	{
+		return -EIO;
+	}
+}
+
+static
+void per_dirent( const char* name, uint32_t size, void* x )
+{
+	var_string& data = *(var_string*) x;
+	
+	const unsigned mac_size = conv::sizeof_mac_from_utf8( name, name + size );
+	
+	const int max_len = dirent_size - 1;
+	
+	char buffer[ 1 + max_len ];
+	
+	if ( mac_size <= max_len )
+	{
+		char* p = buffer;
+		
+		*p++ = mac_size;
+		
+		conv::mac_from_utf8_nothrow( p, mac_size, name, size );
+		
+		data.append( buffer, 1 + mac_size );
+		data.resize( data.size() + max_len - mac_size );
+	}
+}
+
+int try_to_list( int in, int out, const string& path, var_string& data )
+{
+	namespace F = freemount;
+	
+	scoped_zone null_zone;
+	
+	try
+	{
+		data.clear();
+		
+		F::synced_dir( in, out, utf8_from_mac( path ), &per_dirent, &data );
+		
+		return 0;
+	}
+	catch ( const std::bad_alloc& )
+	{
+		return -ENOMEM;
 	}
 	catch ( const F::path_error& e )
 	{
--- v68k/modules/ams-fs/freemount.hh
+++ v68k/modules/ams-fs/freemount.hh
@@ -9,6 +9,11 @@
 // plus
 #include "plus/var_string.hh"
 
+enum
+{
+	dirent_size = 64,
+};
+
 int try_to_get( int I, int O, const plus::string& path, plus::var_string& r );
 
 inline
@@ -20,5 +25,22 @@ int try_to_get( int fd, const plus::string& path, plus::var_string& data )
 int try_to_get( const char* begin, unsigned len, plus::var_string& data );
 
 int try_to_put( int fd, const plus::string& path, const plus::string& data );
+
+int try_to_list( int I, int O, const plus::string& path, plus::var_string& r );
+
+inline
+int try_to_list( int fd, const plus::string& path, plus::var_string& data )
+{
+	return try_to_list( fd, fd, path, data );
+}
+
+inline
+int try_to_list( const plus::string& path, plus::var_string& data )
+{
+	const int in  = 6;
+	const int out = 7;
+	
+	return try_to_list( in, out, path, data );
+}
 
 #endif
