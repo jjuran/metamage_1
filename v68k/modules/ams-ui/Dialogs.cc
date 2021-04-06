jjuran/metamage_1

commit a93951506648fa9e4e6ef1d99d327e9662cf925e

ams-ui:  Draw all dialog text items with its TERec
Use a dialog's TE record to draw all statText and editText items, rather
than just the selected edit field.

v68k/modules/ams-ui/Dialogs.cc
@@ -971,12 +971,6 @@ pascal void DrawDialog_patch( DialogRef dialog )
 				
 				InsetRect( &box, -3, -3 );
 				FrameRect( &box );
-				
-				if ( d->editField + 1 == item_index )
-				{
-					TEUpdate( &bounds, d->textH );
-					break;
-				}
 			}
 			// fall through
 			
@@ -984,7 +978,9 @@ pascal void DrawDialog_patch( DialogRef dialog )
 			{
 				Handle h = item->handle;
 				
-				TETextBox( *h, GetHandleSize( h ), &bounds, 0 );
+				update_edit_record( d->textH, item );
+				
+				TEUpdate( &bounds, d->textH );
 				break;
 			}
 			
@@ -1022,6 +1018,11 @@ pascal void DrawDialog_patch( DialogRef dialog )
 		++item_index;
 	}
 	while ( --n_items_1 >= 0 );
+	
+	if ( const DialogItem* edit = get_editField( d ) )
+	{
+		update_edit_record( d->textH, edit );
+	}
 }
 
 #pragma mark -
@@ -1357,11 +1358,16 @@ pascal void SetIText_patch( Handle h, const unsigned char* text )
 		return;
 	}
 	
-	scoped_port thePort = (DialogRef) d;
-	
 	DialogItem* item = recover_dialog_item( d, h );
 	
-	TETextBox( text + 1, len, &item->bounds, 0 );
+	update_edit_record( hTE, item );
+	
+	TEUpdate( &item->bounds, hTE );
+	
+	if ( const DialogItem* edit = get_editField( d ) )
+	{
+		update_edit_record( hTE, edit );
+	}
 }
 
 pascal void SelIText_patch( GrafPort*  dialog,

commit 0b77d9ed53c1bcfad64ed85a592800ed28b17948

ams-ui:  Ignore key events in edit-less dialogs
It seems this was broken since a93951506648fa9e4e6ef1d99d327e9662cf925e.

v68k/modules/ams-ui/Dialogs.cc
@@ -913,7 +913,7 @@ pascal Boolean DialogSelect_patch( const EventRecord*  event,
 	}
 	else if ( event->what == keyDown  ||  event->what == autoKey )
 	{
-		if ( TEHandle hTE = d->textH )
+		if ( d->editField >= 0 )
 		{
 			const char c = (char) event->message;
 			
@@ -923,7 +923,7 @@ pascal Boolean DialogSelect_patch( const EventRecord*  event,
 				return false;
 			}
 			
-			TEKey( c, hTE );
+			TEKey( c, d->textH );
 			
 			const DialogItem* item = get_editField( d );
 			