af4869a91d1658583d3c31d6857c28b2d0ed3fca
Josh Juran <jjuran@metamage.com>
2021-04-30 23:31:03 -0400

    Amethyst:  Fix to build for Mac OS X 10.2 "Jaguar"
    CGDisplayGetDrawingContext() was added in Panther, but we don't need it.

--- apps/mac/Amethyst/blit_CG.cc
+++ apps/mac/Amethyst/blit_CG.cc
@@ -19,7 +19,11 @@ CG_blitter::CG_blitter( CGDirectDisplayID id, CGRect bounds )
 {
 	image_bounds = bounds;
 	
+#ifdef MAC_OS_X_VERSION_10_3
+	
 	captured_display_context = CGDisplayGetDrawingContext( id );
+	
+#endif
 }
 
 CG_blitter::~CG_blitter()
