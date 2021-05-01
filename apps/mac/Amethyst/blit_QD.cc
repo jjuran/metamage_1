jjuran/metamage_1

commit de23720b4d95df790f877be321086d07b9a98630

Amethyst:  Move QD/CG setup/end/blit to blit units

apps/mac/Amethyst/blit_QD.cc
@@ -1,0 +1,58 @@
+/*
+	blit_QD.cc
+	----------
+*/
+
+#include "blit_QD.hh"
+
+// Amethyst
+#include "quickdraw.hh"
+
+
+static CGrafPtr captured_display_port;
+
+static BitMap dstBits;
+static Rect   dstRect;
+
+#if CONFIG_QUICKDRAW
+
+QD_blitter::QD_blitter( CGDirectDisplayID id, CGRect bounds )
+{
+	dstRect.top    = bounds.origin.y;
+	dstRect.left   = bounds.origin.x;
+	dstRect.bottom = bounds.origin.y + bounds.size.height;
+	dstRect.right  = bounds.origin.x + bounds.size.width;
+	
+	// UInt32 cast required for 10.4 PPC, at least
+	
+	captured_display_port = CreateNewPortForCGDisplayID( (UInt32) id );
+}
+
+QD_blitter::~QD_blitter()
+{
+	Rect bounds;
+	PaintRect( GetPortBounds( captured_display_port, &bounds ) );
+	
+	DisposePort( captured_display_port );
+}
+
+void QD_blitter::prep( int stride, int width, int height )
+{
+	srcBits.rowBytes      = stride;
+	srcBits.bounds.bottom = height;
+	srcBits.bounds.right  = width;
+}
+
+void QD_blitter::blit( const void* src_addr )
+{
+	srcBits.baseAddr = (Ptr) src_addr;
+	
+	CopyBits( &srcBits,
+	          GetPortBitMapForCopyBits( captured_display_port ),
+	          &srcBits.bounds,
+	          &dstRect,
+	          srcCopy,
+	          NULL );
+}
+
+#endif

apps/mac/Amethyst/Amethyst.cc
@@ -23,9 +23,10 @@
 
 // Amethyst
 #include "apple_events.hh"
+#include "blit_CG.hh"
+#include "blit_QD.hh"
 #include "coprocess.hh"
 #include "display.hh"
-#include "image.hh"
 #include "keycodes.hh"
 #include "make_raster.hh"
 #include "quickdraw.hh"
@@ -277,14 +278,11 @@ bool handle_EventRecord( const EventRecord& event )
 	return true;
 }
 
-static
-CGImageRef create_raster_image( void* addr, const raster::raster_desc& desc )
-{
-	return create_bilevel_image( addr,
-	                             desc.stride,
-	                             desc.width,
-	                             desc.height );
-}
+#if CONFIG_QUICKDRAW
+	typedef QD_blitter Blitter;
+#else
+	typedef CG_blitter Blitter;
+#endif
 
 static
 void RunEventLoop()
@@ -309,6 +307,10 @@ void RunEventLoop()
 	
 	CGRect bounds = CGRectInset( display_bounds, x_offset, y_offset );
 	
+	Blitter blitter( captured_display.id(), bounds );
+	
+	blitter.prep( desc.stride, desc.width, desc.height );
+	
 	raster_monitor monitored_raster;
 	
 	coprocess_launch launched_coprocess( raster_path );
@@ -332,20 +334,6 @@ void RunEventLoop()
 #endif
 	
 	cursor_limit = CGPointMake( desc.width, desc.height );
-	
-#if CONFIG_QUICKDRAW
-	
-	const Rect frame =
-	{
-		bounds.origin.y,
-		bounds.origin.x,
-		bounds.origin.y + bounds.size.height,
-		bounds.origin.x + bounds.size.width,
-	};
-	
-	BitMap bitmap = { 0, desc.stride, { 0, 0, desc.height, desc.width } };
-	
-#endif
 	
 	CGWarpMouseCursorPosition( CGPointMake( 15, 15 ) );
 	
@@ -377,30 +365,7 @@ void RunEventLoop()
 		{
 			const uint32_t offset = desc.height * desc.stride * desc.frame;
 			
-			Ptr frame_addr = (Ptr) addr + offset;
-			
-		#if CONFIG_QUICKDRAW
-			
-			bitmap.baseAddr = frame_addr;
-			
-			CopyBits( &bitmap,
-			          GetPortBitMapForCopyBits( captured_display.port() ),
-			          &bitmap.bounds,
-			          &frame,
-			          srcCopy,
-			          NULL );
-			
-			goto next;
-			
-		#endif
-			
-			if ( CGImageRef image = create_raster_image( frame_addr, desc ) )
-			{
-				CGContextDrawImage( captured_display.context(), bounds, image );
-				
-				CGImageRelease( image );
-			}
-			
+			blitter.blit( (Ptr) addr + offset );
 			goto next;
 		}
 		

apps/mac/Amethyst/blit_CG.cc
@@ -1,0 +1,51 @@
+/*
+	blit_CG.cc
+	----------
+*/
+
+#include "blit_CG.hh"
+
+// Amethyst
+#include "image.hh"
+
+
+static CGRect image_bounds;
+
+static CGContextRef captured_display_context;
+
+static int w, h, rowBytes;
+
+CG_blitter::CG_blitter( CGDirectDisplayID id, CGRect bounds )
+{
+	image_bounds = bounds;
+	
+	captured_display_context = CGDisplayGetDrawingContext( id );
+}
+
+CG_blitter::~CG_blitter()
+{
+	/*
+		We need both of these to clear the screen before releasing the capture.
+		CGContextFlush() doesn't help.
+	*/
+	
+	CGContextFillRect ( captured_display_context, image_bounds );
+	CGContextClearRect( captured_display_context, image_bounds );
+}
+
+void CG_blitter::prep( int stride, int width, int height )
+{
+	w        = width;
+	h        = height;
+	rowBytes = stride;
+}
+
+void CG_blitter::blit( const void* src_addr )
+{
+	if ( CGImageRef image = create_bilevel_image( src_addr, rowBytes, w, h ) )
+	{
+		CGContextDrawImage( captured_display_context, image_bounds, image );
+		
+		CGImageRelease( image );
+	}
+}

apps/mac/Amethyst/blit_CG.hh
@@ -1,0 +1,32 @@
+/*
+	blit_CG.hh
+	----------
+*/
+
+#ifndef BLITCG_HH
+#define BLITCG_HH
+
+// Mac OS X
+#ifdef __APPLE__
+// Including just <CoreGraphics/CoreGraphics.h> fails in Mac OS X 10.4.
+#include <ApplicationServices/ApplicationServices.h>
+#endif
+
+
+class CG_blitter
+{
+	private:
+		// non-copyable
+		CG_blitter           ( const CG_blitter& );
+		CG_blitter& operator=( const CG_blitter& );
+	
+	public:
+		CG_blitter( CGDirectDisplayID id, CGRect bounds );
+		~CG_blitter();
+		
+		void prep( int stride, int width, int height );
+		
+		void blit( const void* src_addr );
+};
+
+#endif

apps/mac/Amethyst/blit_QD.hh
@@ -1,0 +1,32 @@
+/*
+	blit_QD.hh
+	----------
+*/
+
+#ifndef BLITQD_HH
+#define BLITQD_HH
+
+// Mac OS X
+#ifdef __APPLE__
+// Including just <CoreGraphics/CoreGraphics.h> fails in Mac OS X 10.4.
+#include <ApplicationServices/ApplicationServices.h>
+#endif
+
+
+class QD_blitter
+{
+	private:
+		// non-copyable
+		QD_blitter           ( const QD_blitter& );
+		QD_blitter& operator=( const QD_blitter& );
+	
+	public:
+		QD_blitter( CGDirectDisplayID id, CGRect bounds );
+		~QD_blitter();
+		
+		void prep( int stride, int width, int height );
+		
+		void blit( const void* src_addr );
+};
+
+#endif

apps/mac/Amethyst/display.cc
@@ -5,48 +5,10 @@
 
 #include "display.hh"
 
-// Mac OS X
-#ifdef __APPLE__
-#include <ApplicationServices/ApplicationServices.h>
-#endif
-
-// Mac OS
-#ifndef CGDIRECTDISPLAY_H_
-#ifndef __CGDIRECT_DISPLAY_H__
-#ifndef __CGDIRECTDISPLAY__
-#include <CGDirectDisplay.h>
-#endif
-#endif
-#endif
-
-// Amethyst
-#include "quickdraw.hh"
-
 
 display_capture::display_capture( CGDirectDisplayID id ) : its_id( id )
 {
 	if ( CGError err = CGDisplayCapture( its_id ) )
-	{
-		throw CGDisplayCapture_Failed();
-	}
-	
-	void* addr;
-	
-#if CONFIG_QUICKDRAW
-	
-	// UInt32 cast required for 10.4 PPC, at least
-	
-	addr =
-	its_port = CreateNewPortForCGDisplayID( (UInt32) its_id );
-	
-#else
-	
-	addr =
-	its_context = CGDisplayGetDrawingContext( its_id );
-	
-#endif
-	
-	if ( addr == NULL )
 	{
 		throw CGDisplayCapture_Failed();
 	}
@@ -57,27 +19,6 @@ display_capture::display_capture( CGDirectDisplayID id ) : its_id( id )
 display_capture::~display_capture()
 {
 	CGDisplayShowCursor( its_id );
-	
-#if CONFIG_QUICKDRAW
-	
-	Rect bounds;
-	PaintRect( GetPortBounds( its_port, &bounds ) );
-	
-	DisposePort( its_port );
-	
-#else
-	
-	CGRect bounds = CGDisplayBounds( its_id );
-	
-	/*
-		We need both of these to clear the screen before releasing the capture.
-		CGContextFlush() doesn't help.
-	*/
-	
-	CGContextFillRect ( its_context, bounds );
-	CGContextClearRect( its_context, bounds );
-	
-#endif
 	
 	CGDisplayRelease( its_id );
 }

apps/mac/Amethyst/display.hh
@@ -8,6 +8,7 @@
 
 // Mac OS X
 #ifdef __APPLE__
+// Including just <CoreGraphics/CoreGraphics.h> fails in Mac OS X 10.4.
 #include <ApplicationServices/ApplicationServices.h>
 #endif
 
@@ -18,8 +19,6 @@ class display_capture
 {
 	private:
 		CGDirectDisplayID  its_id;
-		CGContextRef       its_context;
-		CGrafPtr           its_port;
 		
 		// non-copyable
 		display_capture           ( const display_capture& );
@@ -30,8 +29,6 @@ class display_capture
 		~display_capture();
 		
 		CGDirectDisplayID id() const  { return its_id; }
-		CGContextRef context() const  { return its_context; }
-		CGrafPtr     port   () const  { return its_port;    }
 };
 
 #endif

commit f2abef0018bcec9d498cbcecbcdff99793f588f8

Amethyst:  Fix for building with CONFIG_QUICKDRAW
This has been broken since de23720b4d95df790f877be321086d07b9a98630.

apps/mac/Amethyst/blit_QD.cc
@@ -11,7 +11,7 @@
 
 static CGrafPtr captured_display_port;
 
-static BitMap dstBits;
+static BitMap srcBits;
 static Rect   dstRect;
 
 #if CONFIG_QUICKDRAW