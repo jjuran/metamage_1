jjuran/metamage_1

commit d50199208ff026f01ad8662074be21898de9708d

vfs, relix, Genie:  Detemplatize dynamic groups

posix/vfs/vfs/node/types/dynamic_group.cc
@@ -20,19 +20,40 @@
 #include "vfs/dir_entry.hh"
 #include "vfs/filehandle.hh"
 #include "vfs/node.hh"
+#include "vfs/filehandle/types/dynamic_group.hh"
+#include "vfs/methods/data_method_set.hh"
 #include "vfs/methods/dir_method_set.hh"
+#include "vfs/methods/node_method_set.hh"
 
 
 namespace vfs
 {
 	
-	filehandle_ptr get_dynamic_element_from_node( const node*             that,
-	                                              dynamic_element_getter  getter )
+	struct dynamic_group_extra
 	{
+		const dynamic_group* group;
+	};
+	
+	static
+	filehandle_ptr dge_open( const node* that, int flags, mode_t mode )
+	{
+		dynamic_group_extra& extra = *(dynamic_group_extra*) that->extra();
+		
 		const unsigned id = gear::parse_unsigned_decimal( that->name().c_str() );
 		
-		return getter( id );
+		return get_dynamic_element_from_group_by_id( *extra.group, id );
 	}
+	
+	static const data_method_set dge_data_methods =
+	{
+		&dge_open,
+	};
+	
+	static const node_method_set dge_methods =
+	{
+		NULL,
+		&dge_data_methods,
+	};
 	
 	
 	static node_ptr dynamic_group_lookup( const node*          that,
@@ -53,7 +74,7 @@ namespace vfs
 		return new node( parent,
 		                 name,
 		                 S_IFCHR | 0600,
-		                 extra.methods );
+		                 &dge_methods );
 	}
 	
 	static void dynamic_group_listdir( const node*    that,
@@ -105,7 +126,7 @@ namespace vfs
 		
 		dynamic_group_extra& extra = *(dynamic_group_extra*) result->extra();
 		
-		extra = *(dynamic_group_extra*) args;
+		extra.group = (dynamic_group*) args;
 		
 		return result;
 	}

lamp/Genie/Genie/FS/FSTree_Dev.cc
@@ -33,8 +33,8 @@
 #include "relix/api/current_process.hh"
 #include "relix/config/mini.hh"
 #include "relix/config/pts.hh"
-#include "relix/fs/con_tag.hh"
-#include "relix/fs/pts_tag.hh"
+#include "relix/fs/con_group.hh"
+#include "relix/fs/pts_group.hh"
 #include "relix/fs/simple_device.hh"
 #include "relix/task/process.hh"
 #include "relix/task/process_group.hh"
@@ -188,7 +188,6 @@ namespace Genie
 	}
 	
 	using vfs::dynamic_group_factory;
-	using vfs::dynamic_group_element;
 	
 	const vfs::fixed_mapping dev_Mappings[] =
 	{
@@ -209,11 +208,11 @@ namespace Genie
 		
 		{ "gestalt", &basicdevice_factory, &gestalt_params },
 		
-		{ "con", &dynamic_group_factory, &dynamic_group_element< relix::con_tag >::extra },
+		{ "con", &dynamic_group_factory, &relix::get_con_group() },
 		
 	#if CONFIG_PTS
 		
-		{ "pts", &dynamic_group_factory, &dynamic_group_element< relix::pts_tag  >::extra },
+		{ "pts", &dynamic_group_factory, &relix::get_pts_group() },
 		
 	#endif
 		

lamp/Genie/Genie/FS/gui/new/console.cc
@@ -65,7 +65,7 @@
 // relix
 #include "relix/api/root.hh"
 #include "relix/api/try_again.hh"
-#include "relix/fs/con_tag.hh"
+#include "relix/fs/con_group.hh"
 #include "relix/signal/signal_process_group.hh"
 
 // Genie
@@ -401,7 +401,7 @@ namespace Genie
 	{
 		console_extra& extra = *(console_extra*) that->extra();
 		
-		vfs::get_dynamic_group< relix::con_tag >().erase( extra.id );
+		relix::get_con_group().erase( extra.id );
 		
 		intrusive_ptr_release( extra.tty_file );
 	}
@@ -829,7 +829,9 @@ namespace Genie
 		
 		vfs::filehandle_ptr result( new_tty_handle( *that, id ) );
 		
-		vfs::set_dynamic_element_by_id< relix::con_tag >( id, result.get() );
+		vfs::dynamic_group& group = relix::get_con_group();
+		
+		vfs::set_dynamic_element_of_group_by_id( group, id, result.get() );
 		
 		plus::var_string path = "/dev/con/";
 		

posix/vfs/vfs/filehandle/types/dynamic_group.hh
@@ -20,26 +20,10 @@ namespace vfs
 	
 	typedef std::map< dynamic_element_id, const void* > dynamic_group;
 	
-	template < class Handle >
-	dynamic_group& get_dynamic_group()
-	{
-		static dynamic_group the_group;
-		
-		return the_group;
-	}
-	
 	filehandle_ptr
 	//
 	get_dynamic_element_from_group_by_id( const dynamic_group&  group,
 	                                      dynamic_element_id    id );
-	
-	template < class Handle >
-	filehandle_ptr get_dynamic_element_by_id( dynamic_element_id id )
-	{
-		dynamic_group& group( get_dynamic_group< Handle >() );
-		
-		return get_dynamic_element_from_group_by_id( group, id );
-	}
 	
 	inline
 	void set_dynamic_element_of_group_by_id( dynamic_group&      group,
@@ -47,14 +31,6 @@ namespace vfs
 	                                         const filehandle*   h )
 	{
 		group[ id ] = h;
-	}
-	
-	template < class Handle >
-	void set_dynamic_element_by_id( dynamic_element_id id, const filehandle* h )
-	{
-		dynamic_group& group( get_dynamic_group< Handle >() );
-		
-		set_dynamic_element_of_group_by_id( group, id, h );
 	}
 	
 }

posix/vfs/vfs/node/types/dynamic_group.hh
@@ -11,64 +11,10 @@
 
 // vfs
 #include "vfs/node_ptr.hh"
-#include "vfs/filehandle/types/dynamic_group.hh"
-#include "vfs/methods/data_method_set.hh"
-#include "vfs/methods/node_method_set.hh"
 
 
 namespace vfs
 {
-	
-	typedef filehandle_ptr (*dynamic_element_getter)( std::size_t );
-	
-	
-	filehandle_ptr get_dynamic_element_from_node( const node*             that,
-	                                              dynamic_element_getter  getter );
-	
-	
-	struct dynamic_group_extra
-	{
-		const dynamic_group*    group;
-		const node_method_set*  methods;
-	};
-	
-	
-	template < class Handle >
-	struct dynamic_group_element
-	{
-		static filehandle_ptr open( const node* that, int flags, mode_t mode );
-		
-		static const data_method_set data_methods;
-		static const node_method_set node_methods;
-		
-		static const dynamic_group_extra extra;
-	};
-	
-	template < class Handle >
-	const dynamic_group_extra dynamic_group_element< Handle >::extra =
-	{
-		&get_dynamic_group< Handle >(),
-		&dynamic_group_element< Handle >::node_methods
-	};
-	
-	template < class Handle >
-	filehandle_ptr dynamic_group_element< Handle >::open( const node* that, int flags, mode_t mode )
-	{
-		return get_dynamic_element_from_node( that, &get_dynamic_element_by_id< Handle > );
-	}
-	
-	template < class Handle >
-	const data_method_set dynamic_group_element< Handle >::data_methods =
-	{
-		&dynamic_group_element< Handle >::open
-	};
-	
-	template < class Handle >
-	const node_method_set dynamic_group_element< Handle >::node_methods =
-	{
-		NULL,
-		&data_methods
-	};
 	
 	node_ptr dynamic_group_factory( const node*          parent,
 	                                const plus::string&  name,

relix/kernel/relix/fs/con_group.cc
@@ -1,0 +1,19 @@
+/*
+	con_group.cc
+	------------
+*/
+
+#include "relix/fs/con_group.hh"
+
+
+namespace relix
+{
+
+static vfs::dynamic_group con_group;
+
+vfs::dynamic_group& get_con_group()
+{
+	return con_group;
+}
+
+}

relix/kernel/relix/fs/con_group.hh
@@ -1,0 +1,20 @@
+/*
+	con_group.hh
+	------------
+*/
+
+#ifndef RELIX_FS_CONGROUP_HH
+#define RELIX_FS_CONGROUP_HH
+
+// vfs
+#include "vfs/filehandle/types/dynamic_group.hh"
+
+
+namespace relix
+{
+	
+	vfs::dynamic_group& get_con_group();
+	
+}
+
+#endif

relix/kernel/relix/fs/con_tag.hh
@@ -1,17 +1,0 @@
-/*
-	con_tag.hh
-	----------
-*/
-
-#ifndef RELIX_FS_CONTAG_HH
-#define RELIX_FS_CONTAG_HH
-
-
-namespace relix
-{
-	
-	struct con_tag {};
-	
-}
-
-#endif

commit 2447d80965ce7dc097f8f0dfbec78b857bdb78b1

vfs:  Fix /dev/fd/N causing MacRelix to crash
Broken by d50199208ff026f01ad8662074be21898de9708d, accessing members of
/dev/fd/ caused memory access faults in MacRelix -- typically yielding a
stack trace in classic Mac OS (except in SheepShaver) handled internally
by MacRelix, and outright crashing MacRelix itself in Carbon.  The cause
is a faulty (no pun intended, really, I swear!), incomplete refactoring.
The problem is that a backlink to the dynamic group was never allocated.

posix/vfs/vfs/node/types/dynamic_group.cc
@@ -34,10 +34,15 @@ namespace vfs
 		const dynamic_group* group;
 	};
 	
+	struct dynamic_element_extra
+	{
+		const dynamic_group* group;
+	};
+	
 	static
 	filehandle_ptr dge_open( const node* that, int flags, mode_t mode )
 	{
-		dynamic_group_extra& extra = *(dynamic_group_extra*) that->extra();
+		dynamic_element_extra& extra = *(dynamic_element_extra*) that->extra();
 		
 		const unsigned id = gear::parse_unsigned_decimal( that->name().c_str() );
 		
@@ -71,10 +76,18 @@ namespace vfs
 			poseven::throw_errno( ENOENT );
 		}
 		
-		return new node( parent,
-		                 name,
-		                 S_IFCHR | 0600,
-		                 &dge_methods );
+		node* result = new node( parent,
+		                         name,
+		                         S_IFCHR | 0600,
+		                         &dge_methods,
+		                         sizeof (dynamic_element_extra) );
+		
+
+		dynamic_element_extra& x2 = *(dynamic_element_extra*) result->extra();
+		
+		x2.group = extra.group;
+		
+		return result;
 	}
 	
 	static void dynamic_group_listdir( const node*    that,