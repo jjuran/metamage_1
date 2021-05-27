919f84b57508c1a941c781ab2427a1555391d6ab
Josh Juran <jjuran@metamage.com>
2021-05-27 03:13:39 -0400

    vxo:  Distinguish UniPtrVec_ template from PtrVec_
    The former is non-copyable and therefore doesn't need to call unshare().
    The latter does call unshare(), so as to implement copy-on-write.  Since
    their shared base class PtrVec is copyable but doesn't copy-on-write, it
    allows shared state and doesn't meet the requirements of a regular type.

--- base/vxo/vxo/ptrvec.hh
+++ base/vxo/vxo/ptrvec.hh
@@ -32,7 +32,7 @@ namespace vxo
 			
 			bool empty() const  { return size() == 0; }
 			
-			Item* begin()  { unshare(); return (Item*) u.str.pointer; }
+			Item* begin()  { return (Item*) u.str.pointer; }
 			Item* end()    { return begin() + size(); }
 			
 			Item const* begin() const  { return (Item const*) u.str.pointer; }
@@ -120,6 +120,85 @@ namespace vxo
 			
 			void insert( Item* loc, const_iterator begin, const_iterator end )
 			{
+				unshare();
+				
+				PtrVec::insert( (anyptr_t*) loc,
+				                (anyptr_t*) begin,
+				                (anyptr_t*) end );
+			}
+			
+			Item* insert( Item* loc )
+			{
+				unshare();
+				
+				return (Item*) PtrVec::insert( (anyptr_t*) loc );
+			}
+			
+			void insert( Item* loc, Item item )
+			{
+				*insert( loc ) = item;
+			}
+			
+			void erase( Item* loc )
+			{
+				unshare();
+				
+				PtrVec::erase_n( (anyptr_t*) loc, 1 );
+			}
+			
+			void push_back( Item item )  { unshare(); *expand_by( 1 ) = item; }
+			
+			void pop_back()              { unshare(); --u.str.length; }
+			void clear()                 { unshare(); u.str.length = 0; }
+	};
+	
+	template < class T >
+	class UniPtrVec_ : public PtrVec
+	{
+		private:
+			// non-copyable
+			UniPtrVec_           ( const UniPtrVec_& );
+			UniPtrVec_& operator=( const UniPtrVec_& );
+		
+		public:
+			typedef T* Item;
+			typedef Item value_type;
+			
+			typedef value_type*       iterator;
+			typedef value_type const* const_iterator;
+			
+			typedef value_type& reference;
+			typedef value_type  const_reference;
+			
+			static bool test( const Box& box );  // unimplemented
+			
+			explicit UniPtrVec_( size_t n = 0 ) : PtrVec( n )
+			{
+			}
+			
+			Item* begin()  { return (Item*) u.str.pointer; }
+			Item* end()    { return begin() + size(); }
+			
+			Item const* begin() const  { return (Item const*) u.str.pointer; }
+			Item const* end()   const  { return begin() + size(); }
+			
+			Item const* cbegin() const  { return begin(); }
+			Item const* cend()   const  { return end();   }
+			
+			Item&       operator[]( size_t i )        { return begin()[ i ]; }
+			Item const& operator[]( size_t i ) const  { return begin()[ i ]; }
+			
+			Item&       operator[]( int i )        { return begin()[ i ]; }
+			Item const& operator[]( int i ) const  { return begin()[ i ]; }
+			
+			Item&       front()        { return begin()[ 0 ]; }
+			Item const& front() const  { return begin()[ 0 ]; }
+			
+			Item&       back()        { return end()[ -1 ]; }
+			Item const& back() const  { return end()[ -1 ]; }
+			
+			void insert( Item* loc, const_iterator begin, const_iterator end )
+			{
 				PtrVec::insert( (anyptr_t*) loc,
 				                (anyptr_t*) begin,
 				                (anyptr_t*) end );
@@ -140,9 +219,6 @@ namespace vxo
 				PtrVec::erase_n( (anyptr_t*) loc, 1 );
 			}
 	};
-	
-	// This is a temporary hack that allows us to switch in advance.
-	#define UniPtrVec_ PtrVec_
 	
 }
 
