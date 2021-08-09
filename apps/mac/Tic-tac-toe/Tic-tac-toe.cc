jjuran/metamage_1

commit aee32691295ac56ffa7ec1c4091bb2391898a335

Tic-tac-toe:  Don't destroy regions with each move

apps/mac/Tic-tac-toe/Tic-tac-toe.cc
@@ -113,6 +113,7 @@ static const Rect grow_size =
 	32767, 32767,
 };
 
+static RgnHandle allocRgns[ 10 ];
 static RgnHandle mouseRgns[ 10 ];
 static RgnHandle otherRgn;
 static RgnHandle gMouseRgn;
@@ -122,6 +123,7 @@ void alloc_mouseRgns()
 {
 	for ( short i = 0;  i < 10;  ++i )
 	{
+		allocRgns[ i ] =
 		mouseRgns[ i ] = NewRgn();
 	}
 	
@@ -419,8 +421,6 @@ void click( Point where )
 	
 	XorRgn( otherRgn, rgn, otherRgn );
 	
-	DisposeRgn( rgn );
-	
 	draw_token( current_player, i );
 	
 	if ( sound_enabled )
@@ -552,13 +552,8 @@ void reset()
 	
 	for ( short i = 1;  i < 10;  ++i )
 	{
-		if ( mouseRgns[ i ] == otherRgn )
-		{
-			mouseRgns[ i ] = NewRgn();
-		}
+		mouseRgns[ i ] = allocRgns[ i ];
 	}
-	
-	calibrate_mouseRgns( unitLength );
 	
 	Point mouse;
 	GetMouse( &mouse );

commit 53635f00f8800ab8c58ef0829038161f5a6db9d5

Tic-tac-toe:  Fix mouse region maintenance
This fixes the regression from aee32691295ac56ffa7ec1c4091bb2391898a335.

apps/mac/Tic-tac-toe/Tic-tac-toe.cc
@@ -546,12 +546,12 @@ void calibrate_mouseRgns( short unitLength )
 			rect.left  = globalOffset.h + unitLength * (3 + 9 * j);
 			rect.right = globalOffset.h + unitLength * (3 + 9 * j + 8);
 			
+			RectRgn( allocRgns[ 1 + i3 + j ], &rect );
+			
 			RgnHandle rgn = mouseRgns[ 1 + i3 + j ];
 			
 			if ( rgn != otherRgn )
 			{
-				RectRgn( rgn, &rect );
-				
 				XorRgn( otherRgn, rgn, otherRgn );
 			}
 		}
@@ -632,6 +632,8 @@ void reset()
 	{
 		mouseRgns[ i ] = allocRgns[ i ];
 	}
+	
+	calibrate_mouseRgns( unitLength );
 	
 	/*
 		Elicit a mouse-moved event to change the cursor if it's in a live