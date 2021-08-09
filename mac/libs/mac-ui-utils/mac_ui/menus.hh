45d1e8a007bc257da504ca0ebdc1810c3f2dbe8c
Josh Juran <jjuran@metamage.com>
2021-08-09 08:41:47 -0400

    mac-ui-utils:  Add {enable,disable}_menu_item()

--- mac/libs/mac-ui-utils/mac_ui/menus.hh
+++ mac/libs/mac-ui-utils/mac_ui/menus.hh
@@ -22,6 +22,32 @@ namespace ui  {
 	
 	void delete_all_menu_items( MenuRef menu );
 	
+	inline
+	void enable_menu_item( MenuRef menu, MenuItemIndex item )
+	{
+	#if CALL_NOT_IN_CARBON
+		
+		EnableItem( menu, item );
+		return;
+		
+	#endif
+		
+		EnableMenuItem( menu, item );
+	}
+	
+	inline
+	void disable_menu_item( MenuRef menu, MenuItemIndex item )
+	{
+	#if CALL_NOT_IN_CARBON
+		
+		DisableItem( menu, item );
+		return;
+		
+	#endif
+		
+		DisableMenuItem( menu, item );
+	}
+	
 }
 }
 
