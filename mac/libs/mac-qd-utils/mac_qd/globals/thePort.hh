b5bf5e3fea358ed5c5c5541338f8637274ee4194
Josh Juran <jjuran@metamage.com>
2021-08-23 15:03:32 -0400

    mac-qd-utils:  Add mac::qd::thePort()

---
+++ mac/libs/mac-qd-utils/mac_qd/globals/thePort.hh
@@ -0,0 +1,45 @@
+/*
+	thePort.hh
+	----------
+*/
+
+#ifndef MACQD_GLOBALS_THEPORT_HH
+#define MACQD_GLOBALS_THEPORT_HH
+
+// Mac OS X
+#ifdef __APPLE__
+#include <ApplicationServices/ApplicationServices.h>
+#endif
+
+// Mac OS
+#ifndef __QUICKDRAW__
+#include <Quickdraw.h>
+#endif
+
+// missing-macos
+#ifdef MAC_OS_X_VERSION_10_7
+#ifndef MISSING_QUICKDRAW_H
+#include "missing/Quickdraw.h"
+#endif
+#endif
+
+
+namespace mac {
+namespace qd  {
+
+inline
+GrafPtr thePort()
+{
+#if ! (OPAQUE_TOOLBOX_STRUCTS  ||  TARGET_API_MAC_CARBON)
+	
+	return ::qd.thePort;
+	
+#endif
+	
+	return (GrafPtr) GetQDGlobalsThePort();
+}
+
+}
+}
+
+#endif
