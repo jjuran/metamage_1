75724c9c5577990bf6a4c388d0a869dd31c61934
Josh Juran <jjuran@metamage.com>
2021-08-27 04:35:49 -0400

    Tic-tac-toe:  Axe 2-arg propagate_to_dock_tile()

--- apps/mac/Tic-tac-toe/dock_tile.cc
+++ apps/mac/Tic-tac-toe/dock_tile.cc
@@ -10,13 +10,6 @@
 #include <Carbon/Carbon.h>
 #endif
 
-// missing-macos
-#ifdef MAC_OS_X_VERSION_10_7
-#ifndef MISSING_QUICKDRAW_H
-#include "missing/Quickdraw.h"
-#endif
-#endif
-
 // CGTictactoe
 #include "CGTictactoe.hh"
 
@@ -86,39 +79,6 @@ void draw_dock_tile( signed char board[ 9 ] )
 	CGContextFlush( context );
 	
 	EndCGContextForApplicationDockTile( context );
-	
-#endif
-}
-
-void propagate_to_dock_tile( short unitLength, Point margin )
-{
-#ifdef __APPLE__
-	
-	CGrafPtr gameboard_port = GetQDGlobalsThePort();
-	CGrafPtr dock_tile_port = BeginQDContextForApplicationDockTile();
-	
-	Rect src_rect, dst_rect;
-	
-	src_rect.top  = margin.v;
-	src_rect.left = margin.h;
-	src_rect.bottom = margin.v + unitLength * 32;
-	src_rect.right  = margin.h + unitLength * 32;
-	
-	GetPortBounds( dock_tile_port, &dst_rect );
-	
-	CopyBits( GetPortBitMapForCopyBits( gameboard_port ),
-	          GetPortBitMapForCopyBits( dock_tile_port ),
-	          &src_rect,
-	          &dst_rect,
-	          srcCopy,
-	          NULL );
-	
-	// This is needed in 10.2 and 10.4 PPC, not in 10.5 x86
-	QDFlushPortBuffer( dock_tile_port, NULL );
-	
-	EndQDContextForApplicationDockTile( dock_tile_port );
-	
-	SetPort( gameboard_port );
 	
 #endif
 }
--- apps/mac/Tic-tac-toe/dock_tile.hh
+++ apps/mac/Tic-tac-toe/dock_tile.hh
@@ -6,19 +6,6 @@
 #ifndef DOCKTILE_HH
 #define DOCKTILE_HH
 
-// Mac OS X
-#ifdef __APPLE__
-#include <ApplicationServices/ApplicationServices.h>
-#endif
-
-// Mac OS
-#ifndef __QUICKDRAW__
-#include <Quickdraw.h>
-#endif
-
-
 void draw_dock_tile( signed char board[ 9 ] );
-
-void propagate_to_dock_tile( short unitLength, Point margin );
 
 #endif
