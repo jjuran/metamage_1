a9ba3f11463ac310777751526ecd4051b104f4d9
Josh Juran <jjuran@metamage.com>
2021-09-26 01:25:03 -0400

    damogran:  Don't assume packed input is 4x-padded
    The only client code to use damogran so far did, in fact, pad the packed
    data to a multiple of four bytes, but programs aren't required to do so.

--- graphics/damogran/damogran/unpack.cc
+++ graphics/damogran/damogran/unpack.cc
@@ -16,16 +16,12 @@ typedef signed char int8_t;
 
 const uint8_t* validate( const uint8_t* src, const uint8_t* end, long size )
 {
-	bool unaligned = false;
-	
 	do
 	{
 		if ( src + 2 > end )  return NULL;
 		
 		const int8_t c0 = *src++;
 		const int8_t c1 = *src++;
-		
-		unaligned = ! unaligned;
 		
 		if ( c0 == 0 )
 		{
@@ -38,11 +34,6 @@ const uint8_t* validate( const uint8_t* src, const uint8_t* end, long size )
 			{
 				const size_t n = 2 * (uint8_t) -c1;
 				
-				if ( n & 0x2 )
-				{
-					unaligned = ! unaligned;
-				}
-				
 				if ( (size -= n) < 0 )  return NULL;
 				
 				src += n;
@@ -51,8 +42,6 @@ const uint8_t* validate( const uint8_t* src, const uint8_t* end, long size )
 			}
 			
 			// c1 > 0
-			
-			unaligned = ! unaligned;
 			
 			if ( src + 2 > end )  return NULL;
 			
@@ -74,21 +63,17 @@ const uint8_t* validate( const uint8_t* src, const uint8_t* end, long size )
 			return NULL;  // (c0 < 0) is undefined
 		}
 	}
-	while ( unaligned  ||  size > 0 );
+	while ( size > 0 );
 	
 	return src;
 }
 
 const uint8_t* unpack( const uint8_t* src, uint8_t* dst, uint8_t* end )
 {
-	bool unaligned = false;
-	
 	do
 	{
 		const int8_t c0 = *src++;
 		const int8_t c1 = *src++;
-		
-		unaligned = ! unaligned;
 		
 		if ( c0 == 0 )
 		{
@@ -101,11 +86,6 @@ const uint8_t* unpack( const uint8_t* src, uint8_t* dst, uint8_t* end )
 			{
 				const size_t n = 2 * (uint8_t) -c1;
 				
-				if ( n & 0x2 )
-				{
-					unaligned = ! unaligned;
-				}
-				
 				memcpy( dst, src, n );
 				
 				src += n;
@@ -115,8 +95,6 @@ const uint8_t* unpack( const uint8_t* src, uint8_t* dst, uint8_t* end )
 			}
 			
 			// c1 > 0
-			
-			unaligned = ! unaligned;
 			
 			const uint8_t c2 = *src++;
 			const uint8_t c3 = *src++;
@@ -136,7 +114,7 @@ const uint8_t* unpack( const uint8_t* src, uint8_t* dst, uint8_t* end )
 			dst += n;
 		}
 	}
-	while ( unaligned  ||  dst < end );
+	while ( dst < end );
 	
 	return src;
 }
