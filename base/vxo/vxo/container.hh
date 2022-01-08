e486430fc11f3fa21737a6f7eb1961cfdb8ef097
Josh Juran <jjuran@metamage.com>
2022-01-08 08:52:54 -0500

    vxo:  Don't unshare() in Container::begin()
    We need to rethink how this is going to work, because nothrow semantics,
    copy-on-write, and regular types don't mix.  In many cases, what we have
    is the sole reference, so copy-on-write isn't needed.  In the future, we
    may need to preemptively call (a yet-to-be-written) unshare_nothrow() to
    ensure we don't violate constraints, and violations could panic (like an
    assertion) instead of throwing, surfacing errors without major overhead.

--- base/vxo/vxo/container.hh
+++ base/vxo/vxo/container.hh
@@ -38,7 +38,7 @@ namespace vxo
 			
 			bool empty() const  { return size() == 0; }
 			
-			Item* begin()  { unshare(); return (Item*) u.str.pointer; }
+			Item* begin()  { return (Item*) u.str.pointer; }
 			Item* end()    { return begin() + size(); }
 			
 			Item const* begin() const  { return (Item const*) u.str.pointer; }
