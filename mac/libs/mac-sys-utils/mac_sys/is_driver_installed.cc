f1c3aa7a19124183327517968d1fff1939d1c80a
Josh Juran <jjuran@metamage.com>
2022-04-16 23:29:57 -0400

    mac-sys-utils:  Add is_driver_installed()
    It's almost identical to is_driver_open(), except it doesn't test flags.
    We can revisit the issue of code duplication once we have a program that
    uses both functions.

---
+++ mac/libs/mac-sys-utils/mac_sys/is_driver_installed.cc
@@ -0,0 +1,63 @@
+/*
+	is_driver_installed.cc
+	----------------------
+*/
+
+#include "mac_sys/is_driver_installed.hh"
+
+// Mac OS X
+#ifdef __APPLE__
+#include <CoreServices/CoreServices.h>
+#endif
+
+// Mac OS
+#ifdef __MACOS__
+#ifndef __DEVICES__
+#include <Devices.h>
+#endif
+#endif
+#ifndef __STRINGCOMPARE__
+#include <StringCompare.h>
+#endif
+
+// mac-sys-utils
+#include "mac_sys/unit_table.hh"
+
+
+namespace mac {
+namespace sys {
+
+bool is_driver_installed( const unsigned char* driver_name )
+{
+	const short count = get_unit_table_entry_count();
+	
+#if ! TARGET_API_MAC_CARBON
+	
+	for ( short unit = 0;  unit < count;  ++unit )
+	{
+		DCtlHandle dceHandle = GetDCtlEntry( -unit );
+		
+		if ( dceHandle != NULL  &&  dceHandle[0]->dCtlDriver != NULL )
+		{
+			const int flags = dceHandle[0]->dCtlFlags;
+			const int inRAM = flags & dRAMBasedMask;
+			
+			DRVRHeader* drvr = inRAM ? *(DRVRHeader**) dceHandle[0]->dCtlDriver
+			                         :  (DRVRHeader* ) dceHandle[0]->dCtlDriver;
+			
+			ConstStr255Param name = drvr->drvrName;
+			
+			if ( ::EqualString( name, driver_name, false, true ) )
+			{
+				return true;
+			}
+		}
+	}
+	
+#endif
+	
+	return false;
+}
+
+}
+}
---
+++ mac/libs/mac-sys-utils/mac_sys/is_driver_installed.hh
@@ -0,0 +1,18 @@
+/*
+	is_driver_installed.hh
+	----------------------
+*/
+
+#ifndef MACSYS_ISDRIVERINSTALLED_HH
+#define MACSYS_ISDRIVERINSTALLED_HH
+
+
+namespace mac {
+namespace sys {
+	
+	bool is_driver_installed( const unsigned char* driver_name );
+	
+}
+}
+
+#endif
