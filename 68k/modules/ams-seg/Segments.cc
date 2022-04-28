772f294b6bae2bfaa3994f718b58c6c8e0abd928
Josh Juran <jjuran@metamage.com>
2022-04-28 03:08:04 -0400

    ams-seg:  Axe now-redundant hot patches

--- 68k/modules/ams-seg/Segments.cc
+++ 68k/modules/ams-seg/Segments.cc
@@ -13,9 +13,6 @@
 // POSIX
 #include <unistd.h>
 
-// Standard C
-#include <string.h>
-
 // log-of-war
 #include "logofwar/report.hh"
 
@@ -130,171 +127,6 @@ jump_table_entry* get_jump_table_offset( uint16_t offset : __D0 )
 const uint16_t push_opcode = 0x3F3C;  // MOVE.W   #n,-(SP)
 const uint16_t jump_opcode = 0x4EF9;  // JMP      0xABCD1234
 
-static const uint16_t Lemmings_wait_loop[] =
-{
-	0x42A7,  // CLR.L    -(A7)
-	0xA975,  // _TickCount
-	0xBE9F,  // CMP.L    (A7)+,D7
-	0x64F8,  // BCC.S    *-6
-};
-
-static const uint16_t Lemmings_wait_trap[] =
-{
-	0x5288,  // ADDQ.L   #1,A0
-	0x91F8,  // SUBA.L   0x016A,A0
-	0x016A,  //          ^^^^^^
-	0xA23B,  // _DelayEdge (_Delay | kDelayTickEdgeMask)
-};
-
-static
-void apply_hotpatches( Handle code, short segnum )
-{
-	enum
-	{
-		long_pop_to_absolute_short = 0x31DF,  // dst: mode 7, reg 0
-		long_pop_to_displaced_A5   = 0x3B5F,  // dst: mode 5, reg 5
-		byte_clear_to_displaced_A5 = 0x422D,  // dst: mode 5, reg 5
-		byte_set_T_to_displaced_A5 = 0x50ED,  // dst: mode 5, reg 5
-		
-		_FindControl = 0xA96C,
-	};
-	
-	Size size = GetHandleSize( code );
-	
-	if ( segnum == 1 )
-	{
-		if ( size < 0x3b02 )
-		{
-			return;
-		}
-		
-		uint16_t* p = (uint16_t*) &code[0][ 0x3afe ];
-		
-		if ( *p++ == _FindControl  &&  *p == long_pop_to_absolute_short )
-		{
-			/*
-				Hot-patch Lode Runner to fix a crash.
-				
-				I'm not sure how this happened; it's almost certainly not a
-				compiler bug, seeing how the correct instruction also occurs
-				later in the same function, but it seems an odd error to make
-				when writing assembly language by hand.
-				
-				The word following the opcode is the same, though in d68k it
-				disassembles differently: (int16_t) 0xae28 == -20952
-				The error is that the opcode's destination effective address
-				is wrong in the shipped code.  The reason it "works" at all is
-				because 68000-based Macs lack an MMU and writes never fail,
-				so $ae28 (which is actually $ffffae28) maps to a nonexistent
-				location in I/O space, and since no peripheral is listening
-				for accesses there, nothing at all happens as a result.
-				
-				But xv68k doesn't blithely ignore wild writes to the entire
-				upper half of memory, because its I/O space is limited to only
-				those bytes that actually have a purpose.  So clicking in Lode
-				Runner's game window instead writes to unmapped memory and
-				crashes.  To avoid that, hot-patch the broken instruction
-				to what it should have been.
-				
-				Old:  MOVE.W   (A7)+,0xae28
-				New:  MOVE.W   (A7)+,(-20952,A5)
-			*/
-			
-			*p = long_pop_to_displaced_A5;
-		}
-	}
-	
-	if ( segnum == 2 )
-	{
-		if ( size < 0x537a )
-		{
-			return;
-		}
-		
-		uint16_t* p = (uint16_t*) &code[0][ 0x537a ];
-		
-		if ( *--p == (uint16_t) -342  &&  *--p == byte_clear_to_displaced_A5 )
-		{
-			/*
-				Hot-patch Lemmings to fix a crash that occurs right at the end
-				of a level played with sound -- /before/ you're given the code
-				to the next level!
-				
-				In CODE segment 2, at offset $00536c, there's a function that
-				stops the sound engine and frees various resources involved,
-				one of which is an array of 104-byte elements.  Another array
-				has 38-byte elements.  There's a VBL task struct from (-310,A5)
-				to (-296,A5); anything at an address just higher than that can
-				be considered VBL payload data, including a flag, the element
-				count, and the first array's base address.
-				
-				Here's what the function does:
-				  * sets a flag at (-296,A5)
-				  * clears a flag at (-342,A5)
-				  * tests an element count at (-294,A5), bailing out if zero
-				  * calls a function that marks elements invalid
-				  * removes a VBL task via _VRemove
-				  * loops over the array, calling a no-op on each element
-				  * tests the base address of the array it already looped over
-				  * frees the array via _DisposePtr
-				  * clears the array pointer variable at (-292,A5)
-				  * calls the function at $0051d8
-				
-				Here's what that last function does:
-				  * tests another array base address at (-350,A5)
-				  * loops over that array, calling $00510c on each element
-				  * frees the array via _DisposePtr
-				
-				Here's what /that/ function does:
-				  * calls $005960
-				  * (other stuff that's not relevant)
-				
-				And finally, here's what **that** function does:
-				  * tests the flag at (-342,A5), bailing out if set
-				  * loops over the array at (-292,A5), testing each first byte
-				
-				The VBL task is removed, the first array is destroyed, and the
-				application crashes processing the first element of the second
-				array, because it dereferences the first byte of the first slot
-				of the first array, whose pointer location was already cleared,
-				resulting in a NULL dereference.
-				
-				It's not entirely clear what the exact semantics of the flags
-				are, but they appear to be similar.  The VBL payload flag at
-				(-296,A5) is invariably cleared on VBL install and set on VBL
-				removal -- so it has negative polarity.  It seems to mean 'VBL
-				not installed'.  The other flag, at (-342,A5), is set early in
-				an initialization routine and reset in the same function after
-				VInstall() succeeds.  Most of the subroutines that test it bail
-				out if it's set, including one that then sets it and removes
-				the VBL.  The one exception bails out if it's clear and then
-				immediately clears it, subsequently initializing the first
-				array above and installing the VBL.
-				
-				It's another reversed polarity flag, by all indications except
-				one:  The function at $00536c, which calls VRemove(), /clears/
-				rather than sets the flag.  If we patch that function to set
-				the flag instead, the function at $005960 -- that crashes when
-				it accesses NULL -- will bail out instead.  We can't set it to
-				$01 in just the two words we have to play with, but we can set
-				it to $FF, which is just as good as far as TST is concerned.
-				
-				Old:  CLR.B    (-342,A5)
-				New:  ST.B     (-342,A5)
-			*/
-			
-			*p = byte_set_T_to_displaced_A5;
-		}
-		
-		p = (uint16_t*) &code[0][ 0x2aaa ];
-		
-		if ( fast_memequ( p, Lemmings_wait_loop, sizeof Lemmings_wait_loop ) )
-		{
-			fast_memcpy( p, Lemmings_wait_trap, sizeof Lemmings_wait_trap );
-		}
-	}
-}
-
 static
 void LoadSegment( short segnum : __D0 )
 {
@@ -310,8 +142,6 @@ void LoadSegment( short segnum : __D0 )
 	long addr = (long) *code;
 	
 	NOTICE = hex32( addr ), " -> ", hex32( addr + size ), ": 'CODE' ", segnum;
-	
-	apply_hotpatches( code, segnum );
 	
 	const segment_header* segment = (segment_header*) *code;
 	
