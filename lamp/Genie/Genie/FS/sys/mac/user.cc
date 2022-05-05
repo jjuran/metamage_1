0e68e4080566549b5f5ec6845b1e95142e7c3543
Josh Juran <jjuran@metamage.com>
2022-05-05 04:44:52 -0400

    Genie:  Axe /sys/mac/user/name (and .mac-name)

--- lamp/Genie/Genie/FS/sys/mac/user.cc
+++ lamp/Genie/Genie/FS/sys/mac/user.cc
@@ -5,28 +5,18 @@
 
 #include "Genie/FS/sys/mac/user.hh"
 
-// vfs
-#include "vfs/property.hh"
-#include "vfs/node/types/property_file.hh"
-
 // Genie
 #include "Genie/FS/sys/mac/user/home.hh"
-#include "Genie/FS/sys/mac/user/name.hh"
 #include "Genie/FS/sys/mac/user/prefs.hh"
 
 
 namespace Genie
 {
 	
-	#define PROPERTY( prop )  &vfs::new_property, &prop##_params
-	
 	const vfs::fixed_mapping sys_mac_user_Mappings[] =
 	{
 		{ "home",  &New_FSTree_sys_mac_user_home  },
 		{ "prefs", &New_FSTree_sys_mac_user_prefs },
-		
-		{ ".mac-name", PROPERTY( sys_mac_user_macname ) },
-		{      "name", PROPERTY( sys_mac_user_name    ) },
 		
 		{ NULL, NULL }
 	};
