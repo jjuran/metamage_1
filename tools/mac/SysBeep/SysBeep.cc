d4bf1d6284728bb89fa64a238d27528ba0870d5c
Josh Juran <jjuran@metamage.com>
2022-05-15 17:26:50 -0400

    SysBeep:  Use parse_unsigned_decimal() over atoi()
    Switch to C++ and use gear instead of libc, so we avoid MSL's broken use
    of 32-bit references in a purportedly non-far library in near-code mode.
    Also, it's smaller.

--- tools/mac/SysBeep/SysBeep.cc
+++ tools/mac/SysBeep/SysBeep.cc
@@ -1,18 +1,18 @@
 /*
-	SysBeep.c
-	---------
+	SysBeep.cc
+	----------
 */
 
 // Mac OS
 #include <Sound.h>
 
-// Standard C
-#include <stdlib.h>
+// gear
+#include "gear/parse_decimal.hh"
 
 
 int main( int argc, char** argv )
 {
-	int ticks = argv[1] ? atoi( argv[1] )
+	int ticks = argv[1] ? gear::parse_unsigned_decimal( argv[1] )
 	                    : 30;
 	
 	SysBeep( ticks );
