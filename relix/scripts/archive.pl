9b3c4d8572ab154487973a7b20a248d2ab2111bd
Josh Juran <jjuran@metamage.com>
2022-05-20 16:35:55 -0400

    archive.pl:  Ship postlink-68k-tool with MacRelix

--- relix/scripts/archive.pl
+++ relix/scripts/archive.pl
@@ -126,7 +126,8 @@ my %fsmap =
 		Tools =>
 		[
 			# Build tools
-			\ qw( A-line ar cpres ld mpwrez mwcc mxcpp postlink-68k-tool strip ),
+			\ qw( A-line ar cpres ld mpwrez mwcc mxcpp
+				postlink-68k-drvr postlink-68k-tool strip ),
 			# Debugging
 			\ qw( d68k demangle ),
 			# Functionality testing
