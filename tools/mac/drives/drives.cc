c4f2e3f5f7c7a0f2147daa3cf1dc21c37763e03b
Josh Juran <jjuran@metamage.com>
2022-07-07 01:05:55 -0400

    [NEW] drives:  Report contents of the drive queue

---
+++ tools/mac/drives/drives.cc
@@ -0,0 +1,55 @@
+/*
+	drives.cc
+	---------
+*/
+
+// Mac OS
+#ifndef __APPLE__
+#ifndef __FILES__
+#include <Files.h>
+#endif
+#endif
+
+// Standard C
+#include <stdio.h>
+
+// iota
+#include "iota/char_types.hh"
+
+
+int main( int argc, char** argv )
+{
+#if ! TARGET_API_MAC_CARBON
+	
+	const QHdr& DrvQHdr = *(QHdr*) 0x0308;
+	
+	DrvQEl* drv = (DrvQEl*) DrvQHdr.qHead;
+	
+	while ( drv != NULL )
+	{
+		const int qType = drv->qType;
+		
+		const uint32_t flags = ((uint32_t*) drv)[ -1 ];
+		
+		const uint16_t fsid = drv->dQFSID;
+		const uint32_t size = drv->dQDrvSz + (qType ? drv->dQDrvSz2 << 16 : 0);
+		
+		printf( "Drive:   %d\n",   drv->dQDrive  );
+		printf( "Flags:   %.8x\n", flags );
+		printf( "RefNum:  %d\n",   drv->dQRefNum );
+		printf( "FSID:    %x ('%c%c')\n",
+		        fsid,
+		        iota::is_print( fsid >> 8 ) ? fsid >> 8 : '.',
+		        iota::is_print( fsid      ) ? fsid      : '.' );
+		printf( "qType:   %d\n",   qType );
+		printf( "Size:    %lu\n",  size  );
+		
+		printf( "\n" );
+		
+		drv = (DrvQEl*) drv->qLink;
+	}
+	
+#endif
+	
+	return 0;
+}
