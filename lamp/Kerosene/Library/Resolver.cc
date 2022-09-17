7951bfe667a8fab749cb697a7f88eb3a92627681
Josh Juran <jjuran@metamage.com>
2022-09-17 03:03:42 -0400

    Kerosene:  Unindent Resolver.cc

--- lamp/Kerosene/Library/Resolver.cc
+++ lamp/Kerosene/Library/Resolver.cc
@@ -12,29 +12,26 @@
 #include <netdb.h>
 
 
-	int inet_aton( const char* name, struct in_addr* out_addr )
-	{
-		return -1;
-	}
+int inet_aton( const char* name, struct in_addr* out_addr )
+{
+	return -1;
+}
+
+char* inet_ntoa( struct in_addr addr )
+{
+	static char result[ 16 ] = { 0 };
 	
-	char* inet_ntoa( struct in_addr addr )
-	{
-		static char result[ 16 ] = { 0 };
-		
-		std::sprintf( result, "%u.%u.%u.%u", (addr.s_addr >> 24) & 0xFF,
-		                                     (addr.s_addr >> 16) & 0xFF,
-		                                     (addr.s_addr >>  8) & 0xFF,
-		                                     (addr.s_addr      ) & 0xFF );
-		
-		//::OTInetHostToString( addr.s_addr, result );
-		
-		return result;
-	}
+	std::sprintf( result, "%u.%u.%u.%u", (addr.s_addr >> 24) & 0xFF,
+	                                     (addr.s_addr >> 16) & 0xFF,
+	                                     (addr.s_addr >>  8) & 0xFF,
+	                                     (addr.s_addr      ) & 0xFF );
 	
-	const char* hstrerror( int errnum )
-	{
-		return "hstrerror() is unimplemented";
-	}
-	
-	extern "C" { int h_errno; }
-	
+	return result;
+}
+
+const char* hstrerror( int errnum )
+{
+	return "hstrerror() is unimplemented";
+}
+
+extern "C" { int h_errno; }
