f943c34b561377cd229619579a030035b1708c07
Josh Juran <jjuran@metamage.com>
2022-09-17 03:11:20 -0400

    Kerosene:  Rewrite inet_ntoa() via inet_ntop()

--- lamp/Kerosene/Library/Resolver.cc
+++ lamp/Kerosene/Library/Resolver.cc
@@ -3,25 +3,18 @@
  *	===========
  */
 
-// Standard C/C++
-#include <cstdio>
-
 // POSIX
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <sys/socket.h>
 
 
 char* inet_ntoa( struct in_addr addr )
 {
 	static char result[ INET_ADDRSTRLEN ] = { 0 };
 	
-	std::sprintf( result, "%u.%u.%u.%u", (addr.s_addr >> 24) & 0xFF,
-	                                     (addr.s_addr >> 16) & 0xFF,
-	                                     (addr.s_addr >>  8) & 0xFF,
-	                                     (addr.s_addr      ) & 0xFF );
-	
-	return result;
+	return (char*) inet_ntop( AF_INET, &addr, result, sizeof result );
 }
 
 const char* hstrerror( int errnum )
