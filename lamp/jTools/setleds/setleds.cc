e4073a139b5eb4871f633bffcb178863cc2ec5e1
Josh Juran <jjuran@metamage.com>
2022-11-16 00:13:51 -0500

    setleds:  Unindent namespace

--- lamp/jTools/setleds/setleds.cc
+++ lamp/jTools/setleds/setleds.cc
@@ -45,154 +45,154 @@ namespace Nitrogen
 
 namespace tool
 {
+
+namespace N = Nitrogen;
+namespace p7 = poseven;
+
+
+#if !TARGET_API_MAC_CARBON
+
+
+const UInt8 kLEDValueMask = 7;
+
+static int gChangedBits     = 0;
+static int gChangedBitsMask = 0;
+
+
+static inline bool IsKeyboard( const ADBDataBlock& adbData )
+{
+	const SInt8 kADBKeyboardDevice = 2;
 	
-	namespace N = Nitrogen;
-	namespace p7 = poseven;
+	return adbData.devType == kADBKeyboardDevice;
+}
+
+
+static void PrintLEDs( UInt8 leds )
+{
+	const char* status[] = { "off", "on " };
 	
+	bool num    = leds & 1;
+	bool caps   = leds & 2;
+	bool scroll = leds & 4;
 	
-	#if !TARGET_API_MAC_CARBON
+	plus::var_string message;
 	
+	// Reserve enough memory to hold everything, avoiding reallocation
+	message.reserve( 255 );
 	
-	const UInt8 kLEDValueMask = 7;
+	/*
+	message += "# Device at ADB address ";
 	
-	static int gChangedBits     = 0;
-	static int gChangedBitsMask = 0;
+	message += address + '0';
+	message += "\n";
+	*/
 	
+	message += "Current default flags:  NumLock on    CapsLock off   ScrollLock off\n";
+	message += "Current flags:          NumLock on    CapsLock off   ScrollLock off\n";
+	message += "Current leds:           NumLock ";
 	
-	static inline bool IsKeyboard( const ADBDataBlock& adbData )
+	message += status[ num    ];
+	message += "   " "CapsLock ";
+	message += status[ caps   ];
+	message += "   " "ScrollLock ";
+	message += status[ scroll ];
+	message += "\n";
+	
+	p7::write( p7::stdout_fileno, message );
+}
+
+static void DoLEDs( N::GetIndADB_Result adbDevice, int leds )
+{
+	if ( IsKeyboard( adbDevice ) )
 	{
-		const SInt8 kADBKeyboardDevice = 2;
+		UInt8 leds = GetLEDs( adbDevice );
 		
-		return adbData.devType == kADBKeyboardDevice;
-	}
-	
-	
-	static void PrintLEDs( UInt8 leds )
-	{
-		const char* status[] = { "off", "on " };
-		
-		bool num    = leds & 1;
-		bool caps   = leds & 2;
-		bool scroll = leds & 4;
-		
-		plus::var_string message;
-		
-		// Reserve enough memory to hold everything, avoiding reallocation
-		message.reserve( 255 );
-		
-		/*
-		message += "# Device at ADB address ";
-		
-		message += address + '0';
-		message += "\n";
-		*/
-		
-		message += "Current default flags:  NumLock on    CapsLock off   ScrollLock off\n";
-		message += "Current flags:          NumLock on    CapsLock off   ScrollLock off\n";
-		message += "Current leds:           NumLock ";
-		
-		message += status[ num    ];
-		message += "   " "CapsLock ";
-		message += status[ caps   ];
-		message += "   " "ScrollLock ";
-		message += status[ scroll ];
-		message += "\n";
-		
-		p7::write( p7::stdout_fileno, message );
-	}
-	
-	static void DoLEDs( N::GetIndADB_Result adbDevice, int leds )
-	{
-		if ( IsKeyboard( adbDevice ) )
+		if ( gChangedBitsMask == 0 )
 		{
-			UInt8 leds = GetLEDs( adbDevice );
+			PrintLEDs( leds );
+		}
+		else
+		{
+			leds &= ~gChangedBitsMask;
+			leds |= gChangedBits;
 			
-			if ( gChangedBitsMask == 0 )
-			{
-				PrintLEDs( leds );
-			}
-			else
-			{
-				leds &= ~gChangedBitsMask;
-				leds |= gChangedBits;
-				
-				SetLEDs( adbDevice, leds );
-			}
+			SetLEDs( adbDevice, leds );
 		}
 	}
+}
+
+static const char* gOptionNames[] = { "num", "caps", "scroll", NULL };
+
+#endif
+
+
+int Main( int argc, char** argv )
+{
+#if TARGET_API_MAC_CARBON
 	
-	static const char* gOptionNames[] = { "num", "caps", "scroll", NULL };
+	p7::write( p7::stderr_fileno, STR_LEN( "setleds: ADB unsupported in Carbon\n" ) );
 	
-	#endif
+	return 1;
 	
+#else
 	
-	int Main( int argc, char** argv )
+	int count = N::CountADBs();
+	
+	for ( char** argp = argv + 1;  *argp != NULL;  ++argp )
 	{
-	#if TARGET_API_MAC_CARBON
+		const char* arg = *argp;
 		
-		p7::write( p7::stderr_fileno, STR_LEN( "setleds: ADB unsupported in Carbon\n" ) );
+		bool on = false;
 		
-		return 1;
-		
-	#else
-		
-		int count = N::CountADBs();
-		
-		for ( char** argp = argv + 1;  *argp != NULL;  ++argp )
+		if ( arg[0] == '+' )
 		{
-			const char* arg = *argp;
+			on = true;
+		}
+		else if ( arg[0] != '-' )
+		{
+			std::fprintf( stderr, "setleds: unrecognized argument '%s'\n", arg );
 			
-			bool on = false;
-			
-			if ( arg[0] == '+' )
+			return 2;
+		}
+		
+		for ( const char** pp = gOptionNames;  ;  ++pp )
+		{
+			if ( std::strcmp( *pp, arg + 1 ) == 0 )
 			{
-				on = true;
+				int bit = pp - gOptionNames;
+				
+				gChangedBits     |= on << bit;
+				gChangedBitsMask |= 1  << bit;
+				
+				break;
 			}
-			else if ( arg[0] != '-' )
+			
+			if ( *pp == NULL )
 			{
-				std::fprintf( stderr, "setleds: unrecognized argument '%s'\n", arg );
+				std::fprintf( stderr, "setleds: unrecognized led '%s'\n", arg );
 				
 				return 2;
 			}
-			
-			for ( const char** pp = gOptionNames;  ;  ++pp )
-			{
-				if ( std::strcmp( *pp, arg + 1 ) == 0 )
-				{
-					int bit = pp - gOptionNames;
-					
-					gChangedBits     |= on << bit;
-					gChangedBitsMask |= 1  << bit;
-					
-					break;
-				}
-				
-				if ( *pp == NULL )
-				{
-					std::fprintf( stderr, "setleds: unrecognized led '%s'\n", arg );
-					
-					return 2;
-				}
-			}
 		}
-		
-		bool setting = argc >= 2;
-		
-		int leds = setting ? gear::parse_unsigned_decimal( argv[1] ) & kLEDValueMask
-		                   : -1;
-		
-		N::ADBDevice_Container adbs;
-		
-		typedef N::ADBDevice_Container::const_iterator Iter;
-		
-		for ( Iter it = adbs.begin(), end = adbs.end();  it != end;  ++it )
-		{
-			DoLEDs( *it, leds );
-		}
-		
-		return 0;
-		
-	#endif
 	}
 	
+	bool setting = argc >= 2;
+	
+	int leds = setting ? gear::parse_unsigned_decimal( argv[1] ) & kLEDValueMask
+	                   : -1;
+	
+	N::ADBDevice_Container adbs;
+	
+	typedef N::ADBDevice_Container::const_iterator Iter;
+	
+	for ( Iter it = adbs.begin(), end = adbs.end();  it != end;  ++it )
+	{
+		DoLEDs( *it, leds );
+	}
+	
+	return 0;
+	
+#endif
+}
+
 }
