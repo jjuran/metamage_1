f4c0c316859f6e2454af5df8db1c7d1a1a0059bb
Josh Juran <jjuran@metamage.com>
2022-12-04 05:25:40 -0500

    Amphitheatre:  Drop support for bitmapped textures
    Don't try to make OpenGL accept 1-bit graphics for a texture.  Transcode
    to 8-bit grayscale on the CPU unconditionally.
    
    On one system (with Intel graphics), CPU use while running Vanlandingham
    dropped from ~18% to ~4%.  This is also a win for lower code complexity.

--- apps/mac/Amphitheatre/blit_CGL.cc
+++ apps/mac/Amphitheatre/blit_CGL.cc
@@ -16,9 +16,6 @@
 // transcodex
 #include "transcode/8x_1bpp_to_8bpp.hh"
 
-// Amphitheatre
-#include "bitmapped_texture_check.hh"
-
 
 static int image_width;
 static int image_height;
@@ -45,8 +42,6 @@ static float tex_x1;
 static float tex_y1;
 
 #endif
-
-static bool bitmaps_make_textures;
 
 static GLenum texture_format = GL_LUMINANCE;
 static GLenum texture_type   = GL_UNSIGNED_BYTE;
@@ -130,55 +125,6 @@ CGL_blitter::CGL_blitter( CGDirectDisplayID id, CGRect bounds )
 	
 	CGLSetCurrentContext( context );
 	
-#ifdef GL_TEXTURE_RECTANGLE_ARB
-	
-	/*
-		Don't even check for bitmap support if we don't have rectangle
-		textures.  It's always supported with 2D textures, but not in the
-		older OpenGL hardware -- and falling back to software rendering
-		kills performance.
-		
-		It's true that has_bitmapped_textures() returns false in this case,
-		but the extra check here lets us omit some dead code.
-	*/
-	
-	if ( (bitmaps_make_textures = has_bitmapped_textures()) )
-	{
-		texture_format = GL_COLOR_INDEX;
-		texture_type   = GL_BITMAP;
-		
-		const float palette[] = { 1.0, 0.0 };  // white = 0, black = 1
-		
-		glPixelMapfv( GL_PIXEL_MAP_I_TO_R, 2, palette );
-		
-		/*
-			GL_UNPACK_ALIGNMENT must be a factor of the stride, which is also
-			known as rowBytes.  In practice, screens tend to be a multiple of
-			128px wide, which at 1bpp is 16 bytes -- more than enough to cover
-			OpenGL's default alignment of 4 bytes or even its max of 8 bytes.
-			(An exception is 800px (rowBytes of 100), which is only a multiple
-			of 4 bytes.)
-			
-			Note that color graphics devices in classic Mac OS tend to have
-			extra, unused words at the end of each row.  This is fine when
-			you have qd.screenBits.rowBytes, but OpenGL doesn't have a means
-			to skip words that aren't alignment gaps.  Care must be taken when
-			creating raster files to avoid creating a "gutter" that poses a
-			nuisance for OpenGL, and ideally, choose an image width with no
-			wasted bytes that works regardless of alignment.
-			
-			To ensure compatibility with QuickDraw, the alignment must be at
-			least 2 bytes.  For example, an image width of 520px should have
-			a rowBytes of 66, not 65, and you'd have to call
-			
-				glPixelStorei( GL_UNPACK_ALIGNMENT, 2 );
-			
-			for correct unpacking in OpenGL.
-		*/
-	}
-	
-#endif
-	
 	glGenTextures( 1, &texture );
 	
 	glBindTexture( texture_target, texture );
@@ -260,22 +206,16 @@ void CGL_blitter::prep( int stride, int width, int height )
 	              texture_type,
 	              0 );  // NULL
 	
-	if ( ! bitmaps_make_textures )
-	{
-		texture_data = (uint8_t*) malloc( tex_width * tex_height );
-	}
+	texture_data = (uint8_t*) malloc( tex_width * tex_height );
 }
 
 void CGL_blitter::blit( const void* src_addr )
 {
-	if ( ! bitmaps_make_textures )
-	{
-		const int n_octets = image_width * image_height / 8u;
-		
-		transcode_8x_1bpp_to_8bpp( src_addr, texture_data, n_octets );
-		
-		src_addr = texture_data;
-	}
+	const int n_octets = image_width * image_height / 8u;
+	
+	transcode_8x_1bpp_to_8bpp( src_addr, texture_data, n_octets );
+	
+	src_addr = texture_data;
 	
 	glBindTexture( texture_target, texture );
 	
