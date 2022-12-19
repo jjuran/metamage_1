04daa1d96978b800b2123ff72e94a1d59434e4d2
Josh Juran <jjuran@metamage.com>
2022-12-18 19:50:44 -0500

    Revert "Amphitheatre:  Drop support for rectangle textures"
    Switch back to using rectangle textures where available.  This addresses
    a regression observed using 2D textures: visual artifacts in the form of
    multiple gray pixels appearing in one or two rows above and/or below the
    emulated screen (unrelated to 9236c8d450a6de49b3f8e7d8b94b3ddc993f02a3).
    
    This reverts commit 40d130b9ab8923921c790ee776ba723eed683cca.

--- graphics/glfb-common/glfb/glfb.cc
+++ graphics/glfb-common/glfb/glfb.cc
@@ -31,12 +31,25 @@ static int image_height;
 static int tex_width;
 static int tex_height;
 
+#ifdef GL_TEXTURE_RECTANGLE_ARB
+
+const GLenum texture_target = GL_TEXTURE_RECTANGLE_ARB;
+
+#define glTexCoord_ glTexCoord2i
+
+static int tex_x1;
+static int tex_y1;
+
+#else
+
 const GLenum texture_target = GL_TEXTURE_2D;
 
 #define glTexCoord_ glTexCoord2f
 
 static float tex_x1;
 static float tex_y1;
+
+#endif
 
 static GLenum texture_format = GL_LUMINANCE;
 static GLenum texture_type   = GL_UNSIGNED_BYTE;
@@ -75,9 +88,13 @@ void initialize()
 		matter what GL_TEXTURE_MIN_FILTER is set to.
 	*/
 	
+#ifndef GL_TEXTURE_RECTANGLE_ARB
+	
 	// Leave GL_TEXTURE_BASE_LEVEL set to its default of zero.
 	
 	glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0 );
+	
+#endif
 	
 	glBlendFunc( GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA );
 }
@@ -91,6 +108,16 @@ void set_dimensions( int width, int height )
 	image_width  = width;
 	image_height = height;
 	
+#ifdef GL_TEXTURE_RECTANGLE_ARB
+	
+	tex_width  = image_width;
+	tex_height = image_height;
+	
+	tex_x1 = image_width;
+	tex_y1 = image_height;
+	
+#else
+	
 	int greater = width > height ? width : height;
 	
 	tex_width  =
@@ -98,6 +125,8 @@ void set_dimensions( int width, int height )
 	
 	tex_x1 = 1.0 * image_width  / tex_width;
 	tex_y1 = 1.0 * image_height / tex_height;
+	
+#endif
 	
 	glTexImage2D( texture_target,
 	              0,
