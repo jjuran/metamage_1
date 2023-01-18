jjuran/metamage_1

commit aa88dfda3a8a6002b24f44d9add00f21e6d34974

Pedestal:  Make Clipboard.cc non-{nitric,throwing}

pedestal/Pedestal/Pedestal/Clipboard.cc
@@ -5,40 +5,37 @@
 
 #include "Pedestal/Clipboard.hh"
 
-// Nitrogen
-#include "Mac/Toolbox/Types/OSStatus.hh"
+// Mac OS
+#ifndef __SCRAP__
+#include <Scrap.h>
+#endif
 
-#include "Nitrogen/Scrap.hh"
-#include "Nitrogen/TextEdit.hh"
+
+#pragma exceptions off
 
 
 namespace Pedestal
 {
 	
-	namespace N = Nitrogen;
-	
-	
 	static inline void ClearCarbonScrap()
 	{
 		if ( TARGET_API_MAC_CARBON )
 		{
-			N::ClearCurrentScrap();
+			ClearCurrentScrap();
 		}
 	}
 	
 	static void FlushScrap()
 	{
-		try
-		{
-		#if !TARGET_API_MAC_CARBON
-			
-			N::ZeroScrap();
-			
-		#endif
-			
-			N::TEToScrap();
-		}
-		catch ( const Mac::OSStatus& err )
+	#if !TARGET_API_MAC_CARBON
+		
+		ZeroScrap();
+		
+	#endif
+		
+		OSErr err = TEToScrap();
+		
+		if ( err != noErr )
 		{
 			ClearCarbonScrap();
 		}
@@ -48,7 +45,7 @@ namespace Pedestal
 	{
 		ClearCarbonScrap();
 		
-		N::TECut( hTE );
+		TECut( hTE );
 		
 		FlushScrap();
 	}
@@ -57,7 +54,7 @@ namespace Pedestal
 	{
 		ClearCarbonScrap();
 		
-		N::TECopy( hTE );
+		TECopy( hTE );
 		
 		FlushScrap();
 	}
@@ -65,9 +62,12 @@ namespace Pedestal
 	void Clipboard::TEPaste( TEHandle hTE )
 	{
 		// Update the TE scrap just-in-time
-		N::TEFromScrap();
+		OSErr err = TEFromScrap();
 		
-		N::TEPaste( hTE );
+		if ( err == noErr )
+		{
+			TEPaste( hTE );
+		}
 	}
 	
 }

commit 37b1e37b07e44abb3751d86a4b7f8672dcda251f

Pedestal:  Fix unbounded recursion in Cut and Copy
This fixes the regression from aa88dfda3a8a6002b24f44d9add00f21e6d34974.

pedestal/Pedestal/Pedestal/Clipboard.cc
@@ -45,7 +45,7 @@ namespace Pedestal
 	{
 		ClearCarbonScrap();
 		
-		TECut( hTE );
+		::TECut( hTE );
 		
 		FlushScrap();
 	}
@@ -54,7 +54,7 @@ namespace Pedestal
 	{
 		ClearCarbonScrap();
 		
-		TECopy( hTE );
+		::TECopy( hTE );
 		
 		FlushScrap();
 	}
@@ -66,7 +66,7 @@ namespace Pedestal
 		
 		if ( err == noErr )
 		{
-			TEPaste( hTE );
+			::TEPaste( hTE );
 		}
 	}
 	