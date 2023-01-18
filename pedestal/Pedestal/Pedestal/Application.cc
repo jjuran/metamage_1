jjuran/metamage_1

commit 4f6325ca99a34f7ff8a5e86b79232f0e50c935d8

Pedestal:  Don't throw in DispatchHighLevelEvent()

pedestal/Pedestal/Pedestal/Application.cc
@@ -331,7 +331,7 @@ namespace Pedestal
 		
 		if ( err != errAEEventNotHandled )
 		{
-			Mac::ThrowOSStatus( err );
+			DebugBeep();
 		}
 	}
 	

commit 08bb780fa64e634e6d0f2da566a17737e02082fe

Pedestal:  Don't beep after handling Apple events
This fixes the regression from 4f6325ca99a34f7ff8a5e86b79232f0e50c935d8.

pedestal/Pedestal/Pedestal/Application.cc
@@ -298,7 +298,7 @@ namespace Pedestal
 		
 		OSErr err = AEProcessAppleEvent( &event );
 		
-		if ( err != errAEEventNotHandled )
+		if ( err != noErr  &&  err != errAEEventNotHandled )
 		{
 			DebugBeep();
 		}