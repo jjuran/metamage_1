jjuran/metamage_1

commit 1280e78a4ae050d420db3592c731b1f5abc0878d

Pedestal:  Prefer local min/max to <algorithm>

pedestal/Pedestal/Pedestal/Scroller_beta.cc
@@ -5,9 +5,6 @@
 
 #include "Pedestal/Scroller_beta.hh"
 
-// Standard C++
-#include <algorithm>
-
 // mac-qd-utils
 #include "mac_qd/get_portRect.hh"
 #include "mac_qd/globals/thePort.hh"
@@ -19,6 +16,20 @@
 
 namespace Pedestal
 {
+	
+	static inline
+	int
+	min( int a, int b )
+	{
+		return b > a ? b : a;
+	}
+	
+	static inline
+	int
+	max( int a, int b )
+	{
+		return a > b ? a : b;
+	}
 	
 	static inline
 	bool PtInRect( Point pt, const Rect& rect )
@@ -35,7 +46,7 @@ namespace Pedestal
 		
 		const short viewHeight = scroller.ViewHeight();
 		
-		const int max_voffset = std::max( scroller.ClientHeight() - viewHeight, 0 );
+		const int max_voffset = max( scroller.ClientHeight() - viewHeight, 0 );
 		
 		if ( c == kEndCharCode )
 		{
@@ -49,10 +60,10 @@ namespace Pedestal
 		switch ( c )
 		{
 			case kPageUpCharCode:
-				return std::max( voffset - page_distance, 0 );
+				return max( voffset - page_distance, 0 );
 			
 			case kPageDownCharCode:
-				return std::min( voffset + page_distance, max_voffset );
+				return min( voffset + page_distance, max_voffset );
 			
 			default:
 				break;

pedestal/Pedestal/Pedestal/TextEdit.cc
@@ -5,9 +5,6 @@
 
 #include "Pedestal/TextEdit.hh"
 
-// Standard C++
-#include <algorithm>
-
 // Mac OS X
 #ifdef __APPLE__
 #include <Carbon/Carbon.h>
@@ -57,6 +54,20 @@ namespace Pedestal
 	class NotEnoughSpaceForTEKey   {};
 	class NotEnoughSpaceForTEPaste {};
 	
+	
+	static inline
+	short
+	min( short a, short b )
+	{
+		return b > a ? b : a;
+	}
+	
+	static inline
+	short
+	max( short a, short b )
+	{
+		return a > b ? a : b;
+	}
 	
 	void Preflight_TEKey( char c, TEHandle hTE )
 	{
@@ -480,8 +491,8 @@ namespace Pedestal
 			// Arrow key with Shift
 			// Delete key with Command or Option on empty selection
 			
-			selStart = std::min( gSelectionAnchor, gSelectionExtent );
-			selEnd   = std::max( gSelectionAnchor, gSelectionExtent );
+			selStart = min( gSelectionAnchor, gSelectionExtent );
+			selEnd   = max( gSelectionAnchor, gSelectionExtent );
 			
 			// Update the real selection
 			::TESetSelect( selStart, selEnd, hTE );

pedestal/Pedestal/Pedestal/TrackScrollbar.cc
@@ -15,9 +15,6 @@
 #include <ControlDefinitions.h>
 #endif
 
-// Standard C++
-#include <algorithm>
-
 // mac-config
 #include "mac_config/upp-macros.hh"
 
@@ -39,10 +36,24 @@
 namespace Pedestal
 {
 	
+	static inline
+	short
+	min( short a, short b )
+	{
+		return b > a ? b : a;
+	}
+	
+	static inline
+	short
+	max( short a, short b )
+	{
+		return a > b ? a : b;
+	}
+	
 	static short SetClippedControlValue( ControlRef control, short value )
 	{
-		value = std::max( value, GetControlMinimum( control ) );
-		value = std::min( value, GetControlMaximum( control ) );
+		value = max( value, GetControlMinimum( control ) );
+		value = min( value, GetControlMaximum( control ) );
 		
 		SetControlValue( control, value );
 		

pedestal/Pedestal/Pedestal/Application.cc
@@ -5,9 +5,6 @@
 
 #include "Pedestal/Application.hh"
 
-// Standard C++
-#include <algorithm>
-
 // Mac OS X
 #ifdef __APPLE__
 #include <Carbon/Carbon.h>
@@ -168,6 +165,21 @@ namespace Pedestal
 	static UInt32 gShiftSpaceQuasimodeMask = 0;
 	
 	static boost::intrusive_ptr< Quasimode > gQuasimode;
+	
+	
+	static inline
+	UInt32
+	ticks_min( UInt32 a, UInt32 b )
+	{
+		return b < a ? b : a;
+	}
+	
+	static inline
+	monotonic_clock::clock_t
+	clock_max( monotonic_clock::clock_t a, monotonic_clock::clock_t b )
+	{
+		return a > b ? a : b;
+	}
 	
 	
 	static inline void DebugBeep()
@@ -865,11 +877,11 @@ namespace Pedestal
 			ticksToSleep = 1;
 		}
 		
-		gClockAtNextBusiness = std::max( gClockAtNextBusiness, now );
+		gClockAtNextBusiness = clock_max( gClockAtNextBusiness, now );
 		
 		const UInt32 ticksToWait = ticks_from( gClockAtNextBusiness - now );
 		
-		ticksToSleep = std::min( ticksToSleep, ticksToWait );
+		ticksToSleep = ticks_min( ticksToSleep, ticksToWait );
 		
 		gClockAtNextBusiness = clock_t( -1 );
 		

pedestal/Pedestal/Pedestal/Scrollbar.cc
@@ -5,9 +5,6 @@
 
 #include "Pedestal/Scrollbar.hh"
 
-// Standard C++
-#include <algorithm>
-
 // mac-sys-utils
 #include "mac_sys/has/Appearance.hh"
 
@@ -28,6 +25,13 @@ namespace Pedestal
 	
 	namespace N = Nitrogen;
 	
+	
+	static inline
+	short
+	max( short a, short b )
+	{
+		return a > b ? a : b;
+	}
 	
 	static inline N::ControlProcID GetControlProcIDForScrollbar()
 	{
@@ -120,7 +124,7 @@ namespace Pedestal
 	{
 		if ( itsControl )
 		{
-			short max_offset = std::max( clientLength - viewLength, offset );
+			short max_offset = max( clientLength - viewLength, offset );
 			
 			SetControlMaximum( itsControl, max_offset );
 			

commit 48c515ac12ed222d9ab32e4b0fca17cd143ef30c

Pedestal:  Fix scrolling, text selection extension
This fixes the regression from 1280e78a4ae050d420db3592c731b1f5abc0878d.

pedestal/Pedestal/Pedestal/Scroller_beta.cc
@@ -21,7 +21,7 @@ namespace Pedestal
 	int
 	min( int a, int b )
 	{
-		return b > a ? b : a;
+		return b < a ? b : a;
 	}
 	
 	static inline

pedestal/Pedestal/Pedestal/TextEdit.cc
@@ -59,7 +59,7 @@ namespace Pedestal
 	short
 	min( short a, short b )
 	{
-		return b > a ? b : a;
+		return b < a ? b : a;
 	}
 	
 	static inline

pedestal/Pedestal/Pedestal/TrackScrollbar.cc
@@ -40,7 +40,7 @@ namespace Pedestal
 	short
 	min( short a, short b )
 	{
-		return b > a ? b : a;
+		return b < a ? b : a;
 	}
 	
 	static inline