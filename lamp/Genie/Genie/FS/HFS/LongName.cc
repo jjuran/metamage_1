b8462c8d5644d4eb976f2055edefca2132e5745f
Josh Juran <jjuran@metamage.com>
2023-02-28 11:55:39 -0500

    Genie:  Use non-throwing set_desktop_comment()

--- lamp/Genie/Genie/FS/HFS/LongName.cc
+++ lamp/Genie/Genie/FS/HFS/LongName.cc
@@ -5,33 +5,40 @@
 
 #include "Genie/FS/HFS/LongName.hh"
 
+// Mac OS X
+#ifdef __APPLE__
+#include <CoreServices/CoreServices.h>
+#endif
+
+// Mac OS
+#ifndef __FILES__
+#include <Files.h>
+#endif
+
+// mac-file-utils
+#include "mac_file/desktop.hh"
+
 // plus
 #include "plus/string.hh"
 
 // Nitrogen
-#include "Nitrogen/Files.hh"
+#include "Mac/Toolbox/Utilities/ThrowOSStatus.hh"
 
 
 namespace Genie
 {
 	
-	namespace N = Nitrogen;
-	
-	
 	void SetLongName( const FSSpec& item, const plus::string& name )
 	{
+		using mac::file::set_desktop_comment;
+		
 		if ( name.length() > 31 )
 		{
-			try
+			OSErr err = set_desktop_comment( item, name.data(), name.size() );
+			
+			if ( err != paramErr )
 			{
-				N::FSpDTSetComment( item, name );
-			}
-			catch ( const Mac::OSStatus& err )
-			{
-				if ( err != paramErr )
-				{
-					throw;
-				}
+				Mac::ThrowOSStatus( err );
 			}
 		}
 	}
