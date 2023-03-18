d6ac3b43f45d3d4558bc3909d640279ae6f145e8
Josh Juran <jjuran@metamage.com>
2023-03-18 19:42:50 -0400

    [NEW] ams-dsat:  Show simplified traditional alert

---
+++ 68k/modules/ams-dsat/SysError.cc
@@ -0,0 +1,195 @@
+/*
+	SysError.cc
+	-----------
+*/
+
+#include "SysError.hh"
+
+// Mac OS
+#ifndef __EVENTS__
+#include <Events.h>
+#endif
+#ifndef __QUICKDRAW__
+#include <Quickdraw.h>
+#endif
+
+// POSIX
+#include <unistd.h>
+
+
+BitMap IconBitmap : 0x0A0E;
+
+static const UInt32 icon[] =
+{
+	0x00000808,
+	0x00000010,
+	0x00000420,
+	0x00008040,
+	0x00002200,
+	0x00000800,
+	0x001f002a,
+	0x0020c700,
+	0x00403800,
+	0x00400020,
+	0x00400910,
+	0x03f81008,
+	0x03f82104,
+	0x03f80000,
+	0x0ffe0100,
+	0x3fff8000,
+	0x3fff8000,
+	0x7fffc000,
+	0x7fffc000,
+	0xfffde000,
+	0xffffe000,
+	0xfffee000,
+	0xfffee000,
+	0xfffee000,
+	0xffffe000,
+	0xfffde000,
+	0x7fffc000,
+	0x7ff7c000,
+	0x3fff8000,
+	0x3fff8000,
+	0x0ffe0000,
+	0x03f80000,
+};
+
+void SysError_patch( short error : __D0 )
+{
+	const short width  = 428;
+	const short height = 112;
+	
+	QDGlobals qd;
+	GrafPort port;
+	
+	InitGraf( &qd.thePort );
+	
+	InitCursor();
+	
+	OpenPort( &port );
+	
+	short left = (port.portRect.right  - width ) / 2;
+	short top  = (port.portRect.bottom - height) / 3 - 4;
+	
+	Rect bounds = port.portRect;
+	
+	bounds.top  += top;
+	bounds.left += left;
+	bounds.bottom = bounds.top + height;
+	bounds.right  = bounds.left + width;
+	
+	PenNormal();
+	EraseRect( &bounds );
+	FrameRect( &bounds );
+	
+	InsetRect( &bounds, 3, 3 );
+	PenSize( 2, 2 );
+	FrameRect( &bounds );
+	PenNormal();
+	
+	top  += 8;
+	left += 8;
+	
+	const short icon_h = left + 23;
+	const short icon_v = top  + 12;
+	
+	const Rect icon_rect = { icon_v, icon_h, icon_v + 32, icon_h + 32 };
+	
+	IconBitmap.baseAddr = (Ptr) icon;
+	IconBitmap.rowBytes = 4;
+	IconBitmap.bounds.top  = 0;
+	IconBitmap.bounds.left = 0;
+	IconBitmap.bounds.bottom = 32;
+	IconBitmap.bounds.right  = 32;
+	
+	CopyBits( &IconBitmap,
+	          &port.portBits,
+	          &IconBitmap.bounds,
+	          &icon_rect,
+	          srcXor,
+	          NULL );
+	
+	short button_bottom = top + height - 16 - 12;
+	
+	Rect restart_bounds =
+	{
+		button_bottom - 20, left + 23,
+		button_bottom,      left + 83,
+	};
+	
+	FrameRoundRect( &restart_bounds, 16, 16 );
+	
+	MoveTo( left + 23 + 4, button_bottom - 6 );
+	
+	DrawString( "\p" "Restart" );
+	
+	const short text_h = -left + icon_h * 2 + 32;
+	const short text_v = top  + 12 + 15 + 12;
+	
+	MoveTo( text_h, text_v );
+	
+	DrawString( "\p" "Sorry, a system error occurred." );
+	
+	InsetRect( &restart_bounds, 1, 1 );
+	
+	bool tracking = false;
+	bool hiliting = false;
+	
+	const short eventMask = mDownMask | mUpMask | osMask;
+	
+	RgnHandle tmpRgn   = NULL;
+	RgnHandle mouseRgn = NULL;
+	
+	while ( true )
+	{
+		EventRecord event;
+		
+		if ( WaitNextEvent( eventMask, &event, 0xFFFFFFFF, mouseRgn ) )
+		{
+			bool inside = PtInRect( event.where, &restart_bounds );
+			
+			if ( inside  &&  event.what == mouseDown )
+			{
+				tracking = true;
+				
+				if ( tmpRgn == NULL )
+				{
+					tmpRgn = NewRgn();
+				}
+				
+				if ( tmpRgn )
+				{
+					mouseRgn = tmpRgn;
+					
+					Rect& r = mouseRgn[0]->rgnBBox;
+					
+					r.top  = event.where.v;
+					r.left = event.where.h;
+					r.bottom = r.top + 1;
+					r.right = r.left + 1;
+				}
+			}
+			
+			if ( tracking  &&  inside != hiliting )
+			{
+				hiliting = inside;
+				
+				InvertRoundRect( &restart_bounds, 16, 16 );
+			}
+			
+			if ( event.what == mouseUp )
+			{
+				if ( tracking  &&  inside )
+				{
+					break;
+				}
+				
+				mouseRgn = NULL;
+				tracking = false;
+			}
+		}
+	}
+	
+	_exit( error );
+}
---
+++ 68k/modules/ams-dsat/ams-dsat.cc
@@ -0,0 +1,87 @@
+/*
+	ams-dsat.cc
+	-----------
+*/
+
+// Mac OS
+#ifndef __TRAPS__
+#include <Traps.h>
+#endif
+
+// POSIX
+#include <unistd.h>
+
+// ams-common
+#include "callouts.hh"
+
+// ams-dsat
+#include "SysError.hh"
+
+
+#define PROGRAM  "ams-dsat"
+
+#define STR_LEN( s )  "" s, (sizeof s - 1)
+
+#define WARN( msg )  write( STDERR_FILENO, STR_LEN( PROGRAM ": " msg "\n" ) )
+
+
+void* os_trap_table     [] : 1 * 1024;
+void* toolbox_trap_table[] : 3 * 1024;
+
+#define TBTRAP( Routine )  (toolbox_trap_table[ _ ## Routine & 0x03FF ] = &Routine ## _patch)
+
+static
+asm void unimplemented_trap()
+{
+	MOVEQ    #12,D0
+	
+	_SysError | 0x0400  // auto-pop
+}
+
+static
+void reassign_unimplemented_traps()
+{
+	void* old_addr = toolbox_trap_table[ _Unimplemented & 0x03FF ];
+	void* new_addr = &unimplemented_trap;
+	
+	short n;
+	void** p;
+	
+	n = 256;
+	p = os_trap_table;
+	
+	while ( --n >= 0 )
+	{
+		if ( *p++ == old_addr )
+		{
+			p[ -1 ] = new_addr;
+		}
+	}
+	
+	n = 1024;
+	p = toolbox_trap_table;
+	
+	while ( --n >= 0 )
+	{
+		if ( *p++ == old_addr )
+		{
+			p[ -1 ] = new_addr;
+		}
+	}
+}
+
+int main( int argc, char** argv )
+{
+	if ( argc > 1 )
+	{
+		WARN( "no arguments allowed" );
+		
+		_exit( 1 );
+	}
+	
+	TBTRAP( SysError );  // A9C9
+	
+	reassign_unimplemented_traps();
+	
+	module_suspend();  // doesn't return
+}
