59c63f5ac7827a917144b7f4c478b342330c6d0f
Josh Juran <jjuran@metamage.com>
2023-03-19 18:30:55 -0400

    archive.pl:  Don't ship vols

--- relix/scripts/archive.pl
+++ relix/scripts/archive.pl
@@ -219,7 +219,7 @@ my %fsmap =
 			\ qw( adbs beep cds crsr deicon drives drvr err2text gdev keymods
 				location mac-id macbin mnam qdscreencap ram-disks
 				rom-checksum rominfo
-				screenBits system-info thds units vcbs vols ),
+				screenBits system-info thds units vcbs ),
 			# Mac-specific scripts
 			qw( gestalt ),
 			# Perl scripts
