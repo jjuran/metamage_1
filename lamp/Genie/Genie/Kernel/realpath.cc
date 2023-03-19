deed2a48f12e17601926523516c2da690dc25d26
Josh Juran <jjuran@metamage.com>
2023-03-19 01:59:44 -0400

    Genie:  Recognize /.hfs/<vol>/<dir> in realpath()
    This isn't a full vfs implementation of /.hfs, but just enough for being
    usable from userspace.  This lets clients get a pathname to a directory.

--- lamp/Genie/Genie/Kernel/realpath.cc
+++ lamp/Genie/Genie/Kernel/realpath.cc
@@ -13,6 +13,9 @@
 // more-libc
 #include "more/string.h"
 
+// gear
+#include "gear/parse_decimal.hh"
+
 // plus
 #include "plus/mac_utf8.hh"
 
@@ -34,6 +37,7 @@
 #include "relix/syscall/registry.hh"
 
 // Genie
+#include "Genie/FS/FSSpec.hh"
 
 
 #ifndef REALPATH_OUTPUT_HFS
@@ -43,6 +47,10 @@
 #ifndef REALPATH_OUTPUT_HFS_UTF8
 #define REALPATH_OUTPUT_HFS_UTF8 (-1)
 #endif
+
+#define STRLEN(s)  (sizeof "" s - 1)
+
+#define STR_LEN(s)  "" s, (sizeof s - 1)
 
 
 namespace Genie
@@ -54,6 +62,20 @@ namespace Genie
 		return b < a ? b : a;
 	}
 	
+	static inline
+	bool begins_with( const char* s, const char* prefix, size_t length )
+	{
+		while ( length-- )
+		{
+			if ( *s++ != *prefix++ )
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
 	
 	static plus::string mac_pathname_from_file( const vfs::node& file )
 	{
@@ -63,9 +85,55 @@ namespace Genie
 	
 	static ssize_t _realpathat( int dirfd, const char *path, char *buffer, size_t buffer_size, int flags )
 	{
+		short vRefNum = 0;
+		long  dirID   = 0;
+		
+		if ( begins_with( path, STR_LEN( "/.hfs/" ) ) )
+		{
+			if ( flags )
+			{
+				return relix::set_errno( EINVAL );
+			}
+			
+			const char* p = path + STRLEN( "/.hfs/" );
+			
+			unsigned v = gear::parse_unsigned_decimal( &p );
+			
+			if ( v == 0  ||  v > 0x7fff )
+			{
+				return relix::set_errno( ENOENT );
+			}
+			
+			if ( *p++ != '/' )
+			{
+				return relix::set_errno( ENXIO );
+			}
+			
+			unsigned d = gear::parse_unsigned_decimal( &p );
+			
+			if ( d == 0  ||  d > 0x7fffffff )
+			{
+				return relix::set_errno( ENOENT );
+			}
+			
+			if ( *p == '/' )
+			{
+				++p;
+			}
+			
+			if ( *p != '\0' )
+			{
+				return relix::set_errno( ENOENT );
+			}
+			
+			vRefNum = -v;
+			dirID   =  d;
+		}
+		
 		try
 		{
-			vfs::node_ptr file = relix::resolve_path_at( dirfd, path );
+			vfs::node_ptr file = dirID ? node_from_dirID( vRefNum, dirID )
+			                           : relix::resolve_path_at( dirfd, path );
 			
 			vfs::resolve_links_in_place( *relix::root(), file );
 			
