f409b59d1dba9335781ae5c064f36bf0b7f17de2
Josh Juran <jjuran@metamage.com>
2023-03-20 17:31:18 -0400

    one_path:  Call find_MPW_dir() from find_SDK_dir()

--- lamp/Kerosene/one_path/one_path/find_SDK_dir.cc
+++ lamp/Kerosene/one_path/one_path/find_SDK_dir.cc
@@ -18,7 +18,7 @@
 #include "poseven/types/errno_t.hh"
 
 // one_path
-#include "one_path/find_appl.hh"
+#include "one_path/find_MPW_dir.hh"
 
 
 namespace p7 = poseven;
@@ -26,7 +26,6 @@ namespace p7 = poseven;
 
 static const char env_sdk_dir[] = "SDK_DIR";
 static const char txt_intfs_libs[] = "Interfaces&Libraries";
-static const char sig_ToolServer[] = "MPSX";
 
 
 plus::string find_SDK_dir()
@@ -36,20 +35,17 @@ plus::string find_SDK_dir()
 		return sdk_dir;
 	}
 	
-	plus::var_string pathname = find_appl( sig_ToolServer ).move();
+	plus::var_string pathname = find_MPW_dir().move();
 	
 	const char* p = pathname.data();
 	
 	if ( const char* it = gear::find_last_match( p, pathname.size(), '/' ) )
 	{
-		if (( it = gear::find_last_match( p, it - p, '/' ) ))
-		{
-			pathname.resize( it + 1 - p );
-			
-			pathname += txt_intfs_libs;
-			
-			return pathname.move();
-		}
+		pathname.resize( it + 1 - p );
+		
+		pathname += txt_intfs_libs;
+		
+		return pathname.move();
 	}
 	
 	throw p7::errno_t( ENOENT );
