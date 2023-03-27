68017f835b467758c48134a3ac3565b1bad34ea8
Josh Juran <jjuran@metamage.com>
2023-03-26 21:54:16 -0400

    ams-common:  Define JShieldCursor and JCrsrObscure

--- 68k/ams/common/cursor_jump.hh
+++ 68k/ams/common/cursor_jump.hh
@@ -7,14 +7,19 @@
 #define CURSORJUMP_HH
 
 struct Cursor;
+struct Point;
+struct Rect;
 
 typedef void (*CursorProcPtr)();  // pascal and C have same calling sequence
 
+typedef pascal void (*ShieldCursorProcPtr)( const Rect* r, Point pt );
 typedef pascal void (*SetCursorProcPtr)( const Cursor* crsr );
 
-CursorProcPtr JHideCursor : 0x0800;
-CursorProcPtr JShowCursor : 0x0804;
-CursorProcPtr JInitCrsr   : 0x0814;
-SetCursorProcPtr JSetCrsr : 0x0818;
+CursorProcPtr       JHideCursor   : 0x0800;
+CursorProcPtr       JShowCursor   : 0x0804;
+ShieldCursorProcPtr JShieldCursor : 0x0808;
+CursorProcPtr       JInitCrsr     : 0x0814;
+SetCursorProcPtr    JSetCrsr      : 0x0818;
+CursorProcPtr       JCrsrObscure  : 0x081C;
 
 #endif
