3e29cd95d4e04007fb81b180286a1cdfbdc65f48
Josh Juran <jjuran@metamage.com>
2023-04-04 22:46:34 -0400

    iota -> freestore_free:  Move new/delete overrides

--- base/freestore_free/freestore_free.cc
+++ base/freestore_free/freestore_free.cc
@@ -6,15 +6,36 @@
 	stack, by default get saddled with an unneeded exception-handling runtime
 	when using the Metrowerks toolchain, but this can be avoided.
 	
-	The original approach is to include iota/freestore_free.hh in the main
-	source file of the program, but that only works if the source is in C++.
-	As an alternative, a program can link with this library instead. 
+	Linking with this file overrides global operator new and operator delete.
+	The new definition of operator new calls an undefined function, so if
+	any calls to operator new exist in the program, it will fail to link
+	(in which case you shouldn't be using the freestore_free project anyway).
+	
+	Having established that a program doesn't call operator new, we assume
+	that it also doesn't call operator delete /explicitly/.  Unfortunately,
+	Metrowerks C++ generates runtime code for destructors that unconditionally
+	includes a branch that calls operator delete, even if that branch can
+	never be taken (e.g. since objects are only constructed on the stack).
+	Since operator delete calls __unexpected() given a NULL argument (again,
+	even if that argument can never be NULL, or in this case, even though it
+	will never be called in the first place), the entire exception-handling
+	subsystem gets linked into the code, even though none of it gets used.
 */
 
-// iota
-#include "iota/freestore_free.hh"
+#ifdef __MWERKS__
 
-#ifndef __MWERKS__
+extern void* operator_new_is_undefined;
+
+void* operator new( unsigned long )
+{
+	return operator_new_is_undefined;
+}
+
+void operator delete( void* )
+{
+}
+
+#else
 
 int dummy;
 
