jjuran/metamage_1

commit 6a364b4102d1a11be2c60ce1fb0fe417b9560a1b

amicus:  Disassociate mouse from cursor position
This is the proper way to do it, and fixes the intermittent issue of the
native cursor sometimes failing to stay hidden in macOS 10.13 and later.

Since our current approach relies on CGEvents, but CopyEventCGEvent() is
only available since 10.5, only use the disassociated mode in 10.5+, and
keep using an associated cursor and CGWarpMouseCursorPosition() in 10.4.

mac/libs/amicus/amicus/events.cc
@@ -50,16 +50,28 @@ CGPoint pin_cursor( CGPoint next_cursor_location )
 	{
 		next_cursor_location.x = cursor_limit.x - 1;
 	}
+	else if ( next_cursor_location.x < 0 )
+	{
+		next_cursor_location.x = 0;
+	}
 	
 	if ( next_cursor_location.y >= cursor_limit.y )
 	{
 		next_cursor_location.y = cursor_limit.y - 1;
 	}
+	else if ( next_cursor_location.y < 0 )
+	{
+		next_cursor_location.y = 0;
+	}
+	
+#ifndef MAC_OS_X_VERSION_10_5
 	
 	if ( ! CGPointEqualToPoint( next_cursor_location, unpinned_location ) )
 	{
 		move_cursor_to( next_cursor_location );
 	}
+	
+#endif
 	
 	return next_cursor_location;
 }
@@ -75,6 +87,20 @@ void handle_mouse_moved_event( CGPoint next_cursor_location )
 		
 		last_cursor_location = pinned_cursor;
 	}
+}
+
+static
+void handle_mouse_moved_event( CGEventRef event )
+{
+	long dx = CGEventGetIntegerValueField( event, kCGMouseEventDeltaX );
+	long dy = CGEventGetIntegerValueField( event, kCGMouseEventDeltaY );
+	
+	CGPoint next = last_cursor_location;
+	
+	next.x += dx;
+	next.y += dy;
+	
+	handle_mouse_moved_event( next );
 }
 
 static inline
@@ -216,7 +242,7 @@ bool handle_CGEvent( CGEventRef event, command_handler_proc command_handler )
 		case kCGEventMouseMoved:
 		case kCGEventLeftMouseDragged:
 		case kCGEventRightMouseDragged:
-			handle_mouse_moved_event( CGEventGetLocation( event ) );
+			handle_mouse_moved_event( event );
 			break;
 		
 		default:

mac/libs/amicus/amicus/display.cc
@@ -17,10 +17,22 @@ display_capture::display_capture( CGDirectDisplayID id ) : its_id( id )
 	}
 	
 	CGDisplayHideCursor( its_id );
+	
+#ifdef MAC_OS_X_VERSION_10_5
+	
+	CGAssociateMouseAndMouseCursorPosition( false );
+	
+#endif
 }
 
 display_capture::~display_capture()
 {
+#ifdef MAC_OS_X_VERSION_10_5
+	
+	CGAssociateMouseAndMouseCursorPosition( true );
+	
+#endif
+	
 	CGDisplayShowCursor( its_id );
 	
 	CGDisplayRelease( its_id );

mac/libs/amicus/amicus/skeleton.cc.hh
@@ -200,7 +200,13 @@ void run_event_loop( const raster_load& load, const raster_desc& desc )
 	
 	cursor_limit = CGPointMake( width, height );
 	
-	CGWarpMouseCursorPosition( CGPointMake( 15, 15 ) );
+	last_cursor_location = CGPointMake( 15, 15 );
+	
+#ifndef MAC_OS_X_VERSION_10_5
+	
+	CGWarpMouseCursorPosition( last_cursor_location );
+	
+#endif
 	
 	EventTargetRef target = GetEventDispatcherTarget();
 	

commit f422e95238d525947f6f61dfa78590fbd7efcbf4

amicus:  Unbreak builds using the 10.2 or 10.3 SDK
This fixes the regression from 6a364b4102d1a11be2c60ce1fb0fe417b9560a1b.

mac/libs/amicus/amicus/events.cc
@@ -89,6 +89,8 @@ void handle_mouse_moved_event( CGPoint next_cursor_location )
 	}
 }
 
+#ifdef MAC_OS_X_VERSION_10_4
+
 static
 void handle_mouse_moved_event( CGEventRef event )
 {
@@ -102,6 +104,8 @@ void handle_mouse_moved_event( CGEventRef event )
 	
 	handle_mouse_moved_event( next );
 }
+
+#endif
 
 static inline
 bool any_keys_down()