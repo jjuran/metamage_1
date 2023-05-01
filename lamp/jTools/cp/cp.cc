acbc041258be56cd48e28c19d09408961d4c1307
Josh Juran <jjuran@metamage.com>
2023-05-01 04:43:55 -0400

    cp:  Unindent namespace

--- lamp/jTools/cp/cp.cc
+++ lamp/jTools/cp/cp.cc
@@ -29,95 +29,135 @@
 
 namespace tool
 {
+
+namespace p7 = poseven;
+
+
+static const char* Basename( const char* pathname )
+{
+	const char* name_start = pathname;
 	
-	namespace p7 = poseven;
+	const char* p = pathname;
 	
-	
-	static const char* Basename( const char* pathname )
+	while ( *p != '\0' )
 	{
-		const char* name_start = pathname;
-		
-		const char* p = pathname;
-		
-		while ( *p != '\0' )
+		if ( *p++ == '/' )
 		{
-			if ( *p++ == '/' )
-			{
-				name_start = p;
-			}
+			name_start = p;
 		}
-		
-		return name_start;
 	}
 	
-	static int copyfile_or_pump( const char* src, const char* dest )
+	return name_start;
+}
+
+static int copyfile_or_pump( const char* src, const char* dest )
+{
+	int ok = ::copyfile( src, dest );
+	
+	if ( ok < 0  &&  (errno == EINVAL  ||  errno == EXDEV) )
 	{
-		int ok = ::copyfile( src, dest );
+		int in = ok = open( src, O_RDONLY );
 		
-		if ( ok < 0  &&  (errno == EINVAL  ||  errno == EXDEV) )
+		if ( in >= 0 )
 		{
-			int in = ok = open( src, O_RDONLY );
+			int out = ok = open( dest, O_WRONLY|O_CREAT, 0666 );
 			
-			if ( in >= 0 )
+			if ( out >= 0 )
 			{
-				int out = ok = open( dest, O_WRONLY|O_CREAT, 0666 );
+				ssize_t pumped = pump( in, NULL, out, NULL, 0, 0 );
 				
-				if ( out >= 0 )
-				{
-					ssize_t pumped = pump( in, NULL, out, NULL, 0, 0 );
-					
-					pumped = pump( in, NULL, out, NULL, 0, 0 );
-					
-					ok = -(pumped != 0);
-					
-					close( out );
-				}
+				pumped = pump( in, NULL, out, NULL, 0, 0 );
 				
-				close( in );
+				ok = -(pumped != 0);
+				
+				close( out );
 			}
+			
+			close( in );
 		}
-		
-		return ok;
 	}
 	
-	int Main( int argc, char** argv )
+	return ok;
+}
+
+int Main( int argc, char** argv )
+{
+	int fail = 0;
+	
+	// Check for sufficient number of args
+	if ( argc < 3 )
 	{
-		int fail = 0;
+		fprintf( stderr, "cp: missing %s\n", (argc == 1) ? "file arguments"
+		                                                 : "destination file" );
 		
-		// Check for sufficient number of args
-		if ( argc < 3 )
+		return 1;
+	}
+	
+	struct stat sb;
+	
+	if ( argc > 3 )
+	{
+		// Copy multiple files.
+		
+		// Last arg should be the destination directory.
+		const char* destDir = argv[ argc - 1 ];
+		
+		p7::stat( destDir, sb );
+		
+		if ( bool not_a_dir = (sb.st_mode & S_IFDIR) == 0 )
 		{
-			fprintf( stderr, "cp: missing %s\n", (argc == 1) ? "file arguments"
-			                                                 : "destination file" );
+			fprintf( stderr, "cp: copying multiple files, but last argument (%s) is not a directory.\n",
+			                                                                 destDir );
 			
 			return 1;
 		}
 		
-		struct stat sb;
-		
-		if ( argc > 3 )
+		// Try to copy each file.  Return whether any errors occurred.
+		for ( size_t index = 1;  index < argc - 1;  ++index )
 		{
-			// Copy multiple files.
+			const char* sourcePath = argv[index];
 			
-			// Last arg should be the destination directory.
-			const char* destDir = argv[ argc - 1 ];
+			plus::var_string destFilePath = destDir;
 			
-			p7::stat( destDir, sb );
-			
-			if ( bool not_a_dir = (sb.st_mode & S_IFDIR) == 0 )
+			if ( *(destFilePath.end() - 1) != '/' )
 			{
-				fprintf( stderr, "cp: copying multiple files, but last argument (%s) is not a directory.\n",
-				                                                                 destDir );
-				
-				return 1;
+				destFilePath += '/';
 			}
 			
-			// Try to copy each file.  Return whether any errors occurred.
-			for ( size_t index = 1;  index < argc - 1;  ++index )
+			destFilePath += Basename( sourcePath );
+			
+			if ( -1 == copyfile_or_pump( sourcePath, destFilePath.c_str() ) )
 			{
-				const char* sourcePath = argv[index];
-				
-				plus::var_string destFilePath = destDir;
+				++fail;
+			}
+		}
+	}
+	else
+	{
+		// Copy single file.
+		const char* sourcePath = argv[1];
+		const char* destPath   = argv[2];
+		
+		if ( -1 == lstat( sourcePath, &sb ) )
+		{
+			fprintf( stderr, "cp: %s: %s\n", sourcePath, strerror( errno ) );
+			
+			return 1;
+		}
+		
+		plus::var_string destFilePath = destPath;
+		
+		if ( p7::stat( destPath, sb ) )
+		{
+			// dest exists
+			
+			bool isDir = sb.st_mode & S_IFDIR;
+			
+			if ( isDir )
+			{
+				// dest is a dir.
+				// Copy this -> that/this
+				// set that = that/this
 				
 				if ( *(destFilePath.end() - 1) != '/' )
 				{
@@ -126,54 +166,14 @@ namespace tool
 				
 				destFilePath += Basename( sourcePath );
 				
-				if ( -1 == copyfile_or_pump( sourcePath, destFilePath.c_str() ) )
-				{
-					++fail;
-				}
+				destPath = destFilePath.c_str();
 			}
-		}
-		else
-		{
-			// Copy single file.
-			const char* sourcePath = argv[1];
-			const char* destPath   = argv[2];
-			
-			if ( -1 == lstat( sourcePath, &sb ) )
-			{
-				fprintf( stderr, "cp: %s: %s\n", sourcePath, strerror( errno ) );
-				
-				return 1;
-			}
-			
-			plus::var_string destFilePath = destPath;
-			
-			if ( p7::stat( destPath, sb ) )
-			{
-				// dest exists
-				
-				bool isDir = sb.st_mode & S_IFDIR;
-				
-				if ( isDir )
-				{
-					// dest is a dir.
-					// Copy this -> that/this
-					// set that = that/this
-					
-					if ( *(destFilePath.end() - 1) != '/' )
-					{
-						destFilePath += '/';
-					}
-					
-					destFilePath += Basename( sourcePath );
-					
-					destPath = destFilePath.c_str();
-				}
-			}
-			
-			p7::throw_posix_result( copyfile_or_pump( sourcePath, destPath ) );
 		}
 		
-		return fail;
+		p7::throw_posix_result( copyfile_or_pump( sourcePath, destPath ) );
 	}
+	
+	return fail;
+}
 
 }
