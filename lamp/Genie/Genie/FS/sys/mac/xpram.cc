e44f0f0dc934580ac12148046b7267dcba3d11c5
Josh Juran <jjuran@metamage.com>
2023-06-01 02:14:40 -0400

    Genie:  Use mac::sys::ReadXPRam()

--- lamp/Genie/Genie/FS/sys/mac/xpram.cc
+++ lamp/Genie/Genie/FS/sys/mac/xpram.cc
@@ -7,51 +7,9 @@
 
 #include "Genie/FS/sys/mac/xpram.hh"
 
-// Mac OS
-#ifndef __MIXEDMODE__
-#include <MixedMode.h>
-#endif
-#ifndef __TRAPS__
-#include <Traps.h>
-#endif
+// mac-sys-utils
+#include "mac_sys/xpram.hh"
 
-
-#if TARGET_CPU_68K
-
-static asm void ReadXPRam( void* buffer, UInt16 length, UInt16 offset )
-{
-	MOVEA.L	4(SP),A0  ;  // load buffer address
-	MOVE.L	8(SP),D0  ;  // load combined length/offset
-	
-	_ReadXPRam
-	
-	RTS
-}
-
-#else
-
-static const short ReadXPRam_68k_code[] =
-{
-	0x206f, 0x0004,  // MOVEA.L 4(SP),A0
-	0x202f, 0x0008,  // MOVE.L 8(SP),D0
-	_ReadXPRam,
-	0x4e75           // RTS
-};
-
-static const UInt32 kReadXPRamProcInfo = kCStackBased
-                                       | STACK_ROUTINE_PARAMETER( 1, SIZE_CODE( sizeof (void*)     ) )
-                                       | STACK_ROUTINE_PARAMETER( 2, SIZE_CODE( sizeof (short) * 2 ) );
-
-static UniversalProcPtr gReadXPRamUPP = NewRoutineDescriptor( (ProcPtr) ReadXPRam_68k_code,
-                                                              kReadXPRamProcInfo,
-                                                              kM68kISA );
-
-inline void ReadXPRam( void* buffer, UInt16 length, UInt16 offset )
-{
-	CallUniversalProc( gReadXPRamUPP, kReadXPRamProcInfo, buffer, (length << 16) | offset );
-}
-
-#endif
 
 namespace Genie
 {
@@ -65,7 +23,7 @@ namespace Genie
 		
 		char* p = xpram.reset( length );
 		
-		::ReadXPRam( p, length, offset );
+		mac::sys::ReadXPRam( p, length, offset );
 		
 		return xpram;
 	}
