jjuran/metamage_1

commit d54843853eabafc508ba7e8c53dc1e0cb60b68fc

Genie, relix-kernel:  Switch to the new scheduler
Install a scheduler that maintains a wait queue and a run queue of tasks
in need of scheduling.  Previously, we relied on the system to implement
round-robin scheduling, which it more or less does, and in particular we
assumed that in between runs of the main thread, busy user threads would
get a chance to run so as to at least set a global flag to indicate that
they were, in fact, busy, and so the application shouldn't sleep for too
long in WaitNextEvent().  For some undetermined reason, this didn't work
with the custom threads we use in System 6 (which does't support Apple's
Thread Manager extension), even though it seems to work well enough with
Thread Manager.  However, the whole business was a hack in any case, and
we needn't deliberate over exactly which circumstances in which the hack
does or doesn't have the desired effect when we can uproot it wholesale.

lamp/Genie/Genie/FS/FSTree_FSSpec.cc
@@ -79,8 +79,12 @@
 #include "MacVFS/util/FSSpec_from_node.hh"
 
 // relix-kernel
+#include "relix/api/current_thread.hh"
 #include "relix/api/root.hh"
 #include "relix/fs/fifo.hh"
+#include "relix/task/scheduler.hh"
+#include "relix/task/task.hh"
+#include "relix/task/thread.hh"
 
 // Genie
 #include "Genie/BinaryImage.hh"
@@ -1130,6 +1134,7 @@ namespace Genie
 	
 	struct IterateIntoCache_CInfoPBRec
 	{
+		relix::task*   task;
 		CInfoPBRec     cInfo;
 		NameAndID      items[ kMaxItems ];
 		UInt16         n_items;
@@ -1180,6 +1185,8 @@ namespace Genie
 		done:
 			
 			pb.done = true;
+			
+			async_resume_task( pb.task );
 			
 			mac::sys::request_async_wakeup();
 		}
@@ -1266,6 +1273,7 @@ namespace Genie
 		
 		IterateIntoCache_CInfoPBRec pb;
 		
+		pb.task  = relix::current_thread().get_task();
 		pb.cInfo = extra.cinfo;
 		
 		IterateFilesIntoCache( pb, cache );

lamp/Genie/Genie/App.cc
@@ -25,6 +25,9 @@
 #include "mac_config/apple-events.hh"
 #include "mac_config/upp-macros.hh"
 
+// cthread-either
+#include "cthread-either.hh"
+
 // plus
 #include "plus/string.hh"
 
@@ -36,6 +39,7 @@
 // relix-kernel
 #include "relix/api/os_thread_api.hh"
 #include "relix/task/scheduler.hh"
+#include "relix/task/thread.hh"
 
 // Genie
 #include "AEFramework/AEFramework.h"
@@ -49,6 +53,30 @@ namespace Ped = Pedestal;
 
 namespace Genie
 {
+	
+	static bool in_user_thread;
+	
+	static
+	cthread::thread_id os_thread_scheduler()
+	{
+		using namespace relix;
+		
+		in_user_thread = ! in_user_thread;
+		
+		if ( ! in_user_thread )
+		{
+			return cthread::thread_id();
+		}
+		
+		if ( queue_element* next = schedule_next_task() )
+		{
+			thread* next_thread = thread::from_queue_element( next );
+			
+			return next_thread->get_os_thread();
+		}
+		
+		return cthread::thread_id();
+	}
 	
 	static
 	pascal OSErr Handle_Reply_event( AppleEvent const*  event,
@@ -94,6 +122,8 @@ namespace Genie
 		Ped::gThreadYield_Hook  = &relix::os_thread_yield;
 		Ped::gActivelyBusy_Hook = &relix::is_active;
 		Ped::gReadyToExit_Hook  = &is_ready_to_exit;
+		
+		cthread::set_thread_scheduler( &os_thread_scheduler );
 		
 		SetCommandHandler( Ped::kCmdAbout, &About       );
 		SetCommandHandler( Ped::kCmdNew,   &NewDocument );

lamp/Genie/Genie/Kernel/AESendBlocking.cc
@@ -20,6 +20,7 @@
 #include "relix/api/current_thread.hh"
 #include "relix/signal/check_signals.hh"
 #include "relix/signal/signal.hh"
+#include "relix/task/scheduler.hh"
 #include "relix/task/thread.hh"
 
 // Genie
@@ -61,6 +62,8 @@ OSStatus AESendBlocking( const AppleEvent* appleEventPtr, AppleEvent* replyPtr )
 		
 		thread& current = current_thread();
 		
+		suspend_task( current.get_task() );
+		
 	stop_and_wait:
 		
 		current.mark_current_stack_frame();
@@ -84,6 +87,8 @@ OSStatus AESendBlocking( const AppleEvent* appleEventPtr, AppleEvent* replyPtr )
 		{
 			goto stop_and_wait;
 		}
+		
+		resume_task( current.get_task() );
 	}
 	catch ( const relix::signal& sig )
 	{

lamp/Genie/Genie/Process.cc
@@ -291,8 +291,6 @@ namespace Genie
 	{
 		Process* process = reinterpret_cast< Process* >( param );
 		
-		relix::mark_thread_inactive( process->gettid() );
-		
 		relix::process_image& image = process->get_process().get_process_image();
 		
 		_relix_user_parameter_block& pb = image.param_block();
@@ -557,7 +555,7 @@ namespace Genie
 		
 	#ifdef __RELIX__
 		
-		relix::mark_thread_active( tid );
+		create_task( this );
 		
 	#endif
 	}
@@ -569,6 +567,8 @@ namespace Genie
 		child.get_process().unshare_per_fork();
 		
 		// suspend parent for vfork
+		
+		suspend_task( this );
 		
 		itsForkedChildPID = child.GetPID();
 		
@@ -738,8 +738,6 @@ namespace Genie
 		
 		itsLifeStage       = kProcessLive;
 		
-		relix::mark_thread_active( gettid() );
-		
 		if ( gCurrentProcess != this )
 		{
 			return;
@@ -747,6 +745,7 @@ namespace Genie
 		
 		if ( its_vfork_parent )
 		{
+			resume_task  ( its_vfork_parent );
 			resume.enable( its_vfork_parent );
 			
 			its_vfork_parent = NULL;
@@ -783,6 +782,7 @@ namespace Genie
 		
 		if ( its_vfork_parent )
 		{
+			resume_task  ( its_vfork_parent );
 			resume.enable( its_vfork_parent );
 			
 			its_vfork_parent = NULL;
@@ -807,8 +807,6 @@ namespace Genie
 		swap_os_thread( looseThread );
 		
 		itsLifeStage       = kProcessLive;
-		
-		relix::mark_thread_active( gettid() );
 		
 		return looseThread;
 	}
@@ -860,7 +858,7 @@ namespace Genie
 			}
 		}
 		
-		if ( relix::is_thread_active( gettid() ) )
+		if ( is_on_run_queue( this ) )
 		{
 			return 'Q';
 		}
@@ -1017,6 +1015,8 @@ namespace Genie
 			
 			Process& parent = GetProcess( ppid );
 			
+			resume_task( &parent );
+			
 			if ( ppid > 1  &&  waits_for_children( parent.get_process() ) )
 			{
 				parent.Raise( SIGCHLD );
@@ -1045,7 +1045,7 @@ namespace Genie
 			the thread active again.
 		*/
 		
-		relix::mark_thread_inactive( gettid() );
+		remove_task( this );
 		
 		reset_os_thread();
 		

relix/kernel/relix/api/thread_yield.cc
@@ -51,7 +51,11 @@ namespace relix
 		{
 			current.mark_current_stack_frame();
 			
+			suspend_task( current.get_task() );
+			
 			os_thread_yield();
+			
+			resume_task( current.get_task() );
 		}
 		while ( stopped( current ) );
 	}
@@ -62,8 +66,6 @@ namespace relix
 		
 		const pid_t tid = current.id();
 		
-		mark_thread_active( tid );
-		
 		do
 		{
 			current.mark_current_stack_frame();
@@ -71,8 +73,6 @@ namespace relix
 			os_thread_yield();
 		}
 		while ( stopped( current ) );
-		
-		mark_thread_inactive( tid );
 	}
 	
 }

relix/kernel/relix/task/scheduler.cc
@@ -5,9 +5,6 @@
 
 #include "relix/task/scheduler.hh"
 
-// Standard C++
-#include <set>
-
 // must
 #include "must/pthread.h"
 
@@ -22,8 +19,6 @@
 
 namespace relix
 {
-	
-	static std::set< pid_t > active_threads;
 	
 	static bool pthread_yield_needed;
 	
@@ -162,27 +157,17 @@ namespace relix
 			pthread_yield();
 		}
 		
-		return !active_threads.empty();
+		if ( resumptions_pending )
+		{
+			resume_pending_tasks();
+		}
+		
+		return ! run_queue.empty();
 	}
 	
 	bool is_on_run_queue( const task* that )
 	{
 		return that  &&  that->from == &run_queue;
-	}
-	
-	bool is_thread_active( pid_t tid )
-	{
-		return active_threads.find( tid ) != active_threads.end();
-	}
-	
-	void mark_thread_active( pid_t tid )
-	{
-		active_threads.insert( tid );
-	}
-	
-	void mark_thread_inactive( pid_t tid )
-	{
-		active_threads.erase( tid );
 	}
 	
 }

relix/kernel/relix/task/scheduler.hh
@@ -51,12 +51,6 @@ namespace relix
 	
 	bool is_on_run_queue( const task* that );
 	
-	bool is_thread_active( pid_t tid );
-	
-	void mark_thread_active( pid_t tid );
-	
-	void mark_thread_inactive( pid_t tid );
-	
 	
 	class synchronized_scope
 	{

commit 4c2be941d82cf83ce9eb9f4297b2d1bb6110717c

Genie:  Fix async-dooming extended CInfoPBRec cast
Place the cInfo field first in IterateIntoCache_CInfoPBRec, so that when
we cast the completion routine's CInfoPBRec* parameter to its containing
struct type, we get a valid address.  This fixes a regression that broke
async directory listings since d54843853eabafc508ba7e8c53dc1e0cb60b68fc.

lamp/Genie/Genie/FS/FSTree_FSSpec.cc
@@ -1184,8 +1184,8 @@ namespace Genie
 	
 	struct IterateIntoCache_CInfoPBRec
 	{
-		relix::task*   task;
 		CInfoPBRec     cInfo;
+		relix::task*   task;
 		NameAndID      items[ kMaxItems ];
 		UInt16         n_items;
 		volatile bool  done;