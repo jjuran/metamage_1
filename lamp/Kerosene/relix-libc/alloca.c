jjuran/metamage_1

commit a3dc46a8cde9d11b4ac86696c2ec6df6a214e6fd

libc:  Replace buggy 68K alloca() implementation
Given an odd size, the previous implementation would briefly load an odd
address into the stack pointer.  In Supervisor mode, this is very likely
to go horribly wrong (though only in rare circumstances) -- for example,
executing __alloca() in Trace mode on a 68000 will yield a double fault.

Additionally (no pun intended), the SUBA instruction defaults to SUBA.W,
which works fine for a size within 0K -> 32K and disastrously otherwise.

lamp/Kerosene/relix-libc/alloca.c
@@ -1,0 +1,44 @@
+/*
+	alloca.c
+	--------
+	
+	Implementation of alloca() in 68K assembly language
+	
+	Copyright 2023, Joshua Juran.  All rights reserved.
+	
+	License:  AGPLv3+ (see bottom for legal boilerplate)
+	
+*/
+
+#ifdef __MC68K__
+
+#include <alloca.h>
+
+
+asm
+void* __relix_alloca( unsigned long size : __D0 )
+{
+	MOVEA.L  (SP)+,A1  // pop the return address
+	NEG.L    D0        // reverse the polarity to match the stack direction
+	AND.B    #~3,D0    // round up to a four-byte boundary
+	ADDA.L   D0,SP     // add the alloc size to the stack
+	MOVEA.L  SP,A0     // return the address of the new allocation
+	JMP      (A1)      // jump to the return address
+}
+
+#endif
+
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/

lamp/POSIX-headers/alloca.h
@@ -1,0 +1,54 @@
+/*
+	alloca.h
+	--------
+	
+	Interface to alloca()
+	
+	Copyright 2023, Joshua Juran.  All rights reserved.
+	
+	License:  AGPLv3+ (see bottom for legal boilerplate)
+	
+	To use the provided 68K implementation of alloca(), this header
+	must be included rather than the one in CWCIncludes.  (Including
+	the wrong header will result in a link error.)
+	
+	The distinct name `__relix_alloca()` is used because the signature
+	is different, with the result in A0 rather than D0.  Despite the
+	name, it's functional in any context, not just MacRelix.
+	
+	For PPC, the <alloca.h> headers are interchangeable and it doesn't
+	matter which one is included.  (MWCPPC has __alloca() built-in.)
+	
+*/
+
+#ifndef _ALLOCA_H_
+#define _ALLOCA_H_
+
+
+#ifdef __MC68K__
+
+extern void* __relix_alloca( unsigned long size : __D0 );
+
+#define __alloca( size )  __relix_alloca( size )
+
+#endif
+
+
+#define alloca( size )  __alloca( size )
+
+#endif
+
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/

contrib/openbsd-libc/alloca.c
@@ -1,76 +1,0 @@
-/* $OpenBSD: alloca.S,v 1.4 2005/08/07 16:40:14 espie Exp $ */
-/*-
- * Copyright (c) 1990 The Regents of the University of California.
- * All rights reserved.
- *
- * This code is derived from software contributed to Berkeley by
- * the Systems Programming Group of the University of Utah Computer
- * Science Department.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the University nor the names of its contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- */
-
-/* like alloc, but automatic free in return */
-
-#ifdef __MC68K__
-
-#ifdef OPENBSD
-
-#include "DEFS.h"
-
-ENTRY(alloca)
-	movl	sp@,a0		/* save return addr */
-	movl	sp,d0		/* get current SP value */
-	subl	sp@(4),d0	/* allocate requested space */
-	andb	#~3,d0		/* longword align for efficiency */
-	addql	#8,d0		/* reuse space of call frame */
-	movl	d0,sp		/* set new SP value */
-	lea	sp@(-4),sp	/* account for argument pop in caller */
-	jmp 	a0@		/* funny return */
-
-#endif  // OPENBSD
-
-// Revision for register-based calling sequence by Joshua Juran, 2009.
-// The following code is derived from the preceding code, and the changes are
-// released into the public domain.
-
-#ifdef __MWERKS__
-
-#include <alloca.h>
-
-asm void* __alloca( size_t size : __d0 ) : __d0
-{
-	MOVEA.L	(SP)+,A0  // save return addr
-	SUBA	D0,SP     // extend the stack by subtracting from SP
-	MOVE.L	SP,D0     // get new SP
-	AND.B	#~3,D0    // longword align for efficiency
-	MOVEA.L	D0,SP     // set new SP value
-	JMP		(A0)      // funny return
-}
-
-#endif  // __MWERKS__
-
-#endif  // __MC68K__
-

commit 9531bfd848976a9da5bb3ca736682e780f77bc52

[DEL] Axe hapless 68K alloca() implementation
It turns out that the name `__alloca()` was load-bearing, and MWC68K was
providing an implementation all along.  It's only the /declaration/ that
MWCPPC provides internally and MWC68K doesn't.  The C compiler has to be
in on the scheme so it can save and restore the SP in the prolog/epilog.

This fixes the regression from a3dc46a8cde9d11b4ac86696c2ec6df6a214e6fd.

lamp/Kerosene/relix-libc/alloca.c
@@ -1,44 +1,0 @@
-/*
-	alloca.c
-	--------
-	
-	Implementation of alloca() in 68K assembly language
-	
-	Copyright 2023, Joshua Juran.  All rights reserved.
-	
-	License:  AGPLv3+ (see bottom for legal boilerplate)
-	
-*/
-
-#ifdef __MC68K__
-
-#include <alloca.h>
-
-
-asm
-void* __relix_alloca( unsigned long size : __D0 )
-{
-	MOVEA.L  (SP)+,A1  // pop the return address
-	NEG.L    D0        // reverse the polarity to match the stack direction
-	AND.B    #~3,D0    // round up to a four-byte boundary
-	ADDA.L   D0,SP     // add the alloc size to the stack
-	MOVEA.L  SP,A0     // return the address of the new allocation
-	JMP      (A1)      // jump to the return address
-}
-
-#endif
-
-/*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/

lamp/POSIX-headers/alloca.h
@@ -1,54 +1,0 @@
-/*
-	alloca.h
-	--------
-	
-	Interface to alloca()
-	
-	Copyright 2023, Joshua Juran.  All rights reserved.
-	
-	License:  AGPLv3+ (see bottom for legal boilerplate)
-	
-	To use the provided 68K implementation of alloca(), this header
-	must be included rather than the one in CWCIncludes.  (Including
-	the wrong header will result in a link error.)
-	
-	The distinct name `__relix_alloca()` is used because the signature
-	is different, with the result in A0 rather than D0.  Despite the
-	name, it's functional in any context, not just MacRelix.
-	
-	For PPC, the <alloca.h> headers are interchangeable and it doesn't
-	matter which one is included.  (MWCPPC has __alloca() built-in.)
-	
-*/
-
-#ifndef _ALLOCA_H_
-#define _ALLOCA_H_
-
-
-#ifdef __MC68K__
-
-extern void* __relix_alloca( unsigned long size : __D0 );
-
-#define __alloca( size )  __relix_alloca( size )
-
-#endif
-
-
-#define alloca( size )  __alloca( size )
-
-#endif
-
-/*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/