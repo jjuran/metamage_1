67d4add4335a834f66cb36b9feabb96f3f14442a
Josh Juran <jjuran@metamage.com>
2023-07-23 19:13:20 -0400

    NyanochromeCat:  Update copyright and add version

--- apps/mac/NyanochromeCat/Rez/NYAN.vers.r
+++ apps/mac/NyanochromeCat/Rez/NYAN.vers.r
@@ -1,4 +1,4 @@
-#define COPYRIGHT  "2017-2018 Josh Juran"
+#define COPYRIGHT  "2017-2023 Josh Juran"
 
 #define COPY "\0xA9"
 
@@ -7,4 +7,14 @@ type 'NYAN' as 'STR ';
 resource 'NYAN' (0, "Nyanochrome Cat")
 {
 	"Copyright " COPY " " COPYRIGHT
+};
+
+resource 'vers' (1) {
+	0x00,
+	0x80,
+	release,
+	0,
+	smRoman,
+	"0.8.0",
+	"0.8.0, " COPY " " COPYRIGHT
 };
