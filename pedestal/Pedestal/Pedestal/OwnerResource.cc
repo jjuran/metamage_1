2d4db1c0727fc7e6c6d9a939e685e0c249de6023
Josh Juran <jjuran@metamage.com>
2023-07-28 01:03:03 -0400

    Pedestal:  Axe now-unused resource-getting code

--- pedestal/Pedestal/Pedestal/OwnerResource.cc
+++ pedestal/Pedestal/Pedestal/OwnerResource.cc
@@ -10,20 +10,8 @@
 #include <Resources.h>
 #endif
 
-// more-libc
-#include "more/string.h"
-
-// mac-glue-utils
-#include "mac_glue/Memory.hh"
-
 
 #pragma exceptions off
-
-
-#define STR_LEN( s )  "" s, (sizeof s - 1)
-
-#define TRUNCATED  "TRUNCATED 'STR ' RESOURCE"
-#define EMPTY      "EMPTY 'STR ' RESOURCE"
 
 
 namespace Pedestal
@@ -48,40 +36,6 @@ namespace Pedestal
 		if ( Handle h = GetResource( creator, 0 ) )
 		{
 			GetResInfo( h, NULL, NULL, name );
-			
-			ReleaseResource( h );
-			
-			return true;
-		}
-		
-		return false;
-	}
-	
-	bool GetOwnerResourceData( OSType creator, Str255 data )
-	{
-		if ( Handle h = GetResource( creator, 0 ) )
-		{
-			const Size physical_size = mac::glue::GetHandleSize_raw( h );
-			
-			if ( physical_size > 0 )
-			{
-				const unsigned char* text = (unsigned char*) *h;
-				
-				const UInt16 logical_size = 1 + text[ 0 ];
-				
-				if ( logical_size <= physical_size )
-				{
-					mempcpy( data, text, logical_size );
-				}
-				else
-				{
-					mempcpy( data, "\p" STR_LEN( TRUNCATED ) + 1 );
-				}
-			}
-			else
-			{
-				mempcpy( data, "\p" STR_LEN( EMPTY ) + 1 );
-			}
 			
 			ReleaseResource( h );
 			
