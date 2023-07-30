14e3ca95bf775dfd79d78aa4aeb3b9748ea32946
Josh Juran <jjuran@metamage.com>
2023-07-30 04:06:43 -0400

    NyanochromeCat:  Raise version to 1.0.1

--- apps/mac/NyanochromeCat/Rez/NYAN.vers.r
+++ apps/mac/NyanochromeCat/Rez/NYAN.vers.r
@@ -11,12 +11,12 @@ resource 'NYAN' (0, "Nyanochrome Cat")
 
 resource 'vers' (1) {
 	0x01,
-	0x00,
+	0x01,
 	release,
 	0,
 	smRoman,
-	"1.0",
-	"1.0, " COPY " " COPYRIGHT
+	"1.0.1",
+	"1.0.1, " COPY " " COPYRIGHT
 };
 
 resource 'STR#' (128) {{
