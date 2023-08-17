0fc66c63f88cd514d52845550c199cfc89253acf
Josh Juran <jjuran@metamage.com>
2023-08-17 05:56:29 -0400

    Nostalgia:  Include "missing/Quickdraw.h"

--- mac/compat/Nostalgia/Nostalgia/MacWindows.hh
+++ mac/compat/Nostalgia/Nostalgia/MacWindows.hh
@@ -15,6 +15,15 @@
 #include <MacWindows.h>
 #endif
 
+// missing-macos
+#if ! __LP64__
+#ifdef MAC_OS_X_VERSION_10_7
+#ifndef MISSING_QUICKDRAW_H
+#include "missing/Quickdraw.h"
+#endif
+#endif
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
