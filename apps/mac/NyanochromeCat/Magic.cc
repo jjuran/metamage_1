32020210fb38a81db1f47d8b1ed494810aca868b
Josh Juran <jjuran@metamage.com>
2023-08-22 05:54:53 -0400

    NyanochromeCat:  Manage A5 in StillDown head patch

--- apps/mac/NyanochromeCat/Magic.cc
+++ apps/mac/NyanochromeCat/Magic.cc
@@ -28,7 +28,9 @@ enum
 StillDownPatchUPP old_StillDown;
 
 static
+#ifndef __MC68K__
 inline
+#endif
 void StillDown_payload()
 {
 	if ( animation_timer.pulse() )
@@ -48,6 +50,32 @@ void StillDown_payload()
 
 #if CALL_NOT_IN_CARBON
 
+#ifdef __MC68K__
+
+/*
+	Our StillDown() patch gets called with a different value in A5
+	than when we call DragWindow(), so we have to save/set/restore it.
+	
+	(This hasn't been an issue with TrackGoAway().)
+*/
+
+static
+asm
+pascal Boolean StillDown_patch()
+{
+	MOVE.L   A5,-(SP)
+	MOVEA.L  0x0904,A5           // CurrentA5
+	
+	JSR      StillDown_payload
+	
+	MOVEA.L  old_StillDown,A0    // read A5-based global with our A5
+	MOVEA.L  (SP)+,A5
+	
+	JMP      (A0)
+}
+
+#else  // #ifdef __MC68K__
+
 static
 pascal Boolean StillDown_patch()
 {
@@ -57,6 +85,8 @@ pascal Boolean StillDown_patch()
 }
 
 DEFINE_UPP( StillDownPatch, StillDown_patch )
+
+#endif  // #ifdef __MC68K__
 
 using mac::sys::get_trap_address;
 using mac::sys::set_trap_address;
