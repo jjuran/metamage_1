jjuran/metamage_1

commit a0289d4bbe7b418f6c0dff1922ff7c2a23cacee3

mac-app-utils:  Add color-QD-checking new_window()

mac/libs/mac-app-utils/mac_app/new_window.cc
@@ -1,0 +1,57 @@
+/*
+	new_window.hh
+	-------------
+*/
+
+#include "mac_app/new_window.hh"
+
+// Mac OS
+#ifdef __MC68K__
+#ifndef __SEGLOAD__
+#include <SegLoad.h>
+#endif
+#endif
+
+// mac-sys-utils
+#if TARGET_CPU_68K
+#include "mac_sys/trap_address.hh"
+#endif
+
+
+#pragma exceptions off
+
+
+namespace mac {
+namespace app {
+
+#if TARGET_CPU_68K  &&  ! TARGET_RT_MAC_CFM
+
+using mac::sys::get_trap_address;
+
+enum
+{
+	_Unimplemented = 0xA89F,
+	_NewWindow     = 0xA913,
+	_NewCWindow    = 0xAA45,
+};
+
+static inline
+ProcPtr get_NewWindow_address()
+{
+	const ProcPtr unimplemented = get_trap_address( _Unimplemented );
+	const ProcPtr newCWindow    = get_trap_address( _NewCWindow );
+	
+	return newCWindow != unimplemented ? newCWindow
+	                                   : get_trap_address( _NewWindow );
+}
+
+const ProcPtr gNewWindow = get_NewWindow_address();
+
+#else  // #if TARGET_CPU_68K  &&  ! TARGET_RT_MAC_CFM
+
+int dummy;
+
+#endif
+
+}
+}

mac/libs/mac-app-utils/mac_app/new_window.hh
@@ -1,0 +1,72 @@
+/*
+	new_window.hh
+	-------------
+*/
+
+#ifndef MACAPP_DOCUMENTS_HH
+#define MACAPP_DOCUMENTS_HH
+
+// Mac OS X
+#ifdef __APPLE__
+#include <Carbon/Carbon.h>
+#endif
+
+// Mac OS
+#ifndef __MACWINDOWS__
+#include <MacWindows.h>
+#endif
+
+
+#if ! __LP64__
+
+namespace mac {
+namespace app {
+	
+	extern const ProcPtr gNewWindow;
+	
+	inline
+	pascal
+#if TARGET_CPU_68K  &&  ! TARGET_RT_MAC_CFM
+	asm
+#endif
+	WindowRef new_window( void*             storage,
+	                      const Rect*       bounds,
+	                      ConstStr255Param  title,
+	                      Boolean           visible,
+	                      short             procID,
+	                      WindowRef         predecessor,
+	                      Boolean           closable,
+	                      long              refcon )
+	{
+	#if TARGET_CPU_68K  &&  ! TARGET_RT_MAC_CFM
+		
+		MOVEA.L   gNewWindow,A0
+		JSR       (A0)
+		
+	#else
+		
+		return NewCWindow( storage, bounds, title,
+		                   visible, procID, predecessor, closable, refcon );
+		
+	#endif
+	}
+	
+	inline
+	WindowRef new_window( const Rect&       bounds,
+	                      ConstStr255Param  title,
+	                      Boolean           visible,
+	                      short             procID,
+	                      WindowRef         predecessor,
+	                      Boolean           closable,
+	                      long              refcon = 0 )
+	{
+		return new_window( NULL, &bounds, title, visible,
+		                   procID, predecessor, closable, refcon );
+	}
+	
+}
+}
+
+#endif  // #if ! __LP64__
+
+#endif

commit f2b2463f2aebe57fdba0bf814cc9c5fbf3bf4513

mac-app-utils:  Fix broken _NewCWindow trap check
This fixes the regression from a0289d4bbe7b418f6c0dff1922ff7c2a23cacee3.

mac/libs/mac-app-utils/mac_app/new_window.cc
@@ -40,9 +40,14 @@ ProcPtr get_NewWindow_address()
 {
 	const ProcPtr unimplemented = get_trap_address( _Unimplemented );
 	const ProcPtr newCWindow    = get_trap_address( _NewCWindow );
+	const ProcPtr counterpart   = get_trap_address( _NewCWindow ^ 0x0200 );
 	
-	return newCWindow != unimplemented ? newCWindow
-	                                   : get_trap_address( _NewWindow );
+	if ( newCWindow != counterpart  &&  newCWindow != unimplemented )
+	{
+		return newCWindow;
+	}
+	
+	return get_trap_address( _NewWindow );
 }
 
 const ProcPtr gNewWindow = get_NewWindow_address();