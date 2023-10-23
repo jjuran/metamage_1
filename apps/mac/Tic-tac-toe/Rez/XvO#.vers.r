5738d258988091d17fbd09d4456c93add9d2d0fa
Josh Juran <jjuran@metamage.com>
2023-10-22 21:32:22 -0400

    Tic-tac-toe:  Set version to 0.2

--- apps/mac/Tic-tac-toe/Rez/XvO#.vers.r
+++ apps/mac/Tic-tac-toe/Rez/XvO#.vers.r
@@ -1,10 +1,27 @@
-#define COPYRIGHT  "2018 Josh Juran"
+#define COPYRIGHT  "2018-2024 Josh Juran"
 
 #define COPY "\0xA9"
+
+#define MAJOR 0
+#define MINOR 2
+#define TWEAK 0
+
+#define VERSION  "0.2"
 
 type 'XvO#' as 'STR ';
 
 resource 'XvO#' (0, "Tic-tac-toe")
 {
 	"Copyright " COPY " " COPYRIGHT
+};
+
+resource 'vers' (1)
+{
+	MAJOR,
+	MINOR << 4 | TWEAK,
+	release,
+	0,
+	smRoman,
+	VERSION,
+	VERSION ", " COPY COPYRIGHT
 };
