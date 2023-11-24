9bb99abe161d77952e3ba2701adb23cb455d9d95
Josh Juran <jjuran@metamage.com>
2023-11-23 21:55:37 -0500

    ams-common:  Make fixmulu_w() extern, not inline
    This makes the call sites and overall executable size smaller.

--- 68k/ams/common/math.hh
+++ 68k/ams/common/math.hh
@@ -30,17 +30,7 @@ unsigned short muldivu_w( unsigned short a : __D0,
 	DIVU.W   D2,D0
 }
 
-inline
-asm
 unsigned long fixmulu_w( unsigned short a : __D0,
-                         unsigned long  b : __D1 )
-{
-	MOVE.W   D0,D2    // a
-	MULU.W   D1,D2    // a * b.lo
-	SWAP     D2       // (a * b.lo) >> 16
-	SWAP     D1       // b.hi
-	MULU.W   D1,D0    // a * b.hi
-	ADD.L    D2,D0    // a * b.hi + (a * b.lo >> 16)
-}
+                         unsigned long  b : __D1 );
 
 #endif
