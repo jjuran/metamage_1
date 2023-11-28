49421396a3a9ff65928fe2256349bce33e01a115
Josh Juran <jjuran@metamage.com>
2023-11-28 03:41:11 -0500

    ams-common:  Fix fixmulu_w() not to return garbage
    The sixteen bits to be shifted off the bottom were instead getting added
    to the high word.

--- 68k/ams/common/math.cc
+++ 68k/ams/common/math.cc
@@ -12,6 +12,7 @@ unsigned long fixmulu_w( unsigned short a : __D0,
 {
 	MOVE.W   D0,D2    // a
 	MULU.W   D1,D2    // a * b.lo
+	CLR.W    D2
 	SWAP     D2       // (a * b.lo) >> 16
 	SWAP     D1       // b.hi
 	MULU.W   D1,D0    // a * b.hi
