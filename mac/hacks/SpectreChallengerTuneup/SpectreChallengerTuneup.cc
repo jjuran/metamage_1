f5c842648b59c6c2e40794aabba6dff5dc529f0b
Josh Juran <jjuran@metamage.com>
2023-12-06 18:18:21 -0500

    SpectreChallengerTuneup:  Prevent clicks in sounds

--- mac/hacks/SpectreChallengerTuneup/SpectreChallengerTuneup.cc
+++ mac/hacks/SpectreChallengerTuneup/SpectreChallengerTuneup.cc
@@ -4,7 +4,7 @@
 	
 	Spectre Challenger Tune-up INIT for Advanced Mac Substitute
 	
-	Copyright 2022, Joshua Juran.  All rights reserved.
+	Copyright 2022-2023, Joshua Juran.  All rights reserved.
 	
 	License:  AGPLv3+ (see bottom for legal boilerplate)
 	
@@ -13,6 +13,16 @@
 	below.  This extension patches the convoluted code into a simpler form
 	that's semantically equivalent, and can be detected automatically at
 	run time by xv68k and replaced with more efficient trap-based code.
+	
+	Additionally, Spectre Challenger's Sound Driver client code is faulty:
+	It sends ffMode requests that include a 14-byte Sound Manager header
+	between the 6-byte Sound Driver header and the actual audio samples,
+	resulting in audible clicks when playing a sound (due to interpreting
+	the Sound Manager header as audio samples).  When Spectre Challenger
+	launches, this extension patches the _Write trap to intercept calls
+	to the Sound Driver, temporarily rewriting the corrupted buffer and
+	the I/O parameter block to remove the bogus bytes.  (Restoring them
+	afterward may be unnecessary, but it's best to patch conservatively.)
 	
 */
 
@@ -34,6 +44,12 @@
 #pragma exceptions off
 
 
+enum
+{
+	kSoundDriverRefNum = -4,
+};
+
+static UniversalProcPtr  old_Write;
 static UniversalProcPtr  old_TEInit;
 
 static const UInt16 old_spinloop[] =
@@ -50,9 +66,81 @@ static const UInt16 new_spinloop[] =
 	0x4e71,          // 003730:  NOP
 };
 
+static const UInt16 corrupt_header[] =
+{
+	0x0000,          // ffMode
+	0x0001, 0x0000,  // rate
+	0x56ee, 0x8ba3,  // 22254.[54] as a Fixed
+	0x0000,
+};
+
+static inline
+bool corrupted( const void* buffer )
+{
+	return fast_memcmp( buffer, corrupt_header, sizeof corrupt_header ) == 0;
+}
+
+static inline
+asm
+short call_IO_handler( void* proc : __A1, IOParam* pb : __A0 )
+{
+	JSR      (A1)
+}
+
+static
+short Write_to_Sound_Driver( IOParam* pb : __A0 )
+{
+	Ptr buffer = pb->ioBuffer;
+	UInt32 req = pb->ioReqCount;
+	
+	UInt16 saved_words[ 3 ];
+	
+	Ptr new_start = buffer + 14;
+	
+	fast_memcpy( saved_words, new_start, 6 );
+	
+	if ( corrupted( buffer ) )
+	{
+		fast_memcpy( new_start, buffer, 6 );
+		
+		pb->ioBuffer = new_start;
+		pb->ioReqCount -= 14;
+	}
+	
+	OSErr err = call_IO_handler( old_Write, pb );
+	
+	fast_memcpy( new_start, saved_words, 6 );
+	
+	pb->ioBuffer   = buffer;
+	pb->ioReqCount = req;
+	
+	return err;
+}
+
+static
+asm
+short Write_patch( IOParam* pb : __A0 )
+{
+	CMPI.W   #kSoundDriverRefNum,24(A0)
+	BNE.S    not_Sound
+	
+	JMP      Write_to_Sound_Driver
+	
+not_Sound:
+	
+	// call previous Write handler
+	MOVE.L   old_Write,-(SP)
+	
+	RTS
+}
+
 static inline
 void install_patches( Handle h )
 {
+	old_Write = mac::sys::get_trap_address( _Write );
+	
+	mac::sys::set_trap_address( (ProcPtr) Write_patch, _Write );
+	
 	const int query_offset = 0x00372a;
 	
 	if ( h  &&  GetHandleSize( h ) >= query_offset + sizeof old_spinloop )
