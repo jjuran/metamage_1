jjuran/metamage_1

commit 4658873e660995faae0c1c0c0c9b2fe213050c4b

ams-core:  Set CrsrVis earlier to ease next patch

68k/modules/ams-core/cursor-core.cc
@@ -234,13 +234,14 @@ void hide_cursor()
 	
 	if ( CrsrVis )
 	{
+		CrsrVis = false;
+		
 		--CrsrBusy;
 		
 		erase_cursor();
 		
 		set_empty_rect( &CrsrRect );
 		CrsrAddr = NULL;
-		CrsrVis = false;
 		
 		++CrsrBusy;
 	}
@@ -253,9 +254,9 @@ void show_cursor()
 		return;
 	}
 	
-	--CrsrBusy;
-	
 	CrsrVis = true;
+	
+	--CrsrBusy;
 	
 	paint_cursor( Mouse.h, Mouse.v );
 	

commit d7bc062f9b34334e43466ca628c2181024fea9f4

ams-core:  Fix race condition in show_cursor()
If show_cursor() is interrupted after setting CrsrVis to true and before
decrementing CrsrBusy (thereby taking the lock), an update_cursor() call
will paint a cursor image, which then becomes part of the bits under the
cursor saved by show_cursor()'s own call to paint_cursor() and persists.

This fixes the regression from 4658873e660995faae0c1c0c0c9b2fe213050c4b.

68k/modules/ams-core/cursor-core.cc
@@ -265,15 +265,17 @@ void show_cursor()
 		return;
 	}
 	
-	CrsrVis = true;
-	
 	if ( hardware_cursor )
 	{
+		CrsrVis = true;
+		
 		notify_cursor_vis( true );
 		return;
 	}
 	
 	--CrsrBusy;
+	
+	CrsrVis = true;
 	
 	paint_cursor( Mouse.h, Mouse.v );
 	