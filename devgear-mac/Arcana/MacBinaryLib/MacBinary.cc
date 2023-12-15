jjuran/metamage_1

commit f9a196114bdb738a516027a6899d08507b200113

MacBinaryLib:  Use mac::file::list_directory()

devgear-mac/Arcana/MacBinaryLib/MacBinary.cc
@@ -38,6 +38,7 @@
 // mac-file-utils
 #include "mac_file/desktop.hh"
 #include "mac_file/directory.hh"
+#include "mac_file/listing.hh"
 
 // Debug
 #include "debug/assert.hh"
@@ -46,8 +47,8 @@
 #include "Nitrogen/Files.hh"
 #include "Nitrogen/MacMemory.hh"
 
-// FSContents
-#include "FSContents.h"
+// Io: MacFiles
+#include "MacFiles/Classic.hh"
 
 
 /*
@@ -631,21 +632,31 @@ namespace MacBinary
 	static
 	void EncodeFolder( const CInfoPBRec& cInfo, BlockWriter blockWrite, int output )
 	{
+		using mac::file::directory_listing;
+		using mac::file::list_entry;
+		
 		HeaderBlock u;
 		
 		MakeFolderHeader( cInfo.hFileInfo, u.h );
 		
 		blockWrite( output, u.block.data, sizeof u.block );
 		
-		N::FSDirSpec dir = n::make< N::FSDirSpec >( cInfo );
+		short vRefNum = cInfo.dirInfo.ioVRefNum;
+		long  dirID   = cInfo.dirInfo.ioDrDirID;
 		
-		N::FSSpecContents_Container contents = N::FSContents( dir );
+		directory_listing listing;
 		
-		typedef N::FSSpecContents_Container::const_iterator const_iterator;
+		OSErr err = list_directory( listing, vRefNum, dirID );
 		
-		for ( const_iterator it = contents.begin();  it != contents.end();  ++it )
+		Mac::ThrowOSStatus( err );
+		
+		unsigned n = listing.count();
+		
+		for ( unsigned i = 0;  i < n;  ++i )
 		{
-			Encode( dir / *it, blockWrite, output );
+			const list_entry& entry = listing.get_nth( i );
+			
+			Encode( entry.cInfo, blockWrite, output );
 		}
 		
 		u.h.Set< kFileCreator >( 0xFFFFFFFE );

devgear-mac/Arcana/MacBinaryLib/A-line.conf
@@ -1,4 +1,4 @@
 product lib
 
-use FSContents
 use mac-file-utils
+use MacFiles

commit 9a629a6c761e9f9376ac605bed731c002b1ae0f0

MacBinaryLib:  Fix encode of nested directories
Our preemptive clearing of ioNamePtr in order to prevent errors that are
challenging to reproduce and fiendishly difficult to debug led, in part,
to an error that's 100% reproducible and trivial to debug -- not too bad
a tradeoff, methinks.  Copy the list entry to the stack so it can't move
in memory, and repopulate the ioNamePtr field (pointing into the stack).

This fixes the regression from f9a196114bdb738a516027a6899d08507b200113.

devgear-mac/Arcana/MacBinaryLib/MacBinary.cc
@@ -654,7 +654,9 @@ namespace MacBinary
 		
 		for ( unsigned i = 0;  i < n;  ++i )
 		{
-			const list_entry& entry = listing.get_nth( i );
+			list_entry entry = listing.get_nth( i );
+			
+			entry.cInfo.hFileInfo.ioNamePtr = entry.name;
 			
 			Encode( entry.cInfo, blockWrite, output );
 		}