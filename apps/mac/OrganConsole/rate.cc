b7c07cce6581eca4c452f004a6457308c4cc7764
Josh Juran <jjuran@metamage.com>
2023-12-18 07:46:51 -0500

    OrganConsole:  Recode rate_from_midi_note() in asm
    68K asm, that is.  This is less for speed than for code size -- this way
    we avoid linking 64-bit math routines into the application's executable.

--- apps/mac/OrganConsole/rate.cc
+++ apps/mac/OrganConsole/rate.cc
@@ -6,7 +6,12 @@
 #include "rate.hh"
 
 // Standard C
+#ifndef __MC68K__
 #include <stdint.h>
+#endif
+
+// math
+#include "math/integer.hh"
 
 
 enum
@@ -27,6 +32,75 @@ enum
 	
 	fixed_12th_root = 69433,
 };
+
+#ifdef __MC68K__
+
+using math::integer::long_multiply;
+
+asm
+FTRate rate_from_midi_note( MIDI_note midi_note : __D0 )
+{
+	/*
+		Save D3 so we can use it as a loop counter.
+		
+		Also allocate two longwords at the top of the stack
+		(for long_multiply()'s result) by "saving" D1 and D2.
+	*/
+	
+	LINK     A6,#0
+	MOVEM.L  D1-D3,-(SP)
+	
+	/*
+		Divide the MIDI note value into octave (low word) and key (high word).
+		Then swap the words so that the key (the number of half steps above C)
+		is in the low word.
+	*/
+	
+	MOVE.L   D0,D3
+	MOVEQ    #12,D0
+	DIVU.W   D0,D3
+	SWAP     D3
+	
+	MOVE.L   #C7_FTRate,D0
+	BRA.S    loop_entry
+	
+	/*
+		For each half-step above C, multiply by the 12th root of 2.
+		Since it's a 16.16 fixed-point value, we need the result shifted
+		right by 16 bits.  Since the result is in memory and always fits
+		in 32 bits, we can read bits 16-47 (as a longword) back into D0.
+	*/
+	
+loop:
+	MOVEA.L  SP,A0
+	MOVE.L   #fixed_12th_root,D1
+	
+	JSR      long_multiply
+	
+	MOVE.L   2(SP),D0
+	
+loop_entry:
+	DBRA.S   D3,loop
+	
+	/*
+		Return the octave number to the low word of D3.
+		
+		Subtract the octave number from eight (not seven, because it's
+		biased by one) to get the octave difference, and shift the rate
+		right by that many bits in order to drop that number of octaves.
+	*/
+	
+	SWAP     D3
+	MOVEQ    #8,D1
+	SUB.W    D3,D1
+	LSR.L    D1,D0
+	
+	MOVEM.L  (SP)+,D1-D3
+	UNLK     A6
+	RTS
+}
+
+#else
 
 FTRate rate_from_midi_note( MIDI_note midi_note )
 {
@@ -50,3 +124,5 @@ FTRate rate_from_midi_note( MIDI_note midi_note )
 	
 	return (uint32_t) rate >> (8 - octave1);
 }
+
+#endif
--- apps/mac/OrganConsole/rate.hh
+++ apps/mac/OrganConsole/rate.hh
@@ -11,6 +11,10 @@ typedef unsigned FTRate;
 
 typedef int MIDI_note;
 
+#ifdef __MC68K__
+#pragma parameter rate_from_midi_note( __D0 )
+#endif
+
 FTRate rate_from_midi_note( MIDI_note midi_note );
 
 #endif
