3abcaf89746b4eac46ce3e88a7e46f02c9bf7a4b
Josh Juran <jjuran@metamage.com>
2023-12-30 10:01:29 -0500

    vlib:  Use a table to get Iter namespace members

--- v/vlib/vlib/namespaces/Iter.cc
+++ v/vlib/vlib/namespaces/Iter.cc
@@ -16,6 +16,8 @@
 #include "vlib/types/proc.hh"
 
 
+#define LENGTH(array)  (sizeof (array) / sizeof *(array))
+
 #define QUINE( name )  #name, &name
 
 
@@ -39,27 +41,36 @@ namespace vlib
 	
 	static const proc_info proc_popcount = { QUINE(popcount), NULL, Proc_pure };
 	
+	static const proc_info* procs[] =
+	{
+		&proc_It_max,
+		&proc_It_min,
+		&proc_popcount,
+		&proc_sorted,
+	};
+	
+	static
+	const proc_info* find_proc( const plus::string& name )
+	{
+		for ( int i = 0;  i < LENGTH( procs );  ++i )
+		{
+			const proc_info* proc = procs[ i ];
+			
+			if ( proc->name == name )
+			{
+				return proc;
+			}
+		}
+		
+		return NULL;
+	}
+	
 	static
 	Value getter( const plus::string& name )
 	{
-		if ( name == "max" )
+		if ( const proc_info* proc = find_proc( name ) )
 		{
-			return Proc( proc_It_max );
-		}
-		
-		if ( name == "min" )
-		{
-			return Proc( proc_It_min );
-		}
-		
-		if ( name == "popcount" )
-		{
-			return Proc( proc_popcount );
-		}
-		
-		if ( name == "sorted" )
-		{
-			return Proc( proc_sorted );
+			return Proc( *proc );
 		}
 		
 		THROW( "nonexistent member of namespace Iter" );
