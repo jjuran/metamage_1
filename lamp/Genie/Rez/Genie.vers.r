457e729313720a9cdbedb6e659d2d0ba348da76d
Josh Juran <jjuran@metamage.com>
2024-01-03 18:48:02 -0500

    Genie:  Bump the copyright date (and the version)

--- lamp/Genie/Rez/Genie.vers.r
+++ lamp/Genie/Rez/Genie.vers.r
@@ -1,15 +1,23 @@
-#define COPYRIGHT  "1999-2023 Josh Juran"
+#define COPYRIGHT  "1999-2024 Josh Juran"
 
 #define COPY "\0xA9"
 
+#define MAJOR 0
+#define MINOR 7
+#define TWEAK 9
+#define STAGE alpha
+#define PATCH 2
+
+#define VERSION  "0.7.9-alpha.2"
+
 resource 'vers' (1) {
-	0x00,
-	0x79,
-	alpha,
-	1,
+	MAJOR,
+	MINOR << 4 | TWEAK,
+	STAGE,
+	PATCH,
 	smRoman,
-	"0.7.9-alpha.1",
-	"0.7.9-alpha.1, " COPY COPYRIGHT
+	VERSION,
+	VERSION ", " COPY COPYRIGHT
 };
 
 /* Owner resource */
