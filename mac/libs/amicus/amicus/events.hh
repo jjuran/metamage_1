f5b94e1adde97fbf39188c4e5f166e175e061a3d
Josh Juran <jjuran@metamage.com>
2024-01-26 00:44:35 -0500

    amicus:  Edit block comment about command mode

--- mac/libs/amicus/amicus/events.hh
+++ mac/libs/amicus/amicus/events.hh
@@ -23,10 +23,10 @@ namespace amicus
 	forwarded to the back end application and that to be intepreted by
 	the front end.  Pressing a combination of modifier keys activates the
 	mode; releasing them without having pressed any other keys enters a
-	one-shot mode that interpret the next keystroke as a command and then
-	switch back to pass-through (i.e. deactivating command mode).  If other
+	one-shot mode that interprets the next keystroke as a command and then
+	switches back to pass-through (deactivating command mode).  If other
 	keys are pressed before releasing all the modifier keys, they are all
-	interpreted as commands and releasing the modifier keys will deactivate
+	interpreted as commands, and releasing the modifier keys will deactivate
 	command mode immediately.
 */
 
