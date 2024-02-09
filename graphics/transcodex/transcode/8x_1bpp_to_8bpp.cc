c8d970f18ff1f180bc44210385cc8cd3675ff7dc
Josh Juran <jjuran@metamage.com>
2024-02-09 14:26:26 -0500

    transcodex:  Colorize transcode_8x_1bpp_to_8bpp()
    Let the caller specify arbitrary pixel values coded for by 0 and 1 bits.

--- graphics/transcodex/transcode/8x_1bpp_to_8bpp.cc
+++ graphics/transcodex/transcode/8x_1bpp_to_8bpp.cc
@@ -10,27 +10,27 @@ namespace transcode
 {
 
 static inline
-Byte* _8x_1bpp_to_8bpp( Byte* p, Byte octet )
+Byte* _8x_1bpp_to_8bpp( Byte* p, Byte octet, Byte z, Byte o )
 {
 	typedef signed char int8_t;
 	
-	*p++ = (int8_t) octet < 0 ? 0x00 : 0xFF;  octet <<= 1;
-	*p++ = (int8_t) octet < 0 ? 0x00 : 0xFF;  octet <<= 1;
-	*p++ = (int8_t) octet < 0 ? 0x00 : 0xFF;  octet <<= 1;
-	*p++ = (int8_t) octet < 0 ? 0x00 : 0xFF;  octet <<= 1;
-	*p++ = (int8_t) octet < 0 ? 0x00 : 0xFF;  octet <<= 1;
-	*p++ = (int8_t) octet < 0 ? 0x00 : 0xFF;  octet <<= 1;
-	*p++ = (int8_t) octet < 0 ? 0x00 : 0xFF;  octet <<= 1;
-	*p++ = (int8_t) octet < 0 ? 0x00 : 0xFF;
+	*p++ = (int8_t) octet < 0 ? o : z;  octet <<= 1;
+	*p++ = (int8_t) octet < 0 ? o : z;  octet <<= 1;
+	*p++ = (int8_t) octet < 0 ? o : z;  octet <<= 1;
+	*p++ = (int8_t) octet < 0 ? o : z;  octet <<= 1;
+	*p++ = (int8_t) octet < 0 ? o : z;  octet <<= 1;
+	*p++ = (int8_t) octet < 0 ? o : z;  octet <<= 1;
+	*p++ = (int8_t) octet < 0 ? o : z;  octet <<= 1;
+	*p++ = (int8_t) octet < 0 ? o : z;
 	
 	return p;
 }
 
-void _8x_1bpp_to_8bpp( const Byte* src, Byte* dst, int n )
+void _8x_1bpp_to_8bpp( const Byte* src, Byte* dst, int n, Byte z, Byte o )
 {
 	while ( n-- > 0 )
 	{
-		dst = _8x_1bpp_to_8bpp( dst, *src++ );
+		dst = _8x_1bpp_to_8bpp( dst, *src++, z, o );
 	}
 }
 
--- graphics/transcodex/transcode/8x_1bpp_to_8bpp.hh
+++ graphics/transcodex/transcode/8x_1bpp_to_8bpp.hh
@@ -13,12 +13,12 @@ namespace transcode
 	typedef unsigned char Byte;
 	
 	
-	void _8x_1bpp_to_8bpp( const Byte* src, Byte* dst, int n );
+	void _8x_1bpp_to_8bpp( const Byte* src, Byte* dst, int n, Byte z, Byte o );
 	
 	inline
-	void _8x_1bpp_to_8bpp( const void* src, void* dst, int n )
+	void _8x_1bpp_to_8bpp( const void* src, void* dst, int n, Byte z, Byte o )
 	{
-		_8x_1bpp_to_8bpp( (const Byte*) src, (Byte*) dst, n );
+		_8x_1bpp_to_8bpp( (const Byte*) src, (Byte*) dst, n, z, o );
 	}
 	
 }
@@ -26,7 +26,14 @@ namespace transcode
 inline
 void transcode_8x_1bpp_to_8bpp( const void* src, void* dst, int n )
 {
-	transcode::_8x_1bpp_to_8bpp( src, dst, n );
+	/*
+		This is actually inverted:  The 0 and 1 bits code for $FF and $00,
+		respectively.  This is because this function was originally written
+		for transcoding classic Mac OS (black-on-white) binary image data to
+		conventional white-on-black grayscale as used in OpenGL.
+	*/
+	
+	transcode::_8x_1bpp_to_8bpp( src, dst, n, 0xFF, 0x00 );
 }
 
 #endif
