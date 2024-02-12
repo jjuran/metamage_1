52c6068205cc3be2e46432764c40ae0059565f55
Josh Juran <jjuran@metamage.com>
2024-02-11 21:34:18 -0500

    virtual/transit

--- 68k/libs/v68k-screen/screen/screen.hh
+++ 68k/libs/v68k-screen/screen/screen.hh
@@ -20,13 +20,14 @@ namespace raster
 namespace v68k   {
 namespace screen {
 
-extern raster::clut_data* clut;
-
 extern uint8_t* page_1_virtual_buffer;
 extern uint8_t* page_2_virtual_buffer;
 
 extern uint8_t* page_1_transit_buffer;
 extern uint8_t* page_2_transit_buffer;
+
+extern raster::clut_data* virtual_clut;
+extern raster::clut_data* transit_clut;
 
 void page_flip();
 
