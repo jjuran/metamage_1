e6d76d21b31c12af04ba9551abcf8fab78bf5532
Josh Juran <jjuran@metamage.com>
2024-03-09 19:36:59 -0500

    v68k-callouts:  Add decode_MACE3() callout

--- 68k/libs/v68k-callouts/callout/bridge.cc
+++ 68k/libs/v68k-callouts/callout/bridge.cc
@@ -30,6 +30,7 @@
 #include "ioutils/load.hh"
 
 // v68k-callouts
+#include "callout/codec.hh"
 #include "callout/cursor.hh"
 #include "callout/exceptions.hh"
 #include "callout/fixed.hh"
@@ -847,7 +848,7 @@ static const function_type the_callouts[] =
 	NULL,
 	
 	&send_sound_command_callout,
-	NULL,
+	&decode_MACE3_callout,
 	NULL,
 	NULL,
 	
