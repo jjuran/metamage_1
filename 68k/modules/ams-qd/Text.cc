jjuran/metamage_1

commit cd4cfe988b4d6c9f99e4afdd1c1dd0c5d6bf13b8

ams-qd:  Avoid clipped StdBits calls in StdText

68k/modules/ams-qd/Text.cc
@@ -22,6 +22,8 @@
 #pragma exceptions off
 
 
+Ptr ScrnBase : 0x0824;
+
 const Point OneOne : 0x0A02;
 
 
@@ -197,6 +199,31 @@ void smear( const BitMap&  srcBits,
 	}
 }
 
+static
+bool get_refined_clip_rect( const GrafPort& port, Rect& result )
+{
+	const BitMap& bits = port.portBits;
+	
+	if ( ! SectRect( &bits.bounds, &port.portRect, &result ) )
+	{
+		return false;
+	}
+	
+	bool vis = bits.baseAddr == ScrnBase;
+	
+	if ( vis  &&  ! SectRect( &port.visRgn[0]->rgnBBox, &result, &result ) )
+	{
+		return false;
+	}
+	
+	if ( ! SectRect( &port.clipRgn[0]->rgnBBox, &result, &result ) )
+	{
+		return false;
+	}
+	
+	return true;
+}
+
 pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 {
 	if ( denom.v == 0  ||  denom.h == 0 )
@@ -243,6 +270,13 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 		return;
 	}
 	
+	Rect clipRect;
+	
+	if ( ! get_refined_clip_rect( port, clipRect ) )
+	{
+		return;
+	}
+	
 	const FMInput input =
 	{
 		port.txFont,
@@ -257,6 +291,11 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 	const FMOutPtr output = FMSwapFont( &input );
 	
 	if ( output->fontHandle == NULL )
+	{
+		return;
+	}
+	
+	if ( port.pnLoc.h - output->shadowPixels >= clipRect.right )
 	{
 		return;
 	}
@@ -302,9 +341,19 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 	dstRect.top    = port.pnLoc.v - ascent;
 	dstRect.bottom = port.pnLoc.v - ascent + fRectHeight;
 	
+	if ( dstRect.top - output->shadowPixels >= clipRect.bottom )
+	{
+		return;
+	}
+	
+	if ( dstRect.bottom + output->shadowPixels <= clipRect.top )
+	{
+		return;
+	}
+	
 	// Worst case, for checking cursor intersection only
-	dstRect.left  = port.portBits.bounds.left;
-	dstRect.right = port.portBits.bounds.right;
+	dstRect.left  = clipRect.left;
+	dstRect.right = clipRect.right;
 	
 	redraw_lock lock( port.portBits, dstRect );
 	
@@ -324,6 +373,11 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 		
 		const int8_t character_offset = fixmulu_w( *offset_width++, h_scale );
 		const int8_t character_width  = fixmulu_w( *offset_width,   h_scale );
+		
+		if ( port.pnLoc.h + widMax <= clipRect.left )
+		{
+			goto advance;
+		}
 		
 		const short this_offset = locTable[ c ];
 		const short next_offset = locTable[ c + 1 ];
@@ -353,6 +407,11 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 			UInt8 bold = output->boldPixels;
 			UInt8 edge = output->shadowPixels;
 			
+			if ( dstRect.right + edge + bold <= clipRect.left )
+			{
+				goto advance;
+			}
+			
 			if ( edge )
 			{
 				smear( srcBits, srcRect, dstRect, edge + bold, edge );
@@ -371,7 +430,14 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 			}
 		}
 		
+	advance:
+		
 		port.pnLoc.h += character_width + output->extra;
+		
+		if ( port.pnLoc.h - output->shadowPixels >= clipRect.right )
+		{
+			break;
+		}
 	}
 }
 

commit eb143c44394a8dabf51565bb03fa0a2bd12ee80a

ams-qd:  Update pnLoc even for clipped characters
This fixes the regression from cd4cfe988b4d6c9f99e4afdd1c1dd0c5d6bf13b8.

68k/modules/ams-qd/Text.cc
@@ -270,6 +270,8 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 		return;
 	}
 	
+	bool no_more_blitting = false;
+	
 	Rect clipRect;
 	
 	if ( ! get_refined_clip_rect( port, clipRect ) )
@@ -297,7 +299,7 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 	
 	if ( port.pnLoc.h - output->shadowPixels >= clipRect.right )
 	{
-		return;
+		no_more_blitting = true;
 	}
 	
 	const FontRec& rec = **(FontRec**) output->fontHandle;
@@ -343,12 +345,12 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 	
 	if ( dstRect.top - output->shadowPixels >= clipRect.bottom )
 	{
-		return;
+		no_more_blitting = true;
 	}
 	
 	if ( dstRect.bottom + output->shadowPixels <= clipRect.top )
 	{
-		return;
+		no_more_blitting = true;
 	}
 	
 	// Worst case, for checking cursor intersection only
@@ -374,7 +376,7 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 		const int8_t character_offset = fixmulu_w( *offset_width++, h_scale );
 		const int8_t character_width  = fixmulu_w( *offset_width,   h_scale );
 		
-		if ( port.pnLoc.h + widMax <= clipRect.left )
+		if ( no_more_blitting  ||  port.pnLoc.h + widMax <= clipRect.left )
 		{
 			goto advance;
 		}
@@ -436,7 +438,7 @@ pascal void StdText_patch( short n, const char* p, Point numer, Point denom )
 		
 		if ( port.pnLoc.h - output->shadowPixels >= clipRect.right )
 		{
-			break;
+			no_more_blitting = true;
 		}
 	}
 }