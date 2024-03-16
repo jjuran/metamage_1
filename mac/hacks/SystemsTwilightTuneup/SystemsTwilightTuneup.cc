99deb177a934fa8d6da8115e9c2375099bfe2743
Josh Juran <jjuran@metamage.com>
2024-03-16 13:53:28 -0400

    SystemsTwilightTuneup:  Nix Network Node NIL no-no
    Patch away a write to $0000 that occurs for each portal in the room upon
    entry to The Network Node.

--- mac/hacks/SystemsTwilightTuneup/SystemsTwilightTuneup.cc
+++ mac/hacks/SystemsTwilightTuneup/SystemsTwilightTuneup.cc
@@ -39,6 +39,13 @@
 	This extension also patches InsertMenu() to rename the inadvertent
 	"intMemInv" menu to the more mainstream "Memory" moniker.
 	
+	And finally, well into the game, there's one room (The Network Node)
+	that elicits a crash upon entry.  A hit-testing function writes a word
+	through a pointer, passed to it as an argument, which is always NULL.
+	
+	This extension patches the hit-testing function to skip the pointer
+	write, avoiding the crash in Advanced Mac Substitute.
+	
 */
 
 // Mac OS
@@ -57,6 +64,9 @@
 
 // Standard C
 #include <stddef.h>
+
+// mac-glue-utils
+#include "mac_glue/Memory.hh"
 
 // mac-sys-utils
 #include "mac_sys/load_segment.hh"
@@ -104,6 +114,50 @@ void install_portal_animation_patch()
 	jump_target = &animate_buffered;
 }
 
+/*
+	There's a hit-testing function at CODE 5, offset $000c7a:
+	
+		short HitTestPortals( Point, short*, Boolean );
+	
+	It has only one call site (at CODE 3, offset $003554), which always
+	passes NULL for the pointer (and false for the flag, incidentally).
+	Since the result is discarded anyway, skip the write to avoid a crash.
+*/
+
+static inline
+void install_CODE_5_patch( Handle h )
+{
+	using mac::glue::GetHandleSize_raw;
+	
+	const int query_offset = 0x000cc8;
+	
+	if ( h  &&  GetHandleSize_raw( h ) >= query_offset + sizeof (UInt32) )
+	{
+		UInt32* p;
+		
+		p = (UInt32*) (*h + query_offset);
+		
+		if ( *p == 0x226E000C )
+		{
+			/*
+				Old:  000cc8:  MOVEA.L  (12,A6),A1         // always NULL
+				      000ccc:  MOVE.W   (-3058,A0),(A1)
+				      000cd0:  MOVE.W   D7,D0
+				      000cd2:  BRA.S    *+12    ; $000cde
+				
+				New:  000cc8:  MOVE.W   D7,D0
+				      000cca:  BRA.S    *+20    ; $000cde
+				      000ccc:  ...
+				      
+			*/
+			
+			*p = 0x30076012;
+			
+			HNoPurge( h );
+		}
+	}
+}
+
 static const Byte intMemInv[] = "\p" "intMemInv";
 static const Byte Memory[]    = "\p" "Memory";
 
@@ -142,6 +196,8 @@ void TEInit_patch()
 		ReleaseResource( h );
 		
 		install_portal_animation_patch();
+		
+		install_CODE_5_patch( Get1Resource( 'CODE', 5 ) );
 		
 		install_InsertMenu_patch();
 	}
