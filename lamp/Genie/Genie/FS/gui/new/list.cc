4e82f0aca1fd64aee6dac075a1d4619ab3a54fba
Josh Juran <jjuran@metamage.com>
2024-04-12 02:33:10 -0400

    Pedestal -> Genie:  Merge ListView classes

--- lamp/Genie/Genie/FS/gui/new/list.cc
+++ lamp/Genie/Genie/FS/gui/new/list.cc
@@ -12,10 +12,28 @@
 // Standard C
 #include <string.h>
 
+// Nostalgia
+#include "Nostalgia/MacWindows.hh"
+
+// mac-qd-utils
+#include "mac_qd/get_visRgn.hh"
+#include "mac_qd/globals/thePort.hh"
+#include "mac_qd/globals/thePort_window.hh"
+
 // plus
 #include "plus/serialize.hh"
 #include "plus/simple_map.hh"
 #include "plus/var_string.hh"
+
+// nucleus
+#ifndef NUCLEUS_OWNED_HH
+#include "nucleus/owned.hh"
+#endif
+
+// Nitrogen
+#ifndef MAC_LISTS_TYPES_LISTHANDLE_HH
+#include "Mac/Lists/Types/ListHandle.hh"
+#endif
 
 // poseven
 #include "poseven/types/errno_t.hh"
@@ -32,7 +50,7 @@
 #include "vfs/node/types/property_file.hh"
 
 // Pedestal
-#include "Pedestal/ListView.hh"
+#include "Pedestal/View.hh"
 
 // Genie
 #include "Genie/FS/Views.hh"
@@ -64,19 +82,39 @@ namespace Genie
 	static ListParameterMap gListParameterMap;
 	
 	
-	class ListView : public Ped::ListView
+	class ListView : public Ped::View
 	{
 		private:
 			typedef const vfs::node* Key;
 			
 			Key itsKey;
+			
+			nucleus::owned< ListHandle > itsList;
+		
+		private:
+			void Install( const Rect& bounds );
+			
+			void Uninstall();
 		
 		public:
 			ListView( Key key ) : itsKey( key )
 			{
 			}
 			
+			ListHandle Get() const  { return itsList; }
+			
 			bool IntersectsGrowBox() const;
+			
+			void SetBounds( const Rect& bounds );
+			
+			void AppendCell( const char* data, std::size_t length );
+			
+			void DeleteCells();
+			
+			bool MouseDown( const EventRecord& event );
+			bool KeyDown  ( const EventRecord& event );
+			
+			void Activate( bool activating )  { ::LActivate( activating, itsList ); }
 			
 			void Draw( const Rect& bounds, bool erasing );
 	};
@@ -84,6 +122,161 @@ namespace Genie
 	bool ListView::IntersectsGrowBox() const
 	{
 		return gListParameterMap[ itsKey ].itIntersectsGrowBox;
+	}
+	
+	static Rect AdjustedListBounds( const Rect&  bounds,
+	                                bool         scrollHoriz,
+	                                bool         scrollVert )
+	{
+		Rect result = bounds;
+		
+		/*
+			Mac OS places the scroll bars outside the bounds.
+			We adjust the bounds inward so that the scroll bars
+			draw within the original bounds.
+		*/
+		
+		if ( scrollHoriz )
+		{
+			result.bottom -= 15;
+		}
+		
+		if ( scrollVert )
+		{
+			result.right -= 15;
+		}
+		
+		return result;
+	}
+	
+	static void FixListScrollbarBounds( ListHandle list )
+	{
+		//bool hasGrow     = GetListFlags( list ) & 0x20;
+		const bool hasGrow = true;
+		
+		const bool scrollVert  = ::GetListVerticalScrollBar  ( list );
+		const bool scrollHoriz = ::GetListHorizontalScrollBar( list );
+		
+		// List Manager bug:  LNew() and LSize() ignore hasGrow, so we have to fudge it
+		if ( hasGrow && ( scrollVert != scrollHoriz ) )
+		{
+			// This hack is only necessary with hasGrow and one scrollbar
+			
+			ControlRef scrollbar = scrollVert ? ::GetListVerticalScrollBar  ( list )
+			                                  : ::GetListHorizontalScrollBar( list );
+			
+			Rect bounds;
+			GetControlBounds( scrollbar, &bounds );
+			
+			InvalRect(& bounds );
+			
+			(scrollVert ? bounds.bottom : bounds.right) -= 15;
+			
+			SetControlBounds( scrollbar, &bounds );
+			
+			DrawOneControl( scrollbar );
+		}
+	}
+	
+	void ListView::Install( const Rect& bounds )
+	{
+		const bool drawIt      = true;
+		const bool hasGrow     = IntersectsGrowBox();
+		const bool scrollHoriz = false;
+		const bool scrollVert  = true;
+		
+		const short proc = 0;
+		
+		const Rect dataBounds = { 0, 0, 0, 1 };  // one column, zero rows
+		const Point cSize = {};
+		
+		Rect rView = AdjustedListBounds( bounds, scrollHoriz, scrollVert );
+		
+		ListHandle list = ::LNew( &rView,
+		                          &dataBounds,
+		                          cSize,
+		                          proc,
+		                          mac::qd::thePort_window(),
+		                          drawIt,
+		                          hasGrow,
+		                          scrollHoriz,
+		                          scrollVert );
+		
+		// FIXME:  Check for null handle
+		
+		itsList = nucleus::owned< ListHandle >::seize( list );
+		
+		if ( hasGrow )
+		{
+			FixListScrollbarBounds( itsList );
+		}
+	}
+	
+	void ListView::Uninstall()
+	{
+		itsList.reset();
+	}
+	
+	void ListView::SetBounds( const Rect& bounds )
+	{
+		const Rect r = AdjustedListBounds( bounds, false, true );
+		
+		SetListViewBounds( itsList, &r );
+		
+		LSize( r.right - r.left,
+		       r.bottom - r.top,
+		       itsList );
+		
+		itsList.get()[0]->cellSize.h = r.right - r.left;
+		
+		if ( IntersectsGrowBox() )
+		{
+			FixListScrollbarBounds( itsList );
+		}
+	}
+	
+	inline
+	void ListView::AppendCell( const char* data, std::size_t length )
+	{
+		Rect bounds;
+		
+		GetListDataBounds( itsList, &bounds );
+		
+		const short i_row = LAddRow( 1, bounds.bottom, itsList );
+		
+		const Point loc = { i_row, 0 };
+		
+		LSetCell( data, length, loc, itsList );
+	}
+	
+	inline
+	void ListView::DeleteCells()
+	{
+		LDelRow( 0, 0, itsList );
+	}
+	
+	bool ListView::MouseDown( const EventRecord& event )
+	{
+		Point where = event.where;
+		
+		GlobalToLocal( &where );
+		
+		/*
+			MWC68K mysteriously generates smaller code
+			with the (int) cast than without it.
+		*/
+		
+		LClick( where,
+		        (int) event.modifiers,
+		        itsList.get() );
+		
+		return true;
+	}
+	
+	bool ListView::KeyDown( const EventRecord& event )
+	{
+		// FIXME
+		return false;
 	}
 	
 	void ListView::Draw( const Rect& bounds, bool erasing )
@@ -117,7 +310,13 @@ namespace Genie
 			}
 		}
 		
-		Ped::ListView::Draw( bounds, erasing );
+		using mac::qd::get_visRgn;
+		using mac::qd::thePort;
+		
+		//Rect bounds = Bounds( itsList );
+		//EraseRect( &bounds );
+		
+		LUpdate( get_visRgn( thePort() ), itsList );
 	}
 	
 	
