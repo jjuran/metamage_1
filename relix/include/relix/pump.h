f4cddb749c4ddc8009907ca1218b3aedd72e2283
Josh Juran <jjuran@metamage.com>
2024-04-13 21:10:16 -0400

    relix-include:  Don't guard the pump() declaration
    It's harmless to have it both here and in <fcntl.h> for MacRelix builds.

--- relix/include/relix/pump.h
+++ relix/include/relix/pump.h
@@ -11,14 +11,10 @@
 // POSIX
 #include <sys/types.h>
 
-#ifndef __RELIX__
-
 #ifdef __cplusplus
 extern "C"
 #endif
 
 ssize_t pump( int fd_in, off_t* off_in, int fd_out, off_t* off_out, size_t count, unsigned flags );
-
-#endif
 
 #endif
