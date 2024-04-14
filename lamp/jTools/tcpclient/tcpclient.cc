16087c9e1884050693c560718be2a54123691ae0
Josh Juran <jjuran@metamage.com>
2024-04-14 01:05:54 -0400

    tcpclient:  Rewrite without exceptions

--- lamp/jTools/tcpclient/tcpclient.cc
+++ lamp/jTools/tcpclient/tcpclient.cc
@@ -3,14 +3,19 @@
 	------------
 */
 
-// poseven
-#include "poseven/bundles/inet.hh"
-#include "poseven/functions/dup2.hh"
-#include "poseven/functions/execvp.hh"
-#include "poseven/functions/write.hh"
+// POSIX
+#include <netdb.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
-// Orion
-#include "Orion/Main.hh"
+// Standard C
+#include <errno.h>
+
+// more-posix
+#include "more/perror.hh"
+
+
+#pragma exceptions off
 
 
 #define PROGRAM  "tcpclient"
@@ -20,28 +25,82 @@
 #define STR_LEN( s )  "" s, (sizeof s - 1)
 
 
-namespace tool
+class addrinfo_cleanup
 {
+	private:
+		addrinfo* its_ai;
+		
+		// non-copyable
+		addrinfo_cleanup           ( const addrinfo_cleanup& );
+		addrinfo_cleanup& operator=( const addrinfo_cleanup& );
+	
+	public:
+		addrinfo_cleanup( addrinfo* ai ) : its_ai( ai )
+		{
+		}
+		
+		~addrinfo_cleanup()
+		{
+			::freeaddrinfo( its_ai );
+		}
+};
 
-namespace p7 = poseven;
-
-
-static
-void Connect( const char* hostname, const char* port_str )
+static inline
+int gai_error( const char* node, const char* service, int err )
 {
-	const p7::fd_t tcp_in  = p7::fd_t( 6 );
-	const p7::fd_t tcp_out = p7::fd_t( 7 );
+	const char* error = gai_strerror( err );
 	
-	p7::dup2( p7::connect( hostname, port_str ), tcp_in );
+	more::perror( PROGRAM, node, error );
 	
-	p7::dup2( tcp_in, tcp_out );
+	return -1;
 }
 
-int Main( int argc, char** argv )
+static
+int tcp_connect( const char* node, const char* service )
+{
+	addrinfo hints = {};
+	
+	hints.ai_socktype = SOCK_STREAM;
+	
+	addrinfo* ai;
+	
+	int err = getaddrinfo( node, service, &hints, &ai );
+	
+	if ( err )
+	{
+		return gai_error( node, service, err );
+	}
+	
+	addrinfo_cleanup cleanup( ai );
+	
+	int fd = socket( ai->ai_family, SOCK_STREAM, 0 );
+	
+	if ( fd < 0 )
+	{
+		more::perror( PROGRAM, "socket" );
+		
+		return fd;
+	}
+	
+	int nok = connect( fd, ai->ai_addr, ai->ai_addrlen );
+	
+	if ( nok < 0 )
+	{
+		more::perror( PROGRAM, node );
+		
+		close( fd );
+		
+		return -1;
+	}
+	
+	return fd;
+}
+
+int main( int argc, char** argv )
 {
 	if ( argc < 4 )
 	{
-		p7::write( p7::stderr_fileno, STR_LEN( USAGE ) );
+		write( STDERR_FILENO, STR_LEN( USAGE ) );
 		
 		return 1;
 	}
@@ -49,14 +108,26 @@ int Main( int argc, char** argv )
 	const char* hostname = argv[ 1 ];
 	const char* port_str = argv[ 2 ];
 	
+	int fd = tcp_connect( hostname, port_str );
+	
+	if ( fd < 0 )
+	{
+		return 1;  // errors already reported
+	}
+	
+	const int tcp_in  = 6;
+	const int tcp_out = 7;
+	
+	if ( dup2( fd, tcp_in ) < 0  ||  dup2( fd, tcp_out ) < 0 )
+	{
+		more::perror( PROGRAM, "dup2" );
+		
+		return 1;
+	}
+	
 	char** program_argv = argv + 3;
 	
-	Connect( hostname, port_str );
+	execvp( program_argv[ 0 ], program_argv );
 	
-	p7::execvp( program_argv );
-	
-	// Not reached
-	return 0;
-}
-
+	return errno == ENOENT ? 127 : 126;
 }
