64c88f934c11356c777143d5d21809bfa6b10af9
Josh Juran <jjuran@metamage.com>
2024-06-01 23:34:13 -0400

    Genie:  Normalize whitespace in signal.cc

--- lamp/Genie/Genie/Kernel/signal.cc
+++ lamp/Genie/Genie/Kernel/signal.cc
@@ -1,7 +1,7 @@
-/*	=========
- *	signal.cc
- *	=========
- */
+/*
+	signal.cc
+	---------
+*/
 
 // Standard C
 #include <errno.h>
@@ -32,158 +32,163 @@
 
 namespace Genie
 {
-	
-	static void send_signal( Process& process, int signo )
+
+static
+void send_signal( Process& process, int signo )
+{
+	if ( signo != 0 )
 	{
-		if ( signo != 0 )
-		{
-			process.Raise( signo );
-		}
+		process.Raise( signo );
+	}
+}
+
+static
+int kill_pid( pid_t pid, int signo )
+{
+	if ( Process* process = FindProcess( pid ) )
+	{
+		send_signal( *process, signo );
+		
+		return 0;
 	}
 	
-	static int kill_pid( pid_t pid, int signo )
+	return relix::set_errno( ESRCH );
+}
+
+static
+int kill_tid( pid_t tid, int signo )
+{
+	Process& current = current_process();
+	
+	if ( Process* target = FindProcess( tid ) )
 	{
-		if ( Process* process = FindProcess( pid ) )
+		if ( current.GetPID() == target->GetPID() )
 		{
-			send_signal( *process, signo );
+			if ( signo != 0 )
+			{
+				target->set_pending_signal( signo );
+			}
 			
 			return 0;
 		}
 		
-		return relix::set_errno( ESRCH );
+		/*
+			Not reached by pthread_kill().
+			Someone made an evil call to kill() with __SIGTHREAD set.
+		*/
+		
+		return relix::set_errno( EINVAL );
 	}
 	
-	static
-	int kill_tid( pid_t tid, int signo )
+	return relix::set_errno( ESRCH );
+}
+
+struct kill_param
+{
+	pid_t  id;
+	int    signo;
+	bool   killed_any;
+};
+
+static
+void* kill_process_in_group( void* param, pid_t pid, Process& process )
+{
+	if ( process.GetLifeStage() != kProcessReleased )
 	{
-		Process& current = current_process();
+		kill_param& pb = *(kill_param*) param;
 		
-		if ( Process* target = FindProcess( tid ) )
+		const pid_t id = pb.id;
+		
+		const bool matches = id < 0 ? pid != -id
+		                            : process.GetPGID() == id;
+		
+		if ( matches )
 		{
-			if ( current.GetPID() == target->GetPID() )
-			{
-				if ( signo != 0 )
-				{
-					target->set_pending_signal( signo );
-				}
-				
-				return 0;
-			}
+			send_signal( process, pb.signo );
 			
-			/*
-				Not reached by pthread_kill().
-				Someone made an evil call to kill() with __SIGTHREAD set.
-			*/
-			
-			return relix::set_errno( EINVAL );
-		}
-		
-		return relix::set_errno( ESRCH );
-	}
-	
-	struct kill_param
-	{
-		pid_t  id;
-		int    signo;
-		bool   killed_any;
-	};
-	
-	static void* kill_process_in_group( void* param, pid_t pid, Process& process )
-	{
-		if ( process.GetLifeStage() != kProcessReleased )
-		{
-			kill_param& pb = *(kill_param*) param;
-			
-			const pid_t id = pb.id;
-			
-			const bool matches = id < 0 ? pid != -id
-			                            : process.GetPGID() == id;
-			
-			if ( matches )
-			{
-				send_signal( process, pb.signo );
-				
-				pb.killed_any = true;
-			}
-		}
-		
-		return NULL;
-	}
-	
-	static int kill_pgid( pid_t pgid, int signo )
-	{
-		kill_param param = { pgid ? pgid : -relix::getpid(), signo, false };
-		
-		for_each_process( &kill_process_in_group, &param );
-		
-		return param.killed_any ? 0 : relix::set_errno( ESRCH );
-		
-	}
-	
-	static inline
-	pid_t getpgid()
-	{
-		return current_process().GetPGID();
-	}
-	
-	static int kill( pid_t pid, int signo )
-	{
-		if ( pid == 1  &&  signo == 0 )
-		{
-			// Optimize for canonical 'yield' idiom.
-			return 0;
-		}
-		
-		const bool is_thread = signo & __SIGTHREAD;
-		
-		if ( is_thread )
-		{
-			signo &= ~__SIGTHREAD;
-		}
-		
-		if ( signo < 0  ||  signo >= NSIG )
-		{
-			return set_errno( EINVAL );
-		}
-		
-		try
-		{
-			int result = is_thread ? kill_tid ( pid,       signo )
-			           : pid >   0 ? kill_pid ( pid,       signo )
-			           : pid ==  0 ? kill_pgid( getpgid(), signo )
-			           : pid == -1 ? kill_pgid( 0,         signo )
-			           :             kill_pgid( -pid,      signo );
-			
-			/*
-				Fatal signals terminate the process immediately.
-				Caught signals remain pending until reentering the kernel.
-			*/
-			
-			relix::check_signals( false );
-			
-			return result;
-		}
-		catch ( ... )
-		{
-			return set_errno_from_exception();
+			pb.killed_any = true;
 		}
 	}
 	
+	return NULL;
+}
+
+static
+int kill_pgid( pid_t pgid, int signo )
+{
+	kill_param param = { pgid ? pgid : -relix::getpid(), signo, false };
 	
-	using relix::sigaction;
-	using relix::sigpending;
-	using relix::sigprocmask;
-	using relix::sigsuspend;
+	for_each_process( &kill_process_in_group, &param );
 	
+	return param.killed_any ? 0 : relix::set_errno( ESRCH );
 	
-	#pragma force_active on
+}
+
+static inline
+pid_t getpgid()
+{
+	return current_process().GetPGID();
+}
+
+static
+int kill( pid_t pid, int signo )
+{
+	if ( pid == 1  &&  signo == 0 )
+	{
+		// Optimize for canonical 'yield' idiom.
+		return 0;
+	}
 	
-	REGISTER_SYSTEM_CALL( kill        );
-	REGISTER_SYSTEM_CALL( sigaction   );
-	REGISTER_SYSTEM_CALL( sigpending  );
-	REGISTER_SYSTEM_CALL( sigprocmask );
-	REGISTER_SYSTEM_CALL( sigsuspend  );
+	const bool is_thread = signo & __SIGTHREAD;
 	
-	#pragma force_active reset
+	if ( is_thread )
+	{
+		signo &= ~__SIGTHREAD;
+	}
 	
+	if ( signo < 0  ||  signo >= NSIG )
+	{
+		return set_errno( EINVAL );
+	}
+	
+	try
+	{
+		int result = is_thread ? kill_tid ( pid,       signo )
+		           : pid >   0 ? kill_pid ( pid,       signo )
+		           : pid ==  0 ? kill_pgid( getpgid(), signo )
+		           : pid == -1 ? kill_pgid( 0,         signo )
+		           :             kill_pgid( -pid,      signo );
+		
+		/*
+			Fatal signals terminate the process immediately.
+			Caught signals remain pending until reentering the kernel.
+		*/
+		
+		relix::check_signals( false );
+		
+		return result;
+	}
+	catch ( ... )
+	{
+		return set_errno_from_exception();
+	}
+}
+
+
+using relix::sigaction;
+using relix::sigpending;
+using relix::sigprocmask;
+using relix::sigsuspend;
+
+
+#pragma force_active on
+
+REGISTER_SYSTEM_CALL( kill        );
+REGISTER_SYSTEM_CALL( sigaction   );
+REGISTER_SYSTEM_CALL( sigpending  );
+REGISTER_SYSTEM_CALL( sigprocmask );
+REGISTER_SYSTEM_CALL( sigsuspend  );
+
+#pragma force_active reset
+
 }
