78d7a5296dffd93966c41bb706606de585e62e0a
Josh Juran <jjuran@metamage.com>
2024-06-03 12:24:15 -0400

    Genie:  Normalize whitespace in realpath.cc

--- lamp/Genie/Genie/Kernel/realpath.cc
+++ lamp/Genie/Genie/Kernel/realpath.cc
@@ -55,124 +55,127 @@
 
 namespace Genie
 {
-	
-	template < class T >
-	static inline T min( T a, T b )
+
+template < class T >
+static inline
+T min( T a, T b )
+{
+	return b < a ? b : a;
+}
+
+static inline
+bool begins_with( const char* s, const char* prefix, size_t length )
+{
+	while ( length-- )
 	{
-		return b < a ? b : a;
+		if ( *s++ != *prefix++ )
+		{
+			return false;
+		}
 	}
 	
-	static inline
-	bool begins_with( const char* s, const char* prefix, size_t length )
+	return true;
+}
+
+
+static
+plus::string mac_pathname_from_file( const vfs::node& file )
+{
+	return GetMacPathname( vfs::FSSpec_from_node( file ) );
+}
+
+
+static
+ssize_t _realpathat( int dirfd, const char* path, char* p, size_t n, int flags )
+{
+	short vRefNum = 0;
+	long  dirID   = 0;
+	
+	if ( begins_with( path, STR_LEN( "/.hfs/" ) ) )
 	{
-		while ( length-- )
+		if ( flags )
 		{
-			if ( *s++ != *prefix++ )
-			{
-				return false;
-			}
+			return relix::set_errno( EINVAL );
 		}
 		
-		return true;
-	}
-	
-	
-	static plus::string mac_pathname_from_file( const vfs::node& file )
-	{
-		return GetMacPathname( vfs::FSSpec_from_node( file ) );
-	}
-	
-	
-	static ssize_t _realpathat( int dirfd, const char *path, char *buffer, size_t buffer_size, int flags )
-	{
-		short vRefNum = 0;
-		long  dirID   = 0;
+		const char* p = path + STRLEN( "/.hfs/" );
 		
-		if ( begins_with( path, STR_LEN( "/.hfs/" ) ) )
+		unsigned v = gear::parse_unsigned_decimal( &p );
+		
+		if ( v == 0  ||  v > 0x7fff )
 		{
-			if ( flags )
-			{
-				return relix::set_errno( EINVAL );
-			}
-			
-			const char* p = path + STRLEN( "/.hfs/" );
-			
-			unsigned v = gear::parse_unsigned_decimal( &p );
-			
-			if ( v == 0  ||  v > 0x7fff )
-			{
-				return relix::set_errno( ENOENT );
-			}
-			
-			if ( *p++ != '/' )
-			{
-				return relix::set_errno( ENXIO );
-			}
-			
-			unsigned d = gear::parse_unsigned_decimal( &p );
-			
-			if ( d == 0  ||  d > 0x7fffffff )
-			{
-				return relix::set_errno( ENOENT );
-			}
-			
-			if ( *p == '/' )
-			{
-				++p;
-			}
-			
-			if ( *p != '\0' )
-			{
-				return relix::set_errno( ENOENT );
-			}
-			
-			vRefNum = -v;
-			dirID   =  d;
+			return relix::set_errno( ENOENT );
 		}
 		
-		try
+		if ( *p++ != '/' )
 		{
-			vfs::node_ptr file = dirID ? node_from_dirID( vRefNum, dirID )
-			                           : relix::resolve_path_at( dirfd, path );
-			
-			vfs::resolve_links_in_place( *relix::root(), file );
-			
-			const bool is_mac = flags & REALPATH_OUTPUT_HFS;
-			
-			plus::string resolved = is_mac ? mac_pathname_from_file( *file )
-			                               : pathname( *file );
-			
-			if ( (flags & REALPATH_OUTPUT_HFS_UTF8) == REALPATH_OUTPUT_HFS_UTF8 )
-			{
-				resolved = plus::utf8_from_mac( resolved );
-			}
-			
-			const bool too_big = resolved.size() > buffer_size;
-			
-			const size_t bytes_copied = min( buffer_size, resolved.size() );
-			
-			mempcpy( buffer, resolved.data(), bytes_copied );
-			
-			if ( too_big )
-			{
-				errno = ERANGE;
-				
-				// Return the bitwise inverse of the data size.
-				return ~resolved.size();
-			}
-			
-			return bytes_copied;
+			return relix::set_errno( ENXIO );
 		}
-		catch ( ... )
+		
+		unsigned d = gear::parse_unsigned_decimal( &p );
+		
+		if ( d == 0  ||  d > 0x7fffffff )
 		{
-			return relix::set_errno_from_exception();
+			return relix::set_errno( ENOENT );
 		}
+		
+		if ( *p == '/' )
+		{
+			++p;
+		}
+		
+		if ( *p != '\0' )
+		{
+			return relix::set_errno( ENOENT );
+		}
+		
+		vRefNum = -v;
+		dirID   =  d;
 	}
 	
-	#pragma force_active on
-	
-	REGISTER_SYSTEM_CALL( _realpathat );
-	
-	#pragma force_active reset
-	
+	try
+	{
+		vfs::node_ptr file = dirID ? node_from_dirID( vRefNum, dirID )
+		                           : relix::resolve_path_at( dirfd, path );
+		
+		vfs::resolve_links_in_place( *relix::root(), file );
+		
+		const bool is_mac = flags & REALPATH_OUTPUT_HFS;
+		
+		plus::string resolved = is_mac ? mac_pathname_from_file( *file )
+		                               : pathname( *file );
+		
+		if ( (flags & REALPATH_OUTPUT_HFS_UTF8) == REALPATH_OUTPUT_HFS_UTF8 )
+		{
+			resolved = plus::utf8_from_mac( resolved );
+		}
+		
+		const bool too_big = resolved.size() > n;
+		
+		const size_t bytes_copied = min( n, resolved.size() );
+		
+		mempcpy( p, resolved.data(), bytes_copied );
+		
+		if ( too_big )
+		{
+			errno = ERANGE;
+			
+			// Return the bitwise inverse of the data size.
+			return ~resolved.size();
+		}
+		
+		return bytes_copied;
+	}
+	catch ( ... )
+	{
+		return relix::set_errno_from_exception();
+	}
+}
+
+#pragma force_active on
+
+REGISTER_SYSTEM_CALL( _realpathat );
+
+#pragma force_active reset
+
 }
