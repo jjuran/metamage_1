5c7680c4900e4f7379d8acd25813383987846e2d
Josh Juran <jjuran@metamage.com>
2024-06-11 00:04:44 -0400

    ams-rsrc:  Prefer iota/ints.hh to <stdint.h>

--- 68k/modules/ams-rsrc/rsrc_fork.hh
+++ 68k/modules/ams-rsrc/rsrc_fork.hh
@@ -6,26 +6,28 @@
 #ifndef RSRCFORK_HH
 #define RSRCFORK_HH
 
-// Standard C
-#include <stdint.h>
+// iota
+#include "iota/ints.hh"
 
+
+typedef char** Handle;
 
 struct rsrc_fork_header
 {
-	uint32_t  offset_to_data;
-	uint32_t  offset_to_map;
-	uint32_t  length_of_data;
-	uint32_t  length_of_map;
+	iota::uint32_t  offset_to_data;
+	iota::uint32_t  offset_to_map;
+	iota::uint32_t  length_of_data;
+	iota::uint32_t  length_of_map;
 };
 
 struct rsrc_fork_system_reserved
 {
-	uint8_t bytes[ 112 ];
+	iota::uint8_t bytes[ 112 ];
 };
 
 struct rsrc_fork_application_data
 {
-	uint8_t bytes[ 128 ];
+	iota::uint8_t bytes[ 128 ];
 };
 
 struct rsrc_fork_superheader : rsrc_fork_header
@@ -36,41 +38,41 @@ struct rsrc_fork_superheader : rsrc_fork_header
 
 struct rsrc_data_element
 {
-	uint32_t  length;
-	uint8_t   bytes[ 0x01000000 ];
+	iota::uint32_t  length;
+	iota::uint8_t   bytes[ 0x01000000 ];
 };
 
 struct type_header
 {
-	uint32_t  type;     // a ResType
-	uint16_t  count_1;
-	uint16_t  offset;
+	iota::uint32_t  type;     // a ResType
+	iota::uint16_t  count_1;
+	iota::uint16_t  offset;
 };
 
 struct rsrc_header
 {
-	int16_t   id;
-	uint16_t  name_offset;
-	uint8_t   attrs;
-	uint8_t   offset_high_byte;
-	uint16_t  offset_low_word;
-	char**    handle;
+	iota::int16_t   id;
+	iota::uint16_t  name_offset;
+	iota::uint8_t   attrs;
+	iota::uint8_t   offset_high_byte;
+	iota::uint16_t  offset_low_word;
+	Handle          handle;
 };
 
 struct type_list
 {
-	uint16_t     count_1;
-	type_header  list[ 65536 ];
+	iota::uint16_t  count_1;
+	type_header     list[ 65536 ];
 };
 
 struct rsrc_map_header
 {
 	rsrc_fork_header  fork_header;
-	char**            next_map;
-	int16_t           refnum;
-	uint16_t          attrs;
-	uint16_t          offset_to_types;
-	uint16_t          offset_to_names;
+	Handle            next_map;
+	iota::int16_t     refnum;
+	iota::uint16_t    attrs;
+	iota::uint16_t    offset_to_types;
+	iota::uint16_t    offset_to_names;
 };
 
 #endif
