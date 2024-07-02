jjuran/metamage_1

commit 30eb9993b71747dba2b91dfa1f15adfb5c0f4e9c

mac-ui-utils:  Add set_window_title() for Str255

mac/libs/mac-ui-utils/mac_ui/windows.hh
@@ -8,12 +8,12 @@
 
 // Mac OS X
 #ifdef __APPLE__
-#include <ApplicationServices/ApplicationServices.h>
+#include <Carbon/Carbon.h>
 #endif
 
 // Mac OS
-#ifndef __QUICKDRAW__
-#include <Quickdraw.h>
+#ifndef __MACWINDOWS__
+#include <MacWindows.h>
 #endif
 
 
@@ -27,6 +27,12 @@ namespace ui  {
 	bool invalidate_if_compositing( WindowRef window );
 	
 	void invalidate_window( WindowRef window );
+	
+	inline
+	void set_window_title( WindowRef window, const Byte* name )
+	{
+		SetWTitle( window, name );
+	}
 	
 	OSStatus set_window_title( WindowRef window, const HFSUniStr255& name );
 	

commit 11965326ddb801ba1f14089444eacb5c12390064

[FIX] mac-ui-utils:  Guard SetWTitle() from 64-bit
This fixes the regression from 30eb9993b71747dba2b91dfa1f15adfb5c0f4e9c.

mac/libs/mac-ui-utils/mac_ui/windows.hh
@@ -28,11 +28,15 @@ namespace ui  {
 	
 	void invalidate_window( WindowRef window );
 	
+#if ! __LP64__
+	
 	inline
 	void set_window_title( WindowRef window, const Byte* name )
 	{
 		SetWTitle( window, name );
 	}
+	
+#endif
 	
 	OSStatus set_window_title( WindowRef window, const HFSUniStr255& name );
 	