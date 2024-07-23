13f95c814fc1a52ae02397f3b5812198de44be26
Josh Juran <jjuran@metamage.com>
2024-07-23 05:25:23 -0400

    mac-sys-utils:  Unindent gestalt.cc namespace

--- mac/libs/mac-sys-utils/mac_sys/gestalt.cc
+++ mac/libs/mac-sys-utils/mac_sys/gestalt.cc
@@ -21,42 +21,42 @@
 
 namespace mac {
 namespace sys {
+
+static inline
+bool has_Gestalt_trap()
+{
+	enum { _Gestalt = 0xA1AD };
 	
-	static inline
-	bool has_Gestalt_trap()
+	return ! TARGET_CPU_68K  ||  trap_available( _Gestalt );
+}
+
+long gestalt( unsigned long selector, long default_value )
+{
+	if ( ! has_Gestalt_trap() )
 	{
-		enum { _Gestalt = 0xA1AD };
-		
-		return ! TARGET_CPU_68K  ||  trap_available( _Gestalt );
+		return default_value;
 	}
 	
-	long gestalt( unsigned long selector, long default_value )
+	SInt32 result;
+	
+	const OSErr err = ::Gestalt( selector, &result );
+	
+	return err == noErr ? result : default_value;
+}
+
+bool gestalt_defined( unsigned long selector )
+{
+	if ( ! has_Gestalt_trap() )
 	{
-		if ( ! has_Gestalt_trap() )
-		{
-			return default_value;
-		}
-		
-		SInt32 result;
-		
-		const OSErr err = ::Gestalt( selector, &result );
-		
-		return err == noErr ? result : default_value;
+		return false;
 	}
 	
-	bool gestalt_defined( unsigned long selector )
-	{
-		if ( ! has_Gestalt_trap() )
-		{
-			return false;
-		}
-		
-		SInt32 unused_Gestalt_result;
-		
-		const OSErr err = ::Gestalt( selector, &unused_Gestalt_result );
-		
-		return err == noErr;
-	}
+	SInt32 unused_Gestalt_result;
 	
+	const OSErr err = ::Gestalt( selector, &unused_Gestalt_result );
+	
+	return err == noErr;
+}
+
 }
 }
