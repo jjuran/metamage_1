3d8e89b874b6f6f598659b581272c67e5059625f
Josh Juran <jjuran@metamage.com>
2024-07-31 04:55:22 -0400

    v68k-alloc:  Expand high memory downward by 3 MiB
    There's no reason we can't encroach into ROM space as well as I/O space.

--- 68k/libs/v68k-alloc/v68k-alloc/memory.hh
+++ 68k/libs/v68k-alloc/v68k-alloc/memory.hh
@@ -13,13 +13,13 @@
 namespace v68k  {
 namespace alloc {
 
-const uint32_t start = 0x00800000;  //  8 MiB
+const uint32_t start = 0x00500000;  //  5 MiB
 const uint32_t limit = 0x00E00000;  // 14 MiB
 
 const int page_size_bits = 16;
 const int page_size      = 1 << page_size_bits;  // 64K
 
-const uint32_t n_alloc_bytes = limit - start;  // 6 MiB
+const uint32_t n_alloc_bytes = limit - start;  // 9 MiB
 
 const uint32_t n_alloc_pages = n_alloc_bytes / page_size;
 
