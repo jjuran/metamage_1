47f19fb72ea505ab5833ff8477f7816fa47ef8e9
Josh Juran <jjuran@metamage.com>
2024-08-05 02:15:28 -0400

    umsp:  Set up VBR for exception handling on 68010+

--- 68k/modules/umsp/umsp.cc
+++ 68k/modules/umsp/umsp.cc
@@ -22,6 +22,8 @@
 #pragma exceptions off
 
 
+uint8_t CPUFlag : 0x012F;
+
 struct registers
 {
 	uint32_t d[8];  // D0-D7
@@ -31,6 +33,8 @@ struct registers
 	uint32_t pc;
 	uint16_t fv;
 };
+
+static uint32_t vector_array[ 256 ];
 
 inline uint16_t& select_word( uint32_t& longword, int i )
 {
@@ -196,10 +200,17 @@ bool handle_exception( registers& regs )
 	return false;
 }
 
+/*
+	On a 68000, we install our exception handler at address $0020
+	(i.e. with a vector base of zero) after saving the handler
+	previously installed there.  If we don't adequately emulate
+	the privileged instruction, we jump to the previous handler.
+*/
+
 static void* previous_exception_handler;
 
 static
-asm void exception_handler()
+asm void MC68000_Privilege_Violation_exception_handler()
 {
 	MOVEM.L  D0-D7/A0-A7,-(SP)
 	MOVE     USP,A0
@@ -228,19 +239,118 @@ unhandled:
 	RTS
 }
 
+/*
+	On a 68010 or later, we do things a bit differently.
+	
+	The MOVE from SR instruction is privileged, allowing for
+	proper virtualization (such that kernel code running in
+	a virtual machine might be led to believe that it's in
+	Supervisor mode when it's actually not).
+	
+	Our privileged instruction emulation covers this, but
+	we run into a snag with MacRelix, which installs its own
+	Privilege Violation exception handler /and/ executes
+	MOVE from SR (as part of system-info).  Since MacRelix's
+	exception handler displaces any previously installed one,
+	the system-info process dies on SIGILL, which is not a
+	nominal outcome.
+	
+	The solution is to apply the Vector Base Register, pointing
+	it to a different block of memory that MacRelix won't touch.
+	We mostly populate it with a generic exception handler that
+	merely jumps through the corresponding low memory vector.
+	The Privilege Violation exception handler is mostly the same
+	as for the 68000, except that the exception-unhandled path
+	falls back to the generic exception handler instead of one
+	specific handler address saved from low memory at setup time
+	as in the 68000 case.  (It would be wrong to do so, since it
+	would prevent MacRelix's Privilege Violation exception handler
+	from ever getting called, which would be the opposite problem.)
+*/
+
+static
+asm void default_exception_handler()
+{
+	MOVEM.L  D0/A0-A1,-(SP)
+	
+	MOVE.W   18(SP),D0    // skip 3 saved regs, SR, PC -> format/vector word
+	
+	AND.W    #0x3FF,D0    // just the vector offset
+	MOVEA.W  D0,A0        // make it a vector address
+	MOVEA.L  (A0),A0      // read the exception handler address
+	
+	MOVE.L   A0,8(SP)     // overwrite saved A1 value with next address
+	
+	MOVEM.L  (SP)+,D0/A0  // restore registers (not A1)
+	
+	RTS                   // jump to lowmem exception handler
+}
+
+static
+asm void VBR_Privilege_Violation_exception_handler()
+{
+	MOVEM.L  D0-D7/A0-A7,-(SP)
+	MOVE     USP,A0
+	MOVE.L   A0,60(SP)          // copy user SP to registers
+	
+	MOVE.L   SP,-(SP)
+	
+	JSR      handle_exception
+	TST.B    D0
+	
+	ADDQ.L   #4,SP
+	
+	MOVEA.L  60(SP),A0          // update USP
+	MOVE     A0,USP
+	MOVEM.L  (SP)+,D0-D7/A0-A6  // update registers (not A7)
+	ADDQ.L   #4,SP
+	
+	BEQ.S    unhandled
+	
+	RTE
+	
+unhandled:
+	
+	JMP      default_exception_handler
+}
+
 static
 asm int set_exception_handler()
 {
+	machine 68010
+	
+	MOVE.B  CPUFlag,D7  // read lowmem while still in User mode
+	
 	JSR     0xFFFFFFFA ;  // enter_supervisor_mode()
 	
 	BMI.S   bail ;  // D0 is -1 if branch taken
 	
+	TST.B   D7
+	BEQ.S   no_VBR
+	
+	LEA     default_exception_handler,A0
+	LEA     vector_array,A1
+	MOVEA.L A1,A2
+	
+	MOVE.W  #255,D6
+loop:
+	MOVE.L  A0,(A1)+
+	DBRA.S  D6,loop
+	
+	MOVEC   A2,VBR
+	
+	LEA     VBR_Privilege_Violation_exception_handler,A0
+	MOVE.L  A0,0x0020(A2)
+	BRA.S   rejoin
+	
+no_VBR:
 	MOVE.L  0x0020,previous_exception_handler
 	
-	LEA     exception_handler,A0
+	LEA     MC68000_Privilege_Violation_exception_handler,A0
 	
 	MOVE.L  A0,0x0020  // Privilege Violation
 	
+rejoin:
 	MOVE    D0,SR
 	MOVEQ   #0,D0
 	
