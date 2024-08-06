ac97e06bebcd77175e55fc301df94286509f1499
Josh Juran <jjuran@metamage.com>
2024-08-06 06:23:00 -0400

    async-wakeup:  Unindent namespace

--- mac/libs/async-wakeup/pthread/async_wakeup.cc
+++ mac/libs/async-wakeup/pthread/async_wakeup.cc
@@ -31,82 +31,82 @@
 
 namespace mac {
 namespace sys {
+
+static volatile bool wakeup_pending = false;
+
+static pthread_mutex_t wakeup_mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t  wakeup_cond  = PTHREAD_COND_INITIALIZER;
+
+static
+void* wakeup_thread_proc( void* )
+{
+	const ProcessSerialNumber& psn = current_process();
 	
-	static volatile bool wakeup_pending = false;
+	must_pthread_mutex_lock( &wakeup_mutex );
 	
-	static pthread_mutex_t wakeup_mutex = PTHREAD_MUTEX_INITIALIZER;
-	static pthread_cond_t  wakeup_cond  = PTHREAD_COND_INITIALIZER;
-	
-	static
-	void* wakeup_thread_proc( void* )
+	for ( ;; )
 	{
-		const ProcessSerialNumber& psn = current_process();
-		
-		must_pthread_mutex_lock( &wakeup_mutex );
-		
-		for ( ;; )
+		if ( wakeup_pending )
 		{
-			if ( wakeup_pending )
+			struct timespec now;
+			
+			clock_gettime( CLOCK_MONOTONIC, &now );
+			
+			struct timespec& soon = now;
+			
+			uint32_t ns = now.tv_nsec + 10 * 1000 * 1000;
+			
+			if ( ns >= 1000 * 1000 * 1000 )
 			{
-				struct timespec now;
+				ns -= 1000 * 1000 * 1000;
 				
-				clock_gettime( CLOCK_MONOTONIC, &now );
-				
-				struct timespec& soon = now;
-				
-				uint32_t ns = now.tv_nsec + 10 * 1000 * 1000;
-				
-				if ( ns >= 1000 * 1000 * 1000 )
-				{
-					ns -= 1000 * 1000 * 1000;
-					
-					soon.tv_sec += 1;
-				}
-				
-				soon.tv_nsec = ns;
-				
-				pthread_cond_timedwait( &wakeup_cond, &wakeup_mutex, &soon );
-			}
-			else
-			{
-				must_pthread_cond_wait( &wakeup_cond, &wakeup_mutex );
+				soon.tv_sec += 1;
 			}
 			
-			(void) ::WakeUpProcess( &psn );
+			soon.tv_nsec = ns;
+			
+			pthread_cond_timedwait( &wakeup_cond, &wakeup_mutex, &soon );
+		}
+		else
+		{
+			must_pthread_cond_wait( &wakeup_cond, &wakeup_mutex );
 		}
 		
-	//	must_pthread_mutex_unlock( &wakeup_mutex );  // not reached
+		(void) ::WakeUpProcess( &psn );
 	}
 	
-	void request_async_wakeup()
-	{
-		must_pthread_mutex_lock( &wakeup_mutex );
-		
-		wakeup_pending = true;
-		
-		must_pthread_cond_signal( &wakeup_cond );
-		
-		must_pthread_mutex_unlock( &wakeup_mutex );
-	}
+//	must_pthread_mutex_unlock( &wakeup_mutex );  // not reached
+}
+
+void request_async_wakeup()
+{
+	must_pthread_mutex_lock( &wakeup_mutex );
 	
-	void clear_async_wakeup()
-	{
-		wakeup_pending = false;
-	}
+	wakeup_pending = true;
 	
-	struct wakeup_thread
-	{
-		pthread_t thread;
-		
-		wakeup_thread();
-	};
+	must_pthread_cond_signal( &wakeup_cond );
 	
-	wakeup_thread::wakeup_thread()
-	{
-		must_pthread_create( &thread, NULL, &wakeup_thread_proc, NULL );
-	}
+	must_pthread_mutex_unlock( &wakeup_mutex );
+}
+
+void clear_async_wakeup()
+{
+	wakeup_pending = false;
+}
+
+struct wakeup_thread
+{
+	pthread_t thread;
 	
-	static wakeup_thread the_wakeup_thread;
-	
+	wakeup_thread();
+};
+
+wakeup_thread::wakeup_thread()
+{
+	must_pthread_create( &thread, NULL, &wakeup_thread_proc, NULL );
+}
+
+static wakeup_thread the_wakeup_thread;
+
 }
 }
--- mac/libs/async-wakeup/timer/async_wakeup.cc
+++ mac/libs/async-wakeup/timer/async_wakeup.cc
@@ -39,171 +39,171 @@
 
 namespace mac {
 namespace sys {
-	
-	using mac::sys::gestalt;
-	
-	static volatile bool wakeup_requested;
-	
-	const bool is_osx = TARGET_API_MAC_CARBON  &&  gestalt( 'sysv' ) >= 0x1000;
-	
-	static inline
-	void prime_timer( TMTaskPtr task )
-	{
-		/*
-			Expect the timer task as an argument instead of using a global,
-			so on 68K we don't rely on the value of A5.
-		*/
-		
-		::PrimeTime( (QElemPtr) task, 10 );  // 10ms
-	}
-	
-	struct wakeup_TMTask
-	{
-		TMTask                      tm;
-		const ProcessSerialNumber*  psn;
-	};
-	
-	static pascal
-	void async_wakeup_proc( TMTaskPtr task IN( a1 ) )
-	{
-		if ( wakeup_requested )
-		{
-			prime_timer( task );
-			
-			::WakeUpProcess( ((const wakeup_TMTask*) task)->psn );
-		}
-	}
-	
-	static wakeup_TMTask the_wakeup_timer =
-	{
-		{
-			0,
-			0,
-			::NewTimerUPP( (TimerProcPtr) &async_wakeup_proc ),
-			0,
-			0,
-			0
-		},
-		&current_process()
-	};
-	
-	static pascal
-	void deferred_wakeup( long param IN( a1 ) )
-	{
-		do
-		{
-			::WakeUpProcess( the_wakeup_timer.psn );
-			
-			mac::sys::delay( 1 );  // one tick == 16ms
-		}
-		while ( wakeup_requested );
-	}
-	
-	static DeferredTask the_wakeup_dt =
-	{
-		0,
-		0,
-		0,
-		is_osx ? ::NewDeferredTaskUPP( (DeferredTaskProcPtr) &deferred_wakeup )
-		       : 0,
-		0,
-		0,
-	};
-	
-	static inline
-	void schedule_wakeup( TMTaskPtr task )
-	{
-		if ( is_osx )
-		{
-			OSErr err = DTInstall( &the_wakeup_dt );
-		}
-		else
-		{
-			prime_timer( task );
-		}
-	}
-	
+
+using mac::sys::gestalt;
+
+static volatile bool wakeup_requested;
+
+const bool is_osx = TARGET_API_MAC_CARBON  &&  gestalt( 'sysv' ) >= 0x1000;
+
+static inline
+void prime_timer( TMTaskPtr task )
+{
 	/*
-		Determine whether the ProcessSerialNumber for this process (as
-		determined by mac-sys-utils) is real or not.  In the absence of
-		System 7 or MultiFinder, it's zero in both fields (which is the
-		same as {0, kNoProcess}, which GetCurrentProcess() will never
-		return.)
-		
-		In a single-process system, there's no need to request wakeups,
-		since this process is always in front and WaitNextEvent() will
-		always return nullEvents instead of sleeping.
+		Expect the timer task as an argument instead of using a global,
+		so on 68K we don't rely on the value of A5.
 	*/
 	
-	static inline
-	bool is_multiproc_PSN( const ProcessSerialNumber& psn )
+	::PrimeTime( (QElemPtr) task, 10 );  // 10ms
+}
+
+struct wakeup_TMTask
+{
+	TMTask                      tm;
+	const ProcessSerialNumber*  psn;
+};
+
+static pascal
+void async_wakeup_proc( TMTaskPtr task IN( a1 ) )
+{
+	if ( wakeup_requested )
 	{
-		return ! TARGET_CPU_68K  ||  psn.lowLongOfPSN != 0;
+		prime_timer( task );
+		
+		::WakeUpProcess( ((const wakeup_TMTask*) task)->psn );
 	}
-	
-	const bool multiproc_available = is_multiproc_PSN( *the_wakeup_timer.psn );
-	
-	static bool lock_timer()
+}
+
+static wakeup_TMTask the_wakeup_timer =
+{
 	{
-		register void** mutex = (void**) &the_wakeup_timer.tm.qLink;
-		
-	#if TARGET_CPU_68K
-		
-		const short cpu68000 = 0;
-		
-		if ( CPUFlag == cpu68000 )
-		{
-			// 68000 only
-			
-			register bool acquired = false;  // Initialize to squelch warning
-			
-			asm
-			{
-				TAS    (mutex)
-				SEQ    acquired
-				NEG.B  acquired
-			}
-			
-			return acquired;
-		}
-		
-	#endif
-		
-	#ifndef MAC_OS_X_VERSION_10_8
-		
-		return ::OTCompareAndSwapPtr( NULL, (void*) 0xFFFFFFFF, mutex );
-		
-	#endif
-		
-		return false;
-	}
-	
-	void request_async_wakeup()
+		0,
+		0,
+		::NewTimerUPP( (TimerProcPtr) &async_wakeup_proc ),
+		0,
+		0,
+		0
+	},
+	&current_process()
+};
+
+static pascal
+void deferred_wakeup( long param IN( a1 ) )
+{
+	do
 	{
-		if ( ! multiproc_available )
-		{
-			return;
-		}
-		
-		wakeup_requested = true;
-		
-		if ( lock_timer() )
-		{
-			the_wakeup_timer.tm.tmWakeUp   = 0;
-			the_wakeup_timer.tm.tmReserved = 0;
-			
-			::InsTime( (QElemPtr) &the_wakeup_timer );
-		}
-		
-		schedule_wakeup( &the_wakeup_timer.tm );
-		
 		::WakeUpProcess( the_wakeup_timer.psn );
+		
+		mac::sys::delay( 1 );  // one tick == 16ms
 	}
-	
-	void clear_async_wakeup()
+	while ( wakeup_requested );
+}
+
+static DeferredTask the_wakeup_dt =
+{
+	0,
+	0,
+	0,
+	is_osx ? ::NewDeferredTaskUPP( (DeferredTaskProcPtr) &deferred_wakeup )
+	       : 0,
+	0,
+	0,
+};
+
+static inline
+void schedule_wakeup( TMTaskPtr task )
+{
+	if ( is_osx )
 	{
-		wakeup_requested = false;
+		OSErr err = DTInstall( &the_wakeup_dt );
+	}
+	else
+	{
+		prime_timer( task );
+	}
+}
+
+/*
+	Determine whether the ProcessSerialNumber for this process (as
+	determined by mac-sys-utils) is real or not.  In the absence of
+	System 7 or MultiFinder, it's zero in both fields (which is the
+	same as {0, kNoProcess}, which GetCurrentProcess() will never
+	return.)
+	
+	In a single-process system, there's no need to request wakeups,
+	since this process is always in front and WaitNextEvent() will
+	always return nullEvents instead of sleeping.
+*/
+
+static inline
+bool is_multiproc_PSN( const ProcessSerialNumber& psn )
+{
+	return ! TARGET_CPU_68K  ||  psn.lowLongOfPSN != 0;
+}
+
+const bool multiproc_available = is_multiproc_PSN( *the_wakeup_timer.psn );
+
+static bool lock_timer()
+{
+	register void** mutex = (void**) &the_wakeup_timer.tm.qLink;
+	
+#if TARGET_CPU_68K
+	
+	const short cpu68000 = 0;
+	
+	if ( CPUFlag == cpu68000 )
+	{
+		// 68000 only
+		
+		register bool acquired = false;  // Initialize to squelch warning
+		
+		asm
+		{
+			TAS    (mutex)
+			SEQ    acquired
+			NEG.B  acquired
+		}
+		
+		return acquired;
 	}
 	
+#endif
+	
+#ifndef MAC_OS_X_VERSION_10_8
+	
+	return ::OTCompareAndSwapPtr( NULL, (void*) 0xFFFFFFFF, mutex );
+	
+#endif
+	
+	return false;
+}
+
+void request_async_wakeup()
+{
+	if ( ! multiproc_available )
+	{
+		return;
+	}
+	
+	wakeup_requested = true;
+	
+	if ( lock_timer() )
+	{
+		the_wakeup_timer.tm.tmWakeUp   = 0;
+		the_wakeup_timer.tm.tmReserved = 0;
+		
+		::InsTime( (QElemPtr) &the_wakeup_timer );
+	}
+	
+	schedule_wakeup( &the_wakeup_timer.tm );
+	
+	::WakeUpProcess( the_wakeup_timer.psn );
+}
+
+void clear_async_wakeup()
+{
+	wakeup_requested = false;
+}
+
 }
 }
