1398a8dc8a4abb558a25820bfee28c58b5464d03
Josh Juran <jjuran@metamage.com>
2024-08-07 00:56:09 -0400

    Revert "ams-qd:  Preserve volatile registers in HideCursor"
    This reverts commit 5f820f601c54884e85c9ffb9619526707e85d615.

--- 68k/modules/ams-qd/Cursor.cc
+++ 68k/modules/ams-qd/Cursor.cc
@@ -24,27 +24,9 @@ pascal void SetCursor_patch( const Cursor* crsr )
 	JSetCrsr( crsr );
 }
 
-asm
 pascal void HideCursor_patch()
 {
-	LINK     A6,#0
-	MOVEM.L  D1-D2/A0-A1,-(SP)
-	
-	/*
-		Aside from the Resource Manager, Toolbox traps are allowed to clobber
-		volatile registers, but Vanlandingham assumes that HideCursor() does
-		not.  (Specifically, it relies on D1-D2/A0-A1 being preserved.)
-		
-		TODO:  Replace this workaround with an INIT-loading system that can
-		install patches for individual applications.
-	*/
-	
-	MOVEA.L  JHideCursor,A0
-	JSR      (A0)
-	
-	MOVEM.L  (SP)+,D1-D2/A0-A1
-	UNLK     A6
-	RTS
+	JHideCursor();
 }
 
 pascal void ShowCursor_patch()
