jjuran/metamage_1

commit 10f1eeaac6ebef3c95b962434836361a5877f893

Compile::Driver:  Compile with -fvisibility=hidden

tools/perl/Compile/Driver/Configuration.pm
@@ -90,6 +90,13 @@ sub new
 	
 	$self{ build } = $build  if !exists $self{ build };
 	
+	my $ccv = `cc -v 2>&1 | grep version | tail -1`;
+	
+	my ( $name, $vers ) = $ccv =~ m{^(.*) version ([\d.]+)};
+	
+	$self{ ccname } = $name;
+	$self{ ccvers } = $vers;
+	
 	return bless \%self, $class;
 }
 
@@ -99,7 +106,9 @@ sub platform_mask
 	
 	my %platform = %$self;
 	
-	delete $platform{ build };
+	delete $platform{ build  };
+	delete $platform{ ccname };
+	delete $platform{ ccvers };
 	
 	return Compile::Driver::Platform::mask_for_values( values %platform );
 }

tools/perl/Compile/Driver/Job/Compile.pm
@@ -121,6 +121,11 @@ sub command
 		$d{ TARGET_API_MAC_CARBON } = 1;
 	}
 	
+	if ( $conf->{ccname} ne "gcc"  ||  $conf->{ccvers} ge "4" )
+	{
+		push @f, "-fvisibility=hidden";
+	}
+	
 	if ( $conf->is_apple_gcc )
 	{
 		push @f, "-fpascal-strings";

commit 01f793c68825aed7872b9e1d337dba566a757b82

Compile::Driver:  Fix ccname given OS X 10.2's gcc
This gcc gives us a verbose name that ends in, but doesn't equal, "gcc".
We, in turn, saw it as not gcc and passed -fvisibility, breaking builds.

This fixes the regression from 10f1eeaac6ebef3c95b962434836361a5877f893.

tools/perl/Compile/Driver/Configuration.pm
@@ -92,6 +92,8 @@ sub new
 	
 	my $ccv = `cc -v 2>&1 | grep version | tail -1`;
 	
+	$ccv =~ s{^.* based on }{};
+	
 	my ( $name, $vers ) = $ccv =~ m{^(.*) version ([\d.]+)};
 	
 	$self{ ccname } = $name;