jjuran/metamage_1

commit ab5ab878c85c3971c90b9102d40f382e64de41ba

cpres:  Use non-nitric {Add,Detach}Resource()

lamp/jTools/cpres/cpres.cc
@@ -113,6 +113,10 @@ namespace tool
 			
 			for ( int iRsrc = 1;  iRsrc <= rsrcs;  ++iRsrc )
 			{
+				using mac::sys::res_error;
+				
+				OSErr err;
+				
 				N::Handle h = N::Get1IndResource( type, iRsrc );
 				
 				HNoPurge( h );
@@ -127,19 +131,24 @@ namespace tool
 				{
 					RemoveResource( existing );
 				}
+				else if ( (err = res_error()) != resNotFound )
+				{
+					goto error;
+				}
 				else
 				{
-					OSErr err = mac::sys::res_error();
-					
-					if ( err != resNotFound )
-					{
-						Mac::ThrowOSStatus( err );
-					}
-					
 					// Okay, resource didn't exist in dest file
 				}
 				
-				N::AddResource( N::DetachResource( h ), resInfo );
+				DetachResource( h );
+				
+				AddResource( h, resInfo.type, resInfo.id, resInfo.name );
+				
+				err = res_error();
+				
+			error:
+				
+				Mac::ThrowOSStatus( err );
 			}
 		}
 		

commit 127631dc4bd0b40c4809cf8d43e61c987b26c670

cpres:  Anticipate noErr from null Get1Resource()
Inside Macintosh, Volume I documents that GetResource() might set ResErr
to noErr even while returning NULL.  Get1Resource() carries on the grand
tradition.  The previous code allowed for this, but the refactor didn't.

This fixes the regression from ab5ab878c85c3971c90b9102d40f382e64de41ba.

lamp/jTools/cpres/cpres.cc
@@ -142,7 +142,7 @@ namespace tool
 				{
 					RemoveResource( existing );
 				}
-				else if ( (err = res_error()) != resNotFound )
+				else if ( (err = res_error())  &&  err != resNotFound )
 				{
 					goto error;
 				}