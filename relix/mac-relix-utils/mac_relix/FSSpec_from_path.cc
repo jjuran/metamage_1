d08329eb0781baad427da143b860b40ceb5e1036
Josh Juran <jjuran@metamage.com>
2024-12-29 08:16:14 -0500

    mac-relix-utils:  Add Mach-O optional file logic
    Provide an OS X implementation of FSSpec_from_optional_path() via FSRef.

--- relix/mac-relix-utils/mac_relix/FSSpec_from_path.cc
+++ relix/mac-relix-utils/mac_relix/FSSpec_from_path.cc
@@ -16,6 +16,19 @@
 // Standard C
 #include <errno.h>
 
+#ifdef __APPLE__
+
+// Standard C++
+#include <algorithm>
+
+// chars
+#include "conv/mac_utf8.hh"
+
+// mac-file-utils
+#include "mac_file/directory.hh"
+
+#endif
+
 // mac-relix-utils
 #include "mac_relix/FSSpec_from_stat.hh"
 
@@ -78,7 +91,72 @@ Error FSSpec_from_optional_path( const char* path, FSSpec& result )
 	
 #endif
 	
-	return muxed_errno( ENOSYS );
+#ifdef __APPLE__
+	
+	OSStatus err = FSSpec_from_existing_path( path, result );
+	
+#if ! __LP64__
+	
+	if ( err == fnfErr )
+	{
+		size_t len = strlen( path );
+		
+		char* p = (char*) alloca( len + 1 );
+		
+		memcpy( p, path, len + 1 );
+		
+		path = p;
+		
+		const char* name;
+		
+		if ( (p = strrchr( p, '/' )) )
+		{
+			*p++ = '\0';
+			
+			name = p;
+		}
+		else
+		{
+			name = path;
+			path = ".";
+		}
+		
+		if ( *name )
+		{
+			using conv::mac_from_utf8_nothrow;
+			
+			using mac::types::VRefNum_DirID;
+			
+			Byte* q = result.name;
+			
+			len = strlen( name );
+			
+			size_t n = mac_from_utf8_nothrow( (char*) q + 1, 32, name, len );
+			
+			if ( n > 31 )
+			{
+				return (Error) bdNamErr;
+			}
+			
+			*q++ = n;
+			
+			std::replace( q, q + n, ':', '/' );
+			
+			FSRef ref;
+			Boolean isDir;
+			VRefNum_DirID& vdir = *(VRefNum_DirID*) &result;
+			
+			(err = FSPathMakeRef( (const Byte*) path, &ref, &isDir ))  ||
+			(vdir = mac::file::directory( ref ),
+			 err = vdir.dirID < 0 ? vdir.dirID : noErr);
+		}
+	}
+	
+#endif  // #if ! __LP64__
+	
+	return (Error) err;
+	
+#endif  // #ifdef __APPLE__
 }
 
 #endif  // #if defined( __RELIX__ )  ||  defined( __APPLE__ )
