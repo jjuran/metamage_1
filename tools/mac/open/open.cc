jjuran/metamage_1

commit 8f786a43f671d7a2c77bbbf255fafead2e75dda5

open:  Use mac-relix-utils for path resolution

tools/mac/open/open.cc
@@ -29,6 +29,9 @@
 #include "mac_proc/find_process.hh"
 #include "mac_proc/launch_application.hh"
 
+// mac-relix-utils
+#include "mac_relix/FSSpec_from_path.hh"
+
 #include "Nitrogen/AEInteraction.hh"
 #include "Nitrogen/Aliases.hh"
 #include "Nitrogen/AppleEvents.hh"
@@ -42,9 +45,6 @@
 
 // Nitrogen
 #include "Mac/AppleEvents/Types/DescType_scribe_dynamic.hh"
-
-// Divergence
-#include "Divergence/Utilities.hh"
 
 // Orion
 #include "Orion/Main.hh"
@@ -134,7 +134,6 @@ namespace tool
 	namespace n = nucleus;
 	namespace N = Nitrogen;
 	namespace p7 = poseven;
-	namespace Div = Divergence;
 	
 	
 	enum
@@ -143,10 +142,22 @@ namespace tool
 	};
 	
 	
-	static FSSpec ResolvePathname( const char* pathname, bool macPathname )
+	static
+	FSSpec ResolvePathname( const char* path, bool macPathname )
 	{
-		return macPathname ? N::FSMakeFSSpec         ( N::Str255( pathname ) ) 
-		                   : Div::ResolvePathToFSSpec(            pathname   );
+		using mac::relix::FSSpec_from_existing_path;
+		
+		FSSpec file;
+		
+		OSErr err = macPathname ? FSMakeFSSpec( 0, 0, N::Str255( path ), &file )
+		                        : FSSpec_from_existing_path( path, file );
+		
+		if ( err != fnfErr )
+		{
+			Mac::ThrowOSStatus( err );
+		}
+		
+		return file;
 	}
 	
 	static inline n::owned< Mac::AEDesc_Data > AECoerce_Alias_From_FSSpec( const FSSpec& item )
@@ -327,8 +338,12 @@ namespace tool
 		{
 			// User has specified an application by its pathname
 			
+			using mac::relix::FSSpec_from_existing_path;
+			
 			// Resolve to FSSpec
-			appFile = Div::ResolvePathToFSSpec( gAppNameToOpenIn );
+			OSErr err = FSSpec_from_existing_path( gAppNameToOpenIn, appFile );
+			
+			Mac::ThrowOSStatus( err );
 			
 			// Find it if running
 			ProcessSerialNumber psn;

tools/mac/open/A-line.conf
@@ -3,9 +3,9 @@ product tool
 use command
 use Orion
 use MacFiles
-use Divergence
 use mac-file-utils
 use mac-proc-utils
+use mac-relix-utils
 use mac-sys-utils
 
 frameworks Carbon

commit d565b27af8d8bdacafc1b61b3c0db0b832fb9117

open:  Fix file diagnostics in MacRelix builds
This fixes the regression from 8f786a43f671d7a2c77bbbf255fafead2e75dda5.

tools/mac/open/open.cc
@@ -21,6 +21,7 @@
 
 // mac-sys-utils
 #include "mac_sys/current_process.hh"
+#include "mac_sys/errno_from_mac_error.hh"
 #include "mac_sys/is_front_process.hh"
 
 // mac-file-utils
@@ -129,6 +130,9 @@ namespace tool
 	namespace n = nucleus;
 	namespace N = Nitrogen;
 	
+	using mac::sys::Error;
+	using mac::sys::errno_from_mac_error;
+	
 	
 	enum
 	{
@@ -137,12 +141,12 @@ namespace tool
 	
 	
 	static
-	OSErr ResolvePathname( FSSpec& file, const char* path, bool macPathname )
+	Error ResolvePathname( FSSpec& file, const char* path, bool hfs )
 	{
 		using mac::relix::FSSpec_from_existing_path;
 		
-		OSErr err = macPathname ? FSMakeFSSpec( 0, 0, N::Str255( path ), &file )
-		                        : FSSpec_from_existing_path( path, file );
+		Error err = hfs ? (Error) FSMakeFSSpec( 0, 0, N::Str255( path ), &file )
+		                : FSSpec_from_existing_path( path, file );
 		
 		return err;
 	}
@@ -393,13 +397,16 @@ namespace tool
 			
 			FSSpec item;
 			
-			OSErr err = ResolvePathname( item, pathname, gUseMacPathnames );
+			Error err = ResolvePathname( item, pathname, gUseMacPathnames );
 			
 			if ( err )
 			{
-				if ( err == fnfErr )
+				int errnum = is_errno( err ) ? errno_from_muxed( err )
+				                             : errno_from_mac_error( err );
+				
+				if ( errnum > 0 )
 				{
-					fprintf( stderr, "open: %s: %s\n", pathname, strerror( ENOENT ) );
+					fprintf( stderr, "open: %s: %s\n", pathname, strerror( errnum ) );
 				}
 				else
 				{