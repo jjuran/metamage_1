jjuran/metamage_1

commit 78addfcaab3fb9ad6cd84330b1d09ecfdf558de8

SetFile:  Use mac-relix-utils for path resolution

lamp/jTools/SetFile/SetFile.cc
@@ -3,6 +3,16 @@
  *	==========
  */
 
+// Mac OS X
+#ifdef __APPLE__
+#include <CoreServices/CoreServices.h>
+#endif
+
+// Mac OS
+#ifndef __FILES__
+#include <Files.h>
+#endif
+
 // Standard C
 #include <stdlib.h>
 
@@ -13,18 +23,14 @@
 #include "gear/inscribe_decimal.hh"
 #include "gear/quad.hh"
 
+// mac-relix-utils
+#include "mac_relix/FSSpec_from_path.hh"
+
 // plus
 #include "plus/var_string.hh"
 
 // poseven
 #include "poseven/functions/perror.hh"
-
-// Nitrogen
-#include "Mac/Toolbox/Types/OSStatus.hh"
-#include "Mac/Toolbox/Utilities/ThrowOSStatus.hh"
-
-// Divergence
-#include "Divergence/Utilities.hh"
 
 // OSErrno
 #include "OSErrno/OSErrno.hh"
@@ -114,13 +120,6 @@ namespace tool
 {
 
 namespace p7 = poseven;
-namespace Div = Divergence;
-
-
-static void SetFileInfo( const FSSpec& file )
-{
-	Mac::ThrowOSStatus( set_FInfo( file.vRefNum, file.parID, file.name ) );
-}
 
 
 int Main( int argc, char** argv )
@@ -137,11 +136,17 @@ int Main( int argc, char** argv )
 	{
 		const char* pathname = *free_args++;
 		
-		try
+		OSErr err;
+		FSSpec file;
+		
+		err = mac::relix::FSSpec_from_existing_path( pathname, file );
+		
+		if ( err == noErr )
 		{
-			SetFileInfo( Div::ResolvePathToFSSpec( pathname ) );
+			err = set_FInfo( file.vRefNum, file.parID, file.name );
 		}
-		catch ( const Mac::OSStatus& err )
+		
+		if ( err != noErr )
 		{
 			plus::var_string status = "OSStatus ";
 			

lamp/jTools/SetFile/A-line.conf
@@ -1,8 +1,7 @@
 product tool
 
 use command
-use Divergence
-use Nitrogen
+use mac-relix-utils
 use OSErrno
 use Orion
 

commit 3e56f96c63c84b93d974493e2eafe19c4ae87e53

SetFile:  Fix file diagnostics in MacRelix builds
This fixes the regression from 78addfcaab3fb9ad6cd84330b1d09ecfdf558de8.

lamp/jTools/SetFile/SetFile.cc
@@ -131,21 +131,24 @@ int main( int argc, char** argv )
 	
 	while ( *free_args )
 	{
+		using mac::sys::Error;
+		using mac::sys::errno_from_mac_error;
+		
 		const char* pathname = *free_args++;
 		
-		OSErr err;
 		FSSpec file;
 		
-		err = mac::relix::FSSpec_from_existing_path( pathname, file );
+		Error err = mac::relix::FSSpec_from_existing_path( pathname, file );
 		
 		if ( err == noErr )
 		{
-			err = set_FInfo( file.vRefNum, file.parID, file.name );
+			err = (Error) set_FInfo( file.vRefNum, file.parID, file.name );
 		}
 		
 		if ( err != noErr )
 		{
-			const int errnum = mac::sys::errno_from_mac_error( err );
+			const int errnum = is_errno( err ) ? errno_from_muxed( err )
+			                                   : errno_from_mac_error( err );
 			
 			const char* message = errnum > 0 ? strerror( errnum )
 			                                 : gear::inscribe_decimal( err );