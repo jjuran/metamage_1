jjuran/metamage_1

commit b41e11fe5f3daa3854a487f74c27d63795abadfd

Genie:  Use non-nitric ResolveAliasFile()

lamp/Genie/Genie/FS/FSTree_FSSpec.cc
@@ -11,6 +11,9 @@
 #endif
 
 // Mac OS
+#ifndef __ALIASES__
+#include <Aliases.h>
+#endif
 #ifndef __PROCESSES__
 #include <Processes.h>
 #endif
@@ -66,9 +69,7 @@
 #include "plus/var_string.hh"
 
 // Nitrogen
-#include "Nitrogen/Aliases.hh"
 #include "Nitrogen/Files.hh"
-#include "Nitrogen/Resources.hh"
 
 // MacIO
 #include "MacIO/GetCatInfo_Sync.hh"
@@ -914,7 +915,17 @@ namespace Genie
 			}
 			else if ( aliases_present  &&  fInfo.fdFlags & kIsAlias )
 			{
-				FSSpec target = N::ResolveAliasFile( extra.fsspec, false );
+				OSErr  err;
+				FSSpec target;
+				
+				Boolean dir;
+				Boolean alias;
+				
+				bool resolveChains = false;
+				
+				err = ResolveAliasFile( &target, resolveChains, &dir, &alias );
+				
+				Mac::ThrowOSStatus( err );
 				
 				return FSTreeFromFSSpec( target );
 			}

commit 71d0701ec169c42359b2545383a73ee91992cfa0

Genie:  Fix lstat() via hfs_resolve()
This appeared to affect Mac OS 8.1 but not Mac OS 9, oddly enough.  This
broke ld as it tried to locate system libraries.

This fixes the regression from b41e11fe5f3daa3854a487f74c27d63795abadfd.

lamp/Genie/Genie/FS/FSTree_FSSpec.cc
@@ -923,12 +923,13 @@ namespace Genie
 			else if ( aliases_present  &&  fInfo.fdFlags & kIsAlias )
 			{
 				OSErr  err;
-				FSSpec target;
 				
 				Boolean dir;
 				Boolean alias;
 				
 				bool resolveChains = false;
+				
+				FSSpec target = extra.fsspec;
 				
 				err = ResolveAliasFile( &target, resolveChains, &dir, &alias );
 				