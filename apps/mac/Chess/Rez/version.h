5ef4185add081d43ababdc2a523876650e02dd27
Josh Juran <jjuran@metamage.com>
2025-01-19 19:49:35 -0500

    Chess:  Raise version to 0.1.1

--- apps/mac/Chess/Rez/version.h
+++ apps/mac/Chess/Rez/version.h
@@ -1,11 +1,11 @@
-#define COPYRIGHT  "2024 Josh Juran"
+#define COPYRIGHT  "2024-2025 Josh Juran"
 
 #define CREATOR  'Kngt'
 
-#define VERSION  "0.1.0"
+#define VERSION  "0.1.1"
 
 #define MAJOR  0
 #define MINOR  1
-#define POINT  0
+#define POINT  1
 #define STAGE  release
-#define PATCH  1
+#define PATCH  0
