aef94de5c3d8e8f42e4122895e1d1da6328bd139
Josh Juran <jjuran@metamage.com>
2025-01-22 14:09:06 -0500

    ams-ui:  Clear entire menu bar rect before drawing
    Write black pixels directly to screen memory (circumventing the clipping
    we'd get via the QuickDraw API).  This ensures that the upper corners of
    the screen are black and also subsumes calling PaintRect() for the edge.

--- 68k/modules/ams-ui/MBDF.cc
+++ 68k/modules/ams-ui/MBDF.cc
@@ -6,6 +6,7 @@
 #include "MBDF.hh"
 
 // ams-common
+#include "callouts.hh"
 #include "QDGlobals.hh"
 #include "raster_lock.hh"
 
@@ -13,6 +14,7 @@
 #pragma exceptions off
 
 
+short ScreenRow  : 0x0106;
 short MBarHeight : 0x0BAA;
 
 
@@ -30,14 +32,17 @@ void draw_menu_bar_from_WMgr_port()
 {
 	// WMgrPort and a suitable clipRgn have already been set.
 	
+	const Byte black = 0xFF;
+	
 	const QDGlobals& qd = get_QDGlobals();
 	
 	Rect menu_bar = qd.screenBits.bounds;
 	
-	const short bottom = MBarHeight;
-	
 	raster_lock lock;
 	
-	menu_bar.bottom = bottom;      EraseRect( &menu_bar );
-	menu_bar.top    = bottom - 1;  PaintRect( &menu_bar );
+	fast_memset( qd.screenBits.baseAddr, black, ScreenRow * MBarHeight );
+	
+	menu_bar.bottom = MBarHeight - 1;
+	
+	EraseRect( &menu_bar );
 }
