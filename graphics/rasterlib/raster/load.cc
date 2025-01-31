jjuran/metamage_1

commit d5047c358b9e8c7a93afbe9383a322b1dab59f57

rasterlib:  Make undriven raster memory read-only
Back when we were using an interprocess mutex and condvar to synchronize
screen updates, the front end needed write access to raster memory.  But
once we switched to signals (and now, FIFO blocking) it was unnecessary.

graphics/rasterlib/raster/load.cc
@@ -237,8 +237,10 @@ namespace raster
 			return invalid_raster();
 		}
 		
-		const int mmap_prot  = PROT_READ | PROT_WRITE;
-		const int mmap_flags = synchronized ? MAP_SHARED : MAP_PRIVATE;
+		const int mmap_prot  = raster_sync < 0 ? PROT_READ | PROT_WRITE
+		                                       : PROT_READ;
+		const int mmap_flags = synchronized    ? MAP_SHARED
+		                                       : MAP_PRIVATE;
 		
 		void* const addr = mmap( NULL, end, mmap_prot, mmap_flags, fd, 0 );
 		

commit 6c70b04eead98a2a68499716921e70131474b50a

rasterlib:  Include PROT_WRITE for load_raster()
We currently handle cross-endian SKIF files by writing byte-swapped data
back into the raster_metadata structure, so single-frame image capturers
(e.g. CGSKIF and skif2png) need a (PROT_READ|PROT_WRITE) memory mapping.

This fixes the regression from d5047c358b9e8c7a93afbe9383a322b1dab59f57.

(On the other hand, dynamic playback viewers like Amphitheatre and Amber
/don't/ need this, because kinetic images are necessarily written on the
same host on which they're read.)

graphics/rasterlib/raster/load.cc
@@ -237,10 +237,10 @@ namespace raster
 			return invalid_raster();
 		}
 		
-		const int mmap_prot  = raster_sync < 0 ? PROT_READ | PROT_WRITE
-		                                       : PROT_READ;
-		const int mmap_flags = synchronized    ? MAP_SHARED
-		                                       : MAP_PRIVATE;
+		const int mmap_prot  = raster_sync <= 0 ? PROT_READ | PROT_WRITE
+		                                        : PROT_READ;
+		const int mmap_flags = synchronized     ? MAP_SHARED
+		                                        : MAP_PRIVATE;
 		
 		void* const addr = mmap( NULL, end, mmap_prot, mmap_flags, fd, 0 );
 		