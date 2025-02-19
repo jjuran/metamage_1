b82f65ab43226c6ae18c810d6cce7a77672346cb
Josh Juran <jjuran@metamage.com>
2025-02-18 23:37:29 -0500

    vlib:  Compute stdint type size at compile time

--- v/vlib/vlib/types/integer/stdint.cc
+++ v/vlib/vlib/types/integer/stdint.cc
@@ -17,52 +17,32 @@ namespace vlib
 	class StdInt : public Field
 	{
 		public:
-			StdInt( const type_info&  type,
+			StdInt( unsigned          size,
+			        const type_info&  type,
 			        const proc_info&  encode,
 			        const proc_info&  decode );
 	};
 	
-	static
-	unsigned type_size( const type_info& type )
-	{
-		const char* name = type.name;
-		
-		++name;
-		
-		uint8_t a = *name++;
-		uint8_t b = *name;
-		
-		unsigned size = 1;  // if either i8 or u8
-		
-		if ( b != 0 )
-		{
-			a -= '0';
-			b -= '0';
-			
-			size = (a * 10 + b) / 8;
-		}
-		
-		return size;
-	}
-	
-	StdInt::StdInt( const type_info&  type,
+	StdInt::StdInt( unsigned          size,
+	                const type_info&  type,
 	                const proc_info&  encode,
 	                const proc_info&  decode )
 	:
-		Field( type_size( type ), type, encode, decode )
+		Field( size, type, encode, decode )
 	{
 	}
 	
-	#define DEFINE_STDINT_2( type )  \
+	#define DEFINE_STDINT_2( n_bits, type )  \
 	Value type##_type()  \
 	{                    \
-		return StdInt( type##_vtype,          \
+		return StdInt( n_bits / 8,            \
+		               type##_vtype,          \
 		               proc_encode_##type,    \
 		               proc_decode_##type );  \
 	}
 	
 	#define DEFINE_STDINT( signedness, n_bits )  \
-		DEFINE_STDINT_2( signedness##n_bits )
+		DEFINE_STDINT_2( n_bits, signedness##n_bits )
 	
 	DEFINE_STDINT( i, 8  )
 	DEFINE_STDINT( i, 16 )
