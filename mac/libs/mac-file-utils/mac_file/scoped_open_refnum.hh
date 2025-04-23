72d790012e41887f3ff7d98c98c4a8ddc3928884
Josh Juran <jjuran@metamage.com>
2025-04-23 03:45:19 -0400

    mac-file-utils:  Add scoped_open_refnum

---
+++ mac/libs/mac-file-utils/mac_file/scoped_open_refnum.hh
@@ -0,0 +1,62 @@
+/*
+	scoped_open_refnum.hh
+	---------------------
+*/
+
+#ifndef MACFILE_SCOPEDOPENREFNUM_HH
+#define MACFILE_SCOPEDOPENREFNUM_HH
+
+// Mac OS X
+#ifdef __APPLE__
+#include <CoreServices/CoreServices.h>
+#endif
+
+// Mac OS
+#ifndef __FILES__
+#include <Files.h>
+#endif
+
+// iota
+#include "iota/class.hh"
+
+
+namespace mac  {
+namespace file {
+	
+	class scoped_open_refnum
+	{
+		NON_COPYABLE( scoped_open_refnum )
+		NO_NEW_DELETE
+		
+		private:
+			FSIORefNum its_refnum;
+		
+		public:
+			scoped_open_refnum( FSIORefNum refnum ) : its_refnum( refnum )
+			{
+			}
+			
+			~scoped_open_refnum();
+			
+			FSIORefNum get() const  { return its_refnum; }
+			
+			operator FSIORefNum() const  { return get(); }
+	};
+	
+#if ! __LP64__
+	
+	inline
+	scoped_open_refnum::~scoped_open_refnum()
+	{
+		if ( its_refnum > 0 )
+		{
+			FSClose( its_refnum );
+		}
+	}
+	
+#endif  // #if ! __LP64__
+	
+}
+}
+
+#endif
