ca952aebafa650a096b8b0ee49c57a1c03db3f2d
Josh Juran <jjuran@metamage.com>
2025-06-02 05:13:14 -0400

    MacBinaryDecoder:  Refactor 'vers' resource source

---
+++ apps/mac/MacBinaryDecoder/Rez/version.h
@@ -0,0 +1,11 @@
+#define COPYRIGHT  "2007-2016 Josh Juran"
+
+#define CREATOR  'mBin'
+
+#define VERSION  "0.5.0"
+
+#define MAJOR  0
+#define MINOR  5
+#define POINT  0
+#define STAGE  release
+#define PATCH  0
