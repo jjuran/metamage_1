911ceef1674b26ddab787b9b4826d27b6f957b9c
Josh Juran <jjuran@metamage.com>
2025-06-02 05:22:37 -0400

    MacBinaryDecoder:  Remove dependency on Pedestal

--- apps/mac/MacBinaryDecoder/MacBinaryDecoder.cc
+++ apps/mac/MacBinaryDecoder/MacBinaryDecoder.cc
@@ -3,40 +3,237 @@
 	-------------------
 */
 
-// Pedestal
-#include "Pedestal/AboutBox.hh"
-#include "Pedestal/Application.hh"
-#include "Pedestal/Commands.hh"
+// Mac OS X
+#ifdef __APPLE__
+#include <Carbon/Carbon.h>
+#endif
+
+// Mac OS
+#ifndef __MACWINDOWS__
+#include <MacWindows.h>
+#endif
+
+// mac-config
+#include "mac_config/apple-events.hh"
+#include "mac_config/desk-accessories.hh"
+
+// mac-sys-utils
+#include "mac_sys/gestalt.hh"
+#include "mac_sys/trap_available.hh"
+
+// mac-qd-utils
+#include "mac_qd/wide_drag_area.hh"
+
+// mac-app-utils
+#include "mac_app/about_box.hh"
+#include "mac_app/DAs.hh"
+#include "mac_app/event_handlers.hh"
+#include "mac_app/init.hh"
+#include "mac_app/menus.hh"
+#include "mac_app/state.hh"
 
 // MacBinaryDecoder
 #include "file_open.hh"
 
 
-namespace Ped = Pedestal;
+#pragma exceptions off
 
-static bool About( Ped::CommandCode )
+
+#define CONFIG_DAs CONFIG_DESK_ACCESSORIES
+
+#if TARGET_API_MAC_CARBON
+#define SystemTask()  /**/
+#endif
+
+
+using mac::qd::wide_drag_area;
+
+
+static inline
+void close_window( WindowRef window )
 {
-	Ped::ShowAboutBox();
-	
-	return true;
+	if ( ! mac::app::close_About_box( window ) )
+	{
+		// shouldn't happen
+	}
 }
 
 static
-bool FileOpenDialog( Ped::CommandCode )
+void menu_item_chosen( long choice )
 {
-	file_open();
+	short menu = choice >> 16;
+	short item = choice;
 	
-	return true;
+	switch ( menu )
+	{
+		case 1:  // (Apple)
+			if ( item == 1 )
+			{
+				// About...
+				const OSType creator = 'mBin';
+				
+				mac::app::show_About_box( creator );
+			}
+			else if ( ! TARGET_API_MAC_CARBON )
+			{
+				mac::app::open_DA_from_menu( item );
+			}
+			break;
+		
+		case 2:  // File
+			switch ( item )
+			{
+				case 1:  // New
+					break;
+				
+				case 2:  // Open
+					file_open();
+					break;
+				
+				case 3:  // Close
+					if ( CONFIG_DAs  &&  mac::app::close_front_DA() )
+					{
+						// done
+					}
+					else if ( WindowRef window = FrontWindow() )
+					{
+						close_window( window );
+					}
+					break;
+				
+				case 4:  // -
+				case 5:  // Quit
+					mac::app::quitting = true;
+					break;
+				
+				default:
+					break;
+			}
+		
+		case 3:  // Edit
+			break;
+		
+		default:
+			break;
+	}
+	
+	HiliteMenu( 0 );
+}
+
+static inline
+bool has_WaitNextEvent()
+{
+	enum { _WaitNextEvent = 0xA860 };
+	
+	return ! TARGET_CPU_68K  ||  mac::sys::trap_available( _WaitNextEvent );
+}
+
+static inline
+Boolean WaitNextEvent( EventRecord& event )
+{
+	return WaitNextEvent( everyEvent, &event, 0x7FFFFFFF, NULL );
+}
+
+static
+Boolean wait_next_event( EventRecord& event )
+{
+	SystemTask();
+	
+	return GetNextEvent( everyEvent, &event );
 }
 
 int main( void )
 {
-	Ped::Application app;
+	using mac::app::quitting;
+	
+	mac::app::init_toolbox();
+	mac::app::install_menus();
+	
+	// gestaltAppleEventsAttr = 'evnt'
+	
+	const bool apple_events_present =
+		CONFIG_APPLE_EVENTS  &&
+			(CONFIG_APPLE_EVENTS_GRANTED  ||
+				mac::sys::gestalt( 'evnt' ) != 0);
+	
+	if ( apple_events_present )
+	{
+		mac::app::install_basic_event_handlers();
+	}
 	
 	open_launched_documents();
 	
-	SetCommandHandler( Ped::kCmdAbout, &About );
-	SetCommandHandler( Ped::kCmdOpen,  &FileOpenDialog );
+	const bool has_WNE = has_WaitNextEvent();
 	
-	return app.Run();
+	while ( ! quitting )
+	{
+		EventRecord event;
+		
+		if ( has_WNE ? WaitNextEvent( event ) : wait_next_event( event ) )
+		{
+			WindowRef window;
+			
+			switch ( event.what )
+			{
+				case mouseDown:
+					switch ( FindWindow( event.where, &window ) )
+					{
+						case inMenuBar:
+							menu_item_chosen( MenuSelect( event.where ) );
+							break;
+						
+					#if CONFIG_DESK_ACCESSORIES
+						
+						case inSysWindow:
+							SystemClick( &event, window );
+							break;
+						
+					#endif
+						
+						case inDrag:
+							DragWindow( window, event.where, wide_drag_area() );
+							break;
+						
+						case inGoAway:
+							if ( TrackGoAway( window, event.where ) )
+							{
+								close_window( window );
+							}
+							break;
+						
+						case inContent:
+							SelectWindow( window );
+							break;
+						
+						default:
+							break;
+					}
+					break;
+				
+				case keyDown:
+					if ( event.modifiers & cmdKey )
+					{
+						menu_item_chosen( MenuKey( event.message ) );
+					}
+					break;
+				
+				case updateEvt:
+					// shouldn't happen
+					break;
+				
+			#if CONFIG_APPLE_EVENTS
+				
+				case kHighLevelEvent:
+					(void) AEProcessAppleEvent( &event );
+					break;
+				
+			#endif
+				
+				default:
+					break;
+			}
+		}
+	}
+	
+	return 0;
 }
