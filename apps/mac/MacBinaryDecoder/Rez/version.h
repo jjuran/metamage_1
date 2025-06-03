f9f7cc51bfb96b1eb368ef9d24e5cf95e8bc892d
Josh Juran <jjuran@metamage.com>
2025-06-03 00:40:01 -0400

    MacBinaryDecoder:  Raise version to v0.5.1

--- apps/mac/MacBinaryDecoder/Rez/version.h
+++ apps/mac/MacBinaryDecoder/Rez/version.h
@@ -1,11 +1,11 @@
-#define COPYRIGHT  "2007-2016 Josh Juran"
+#define COPYRIGHT  "2007-2025 Josh Juran"
 
 #define CREATOR  'mBin'
 
-#define VERSION  "0.5.0"
+#define VERSION  "0.5.1"
 
 #define MAJOR  0
 #define MINOR  5
-#define POINT  0
+#define POINT  1
 #define STAGE  release
 #define PATCH  0
