c8619d329dac5e19bf9ebccd379211e9d196b8a8
Josh Juran <jjuran@metamage.com>
2025-06-10 10:46:24 -0400

    ams-qd:  Use substitute QDProcs for NULL grafProcs
    These pointer-addressable trap calls are used to replace the inline trap
    words at call sites, simplifying the implementation of custom grafProcs.

--- 68k/modules/ams-qd/GrafProcs.cc
+++ 68k/modules/ams-qd/GrafProcs.cc
@@ -5,9 +5,67 @@
 
 #include "GrafProcs.hh"
 
+// Mac OS
+#ifndef __QUICKDRAW__
+#include <Quickdraw.h>
+#endif
+
 // log-of-war
 #include "logofwar/report.hh"
 
+
+enum
+{
+	_StdText    = 0xA882,
+	_StdLine    = 0xA890,
+	_StdRect    = 0xA8A0,
+	_StdRRect   = 0xA8AF,
+	_StdOval    = 0xA8B6,
+	_StdArc     = 0xA8BD,
+	_StdPoly    = 0xA8C5,
+	_StdRgn     = 0xA8D1,
+	_StdBits    = 0xA8EB,
+	_StdTxMeas  = 0xA8ED,
+	_StdGetPic  = 0xA8EE,
+	_StdPutPic  = 0xA8F0,
+	_StdComment = 0xA8F1,
+	
+	autoPop = 0x0400,
+};
+
+static const UInt16 QDProc_traps[] =
+{
+	_StdText    | autoPop,
+	_StdLine    | autoPop,
+	_StdRect    | autoPop,
+	_StdRRect   | autoPop,
+	_StdOval    | autoPop,
+	_StdArc     | autoPop,
+	_StdPoly    | autoPop,
+	_StdRgn     | autoPop,
+	_StdBits    | autoPop,
+	_StdComment | autoPop,
+	_StdTxMeas  | autoPop,
+	_StdGetPic  | autoPop,
+	_StdPutPic  | autoPop,
+};
+
+const QDProcs null_QDProcs =
+{
+	(QDTextUPP)    &QDProc_traps[  0 ],
+	(QDLineUPP)    &QDProc_traps[  1 ],
+	(QDRectUPP)    &QDProc_traps[  2 ],
+	(QDRRectUPP)   &QDProc_traps[  3 ],
+	(QDOvalUPP)    &QDProc_traps[  4 ],
+	(QDArcUPP)     &QDProc_traps[  5 ],
+	(QDPolyUPP)    &QDProc_traps[  6 ],
+	(QDRgnUPP)     &QDProc_traps[  7 ],
+	(QDBitsUPP)    &QDProc_traps[  8 ],
+	(QDCommentUPP) &QDProc_traps[  9 ],
+	(QDTxMeasUPP)  &QDProc_traps[ 10 ],
+	(QDGetPicUPP)  &QDProc_traps[ 11 ],
+	(QDPutPicUPP)  &QDProc_traps[ 12 ],
+};
 
 bool custom_grafProc( const char* proc_name )
 {
--- 68k/modules/ams-qd/GrafProcs.hh
+++ 68k/modules/ams-qd/GrafProcs.hh
@@ -11,16 +11,23 @@
 	if ((port).grafProcs  &&  (port).grafProcs->name) \
 		{static bool has = custom_grafProc(#name);} else
 
-#define GRAFPROC_ARC( port )     StdArc
-#define GRAFPROC_BITS( port )    StdBits
-#define GRAFPROC_LINE( port )    StdLine
-#define GRAFPROC_OVAL( port )    StdOval
-#define GRAFPROC_POLY( port )    StdPoly
-#define GRAFPROC_RECT( port )    StdRect
-#define GRAFPROC_RGN( port )     StdRgn
-#define GRAFPROC_RRECT( port )   StdRRect
-#define GRAFPROC_TEXT( port )    StdText
-#define GRAFPROC_TXMEAS( port )  StdTxMeas
+#define GRAFPROCS( port, name )  \
+	(&null_QDProcs)
+
+#define GRAFPROC( port, name )  GRAFPROCS( port, name )->name
+
+#define GRAFPROC_ARC( port )     GRAFPROC( port, arcProc     )
+#define GRAFPROC_BITS( port )    GRAFPROC( port, bitsProc    )
+#define GRAFPROC_LINE( port )    GRAFPROC( port, lineProc    )
+#define GRAFPROC_OVAL( port )    GRAFPROC( port, ovalProc    )
+#define GRAFPROC_POLY( port )    GRAFPROC( port, polyProc    )
+#define GRAFPROC_RECT( port )    GRAFPROC( port, rectProc    )
+#define GRAFPROC_RGN( port )     GRAFPROC( port, rgnProc     )
+#define GRAFPROC_RRECT( port )   GRAFPROC( port, rRectProc   )
+#define GRAFPROC_TEXT( port )    GRAFPROC( port, textProc    )
+#define GRAFPROC_TXMEAS( port )  GRAFPROC( port, txMeasProc  )
+
+extern const struct QDProcs null_QDProcs;
 
 bool custom_grafProc( const char* proc_name );
 
