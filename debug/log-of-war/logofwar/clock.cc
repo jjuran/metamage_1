9a2fd0066ab763ac1970761918e90cf1014c2397
Josh Juran <jjuran@metamage.com>
2025-06-11 19:42:34 -0400

    log-of-war:  Optimize 68K division in timeofday()

--- debug/log-of-war/logofwar/clock.cc
+++ debug/log-of-war/logofwar/clock.cc
@@ -12,13 +12,32 @@
 namespace logofwar
 {
 
+#ifdef __MC68K__
+
+static inline
+asm
+unsigned short divu_1000( unsigned x : __D0 )
+{
+	DIVU.W   #1000,D0
+}
+
+#else
+
+static inline
+unsigned short divu_1000( unsigned x )
+{
+	return x / 1000;
+}
+
+#endif
+
 static
 unsigned long long timeofday()
 {
 	timeval tv;
 	gettimeofday( &tv, 0 );  // NULL
 	
-	return tv.tv_sec * 1000ull + tv.tv_usec / 1000;
+	return tv.tv_sec * 1000ull + divu_1000( tv.tv_usec );
 }
 
 static unsigned long long start = timeofday();
