055a854ba51fdd76f8f325e3b7fb4c2bf6aed065
Josh Juran <jjuran@metamage.com>
2025-06-29 19:02:57 -0400

    vxo:  Add datum_max_size constant

--- base/vxo/vxo/datum_storage.hh
+++ base/vxo/vxo/datum_storage.hh
@@ -10,6 +10,8 @@
 namespace vxo
 {
 	
+	typedef long datum_ssize_t;
+	
 	struct datum_allocation
 	{
 		const char*  pointer;
@@ -21,7 +23,8 @@ namespace vxo
 	enum
 	{
 		datum_buffer_size = sizeof (datum_allocation),
-		datum_max_offset  = datum_buffer_size - 1
+		datum_max_offset  = datum_buffer_size - 1,
+		datum_max_size    = (1ul << (sizeof (datum_ssize_t) * 8 - 1)) - 1,
 	};
 	
 	struct datum_storage
