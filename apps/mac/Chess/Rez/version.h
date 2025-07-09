3a337195731d1e4fdcf48679b1766dd2fde646dd
Josh Juran <jjuran@metamage.com>
2025-07-09 10:10:06 -0400

    Chess:  Raise version to 0.2.2

--- apps/mac/Chess/Rez/version.h
+++ apps/mac/Chess/Rez/version.h
@@ -2,10 +2,10 @@
 
 #define CREATOR  'Kngt'
 
-#define VERSION  "0.2.1"
+#define VERSION  "0.2.2"
 
 #define MAJOR  0
 #define MINOR  2
-#define POINT  1
+#define POINT  2
 #define STAGE  release
 #define PATCH  0
