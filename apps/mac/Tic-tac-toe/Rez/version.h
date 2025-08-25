d29df238f5d5c11a6973de4c9d197019aa28ae2a
Josh Juran <jjuran@metamage.com>
2025-08-25 03:39:22 -0400

    Tic-tac-toe:  Raise classic version to 0.3.7

--- apps/mac/Tic-tac-toe/Rez/version.h
+++ apps/mac/Tic-tac-toe/Rez/version.h
@@ -2,10 +2,10 @@
 
 #define CREATOR  'XvO#'
 
-#define VERSION  "0.3.6"
+#define VERSION  "0.3.7"
 
 #define MAJOR  0
 #define MINOR  3
-#define POINT  6
+#define POINT  7
 #define STAGE  release
 #define PATCH  0
