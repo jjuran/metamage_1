3eef33b19c256239552527f47155c8ff64d63dd7
Josh Juran <jjuran@metamage.com>
2025-08-28 22:08:56 -0400

    MacPaintTuneup:  Throttle StillDown() to save CPU

--- mac/hacks/MacPaintTuneup/MacPaintTuneup.cc
+++ mac/hacks/MacPaintTuneup/MacPaintTuneup.cc
@@ -22,6 +22,16 @@
 	is also sometimes paired with HideCursor(), so we also
 	have to patch the latter for parity with the former.
 	
+	Another issue affects hand-tool scrolling, as well as
+	many other (if not all) operations that track the mouse.
+	The consistent pattern is a loop that continues until
+	StillDown() is false, and in the interim, consumes all
+	the CPU time that's available (to a single thread).
+	
+	In this case, we patch StillDown() to sleep for one
+	tick if the mouse is stationary with the button held
+	down and StillDown() is being called repeatedly.
+	
 */
 
 // Mac OS
@@ -31,6 +41,9 @@
 #ifndef __TRAPS__
 #include <Traps.h>
 #endif
+
+// mac-glue-utils
+#include "mac_glue/OSUtils.hh"
 
 // mac-sys-utils
 #include "mac_sys/trap_address.hh"
@@ -42,12 +55,18 @@
 #pragma exceptions off
 
 
+UInt32 Mouse : 0x0830;
+
+
 typedef pascal void (*ShieldCursor_ProcPtr)( const Rect* rect, Point offset );
+
+typedef pascal Boolean (*StillDown_ProcPtr)();
 
 static UniversalProcPtr  old_ExitToShell;
 static UniversalProcPtr  old_HideCursor;
 static UniversalProcPtr  old_ShieldCursor;
 static UniversalProcPtr  old_ShowCursor;
+static UniversalProcPtr  old_StillDown;
 static UniversalProcPtr  old_TEInit;
 
 static
@@ -78,12 +97,43 @@ void ShowCursor_patch()
 }
 
 static
+pascal
+Boolean StillDown_patch()
+{
+	StillDown_ProcPtr StillDown = (StillDown_ProcPtr) old_StillDown;
+	
+	Boolean stillDown = StillDown();
+	
+	if ( stillDown )
+	{
+		static UInt32 prev_Mouse;
+		static UInt32 prev_Ticks;
+		
+		UInt32 mouse = Mouse;
+		UInt32 ticks = get_Ticks();
+		
+		if ( mouse == prev_Mouse  &&  ticks == prev_Ticks )
+		{
+			mac::glue::delay( 1 );  // throttle to avoid 100% CPU use
+			
+			return StillDown();
+		}
+		
+		prev_Mouse = mouse;
+		prev_Ticks = ticks;
+	}
+	
+	return stillDown;
+}
+
+static
 void ExitToShell_patch()
 {
 	mac::sys::set_trap_address( old_ExitToShell,  _ExitToShell  );
 	mac::sys::set_trap_address( old_HideCursor,   _HideCursor   );
 	mac::sys::set_trap_address( old_ShieldCursor, _ShieldCursor );
 	mac::sys::set_trap_address( old_ShowCursor,   _ShowCursor   );
+	mac::sys::set_trap_address( old_StillDown,    _StillDown    );
 	
 	asm
 	{
@@ -98,11 +148,13 @@ void install_trap_patches()
 	old_HideCursor   = mac::sys::get_trap_address( _HideCursor   );
 	old_ShieldCursor = mac::sys::get_trap_address( _ShieldCursor );
 	old_ShowCursor   = mac::sys::get_trap_address( _ShowCursor   );
+	old_StillDown    = mac::sys::get_trap_address( _StillDown    );
 	
 	mac::sys::set_trap_address( (ProcPtr) ExitToShell_patch,  _ExitToShell  );
 	mac::sys::set_trap_address( (ProcPtr) HideCursor_patch,   _HideCursor   );
 	mac::sys::set_trap_address( (ProcPtr) ShieldCursor_patch, _ShieldCursor );
 	mac::sys::set_trap_address( (ProcPtr) ShowCursor_patch,   _ShowCursor   );
+	mac::sys::set_trap_address( (ProcPtr) StillDown_patch,    _StillDown    );
 }
 
 static
