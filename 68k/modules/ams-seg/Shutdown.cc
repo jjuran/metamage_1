d8f185bcfc9700dca6a91654b8f6cb555cb2ccc2
Josh Juran <jjuran@metamage.com>
2025-08-30 17:41:41 -0400

    ams-seg:  Add ShutDwnInstall() call

--- 68k/modules/ams-seg/Shutdown.cc
+++ 68k/modules/ams-seg/Shutdown.cc
@@ -6,8 +6,17 @@
 #include "Shutdown.hh"
 
 // Mac OS
+#ifndef __MACMEMORY__
+#include <MacMemory.h>
+#endif
+#ifndef __OSUTILS__
+#include <OSUtils.h>
+#endif
 #ifndef __SCRAP__
 #include <Scrap.h>
+#endif
+#ifndef __SHUTDOWN__
+#include <ShutDown.h>
 #endif
 
 // POSIX
@@ -26,6 +35,44 @@
 #define COUNT(array)  (sizeof (array) / sizeof *(array))
 
 
+struct shutdown_task
+{
+	QElemPtr    qLink;
+	short       qType;
+	short       flags;
+	ShutDwnUPP  proc;
+};
+
+static QHdr ShutdownQueue;
+
+static
+void call_procs( short selector : __D0, short phase )
+{
+	volatile QElemPtr* addr = &ShutdownQueue.qHead;
+	
+	while ( shutdown_task* next = (shutdown_task*) *addr )
+	{
+		const short flags = next->flags;
+		const short which = flags & sdRestartOrPower;  // 1 or 2, or 0
+		
+		if ( which == 0  ||  which == selector )
+		{
+			if ( flags - which == phase )
+			{
+				next->proc();
+				
+				*addr = next->qLink;
+				
+				DisposePtr( (Ptr) next );
+				
+				continue;
+			}
+		}
+		
+		addr = &next->qLink;
+	}
+}
+
 static
 void unimplemented_call( short selector : __D0 )
 {
@@ -39,10 +86,16 @@ void ShutDown_call( short selector : __D0 )
 {
 	/*
 		TODO:  Perform various housekeeping tasks, like
-		closing drivers and calling shutdown handlers.
+		telling drivers goodbye and unmounting volumes.
 	*/
 	
+	call_procs( selector, sdOnDrivers );
+	
 	UnloadScrap();
+	
+	call_procs( selector, sdOnUnmount );
+	
+	call_procs( selector, 0 );  // either sdOnPowerOff or sdOnRestart
 	
 	if ( linger_on_exit )
 	{
@@ -52,11 +105,26 @@ void ShutDown_call( short selector : __D0 )
 	_exit( 0 );
 }
 
+static
+pascal
+void ShutDwnInstall_call( ShutDwnUPP proc, short flags )
+{
+	typedef shutdown_task Task;
+	
+	Task* task = (Task*) NewPtrSysClear( sizeof (Task) );
+	
+	task->flags = flags;
+	task->proc  = proc;
+	
+	Enqueue( (QElemPtr) task, &ShutdownQueue );
+}
+
 static void* handlers[] =
 {
 	&unimplemented_call,
-	&ShutDown_call,  // ShutDwnPower
-	&ShutDown_call,  // ShutDwnStart
+	&ShutDown_call,        // ShutDwnPower
+	&ShutDown_call,        // ShutDwnStart
+	&ShutDwnInstall_call,
 };
 
 enum
