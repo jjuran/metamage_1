7abbbe67c87a109a22e82e4acf72673df6c310a7
Josh Juran <jjuran@metamage.com>
2025-09-08 06:39:55 -0400

    mac-rsrc-utils:  Use OSErr type alias in header

--- mac/libs/mac-rsrc-utils/mac_rsrc/create_res_file.hh
+++ mac/libs/mac-rsrc-utils/mac_rsrc/create_res_file.hh
@@ -9,11 +9,13 @@
 struct FSRef;
 struct FSSpec;
 
+typedef short OSErr;
+
 namespace mac  {
 namespace rsrc {
 	
-	short create_res_file( const FSSpec& file );
-	short create_res_file( const FSRef&  file );
+	OSErr create_res_file( const FSSpec& file );
+	OSErr create_res_file( const FSRef&  file );
 	
 }
 }
