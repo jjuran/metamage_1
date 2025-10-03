jjuran/metamage_1

commit cfe5900d73b46d4e68393a0dbdb7b45e61512a87

ams-rsrc:  Detect and report compressed resources

68k/modules/ams-rsrc/dcmp.cc
@@ -1,0 +1,81 @@
+/*
+	dcmp.cc
+	-------
+*/
+
+#include "dcmp.hh"
+
+// Mac OS
+#ifndef __MACERRORS__
+#include <MacErrors.h>
+#endif
+#ifndef __MACTYPES__
+#include <MacTypes.h>
+#endif
+
+// log-of-war
+#include "logofwar/report.hh"
+
+// ams-common
+#include "callouts.hh"
+
+
+enum
+{
+	kCompressedResourceSignature = 0xA89F6572,
+};
+
+
+short ResErr : 0x0A60;
+
+
+struct compressed_rsrc_header
+{
+	UInt32 signature;
+	UInt16 length;
+	UInt8  format;
+	UInt8  unknown;
+};
+
+Handle PtrToHand_dcmp( const void* p : __A0, UInt32 size : __D0 )
+{
+	compressed_rsrc_header header;
+	
+	ResErr = CantDecompress;
+	
+	if ( size < sizeof header )
+	{
+		ERROR = "Compressed resource lacks a metadata header";
+		return NULL;
+	}
+	
+	fast_memcpy( &header, p, sizeof header );  // possibly misaligned
+	
+	if ( header.signature != kCompressedResourceSignature )
+	{
+		ERROR = "Compressed resource has unrecognized signature";
+		return NULL;
+	}
+	
+	if ( header.length < sizeof header )
+	{
+		ERROR = "Compressed resource has corrupted header";
+		return NULL;
+	}
+	
+	if ( size < header.length )
+	{
+		ERROR = "Compressed resource has truncated header";
+		return NULL;
+	}
+	
+	if ( header.format - 8 > 1 )
+	{
+		ERROR = "Compressed resource has unknown format ", header.format;
+		return NULL;
+	}
+	
+	ERROR = "Resource compression is unimplemented";
+	
+	return NULL;
+}

68k/modules/ams-rsrc/Resources.cc
@@ -32,6 +32,7 @@
 #include "unglue.hh"
 
 // ams-rsrc
+#include "dcmp.hh"
 #include "rsrc_fork.hh"
 
 
@@ -45,6 +46,8 @@ enum
 	resLocked    = 16,
 	resProtected = 8,
 	resChanged   = 2,
+	
+	resCompressed = 1,  // undocumented
 };
 
 
@@ -603,7 +606,34 @@ Handle new_res_handle( RsrcMapHandle rsrc_map, rsrc_header& rsrc, ResType type )
 		Size data_length;
 		fast_memcpy( &data_length, &data->length, sizeof (Size) );
 		
-		if ( Handle h = PtrToHand( data->bytes, data_length ) )
+		/*
+			Pessimistically assume memFullErr.  If we fail otherwise
+			in PtrToHand_dcmp(), it will have already set ResErr.
+		*/
+		
+		ResErr = memFullErr;
+		
+		Handle h;
+		
+		if ( rsrc.attrs & resCompressed )
+		{
+			if ( type == 'dcmp' )
+			{
+				ERROR = "Resource decompression code 'dcmp' id=", rsrc.id,
+				        " is itself compressed";
+				
+				ResErr = CantDecompress;
+				return NULL;
+			}
+			
+			h = PtrToHand_dcmp( data->bytes, data_length );
+		}
+		else
+		{
+			h = PtrToHand( data->bytes, data_length );
+		}
+		
+		if ( h )
 		{
 			master_pointer& mp = *(master_pointer*) h;
 			
@@ -626,7 +656,6 @@ Handle new_res_handle( RsrcMapHandle rsrc_map, rsrc_header& rsrc, ResType type )
 		}
 		else
 		{
-			ResErr = memFullErr;
 			return NULL;
 		}
 	}

68k/modules/ams-rsrc/dcmp.hh
@@ -1,0 +1,15 @@
+/*
+	dcmp.hh
+	-------
+*/
+
+#ifndef DCMP_HH
+#define DCMP_HH
+
+
+typedef char**         Handle;
+typedef unsigned long  UInt32;
+
+Handle PtrToHand_dcmp( const void* p : __A0, UInt32 size : __D0 );
+
+#endif

commit 4f0708a99906d081bf27598cc03ae9de750f8f15

ams-rsrc:  Load spuriously resCompressed resources
Spectre Challenger has uncompressed 'snd ' resources attributed as such.

This fixes the regression from cfe5900d73b46d4e68393a0dbdb7b45e61512a87.

68k/modules/ams-rsrc/dcmp.cc
@@ -18,6 +18,7 @@
 
 // ams-common
 #include "callouts.hh"
+#include "unglue.hh"
 
 
 #pragma exceptions off
@@ -44,21 +45,31 @@ Handle PtrToHand_dcmp( const void* p : __A0, UInt32 size : __D0 )
 {
 	compressed_rsrc_header header;
 	
-	ResErr = CantDecompress;
+	/*
+		If the resource is shorter than the compressed
+		resource header or the signature doesn't match,
+		then assume the resource isn't actually compressed.
+	*/
 	
 	if ( size < sizeof header )
 	{
-		ERROR = "Compressed resource lacks a metadata header";
-		return NULL;
+		ERROR = "resCompressed resource lacks a metadata header";
+		
+		goto uncompressed;
 	}
 	
 	fast_memcpy( &header, p, sizeof header );  // possibly misaligned
 	
 	if ( header.signature != kCompressedResourceSignature )
 	{
-		ERROR = "Compressed resource has unrecognized signature";
-		return NULL;
+		ERROR = "resCompressed resource has unrecognized signature";
+		
+	uncompressed:
+		
+		return PtrToHand( p, size );
 	}
+	
+	ResErr = CantDecompress;
 	
 	if ( header.length < sizeof header )
 	{