c8cc646cdc7c58e82e883700de11da39759b2fc2
Josh Juran <jjuran@metamage.com>
2025-10-14 03:59:27 -0400

    StuntCopterTuneup:  Raise version to 0.1.1

--- mac/hacks/StuntCopterTuneup/Rez/version.h
+++ mac/hacks/StuntCopterTuneup/Rez/version.h
@@ -1,9 +1,9 @@
 #define COPYRIGHT  "2025 Josh Juran"
 
-#define VERSION  "0.1.0"
+#define VERSION  "0.1.1"
 
 #define MAJOR  0
 #define MINOR  1
-#define POINT  0
+#define POINT  1
 #define STAGE  release
 #define PATCH  0
