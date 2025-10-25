jjuran/metamage_1

commit ab19999df7ca2f2a2f7359352fd176da1f82a205

mac-app-utils:  Add minimal wait_next_event()

mac/libs/mac-app-utils/mac_app/wait_next_event.cc
@@ -1,0 +1,51 @@
+/*
+	wait_next_event.cc
+	------------------
+*/
+
+#include "mac_app/wait_next_event.hh"
+
+// Mac OS X
+#ifdef __APPLE__
+#include <Carbon/Carbon.h>
+#endif
+
+// Mac OS
+#ifndef __EVENTS__
+#include <Events.h>
+#endif
+
+// mac-sys-utils
+#include "mac_sys/has/WaitNextEvent.hh"
+
+
+#ifdef TARGET_CPU_68K
+#define STATIC_68K  static
+#else
+#define STATIC_68K  /**/
+#endif
+
+
+namespace mac {
+namespace app {
+
+Boolean wait_next_event( EventMask event_mask, EventRecord& event )
+{
+	STATIC_68K const bool has_WNE = mac::sys::has_WaitNextEvent();
+	
+	if ( has_WNE )
+	{
+		return WaitNextEvent( everyEvent, &event, 0x7FFFFFFF, NULL );
+	}
+	
+#if CALL_NOT_IN_CARBON
+	
+	SystemTask();
+	
+#endif
+	
+	return GetNextEvent( everyEvent, &event );
+}
+
+}
+}

mac/libs/mac-app-utils/mac_app/wait_next_event.hh
@@ -1,0 +1,34 @@
+/*
+	wait_next_event.hh
+	------------------
+*/
+
+#ifndef MACAPP_WAITNEXTEVENT_HH
+#define MACAPP_WAITNEXTEVENT_HH
+
+
+struct EventRecord;
+
+typedef unsigned char   Boolean;
+typedef unsigned short  EventMask;
+
+namespace mac {
+namespace app {
+	
+	Boolean wait_next_event( EventMask event_mask, EventRecord& event );
+	
+	inline
+	Boolean wait_next_event( EventRecord& event )
+	{
+		enum
+		{
+			everyEvent = 0xFFFF,
+		};
+		
+		return wait_next_event( everyEvent, event );
+	}
+	
+}
+}
+
+#endif

commit 40c74f5bea556a55fe9c84248c219648e1a8acd8

mac-app-utils:  Fix 64-bit builds
Amethyst and Amphitheatre are 64-bit-buildable clients of mac-app-utils.

This fixes the regression from ab19999df7ca2f2a2f7359352fd176da1f82a205.

mac/libs/mac-app-utils/mac_app/wait_next_event.cc
@@ -29,6 +29,12 @@
 namespace mac {
 namespace app {
 
+#if __LP64__
+
+int dummy;
+
+#else
+
 Boolean wait_next_event( EventMask event_mask, EventRecord& event )
 {
 	STATIC_68K const bool has_WNE = mac::sys::has_WaitNextEvent();
@@ -46,6 +52,8 @@ Boolean wait_next_event( EventMask event_mask, EventRecord& event )
 	
 	return GetNextEvent( everyEvent, &event );
 }
+
+#endif
 
 }
 }