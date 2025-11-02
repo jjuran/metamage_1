2f5fdbc43f87cc187c7211ec8cf719372b141358
Josh Juran <jjuran@metamage.com>
2025-11-01 20:12:24 -0400

    aglow -> amicus:  Move menu definitions

--- mac/libs/aglow/aglow/menus.cc
+++ mac/libs/aglow/aglow/menus.cc
@@ -5,37 +5,17 @@
 
 #include "aglow/menus.hh"
 
+// amicus
+#include "amicus/menu_defs.hh"
 
-#define COUNT( array )  (sizeof (array) / sizeof *(array))
-
-#define DEFINE_MENU_WITH_TITLE(name, title)  \
-static const menu_description name##_desc =  \
-{ "\p" title, name##_items, COUNT( name##_items ) }
-
-#define DEFINE_MENU(title)  DEFINE_MENU_WITH_TITLE(title, #title)
 
 #define SET_UP_MENU(name)  (name = set_up_menu( name##_desc ))
 
-
-typedef unsigned char Byte;
 
 MenuRef Apple;
 MenuRef File;
 MenuRef Edit;
 MenuRef View;
-
-struct menu_item_description
-{
-	const Byte*  format;
-	MenuCommand  command;
-};
-
-struct menu_description
-{
-	const Byte*                   title;
-	const menu_item_description*  items;
-	short                         count;
-};
 
 static
 MenuRef set_up_menu( const menu_description& desc )
@@ -62,44 +42,6 @@ MenuRef set_up_menu( const menu_description& desc )
 	
 	return menu;
 }
-
-static const menu_item_description Apple_items[] =
-{
-	{ "\p" "About Advanced Mac Substitute", kHICommandAbout },
-};
-
-static const menu_item_description File_items[] =
-{
-	{ "\p" "Close", kHICommandClose },
-};
-
-static const menu_item_description Edit_items[] =
-{
-	{ "\p" "Undo",  kHICommandUndo  },
-	{},
-	{ "\p" "Cut",   kHICommandCut   },
-	{ "\p" "Copy",  kHICommandCopy  },
-	{ "\p" "Paste", kHICommandPaste },
-	{ "\p" "Clear", kHICommandClear },
-};
-
-static const menu_item_description View_items[] =
-{
-	{ "\p"  "50%", kZoom50Percent  },
-	{ "\p" "100%", kZoom100Percent },
-	{ "\p" "150%", kZoom150Percent },
-	{ "\p" "200%", kZoom200Percent },
-	{ "\p" "250%", kZoom250Percent },
-	{ "\p" "300%", kZoom300Percent },
-	{ "\p" "350%", kZoom350Percent },
-	{ "\p" "400%", kZoom400Percent },
-};
-
-DEFINE_MENU_WITH_TITLE( Apple, "\x14" );
-
-DEFINE_MENU( File );
-DEFINE_MENU( Edit );
-DEFINE_MENU( View );
 
 void set_up_menus()
 {
