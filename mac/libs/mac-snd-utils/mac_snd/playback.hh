48ae2832bde876978f913c348f9336a8b36161d3
Josh Juran <jjuran@metamage.com>
2025-11-09 18:17:50 -0500

    mac-snd-utils:  Add play_now_async(), full_stop()

--- mac/libs/mac-snd-utils/mac_snd/playback.hh
+++ mac/libs/mac-snd-utils/mac_snd/playback.hh
@@ -6,9 +6,56 @@
 #ifndef MACSND_PLAYBACK_HH
 #define MACSND_PLAYBACK_HH
 
+// Mac OS X
+#ifdef __APPLE__
+#include <CoreServices/CoreServices.h>
+#endif
+
+// Mac OS
+#ifndef MAC_OS_X_VERSION_10_5
+#ifndef __DEVICES__
+#include <Devices.h>
+#endif
+#endif
+
 
 namespace mac {
 namespace snd {
+
+enum
+{
+	kSoundDriverRefNum = -4,
+};
+
+inline
+OSErr play_now_async( ParamBlockRec& pb, Ptr p, Size n )
+{
+	pb.ioParam.ioRefNum   = kSoundDriverRefNum;
+	pb.ioParam.ioBuffer   = p;
+	pb.ioParam.ioReqCount = n;
+	
+#if CALL_NOT_IN_CARBON
+	
+	PBKillIOSync( &pb );  // this sync call clears ioCompletion
+	
+#endif
+	
+	return PBWriteAsync( &pb );
+}
+
+inline
+OSErr full_stop( ParamBlockRec& pb )
+{
+	pb.ioParam.ioRefNum = kSoundDriverRefNum;
+	
+#if CALL_NOT_IN_CARBON
+	
+	return PBKillIOSync( &pb );
+	
+#endif
+	
+	return noErr;
+}
 
 }
 }
