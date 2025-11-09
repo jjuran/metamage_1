jjuran/metamage_1

commit f7b3c872a26413aecc0bd5a5dde3cffd04485fd6

system-info:  Report 64-bit PowerPC architecture

tools/mac/system-info/system-info.cc
@@ -71,6 +71,7 @@ using mac::sys::gestalt_defined;
 
 #define M68K    "Motorola 68K"
 #define PPC     "PowerPC"
+#define PPC64   "PowerPC (64-bit)"
 #define X86     "x86"
 #define X86_64  "x86_64"
 #define ARM     "Apple Silicon"
@@ -122,6 +123,7 @@ void compiled()
 	
 	const char* arch = TARGET_CPU_68K    ? COMPILED M68K
 	                 : TARGET_CPU_PPC    ? COMPILED PPC
+	                 : TARGET_CPU_PPC64  ? COMPILED PPC64
 	                 : TARGET_CPU_X86    ? COMPILED X86
 	                 : TARGET_CPU_X86_64 ? COMPILED X86_64
 	                 : TARGET_CPU_ARM64  ? COMPILED ARM

commit 12ef6a306830ed6a210489886e1713fb7328fcbc

system-info:  Fix pre-Mac-OS-X-10.5 builds
This fixes the regression from f7b3c872a26413aecc0bd5a5dde3cffd04485fd6.

tools/mac/system-info/system-info.cc
@@ -53,6 +53,10 @@ enum { kMachineNameStrID = -16395 };
 
 #define STRLEN( s )  (sizeof "" s - 1)
 
+#ifndef TARGET_CPU_PPC64
+#define TARGET_CPU_PPC64  0
+#endif
+
 #ifndef TARGET_CPU_X86_64
 #define TARGET_CPU_X86_64  0
 #endif