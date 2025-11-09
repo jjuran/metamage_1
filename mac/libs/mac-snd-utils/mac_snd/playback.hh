e3718e531acb86a19281d6ffec4d485a9e694af3
Josh Juran <jjuran@metamage.com>
2025-11-09 17:54:18 -0500

    mac-snd-utils:  Axe unused playback routines

--- mac/libs/mac-snd-utils/mac_snd/playback.hh
+++ mac/libs/mac-snd-utils/mac_snd/playback.hh
@@ -6,32 +6,9 @@
 #ifndef MACSND_PLAYBACK_HH
 #define MACSND_PLAYBACK_HH
 
-// SoundDriver
-#include "SoundDriver/SoundDriver.h"
-
 
 namespace mac {
 namespace snd {
-	
-	short play_sound( const void* buffer, long size );
-	
-	inline
-	short play( const SWSynthRec& rec, unsigned size )
-	{
-		return play_sound( &rec, size );
-	}
-	
-	inline
-	short play( const FTSynthRec& rec, unsigned size = sizeof (FTSynthRec) )
-	{
-		return play_sound( &rec, size );
-	}
-	
-	inline
-	short play( const FFSynthRec& rec, unsigned size )
-	{
-		return play_sound( &rec, size );
-	}
 
 }
 }
