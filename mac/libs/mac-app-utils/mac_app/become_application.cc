jjuran/metamage_1

commit cb3d0bc5276e61eaa93d4d94e2a515fd8e2a3891

mac-app-utils:  Add become_application()

mac/libs/mac-app-utils/mac_app/become_application.cc
@@ -1,0 +1,55 @@
+/*
+	become_application.cc
+	---------------------
+*/
+
+#include "mac_app/become_application.hh"
+
+// Mac OS X
+#ifdef __APPLE__
+#include <ApplicationServices/ApplicationServices.h>
+#endif
+
+// Standard C
+#include <stdlib.h>
+#include <string.h>
+
+
+#define STR_LEN( s )  "" s, (sizeof s - 1)
+
+
+namespace mac {
+namespace app {
+
+bool launched_as_application()
+{
+	if ( const char* _ = getenv( "_" ) )
+	{
+		// We were launched from a shell (possibly via open).
+		
+		if ( memcmp( _, STR_LEN( "/usr/bin/open" ) ) != 0 )
+		{
+			// We were not launched via open.
+			
+			return false;
+		}
+	}
+	
+	return true;
+}
+
+void become_application_Carbon()
+{
+	ProcessSerialNumber psn = { 0, kCurrentProcess };
+	
+#ifdef MAC_OS_X_VERSION_10_3
+	
+	TransformProcessType( &psn, kProcessTransformToForegroundApplication );
+	
+#endif
+	
+	SetFrontProcess( &psn );
+}
+
+}
+}

mac/libs/mac-app-utils/mac_app/become_application.hh
@@ -1,0 +1,51 @@
+/*
+	become_application.hh
+	---------------------
+*/
+
+#ifndef MACAPP_BECOMEAPPLICATION_HH
+#define MACAPP_BECOMEAPPLICATION_HH
+
+// Mac OS X
+#ifdef __APPLE__
+#include <AvailabilityMacros.h>
+#endif
+
+
+namespace mac {
+namespace app {
+
+bool launched_as_application();
+
+void become_application_Carbon();
+void become_application_Cocoa();
+
+inline
+int become_application()
+{
+	if ( launched_as_application() )
+	{
+		return -1;  // already an application
+	}
+	
+#if defined( MAC_OS_X_VERSION_10_6 )  &&  defined( __OBJC__ )
+	
+	become_application_Cocoa();
+	
+#elif defined( MAC_OS_X_VERSION_10_3 )
+	
+	become_application_Carbon();
+	
+#else
+	
+	return 0;  // can't become an application
+	
+#endif
+	
+	return 1;  // became an application
+}
+
+}
+}
+
+#endif

mac/libs/mac-app-utils/mac_app/become_application.mm
@@ -1,0 +1,29 @@
+/*
+	become_application.mm
+	---------------------
+*/
+
+#include "mac_app/become_application.hh"
+
+// Mac OS X
+#ifdef __APPLE__
+#include <AppKit/AppKit.h>
+#endif
+
+
+namespace mac {
+namespace app {
+
+void become_application_Cocoa()
+{
+#ifdef MAC_OS_X_VERSION_10_6
+	
+	[NSApp setActivationPolicy: NSApplicationActivationPolicyRegular];
+	
+#endif
+	
+	[NSApp activateIgnoringOtherApps: YES];
+}
+
+}
+}

commit 108e8802e157b0004531a9a53d7e70a1da100ada

mac-app-utils:  Fix classic Mac OS builds
This fixes the regression from cb3d0bc5276e61eaa93d4d94e2a515fd8e2a3891.

mac/libs/mac-app-utils/mac_app/become_application.cc
@@ -10,6 +10,11 @@
 #include <ApplicationServices/ApplicationServices.h>
 #endif
 
+// Mac OS
+#ifndef __PROCESSES__
+#include <Processes.h>
+#endif
+
 // Standard C
 #include <stdlib.h>
 #include <string.h>