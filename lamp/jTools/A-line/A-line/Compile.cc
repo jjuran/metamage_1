jjuran/metamage_1

commit 46a11eebb90ff5e541f1c2e80cf6f828fd586075

A-line:  Don't derive .cpp filenames from .o names
If the .o filename is long enough to require hashing, then any truncated
string will bear an invalid hash, and we want to be MacRelix-compatible.

lamp/jTools/A-line/A-line/Compile.cc
@@ -718,35 +718,16 @@ namespace tool
 	TaskPtr make_unit_tasks( const Project&          project,
 	                         const CompilerOptions&  cpp_options,
 	                         const CompilerOptions&  cc_options,
-	                         const plus::string&     cpp_dir,
 	                         const TaskPtr&          precompile,
 	                         const plus::string&     source_pathname,
+	                         const plus::string&     cppout_pathname,
 	                         const plus::string&     object_pathname )
 	{
-		using gear::find_last_match;
-		
-		const bool preprocessing = ! cpp_dir.empty();
-		
-		plus::string cpp_path;
-		
-		if ( preprocessing )
-		{
-			const char* p = object_pathname.data();
-			
-			const char* it = find_last_match( p, object_pathname.size(), '/' );
-			
-			it = it ? it + 1 : p;
-			
-			plus::string name( it, &*object_pathname.end() - 2 );
-			
-			cpp_path = cpp_dir / name;
-		}
-		
 		TaskPtr task = add_cpp_and_cc_tasks( project,
 		                                     cpp_options,
 		                                     cc_options,
 		                                     source_pathname,
-		                                     cpp_path,
+		                                     cppout_pathname,
 		                                     object_pathname,
 		                                     ProjectDiagnosticsDirPath( project.Name() ),
 		                                     precompile );
@@ -940,6 +921,7 @@ namespace tool
 			cpp_dir = mkdir_path( "cpp" / project.Name() );
 		}
 		
+		StringVector cppout_paths;
 		StringVector object_paths;
 		
 		if ( can_run_Classic() )
@@ -951,18 +933,29 @@ namespace tool
 		
 		const StringVector& source_paths = project.Sources();
 		
+		if ( preprocessing )
+		{
+			const char* _i  = ".i";
+			const char* _ii = ".ii";
+			
+			FillOutputFiles( cpp_dir, source_paths, cppout_paths, _i, _ii );
+		}
+		
 		const size_t n_tools = project.ToolCount();
 		
 		tool_dependencies.reserve( n_tools );
 		
 		for ( size_t i = 0;  i < n_tools;  ++i )
 		{
+			const plus::string& cppout = preprocessing ? cppout_paths[ i ]
+			                                           : plus::string::null;
+			
 			tool_dependencies.push_back( make_unit_tasks( project,
 			                                              preprocess_options,
 			                                              options,
-			                                              cpp_dir,
 			                                              precompile_task,
 			                                              source_paths[ i ],
+			                                              cppout,
 			                                              object_paths[ i ] ) );
 		}
 		
@@ -970,12 +963,15 @@ namespace tool
 		
 		for ( size_t i = n_tools;  i < n;  ++i )
 		{
+			const plus::string& cppout = preprocessing ? cppout_paths[ i ]
+			                                           : plus::string::null;
+			
 			TaskPtr task = make_unit_tasks( project,
 			                                preprocess_options,
 			                                options,
-			                                cpp_dir,
 			                                precompile_task,
 			                                source_paths[ i ],
+			                                cppout,
 			                                object_paths[ i ] );
 			
 			task->AddDependent( source_dependency );

commit 143de9d13ad473cd46f49e8ef5ff33c7c15b5394

A-line:  Silence .i warning with --cpp in MacRelix
When preprocessing separately within MacRelix (which doesn't use nexus),
use a .cpp filename extension (so .i or .ii doesn't confuse Metrowerks).

This fixes the regression from 46a11eebb90ff5e541f1c2e80cf6f828fd586075.

lamp/jTools/A-line/A-line/Compile.cc
@@ -84,8 +84,6 @@ namespace tool
 	                                        const plus::string&     source_pathname,
 	                                        const plus::string&     output_pathname );
 	
-#ifndef __RELIX__
-	
 	static
 	bool using_nexus()
 	{
@@ -93,8 +91,6 @@ namespace tool
 		
 		return nexus_peered;
 	}
-	
-#endif
 	
 	static plus::string diagnostics_file_path( const plus::string&  dir_path,
 	                                           const plus::string&  target_path )
@@ -950,7 +946,13 @@ namespace tool
 			const char* _i  = ".i";
 			const char* _ii = ".ii";
 			
+			bool in_MacRelix = true;
+			
 		#ifndef __RELIX__
+			
+			in_MacRelix = false;
+			
+		#endif
 			
 			/*
 				We're preprocessing separately, which already implies
@@ -967,12 +969,10 @@ namespace tool
 				(If we can't run Classic, we can assume the use of nexus.)
 			*/
 			
-			if ( can_run_Classic()  &&  ! using_nexus() )
+			if ( in_MacRelix  ||  (can_run_Classic()  &&  ! using_nexus()) )
 			{
 				_i = _ii = ".cpp";
 			}
-			
-		#endif
 			
 			FillOutputFiles( cpp_dir, source_paths, cppout_paths, _i, _ii );
 		}