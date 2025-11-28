d0bf40c20891cea2321d289ba5aa9a6d9d33297d
Josh Juran <jjuran@metamage.com>
2025-11-27 22:03:56 -0500

    mac-cg-utils:  Add color-space-less overloads

--- mac/libs/mac-cg-utils/mac_cg/colorspaces.hh
+++ mac/libs/mac-cg-utils/mac_cg/colorspaces.hh
@@ -27,9 +27,22 @@ CGColorSpaceRef generic_or_device_RGB();
 
 CGColorSpaceRef create_inverted_grayscale( CGColorSpaceRef gray, int count );
 
+inline
+CGColorSpaceRef create_inverted_grayscale( int count )
+{
+	return create_inverted_grayscale( generic_or_device_gray(), count );
+}
+
 CGColorSpaceRef create_RGB_palette( CGColorSpaceRef  rgb,
                                     const UInt16*    colors,
                                     int              count );
+
+inline
+CGColorSpaceRef create_RGB_palette( const UInt16*  colors,
+                                    int            count )
+{
+	return create_RGB_palette( generic_or_device_RGB(), colors, count );
+}
 
 }
 }
