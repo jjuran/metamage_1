jjuran/metamage_1

commit 48bb85fe846b7929d8571a81c66ce80e767bb5d4

ams-qd:  Use fused multiply-divide ops in MapRgn()

68k/modules/ams-qd/QDUtils.cc
@@ -353,11 +353,11 @@ pascal void MapRgn_patch( RgnHandle rgn, const Rect* src, const Rect* dst )
 {
 	using quickdraw::Region_end;
 	
-	const int srcHeight = src->bottom - src->top;
-	const int dstHeight = dst->bottom - dst->top;
+	const UInt16 srcHeight = src->bottom - src->top;
+	const UInt16 dstHeight = dst->bottom - dst->top;
 	
-	const int srcWidth = src->right - src->left;
-	const int dstWidth = dst->right - dst->left;
+	const UInt16 srcWidth = src->right - src->left;
+	const UInt16 dstWidth = dst->right - dst->left;
 	
 	short* p = (short*) (*rgn + 1);
 	
@@ -365,13 +365,13 @@ pascal void MapRgn_patch( RgnHandle rgn, const Rect* src, const Rect* dst )
 	{
 		const short v = *p;
 		
-		*p++ = (v - src->top ) * dstHeight / srcHeight + dst->top;
+		*p++ = muldivu_w( v - src->top, dstHeight, srcHeight ) + dst->top;
 		
 		while ( *p != Region_end )
 		{
 			const short h = *p;
 			
-			*p++ = (h - src->left) * dstWidth / srcWidth + dst->left;
+			*p++ = muldivu_w( h - src->left, dstWidth, srcWidth ) + dst->left;
 		}
 		
 		++p;  // skip end-of-row sentinel

commit 04e2b43be4f144177bdcb4c0cbec3942c1058e96

ams-qd:  Fix Map{Pt,Rect,Rgn}() unsigned overflow
This fixes the regression from 48bb85fe846b7929d8571a81c66ce80e767bb5d4.
This fixes the regression from d33c04e3cd6e7133cf529f367e08169485314b2b.

68k/modules/ams-qd/QDUtils.cc
@@ -151,8 +151,8 @@ pascal void MapPt_patch( Point* pt, const Rect* src, const Rect* dst )
 	const UInt16 srcWidth = src->right - src->left;
 	const UInt16 dstWidth = dst->right - dst->left;
 	
-	pt->v = muldivu_w( pt->v - src->top,  dstHeight, srcHeight ) + dst->top;
-	pt->h = muldivu_w( pt->h - src->left, dstWidth,  srcWidth  ) + dst->left;
+	pt->v = muldivs_w( pt->v - src->top,  dstHeight, srcHeight ) + dst->top;
+	pt->h = muldivs_w( pt->h - src->left, dstWidth,  srcWidth  ) + dst->left;
 }
 
 pascal void MapRect_patch( Rect* r, const Rect* src, const Rect* dst )
@@ -362,13 +362,13 @@ pascal void MapRgn_patch( RgnHandle rgn, const Rect* src, const Rect* dst )
 	{
 		const short v = *p;
 		
-		*p++ = muldivu_w( v - src->top, dstHeight, srcHeight ) + dst->top;
+		*p++ = muldivs_w( v - src->top, dstHeight, srcHeight ) + dst->top;
 		
 		while ( *p != Region_end )
 		{
 			const short h = *p;
 			
-			*p++ = muldivu_w( h - src->left, dstWidth, srcWidth ) + dst->left;
+			*p++ = muldivs_w( h - src->left, dstWidth, srcWidth ) + dst->left;
 		}
 		
 		++p;  // skip end-of-row sentinel