801e6fe0f7ac58e75be67e257e0157a559bb31f4
Josh Juran <jjuran@metamage.com>
2025-12-23 10:52:03 -0500

    OrganConsole:  Draw two manuals, showing real keys
    That is, the manuals contain all the keys that function, and only those.

--- apps/mac/OrganConsole/window.cc
+++ apps/mac/OrganConsole/window.cc
@@ -24,11 +24,16 @@
 
 // mac-qd-utils
 #include "mac_qd/get_portRect.hh"
+#include "mac_qd/globals/patterns.hh"
 #include "mac_qd/main_display_bounds.hh"
 
 
 #pragma exceptions off
 
+
+const short n_keys_wide = 13;  // A3 through F5 in upper manual
+
+static short key_width;
 
 static
 Rect get_window_bounds()
@@ -41,8 +46,16 @@ Rect get_window_bounds()
 	short desk_height = bounds.bottom - bounds.top;
 	short desk_width  = bounds.right - bounds.left;
 	
-	short v_margin = desk_height / 4u;
-	short h_margin = desk_width  / 4u;
+	key_width = desk_width < n_keys_wide * 32 ? 20  // fits 320x240
+	          : desk_width < n_keys_wide * 64 ? 32  // fits 512x342
+	          :                                 48;
+	
+	short window_width = key_width * n_keys_wide;
+	
+	short window_height = window_width * 11 / 16u;
+	
+	short v_margin = (desk_height - window_height) / 2u;
+	short h_margin = (desk_width  - window_width)  / 2u;
 	
 	bounds.top += v_margin;
 	bounds.left += h_margin;
@@ -98,40 +111,81 @@ static inline
 void draw_white_keys( const Rect& bounds )
 {
 	const short manual_width = bounds.right - bounds.left;
+	const short padded_width = manual_width * 16 / n_keys_wide;
+	
+	short bottom = bounds.bottom;
+	short midway = bounds.bottom / 2u;
 	
 	Rect key_rect = bounds;
 	
-	for ( int i = 1;  i <= 8;  ++i )
+	key_rect.right = padded_width / 16u;
+	
+	for ( int i = 0;  i < n_keys_wide;  ++i )
 	{
-		key_rect.right = manual_width * i / 8u;
-		
 		FrameRect( &key_rect );
 		
-		key_rect.left = key_rect.right;
+		key_rect.left   = key_rect.right;
+		key_rect.right += key_width;
 	}
+	
+	key_rect.top   = midway;
+	key_rect.left  = 0;
+	key_rect.right = padded_width / 16u;
+	
+	FillRect( &key_rect, &mac::qd::ltGray() );
+	FrameRect( &key_rect );
+	
+	key_rect.left  = padded_width * 11 / 16u;
+	key_rect.right = bounds.right;
+	
+	FillRect( &key_rect, &mac::qd::ltGray() );
+	FrameRect( &key_rect );
+	
+	MoveTo( 0, key_rect.top );
+	Line  ( manual_width, 0 );
 }
 
 static inline
 void draw_black_keys( const Rect& bounds )
 {
 	const short manual_width = bounds.right - bounds.left;
+	const short padded_width = manual_width * 16 / n_keys_wide;
 	
 	Rect key_rect = bounds;
 	
-	key_rect.bottom  = bounds.bottom * 5 / 8u;
-	key_rect.left   += manual_width / 16u;
+	key_rect.bottom = bounds.bottom * 5 / 8u / 2;
+	key_rect.left   = padded_width / 32u;
 	
-	for ( int i = 1;  i < 7;  ++i )
+	for ( int i = 1;  i <= 11;  ++i )
 	{
-		if ( i == 3 )
+		if ( i == 2  ||  i == 5  ||  i == 9 )
 		{
 			continue;
 		}
 		
-		const short edge = manual_width * i / 8u;
+		const short edge = padded_width * i / 16u;
 		
-		key_rect.left  = edge - manual_width / 32u;
-		key_rect.right = edge + manual_width / 32u;
+		key_rect.left  = edge - padded_width / 64u;
+		key_rect.right = edge + padded_width / 64u;
+		
+		PaintRect( &key_rect );
+	}
+	
+	OffsetRect( &key_rect, 0, bounds.bottom / 2u );
+	
+	key_rect.left = padded_width / 32u;
+	
+	for ( int i = 3;  i <= 10;  ++i )
+	{
+		if ( i == 5  ||  i == 9 )
+		{
+			continue;
+		}
+		
+		const short edge = padded_width * i / 16u;
+		
+		key_rect.left  = edge - padded_width / 64u;
+		key_rect.right = edge + padded_width / 64u;
 		
 		PaintRect( &key_rect );
 	}
