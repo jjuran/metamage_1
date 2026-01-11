jjuran/metamage_1

commit 6a1a1d67ed179c8205c12f4b039ef92e65afc082

ams-common:  'SICN'-fix large_icon_key() predicate
Menu items with a 'SICN' resource for the icon were being misreported as
having a large icon.  A new MDEF-0 build is required to draw those items
with the correct height, and a new ams-ui is needed to properly refigure
the menu size when swapping between a large icon and a 'SICN' (if ever).

68k/ams/common/menu_iterator.hh
@@ -7,6 +7,12 @@
 #define MENUITERATOR_HH
 
 
+enum
+{
+	kMenuItemKey_icon_reduction = 0x1D,
+	kMenuItemKey_icon_from_SICN = 0x1E,
+};
+
 struct MenuInfo;
 
 typedef unsigned char Byte;
@@ -14,15 +20,9 @@ typedef unsigned char Byte;
 typedef MenuInfo** MenuRef;
 
 inline
-bool small_icon_key( Byte key )
-{
-	return key == 0x1D;
-}
-
-inline
 bool large_icon_key( Byte key )
 {
-	return ! small_icon_key( key );
+	return key - 0x1D > 1;  // neither $1D nor $1E
 }
 
 class menu_iterator

commit f052ea239376c72965396250997797a862b081d6

ams-common:  Fix large_icon_key() predicate
We ran afoul of C/C++'s automatic integer type promotion.  The result of
(key - 0x1D) we were comparing to 1 was an an int, not an unsigned char.
So a key of 0x00 would yield -0x1D (rather than 0xE3), failing the test.

We could still cast the subtraction result back to (Byte), but we'll use
a bit shift instead -- 0x1D and 0x1E subtract to 0 and 1, shifting to 0,
meaning false.  Any other value yields true.  (It's smaller code, even.)

This fixes the regression from 6a1a1d67ed179c8205c12f4b039ef92e65afc082.

68k/ams/common/menu_iterator.hh
@@ -22,7 +22,7 @@ typedef MenuInfo** MenuRef;
 inline
 bool large_icon_key( Byte key )
 {
-	return key - 0x1D > 1;  // neither $1D nor $1E
+	return key - 0x1D >> 1;  // neither $1D nor $1E
 }
 
 class menu_iterator