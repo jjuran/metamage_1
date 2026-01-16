jjuran/metamage_1

commit 266bd0ca17736518fa3e38520aeec932588f8783

Tic-tac-toe -> mac-qd-utils:  Move CGContextForPort
Also, split it into CGContext_for_port (which takes a port argument) and
a derived class CGContext_for_thePort (which doesn't require arguments).

mac/libs/mac-qd-utils/mac_qd/CGContext_for_port.hh
@@ -1,0 +1,66 @@
+/*
+	CGContext_for_port.hh
+	---------------------
+*/
+
+#ifndef MACQD_CGCONTEXTFORPORT_HH
+#define MACQD_CGCONTEXTFORPORT_HH
+
+// Mac OS X
+#ifdef __APPLE__
+#include <ApplicationServices/ApplicationServices.h>
+#endif
+
+// Mac OS
+#ifndef CGCONTEXT_H_
+#ifndef __CGCONTEXT__
+#include <CGContext.h>
+#endif
+#endif
+#ifndef __QUICKDRAW__
+#include <Quickdraw.h>
+#endif
+
+// missing-macos
+#ifdef MAC_OS_X_VERSION_10_7
+#ifndef MISSING_QUICKDRAW_H
+#include "missing/Quickdraw.h"
+#endif
+#endif
+
+// iota
+#include "iota/class.hh"
+
+
+namespace mac {
+namespace qd  {
+
+class CGContext_for_port
+{
+	NON_COPYABLE( CGContext_for_port )
+	
+	private:
+		CGContextRef context;
+		CGRect       bounds_rect;
+	
+	public:
+		CGContext_for_port( CGrafPtr port );
+		~CGContext_for_port();
+		
+		operator CGContextRef() const  { return context; }
+		
+		const CGRect& bounds() const  { return bounds_rect; }
+};
+
+class CGContext_for_thePort : public CGContext_for_port
+{
+	public:
+		CGContext_for_thePort() : CGContext_for_port( GetQDGlobalsThePort() )
+		{
+		}
+};
+
+}
+}
+
+#endif

apps/mac/Tic-tac-toe/CGContextForPort.hh
@@ -1,42 +1,0 @@
-/*
-	CGContextForPort.hh
-	-------------------
-*/
-
-#ifndef TICTACTOE_CGCONTEXTFORPORT_HH
-#define TICTACTOE_CGCONTEXTFORPORT_HH
-
-// Mac OS X
-#ifdef __APPLE__
-#include <ApplicationServices/ApplicationServices.h>
-#endif
-
-// Mac OS
-#ifndef CGCONTEXT_H_
-#ifndef __CGCONTEXT__
-#include <CGContext.h>
-#endif
-#endif
-
-// iota
-#include "iota/class.hh"
-
-
-class CGContextForPort
-{
-	NON_COPYABLE( CGContextForPort )
-	
-	private:
-		CGContextRef context;
-		CGRect       bounds_rect;
-	
-	public:
-		CGContextForPort();
-		~CGContextForPort();
-		
-		operator CGContextRef() const  { return context; }
-		
-		const CGRect& bounds() const  { return bounds_rect; }
-};
-
-#endif

apps/mac/Tic-tac-toe/window.cc
@@ -13,6 +13,7 @@
 #endif
 
 // mac-qd-utils
+#include "mac_qd/CGContext_for_port.hh"
 #include "mac_qd/get_portRect.hh"
 #include "mac_qd/globals/arrow.hh"
 #include "mac_qd/globals/thePort_window.hh"
@@ -28,7 +29,6 @@
 #include "CGTictactoe.hh"
 
 // Tic-tac-toe
-#include "CGContextForPort.hh"
 #include "cursors.hh"
 #include "dock_tile.hh"
 #include "menus.hh"
@@ -127,10 +127,12 @@ void draw_window( const Rect& portRect )
 {
 	if ( CONFIG_USE_COREGRAPHICS )
 	{
+		using mac::qd::CGContext_for_thePort;
+		
 		float white_or_black =   fullscreen::in_effect;
 		float black_or_white = ! fullscreen::in_effect;
 		
-		CGContextForPort port_context;
+		CGContext_for_thePort port_context;
 		
 		CGContextSetGrayStrokeColor( port_context, white_or_black, 1 );
 		CGContextSetGrayFillColor  ( port_context, black_or_white, 1 );
@@ -213,9 +215,11 @@ void draw_token( player_t token, short index )
 	
 	if ( CONFIG_USE_COREGRAPHICS )
 	{
+		using mac::qd::CGContext_for_thePort;
+		
 		float white_or_black = fullscreen::in_effect;
 		
-		CGContextForPort port_context;
+		CGContext_for_thePort port_context;
 		
 		CGContextTranslateCTM( port_context, margin.h, margin.v );
 		CGContextScaleCTM( port_context, unitLength, unitLength );

mac/libs/mac-qd-utils/mac_qd/CGContext_for_port.cc
@@ -1,21 +1,21 @@
 /*
-	CGContextForPort.cc
-	-------------------
+	CGContext_for_port.cc
+	---------------------
 */
 
-#include "CGContextForPort.hh"
+#include "mac_qd/CGContext_for_port.hh"
 
 // mac-qd-utils
 #include "mac_qd/get_portRect.hh"
 
 
-using mac::qd::get_portRect;
+namespace mac {
+namespace qd  {
 
+#if ! __LP64__
 
-CGContextForPort::CGContextForPort()
+CGContext_for_port::CGContext_for_port( CGrafPtr port )
 {
-	CGrafPtr port = GetQDGlobalsThePort();
-	
 	CreateCGContextForPort( port, &context );
 	
 	const Rect& portRect = get_portRect( port );
@@ -31,8 +31,13 @@ CGContextForPort::CGContextForPort()
 	CGContextScaleCTM    ( context, 1, -1 );
 }
 
-CGContextForPort::~CGContextForPort()
+#endif
+
+CGContext_for_port::~CGContext_for_port()
 {
 	CGContextFlush  ( context );  // required in Mac OS X 10.4
 	CGContextRelease( context );
+}
+
+}
 }

commit b89024f782a30a8efec538898bafc36e365f184c

mac-qd-utils:  Fix 10.7+ builds sans missing-macos
Checking out the macward-compat repository should be generally optional.

This fixes the regression from 266bd0ca17736518fa3e38520aeec932588f8783.

mac/libs/mac-qd-utils/mac_qd/CGContext_for_port.hh
@@ -22,9 +22,11 @@
 #endif
 
 // missing-macos
+#if ! __LP64__
 #ifdef MAC_OS_X_VERSION_10_7
 #ifndef MISSING_QUICKDRAW_H
 #include "missing/Quickdraw.h"
+#endif
 #endif
 #endif
 