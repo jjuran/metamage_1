d0652ba58127e71308792a01e231dd2f295b0b29
Josh Juran <jjuran@metamage.com>
2026-01-20 16:47:01 -0500

    VanlandinghamTuneup:  Raise version to 1.5

--- mac/hacks/VanlandinghamTuneup/Rez/version.h
+++ mac/hacks/VanlandinghamTuneup/Rez/version.h
@@ -1,9 +1,9 @@
 #define COPYRIGHT  "2024-2026 Josh Juran"
 
-#define VERSION  "1.0"
+#define VERSION  "1.5"
 
 #define MAJOR  1
-#define MINOR  0
+#define MINOR  5
 #define POINT  0
 #define STAGE  release
 #define PATCH  0
