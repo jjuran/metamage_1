09b0e2706d0514bfd63a2142ef5cd317f8c5e45e
Josh Juran <jjuran@metamage.com>
2026-01-24 17:15:18 -0500

    Carbonate:  Axe never-used Files unit

--- mac/compat/Carbonate/Carbonate/Processes.cc
+++ mac/compat/Carbonate/Carbonate/Processes.cc
@@ -7,12 +7,6 @@
 
 #if !TARGET_API_MAC_CARBON
 
-#if ACCESSOR_CALLS_ARE_FUNCTIONS
-// Compile the Carbon accessors as extern pascal functions.
-#define CARBONATE_LINKAGE pascal
-//#include "Carbonate/Files.hh"
-#endif
-
 // These functions are always declared in the headers and are always extern.
 
 OSStatus GetProcessBundleLocation( const ProcessSerialNumber* psn, FSRef* result )
