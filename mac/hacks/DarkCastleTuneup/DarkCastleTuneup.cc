10a369859e6179d815a63b02557a67ad8cf86c9a
Josh Juran <jjuran@metamage.com>
2026-02-03 18:57:30 -0500

    [NEW] DarkCastleTuneupHelper <- DarkCastleTuneup
    Factor the bulk of DarkCastleTuneup into a new header-only project.

--- mac/hacks/DarkCastleTuneup/DarkCastleTuneup.cc
+++ mac/hacks/DarkCastleTuneup/DarkCastleTuneup.cc
@@ -6,183 +6,23 @@
 	
 	Copyright 2020-2026, Joshua Juran.  All rights reserved.
 	
-	License:  AGPLv3+ (see bottom for legal boilerplate)
-	
 	This is a set of application hot patches for Dark Castle.
 	Note that the Dark Castle application is actually a shim
 	that launches "Data A", which is the real application.
 	Also note that both contain the 'DCAS' owner resource,
 	although only Data A has 'CODE' id=3 or id=4.
 	
-	As part of its environment check at launch, Data A peeks into
-	I/O space at $f80030 for the string "DEADFACEBEEFCAFE".  In
-	practice, whether that string exists at that location or not
-	is irrelevant, because it only matters if Data A is launched
-	with a non-negative CurPageOption -- but Dark Castle doesn't
-	do that, at least not in configurations this author is aware
-	of.  In any case, the presence of the string presumably signals
-	some kind of debugging environment which doesn't apply to real
-	Macs nor to Advanced Mac Substitute.
-	
-	What does apply to Advanced Mac Substitute is access to unmapped
-	memory regions, of which this signature check definitely is one.
-	This patch NOPs out the JSR instruction, preventing the access.
-	
-	Dark Castle has exceptionally high CPU usage in the title
-	screen (~80%) and during gameplay (100%) due to spinlooping.
-	The spinloop has an inner loop that executes 6 NOPs and
-	increments a counter that is never read anywhere else.
-	
-	To mitigate this, we patch the outer loop branch offset to
-	return directly to the preceding TST.B instruction, skipping
-	the inner loop.  The resulting code matches a pattern that
-	xv68k recognizes and further patches into a custom A-trap
-	invocation.  The trap handler uses the event reactor to wait
-	for the loop exit condition without dominating the CPU.
-	
 */
 
-// Mac OS
-#ifndef __RESOURCES__
-#include <Resources.h>
-#endif
+#define IDENTIFYING_RESTYPE  'DCAS'
 
-// mac-glue-utils
-#include "mac_glue/Memory.hh"
+#define ENVCHECK_CODE_RESID  3
+#define SPINLOOP_CODE_RESID  4
 
-// mac-sys-utils
-#include "mac_sys/trap_address.hh"
+#define ENVCHECK_JSR  0x4EAD0072
 
+#define OFFSET_TO_ENVCHECK_JSR  0x0496
+#define OFFSET_TO_SPINLOOP_BNE  0x074a
 
-static UniversalProcPtr old_TEInit;
-
-
-static inline
-void install_envcheck_patch( Handle h, Size handle_size )
-{
-	enum
-	{
-		// These are offsets relative to the start of the 'CODE' resource.
-		
-		offset_to_JSR       = 0x0496,
-		minimum_handle_size = offset_to_JSR + 4,
-	};
-	
-	if ( handle_size > minimum_handle_size )
-	{
-		UInt32* p = (UInt32*) (*h + offset_to_JSR);
-		
-		/*
-			Old:
-				JSR      (114,A5)
-			
-			New:
-				NOP
-				NOP
-		*/
-		
-		if ( *p == 0x4EAD0072 )
-		{
-			*p = 0x4E714E71;
-			
-			HNoPurge( h );
-		}
-	}
-}
-
-static inline
-void install_spinloop_patch( Handle h, Size handle_size )
-{
-	enum
-	{
-		// These are offsets relative to the start of the 'CODE' resource.
-		
-		offset_to_BNE       = 0x074a,
-		minimum_handle_size = offset_to_BNE + 2,
-	};
-	
-	if ( handle_size > minimum_handle_size )
-	{
-		UInt16* p = (UInt16*) (*h + offset_to_BNE);
-		
-		/*
-			Old:
-				BNE.S    *-18    // $000738
-			
-			New:
-				BNE.S    *-4     // $000746
-		*/
-		
-		if ( *p == 0x66EC )
-		{
-			*p = 0x66FA;
-			
-			HNoPurge( h );
-		}
-	}
-}
-
-static
-void TEInit_handler()
-{
-	using mac::glue::GetHandleSize_raw;
-	
-	if ( Handle h = GetResource( 'DCAS', 0 ) )
-	{
-		ReleaseResource( h );
-		
-		if ( (h = GetResource( 'CODE', 3 )) )
-		{
-			install_envcheck_patch( h, GetHandleSize_raw( h ) );
-		}
-		
-		if ( (h = GetResource( 'CODE', 4 )) )
-		{
-			install_spinloop_patch( h, GetHandleSize_raw( h ) );
-		}
-	}
-}
-
-static
-pascal asm void TEInit_patch()
-{
-	LINK     A6,#0
-	JSR      TEInit_handler
-	UNLK     A6
-	
-	MOVEA.L  old_TEInit,A0
-	JMP      (A0)
-}
-
-int main()
-{
-	enum
-	{
-		_TEInit = 0xA9CC,
-	};
-	
-	Handle self = GetResource( 'INIT', 0 );
-	
-	DetachResource( self );
-	
-	old_TEInit = mac::sys::get_trap_address( _TEInit );
-	
-	mac::sys::set_trap_address( (ProcPtr) TEInit_patch, _TEInit );
-	
-	return 0;
-}
-
-/*
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
+// DarkCastleTuneupHelper
+#include "DarkCastleTuneupHelper.cc.hh"
---
+++ mac/hacks/DarkCastleTuneupHelper/DarkCastleTuneupHelper.cc.hh
@@ -0,0 +1,190 @@
+/*
+	DarkCastleTuneupHelper.cc.hh
+	----------------------------
+	
+	Dark Castle Tune-up INIT template for Advanced Mac Substitute
+	
+	Copyright 2020-2026, Joshua Juran.  All rights reserved.
+	
+	License:  AGPLv3+ (see bottom for legal boilerplate)
+	
+	This is a set of application hot patches for Dark Castle
+	and Beyond Dark Castle.  The two games share a great deal
+	of code (with similar sets of compatibility issues with
+	Advanced Mac Substitute), but in different locations.
+	
+	The subsequent comments refer to "Dark Castle", but apply
+	equally to both Dark Castle and Beyond Dark Castle.
+	
+	As part of its environment check at launch, Data A peeks into
+	I/O space at $f80030 for the string "DEADFACEBEEFCAFE".  In
+	practice, whether that string exists at that location or not
+	is irrelevant, because it only matters if Data A is launched
+	with a non-negative CurPageOption -- but Dark Castle doesn't
+	do that, at least not in configurations this author is aware
+	of.  In any case, the presence of the string presumably signals
+	some kind of debugging environment which doesn't apply to real
+	Macs nor to Advanced Mac Substitute.
+	
+	What does apply to Advanced Mac Substitute is access to unmapped
+	memory regions, of which this signature check definitely is one.
+	This patch NOPs out the JSR instruction, preventing the access.
+	
+	Dark Castle has exceptionally high CPU usage in the title
+	screen (~80%) and during gameplay (100%) due to spinlooping.
+	The spinloop has an inner loop that executes 6 NOPs and
+	increments a counter that is never read anywhere else.
+	
+	To mitigate this, we patch the outer loop branch offset to
+	return directly to the preceding TST.B instruction, skipping
+	the inner loop.  The resulting code matches a pattern that
+	xv68k recognizes and further patches into a custom A-trap
+	invocation.  The trap handler uses the event reactor to wait
+	for the loop exit condition without dominating the CPU.
+	
+*/
+
+// Mac OS
+#ifndef __RESOURCES__
+#include <Resources.h>
+#endif
+
+// mac-glue-utils
+#include "mac_glue/Memory.hh"
+
+// mac-sys-utils
+#include "mac_sys/trap_address.hh"
+
+
+static UniversalProcPtr old_TEInit;
+
+
+static inline
+void install_envcheck_patch( Handle h, Size handle_size )
+{
+	enum
+	{
+		// These are offsets relative to the start of the 'CODE' resource.
+		
+		offset_to_JSR       = OFFSET_TO_ENVCHECK_JSR,
+		minimum_handle_size = offset_to_JSR + 4,
+	};
+	
+	if ( handle_size > minimum_handle_size )
+	{
+		UInt32* p = (UInt32*) (*h + offset_to_JSR);
+		
+		/*
+			Old:
+				JSR      (114,A5)
+			
+			New:
+				NOP
+				NOP
+		*/
+		
+		if ( *p == ENVCHECK_JSR )
+		{
+			*p = 0x4E714E71;
+			
+			HNoPurge( h );
+		}
+	}
+}
+
+static inline
+void install_spinloop_patch( Handle h, Size handle_size )
+{
+	enum
+	{
+		// These are offsets relative to the start of the 'CODE' resource.
+		
+		offset_to_BNE       = OFFSET_TO_SPINLOOP_BNE,
+		minimum_handle_size = offset_to_BNE + 2,
+	};
+	
+	if ( handle_size > minimum_handle_size )
+	{
+		UInt16* p = (UInt16*) (*h + offset_to_BNE);
+		
+		/*
+			Old:
+				BNE.S    *-18    // $000738
+			
+			New:
+				BNE.S    *-4     // $000746
+		*/
+		
+		if ( *p == 0x66EC )
+		{
+			*p = 0x66FA;
+			
+			HNoPurge( h );
+		}
+	}
+}
+
+static
+void TEInit_handler()
+{
+	using mac::glue::GetHandleSize_raw;
+	
+	if ( Handle h = GetResource( IDENTIFYING_RESTYPE, 0 ) )
+	{
+		ReleaseResource( h );
+		
+		if ( (h = GetResource( 'CODE', ENVCHECK_CODE_RESID )) )
+		{
+			install_envcheck_patch( h, GetHandleSize_raw( h ) );
+		}
+		
+		if ( (h = GetResource( 'CODE', SPINLOOP_CODE_RESID )) )
+		{
+			install_spinloop_patch( h, GetHandleSize_raw( h ) );
+		}
+	}
+}
+
+static
+pascal asm void TEInit_patch()
+{
+	LINK     A6,#0
+	JSR      TEInit_handler
+	UNLK     A6
+	
+	MOVEA.L  old_TEInit,A0
+	JMP      (A0)
+}
+
+int main()
+{
+	enum
+	{
+		_TEInit = 0xA9CC,
+	};
+	
+	Handle self = GetResource( 'INIT', 0 );
+	
+	DetachResource( self );
+	
+	old_TEInit = mac::sys::get_trap_address( _TEInit );
+	
+	mac::sys::set_trap_address( (ProcPtr) TEInit_patch, _TEInit );
+	
+	return 0;
+}
+
+/*
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
