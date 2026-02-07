90610b5c61fdc651235d4bef57c50396fc861ff4
Josh Juran <jjuran@metamage.com>
2026-02-06 22:29:43 -0500

    Amaretto:  Pace updates to avoid thrash under load
    The fireplace animation of Beyond Dark Castle's menu screen gets blitted
    directly to the screen, resulting in a blistering rate of screen updates
    that brings Amaretto to its metaphorical knees, consuming first 130% CPU
    (on the machine where it was tested), and soon 145% with inputs becoming
    unresponsive.  As in c91c181d1b4e27b940c8f0dc3b61772e81802690, sleep for
    a bit before checking for updates to avoid overwhelming the main thread.

--- apps/mac/Amaretto/raster_task.mm
+++ apps/mac/Amaretto/raster_task.mm
@@ -56,6 +56,8 @@ void raster_event_loop( const clut_data* clut, const sync_relay* sync )
 		using frend::cursor_state;
 		using frend::wait_for_update;
 		
+		usleep( 500 );
+		
 		wait_for_update();
 		
 		if ( cursor_state  &&  cursor_state->seed != cursor_seed )
