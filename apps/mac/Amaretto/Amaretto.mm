jjuran/metamage_1

commit 599c9ba3df685dbc9e3405e1dfb40bc0621dbaa1

Amaretto:  Signal main thread via CFRunLoopSource
Apple documents that performSelectorOnMainThread... isn't an appropriate
tool for high-frequency, low-latency tolerances (like graphics updates).
Aggressive throttling helped mitigate the worst, but we had to raise the
threshold in order to accommodate drawing complex (but static) pictures.

This fixes the regression from 987f2861f2dce374952209d05456397bd9e4bd3b,
at least with respect to Amaretto.  Pearl will need its own fix, though.

apps/mac/Amaretto/Amaretto.mm
@@ -74,6 +74,45 @@ void sigchld( int )
 	frend::unblock_update_waiters();
 }
 
+static
+void on_raster_event( void* info )
+{
+	AmarettoAppDelegate* appDelegate = (AmarettoAppDelegate*) info;
+	
+	if ( raster_events.cursorBits )
+	{
+		raster_events.cursorBits = false;
+		
+		[appDelegate onCursorBits];
+	}
+	
+	if ( raster_events.newPalette )
+	{
+		raster_events.newPalette = false;
+		
+		[appDelegate onNewPalette];
+	}
+	
+	if ( raster_events.screenBits )
+	{
+		raster_events.screenBits = false;
+		
+		[appDelegate onScreenBits];
+	}
+	
+	if ( raster_events.repaintDue )
+	{
+		raster_events.repaintDue = false;
+		
+		[appDelegate onRepaintDue];
+	}
+	
+	if ( raster_events.rasterDone )
+	{
+		[appDelegate onRasterDone];
+	}
+}
+
 int main( int argc, char** argv )
 {
 	releasing pool( [NSAutoreleasePool new] );
@@ -118,7 +157,24 @@ int main( int argc, char** argv )
 		
 		releasing appDelegate( [[AmarettoAppDelegate alloc] initWithRaster: load] );
 		
-		raster_event_target = appDelegate;
+		CFRunLoopSourceContext context =
+		{
+			0,
+			appDelegate,
+			NULL,  // retain
+			NULL,  // release
+			NULL,  // copyDescription
+			NULL,  // equal
+			NULL,  // hash
+			NULL,  // schedule
+			NULL,  // cancel
+			&on_raster_event,
+		};
+		
+		mainRunLoop = CFRunLoopGetCurrent();
+		inputSource = CFRunLoopSourceCreate( NULL, 0, &context );
+		
+		CFRunLoopAddSource( mainRunLoop, inputSource, kCFRunLoopDefaultMode );
 		
 		raster_updating   update_fifo;
 		raster_monitor    monitored_raster( live_raster.get() );

apps/mac/Amaretto/raster_task.hh
@@ -20,6 +20,15 @@ namespace raster
 	
 }
 
+struct raster_event_set
+{
+	bool screenBits;
+	bool cursorBits;
+	bool newPalette;
+	bool repaintDue;
+	bool rasterDone;
+};
+
 class raster_monitor
 {
 	NON_COPYABLE( raster_monitor )
@@ -30,6 +39,9 @@ class raster_monitor
 		~raster_monitor();
 };
 
-extern id raster_event_target;
+extern raster_event_set raster_events;
+
+extern CFRunLoopRef        mainRunLoop;
+extern CFRunLoopSourceRef  inputSource;
 
 #endif

apps/mac/Amaretto/raster_task.mm
@@ -34,14 +34,17 @@ static bool monitoring;
 
 static poseven::thread raster_thread;
 
-id raster_event_target = nil;
+raster_event_set raster_events;
+
+CFRunLoopRef        mainRunLoop;
+CFRunLoopSourceRef  inputSource;
 
 static
-void post( SEL selector )
+void signal_runloop()
 {
-	[raster_event_target performSelectorOnMainThread: selector
-	                     withObject:                  nil
-	                     waitUntilDone:               NO];
+	CFRunLoopSourceSignal( inputSource );
+	
+	CFRunLoopWakeUp( mainRunLoop );
 }
 
 static
@@ -62,29 +65,33 @@ void raster_event_loop( const clut_data* clut, const sync_relay* sync )
 		{
 			cursor_seed = cursor_state->seed;
 			
-			post( @selector(onCursorBits) );
+			raster_events.cursorBits = true;
 		}
 		
 		if ( clut  &&  clut->seed != colors_seed )
 		{
 			colors_seed = clut->seed;
 			
-			post( @selector(onNewPalette) );
+			raster_events.newPalette = true;
 		}
 		
 		if ( raster_seed != sync->seed )
 		{
 			raster_seed = sync->seed;
 			
-			post( @selector(onScreenBits) );
+			raster_events.screenBits = true;
 		}
 		
-		post( @selector(onRepaintDue) );
+		raster_events.repaintDue = true;
+		
+		signal_runloop();
 		
 		usleep( sleep );
 	}
 	
-	post( @selector(onRasterDone) );
+	raster_events.rasterDone = true;
+	
+	signal_runloop();
 }
 
 static

commit 9028c60f38a4faa6fe41758f5ae7f5f7c2ba7403

Amaretto:  Update the screen while selecting menus
This fixes the regression from 599c9ba3df685dbc9e3405e1dfb40bc0621dbaa1.

apps/mac/Amaretto/Amaretto.mm
@@ -220,7 +220,7 @@ int main( int argc, char** argv )
 		mainRunLoop = CFRunLoopGetCurrent();
 		inputSource = CFRunLoopSourceCreate( NULL, 0, &context );
 		
-		CFRunLoopAddSource( mainRunLoop, inputSource, kCFRunLoopDefaultMode );
+		CFRunLoopAddSource( mainRunLoop, inputSource, kCFRunLoopCommonModes );
 		
 		raster_updating   update_fifo;
 		raster_monitor    monitored_raster( live_raster.get() );