73afe8951982114a3c25d5999376a366669c3238
Josh Juran <jjuran@metamage.com>
2026-03-26 22:08:45 -0400

    Amethyst:  Use display_task, custom display_events
    This actually increases CPU use very slightly (regardless of which of QD
    or CG is used for the blitter), but this is outweighed by a reduction in
    duplicate logic, especially considering that Amethyst isn't recommended.

--- apps/mac/Amethyst/Amethyst.cc
+++ apps/mac/Amethyst/Amethyst.cc
@@ -3,11 +3,16 @@
 	-----------
 */
 
+// frontend-common
+#include "frend/display_events.hh"
+
 // Amethyst
 #include "blit_CG.hh"
 #include "blit_QD.hh"
 #include "quickdraw.hh"
 
+
+#define ON_DISPLAY_EVENT  &on_display_event
 
 #if CONFIG_QUICKDRAW
 	typedef QD_blitter Blitter;
@@ -16,5 +21,44 @@
 #endif
 
 static bool overlay_enabled;
+
+static
+void on_display_event( void* info )
+{
+	using frend::display_event_set;
+	
+	display_event_set& events = *(display_event_set*) info;
+	
+	if ( events.newPalette )
+	{
+		events.newPalette = false;
+		
+		if ( const unsigned short* palette = events.clut_palette )
+		{
+			Blitter::set_palette( palette, events.clut_maximum + 1 );
+			
+			/*
+				We need Blitter::blit() to be called, since
+				that's where transcoding samples the palette.
+			*/
+			
+			events.screenBits = true;
+		}
+	}
+	
+	if ( events.screenBits )
+	{
+		events.screenBits = false;
+		
+		Blitter::blit( (char*) events.addr );
+	}
+	
+	events.repaintDue = false;
+	
+	if ( events.rasterDone  &&  events.finish )
+	{
+		events.finish();
+	}
+}
 
 #include "amnesty/skeleton.cc.hh"
