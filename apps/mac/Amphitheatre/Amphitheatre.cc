7f8ddc1395cd4b5feffd55218967e812d175ca15
Josh Juran <jjuran@metamage.com>
2026-03-26 22:04:23 -0400

    Amphitheatre:  Switch to display_task with glitter
    This doesn't provide nearly the same windfall performance benefit as the
    prior transitions in Amaretto and Amber, but it is a slight improvement.
    While the bottleneck we had observed in Amber clearly must have occurred
    within RunApplicationEventLoop(), it's not the Carbon events themselves.

--- apps/mac/Amphitheatre/Amphitheatre.cc
+++ apps/mac/Amphitheatre/Amphitheatre.cc
@@ -6,9 +6,14 @@
 // glfb-common
 #include "glfb/glfb.hh"
 
+// glitter
+#include "glitter/display_events.hh"
+
 // Amphitheatre
 #include "blit_CGL.hh"
 
+
+#define ON_DISPLAY_EVENT  &::glitter::on_display_event
 
 #define HARDWARE_CURSOR 1
 
