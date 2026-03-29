jjuran/metamage_1

commit 4d79841e7be5de3504593b0d64db185f0fd87ce4

ams-ui:  Rank DialogItem fields over 'CNTL' fields
For dialog controls defined using a 'CNTL' resource, defer to the 'DITL'
(dialog item list) resource with respect to any fields existing in both,
specifically the bounds and the title (with our interest in the bounds).

This fixes the errant scroll bar in TaskMaker's Product Information box.

68k/modules/ams-ui/Dialogs.cc
@@ -462,6 +462,18 @@ Handle new_dialog_control( WindowRef window, DialogItem* item )
 	
 	const partial_CNTL_template* subcntl = &default_NewControl_arguments;
 	
+	if ( (item->type & 0x7F) == ctrlItem + resCtrl )
+	{
+		if ( Handle h = GetResource( 'CNTL', item_ResID( item ) ) )
+		{
+			const ControlTemplate* cntl = (const ControlTemplate*) *h;
+			
+			subcntl = (const partial_CNTL_template*) &cntl->controlValue;
+			
+			procID = subcntl->procID;
+		}
+	}
+	
 	Handle h = (Handle) NewControl( window,
 	                                &item->bounds,
 	                                &item->length,
@@ -558,11 +570,8 @@ pascal DialogRef NewDialog_patch( void*                 storage,
 			case ctrlItem + btnCtrl:
 			case ctrlItem + chkCtrl:
 			case ctrlItem + radCtrl:
-				item->handle = new_dialog_control( window, item );
-				break;
-			
 			case ctrlItem + resCtrl:
-				item->handle = (Handle) GetNewControl( item_ResID( item ), window );
+				item->handle = new_dialog_control( window, item );
 				break;
 			
 			case editText:

commit 32a921665a4d7324a640c6b1391b1f831f217fb9

ams-ui:  Prefer control title via 'CNTL' to 'DITL'
If a control is defined by a 'CNTL' resource, take its title from there,
not the dialog item list.  This fixes World Builder alert button titles.

This fixes the regression from 4d79841e7be5de3504593b0d64db185f0fd87ce4.

68k/modules/ams-ui/Dialogs.cc
@@ -459,6 +459,7 @@ struct partial_CNTL_template
 	short    min;
 	short    procID;
 	long     refCon;
+	Byte     length;   // all we need of the title, replacing it
 //	Str255   title;    // not appearing in this struct
 };
 
@@ -471,12 +472,15 @@ static const partial_CNTL_template default_NewControl_arguments =
 	0,     // min
 	0,     // (procID, not used)
 	0,     // refcon
+	0,     // empty string
 };
 
 static
 Handle new_dialog_control( WindowRef window, DialogItem* item )
 {
 	short procID = item->type & 0x03;
+	
+	const Byte* title = &item->length;
 	
 	const partial_CNTL_template* subcntl = &default_NewControl_arguments;
 	
@@ -488,13 +492,14 @@ Handle new_dialog_control( WindowRef window, DialogItem* item )
 			
 			subcntl = (const partial_CNTL_template*) &cntl->controlValue;
 			
+			title = &subcntl->length;
 			procID = subcntl->procID;
 		}
 	}
 	
 	Handle h = (Handle) NewControl( window,
 	                                &item->bounds,
-	                                &item->length,
+	                                title,
 	                                subcntl->visible,
 	                                subcntl->value,
 	                                subcntl->min,