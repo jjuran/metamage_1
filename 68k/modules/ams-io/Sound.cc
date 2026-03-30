jjuran/metamage_1

commit 3fd9fcc6eec153ac40d70b5a0b8d90a8c5a5ec74

ams-io:  Set volume for .Sound Control csCode=2
Set SdVolume in low memory (for applications that examine it) and update
VIA Register A (which alters the actual audio output level in sndtrack).

This lets Dark Castle set the volume level when outside of 64K ROM mode.

68k/modules/ams-io/Sound.cc
@@ -35,7 +35,16 @@
 #define STR_LEN( s )  "" s, (sizeof s - 1)
 
 
+enum
+{
+	kSoundSetVolume = 2,
+};
+
+Byte SdVolume : 0x0260;
+
 DCtlEntry* SoundDCE : 0x027A;
+
+Byte VIA_Reg_A : 0x00effffe;
 
 const short noQueueMask   = 1 << noQueueBit;
 const short asyncTrapMask = 1 << asyncTrpBit;
@@ -408,6 +417,26 @@ done:
 	return err;
 }
 
+static inline
+OSErr set_sound_volume( UInt16 level )
+{
+	if ( level > 7 )
+	{
+		return paramErr;
+	}
+	
+	SdVolume = level;
+	
+	Byte reg_A = VIA_Reg_A;
+	
+	reg_A &= 0xf8;
+	reg_A |= level;
+	
+	VIA_Reg_A = reg_A;
+	
+	return noErr;
+}
+
 short Sound_control( short trap : __D1, CntrlParam* pb : __A0, DCE* dce : __A1 )
 {
 	OSErr err = noErr;
@@ -420,6 +449,9 @@ short Sound_control( short trap : __D1, CntrlParam* pb : __A0, DCE* dce : __A1 )
 			cancel_timer();
 			
 			return pb->ioResult = noErr;
+		
+		case kSoundSetVolume:
+			return pb->ioResult = set_sound_volume( pb->csParam[ 0 ] );
 		
 		default:
 			err = controlErr;

commit 55c8cdb31a484dd54a311f413027069b9039c900

ams-io:  Fix non-immed kSoundSetVolume call
We were returning instead of calling IODone(), even for non-immed calls.
This was causing an impossible-to-debug Address Error exception in World
Builder 1.2 runtimes (occurring only if specifying a 128K ROM or later).

This fixes the regression from 3fd9fcc6eec153ac40d70b5a0b8d90a8c5a5ec74.

68k/modules/ams-io/Sound.cc
@@ -451,7 +451,8 @@ short Sound_control( short trap : __D1, CntrlParam* pb : __A0, DCE* dce : __A1 )
 			return pb->ioResult = noErr;
 		
 		case kSoundSetVolume:
-			return pb->ioResult = set_sound_volume( pb->csParam[ 0 ] );
+			err = set_sound_volume( pb->csParam[ 0 ] );
+			break;
 		
 		default:
 			err = controlErr;