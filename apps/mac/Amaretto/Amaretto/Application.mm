73555baac6b00b911568e59867932d6cb9b22d18
Josh Juran <jjuran@metamage.com>
2026-05-22 18:26:59 -0400

    Amaretto:  Reroute mouse events when cursor pinned
    While the cursor is pinned, skip the usual view-based event dispatching.

--- apps/mac/Amaretto/Amaretto/Application.mm
+++ apps/mac/Amaretto/Amaretto/Application.mm
@@ -5,6 +5,31 @@
 
 #include "Amaretto/Application.h"
 
+// frontend-common
+#include "frend/cursor.hh"
+
+// amicus
+#include "amicus/events.hh"
+
+
+static
+BOOL is_mouse_event( NSEventType type )
+{
+	switch ( type )
+	{
+		case NSLeftMouseDown:
+		case NSLeftMouseUp:
+		case NSRightMouseDown:
+		case NSRightMouseUp:
+		case NSMouseMoved:
+		case NSLeftMouseDragged:
+		case NSRightMouseDragged:
+			return true;
+		
+		default:
+			return false;
+	}
+}
 
 @implementation AmarettoApplication
 
@@ -20,6 +45,10 @@
 	if ( type == NSKeyUp  &&  [event modifierFlags] & NSCommandKeyMask )
 	{
 		[[self keyWindow] sendEvent: event];
+	}
+	else if ( frend::cursor_pinned  &&  is_mouse_event( type ) )
+	{
+		amicus::handle_CGEvent( [event CGEvent] );
 	}
 	else
 	{
