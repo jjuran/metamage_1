fae531b5ca92591f81f821236950d5c88dcc1c98
Josh Juran <jjuran@metamage.com>
2026-06-22 11:13:40 -0400

    ams-core:  Implement ReadLocation() in ReadXPRam()
    For the moment, the ReadLocation() callout just zeroes the given buffer,
    rendering this change a behavioral no-op, but it lays the groundwork for
    reading and writing Parameter RAM generally, and location in particular.

--- 68k/modules/ams-core/PRAM.cc
+++ 68k/modules/ams-core/PRAM.cc
@@ -9,9 +9,35 @@
 #include "callouts.hh"
 
 
+enum
+{
+	location_begin = 0xE4,
+	location_end   = 0xE4 + 12,  // $F0
+};
+
+static unsigned char xpram[ 256 ];
+
 pascal void ReadXPRam_patch( void* buffer : __A0, long length_offset : __D0 )
 {
+	short offset = length_offset;
 	short length = length_offset >> 16;
 	
 	fast_memset( buffer, '\0', length );
+	
+	if ( offset >= 256 )
+	{
+		return;
+	}
+	
+	if ( length > 256 - offset )
+	{
+		length = 256 - offset;
+	}
+	
+	if ( offset < location_end  &&  offset + length > location_begin )
+	{
+		ReadLocation( (MachineLocation*) &xpram[ location_begin ] );
+		
+		fast_memcpy( buffer, xpram + offset, length );
+	}
 }
