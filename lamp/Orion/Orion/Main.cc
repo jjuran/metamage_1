7153d93c0d20ae25950524e6579dcf0f0176c48b
Josh Juran <jjuran@metamage.com>
2026-06-23 07:40:11 -0400

    Orion:  Axe the namespace, silencing a warning
    Clang 21 complains about main() being declared `extern "C"`.

--- lamp/Orion/Orion/Main.cc
+++ lamp/Orion/Orion/Main.cc
@@ -29,103 +29,96 @@
 #include "poseven/types/gai_error.hh"
 
 
-namespace Orion
+namespace p7 = poseven;
+
+
+static void ShowDebuggingContext( const void* stack_bottom_limit )
 {
+#if CONFIG_DEBUGGING
 	
-	namespace p7 = poseven;
+	using namespace recall;
 	
-	extern "C" int main( int argc, char** argv );
-	
-	
-	static void ShowDebuggingContext( const void* stack_bottom_limit )
+	try
 	{
-	#if CONFIG_DEBUGGING
+		throw;
+	}
+	catch ( const debugging_context& debugging )
+	{
+		const unsigned size = debugging.get_stack_crawl_size();
 		
-		using namespace recall;
-		
-		try
+		if ( size < 2 )
 		{
-			throw;
-		}
-		catch ( const debugging_context& debugging )
-		{
-			const unsigned size = debugging.get_stack_crawl_size();
-			
-			if ( size < 2 )
-			{
-				return;
-			}
-			
-			const frame_data* begin = debugging.get_stack_crawl_data();
-			
-			const frame_data* end = begin + size;
-			
-			++begin;  // skip recall::debugging_context::debugging_context( void )
-			
-			if ( begin->frame_pointer < stack_bottom_limit )
-			{
-				const frame_data* last = end - 1;
-				
-				while ( last->frame_pointer > stack_bottom_limit )
-				{
-					--last;
-				}
-				
-				end = last + 1;
-			}
-			
-			plus::var_string report;
-			
-			make_report_from_stack_crawl( report, begin, end );
-			
-			p7::write( p7::stderr_fileno, report );
-			
 			return;
 		}
-		catch ( ... )
+		
+		const frame_data* begin = debugging.get_stack_crawl_data();
+		
+		const frame_data* end = begin + size;
+		
+		++begin;  // skip recall::debugging_context::debugging_context( void )
+		
+		if ( begin->frame_pointer < stack_bottom_limit )
 		{
+			const frame_data* last = end - 1;
+			
+			while ( last->frame_pointer > stack_bottom_limit )
+			{
+				--last;
+			}
+			
+			end = last + 1;
 		}
 		
-	#endif
+		plus::var_string report;
+		
+		make_report_from_stack_crawl( report, begin, end );
+		
+		p7::write( p7::stderr_fileno, report );
+		
+		return;
 	}
-	
-	
-	int main( int argc, char** argv )
+	catch ( ... )
 	{
-		const void* stackBottom = recall::get_stack_frame_pointer();
-		
-		try
-		{
-			return tool::Main( argc, argv );
-		}
-		catch ( const p7::exit_t& status )
-		{
-			return status;
-		}
-		catch ( const p7::gai_error& err )
-		{
-			p7::perror( argv[0], gai_strerror( err ) );
-		}
-		catch ( const p7::errno_t& err )
-		{
-			p7::perror( argv[0], "exception" );
-			
-			ShowDebuggingContext( stackBottom );
-		}
-		catch ( const std::exception& e )
-		{
-			p7::perror( argv[0], "exception", e.what() );
-			
-			ShowDebuggingContext( stackBottom );
-		}
-		catch ( ... )
-		{
-			p7::perror( argv[0], "uncaught exception" );
-			
-			ShowDebuggingContext( stackBottom );
-		}
-		
-		return 1;  // EXIT_FAILURE
 	}
 	
+#endif
+}
+
+
+int main( int argc, char** argv )
+{
+	const void* stackBottom = recall::get_stack_frame_pointer();
+	
+	try
+	{
+		return tool::Main( argc, argv );
+	}
+	catch ( const p7::exit_t& status )
+	{
+		return status;
+	}
+	catch ( const p7::gai_error& err )
+	{
+		p7::perror( argv[0], gai_strerror( err ) );
+	}
+	catch ( const p7::errno_t& err )
+	{
+		p7::perror( argv[0], "exception" );
+		
+		ShowDebuggingContext( stackBottom );
+	}
+	catch ( const std::exception& e )
+	{
+		p7::perror( argv[0], "exception", e.what() );
+		
+		ShowDebuggingContext( stackBottom );
+	}
+	catch ( ... )
+	{
+		p7::perror( argv[0], "uncaught exception" );
+		
+		ShowDebuggingContext( stackBottom );
+	}
+	
+	return 1;  // EXIT_FAILURE
 }
