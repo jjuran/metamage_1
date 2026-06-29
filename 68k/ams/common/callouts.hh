jjuran/metamage_1

commit 61339a5d24fba9dd752977f344f5c131818c4341

ams-common:  Define ReadLocation() callout

68k/ams/common/callouts.hh
@@ -205,6 +205,11 @@ DEFINE_CALLOUT( long,
                   unsigned     c1   : __D2 ),
                 0xFF86 )
 
+DEFINE_CALLOUT( void,
+                ReadLocation,
+                ( struct MachineLocation* dst : __A0 ),
+                0xFF76 )
+
 #undef DEFINE_CALLOUT
 
 inline

commit 9f4300c3bc6780204364dc3463586ec552300467

ams-common, ams-core:  Fix callout name conflict
This fixes the regression from 61339a5d24fba9dd752977f344f5c131818c4341.

68k/ams/common/callouts.hh
@@ -206,7 +206,7 @@ DEFINE_CALLOUT( long,
                 0xFF86 )
 
 DEFINE_CALLOUT( void,
-                ReadLocation,
+                read_location,
                 ( struct MachineLocation* dst : __A0 ),
                 0xFF76 )
 

68k/modules/ams-core/PRAM.cc
@@ -36,7 +36,7 @@ pascal void ReadXPRam_patch( void* buffer : __A0, long length_offset : __D0 )
 	
 	if ( offset < location_end  &&  offset + length > location_begin )
 	{
-		ReadLocation( (MachineLocation*) &xpram[ location_begin ] );
+		read_location( (MachineLocation*) &xpram[ location_begin ] );
 		
 		fast_memcpy( buffer, xpram + offset, length );
 	}