jjuran/metamage_1

commit af968d12baef27b61642137d013e95a07f96023a

Amaretto:  Spawn coprocess from the app event loop
Don't launch the emulator until after we've called the application's run
method.  This is necessary for refactoring our handling of Apple events.

We also transfer the cursor_limit assignment to limit include footprint.

apps/mac/Amaretto/Amaretto.mm
@@ -28,7 +28,6 @@
 #include "glfb/glfb.hh"
 
 // frontend-common
-#include "frend/coprocess.hh"
 #include "frend/cursor.hh"
 #include "frend/display_events.hh"
 #include "frend/displayfs.hh"
@@ -49,7 +48,6 @@
 // amicus
 #include "amicus/apple_events.hh"
 #include "amicus/display_task.hh"
-#include "amicus/events.hh"
 
 // Amaretto
 #include "Amaretto/AppDelegate.hh"
@@ -74,16 +72,13 @@ using frend::raster_path;
 
 using frend::display_events;
 
-using frend::coprocess_launch;
 using frend::cursor_lifetime;
 using frend::raster_lifetime;
 using frend::raster_updating;
 
 using glitter::on_display_event;
 
-using amicus::cursor_limit;
 using amicus::display_monitor;
-using amicus::events_fd;
 using amicus::wait_for_first_Apple_event;
 
 
@@ -153,19 +148,10 @@ int main( int argc, char** argv )
 		
 		raster_updating   update_fifo;
 		display_monitor   monitored_display( live_raster.get(), perform );
-		coprocess_launch  launched_coprocess( bindir_fd, works_path );
-		
-		events_fd = launched_coprocess.socket();
-		
-		close( bindir_fd );
-		
-		cursor_limit = CGPointMake( desc.width - 1, desc.height - 1 );
 		
 		[NSApp setDelegate: appDelegate];
 		
 		[NSApp run];
-		
-		close( events_fd );
 	}
 	
 	rmdir( works_path );

apps/mac/Amaretto/Amaretto/AppDelegate.mm
@@ -5,6 +5,9 @@
 
 #include "Amaretto/AppDelegate.hh"
 
+// POSIX
+#include <unistd.h>
+
 // mac-config
 #include "mac_config/upp-macros.hh"
 
@@ -15,6 +18,7 @@
 #include "splode/write-a-splode.hh"
 
 // frontend-common
+#include "frend/coprocess.hh"
 #include "frend/zoom.hh"
 
 // rasterlib
@@ -25,6 +29,7 @@
 
 // amicus
 #include "amicus/cursor.hh"
+#include "amicus/events.hh"
 #include "amicus/menu_defs.hh"
 #include "amicus/zoom.hh"
 
@@ -36,17 +41,34 @@
 using mac::app::quit;
 
 using frend::cap_zoom_index;
+using frend::coprocess_state;
 using frend::current_zoom_index;
+using frend::launch_coprocess;
 using frend::maximum_zoom_index;
+using frend::wait_for_coprocess;
 using frend::Zoom_index_2_0;
 
 using amicus::command_ID_for_zoom_index;
+using amicus::cursor_limit;
+using amicus::events_fd;
 using amicus::top_zoom_index;
 
+
+static coprocess_state coprocess;
 
 int bindir_fd;
 
 const char* works_path;
+
+static
+void launch_coprocess()
+{
+	coprocess = launch_coprocess( bindir_fd, works_path );
+	
+	events_fd = coprocess.socket_fd;
+	
+	close( bindir_fd );
+}
 
 static
 pascal OSErr handle_Quit_Apple_event( AppleEvent const* event,
@@ -223,6 +245,8 @@ NSMenu* set_up_menus( unsigned default_zoom_command )
 	
 	cap_zoom_index( desc.width, desc.height, space.width, space.height );
 	
+	cursor_limit = CGPointMake( desc.width - 1, desc.height - 1 );
+	
 	_desc = &desc;
 	
 	_zoomLevel = kZoom100Percent;
@@ -232,6 +256,13 @@ NSMenu* set_up_menus( unsigned default_zoom_command )
 
 - (void) dealloc
 {
+	close( events_fd );
+	
+	if ( int pid = coprocess.child_pid )
+	{
+		wait_for_coprocess( pid );
+	}
+	
 	[_mainWindow release];
 	
 	glfb::terminate();
@@ -344,6 +375,8 @@ NSMenu* set_up_menus( unsigned default_zoom_command )
 	pt.y -= frame.origin.y;
 	
 	[_mainGLView handleMouseMovedTo: pt];
+	
+	launch_coprocess();
 }
 
 @end

commit 5758bd8bdf040781d45222058009582b1b6223d2

Amaretto:  Fix thread-join deadlock on quit
Release the application delegate (which in turn closes events_fd) before
entering the display_monitor destructor, which joins a thread that won't
exit until the back end terminates (which happens when events shut off).

This fixes the regression from af968d12baef27b61642137d013e95a07f96023a.

apps/mac/Amaretto/Amaretto.mm
@@ -146,12 +146,12 @@ int main( int argc, char** argv )
 		
 		set_palette( load );
 		
-		releasing appDelegate( [[AmarettoAppDelegate alloc] initWithRaster: load] );
-		
 		display_monitor::perform_proc perform = &on_display_event;
 		
 		raster_updating   update_fifo;
 		display_monitor   monitored_display( live_raster.get(), perform );
+		
+		releasing appDelegate( [[AmarettoAppDelegate alloc] initWithRaster: load] );
 		
 		[NSApp setDelegate: appDelegate];
 		