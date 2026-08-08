76663e1908c34e8b96ce5ba62454e09f8e559023
Josh Juran <jjuran@metamage.com>
2026-08-07 21:35:15 -0400

    Vertico:  Add new app icon / version resources

---
+++ apps/mac/Vertico/Rez/version.h
@@ -0,0 +1,11 @@
+#define COPYRIGHT  "2006-2026 Josh Juran"
+
+#define CREATOR  'Vrtc'
+
+#define VERSION  "0.4.0"
+
+#define MAJOR  0
+#define MINOR  4
+#define POINT  0
+#define STAGE  release
+#define PATCH  0
