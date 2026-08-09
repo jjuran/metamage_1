c5d17c9e02cbbc6eb96765689f513172afcc5873
Josh Juran <jjuran@metamage.com>
2026-08-09 13:02:42 -0400

    Genie:  Raise version to 0.8.0-alpha.2

--- lamp/Genie/Rez/version.h
+++ lamp/Genie/Rez/version.h
@@ -1,13 +1,13 @@
 #define APP_NAME "MacRelix"
 
-#define COPYRIGHT  "1999-2025 Josh Juran"
+#define COPYRIGHT  "1999-2026 Josh Juran"
 
 #define CREATOR  'Poof'
 
-#define VERSION  "0.7.9-alpha.4"
+#define VERSION  "0.8.0-alpha.2"
 
 #define MAJOR  0
-#define MINOR  7
-#define POINT  9
+#define MINOR  8
+#define POINT  0
 #define STAGE  alpha
-#define PATCH  4
+#define PATCH  2
