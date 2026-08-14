jjuran/metamage_1

commit 882009acfd9c139fbabe9b03f4f2cdd16dad37fb

Amaretto:  Don't rely on app delegate dealloc call
Recent versions of macOS leak the app delegate, so dealloc isn't called.

This fixes the regression from af968d12baef27b61642137d013e95a07f96023a,
making partial fix 5758bd8bdf040781d45222058009582b1b6223d2 superfluous.

apps/mac/Amaretto/Amaretto.mm
@@ -151,11 +151,23 @@ int main( int argc, char** argv )
 		raster_updating   update_fifo;
 		display_monitor   monitored_display( live_raster.get(), perform );
 		
+		/*
+			Create and install the application delegate object.
+			
+			After run returns, we explicitly destruct it -- we
+			have to use a custom method instead of dealloc (as
+			we once did) because macOS 11 (if not earlier) and
+			later leak the object, preventing the retain count
+			from reaching zero, and it never gets deallocated.
+		*/
+		
 		releasing appDelegate( [[AmarettoAppDelegate alloc] initWithRaster: load] );
 		
 		[NSApp setDelegate: appDelegate];
 		
 		[NSApp run];
+		
+		[appDelegate destruct];
 	}
 	
 	rmdir( works_path );

apps/mac/Amaretto/Amaretto/AppDelegate.hh
@@ -32,7 +32,7 @@ extern const char* works_path;
 
 - (id) initWithRaster: (const raster::raster_load&) load;
 
-- (void) dealloc;
+- (void) destruct;
 
 - (void) setCursorPinning: (BOOL) pinning;
 

apps/mac/Amaretto/Amaretto/AppDelegate.mm
@@ -304,7 +304,7 @@ NSMenu* set_up_menus( unsigned default_zoom_command )
 	return self;
 }
 
-- (void) dealloc
+- (void) destruct
 {
 	close( events_fd );
 	
@@ -321,8 +321,6 @@ NSMenu* set_up_menus( unsigned default_zoom_command )
 	[_mainWindow release];
 	
 	glfb::terminate();
-	
-	[super dealloc];
 }
 
 - (void) setCursorPinning: (BOOL) pinning

commit 1f65b90deb5cb252ff2ede99747a28ce46b9d441

Amaretto:  Silence "invalid receiver" warning
GCC 4.0.1 complained about this; GCC 4.2.1 did not.

This fixes the regression from 882009acfd9c139fbabe9b03f4f2cdd16dad37fb.

apps/mac/Amaretto/Amaretto.mm
@@ -167,7 +167,7 @@ int main( int argc, char** argv )
 		
 		[NSApp run];
 		
-		[appDelegate destruct];
+		[appDelegate.get() destruct];
 	}
 	
 	rmdir( works_path );

apps/mac/Amaretto/releasing.hh
@@ -30,6 +30,7 @@ class releasing
 			[_object release];
 		}
 		
+		id get()      const  { return _object; }
 		operator id() const  { return _object; }
 };
 