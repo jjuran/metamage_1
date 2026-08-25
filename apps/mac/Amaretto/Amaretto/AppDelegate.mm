jjuran/metamage_1

commit 453c5b3f5f7604c0509cd7cf843caf6104888049

Amaretto:  Don't yank cursor into window on launch
Don't sync the cursor on launch if it's visible.  If the cursor isn't in
the window, leave it where it is.  If it is, then we already updated the
guest cursor to match, so synching back to the guest would be redundant.

apps/mac/Amaretto/Amaretto/AppDelegate.mm
@@ -371,18 +371,21 @@ NSMenu* set_up_menus( unsigned default_zoom_command )
 		because it stays within the window.)
 	*/
 	
-	cursor_pinned = pinning;
-	
-	if ( pinning )
+	if ( pinning != cursor_pinned )
 	{
-		ignore_next_mouse_moved_event = true;
+		cursor_pinned = pinning;
 		
-		set_cursor_hidden( true );
+		if ( pinning )
+		{
+			ignore_next_mouse_moved_event = true;
+			
+			set_cursor_hidden( true );
+		}
+		
+		synchronize_cursor_location( _mainGLView );
+		
+		CGAssociateMouseAndMouseCursorPosition( ! pinning );
 	}
-	
-	synchronize_cursor_location( _mainGLView );
-	
-	CGAssociateMouseAndMouseCursorPosition( ! pinning );
 }
 
 - (void) doZoom: (long) commandID

commit 84f9b4cd54d828e2f09d2cea3d68e4f4a1c37c6d

Amaretto:  Fix another 10.9 cursor unhiding bug
This time, the host cursor began hidden but showed once the mouse moved.

This fixes the regression from 453c5b3f5f7604c0509cd7cf843caf6104888049.

apps/mac/Amaretto/Amaretto/AppDelegate.mm
@@ -105,8 +105,8 @@ void synchronize_cursor_location( NSView* view )
 	
 	/*
 		Observed in OS X 10.9.5 on a 2013 Retina MacBook Pro with
-		Nvidia graphics, but not in OS X 10.9.5 on a Mac Mini with
-		Intel graphics (nor anywhere else):
+		Nvidia graphics, but not in OS X 10.9.5 on a 2012 Mac Mini
+		with Intel graphics (nor anywhere else):
 		
 		The CGWarpMouseCursorPosition() call above cancels the
 		cursor hiding.  Compensate by turning it off and on again.
@@ -396,6 +396,7 @@ NSMenu* set_up_menus( unsigned default_zoom_command )
 
 - (void) setCursorPinning: (BOOL) pinning
 {
+	using amicus::cursor_hidden;
 	using amicus::ignore_next_mouse_moved_event;
 	using amicus::set_cursor_hidden;
 	
@@ -419,6 +420,10 @@ NSMenu* set_up_menus( unsigned default_zoom_command )
 		synchronize_cursor_location( _mainGLView );
 		
 		CGAssociateMouseAndMouseCursorPosition( ! pinning );
+	}
+	else if ( cursor_hidden )
+	{
+		synchronize_cursor_location( _mainGLView );
 	}
 }
 