d85c83d773f5cfe5ebf2edbac733773d6f346e26
Josh Juran <jjuran@metamage.com>
2026-08-25 18:19:27 -0400

    Tic-tac-toe:  Raise classic version to 0.4.1

--- apps/mac/Tic-tac-toe/Rez/version.h
+++ apps/mac/Tic-tac-toe/Rez/version.h
@@ -1,11 +1,11 @@
-#define COPYRIGHT  "2018-2025 Josh Juran"
+#define COPYRIGHT  "2018-2026 Josh Juran"
 
 #define CREATOR  'XvO#'
 
-#define VERSION  "0.4.0"
+#define VERSION  "0.4.1"
 
 #define MAJOR  0
 #define MINOR  4
-#define POINT  0
+#define POINT  1
 #define STAGE  release
 #define PATCH  0
